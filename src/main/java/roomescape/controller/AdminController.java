4d762c099cbd23ef7770bcdad9f856c1b5a44ea5
Nam Gi Beom <rlqja0523@naver.com>
2024-04-16 15:13:19 +0900

    feat: 예약 조회 end point 추가
    Co-authored-by: Minjoo522 <m0522inju@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -1,12 +1,37 @@
 package roomescape.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import roomescape.domain.reservation.Reservation;
+import roomescape.domain.reservation.ReservationRepository;
 
 @Controller
 public class AdminController {
+    private final ReservationRepository reservationRepository;
+
+    @Autowired
+    public AdminController(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
     @GetMapping("/admin")
     public String admin() {
         return "admin/index";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String reservation() {
+        return "admin/reservation-legacy";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> reservations() {
+        return reservationRepository.findAll();
     }
 }

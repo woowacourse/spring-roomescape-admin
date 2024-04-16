ed61f694f94f173be9fdbf1125f4059b13131972
Arachneee <jhg2819@naver.com>
2024-04-16 22:58:37 +0900

    refactor(ReservationRequest): LocalDateTime 변환 로직 수정
    Co-authored-by: hyxrxn <gpfls981220@gmail.com>

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -2,17 +2,17 @@ package roomescape.dto;
 
 import roomescape.domain.Reservation;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
 
 public record ReservationRequest(
         String name,
-        String date,
-        String time
+        LocalDate date,
+        LocalTime time
 ) {
 
     public Reservation toReservation(long id) {
-        LocalDateTime localDateTime = LocalDateTime.parse(date + time, DateTimeFormatter.ofPattern("yyyy-MM-ddHH:mm"));
-        return new Reservation(id, name, localDateTime);
+        return new Reservation(id, name, LocalDateTime.of(date, time));
     }
 }

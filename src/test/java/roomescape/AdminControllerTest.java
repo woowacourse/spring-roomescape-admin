3c6572c3668c7f63e65ba1e2ab1d764e5944de28
hoyeonyy <highoption@naver.com>
2024-04-16 16:41:01 +0900

    feat(AdminController) : root, /admin 구현
    Co-authored-by: Haiseong <wjdgotjd9908@gmail.com>

--- src/test/java/roomescape/AdminControllerTest.java
+++ src/test/java/roomescape/AdminControllerTest.java
@@ -1,20 +1,30 @@
 package roomescape;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+public class AdminControllerTest {
 
     @Test
-    void 일단계() {
+    @DisplayName("root url 을 요청하면 welcome page 를 반환한다.")
+    void requestRootUrl() {
         RestAssured.given().log().all()
                 .when().get("/")
                 .then().log().all()
                 .statusCode(200);
     }
 
+    @Test
+    @DisplayName("/admin 을 요청하면 index.html 를 반환한다.")
+    void requestAdmin() {
+        RestAssured.given().log().all()
+                .when().get("/admin")
+                .then().log().all()
+                .statusCode(200);
+    }
 }

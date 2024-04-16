437720ca5024ae84b633f255f08d71b802d18119
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-04-16 17:34:04 +0900

    test(MissionStepTest): 2단계 테스트 작성
    - 1단계 테스트를 @Nested 사용하여 수정
    
    Co-authored-by: zangsu <zangsu_@naver.com>

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,6 +1,8 @@
 package roomescape;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -9,11 +11,39 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class MissionStepTest {
 
-    @Test
-    void 일단계() {
-        RestAssured.given().log().all()
-                .when().get("/admin")
-                .then().log().all()
-                .statusCode(200);
+    @Nested
+    class 일단계 {
+
+        @Test
+        @DisplayName("메인 페이지 조회")
+        void getMainPageTest() {
+            RestAssured.given().log().all()
+                    .when().get("/admin")
+                    .then().log().all()
+                    .statusCode(200);
+        }
+    }
+
+    @Nested
+    class 이단계 {
+
+        @Test
+        @DisplayName("예약 페이지 조회")
+        void getReservationPageTest() {
+            RestAssured.given().log().all()
+                    .when().get("/admin/reservation")
+                    .then().log().all()
+                    .statusCode(200);
+        }
+
+        @Test
+        @DisplayName("예약 목록 조회")
+        void getReservationsTest() {
+            RestAssured.given().log().all()
+                    .when().get("/reservations")
+                    .then().log().all()
+                    .statusCode(200);
+            // TODO: 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
+        }
     }
 }

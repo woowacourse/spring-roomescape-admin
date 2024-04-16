00a0f8acff68da37b39b97c09a3a0637fd031f8c
libienz <leekh10195@gmail.com>
2024-04-16 21:15:43 +0900

    feat: 예약 삭제 기능 구현
    Co-authored-by: SeongjuLee <sherlocky31415@gmail.com>

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -4,9 +4,13 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,7 +37,7 @@ public class ReservationController {
     @PostMapping("/reservations")
     @ResponseBody
     public Reservation createReservation(@RequestBody ReservationDto reservationDto) {
-        long id = index.incrementAndGet();
+        long id = index.getAndIncrement();
         String name = reservationDto.getName();
         LocalDate date = reservationDto.getDate();
         LocalTime time = reservationDto.getTime();
@@ -41,5 +45,15 @@ public class ReservationController {
         Reservation newReservation = new Reservation(id, name, date, time);
         reservations.add(newReservation);
         return newReservation;
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") long id) {
+        Reservation findReservation = reservations.stream()
+                .filter(reservation -> reservation.getId() == id)
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException("id에 해당하는 예약을 찾을 수 없습니다: " + id));
+        reservations.remove(findReservation);
+        return ResponseEntity.ok().build();
     }
 }

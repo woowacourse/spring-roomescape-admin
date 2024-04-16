859fca36cae28570ff99d908ee5e1d1edfc102d7
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-04-16 17:34:31 +0900

    feat: 웰컴페이지를 어드민 메인 페이지로 응답한다.

--- src/main/java/roomescape/AdminController.java
+++ src/main/java/roomescape/AdminController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
+    @GetMapping("/")
+    public String enterWellComePage() {
+        return "admin/index";
+    }
+
     @GetMapping("/admin")
     public String enterAdminPage() {
         return "admin/index";

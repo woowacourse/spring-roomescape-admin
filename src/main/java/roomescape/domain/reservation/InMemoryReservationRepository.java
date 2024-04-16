374207813aade785df2ba970704d66295aba9e00
Nam Gi Beom <rlqja0523@naver.com>
2024-04-16 16:06:14 +0900

    feat: 메서드 시그니처, id autoincrement 추가
    Co-authored-by: Minjoo522 <m0522inju@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

--- src/main/java/roomescape/domain/reservation/InMemoryReservationRepository.java
+++ src/main/java/roomescape/domain/reservation/InMemoryReservationRepository.java
@@ -4,16 +4,20 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class InMemoryReservationRepository implements ReservationRepository {
+    private AtomicLong index = new AtomicLong(1L);
     private final Map<Long, Reservation> reservations = new HashMap<>();
 
     @Override
-    public void save(Reservation reservation) {
-        reservations.put(reservation.getId(), reservation);
+    public Reservation save(Reservation reservation) {
+        Reservation newReservation = new Reservation(index.getAndIncrement(), reservation);
+        reservations.put(newReservation.getId(), newReservation);
+        return newReservation;
     }
 
     @Override
--- src/main/java/roomescape/domain/reservation/ReservationRepository.java
+++ src/main/java/roomescape/domain/reservation/ReservationRepository.java
@@ -3,7 +3,7 @@ package roomescape.domain.reservation;
 import java.util.List;
 
 public interface ReservationRepository {
-    void save(Reservation reservation);
+    Reservation save(Reservation reservation);
 
     List<Reservation> findAll();
 }

d34afbd9922e4bf3d407d0bd409b381b5d55485f
donghoony <aru0504@naver.com>
2024-04-16 15:24:26 +0900

    feat : 홈 화면 응답
    Co-authored-by: ikjo39 <chi4321@naver.com>

---
+++ src/main/java/roomescape/controller/AdminController.java
@@ -0,0 +1,13 @@
+package roomescape.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String landHomePage() {
+        return "admin/index";
+    }
+}

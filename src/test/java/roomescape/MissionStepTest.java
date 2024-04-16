2ef38dc2df2c28c05217bf2762069a396a74f1ec
seunghye218 <seunghye@student.42seoul.kr>
2024-04-16 17:10:06 +0900

    feat: 어드민 메인 페이지를 응답한다

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -12,9 +12,8 @@ public class MissionStepTest {
     @Test
     void 일단계() {
         RestAssured.given().log().all()
-                .when().get("/")
+                .when().get("/admin")
                 .then().log().all()
                 .statusCode(200);
     }
-
 }

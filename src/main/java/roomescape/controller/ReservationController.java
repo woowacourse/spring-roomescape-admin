3e855740a468546d26a7f298bc9e346a936bc341
devhoya97 <diajbh123@gmail.com>
2024-04-16 18:04:31 +0900

    feat: 예약 삭제 API 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,13 +1,11 @@
 package roomescape.controller;
 
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import roomescape.domain.Reservation;
 import roomescape.dto.ReservationCreateDto;
 import roomescape.dto.ReservationDto;
@@ -18,9 +16,9 @@ import java.util.List;
 @Controller
 public class ReservationController {
 
-    private AtomicLong index = new AtomicLong(0);
+    private final AtomicLong index = new AtomicLong(0);
 
-    private List<Reservation> reservations = new ArrayList<>();
+    private final List<Reservation> reservations = new ArrayList<>();
 
     @GetMapping("/admin")
     public String getAdminPage() {
@@ -29,14 +27,14 @@ public class ReservationController {
 
     @GetMapping("/admin/reservation")
     public String getReservationPage(Model model) {
-        List<ReservationDto> reservationDtos = reservations.stream().map(reservation -> ReservationDto.fromEntity(reservation)).toList();
+        List<ReservationDto> reservationDtos = reservations.stream().map(ReservationDto::fromEntity).toList();
         model.addAttribute("reservationDtos", reservationDtos);
         return "/admin/reservation-legacy";
     }
 
     @GetMapping("/reservations")
     public ResponseEntity<List<ReservationDto>> getReservations() {
-        List<ReservationDto> reservationDtos = reservations.stream().map(reservation -> ReservationDto.fromEntity(reservation)).toList();
+        List<ReservationDto> reservationDtos = reservations.stream().map(ReservationDto::fromEntity).toList();
         return ResponseEntity.ok(reservationDtos);
     }
 
@@ -46,5 +44,15 @@ public class ReservationController {
         Reservation reservation = reservationCreateDto.toEntity(index.incrementAndGet());
         reservations.add(reservation);
         return ResponseEntity.ok(reservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
+        Reservation found = reservations.stream().filter(reservation -> Objects.equals(reservation.getId(), id))
+                .findFirst().orElseThrow(() -> new IllegalArgumentException(id + "는 존재하지 않습니다."));
+
+        reservations.remove(found);
+        return ResponseEntity.ok().build();
     }
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -50,5 +50,22 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
     }
 }

7f1d3db4c9d216cd71800bf3324c8342c8a9761a
Nam Gi Beom <rlqja0523@naver.com>
2024-04-16 16:20:35 +0900

    feat: reservation delete end point 추가
    Co-authored-by: Minjoo522 <m0522inju@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -3,11 +3,16 @@ package roomescape.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import roomescape.domain.reservation.Reservation;
 import roomescape.domain.reservation.ReservationRepository;
@@ -41,5 +46,11 @@ public class AdminController {
     @ResponseBody
     public Reservation createReservation(@RequestBody Reservation reservation) {
         return reservationRepository.save(reservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable(value = "id") Long id) {
+        reservationRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }

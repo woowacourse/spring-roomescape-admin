0490fcf2192ea0aa2419f0373c8c5b0030bd9f29
juha <khabh@naver.com>
2024-04-16 16:29:59 +0900

    test: 예약 홈 화면 조회 테스트 추가

---
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -0,0 +1,19 @@
+package roomescape.controller;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class AdminControllerTest {
+
+    @DisplayName("홈 화면 조회")
+    @Test
+    void home() {
+        RestAssured.given().log().all()
+                .when().get("/admin")
+                .then().log().all()
+                .statusCode(200);
+    }
+}

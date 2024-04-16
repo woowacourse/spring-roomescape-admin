528a9ae9c451e2d58983b5d3e2550272f2879f63
Nam Gi Beom <rlqja0523@naver.com>
2024-04-16 15:12:58 +0900

    test: 2단계 테스트 추가
    Co-authored-by: Minjoo522 <m0522inju@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,5 +1,7 @@
 package roomescape;
 
+import static org.hamcrest.Matchers.*;
+
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,4 +19,17 @@ public class MissionStepTest {
                 .statusCode(200);
     }
 
+    @Test
+    void 이단계() {
+        RestAssured.given().log().all()
+                .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
 }

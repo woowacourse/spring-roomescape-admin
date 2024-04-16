67454dbb567a2fd3f73bf1c5de349aea2efbcffb
Arachneee <jhg2819@naver.com>
2024-04-16 17:01:55 +0900

    feat(RoomEscapeController): welcome page 리다이렉트
    Co-authored-by: hyxrxn <gpfls981220@gmail.com>

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RoomEscapeController {
 
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/admin";
+    }
+
     @GetMapping("/admin")
     public String admin() {
         return "admin/index";

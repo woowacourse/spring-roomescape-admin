169d3d737f8980fffe1213796df411b55de7b27f
linirini <2001yerin@naver.com>
2024-04-16 16:26:01 +0900

    feat: admin 메인 페이지 접근 기능 구현

---
+++ src/main/java/roomescape/AdminController.java
@@ -0,0 +1,15 @@
+package roomescape;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("")
+    public String findAdminPage() {
+        return "admin/index";
+    }
+}
---
+++ src/test/java/roomescape/AdminControllerTest.java
@@ -0,0 +1,27 @@
+package roomescape;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class AdminControllerTest {
+
+    @DisplayName("Admin Page 홈화면 접근 성공 테스트")
+    @Test
+    void responseAdminPage() {
+        Response response = RestAssured.given().log().all()
+                .when().get("/admin")
+                .then().log().all().extract().response();
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+}

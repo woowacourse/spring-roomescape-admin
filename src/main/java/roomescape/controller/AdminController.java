d48466ab8ea9f4492db3a0738c15d9bf1335c898
J-I-H-O <jeongjiho0731@gmail.com>
2024-04-16 16:37:24 +0900

    feat: admin 홈 화면 구현
    Co-authored-by: shin-jisong <songjin1127@naver.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

---
+++ src/main/java/roomescape/controller/AdminController.java
@@ -0,0 +1,14 @@
+package roomescape.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    // TODO: 메서드 이름 정하기
+    @GetMapping("/admin")
+    public String readAdminPage() {
+        return "admin/index";
+    }
+}

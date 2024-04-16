5e8703630d173d2094e9d0529fa60c2e96dcc264
takoyakimchi <c4nyou@gmail.com>
2024-04-16 17:15:11 +0900

    feat: 예약 삭제 API 구현
    Co-authored-by: cutehumanS2 <oddpinkjadeite@gmail.com>

--- src/main/java/roomescape/controller/RoomescapeController.java
+++ src/main/java/roomescape/controller/RoomescapeController.java
@@ -2,10 +2,13 @@ package roomescape.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -41,5 +44,15 @@ public class RoomescapeController {
             index.getAndIncrement(), request.name(), request.date(), request.time());
         reservations.add(newReservation);
         return newReservation;
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    @ResponseBody
+    public void deleteReservation(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+            .filter(it -> Objects.equals(it.getId(), id))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("[ERROR] 예약 번호가 잘못되었습니다."));
+        reservations.remove(reservation);
     }
 }

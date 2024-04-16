e094e24d4652f58a31374e8a28c515e87e780bf9
hyeonjilee <skylar12200@gmail.com>
2024-04-16 17:04:04 +0900

    feat: 스텝2 구현

---
+++ src/main/java/roomescape/Reservation.java
@@ -0,0 +1,20 @@
+package roomescape;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
+public class Reservation {
+
+    private final Long id;
+    private final String name;
+    private final LocalDate date;
+    private final LocalTime time;
+
+    public Reservation(final Long id, final String name, final LocalDate date, final LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+}

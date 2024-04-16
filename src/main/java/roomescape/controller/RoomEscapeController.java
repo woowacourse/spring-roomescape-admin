12095bd177b7574cf7e38c1dada6e7bba7462057
ay-eonii <naayen@naver.com>
2024-04-16 14:51:39 +0900

    feat(RoomEscapeController): 어드민 조회페이지 응답
    Co-authored-by: jongmee <ra123221@gmail.com>

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -2,12 +2,19 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class RoomEscapeController {
 
-    @GetMapping("/admin")
+    @GetMapping
     public String getAdminPage() {
         return "/admin/index";
+    }
+
+    @GetMapping("/reservation")
+    public String getAdminReservations() {
+        return "/admin/reservation-legacy";
     }
 }

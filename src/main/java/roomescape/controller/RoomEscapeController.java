0ea9eeb915f9b834bb9e49a7468920c46bc47935
Arachneee <jhg2819@naver.com>
2024-04-16 20:00:12 +0900

    feat(RoomEscapeController): 예약 삭제
    Co-authored-by: hyxrxn <gpfls981220@gmail.com>

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -2,7 +2,9 @@ package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import roomescape.domain.Reservation;
@@ -11,6 +13,7 @@ import roomescape.dto.ReservationRequest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
@@ -50,5 +53,17 @@ public class RoomEscapeController {
 
         return ResponseEntity.ok()
                 .body(ReservationDto.from(reservation));
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Reservation findedReservation = reservations.stream()
+                .filter(reservation -> Objects.equals(reservation.getId(), id))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 예약입니다."));
+        reservations.remove(findedReservation);
+
+        return ResponseEntity.ok()
+                .build();
     }
 }

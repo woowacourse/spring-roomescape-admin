a2bb5cbe6d3a36755cef46ebf35eb4c781bb144c
김민수 MINSOO KIM <alstn113@gmail.com>
2024-04-16 19:58:34 +0900

    refactor: 전체 코드 정리

--- src/main/java/roomescape/dto/CreateReservationRequest.java
+++ src/main/java/roomescape/dto/CreateReservationRequest.java
@@ -2,9 +2,9 @@ package roomescape.dto;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import roomescape.Reservation;
+import roomescape.domain.Reservation;
 
-public record ReservationRequest(String name, LocalDate date, LocalTime time) {
+public record CreateReservationRequest(String name, LocalDate date, LocalTime time) {
 
     public Reservation toReservation(final Long id) {
        return new Reservation(id, name, date, time);
---
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -0,0 +1,15 @@
+package roomescape.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import roomescape.domain.Reservation;
+
+public record ReservationResponse(Long id, String name, LocalDate date, LocalTime time) {
+    public static ReservationResponse from(Reservation reservation) {
+        return new ReservationResponse(
+                reservation.getId(),
+                reservation.getName(),
+                reservation.getDate(),
+                reservation.getTime());
+    }
+}

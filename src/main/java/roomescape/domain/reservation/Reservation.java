dd9f180e857e3731f0b73b25a82b001e7d6b2cd1
Nam Gi Beom <rlqja0523@naver.com>
2024-04-16 16:07:57 +0900

    feat: 생성자 추가
    Co-authored-by: Minjoo522 <m0522inju@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

--- src/main/java/roomescape/domain/reservation/Reservation.java
+++ src/main/java/roomescape/domain/reservation/Reservation.java
@@ -1,16 +1,23 @@
 package roomescape.domain.reservation;
 
 public class Reservation {
-    private final Long id;
-    private final String name;
-    private final String date;
-    private final String time;
+    private Long id;
+    private String name;
+    private String date;
+    private String time;
+
+    public Reservation() {
+    }
 
     public Reservation(Long id, String name, String date, String time) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    public Reservation(Long id, Reservation reservation) {
+        this(id, reservation.name, reservation.date, reservation.time);
     }
 
     public Long getId() {

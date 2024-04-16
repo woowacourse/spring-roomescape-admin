aa7f0a1fa8ead72b264feebd45476b9ab5b4c9b8
eun-byeol <joeunbyeol98@gmail.com>
2024-04-16 16:35:57 +0900

    feat: 관리자 메인 페이지 조회 기능 구현
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -7,14 +7,13 @@ import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+class MissionStepTest {
 
     @Test
     void 일단계() {
         RestAssured.given().log().all()
-                .when().get("/")
-                .then().log().all()
-                .statusCode(200);
+            .when().get("/")
+            .then().log().all()
+            .statusCode(200);
     }
-
 }

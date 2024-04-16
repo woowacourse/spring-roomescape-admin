a4611f42ba6a64908598a5a2d91699d79a15676e
hangillee <skfcb10@naver.com>
2024-04-16 16:25:18 +0900

    refactor: HTTP Request와 Response용 DTO로 분리

---
+++ src/main/java/roomescape/dto/ReservationRequestDto.java
@@ -0,0 +1,31 @@
+package roomescape.dto;
+
+public class ReservationRequestDto {
+    private final String date;
+    private final String name;
+    private final String time;
+
+    public ReservationRequestDto() {
+        this.date = null;
+        this.name = null;
+        this.time = null;
+    }
+
+    public ReservationRequestDto(final String date, final String name, final String time) {
+        this.date = date;
+        this.name = name;
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}

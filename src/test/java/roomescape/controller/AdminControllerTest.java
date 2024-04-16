010dcbb22769f61e9079b9022053ef32ca6bcd78
robinjoon <robin980108@naver.com>
2024-04-16 16:20:35 +0900

    feat(AdminController): 관리자 메인페이지 불러오는 기능 구현
    - 메인 페이지의 경로를 매핑하는지 확인하는 단위 테스트 작성
    - 메인 페이지 어드민 접속 확인 통합 테스트 작성
    
    Co-authored-by: jinchiim <kyunellroll@gmail.com>

---
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -0,0 +1,17 @@
+package roomescape.controller;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AdminControllerTest {
+
+    @Test
+    @DisplayName("관리자 메인 페이지 경로를 정해진 경로로 매핑한다.")
+    void mainPage() {
+        AdminController adminController = new AdminController();
+        String mainPage = adminController.mainPage();
+        Assertions.assertThat(mainPage)
+                .isEqualTo("admin/index");
+    }
+}
--- src/test/java/roomescape/integration/AdminIntegrationTest.java
+++ src/test/java/roomescape/integration/AdminIntegrationTest.java
@@ -1,18 +1,20 @@
-package roomescape;
+package roomescape.integration;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+public class AdminIntegrationTest {
 
     @Test
-    void 일단계() {
+    @DisplayName("관리자 메인 페이지가 잘 접속된다.")
+    void adminMainPageLoad() {
         RestAssured.given().log().all()
-                .when().get("/")
+                .when().get("/admin")
                 .then().log().all()
                 .statusCode(200);
     }

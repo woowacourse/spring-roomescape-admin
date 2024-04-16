168ee69e8eeefe67f4619a1783b95d5f0bd27e0c
ay-eonii <naayen@naver.com>
2024-04-16 17:41:05 +0900

    refactor(ReservationRepository): 레포지토리 분리
    Co-authored-by: jongmee <ra123221@gmail.com>

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.*;
 import roomescape.domain.Reservation;
 import roomescape.dto.ReservationResponse;
 import roomescape.dto.ReservationSaveRequest;
+import roomescape.repository.ReservationRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class RoomEscapeController {
-    private final List<Reservation> reservations = new ArrayList<>();
+    private final ReservationRepository reservationRepository = new ReservationRepository();
     private final AtomicLong index = new AtomicLong(1);
 
     @GetMapping("/admin")
@@ -28,7 +28,8 @@ public class RoomEscapeController {
 
     @GetMapping("/reservations")
     public ResponseEntity<List<ReservationResponse>> getReservations() {
-        var reservationResponses = reservations.stream()
+        var reservationResponses = reservationRepository.findAll()
+                .stream()
                 .map(Reservation::toDto)
                 .toList();
         return ResponseEntity.ok(reservationResponses);
@@ -37,19 +38,13 @@ public class RoomEscapeController {
     @PostMapping("/reservations")
     public ResponseEntity<ReservationResponse> createReservation(@RequestBody ReservationSaveRequest request) {
         Reservation reservation = request.toReservation(index.getAndIncrement());
-        reservations.add(reservation);
+        reservationRepository.save(reservation);
         return ResponseEntity.ok(reservation.toDto());
     }
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
-        Reservation target = reservations.stream()
-                .filter(reservation -> reservation.isSameId(id))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 예약이 없습니다."));
-
-        reservations.remove(target);
-
+        reservationRepository.deleteById(id);
         return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -0,0 +1,26 @@
+package roomescape.repository;
+
+import roomescape.domain.Reservation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservationRepository {
+    private final List<Reservation> reservations = new ArrayList<>();
+
+    public void save(Reservation reservation) {
+        reservations.add(reservation);
+    }
+
+    public List<Reservation> findAll() {
+        return new ArrayList<>(reservations);
+    }
+
+    public void deleteById(Long id) {
+        Reservation target = reservations.stream()
+                .filter(reservation -> reservation.isSameId(id))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 예약이 없습니다."));
+        reservations.remove(target);
+    }
+}

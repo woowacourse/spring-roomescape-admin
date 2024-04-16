df24b05b88d5480763969ca3c4bc6e234cb0b0ad
hyun <osh123chm@gmail.com>
2024-04-16 17:19:32 +0900

    feat: 2단계 예약 조회 구현
    Co-authored-by: kyum-q <kyungmi.dev@gmail.com>

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -1,13 +1,30 @@
 package roomescape.controller;
 
+import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import roomescape.domain.Reservation;
 
 @Controller
 public class RoomEscapeController {
+    private List<Reservation> reservations = List.of(
+            new Reservation(1, "test", "2024-04-16", "10:00")
+    );
 
     @GetMapping("/admin")
     public String openAdminPage() {
         return "/admin/index";
     }
+
+    @GetMapping("/admin/reservation")
+    public String openReservationPage() {
+        return "/admin/reservation-legacy";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> readReservations() {
+        return ResponseEntity.ok(reservations);
+    }
+
 }
---
+++ src/main/java/roomescape/domain/Reservation.java
@@ -0,0 +1,31 @@
+package roomescape.domain;
+
+public class Reservation {
+    private long id;
+    private String name;
+    private String date;
+    private String time;
+
+    public Reservation(long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}

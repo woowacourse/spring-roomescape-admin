4f0f86d8d47be88e5bf954ec5b618e4c3db65d1f
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-04-16 21:11:07 +0900

    feat(CustomDateTimeFormatter): LocalDateTime를 포매팅하는 객체 생성
    Co-authored-by: zangsu <zangsu_@naver.com>

---
+++ src/main/java/roomescape/CustomDateTimeFormatter.java
@@ -0,0 +1,24 @@
+package roomescape;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+// TODO: DateTimeFormatter 고민하기
+public class CustomDateTimeFormatter {
+    public static LocalDateTime getLocalDateTime(final String date, final String time) {
+        LocalDate localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalTime localTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
+
+        return LocalDateTime.of(localDate, localTime);
+    }
+
+    public static String getFormattedDate(final LocalDateTime dateTime) {
+        return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    public static String getFormattedTime(final LocalDateTime dateTime) {
+        return dateTime.format(DateTimeFormatter.ofPattern("HH:mm"));
+    }
+}
---
+++ src/test/java/roomescape/CustomDateTimeFormatterTest.java
@@ -0,0 +1,31 @@
+package roomescape;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CustomDateTimeFormatterTest {
+
+    @DisplayName("날짜와 시간 문자열 값을 받아 LocalDateTime 객체를 반환한다.")
+    @Test
+    void getLocalDateTimeTest() {
+        LocalDateTime localDateTime = CustomDateTimeFormatter.getLocalDateTime("2023-08-05", "15:40");
+        assertThat(localDateTime).isEqualTo(LocalDateTime.of(2023, 8, 5, 15, 40));
+    }
+
+    @DisplayName("LocalDateTime 객체로부터 날짜 문자열 값을 반환한다.")
+    @Test
+    void getFormattedDateTest() {
+        String formattedDate = CustomDateTimeFormatter.getFormattedDate(LocalDateTime.of(2023, 8, 5, 15, 40));
+        assertThat(formattedDate).isEqualTo("2023-08-05");
+    }
+
+    @DisplayName("LocalDateTime 객체로부터 시간 문자열 값을 반환한다.")
+    @Test
+    void getFormattedTimeTest() {
+        String formattedDate = CustomDateTimeFormatter.getFormattedTime(LocalDateTime.of(2023, 8, 5, 15, 40));
+        assertThat(formattedDate).isEqualTo("15:40");
+    }
+}

07261bdc693443b577a1144b0846c16501774e89
Nam Gi Beom <rlqja0523@naver.com>
2024-04-17 14:35:55 +0900

    refactor: service 클래스 삭제
    Co-authored-by: Minjoo522 <m0522inju@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,7 +1,6 @@
 package roomescape.controller;
 
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,35 +9,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import roomescape.domain.reservation.Reservation;
-import roomescape.domain.reservation.ReservationService;
+import roomescape.domain.reservation.ReservationRepository;
 import roomescape.dto.ReservationDto;
 
 @Controller
 @RequestMapping("/reservations")
 public class ReservationController {
-    private final ReservationService reservationService;
+    private final ReservationRepository reservationRepository;
 
-    public ReservationController(ReservationService reservationService) {
-        this.reservationService = reservationService;
+    public ReservationController(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
     }
 
     @GetMapping
     public ResponseEntity<List<Reservation>> reservations() {
-        List<Reservation> reservations = reservationService.findReservations();
+        List<Reservation> reservations = reservationRepository.findAll();
         return ResponseEntity.ok(reservations);
     }
 
     @PostMapping
     public ResponseEntity<Reservation> createReservation(@RequestBody ReservationDto reservationDto) {
-        Reservation newReservation = reservationService.create(reservationDto);
+        Reservation newReservation = reservationRepository.save(reservationDto);
         return ResponseEntity.ok(newReservation);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable(value = "id") Long id) {
-        reservationService.cancel(id);
+        reservationRepository.deleteById(id);
         return ResponseEntity.ok().build();
     }
 }

186f45d97936b8e89b35c599165b04538c9ab0e8
BurningFalls <sherlocky31415@gmail.com>
2024-04-17 16:45:40 +0900

    refactor: 테스트 코드 분리 및 네이밍 명시

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,21 +1,22 @@
 package roomescape;
 
-import static org.hamcrest.Matchers.is;
-
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import java.util.HashMap;
-import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
     @Test
-    void 일단계() {
+    void returnSuccessWhenAccessAdminHomePage() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
@@ -23,21 +24,24 @@ public class MissionStepTest {
     }
 
     @Test
-    void 이단계() {
+    void returnSuccessWhenAccessAdminReservationPage() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
+    }
 
+    @Test
+    void shouldReadReservationSuccessfullyWithHeaders() {
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
+                .body("size()", is(0));
     }
 
     @Test
-    void 삼단계() {
+    void shouldCreateAndDeleteReservationSuccessfully() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");

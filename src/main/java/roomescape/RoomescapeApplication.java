98ee57eedc728ec8f1b69b1eeae433bd2d266832
Seokmyung Ham <gkatjraud1@gmail.com>
2024-04-17 20:59:45 +0900

    refactor: 클래스 이름에 포함된 단어를 RoomEscape에서 Roomescape로 수정

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoomEscapeApplication {
+public class RoomescapeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RoomEscapeApplication.class, args);
+        SpringApplication.run(RoomescapeApplication.class, args);
     }
 
 }

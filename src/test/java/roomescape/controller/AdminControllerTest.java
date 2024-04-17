ffbb58769529005c3e9839d53a1a5ac1f7476cbf
robinjoon <robin980108@naver.com>
2024-04-17 14:10:45 +0900

    test(AdminController): 누락된 예약 정보 테스트 추가
    Co-authored-by: jinchiim <kyunellroll@gmail.com>

--- src/test/java/roomescape/controller/AdminControllerTest.java
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -13,4 +13,13 @@ class AdminControllerTest {
         Assertions.assertThat(mainPage)
                 .isEqualTo("admin/index");
     }
+
+    @Test
+    @DisplayName("관리자 예약 정보 페이지 경로를 정해진 경로로 매핑한다.")
+    void reservationPage() {
+        AdminController adminController = new AdminController();
+        String reservationPage = adminController.reservationPage();
+        Assertions.assertThat(reservationPage)
+                .isEqualTo("admin/reservation-legacy");
+    }
 }

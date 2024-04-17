58db3f28e4272b7918e7d6ae42d84a77ec576a08
fromitive <46563149+fromitive@users.noreply.github.com>
2024-04-18 00:31:42 +0900

    [1 - 3단계 - 방탈출 예약 관리] 포케(이무송) 미션 제출합니다. (#69)
    * feat: 어드민 메인 페이지 반환 기능 추가
    
    Co-authored-by: fromitive<fromitive@gmail.com>
    
    * feat: 예약 조회 기능 추가
    
    Co-authored-by: fromitive<fromitive@gmail.com>
    
    * feat: 예약 추가 취소 기능 구현
    
    Co-authored-by: fromitive<fromitive@gmail.com>
    
    * refactor : import문 최적화
    
    Co-authored-by: fromitive<fromitive@gmail.com>
    
    * refactor : 매핑 url 수정
    
    Co-authored-by: fromitive<fromitive@gmail.com>
    
    * refactor : 메서드명 변경
    
    Co-authored-by: fromitive<fromitive@gmail.com>
    
    * refactor : 미사용 getter 제거
    
    Co-authored-by: fromitive<fromitive@gmail.com>
    
    * refactor : VO에 Json 변환을 위한 getter 추가
    
    Co-authored-by: fromitive<fromitive@gmail.com>
    
    ---------
    
    Co-authored-by: Hyeon0208 <dmsgktn0208@gmail.com>

---
+++ src/main/java/roomescape/reservation/ReservationController.java
@@ -0,0 +1,13 @@
+package roomescape.reservation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReservationController {
+
+    @GetMapping("/admin/reservation")
+    public String home() {
+        return "admin/reservation-legacy";
+    }
+}

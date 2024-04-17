2384a7a05fc330b962e0e50a8fe15a1a8434720a
Hyeon0208 <dmsgktn0208@gmail.com>
2024-04-17 16:00:40 +0900

    refactor : 미사용 getter 제거
    Co-authored-by: fromitive<fromitive@gmail.com>

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -19,16 +19,4 @@ public class Reservation {
     public Long getId() {
         return id;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
 }

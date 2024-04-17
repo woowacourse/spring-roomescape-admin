77592dbc9bb5c7c4e475accb184475715293be02
jinwoo22 <jenny010925@naver.com>
2024-04-17 14:46:55 +0900

    refactor: 일급 컬렉션 생성

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,24 +1,24 @@
 package roomescape.controller;
 
-import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import roomescape.domain.Reservation;
+import roomescape.domain.Reservations;
 import roomescape.dto.request.ReservationCreateRequest;
-import roomescape.dto.response.ReservationResponse;
 
 import java.util.ArrayList;
-import java.util.List;
+import roomescape.dto.response.ReservationResponse;
+import roomescape.dto.response.ReservationResponses;
 
 @Controller
 public class ReservationController {
 
     private final AtomicLong index = new AtomicLong(0);
 
-    private final List<Reservation> reservations = new ArrayList<>();
+    private final Reservations reservations = new Reservations(new ArrayList<>());
 
     @GetMapping("/admin")
     public String getAdminPage() {
@@ -27,30 +27,29 @@ public class ReservationController {
 
     @GetMapping("/admin/reservation")
     public String getReservationPage(Model model) {
-        List<ReservationResponse> reservationResponses = reservations.stream().map(ReservationResponse::fromEntity).toList();
-        model.addAttribute("reservationDtos", reservationResponses);
+        ReservationResponses reservationResponses = ReservationResponses.fromEntity(reservations);
+        model.addAttribute("reservationResponses", reservationResponses);
         return "/admin/reservation-legacy";
     }
 
     @GetMapping("/reservations")
-    public ResponseEntity<List<ReservationResponse>> getReservations() {
-        List<ReservationResponse> reservationResponses = reservations.stream().map(ReservationResponse::fromEntity).toList();
+    public ResponseEntity<ReservationResponses> getReservations() {
+        ReservationResponses reservationResponses = ReservationResponses.fromEntity(reservations);
         return ResponseEntity.ok(reservationResponses);
     }
 
     @PostMapping("/reservations")
-    public ResponseEntity<Reservation> createReservations(@RequestBody ReservationCreateRequest reservationCreateDto) {
-        Reservation reservation = reservationCreateDto.toEntity(index.incrementAndGet());
+    public ResponseEntity<ReservationResponse> createReservations(@RequestBody ReservationCreateRequest reservationCreateRequest) {
+        Reservation reservation = reservationCreateRequest.toEntity(index.incrementAndGet());
         reservations.add(reservation);
-        return ResponseEntity.ok(reservation);
+
+        ReservationResponse reservationResponse = ReservationResponse.fromEntity(reservation);
+        return ResponseEntity.ok(reservationResponse);
     }
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
-        Reservation found = reservations.stream().filter(reservation -> Objects.equals(reservation.getId(), id))
-                .findFirst().orElseThrow(() -> new IllegalArgumentException(id + "는 존재하지 않습니다."));
-
-        reservations.remove(found);
+        reservations.remove(id);
         return ResponseEntity.ok().build();
     }
 }

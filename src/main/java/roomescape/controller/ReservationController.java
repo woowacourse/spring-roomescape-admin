2c18077438f178bb8179b744bc80633970da1db4
이상진 <priceelees@gmail.com>
2024-04-17 13:47:25 +0900

    feat(ReservationController): 1단계 기능 구현 및 테스트
    Co-authored-by: nak-honest <e5slnh9876@gmail.com>

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,13 @@
+package roomescape.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReservationController {
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin/index";
+    }
+}

54d253923054b7096acb9a6ecc67fc02408cacdb
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-04-17 13:16:18 +0900

    refactor: Controller 메소드 명 수정

--- src/main/java/roomescape/AdminController.java
+++ src/main/java/roomescape/AdminController.java
@@ -12,22 +12,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminController {
 
     @GetMapping("/")
-    public String enterWellComePage() {
+    public String wellComePage() {
         return "admin/index";
     }
 
     @GetMapping("/admin")
-    public String enterAdminPage() {
+    public String adminPage() {
         return "admin/index";
     }
 
     @GetMapping("/admin/reservation")
-    public String readReservations() {
+    public String reservationPage() {
         return "admin/reservation-legacy";
     }
 
     @GetMapping("/reservations")
-    public ResponseEntity<List<Reservation>> getReservations() {
+    public ResponseEntity<List<Reservation>> reservationList() {
         ArrayList<Reservation> reservations = new ArrayList<>();
         reservations.add(new Reservation(1L, "브라운", LocalDate.of(2024, 4, 1), LocalTime.of(10, 0)));
         reservations.add(new Reservation(2L, "솔라", LocalDate.of(2024, 4, 1), LocalTime.of(11, 0)));

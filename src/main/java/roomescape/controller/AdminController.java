4c654c40be6976c6a784c8baa814f81ca5edfc53
Seokmyung Ham <gkatjraud1@gmail.com>
2024-04-18 01:18:57 +0900

    refactor(AdminController): 클래스 이름을 변경하고 공통된 매핑 URL을 @RequestMapping을 사용해서 분리

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -2,16 +2,18 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class RoomescapeController {
+@RequestMapping("/admin")
+public class AdminController {
 
-    @GetMapping("/admin")
+    @GetMapping
     public String getIndexPage() {
         return "admin/index";
     }
 
-    @GetMapping("/admin/reservation")
+    @GetMapping("/reservation")
     public String getReservationPage() {
         return "admin/reservation-legacy";
     }

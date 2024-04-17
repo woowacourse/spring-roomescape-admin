931c3b71e914e4560a847456f8513210f282445a
이상진 <priceelees@gmail.com>
2024-04-17 23:17:49 +0900

    refactor(ReservationController): ResponseEntity가 아닌 @ResponseBody를 이용하도록 수정

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -4,48 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import roomescape.entity.Reservation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.ReservationRequestDto;
+import roomescape.entity.Reservation;
 
-@Controller
+@RequestMapping("/reservations")
+@RestController
 public class ReservationController {
 
     private final List<Reservation> reservations = new ArrayList<>();
     private final AtomicLong index = new AtomicLong(1);
 
-    @GetMapping("/admin")
-    public String admin() {
-        return "/admin/index";
+    @GetMapping
+    public List<Reservation> findAllReservations() {
+        return reservations;
     }
 
-    @GetMapping("/admin/reservation")
-    public String reservation() {
-        return "/admin/reservation-legacy";
-    }
-
-    @GetMapping("/reservations")
-    public ResponseEntity<List<Reservation>> findAllReservations() {
-        return ResponseEntity.ok(reservations);
-    }
-
-    @PostMapping("/reservations")
-    public ResponseEntity<Reservation> addReservation(@RequestBody ReservationRequestDto reservationRequestDto) {
+    @PostMapping
+    public Reservation addReservation(@RequestBody ReservationRequestDto reservationRequestDto) {
         Reservation reservation = reservationRequestDto.toEntity(index.getAndIncrement());
         reservations.add(reservation);
-        return ResponseEntity.ok(reservation);
+        return reservation;
     }
 
-    @DeleteMapping("/reservations/{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public void deleteReservation(@PathVariable Long id) {
         reservations.removeIf(reservation -> Objects.equals(reservation.getId(), id));
-
-        return ResponseEntity.ok().build();
     }
 }

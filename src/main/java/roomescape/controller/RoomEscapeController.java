abefe8412aadc1df697a5dec8740be92bf70f203
Seokmyung Ham <gkatjraud1@gmail.com>
2024-04-17 09:28:56 +0900

    feat(RoomEscapeController): "/admin/reservation" GET 요청 시 예약 관리 페이지를 응답하는 기능 추가
    Co-authored-by: tsulocalize <tsulocalizing@gmail.com>

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -1,13 +1,34 @@
 package roomescape.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import roomescape.entity.ReservationEntity;
 
 @Controller
 public class RoomEscapeController {
 
+    private final List<ReservationEntity> reservationEntities;
+
+    public RoomEscapeController() {
+        this.reservationEntities = new ArrayList<>();
+    }
+
     @GetMapping("/admin")
     public String getIndexPage() {
         return "admin/index";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String getReservationPage() {
+        return "admin/reservation-legacy";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<ReservationEntity>> getReservations() {
+        return new ResponseEntity<>(reservationEntities, HttpStatus.OK);
     }
 }

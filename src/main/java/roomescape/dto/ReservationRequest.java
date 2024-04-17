97c0da7698ed7c5e3b6439801a39c6d76ef4bc13
woojong <dwax1324@gmail.com>
2024-04-17 22:40:53 +0900

    [1 - 3단계 - 방탈출 예약 관리] 종이(최우종) 미션 제출합니다. (#12)
    * docs: 1단게 요구사항 작성
    
    * feat: Admin 페이지 응답 구현
    
    * feat: welcome 페이지 연결 구현
    
    * docs: 2단계 요구사항 작성
    
    * feat: 예약 조회 페이지 연결 구현
    
    * docs: 3단계 요구사항 작성
    
    * feat: 예약 추가 및 삭제 API 구현
    
    * fix: 수정사항 반영
    
    * refactor: 피드백 반영
    
    ---------
    
    Co-authored-by: hjk0761 <hjk0761@naver.com>

---
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -0,0 +1,18 @@
+package roomescape.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import roomescape.domain.Reservation;
+
+public record ReservationRequest(
+        String name,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul") LocalDate date,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm", timezone = "Asia/Seoul") LocalTime time
+) {
+    public Reservation toReservation(final long id) {
+        return new Reservation(id, name, LocalDateTime.of(date, time));
+    }
+}
---
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -0,0 +1,18 @@
+package roomescape.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import roomescape.domain.Reservation;
+
+public record ReservationResponse(
+        Long id,
+        String name,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul") LocalDate date,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm", timezone = "Asia/Seoul") LocalTime time
+) {
+    public static ReservationResponse from(Reservation reservation) {
+        return new ReservationResponse(reservation.getId(), reservation.getName(), reservation.getDate(),
+                reservation.getTime());
+    }
+}

d51541686cd484c58dbbbe52156b1852144ca165
ikjo39 <chi4321@naver.com>
2024-04-17 17:39:28 +0900

    refactor: @RestController 적용

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -5,18 +5,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 import roomescape.dto.ReservationDto;
 
-@Controller
+@RestController
 @RequestMapping("/reservations")
 public class ReservationController {
 
@@ -24,7 +23,6 @@ public class ReservationController {
     private final Map<Long, Reservation> reservations = new HashMap<>();
 
     @GetMapping
-    @ResponseBody
     public ResponseEntity<List<ReservationDto>> getAll() {
         List<ReservationDto> totalReservations = reservations.values()
                 .stream()
@@ -34,7 +32,6 @@ public class ReservationController {
     }
 
     @PostMapping
-    @ResponseBody
     public ResponseEntity<ReservationDto> create(@RequestBody ReservationDto reservationDto) {
         long id = idCount.getAndIncrement();
         Reservation reservation = reservationDto.toEntity(id);

706a0dc8674f0c5a5bf20ef762a1500dd3c6c9cc
Seokmyung Ham <gkatjraud1@gmail.com>
2024-04-17 09:35:03 +0900

    feat(RoomEscapeController): 예약을 추가 및 삭제하는 API 기능 추가
    Co-authored-by: tsulocalize <tsulocalizing@gmail.com>

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import roomescape.entity.ReservationEntity;
 
 @Controller
@@ -30,5 +34,17 @@ public class RoomEscapeController {
     @GetMapping("/reservations")
     public ResponseEntity<List<ReservationEntity>> getReservations() {
         return new ResponseEntity<>(reservationEntities, HttpStatus.OK);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<ReservationEntity> createReservation(@RequestBody ReservationEntity reservationEntity) {
+        reservationEntities.add(reservationEntity);
+        return new ResponseEntity<>(reservationEntity, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+        reservationEntities.removeIf(reservationEntity -> reservationEntity.getId().equals(id));
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }

2b7b8d8db7a142efb87526a2a9e35a2e0b3baaae
nayonsoso <panda0329@naver.com>
2024-04-17 16:34:22 +0900

    refactor: 템플릿 사용으로 중복 코드 제거
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,9 +1,9 @@
 package roomescape;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -11,11 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import roomescape.model.Reservation;
 import roomescape.repository.ReservationRepository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.hamcrest.Matchers.is;
+import java.util.*;
 
 @Transactional
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -28,10 +24,7 @@ class MissionStepTest {
     @DisplayName("관리자 메인 페이지 응답")
     @Test
     void moveToAdminPage() {
-        RestAssured.given().log().all()
-                .when().get("/admin")
-                .then().log().all()
-                .statusCode(200);
+        HttpRestTestTemplate.assertGetOk("/admin");
     }
 
     @DisplayName("예약 페이지 응답")
@@ -40,54 +33,32 @@ class MissionStepTest {
         List<Reservation> reservations = reservationRepository.findAll();
         int reservationSize = reservations.size();
 
-        RestAssured.given().log().all()
-                .when().get("/admin/reservation")
-                .then().log().all()
-                .statusCode(200);
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(reservationSize));
+        HttpRestTestTemplate.assertGetOk("/admin/reservation");
+        HttpRestTestTemplate.assertGetOk("/reservations", "size()", reservationSize);
     }
 
-    @DisplayName("예약 추가 후 삭제")
-    @Test
-    void addAndRemoveReservation() {
+    @DisplayName("예약_추가_삭제_시나리오")
+    @TestFactory
+    Collection<DynamicTest> addAndRemoveReservation() {
         List<Reservation> reservations = reservationRepository.findAll();
         int reservationSize = reservations.size();
         int lastIndex = Math.toIntExact(reservations.get(reservationSize - 1)
                 .getId());
 
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
+        return Arrays.asList(
+                DynamicTest.dynamicTest("예약 추가", () -> {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("name", "브라운");
+                    params.put("date", "2023-08-05");
+                    params.put("time", "15:40");
 
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("id", is(lastIndex + 1));
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(reservationSize + 1));
-
-        RestAssured.given().log().all()
-                .when().delete("/reservations/" + (lastIndex + 1))
-                .then().log().all()
-                .statusCode(200);
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(reservationSize));
+                    HttpRestTestTemplate.assertPostOk(params, "/reservations", "id", lastIndex + 1);
+                    HttpRestTestTemplate.assertGetOk("/reservations", "size()", reservationSize + 1);
+                }),
+                DynamicTest.dynamicTest("예약 삭제", () -> {
+                    HttpRestTestTemplate.assertDeleteOk("/reservations/" + (lastIndex + 1));
+                    HttpRestTestTemplate.assertGetOk("/reservations", "size()", reservationSize);
+                })
+        );
     }
 }

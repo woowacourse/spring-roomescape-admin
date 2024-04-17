bd8a073cf9417e347cb200b36e6bbf32da97c370
Hyeon0208 <dmsgktn0208@gmail.com>
2024-04-17 16:00:25 +0900

    refactor : 매핑 url 수정
    Co-authored-by: fromitive<fromitive@gmail.com>

--- src/main/java/roomescape/home/HomeController.java
+++ src/main/java/roomescape/home/HomeController.java
@@ -1,4 +1,4 @@
-package roomescape.admin;
+package roomescape.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/admin")
     public String home() {
         return "admin/index";
     }

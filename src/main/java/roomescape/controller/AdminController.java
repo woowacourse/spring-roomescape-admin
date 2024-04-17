dd4f0ec7aafb5cae6833dd80eccc3fb4c210133e
juha <khabh@naver.com>
2024-04-17 14:15:45 +0900

    refactor: Reservation 컨트롤러 추가 및 서비스 삭제

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -1,24 +1,11 @@
 package roomescape.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import roomescape.dto.ReservationResponseDto;
-import roomescape.service.ReservationService;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-    private final ReservationService reservationService;
-
-    @Autowired
-    public AdminController(final ReservationService reservationService) {
-        this.reservationService = reservationService;
-    }
 
     @GetMapping
     public String home() {
@@ -26,9 +13,7 @@ public class AdminController {
     }
 
     @GetMapping("/reservation")
-    public String reservation(final Model model) {
-        final List<ReservationResponseDto> reservationResponseDtos = reservationService.getReservations();
-        model.addAttribute("reservations", reservationResponseDtos);
+    public String reservation() {
         return "/admin/reservation-legacy";
     }
 }
--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,17 +1,21 @@
-package roomescape.service;
+package roomescape.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import roomescape.dto.ReservationResponseDto;
+import roomescape.model.Reservation;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-import roomescape.dto.ReservationResponseDto;
-import roomescape.model.Reservation;
-
-@Service
-public class ReservationService {
+@Controller
+@RequestMapping("/reservations")
+public class ReservationController {
 
     private final List<Reservation> reservations = new ArrayList<>();
 
+    @GetMapping
+    @ResponseBody
     public List<ReservationResponseDto> getReservations() {
         return reservations.stream()
                 .map(ReservationResponseDto::from)

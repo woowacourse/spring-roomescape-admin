6e47a8a365ae429c4065b464f7e183dada126dc8
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-04-17 14:43:10 +0900

    test: 테스트 분리

--- src/test/java/roomescape/reservation/ReservationControllerTest.java
+++ src/test/java/roomescape/reservation/ReservationControllerTest.java
@@ -1,4 +1,4 @@
-package roomescape;
+package roomescape.reservation;
 
 import static org.hamcrest.Matchers.is;
 
@@ -6,38 +6,17 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
-
+class ReservationControllerTest {
+    @DisplayName("예약 CRUD에 성공한다.")
     @Test
-    void 일단계() {
-        RestAssured.given().log().all()
-                .when().get("/admin")
-                .then().log().all()
-                .statusCode(200);
-    }
-
-    @Test
-    void 이단계() {
-        RestAssured.given().log().all()
-                .when().get("/admin/reservation")
-                .then().log().all()
-                .statusCode(200);
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
-    }
-
-    @Test
-    void 삼단계() {
+    void reservation() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");

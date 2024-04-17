aea793d919115ce5c81e6ebc0a6db961bb6106c8
tackyu <tack970913@gmail.com>
2024-04-17 15:21:16 +0900

    feat: 예약 내역 조회 구현

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,10 +1,18 @@
 package roomescape;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import roomescape.domain.Reservation;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 @Controller
 public class RoomescapeController {
+    private List<Reservation> reservations = List.of(new Reservation((long) 1,"a", LocalDate.now(),LocalTime.now()));
+
     @GetMapping("admin")
     public String admin(){
         return "/admin/index";
@@ -13,6 +21,11 @@ public class RoomescapeController {
     @GetMapping("admin/reservation")
     public String reservation() {
         return "/admin/reservation-legacy";
+    }
+
+    @GetMapping("reservations")
+    public ResponseEntity<List<Reservation>> read(){
+        return ResponseEntity.ok().body(reservations);
     }
 
 }
---
+++ src/main/java/roomescape/domain/Reservation.java
@@ -0,0 +1,34 @@
+package roomescape.domain;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+    private Long id;
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
+
+    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+}

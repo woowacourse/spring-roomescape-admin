7598dced0cc768896a8073e76a9183b66280a131
kunsanglee <gosmdochee2@gmail.com>
2024-04-17 15:26:59 +0900

    refactor: 클래스 분리 및 자료구조 개선

---
+++ src/main/java/roomescape/ReservationController.java
@@ -0,0 +1,49 @@
+package roomescape;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("reservations")
+public class ReservationController {
+
+    private Map<Long, Reservation> reservations = new HashMap<>();
+    private AtomicLong index = new AtomicLong();
+
+    @GetMapping("")
+    @ResponseBody
+    public List<Reservation> findAll() {
+        return reservations.values().stream().toList();
+    }
+
+    @PostMapping("")
+    @ResponseBody
+    public ResponseEntity<Reservation> create(@RequestBody ReservationRequest request) {
+        long id = index.incrementAndGet();
+        Reservation reservation = new Reservation(id, request.getName(),
+                request.getDate(), request.getTime());
+        reservations.put(id, reservation);
+        return ResponseEntity.ok(reservation);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id, Model model) {
+        reservations.remove(id);
+        model.addAttribute(id);
+        return ResponseEntity.ok().build();
+    }
+}
--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,57 +1,21 @@
 package roomescape;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("admin")
 public class RoomescapeController {
 
-    private List<Reservation> reservations = new ArrayList<>();
-    private AtomicLong index = new AtomicLong();
-
-    @GetMapping("admin")
+    @GetMapping("")
     public String welcome() {
         return "admin/index";
     }
 
-    @GetMapping("admin/reservation")
+    @GetMapping("reservation")
     public String getReservationPage() {
         return "admin/reservation-legacy";
     }
 
-    @GetMapping("reservations")
-    @ResponseBody
-    public List<Reservation> findAll() {
-        return reservations;
-    }
-
-    @PostMapping("reservations")
-    @ResponseBody
-    public ResponseEntity<Reservation> create(@RequestBody ReservationRequest reservationRequest) {
-        Reservation reservation = new Reservation(index.incrementAndGet(), reservationRequest.getName(),
-                reservationRequest.getDate(), reservationRequest.getTime());
-        reservations.add(reservation);
-        return ResponseEntity.ok(reservation);
-    }
-
-    @DeleteMapping("reservations/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id, Model model) {
-        Reservation findReservation = reservations.stream()
-                .filter(reservation -> reservation.getId().equals(id))
-                .findAny()
-                .orElseThrow();
-        reservations.remove(findReservation);
-        model.addAttribute(id);
-        return ResponseEntity.ok().build();
-    }
 }

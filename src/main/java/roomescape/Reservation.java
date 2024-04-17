cc73670133a25b37204dfbff0fd2f74b825ebe3f
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-04-17 14:33:24 +0900

    refactor: 불 필요한 메서드 삭제

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -17,16 +17,6 @@ public class Reservation {
         this.time = time;
     }
 
-    public Reservation(String name, LocalDate date, LocalTime time) {
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
-
-    public static Reservation toEntity(Reservation reservation, Long id) {
-        return new Reservation(id, reservation.name, reservation.date, reservation.time);
-    }
-
     public Long getId() {
         return id;
     }

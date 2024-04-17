a015884c7fdfc1f788e392748182f472a0f5a98d
libienz <leekh10195@gmail.com>
2024-04-17 20:17:29 +0900

    test: 테스트 코드 가독성 개선

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -6,38 +6,46 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+class MissionStepTest {
 
+    @DisplayName("어드민 메인 페이지 요청 테스트")
     @Test
-    void 일단계() {
+    void adminMainPageTest() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
                 .statusCode(200);
     }
 
+    @DisplayName("어드민 예약 페이지 요청 테스트")
     @Test
-    void 이단계() {
+    void adminReservationPageTest() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
+    }
 
+    @DisplayName("전체 예약 정보 요청 테스트")
+    @Test
+    void reservationsTest() {
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
+                .body("size()", is(0));
     }
 
+    @DisplayName("예약 생성, 삭제 테스트")
     @Test
-    void 삼단계() {
+    void reservationCreationAndDeleteTest() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");

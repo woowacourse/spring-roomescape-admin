fe556267e784c44a2afea0afccc2ef8ebad82155
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-04-17 15:28:19 +0900

    refactor(CustomDateTimeFormatter): formatter를 상수로 추출
    - 인스턴스 생성 방지를 위해 private인 기본 생성자 추가
    
    Co-authored-by: zangsu <zangsu_@naver.com>

--- src/main/java/roomescape/util/CustomDateTimeFormatter.java
+++ src/main/java/roomescape/util/CustomDateTimeFormatter.java
@@ -5,20 +5,25 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-// TODO: DateTimeFormatter 고민하기
 public class CustomDateTimeFormatter {
+    private static final DateTimeFormatter ISO_LOCAL_DATE_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE;
+    private static final DateTimeFormatter TIME_WITHOUT_SECOND_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
+    private CustomDateTimeFormatter() {
+    }
+
     public static LocalDateTime getLocalDateTime(final String date, final String time) {
-        LocalDate localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        LocalTime localTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
+        LocalDate localDate = LocalDate.parse(date, ISO_LOCAL_DATE_FORMATTER);
+        LocalTime localTime = LocalTime.parse(time, TIME_WITHOUT_SECOND_FORMATTER);
 
         return LocalDateTime.of(localDate, localTime);
     }
 
     public static String getFormattedDate(final LocalDateTime dateTime) {
-        return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        return dateTime.format(ISO_LOCAL_DATE_FORMATTER);
     }
 
     public static String getFormattedTime(final LocalDateTime dateTime) {
-        return dateTime.format(DateTimeFormatter.ofPattern("HH:mm"));
+        return dateTime.format(TIME_WITHOUT_SECOND_FORMATTER);
     }
 }

3dbb38b9cb4c5060ae6ee658bac91abf2c09a050
kunsanglee <gosmdochee2@gmail.com>
2024-04-17 15:15:37 +0900

    feat: 3단계 예약 추가 취소 기능 구현

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,17 +1,23 @@
 package roomescape;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RoomescapeController {
 
     private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong();
 
     @GetMapping("admin")
     public String welcome() {
@@ -29,4 +35,23 @@ public class RoomescapeController {
         return reservations;
     }
 
+    @PostMapping("reservations")
+    @ResponseBody
+    public ResponseEntity<Reservation> create(@RequestBody ReservationRequest reservationRequest) {
+        Reservation reservation = new Reservation(index.incrementAndGet(), reservationRequest.getName(),
+                reservationRequest.getDate(), reservationRequest.getTime());
+        reservations.add(reservation);
+        return ResponseEntity.ok(reservation);
+    }
+
+    @DeleteMapping("reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id, Model model) {
+        Reservation findReservation = reservations.stream()
+                .filter(reservation -> reservation.getId().equals(id))
+                .findAny()
+                .orElseThrow();
+        reservations.remove(findReservation);
+        model.addAttribute(id);
+        return ResponseEntity.ok().build();
+    }
 }

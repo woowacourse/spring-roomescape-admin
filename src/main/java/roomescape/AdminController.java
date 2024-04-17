f7c965118567d3904ccb965ff9bb3d10aa25a980
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-04-17 14:15:52 +0900

    feat: 예약 삭제 기능 구현

--- src/main/java/roomescape/AdminController.java
+++ src/main/java/roomescape/AdminController.java
@@ -2,10 +2,13 @@ package roomescape;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -41,5 +44,19 @@ public class AdminController {
                 reservationDto.getDate(), reservationDto.getTime());
         reservations.add(reservation);
         return ResponseEntity.ok(reservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> reservationRemove(@PathVariable("id") Long id) {
+        Optional<Reservation> reservationOptional = reservations.stream()
+                .filter(it -> it.getId().equals(id))
+                .findFirst();
+
+        if (reservationOptional.isPresent()) {
+            reservations.remove(reservationOptional.get());
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.badRequest().build();
     }
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -65,4 +65,41 @@ public class MissionStepTest {
                 .statusCode(200)
                 .body("size()", is(1));
     }
+
+    @DisplayName("/reservations/{id}를 delete로 요청 시 예약을 삭제할 수 있다.")
+    @Test
+    void should_remove_reservation_when_delete_request_reservations_id() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @DisplayName("/reservations/{id}를 delete로 요청 시 id값이 존재하지 않으면 bad request를 응답한다.")
+    @Test
+    void should_response_bad_request_when_nonExist_id() {
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(400);
+    }
 }

de3c77503750d027fd072dd7a34f9e5ee93ef458
le2sky <leehaneul990623@gmail.com>
2024-04-17 13:28:45 +0900

    refactor: dto를 record 클래스로 변경

--- src/main/java/roomescape/CreateReservationRequest.java
+++ src/main/java/roomescape/CreateReservationRequest.java
@@ -3,27 +3,5 @@ package roomescape;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class CreateReservationRequest {
-
-    private final LocalDate date;
-    private final String name;
-    private final LocalTime time;
-
-    public CreateReservationRequest(LocalDate date, String name, LocalTime time) {
-        this.date = date;
-        this.name = name;
-        this.time = time;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
+public record CreateReservationRequest(LocalDate date, String name, LocalTime time) {
 }
--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,17 +1,13 @@
 package roomescape;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/reservations")
@@ -29,9 +25,9 @@ public class ReservationController {
     public ResponseEntity<Reservation> reserve(@RequestBody CreateReservationRequest request) {
         Reservation newReservation = new Reservation(
                 id.incrementAndGet(),
-                request.getName(),
-                request.getDate(),
-                request.getTime());
+                request.name(),
+                request.date(),
+                request.time());
         reservations.add(newReservation);
         return ResponseEntity.ok(newReservation);
     }

8b22cb8882d557e859f12bc6d6da5b24cd151b2f
kunsanglee <gosmdochee2@gmail.com>
2024-04-17 15:15:37 +0900

    feat: 3단계 예약 추가 취소 기능 구현

--- src/main/java/roomescape/ReservationRequest.java
+++ src/main/java/roomescape/ReservationRequest.java
@@ -3,30 +3,24 @@ package roomescape;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class Reservation {
+public class ReservationRequest {
 
-    private Long id;
-    private String name;
     private LocalDate date;
+    private String name;
     private LocalTime time;
 
-    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
-        this.id = id;
-        this.name = name;
+    public ReservationRequest(LocalDate date, String name, LocalTime time) {
         this.date = date;
+        this.name = name;
         this.time = time;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public LocalDate getDate() {
         return date;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public LocalTime getTime() {

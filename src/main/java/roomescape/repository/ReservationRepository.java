2245b132e759eabb578c3ccbf5adfbe4d687ad16
hangillee <skfcb10@naver.com>
2024-04-17 16:42:45 +0900

    feat: DB 적용 및 Reservation DAO 구현

---
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -0,0 +1,52 @@
+package roomescape.repository;
+
+import java.sql.PreparedStatement;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.Reservation;
+
+@Repository
+public class ReservationRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationRepository(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Long save(final Reservation reservation) {
+        final String query = "INSERT INTO reservation(name, date, time) VALUES (?, ?, ?)";
+        final KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            final PreparedStatement preparedStatement = connection.prepareStatement(query, new String[]{"id"});
+            preparedStatement.setString(1, reservation.getName());
+            preparedStatement.setString(2, reservation.getDate());
+            preparedStatement.setString(3, reservation.getTime());
+            return preparedStatement;
+        }, keyHolder);
+        
+        return keyHolder.getKey().longValue();
+    }
+
+    public List<Reservation> findAll() {
+        return jdbcTemplate.query("SELECT id, name, date, time FROM reservation", (resultSet, rowNum) -> {
+            final Long id = resultSet.getLong("id");
+            final String name = resultSet.getString("name");
+            final LocalDate date = LocalDate.parse(resultSet.getString("date"),
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            final LocalTime time = LocalTime.parse(resultSet.getString("time"),
+                    DateTimeFormatter.ofPattern("HH:mm"));
+
+            return new Reservation(id, name, date, time);
+        });
+    }
+
+    public int deleteById(final Long id) {
+        return jdbcTemplate.update("DELETE FROM reservation WHERE id = ?", id);
+    }
+}

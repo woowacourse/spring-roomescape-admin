18e3362bebe7d842e2b2fc83c8989fc7a55bc818
BurningFalls <sherlocky31415@gmail.com>
2024-04-17 16:22:42 +0900

    refactor: CRUD mapping에서 빈 문자열을 공백으로 대체

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminController {
 
-    @GetMapping("")
+    @GetMapping()
     public String showAdminMainPage() {
         return "admin/index.html";
     }

3d94ed4f5b51a61e8a58cb373ff4280779cc07ad
SCY <helenason@naver.com>
2024-04-17 14:04:15 +0900

    feat: 예약 추가 및 취소 구현
    Co-authored-by: zeus6768 <akaps6768@gmail.com>

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,14 +1,22 @@
 package roomescape;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RoomescapeController {
+
+    private AtomicLong index = new AtomicLong(0);
+    private final List<Reservation> reservations = new ArrayList<>();
 
     @GetMapping("/admin")
     public String admin() {
@@ -23,10 +31,34 @@ public class RoomescapeController {
     @GetMapping("/reservations")
     @ResponseBody
     public List<ReservationDto> reservations() {
-        return List.of(
-                ReservationDto.from(new Reservation(1L, "브라운", LocalDate.of(2023, 1, 1), LocalTime.of(10, 0))),
-                ReservationDto.from(new Reservation(2L, "브라운", LocalDate.of(2023, 1, 2), LocalTime.of(11, 0))),
-                ReservationDto.from(new Reservation(3L, "브라운", LocalDate.of(2023, 1, 3), LocalTime.of(12, 0)))
+        return reservations.stream()
+                .map(ReservationDto::from)
+                .toList();
+    }
+
+    @PostMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<ReservationDto> addReservation(@RequestBody ReservationDto reservationDto) {
+        Reservation reservation = new Reservation(
+                index.incrementAndGet(),
+                reservationDto.getName(),
+                reservationDto.getDate(),
+                reservationDto.getTime()
         );
+        reservations.add(reservation);
+        return ResponseEntity.ok(
+                new ReservationDto(
+                        reservation.getId(),
+                        reservation.getName(),
+                        reservation.getDate(),
+                        reservation.getTime()
+                )
+        );
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable("id") long id) {
+        reservations.removeIf(reservation -> reservation.getId() == id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -3,6 +3,9 @@ package roomescape;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -30,6 +33,39 @@ public class MissionStepTest {
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(3));
+                .body("size()", is(0));
+    }
+
+    @Test
+    void 삼단계() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
     }
 }

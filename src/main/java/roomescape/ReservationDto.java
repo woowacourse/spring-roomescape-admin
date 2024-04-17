ca98f3abc9c397e129e5185414896b5026967129
SCY <88581911+helenason@users.noreply.github.com>
2024-04-18 02:40:47 +0900

    [1 - 3단계 방탈출 예약 관리] 에버(손채영) 미션 제출합니다. (#49)
    * chore: ignore 파일 수정
    
    Co-authored-by: zeus6768 <akaps6768@gmail.com>
    
    * docs: 기능 목록 작성
    
    Co-authored-by: zeus6768 <akaps6768@gmail.com>
    
    * feat: 어드민 메인 페이지 생성
    
    Co-authored-by: zeus6768 <akaps6768@gmail.com>
    
    * feat: 예약 목록 조회 페이지 생성
    
    Co-authored-by: zeus6768 <akaps6768@gmail.com>
    
    * refactor: 시간 포맷팅 방식 변경
    
    Co-authored-by: zeus6768 <akaps6768@gmail.com>
    
    * feat: 예약 추가 및 취소 구현
    
    Co-authored-by: zeus6768 <akaps6768@gmail.com>
    
    * refactor: DTO -> 엔티티 변환 메서드 추가
    
    Co-authored-by: zeus6768 <akaps6768@gmail.com>
    
    * feat: 존재하지 않는 예약을 삭제하는 경우 예외 발생
    
    Co-authored-by: zeus6768 <akaps6768@gmail.com>
    
    * refactor: 자동 증가 인덱스 객체 불변 처리 및 초깃값 변경
    
    * refactor: 메서드의 응답 반환 방식 통일
    
    ---------
    
    Co-authored-by: zeus6768 <akaps6768@gmail.com>

---
+++ src/main/java/roomescape/ReservationDto.java
@@ -0,0 +1,46 @@
+package roomescape;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ReservationDto {
+
+    private final Long id;
+    private final String name;
+    private final LocalDate date;
+    private final LocalTime time;
+
+    public ReservationDto(Long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public static ReservationDto from(Reservation reservation) {
+        return new ReservationDto(reservation.getId(), reservation.getName(), reservation.getDate(),
+                reservation.getTime());
+    }
+
+    public Reservation toEntity(long id) {
+        return new Reservation(id, name, date, time);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    @JsonFormat(pattern = "HH:mm")
+    public LocalTime getTime() {
+        return time;
+    }
+}

1eb2caf66ab6550c33f11fce047b412793dacde1
linirini <2001yerin@naver.com>
2024-04-17 17:56:56 +0900

    test: 엔티티, DAO 분리에 따른 테스트 수정

--- src/test/java/roomescape/AdminControllerTest.java
+++ src/test/java/roomescape/AdminControllerTest.java
@@ -13,7 +13,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AdminControllerTest {
-
     @LocalServerPort
     private int port;
 
--- src/test/java/roomescape/ReservationControllerTest.java
+++ src/test/java/roomescape/ReservationControllerTest.java
@@ -6,53 +6,63 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import roomescape.dao.ReservationDao;
+import roomescape.domain.Reservation;
 import roomescape.dto.ReservationRequest;
 import roomescape.dto.ReservationResponse;
+import roomescape.entity.ReservationEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ReservationControllerTest {
-
     @LocalServerPort
     private int port;
+
+    @Autowired
+    private ReservationDao reservationDao;
 
     @BeforeEach
     void init() {
         RestAssured.port = port;
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all().extract().response().jsonPath().getList("id")
-                .forEach(id -> RestAssured.given().log().all()
-                        .when().delete("/reservations/" + id));
+        for (ReservationEntity reservationEntity : reservationDao.findAll()) {
+            reservationDao.deleteById(reservationEntity.getId());
+        }
     }
 
     @DisplayName("예약 추가 테스트")
     @Test
     void createReservation() {
+        //when
         Response response = RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(new ReservationRequest("브라운", "2023-08-05", "15:40"))
                 .when().post("/reservations")
                 .then().log().all().extract().response();
 
+        //then
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
     @DisplayName("모든 예약 내역 조회 테스트")
     @Test
     void findAllReservations() {
-        createReservation();
+        //given
+        Reservation reservation = new Reservation("브라운", "2023-08-05", "15:40");
+        reservationDao.save(reservation);
 
+        //when
         Response response = RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all().extract().response();
 
+        //then
         assertAll(
                 () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK.value()),
                 () -> assertThat(response.jsonPath().getList("", ReservationResponse.class)).hasSize(1)
@@ -62,31 +72,34 @@ public class ReservationControllerTest {
     @DisplayName("예약 취소 성공 테스트")
     @Test
     void deleteReservationSuccess() {
-        createReservation();
+        //given
+        Reservation reservation = new Reservation("브라운", "2023-08-05", "15:40");
+        long id = reservationDao.save(reservation);
 
-        var id = RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all().extract().response().jsonPath().getList("id").get(0);
+        //when
         Response deleteResponse = RestAssured.given().log().all()
                 .when().delete("/reservations/" + id)
                 .then().log().all().extract().response();
-        Response getResponse = RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all().extract().response();
 
+        //then
         assertAll(
                 () -> assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.OK.value()),
-                () -> assertThat(getResponse.jsonPath().getList("", ReservationResponse.class)).hasSize(0)
+                () -> assertThat(reservationDao.findAll()).hasSize(0)
         );
     }
 
     @DisplayName("예약 취소 실패 테스트")
     @Test
     void deleteReservationFail() {
+        //given
+        long invalidId = 0;
+
+        //when
         Response response = RestAssured.given().log().all()
-                .when().delete("/reservations/0")
+                .when().delete("/reservations/" + invalidId)
                 .then().log().all().extract().response();
 
+        //then
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
 }

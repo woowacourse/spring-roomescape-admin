e76a375afe228abf29e3f25fb215c2565354213f
nayonsoso <panda0329@naver.com>
2024-04-17 12:55:16 +0900

    refactor: URI 변경 내용 반영
    Co-authored-by: nayonsoso <panda0329@naver.com>
    Co-authored-by: eun-byeol <joeunbyeol98@gmail.com>

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,35 +1,40 @@
 package roomescape;
 
-import static org.hamcrest.Matchers.is;
-
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class MissionStepTest {
 
+    @DisplayName("관리자 메인 페이지 응답 성공")
     @Test
-    void 일단계() {
+    void moveToAdminPage() {
         RestAssured.given().log().all()
-            .when().get("/")
-            .then().log().all()
-            .statusCode(200);
+                .when().get("/admin")
+                .then().log().all()
+                .statusCode(200);
     }
 
+    @DisplayName("예약 페이지 응답 성공")
     @Test
-    void 이단계() {
-        RestAssured.given().log().all()
-            .when().get("/admin/reservation")
-            .then().log().all()
-            .statusCode(200);
+    void moveToReservationPage() {
+        int initialDataSize = 2;
 
         RestAssured.given().log().all()
-            .when().get("/reservations")
-            .then().log().all()
-            .statusCode(200)
-            .body("size()", is(2));
+                .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(initialDataSize));
     }
 }

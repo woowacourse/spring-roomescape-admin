a86d19dfe5b9f258bcce3aa3dc4ed84554ead304
reddevilmidzy <midzy0228@gmail.com>
2024-04-17 14:49:32 +0900

    refactor: @Controller 대신 @RestController로 변경

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -5,27 +5,25 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.ReservationRequestDto;
 import roomescape.dto.ReservationResponseDto;
 import roomescape.model.Reservation;
 
-@Controller
+@RestController
 @RequestMapping("/reservations")
 public class ReservationController {
 
-    private final AtomicLong reservationId = new AtomicLong(1); //TODO 이름 변경하기 ㅎ.ㅎ
+    private final AtomicLong reservationId = new AtomicLong(1); //TODO: 이름 변경하기
     private final List<Reservation> reservations = new ArrayList<>();
 
     @GetMapping
-    @ResponseBody
     public List<ReservationResponseDto> getReservations() {
         return reservations.stream()
                 .map(ReservationResponseDto::from)

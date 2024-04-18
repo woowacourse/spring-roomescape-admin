6de5ef147e14d495ef4611333c44272dcb97d32d
slimsha2dy <chy2581@naver.com>
2024-04-18 16:12:15 +0900

    test: 테스트 분리 및 메서드 명 수정

--- src/test/java/roomescape/RoomescapeRestControllerTest.java
+++ src/test/java/roomescape/RoomescapeRestControllerTest.java
@@ -1,21 +1,21 @@
 package roomescape;
 
+import static org.hamcrest.Matchers.is;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.hamcrest.Matchers.is;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+class RoomescapeRestControllerTest {
     @LocalServerPort
     int port;
 
@@ -24,21 +24,9 @@ public class MissionStepTest {
         RestAssured.port = port;
     }
 
+    @DisplayName("예약 조회 API 테스트")
     @Test
-    void 일단계() {
-        RestAssured.given().log().all()
-                .when().get("/admin")
-                .then().log().all()
-                .statusCode(200);
-    }
-
-    @Test
-    void 이단계() {
-        RestAssured.given().log().all()
-                .when().get("/admin/reservation")
-                .then().log().all()
-                .statusCode(200);
-
+    void lookUpReservation() {
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
@@ -46,8 +34,9 @@ public class MissionStepTest {
                 .body("size()", is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
     }
 
+    @DisplayName("예약 추가 API 테스트")
     @Test
-    void 삼단계() {
+    void addReservation() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
@@ -66,6 +55,22 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
+    }
+
+    @DisplayName("예약 취소 API 테스트")
+    @Test
+    void cancelReservation() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200);
 
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")

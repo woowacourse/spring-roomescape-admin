d1110e58ae03bf265771fec42c5eae4d6f70b6b3
tsulocalize <tsulocalize@gmail.com>
2024-04-18 16:40:59 +0900

    refactor(ReservationRequestDto): 불필요한 변환 코드 삭제

--- src/main/java/roomescape/dto/ReservationRequestDto.java
+++ src/main/java/roomescape/dto/ReservationRequestDto.java
@@ -1,28 +1,22 @@
 package roomescape.dto;
 
+import roomescape.entity.ReservationEntity;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import roomescape.entity.ReservationEntity;
 
 public class ReservationRequestDto {
 
-    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    private static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-
     private final String name;
-    private final String date;
-    private final String time;
+    private final LocalDate date;
+    private final LocalTime time;
 
-    public ReservationRequestDto(String name, String date, String time) {
+    public ReservationRequestDto(String name, LocalDate date, LocalTime time) {
         this.name = name;
         this.date = date;
         this.time = time;
     }
 
     public ReservationEntity toEntity() {
-        LocalDate localDate = LocalDate.parse(date, dateFormatter);
-        LocalTime localTime = LocalTime.parse(time, timeFormatter);
-        return new ReservationEntity(name, localDate, localTime);
+        return new ReservationEntity(name, date, time);
     }
 }

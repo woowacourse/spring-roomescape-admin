4e980d6186fe71733242d0fcabec83843b832d4f
SungKyum Kim <sungkyum1@naver.com>
2024-04-18 14:21:20 +0900

    refactor: 테스트 파일 분리

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -1,4 +1,4 @@
-package roomescape;
+package roomescape.controller;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 
-import javax.sound.sampled.Port;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,7 +16,7 @@ import static org.hamcrest.CoreMatchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+public class ReservationControllerTest {
 
     @LocalServerPort
     int port;
@@ -25,16 +24,6 @@ public class MissionStepTest {
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
-    }
-
-    @Test
-    void 어드민_페이지를_요청하면_200_코드를_반환한다() {
-        testPageStatus("/admin", 200);
-    }
-
-    @Test
-    void 예약_페이지를_요청하면_200_코드를_반환한다() {
-        testPageStatus("/admin/reservation",200);
     }
 
     @Test

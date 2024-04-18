adcccbf8cb6c955fdac15a4420e6498edddc9eef
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-04-18 12:45:26 +0900

    refactor: 컨트롤러 패키지 분류

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,4 +1,4 @@
-package roomescape;
+package roomescape.controller;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.CreateReservationRequest;
 import roomescape.domain.Reservation;
 
 @RestController

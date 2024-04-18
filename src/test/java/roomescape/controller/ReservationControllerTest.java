ea847b2df7396ddf45c86019036a121d3ae1ba9b
takoyakimchi <c4nyou@gmail.com>
2024-04-18 14:04:17 +0900

    test: 테스트 케이스를 DTO로 추가하도록 변경

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -4,12 +4,11 @@ import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import java.util.HashMap;
-import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.controller.dto.ReservationRequestDto;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -28,14 +27,9 @@ class ReservationControllerTest {
     @Test
     @DisplayName("예약을 추가한다.")
     void createReservation() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
-
         RestAssured.given().log().all()
             .contentType(ContentType.JSON)
-            .body(params)
+            .body(new ReservationRequestDto("브라운", "2023-08-05", "15:40"))
             .when().post("/reservations")
             .then().log().all()
             .statusCode(200)
@@ -51,14 +45,9 @@ class ReservationControllerTest {
     @Test
     @DisplayName("예약을 삭제한다.")
     void deleteReservation() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
-
         RestAssured.given().log().all()
             .contentType(ContentType.JSON)
-            .body(params)
+            .body(new ReservationRequestDto("브라운", "2023-08-05", "15:40"))
             .when().post("/reservations")
             .then().log().all()
             .statusCode(200)

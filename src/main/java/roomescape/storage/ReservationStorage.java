c8ee5fa2e5cc76ee592cabea4627af1387d818bd
robinjoon <robin980108@naver.com>
2024-04-18 10:46:59 +0900

    refactor(ReservationStorage): 생성자 추가
    Co-authored-by: jinchiim <kyunellroll@gmail.com>

--- src/main/java/roomescape/storage/ReservationStorage.java
+++ src/main/java/roomescape/storage/ReservationStorage.java
@@ -13,14 +13,19 @@ import roomescape.dto.ReservationRequest;
 @Component
 public class ReservationStorage {
     private final List<Reservation> reservations;
-    private final AtomicLong atomicLong = new AtomicLong(0);
+    private final AtomicLong atomicLong;
 
     public ReservationStorage() {
         this(new ArrayList<>());
     }
 
     public ReservationStorage(List<Reservation> reservations) {
+        this(reservations, new AtomicLong(0));
+    }
+
+    public ReservationStorage(List<Reservation> reservations, AtomicLong atomicLong) {
         this.reservations = reservations;
+        this.atomicLong = atomicLong;
     }
 
     public Reservation save(ReservationRequest reservationRequest) {
@@ -51,6 +56,7 @@ public class ReservationStorage {
                 .ifPresent(reservations::remove);
     }
 
+    //Todo 메서드 제거하고, 외부에서 내부 요소를 주입받을 수 있게 하는 것은 어떤가? TestConfig 에서 하면 될 듯
     public void removeAll() {
         atomicLong.set(0);
         reservations.clear();

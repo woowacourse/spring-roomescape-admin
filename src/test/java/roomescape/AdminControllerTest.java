424e0cc9da42bae3506e78482aec9394531ad2ab
J-I-H-O <jeongjiho0731@gmail.com>
2024-04-18 17:14:47 +0900

    refactor: 테스트 분리
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

---
+++ src/test/java/roomescape/AdminControllerTest.java
@@ -0,0 +1,44 @@
+package roomescape;
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class AdminControllerTest {
+
+    @Test
+    @DisplayName("/ 로 get 요청을 보내면 응답 코드는 200이다.")
+    void getMainPage() {
+        RestAssured.given().log().all()
+                .when().get("/")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("/admin 으로 get 요청을 보내면 응답 코드는 200이다.")
+    void getAdminPage() {
+        RestAssured.given().log().all()
+                .when().get("/admin")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("/admin/reservation 으로 get 요청을 보내면 응답 코드는 200이다.")
+    void getAdminReservationPage() {
+        RestAssured.given().log().all()
+                .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200);
+    }
+}
---
+++ src/test/java/roomescape/ReservationControllerTest.java
@@ -0,0 +1,97 @@
+package roomescape;
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReservationControllerTest {
+
+    private static final Map<String, String> PARAMS = new HashMap<>();
+
+    static {
+        PARAMS.put("name", "썬");
+        PARAMS.put("date", "2024-04-18");
+        PARAMS.put("time", "17:00");
+    }
+
+    @Test
+    @DisplayName("처음으로 등록하는 예약의 id는 1이다.")
+    void firstPost() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(PARAMS)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+    }
+
+    @Test
+    @DisplayName("아무 예약도 하지 않은 경우, 예약 목록 조회 결과 개수는 0개이다.")
+    void readEmptyReservations() {
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @Test
+    @DisplayName("하나의 예약만 등록한 경우, 예약 목록 조회 결과 개수는 1개이다.")
+    void readReservationsSizeAfterFirstPost() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(PARAMS)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+    }
+
+    @Test
+    @DisplayName("값이 없을 때 예약을 삭제하려 시도하는 경우, 상태 코드는 404이다.")
+    void emptyReservationsDelete() {
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(404);
+    }
+
+    @Test
+    @DisplayName("하나의 예약만 등록한 경우, 예약 삭제 뒤 예약 목록 조회 결과 개수는 0개이다.")
+    void readReservationsSizeAfterPostAndDelete() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(PARAMS)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+}

59b5150c2051bb25c1584f574ffb6e8cb1488e53
Hyeon0208 <dmsgktn0208@gmail.com>
2024-04-18 10:55:38 +0900

    refactor : 필드에 final 키워드 추가

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -5,7 +5,7 @@ import java.time.LocalTime;
 
 public class Reservation {
     private final Long id;
-    private String name;
+    private final String name;
     private final LocalDate date;
     private final LocalTime time;
 

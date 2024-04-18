15ee514fbfb5aecc607e52076448500ca89eff43
kelly6bf <kelly6bf@gmail.com>
2024-04-18 15:25:44 +0900

    test: MemoryReservationRepositoryTest 정정

--- src/test/java/roomescape/repository/MemoryReservationRepositoryTest.java
+++ src/test/java/roomescape/repository/MemoryReservationRepositoryTest.java
@@ -1,8 +1,8 @@
 package roomescape.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import roomescape.domain.ClientName;
@@ -17,10 +17,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MemoryReservationRepositoryTest {
-    private final MemoryReservationRepository memoryReservationRepository = new MemoryReservationRepository();
+    private MemoryReservationRepository memoryReservationRepository;
+
+    @BeforeEach
+    public void initRepository() {
+        memoryReservationRepository = new MemoryReservationRepository();
+        Reservation reservation = new Reservation(
+                new ClientName("켈리"),
+                LocalDateTime.of(2023, 1, 12, 1, 12)
+        );
+        memoryReservationRepository.save(reservation);
+    }
 
     @DisplayName("예약 정보를 저장한다.")
-    @Order(1)
     @Test
     void saveTest() {
         // Given
@@ -33,11 +42,10 @@ class MemoryReservationRepositoryTest {
         Reservation savedReservation = memoryReservationRepository.save(reservation);
 
         // Then
-        assertThat(savedReservation.getId()).isEqualTo(1L);
+        assertThat(savedReservation.getId()).isEqualTo(2L);
     }
 
     @DisplayName("예약 정보를 전체 조회한다.")
-    @Order(2)
     @Test
     void findAllTest() {
         // When
@@ -48,7 +56,6 @@ class MemoryReservationRepositoryTest {
     }
 
     @DisplayName("예약 정보를 삭제한다.")
-    @Order(3)
     @Test
     void deleteByIdTest() {
         // When
@@ -60,11 +67,10 @@ class MemoryReservationRepositoryTest {
     }
 
     @DisplayName("존재하지 않는 예약 정보를 삭제하려고하면 예외가 발생한다.")
-    @Order(4)
     @Test
     void throwExceptionWhenDeleteNotExistReservationTest() {
         // When & Then
-        assertThatThrownBy(() -> memoryReservationRepository.deleteById(1L))
+        assertThatThrownBy(() -> memoryReservationRepository.deleteById(2L))
                 .isInstanceOf(NoSuchElementException.class)
                 .hasMessage("존재하지 않는 예약입니다.");
     }

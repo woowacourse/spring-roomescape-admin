369a71feba4e4e15f77c60ca4c9065664305f1db
KimSangHyeok <tkdgur0906@naver.com>
2024-04-18 15:47:47 +0900

    test: SpringBootTest 어노테이션 추가

--- src/test/java/roomescape/controller/AdminControllerTest.java
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -3,7 +3,9 @@ package roomescape.controller;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class AdminControllerTest {
 
     @Test

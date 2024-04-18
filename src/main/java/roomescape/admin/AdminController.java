1a31fa7ec83e1aab2c37cabefb2dfcfd51760f10
gyungchan Jo <namejgc@naver.com>
2024-04-18 13:41:59 +0900

    refactor: 불필요한 의존성 주입 제거

--- src/main/java/roomescape/admin/AdminController.java
+++ src/main/java/roomescape/admin/AdminController.java
@@ -1,24 +1,12 @@
 package roomescape.admin;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import roomescape.reservation.Reservation;
-import roomescape.reservation.ReservationController;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-    private final ReservationController reservationController;
-
-    @Autowired
-    public AdminController(ReservationController reservationController) {
-        this.reservationController = reservationController;
-    }
 
     @GetMapping
     public String admin() {
@@ -26,9 +14,7 @@ public class AdminController {
     }
 
     @GetMapping("/reservation")
-    public String reservation(Model model) {
-        ResponseEntity<List<Reservation>> reservations = reservationController.reservations();
-        model.addAttribute("reservations", reservations.getBody());
+    public String reservation() {
         return "admin/reservation-legacy";
     }
 }

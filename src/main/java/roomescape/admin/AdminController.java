0e906d7dcc93dec2289fe31e56dfde370bc430ee
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-04-18 17:47:23 +0900

    refactor: 불필요 어노테이션 제거

--- src/main/java/roomescape/admin/AdminController.java
+++ src/main/java/roomescape/admin/AdminController.java
@@ -1,7 +1,6 @@
 package roomescape.admin;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +14,6 @@ import roomescape.reservation.ReservationController;
 public class AdminController {
     private final ReservationController reservationController;
 
-    @Autowired
     public AdminController(ReservationController reservationController) {
         this.reservationController = reservationController;
     }

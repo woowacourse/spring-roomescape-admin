1c9f6fd93967335f84187df19cd47d649a6b193c
reddevilmidzy <midzy0228@gmail.com>
2024-04-18 12:56:39 +0900

    fix: AdminControllerTest 테스트 port 설정

--- src/test/java/roomescape/controller/AdminControllerTest.java
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -1,13 +1,25 @@
 package roomescape.controller;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class AdminControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void beforeEach() {
+        RestAssured.port = port;
+    }
 
     @DisplayName("홈 화면 조회")
     @Test

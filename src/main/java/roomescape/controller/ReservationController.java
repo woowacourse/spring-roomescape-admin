9976dcaa26c6425431201795c2210e497013f856
eunjungL <62099953+eunjungL@users.noreply.github.com>
2024-04-18 21:19:45 +0900

    [1 - 3단계 - 방탈출 예약 관리] 클로버(이은정) 미션 제출합니다. (#29)
    * docs: README.md 1단계 요구사항 작성
    
    * feat: admin page 구현
    
    * docs: README.md 2단계 요구사항 작성
    
    * feat: 에약 조회 api 구현
    
    * docs: README.md 3단계 요구사항 작성
    
    * feat: 예약 추가 및 삭제 API 구현
    
    * refactor: 메서드 네이밍 수정
    
    * fix: index.html에서 reservation 버튼 연결 링크 수정
    
    * fix: Reservation의 getter 다시 추가
    
    * refactor: ReservationController의 reservations를 동기화 리스트로 변경
    
    * refactor: Controller requestMapping에 불필요한 괄호 제거
    
    * test: 테스트 메서드명 수정 및 DisplayName 추가
    
    * test: 테스트 환경에서 랜덤 포트 사용하게 수정
    
    * refactor: MissionStepTest의 setUp() method public 제거

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,37 @@
+package roomescape.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import roomescape.domain.Reservation;
+import roomescape.controller.dto.ReservationCreateRequest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+
+    private final AtomicInteger atomicInteger = new AtomicInteger(1);
+    private final List<Reservation> reservations = Collections.synchronizedList(new ArrayList<>());
+
+    @GetMapping
+    public ResponseEntity<List<Reservation>> readReservations() {
+        return ResponseEntity.ok(reservations);
+    }
+
+    @PostMapping
+    public ResponseEntity<Reservation> createReservation(@RequestBody ReservationCreateRequest request) {
+        Reservation reservation = request.toReservation(atomicInteger.getAndIncrement());
+        reservations.add(reservation);
+        return ResponseEntity.ok(reservation);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable int id) {
+        reservations.removeIf((reservation) -> reservation.getId() == id);
+        return ResponseEntity.ok().build();
+    }
+}
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,20 +1,109 @@
 package roomescape;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @DisplayName("/admin으로 GET 요청이 들어오면 어드민 페이지를 반환한다.")
     @Test
-    void 일단계() {
+    void readAdminPage() {
         RestAssured.given().log().all()
-                .when().get("/")
+                .when().get("/admin")
                 .then().log().all()
                 .statusCode(200);
     }
 
+    @DisplayName("/admin/reservation으로 GET 요청이 들어오면 예약 목록을 조회한다.")
+    @Test
+    void readReservations() {
+        RestAssured.given().log().all()
+                .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @DisplayName("/reservations로 POST 요청이 들어오면 예약을 생성한다.")
+    @Test
+    void createReservation() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+    }
+
+    @DisplayName("/reservations로 DELETE 요청이 들어오면 예약을 삭제한다.")
+    @Test
+    void deleteReservation() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
 }

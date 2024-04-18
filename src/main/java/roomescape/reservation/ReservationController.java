b64f85db613c7a48584e667391917b0010c27175
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-04-18 18:50:37 +0900

    refactor: 불필요 final 제거

--- src/main/java/roomescape/reservation/ReservationController.java
+++ src/main/java/roomescape/reservation/ReservationController.java
@@ -18,7 +18,7 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
-    public ReservationController(final ReservationService reservationService) {
+    public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
     }
 

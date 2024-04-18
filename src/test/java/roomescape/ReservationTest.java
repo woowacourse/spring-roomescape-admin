23da99d52452386ed01ff405c9ccb4bfa0afbcb5
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-04-18 23:45:43 +0900

    [1 - 3단계 - 방탈출 예약 관리] 러쉬(이태훈) 미션 제출합니다. (#37)
    * docs(README.md): 구현할 기능 목록 작성
    
    * feat(RoomescapeController): 어드민 메인 페이지 응답 기능 구현
    
    * feat: 예약 관리 페이지 및 조회 기능
    
    * feat(RoomescapeController): 예약 추가 API 기능 구현
    
    * feat(RoomescapeController): 예약 취소 api 기능 구현
    
    * fix: 기능 변경에 따른 테스트 값 변경
    
    * refactor(RoomescapeController):  모든 예약 조회 api ResponseEntity 사용하도록 수정
    
    * refactor(RoomescapeController): 에러 메시지 추가
    
    * refactor(Reservation): Member class명 변경
    
    * fix(reservation-legacy.html): 경로 수정
    
    * feat(ReservationRequest): Reservation 요청 dto 생성
    
    * refactor(ReservationTest): ReservationTest 테스트 분리 및 수정
    
    * refactor(ReservationController): Controller 명 수정
    
    * refactor(AdminController): Admin Controller 분리
    
    * refactor(ReservationController): final 추가
    
    * refactor(ReservationTest): 주석제거
    
    * style: 문자열 포맷팅
    
    ---------
    
    Co-authored-by: lemone <lejinhy@gmail.com>

---
+++ src/test/java/roomescape/ReservationTest.java
@@ -0,0 +1,89 @@
+package roomescape;
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import roomescape.dto.ReservationRequest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class ReservationTest {
+
+    @DisplayName("어드민 메인 페이지 테스트")
+    @Test
+    void adminMainPage() {
+        RestAssured.given().log().all()
+                .when().get("/admin")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @DisplayName("예약 조회 API 테스트")
+    @Test
+    void readReservations() {
+        RestAssured.given().log().all()
+                .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @DisplayName("예약 추가 API 테스트")
+    @Test
+    void createReservation() {
+        ReservationRequest reservationRequest = new ReservationRequest("브라운", LocalDate.of(2023, 8, 5),
+                LocalTime.of(15, 40));
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationRequest)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+    }
+
+    @DisplayName("예약 취소 API 테스트")
+    @Test
+    void deleteReservation() {
+        ReservationRequest reservationRequest = new ReservationRequest("브라운", LocalDate.of(2023, 8, 5),
+                LocalTime.of(15, 40));
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationRequest)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+}

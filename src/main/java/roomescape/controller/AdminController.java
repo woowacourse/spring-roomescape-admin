c21deefe87694a54ca92fecdad846af9ccf3c89b
3juhwan <13selfesteem91@naver.com>
2024-04-18 15:30:54 +0900

    refactor: admin api의 uri prefix 공통 처리

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -2,15 +2,17 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
-    @GetMapping("/admin")
+    @GetMapping
     public String home() {
         return "admin/index";
     }
 
-    @GetMapping("/admin/reservation")
+    @GetMapping("/reservation")
     public String reservations() {
         return "admin/reservation-legacy";
     }

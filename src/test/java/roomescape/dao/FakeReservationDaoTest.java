82513f4d30d08dbd08aea7b5f60a34054c5e2549
HyungHoKim00 <rlagudgh1109@gmail.com>
2024-04-18 14:15:03 +0900

    test: 단위 테스트 추가

---
+++ src/test/java/roomescape/dao/FakeReservationDaoTest.java
@@ -0,0 +1,57 @@
+package roomescape.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import roomescape.domain.Reservation;
+
+public class FakeReservationDaoTest {
+    private ReservationDao fakeReservationDao;
+
+    @BeforeEach
+    void init() {
+        fakeReservationDao = new FakeReservationDao();
+    }
+
+    @DisplayName("존재하는 모든 entity를 보여준다.")
+    @Test
+    void findAll() {
+        assertThat(fakeReservationDao.findAll()).isEmpty();
+    }
+
+    @DisplayName("도메인을 저장한다.")
+    @Test
+    void save() {
+        //given
+        Reservation reservation = new Reservation("aa", "2023-10-10", "10:00");
+        //when
+        fakeReservationDao.save(reservation);
+        //then
+        assertThat(fakeReservationDao.findAll()).hasSize(1);
+    }
+
+    @DisplayName("해당 id의 도메인이 존재하는지 판별한다.")
+    @Test
+    void existsById() {
+        //given
+        Reservation reservation = new Reservation("aa", "2023-10-10", "10:00");
+        //when
+        fakeReservationDao.save(reservation);
+        //then
+        assertThat(fakeReservationDao.existsById(1)).isTrue();
+    }
+
+    @DisplayName("해당 id의 도메인을 삭제한다.")
+    @Test
+    void deleteById() {
+        //given
+        Reservation reservation = new Reservation("aa", "2023-10-10", "10:00");
+        fakeReservationDao.save(reservation);
+        //when
+        fakeReservationDao.deleteById(1);
+        //then
+        assertThat(fakeReservationDao.existsById(1)).isFalse();
+    }
+}

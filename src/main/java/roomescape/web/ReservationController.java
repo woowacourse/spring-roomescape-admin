074731e38e708c295beb224346c6bab05030aea7
Choo <choo000407@naver.com>
2024-04-18 20:48:00 +0900

    refactor: `Controller` 대신 `RestController` 사용하게 수정

--- src/main/java/roomescape/web/ReservationController.java
+++ src/main/java/roomescape/web/ReservationController.java
@@ -2,20 +2,20 @@ package roomescape.web;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.service.ReservationService;
 import roomescape.web.dto.ReservationFindAllResponse;
 import roomescape.web.dto.ReservationFindResponse;
 import roomescape.web.dto.ReservationSaveRequest;
 
 @RequestMapping("/reservations")
-@Controller
+@RestController
 public class ReservationController {
     private final ReservationService reservationService;
 

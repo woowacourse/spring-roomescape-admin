ab45376bee48dd418dcaefd2ad8841adcd3df7b8
jongmee <ra123221@gmail.com>
2024-04-18 13:06:43 +0900

    feat(Reservation): Reservation 필드 검증

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -3,17 +3,36 @@ package roomescape.domain;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Reservation {
+    private static final Pattern NAME_PATTERN = Pattern.compile("^\\d+$");
+
     private Long id;
     private final String name;
     private final LocalDate date;
     private final LocalTime time;
 
     public Reservation(String name, LocalDate date, LocalTime time) {
+        validateName(name);
+        validateDateTime(date, time);
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    private void validateName(String name) {
+        Matcher matcher = NAME_PATTERN.matcher(name);
+        if (matcher.matches()) {
+            throw new IllegalArgumentException("예약자 이름은 숫자로만 구성될 수 없습니다.");
+        }
+    }
+
+    private void validateDateTime(LocalDate date, LocalTime time) {
+        if (date == null || time == null) {
+            throw new IllegalArgumentException("예약은 시간과 날짜가 null일 수 없습니다.");
+        }
     }
 
     public void initializeId(Long id) {
--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -11,6 +11,39 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ReservationTest {
 
     @Test
+    @DisplayName("예약자 이름은 숫자로만 구성될 수 없다.")
+    void validateName() {
+        // given
+        String invalidName = "123";
+
+        // when & then
+        assertThatThrownBy(() -> new Reservation(invalidName, LocalDate.now(), LocalTime.now()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("예약 날짜는 null일 수 없다.")
+    void validateDate() {
+        // given
+        LocalDate invalidDate = null;
+
+        // when & then
+        assertThatThrownBy(() -> new Reservation("미아", invalidDate, LocalTime.now()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("예약 시간은 null일 수 없다.")
+    void validateTime() {
+        // given
+        LocalTime invalidTime = null;
+
+        // when & then
+        assertThatThrownBy(() -> new Reservation("미아", LocalDate.now(), invalidTime))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     @DisplayName("이미 초기화된 예약 ID를 초기화(수정)할 경우 예외가 발생한다.")
     void initializeId() {
         // given
@@ -21,5 +54,4 @@ class ReservationTest {
         assertThatThrownBy(() -> reservation.initializeId(2L))
                 .isInstanceOf(IllegalStateException.class);
     }
-
 }

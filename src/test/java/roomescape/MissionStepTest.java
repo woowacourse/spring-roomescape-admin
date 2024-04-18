2bf89dc9a4300b643ff2d108e66c39fc340878ff
gyungchan Jo <namejgc@naver.com>
2024-04-18 14:38:32 +0900

    test: 테스트 분리 및 테스트 메서드 변경

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -6,6 +6,8 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -15,7 +17,8 @@ import org.springframework.test.annotation.DirtiesContext;
 public class MissionStepTest {
 
     @Test
-    void 일단계() {
+    @DisplayName("어드민 페이지를 조회한다.")
+    void readAdminPage() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
@@ -23,12 +26,17 @@ public class MissionStepTest {
     }
 
     @Test
-    void 이단계() {
+    @DisplayName("어드민 예약 페이지를 조회한다.")
+    void readAdminReservationPage() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
+    }
 
+    @Test
+    @DisplayName("예약 정보를 조회한다.")
+    void readReservations() {
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
@@ -37,7 +45,8 @@ public class MissionStepTest {
     }
 
     @Test
-    void 삼단계() {
+    @DisplayName("예약을 추가한다.")
+    void createReservation() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
@@ -56,6 +65,23 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
+    }
+
+    @Test
+    @DisplayName("예약을 삭제한다.")
+    void deleteReservation() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
 
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")

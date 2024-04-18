0df8575347572fbe4f75da29f2abd077570d5ef1
SungKyum Kim <sungkyum1@naver.com>
2024-04-18 14:17:27 +0900

    refactor: 테스트 분리, 랜덤포트 사용으로 변경

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -2,43 +2,48 @@ package roomescape;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 
+import javax.sound.sampled.Port;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
-    @Test
-    void 일단계() {
-        RestAssured.given().log().all()
-                .when().get("/admin")
-                .then().log().all()
-                .statusCode(200);
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
     }
 
     @Test
-    void 이단계() {
-        RestAssured.given().log().all()
-                .when().get("/admin/reservation")
-                .then().log().all()
-                .statusCode(200);
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
+    void 어드민_페이지를_요청하면_200_코드를_반환한다() {
+        testPageStatus("/admin", 200);
     }
 
     @Test
-    void 삼단계() {
+    void 예약_페이지를_요청하면_200_코드를_반환한다() {
+        testPageStatus("/admin/reservation",200);
+    }
+
+    @Test
+    void 예약_목록을_요청하면_200_코드와_데이터를_반환한다() {
+        testResponseBodyDataSize("/reservations", 200, 0);
+    }
+
+    @Test
+    void 예약추가_및_삭제를_수행한다() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
@@ -52,21 +57,26 @@ public class MissionStepTest {
                 .statusCode(200)
                 .body("id", is(1));
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
+        testResponseBodyDataSize("/reservations", 200, 1);
+
 
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
                 .statusCode(200);
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
+        testResponseBodyDataSize("/reservations", 200, 0);
+    }
+
+    private void testResponseBodyDataSize(String path, int expectedStatusCode, int size) {
+        testPageStatus(path, expectedStatusCode)
+                .body("size()", is(size));
+    }
+
+    private ValidatableResponse testPageStatus(String path, int expectedStatusCode) {
+        return RestAssured.given().log().all()
+                .when().get(path)
                 .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
+                .statusCode(expectedStatusCode);
     }
 }

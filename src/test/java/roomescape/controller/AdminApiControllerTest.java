1f25a8145f06818d458efbc49b69a3c51547c723
kelly6bf <kelly6bf@gmail.com>
2024-04-18 16:13:26 +0900

    test: AdminApiControllerTest 테스트 단위 분리

--- src/test/java/roomescape/controller/AdminApiControllerTest.java
+++ src/test/java/roomescape/controller/AdminApiControllerTest.java
@@ -2,6 +2,7 @@ package roomescape.controller;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +16,33 @@ import static org.hamcrest.Matchers.is;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class AdminApiControllerTest {
-    @DisplayName("예약 정보를 저장하고 삭제한다.")
+
+    @BeforeEach
+    public void initReservation() {
+        for (int i = 0; i < 4; i++) {
+            Map<String, String> params = new HashMap<>();
+            params.put("name", "브라운");
+            params.put("date", "2023-08-05");
+            params.put("time", "15:40");
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/reservations");
+        }
+    }
+
+    @DisplayName("전체 예약 정보를 조회한다.")
+    @Test
+    void getReservationsTest() {
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(4));
+    }
+
+    @DisplayName("예약 정보를 저장한다.")
     @Test
     void saveReservationTest() {
         Map<String, String> params = new HashMap<>();
@@ -29,23 +56,15 @@ class AdminApiControllerTest {
                 .when().post("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("id", is(1));
+                .body("id", is(5));
+    }
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
-
+    @DisplayName("예약 정보를 삭제한다.")
+    @Test
+    void deleteReservationTest() {
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
                 .statusCode(200);
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
     }
 }

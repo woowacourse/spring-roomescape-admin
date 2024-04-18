e36be3c97d3e94720ed04cb7ee20e73ffce610ad
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-04-18 13:57:21 +0900

    refactor: dto 계층 분리

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.CreateReservationRequest;
 import roomescape.domain.Reservation;
+import roomescape.dto.CreateReservationRequest;
 
 @RestController
 @RequestMapping("/reservations")

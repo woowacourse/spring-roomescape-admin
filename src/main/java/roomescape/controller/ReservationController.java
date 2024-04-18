320a11bcd8613f2d7f43143aac425b0ae846254f
Minjoo <m0522inju@gmail.com>
2024-04-18 13:35:06 +0900

    refactor: @RestController 어노테이션 사용

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,18 +2,18 @@ package roomescape.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.reservation.Reservation;
 import roomescape.domain.reservation.repository.ReservationRepository;
 import roomescape.dto.ReservationDto;
 
-@Controller
+@RestController
 @RequestMapping("/reservations")
 public class ReservationController {
     private final ReservationRepository reservationRepository;

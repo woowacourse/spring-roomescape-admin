64cf9d775f46d74e0da9fa49fe78e20913c035ef
zangsu <zangsu_@naver.com>
2024-04-18 13:06:23 +0900

    refactor(MissionStepTest) : 가독성 개선
    - Map.of() 를 사용해 픽스쳐 생성
    - 테스트에서 불필요한 Step 구분 제거

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -4,10 +4,8 @@ import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -16,77 +14,66 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class MissionStepTest {
 
-    @Nested
-    class 일단계 {
-
-        @Test
-        @DisplayName("메인 페이지 조회")
-        void getMainPageTest() {
-            RestAssured.given().log().all()
-                    .when().get("/admin")
-                    .then().log().all()
-                    .statusCode(200);
-        }
+    @Test
+    @DisplayName("메인 페이지 조회")
+    void getMainPageTest() {
+        RestAssured.given().log().all()
+                .when().get("/admin")
+                .then().log().all()
+                .statusCode(200);
     }
 
-    @Nested
-    class 이단계 {
-
-        @Test
-        @DisplayName("예약 페이지 조회")
-        void getReservationPageTest() {
-            RestAssured.given().log().all()
-                    .when().get("/admin/reservation")
-                    .then().log().all()
-                    .statusCode(200);
-        }
-
-        @Test
-        @DisplayName("예약 목록 조회")
-        void getReservationsTest() {
-            RestAssured.given().log().all()
-                    .when().get("/reservations")
-                    .then().log().all()
-                    .statusCode(200)
-                    .body("size()", is(0));
-        }
+    @Test
+    @DisplayName("예약 페이지 조회")
+    void getReservationPageTest() {
+        RestAssured.given().log().all()
+                .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200);
     }
 
-    @Nested
-    class 삼단계 {
+    @Test
+    @DisplayName("예약 목록 조회")
+    void getReservationsTest() {
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
 
-        @DisplayName("예약 추가 및 삭제 테스트")
-        @Test
-        void createAndDeleteReservation() {
-            Map<String, String> params = new HashMap<>();
-            params.put("name", "브라운");
-            params.put("date", "2023-08-05");
-            params.put("time", "15:40");
+    @DisplayName("예약 추가 및 삭제 테스트")
+    @Test
+    void createAndDeleteReservation() {
+        Map<String, String> params = Map.of(
+                "name", "브라운",
+                "date", "2023-08-05",
+                "time", "15:40"
+        );
 
-            RestAssured.given().log().all()
-                    .contentType(ContentType.JSON)
-                    .body(params)
-                    .when().post("/reservations")
-                    .then().log().all()
-                    .statusCode(200)
-                    .body("id", is(1));
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
 
-            RestAssured.given().log().all()
-                    .when().get("/reservations")
-                    .then().log().all()
-                    .statusCode(200)
-                    .body("size()", is(1));
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
 
-            RestAssured.given().log().all()
-                    .when().delete("/reservations/1")
-                    .then().log().all()
-                    .statusCode(200);
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
 
-            RestAssured.given().log().all()
-                    .when().get("/reservations")
-                    .then().log().all()
-                    .statusCode(200)
-                    .body("size()", is(0));
-        }
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
     }
 }

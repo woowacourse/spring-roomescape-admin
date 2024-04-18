16a398bf3e345ad622ed72b7bd1ccf9b6e8c0ebd
3juhwan <13selfesteem91@naver.com>
2024-04-18 11:28:40 +0900

    fix: API 포맷 변경 HH:mm:ss -> HH:mm

--- src/main/java/roomescape/dto/ReservationDto.java
+++ src/main/java/roomescape/dto/ReservationDto.java
@@ -2,9 +2,11 @@ package roomescape.dto;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import roomescape.domain.Reservation;
 
-public record ReservationDto(long id, String name, LocalDate date, LocalTime time) {
+public record ReservationDto(long id, String name, LocalDate date, @JsonFormat(pattern = "HH:mm") LocalTime time) {
     public static ReservationDto from(Reservation reservation) {
         return new ReservationDto(
                 reservation.getId(),

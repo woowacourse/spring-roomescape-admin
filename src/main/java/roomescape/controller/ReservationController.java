52ece42017f3fee1098bb68feaa56488f198052c
zangsu <zangsu_@naver.com>
2024-04-18 12:54:51 +0900

    refactor(ReservationController) : ResponseEntity 를 제거하고 @ResponseStatus 사용

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 
@@ -22,25 +23,25 @@ public class ReservationController {
     private final AtomicLong index = new AtomicLong(1);
 
     @GetMapping
-    public ResponseEntity<List<FindReservationResponse>> getReservation() {
-        List<FindReservationResponse> reservationsDtos = reservations.stream()
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<FindReservationResponse> getReservation() {
+        return reservations.stream()
                 .map(FindReservationResponse::of)
                 .toList();
-        return ResponseEntity.ok(reservationsDtos);
     }
 
     @PostMapping
-    public ResponseEntity<CreateReservationResponse> createReservation(
+    @ResponseStatus(value = HttpStatus.OK)
+    public CreateReservationResponse createReservation(
             @RequestBody CreateReservationRequest createReservationRequest) {
         Reservation newReservation = createReservationRequest.to(index.getAndIncrement());
         reservations.add(newReservation);
-        return ResponseEntity.ok()
-                .body(CreateReservationResponse.of(newReservation));
+        return CreateReservationResponse.of(newReservation);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+    @ResponseStatus(value = HttpStatus.OK)
+    public void deleteReservation(@PathVariable Long id) {
         reservations.removeIf(reservation -> Objects.equals(reservation.getId(), id));
-        return ResponseEntity.ok().build();
     }
 }

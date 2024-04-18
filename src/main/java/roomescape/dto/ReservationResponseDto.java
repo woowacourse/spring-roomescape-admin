506c641ceb31f8be3871fcb0062d9fc7dea80157
mzeong <mjeong@sookmyung.ac.kr>
2024-04-18 20:13:27 +0900

    refactor: Reservation id에 Long 대신 long을 사용하도록 수정

--- src/main/java/roomescape/dto/ReservationResponseDto.java
+++ src/main/java/roomescape/dto/ReservationResponseDto.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import roomescape.domain.Reservation;
 
-public record ReservationResponseDto(Long id, String name, LocalDate date, LocalTime time) {
+public record ReservationResponseDto(long id, String name, LocalDate date, LocalTime time) {
 
     public ReservationResponseDto(Reservation reservation) {
         this(reservation.getId(), reservation.getName(), reservation.getDate(), reservation.getTime());

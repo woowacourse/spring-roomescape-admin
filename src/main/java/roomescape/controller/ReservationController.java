3120356957b09be42dd362667ffb965026c9ec82
jinwoo22 <jenny010925@naver.com>
2024-04-18 20:12:34 +0900

    refactor: @Controller와 @RestController 사용 클래스 분리

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,8 +2,6 @@ package roomescape.controller;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import roomescape.domain.Reservation;
 import roomescape.domain.Reservations;
@@ -11,42 +9,30 @@ import roomescape.dto.request.ReservationCreateRequest;
 import java.util.ArrayList;
 import roomescape.dto.response.ReservationResponse;
 
-@Controller
+@RestController
 public class ReservationController {
 
     private final AtomicLong id = new AtomicLong(0);
     private final Reservations reservations = new Reservations(new ArrayList<>());
 
-    @GetMapping("/admin")
-    public String getAdminPage() {
-        return "/admin/index";
-    }
-
-    @GetMapping("/admin/reservation")
-    public String getReservationPage() {
-        return "/admin/reservation-legacy";
-    }
-
     @GetMapping("/reservations")
-    public ResponseEntity<List<ReservationResponse>> getReservations() {
-        List<ReservationResponse> reservationResponses = reservations.getReservations()
+    public List<ReservationResponse> getReservations() {
+        return reservations.getReservations()
                 .stream()
-                .map(ReservationResponse::fromReservation).toList();
-        return ResponseEntity.ok(reservationResponses);
+                .map(ReservationResponse::fromReservation)
+                .toList();
     }
 
     @PostMapping("/reservations")
-    public ResponseEntity<ReservationResponse> createReservations(@RequestBody ReservationCreateRequest reservationCreateRequest) {
+    public ReservationResponse createReservations(@RequestBody ReservationCreateRequest reservationCreateRequest) {
         Reservation reservation = reservationCreateRequest.toReservation(id.incrementAndGet());
         reservations.add(reservation);
 
-        ReservationResponse reservationResponse = ReservationResponse.fromReservation(reservation);
-        return ResponseEntity.ok(reservationResponse);
+        return ReservationResponse.fromReservation(reservation);
     }
 
     @DeleteMapping("/reservations/{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
+    public void deleteReservation(@PathVariable("id") Long id) {
         reservations.remove(id);
-        return ResponseEntity.ok().build();
     }
 }

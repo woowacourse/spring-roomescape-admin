8e188ddcd2882e31c62ad2e148aca2cbcf89758c
Minjoo <m0522inju@gmail.com>
2024-04-18 13:45:08 +0900

    refactor: 테스트 메서드 명 변경 및 DisplayName 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -4,18 +4,22 @@ import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.dto.ReservationDto;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+class MissionStepTest {
 
     @Test
-    void 일단계() {
+    @DisplayName("'/admin'으로 접속하면 관리자 페이지를 보여준다.")
+    void printAdminView() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
@@ -23,12 +27,17 @@ public class MissionStepTest {
     }
 
     @Test
-    void 이단계() {
+    @DisplayName("'/admin/reservation'으로 접속하면 예약 페이지를 보여준다.")
+    void printReservationView() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
+    }
 
+    @Test
+    @DisplayName("현재 존재하는 모든 예약을 반환한다.")
+    void findAllReservations() {
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
@@ -37,11 +46,11 @@ public class MissionStepTest {
     }
 
     @Test
-    void 삼단계() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
+    @DisplayName("예약을 생성하고 삭제한다.")
+    void createReservationAndDelete() {
+        ReservationDto params = new ReservationDto(
+                "브라운", LocalDate.of(2023, 8, 5), LocalTime.of(15, 40)
+        );
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)

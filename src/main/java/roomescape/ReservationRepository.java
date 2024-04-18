c4eb0c8e7ab2875ccf0c154e0cad214e70432164
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-04-18 16:31:22 +0900

    feat(ReservationRepository): 데이터베이스를 이용한 조회 기능 구현

--- src/main/java/roomescape/ReservationRepository.java
+++ src/main/java/roomescape/ReservationRepository.java
@@ -3,16 +3,30 @@ package roomescape;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ReservationRepository {
 
+    private static final String TABLE_NAME = "reservation";
+    private static final RowMapper<Reservation> ROW_MAPPER = (resultSet, rowNum) -> new Reservation(
+            resultSet.getLong("id"),
+            resultSet.getString("name"),
+            resultSet.getString("date"),
+            resultSet.getString("time"));
+
     private final List<Reservation> reservations = new ArrayList<>();
     private final AtomicLong autoIncreaseId = new AtomicLong(1);
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public List<Reservation> findAll() {
-        return List.copyOf(reservations);
+        return jdbcTemplate.query("SELECT id, name, date, time FROM %s".formatted(TABLE_NAME), ROW_MAPPER);
     }
 
     public Reservation create(Reservation reservation) {
@@ -27,10 +41,13 @@ public class ReservationRepository {
     }
 
     private Reservation findById(Long id) {
-        return reservations.stream()
-                .filter(reservation -> reservation.id().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException("해당 예약이 저장되어 있지 않습니다."));
+        Reservation reservation = jdbcTemplate.queryForObject("SELECT id, name, date, time FROM %s WHERE id = ?",
+                ROW_MAPPER, id);
+
+        if (reservation == null) {
+            throw new IllegalStateException("해당 예약이 없습니다.");
+        }
+        return reservation;
     }
 
 }

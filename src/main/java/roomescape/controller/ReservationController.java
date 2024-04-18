7630c1b9418c41223d5b598df42fbde3c925c067
ehtjsv2 <ehtjsv2@naver.com>
2024-04-18 12:05:35 +0900

    refactor: ReservationController RestController로 변경

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -5,32 +5,32 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationDto;
 
-@Controller
+@RestController
 public class ReservationController {
 
     private final List<Reservation> reservations = new ArrayList<>();
     private final AtomicLong index = new AtomicLong(1);
 
     @GetMapping("/reservations")
-    public ResponseEntity<List<Reservation>> reservationList() {
-        return ResponseEntity.ok(reservations);
+    public List<Reservation> reservationList() {
+        return reservations;
     }
 
     @PostMapping("/reservations")
-    public ResponseEntity<Reservation> reservationAdd(@RequestBody ReservationDto reservationDto) {
+    public Reservation reservationAdd(@RequestBody ReservationDto reservationDto) {
         Reservation reservation = new Reservation(index.getAndIncrement(), reservationDto.getName(),
                 reservationDto.getDate(), reservationDto.getTime());
         reservations.add(reservation);
-        return ResponseEntity.ok(reservation);
+        return reservation;
     }
 
     @DeleteMapping("/reservations/{id}")

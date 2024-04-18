b6a35373b464502edb971cdaa2e3217dc9b26034
Hyeon0208 <dmsgktn0208@gmail.com>
2024-04-18 14:51:33 +0900

    refactor : dto를 record 클래스로 변경

--- src/main/java/roomescape/reservation/ReservationRequest.java
+++ src/main/java/roomescape/reservation/ReservationRequest.java
@@ -3,16 +3,7 @@ package roomescape.reservation;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class ReservationRequest {
-    private final String name;
-    private final LocalDate date;
-    private final LocalTime time;
-
-    public ReservationRequest(final String name, final LocalDate date, final LocalTime time) {
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
+public record ReservationRequest(String name, LocalDate date, LocalTime time) {
 
     public Reservation toVo(final Long id) {
         return new Reservation(id, name, date, time);

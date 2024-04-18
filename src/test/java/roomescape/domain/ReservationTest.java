84e72059978a4c77e2dc6612879cdea58ee01988
Eugene Jang <kyunellroll@gmail.com>
2024-04-18 13:27:38 +0900

    test(ReservationTest): 같은 값의 id를 가지는지 확인하는 테스트 작성

--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -1,9 +1,12 @@
 package roomescape.domain;
 
-import java.time.LocalDateTime;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ReservationTest {
     @Test
@@ -12,7 +15,19 @@ class ReservationTest {
         Reservation first = new Reservation(1L, "폴라", LocalDateTime.of(1999, 12, 1, 16, 30));
         Reservation second = new Reservation(2L, "로빈", LocalDateTime.of(1998, 1, 8, 10, 30));
         int compareTo = first.compareTo(second);
+
         Assertions.assertThat(compareTo)
                 .isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("전달된 id와 같은 값의 id인지 확인.")
+    void hasSameId() {
+        Reservation reservation = new Reservation(1L, "폴라", LocalDateTime.of(1999, 12, 1, 16, 30));
+
+        assertAll(
+                () -> Assertions.assertThat(reservation.hasSameId(2L)).isFalse(),
+                () -> Assertions.assertThat(reservation.hasSameId(1L)).isTrue()
+        );
     }
 }

4dbbdd7367462df0060f3a6341b861b910838a12
gyungchan Jo <namejgc@naver.com>
2024-04-18 13:53:28 +0900

    refactor: ReservationController어노테이션 변경
    @Controller -> @RestController로 변경

--- src/main/java/roomescape/reservation/ReservationController.java
+++ src/main/java/roomescape/reservation/ReservationController.java
@@ -1,32 +1,24 @@
 package roomescape.reservation;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@RestController
 @RequestMapping("/reservations")
 public class ReservationController {
     private final List<Reservation> reservations = new ArrayList<>();
     private final AtomicLong atomicLong = new AtomicLong();
 
     @GetMapping
-    @ResponseBody
     public ResponseEntity<List<Reservation>> reservations() {
         return ResponseEntity.ok(reservations);
     }
 
     @PostMapping
-    @ResponseBody
     public ResponseEntity<Reservation> create(@RequestBody ReservationRequest reservationRequest) {
         Reservation reservation = new Reservation(
                 atomicLong.incrementAndGet(),

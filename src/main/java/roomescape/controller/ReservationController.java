c04766b58965816e0ff30ef0fef12f1aefcb53d5
BurningFalls <sherlocky31415@gmail.com>
2024-04-18 15:12:59 +0900

    refactor: DELETE API method도 RestController annotation으로 통일

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,7 +1,5 @@
 package roomescape.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import roomescape.controller.dto.ReservationDto;
 import roomescape.entity.Reservation;
@@ -14,20 +12,18 @@ import java.util.NoSuchElementException;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RequestMapping("/reservations")
-@Controller
+@RestController
 public class ReservationController {
-
+    
     private List<Reservation> reservations = new ArrayList<>();
     private final AtomicLong index = new AtomicLong(1);
 
     @GetMapping
-    @ResponseBody
     public List<Reservation> readAllReservations() {
         return reservations;
     }
 
     @PostMapping
-    @ResponseBody
     public Reservation createReservation(@RequestBody ReservationDto reservationDto) {
         long id = index.getAndIncrement();
         String name = reservationDto.name();
@@ -40,12 +36,11 @@ public class ReservationController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteReservationById(@PathVariable("id") long id) {
+    public void deleteReservationById(@PathVariable("id") long id) {
         Reservation findReservation = reservations.stream()
                 .filter(reservation -> reservation.getId() == id)
                 .findAny()
                 .orElseThrow(() -> new NoSuchElementException("id에 해당하는 예약을 찾을 수 없습니다: " + id));
         reservations.remove(findReservation);
-        return ResponseEntity.ok().build();
     }
 }

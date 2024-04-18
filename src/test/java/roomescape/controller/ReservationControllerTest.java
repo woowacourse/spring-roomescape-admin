caff1ae18195138e48c0421d25650691fbd1294a
jongmee <ra123221@gmail.com>
2024-04-18 14:21:53 +0900

    test(ReservationControllerTest): ReservationController 슬라이싱 테스트

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -1,25 +1,87 @@
 package roomescape.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import roomescape.repository.ReservationRepository;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import roomescape.domain.Reservation;
+import roomescape.dto.ReservationSaveRequest;
 import roomescape.service.ReservationService;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
 class ReservationControllerTest {
-    private final ReservationRepository reservationRepository = new ReservationRepository();
-    private final ReservationService reservationService = new ReservationService(reservationRepository);
-    private final ReservationController reservationController = new ReservationController(reservationService);
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private ReservationService reservationService;
 
     @Test
-    @DisplayName("존재하지 않는 예약 ID로 삭제를 요청할 경우 예외가 발생한다.")
-    void deleteReservation() {
+    @DisplayName("예약 목록 GET 요청 시 상태코드 200을 반환한다.")
+    void getReservations() throws Exception {
         // given
-        Long reservationId = 1L;
+        BDDMockito.given(reservationService.getReservations())
+                .willReturn(List.of(new Reservation("미아", LocalDate.now(), LocalTime.now())));
 
         // when & then
-        assertThatThrownBy(() -> reservationController.deleteReservation(reservationId))
-                .isInstanceOf(IllegalArgumentException.class);
+        mockMvc.perform(get("/reservations").contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("미아"))
+                .andExpect(jsonPath("$[0].date").value(LocalDate.now().toString()));
+    }
+
+    @Test
+    @DisplayName("예약 POST 요청 시 상태코드 200을 반환한다.")
+    void createReservation() throws Exception {
+        // given
+        ReservationSaveRequest request = new ReservationSaveRequest("미아", LocalDate.now(), LocalTime.now());
+        Reservation expectedReservation = request.toReservation();
+
+        BDDMockito.given(reservationService.createReservation(any()))
+                .willReturn(expectedReservation);
+
+        // when & then
+        mockMvc.perform(post("/reservations")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(request)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(expectedReservation.getName()))
+                .andExpect(jsonPath("$.date").value(expectedReservation.getDate().toString()));
+    }
+
+    @Test
+    @DisplayName("예약 DELETE 요청 시 상태코드 200을 반환한다.")
+    void deleteReservation() throws Exception {
+        // given
+        BDDMockito.willDoNothing()
+                .given(reservationService)
+                .deleteReservation(anyLong());
+
+        // when & then
+        mockMvc.perform(delete("/reservations/{id}", anyLong())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }

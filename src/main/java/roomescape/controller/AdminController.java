e7ffb9715b881199d5210ff9b682ff0506658b03
takoyakimchi <c4nyou@gmail.com>
2024-04-18 13:53:54 +0900

    refactor: @GetMapping() -> @GetMapping

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping()
+    @GetMapping
     public String mainPage() {
         return "admin/index";
     }

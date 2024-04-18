60f8ddf05148fe5ec91a3b19d98f2d412cfe6891
devhoya97 <diajbh123@gmail.com>
2024-04-18 13:38:38 +0900

    refactor: JsonFormat 어노테이션 적용

--- src/main/java/roomescape/dto/response/ReservationResponse.java
+++ src/main/java/roomescape/dto/response/ReservationResponse.java
@@ -1,16 +1,23 @@
 package roomescape.dto.response;
 
-import java.time.format.DateTimeFormatter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import roomescape.domain.Reservation;
 
-public record ReservationResponse(Long id, String name, String date, String time) {
+public record ReservationResponse(Long id,
+                                  String name,
+                                  @JsonFormat(pattern = "yyyy-MM-dd")
+                                  LocalDate date,
+                                  @JsonFormat(pattern = "HH:mm")
+                                  LocalTime time) {
 
     public static ReservationResponse of(Long id, Reservation reservation) {
         return new ReservationResponse(
                 id,
                 reservation.getName(),
-                reservation.getDate().format(DateTimeFormatter.ISO_DATE),
-                reservation.getTime().format(DateTimeFormatter.ofPattern("HH:mm"))
+                reservation.getDate(),
+                reservation.getTime()
         );
     }
 }

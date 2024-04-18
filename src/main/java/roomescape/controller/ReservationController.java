089283a42d3770385801977fd2a1c6be69f3c826
masonkimseoul <masonkimseoul@gmail.com>
2024-04-18 22:47:49 +0900

    refactor : deleteReservation 메서드 반환타입 수정 및 예외처리 추가

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,15 +1,20 @@
 package roomescape.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import roomescape.dto.ReservationDto;
-import roomescape.domain.Reservation;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import roomescape.domain.Reservation;
+import roomescape.dto.ReservationDto;
 
 @Controller
 @RequestMapping("reservations")
@@ -21,7 +26,7 @@ public class ReservationController {
     @GetMapping
     public ResponseEntity<List<Reservation>> reservations() {
         return ResponseEntity.ok()
-                             .body(reservations);
+                .body(reservations);
     }
 
     @PostMapping
@@ -29,18 +34,25 @@ public class ReservationController {
         Reservation reservation = Reservation.toEntity(index.getAndIncrement(), reservationDto);
         reservations.add(reservation);
         return ResponseEntity.ok()
-                             .body(reservation);
+                .body(reservation);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable final Long id) {
-        final Reservation reservation = reservations.stream()
-                                                    .filter(it -> Objects.equals(it.getId(), id))
-                                                    .findFirst()
-                                                    .orElseThrow(RuntimeException::new);
-
+    public ResponseEntity<String> deleteReservation(@PathVariable final Long id) {
+        final Reservation reservation;
+        try {
+            reservation = reservations.stream()
+                    .filter(it -> Objects.equals(it.getId(), id))
+                    .findFirst()
+                    .orElseThrow(() ->
+                            new RuntimeException("삭제할 예약이 존재하지 않습니다.")
+                    );
+        }
+        catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("삭제할 예약이 존재하지 않습니다.");
+        }
         reservations.remove(reservation);
         return ResponseEntity.ok()
-                             .build();
+                .build();
     }
 }

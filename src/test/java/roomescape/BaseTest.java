54e7a88967bed7905fe1f3813b9022bb3d938712
김민수 MINSOO KIM <alstn113@gmail.com>
2024-04-18 16:09:20 +0900

    refactor: base test class 작성

---
+++ src/test/java/roomescape/BaseTest.java
@@ -0,0 +1,7 @@
+package roomescape;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public abstract class BaseTest {
+}
--- src/test/java/roomescape/controller/BaseControllerTest.java
+++ src/test/java/roomescape/controller/BaseControllerTest.java
@@ -4,9 +4,10 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import roomescape.BaseTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public abstract class BaseControllerTest {
+public abstract class BaseControllerTest extends BaseTest {
 
     @LocalServerPort
     private int port;

533e17371d800317bc88c3f4e78b47f7e08ebf11
Nam Gi Beom <rlqja0523@naver.com>
2024-04-20 01:59:30 +0900

    refactor: random port로 변경

--- src/test/java/roomescape/acceptance/AdminAcceptanceTest.java
+++ src/test/java/roomescape/acceptance/AdminAcceptanceTest.java
@@ -4,9 +4,10 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AdminAcceptanceTest {
     private static final String PATH = "/admin";
 

10aab5a6d94d5712dcb93dca395e39258ba8f7fb
leetaehoon <xogns1514@gmail.com>
2024-04-19 20:02:18 +0900

    feat(schema.sql): DB schema 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,3 @@
-CREATE TABLE reservation
-(
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255) NOT NULL,
-    date    VARCHAR(255) NOT NULL,
-    time    VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
-
 CREATE TABLE reservation_time
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
@@ -14,4 +5,13 @@ CREATE TABLE reservation_time
     PRIMARY KEY (id)
 );
 
+CREATE TABLE reservation
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    date VARCHAR(255) NOT NULL,
+    time_id BIGINT,                           -- 컬럼 수정
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id) -- 외래키 추가
+);
 

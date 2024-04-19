7781a0fbdc8b2481f87b000c3cefc03d7672d6f7
Arachneee <jhg2819@naver.com>
2024-04-19 16:05:11 +0900

    fix: reservation table의 time_id null 가능 버그 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE reservation_time
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL,
+    start_at VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
@@ -10,7 +10,7 @@ CREATE TABLE reservation
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     date VARCHAR(255) NOT NULL,
-    time_id BIGINT,
+    time_id BIGINT    NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id)
 );

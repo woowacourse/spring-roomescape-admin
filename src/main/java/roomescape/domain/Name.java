1efa43d1de8ed6cd364392b76bd670e094a772fe
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-04-19 16:34:53 +0900

    feat: 이름 값 객체 구현

---
+++ src/main/java/roomescape/domain/Name.java
@@ -0,0 +1,23 @@
+package roomescape.domain;
+
+import java.util.regex.Pattern;
+
+public class Name {
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final String NAME_RULE = "[가-힣a-zA-Z]{" + MIN_NAME_LENGTH + ",}";
+    private static final Pattern NAME_RULE_MATCHER = Pattern.compile(NAME_RULE);
+
+    private final String name;
+
+    public Name(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name != null && NAME_RULE_MATCHER.matcher(name).matches()) {
+            return;
+        }
+        throw new IllegalArgumentException("이름은 " + MIN_NAME_LENGTH + "글자 이상의 영/한글이어야 합니다");
+    }
+}
---
+++ src/test/java/roomescape/domain/NameTest.java
@@ -0,0 +1,37 @@
+package roomescape.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class NameTest {
+
+    @DisplayName("실패 : 예약자의 이름은 1글자 이상이어야 한다")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void should_ReturnIllegalArgumentException_When_NameLengthLessThanMinNameLenth(String invalidName) {
+        assertThatThrownBy(() -> new Name(invalidName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 1글자 이상의 영/한글이어야 합니다");
+    }
+
+    @DisplayName("실패 : 예약자의 이름은 영/한글로 구성되어야 한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "a1", "*"})
+    void should_ReturnIllegalArgumentException_When_NameIsNotEnglishOrKorean(String invalidName) {
+        assertThatThrownBy(() -> new Name(invalidName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 1글자 이상의 영/한글이어야 합니다");
+    }
+
+    @DisplayName("성공 : 예약자의 이름은 한글자 이상의 영/한글로 구성된다")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "콜리", "sudal"})
+    void should_CreateNameObject_When_GiveValidName(String validName) {
+        assertThatCode(() -> new Name(validName)).doesNotThrowAnyException();
+    }
+}

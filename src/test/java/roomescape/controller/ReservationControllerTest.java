0b215807725aefeeba3b94a494ee9b605948684f
SungKyum Kim <sungkyum1@naver.com>
2024-04-19 17:18:12 +0900

    test: 예약추가 및 조회 테스트 구현

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -5,13 +5,18 @@ import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.domain.Reservation;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -20,6 +25,9 @@ public class ReservationControllerTest {
 
     @LocalServerPort
     int port;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @BeforeEach
     void setUp() {
@@ -31,31 +39,47 @@ public class ReservationControllerTest {
         testResponseBodyDataSize("/reservations", 200, 0);
     }
 
+//    @Test
+//    void 예약추가_및_삭제를_수행한다() {
+//        Map<String, String> params = new HashMap<>();
+//        params.put("name", "브라운");
+//        params.put("date", "2023-08-05");
+//        params.put("time", "15:40");
+//
+//        RestAssured.given().log().all()
+//                .contentType(ContentType.JSON)
+//                .body(params)
+//                .when().post("/reservations")
+//                .then().log().all()
+//                .statusCode(200)
+//                .body("id", is(1));
+//
+//        testResponseBodyDataSize("/reservations", 200, 1);
+//
+//
+//        RestAssured.given().log().all()
+//                .when().delete("/reservations/1")
+//                .then().log().all()
+//                .statusCode(200);
+//
+//        testResponseBodyDataSize("/reservations", 200, 0);
+//    }
+
     @Test
-    void 예약추가_및_삭제를_수행한다() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
+    void 예약추가_및_조회를_수행한다() {
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
 
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
+        List<Reservation> reservations = RestAssured.given().log().all()
+                .when().get("/reservations")
                 .then().log().all()
-                .statusCode(200)
-                .body("id", is(1));
+                .statusCode(200).extract()
+                .jsonPath().getList(".", Reservation.class);
 
-        testResponseBodyDataSize("/reservations", 200, 1);
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
 
-
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
-                .then().log().all()
-                .statusCode(200);
-
-        testResponseBodyDataSize("/reservations", 200, 0);
+        assertThat(reservations.size()).isEqualTo(count);
     }
+
 
     private void testResponseBodyDataSize(String path, int expectedStatusCode, int size) {
         testPageStatus(path, expectedStatusCode)

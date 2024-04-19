d6ffe51cc3160a227a16bf766909047e091a7427
devhoya97 <diajbh123@gmail.com>
2024-04-19 16:38:39 +0900

    style: 가독성을 위한 줄바꿈 추가

--- src/main/java/roomescape/dto/response/ReservationResponse.java
+++ src/main/java/roomescape/dto/response/ReservationResponse.java
@@ -5,12 +5,14 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import roomescape.domain.Reservation;
 
-public record ReservationResponse(Long id,
-                                  String name,
-                                  @JsonFormat(pattern = "yyyy-MM-dd")
-                                  LocalDate date,
-                                  @JsonFormat(pattern = "HH:mm")
-                                  LocalTime time) {
+public record ReservationResponse(
+        Long id,
+        String name,
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDate date,
+        @JsonFormat(pattern = "HH:mm")
+        LocalTime time
+) {
 
     public static ReservationResponse of(Long id, Reservation reservation) {
         return new ReservationResponse(

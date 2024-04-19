7e2ac54482f8251698daf45ccf89b14e406fde68
devhoya97 <diajbh123@gmail.com>
2024-04-19 18:47:40 +0900

    refactor: Reservation을 record로 변경

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -3,27 +3,5 @@ package roomescape.domain;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class Reservation {
-
-    private final String name;
-    private final LocalDate date;
-    private final LocalTime time;
-
-    public Reservation(String name, LocalDate date, LocalTime time) {
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
+public record Reservation(String name, LocalDate date, LocalTime time) {
 }

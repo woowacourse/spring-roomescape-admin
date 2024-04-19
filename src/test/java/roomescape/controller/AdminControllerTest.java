b1c3634a35b86daae828f509faaaa7eff07cb3db
juha <khabh@naver.com>
2024-04-19 16:11:34 +0900

    test: Reservation과 Admin 컨트롤러 분리

--- src/test/java/roomescape/controller/AdminControllerTest.java
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -3,14 +3,23 @@ package roomescape.controller;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AdminControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void initPort() {
+        RestAssured.port = port;
+    }
 
     @DisplayName("홈 화면 조회")
     @Test
@@ -26,40 +35,6 @@ class AdminControllerTest {
     void reservation() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
-                .then().log().all()
-                .statusCode(200);
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
-    }
-
-    @DisplayName("예약 추가 및 삭제")
-    @Test
-    void saveReservation() {
-        final Map<String, String> params = Map.of(
-                "name", "브라운",
-                "date", "2023-08-05",
-                "time", "15:40");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("id", is(1));
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
-
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
                 .then().log().all()
                 .statusCode(200);
 

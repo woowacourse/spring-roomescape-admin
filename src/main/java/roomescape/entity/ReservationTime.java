bd3c908169b53abd2b7cb5b3eba3fce5bbf12741
libienz <leekh10195@gmail.com>
2024-04-20 02:01:25 +0900

    feat: ReservationTime 도메인 Getter 프로퍼티 추가

--- src/main/java/roomescape/entity/ReservationTime.java
+++ src/main/java/roomescape/entity/ReservationTime.java
@@ -1,21 +1,31 @@
 package roomescape.entity;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class ReservationTime {
     private static final int RESERVATION_DURATION_HOUR = 1;
 
-    private final LocalDateTime startTime;
+    private final LocalDateTime startDateTime;
 
-    public ReservationTime(LocalDateTime startTime) {
-        this.startTime = startTime;
+    public ReservationTime(LocalDateTime startDateTime) {
+        this.startDateTime = startDateTime;
     }
 
     public boolean isConflictWith(ReservationTime other) {
-        return !(endTime().isBefore(other.startTime) || startTime.isAfter(other.endTime()));
+        return !(endTime().isBefore(other.startDateTime) || startDateTime.isAfter(other.endTime()));
     }
 
     private LocalDateTime endTime() {
-        return startTime.plusHours(RESERVATION_DURATION_HOUR);
+        return startDateTime.plusHours(RESERVATION_DURATION_HOUR);
+    }
+
+    public LocalDate getStartDate() {
+        return startDateTime.toLocalDate();
+    }
+
+    public LocalTime getStartTime() {
+        return startDateTime.toLocalTime();
     }
 }

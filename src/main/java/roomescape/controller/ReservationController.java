284c458e834ca9d2205a57576474b12fe8df9360
yoonseo choi <choiyoonseo02@gmail.com>
2024-04-19 22:06:38 +0900

    [1 - 3단계 - 방탈출 예약 관리] 릴리(최윤서) 미션 제출합니다. (#82)
    * feat: admin 페이지 응답 기능
    
    Co-authored-by: unifolio0 <boho82ha@gmail.com>
    
    * feat: 예약 목록 출력 기능 구현
    
    Co-Authored-By: yoonseo choi <choiyoonseo02@gmail.com>
    
    * feat : 3단계 기능 구현
    
    Co-Authored-By: yoonseo choi <choiyoonseo02@gmail.com>
    
    * refactor : Reservation 객체 생성 위치 변경
    
    Co-Authored-By: yoonseo choi <choiyoonseo02@gmail.com>
    
    * refactor : 마지막 줄 개행 추가
    
    Co-Authored-By: yoonseo choi <choiyoonseo02@gmail.com>
    
    * fix: 아이콘 클릭 시 에러페이지 나오는 버그 수정
    
    * refactor: @SpringBootTest 어노테이션 속성 수정
    
    * refactor: Reservation 객체 생성하는 메서드 네이밍 변경
    
    * refactor: RequestDto 네이밍 변경
    
    * refactor: 컨트롤러 분리
    
    * refactor: Reservations 가 로직 처리하도록 변경
    
    * refactor: 예외처리 수정
    
    * refactor: Model 삭제
    
    * style: package 구조 변경
    
    ---------
    
    Co-authored-by: unifolio0 <boho82ha@gmail.com>

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,43 @@
+package roomescape.controller;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.model.Reservation;
+import roomescape.model.Reservations;
+import roomescape.dto.ReservationCreateRequestDto;
+
+@RestController
+public class ReservationController {
+
+    private final Reservations reservations;
+    private final AtomicLong index = new AtomicLong(0);
+
+    public ReservationController(Reservations reservations) {
+        this.reservations = reservations;
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> reservations() {
+        return ResponseEntity.ok(reservations.getReservations());
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> create(@RequestBody ReservationCreateRequestDto reservationCreateRequestDto) {
+        Reservation reservation = reservationCreateRequestDto.createReservation(index.incrementAndGet());
+        reservations.add(reservation);
+        return ResponseEntity.ok(reservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        reservations.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/roomescape/model/Reservation.java
@@ -0,0 +1,37 @@
+package roomescape.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+
+    private final long id;
+    private final String name;
+    private final LocalDate date;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+    private final LocalTime time;
+
+    public Reservation(long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public boolean isSameReservationId(long id) {
+        return this.id == id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+}

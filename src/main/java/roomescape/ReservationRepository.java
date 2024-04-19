269debf60c7a32d933bd9b665ff9b60806fe8970
jhon3242 <rldotnr2@gmail.com>
2024-04-19 20:36:26 +0900

    refactor : SimpleJdbcInsert 도입

--- src/main/java/roomescape/ReservationRepository.java
+++ src/main/java/roomescape/ReservationRepository.java
@@ -2,12 +2,12 @@ package roomescape;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import roomescape.dto.RequestReservation;
 
-import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
@@ -17,16 +17,11 @@ public class ReservationRepository {
     private JdbcTemplate jdbcTemplate;
 
     public Long add(RequestReservation requestReservation) {
-        String SQL = "INSERT INTO RESERVATION (name, date, time) VALUES(?, ?, ?)";
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(SQL, new String[]{"id"});
-            ps.setString(1, requestReservation.name());
-            ps.setString(2, requestReservation.date());
-            ps.setString(3, requestReservation.time());
-            return ps;
-        }, keyHolder);
-        return keyHolder.getKey().longValue();
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("RESERVATION")
+                .usingGeneratedKeyColumns("id");
+        SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(requestReservation);
+        return simpleJdbcInsert.executeAndReturnKey(sqlParameterSource).longValue();
     }
 
     public List<Reservation> findAll() {

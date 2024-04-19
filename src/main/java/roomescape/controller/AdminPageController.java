0b2a47805375acb6b9b9237065618a0ecdd7114b
masonkimseoul <masonkimseoul@gmail.com>
2024-04-19 16:37:21 +0900

    refactor : Controller를 RestController로 변경

--- src/main/java/roomescape/controller/AdminPageController.java
+++ src/main/java/roomescape/controller/AdminPageController.java
@@ -1,10 +1,10 @@
 package roomescape.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("admin")
 public class AdminPageController {
 
--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -6,17 +6,17 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 import roomescape.dto.ReservationCreateRequest;
 
-@Controller
+@RestController
 @RequestMapping("reservations")
 public class ReservationController {
 

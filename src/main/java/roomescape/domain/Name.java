793b8b0222eb635fb535d8dd00e8416b9d6966d3
slimsha2dy <chy2581@naver.com>
2024-04-19 17:11:37 +0900

    feat: 이름 클래스 예외 처리 구현

--- src/main/java/roomescape/domain/Name.java
+++ src/main/java/roomescape/domain/Name.java
@@ -1,10 +1,40 @@
 package roomescape.domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Name {
+    private static final Pattern PATTERN = Pattern.compile("[가-힣a-zA-Z-_0-9]+");
     private final String name;
 
     public Name(String name) {
+        validateName(name);
         this.name = name;
+    }
+
+    private void validateName(String name) {
+        validateBlank(name);
+        validateLength(name);
+        validateCharacter(name);
+    }
+
+    private void validateBlank(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름에 공백을 입력할 수 없습니다.");
+        }
+    }
+
+    private void validateLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름의 길이는 5 이하여야 합니다.");
+        }
+    }
+
+    private void validateCharacter(String name) {
+        Matcher matcher = PATTERN.matcher(name);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("이름은 알파벳, 한글, 숫자, '_', '-'로만 이루어져야 합니다.");
+        }
     }
 
     public String getName() {

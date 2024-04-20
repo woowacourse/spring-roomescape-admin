091d797bc7dc1f297ff7ff1ae32c720746af2abd
hoyeonyy <highoption@naver.com>
2024-04-20 17:02:10 +0900

    refactor(ReservationApiController) : RestController 로 변경

--- src/main/java/roomescape/reservation/ReservationApiController.java
+++ src/main/java/roomescape/reservation/ReservationApiController.java
@@ -4,21 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ReservationApiController {
 
     private final List<Reservation> reservations = new ArrayList<>();
     private final AtomicLong atomicLong = new AtomicLong();
 
-    @ResponseBody
     @GetMapping("/reservations")
     public ResponseEntity<List<ResponseReservation>> findAll() {
         List<ResponseReservation> responseReservations = reservations.stream()
@@ -29,7 +27,6 @@ public class ReservationApiController {
         return ResponseEntity.ok(responseReservations);
     }
 
-    @ResponseBody
     @PostMapping("/reservations")
     public ResponseEntity<ResponseReservation> create(@RequestBody RequestReservation requestReservation) {
         Reservation reservation = new Reservation(atomicLong.incrementAndGet(), requestReservation.name(),

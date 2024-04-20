13738756c91377aaee512399c60a78a5e45d6883
Hyeon0208 <dmsgktn0208@gmail.com>
2024-04-20 21:53:00 +0900

    test : 5, 6단계 테스트 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -8,8 +8,10 @@ import io.restassured.http.ContentType;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.reservation.Reservation;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -33,6 +36,7 @@ public class MissionStepTest {
         RestAssured.port = port;
     }
 
+    @DisplayName("/admin 경로 GET 요청 API 테스트")
     @Test
     void 일단계() {
         RestAssured.given().log().all()
@@ -41,6 +45,7 @@ public class MissionStepTest {
                 .statusCode(200);
     }
 
+    @DisplayName("/reservation 경로 GET 요청 API 테스트")
     @Test
     void 이단계() {
         RestAssured.given().log().all()
@@ -55,6 +60,7 @@ public class MissionStepTest {
                 .body("size()", is(0));
     }
 
+    @DisplayName("/reservations 경로 POST, DELETE 요청 API 테스트")
     @Test
     void 삼단계() {
         Map<String, String> param = new HashMap<>();
@@ -67,7 +73,7 @@ public class MissionStepTest {
                 .body(param)
                 .when().post("/reservations")
                 .then().log().all()
-                .statusCode(200)
+                .statusCode(201)
                 .body("id", is(1));
 
         RestAssured.given().log().all()
@@ -79,7 +85,7 @@ public class MissionStepTest {
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(204);
 
         RestAssured.given().log().all()
                 .when().get("/reservations")
@@ -88,6 +94,7 @@ public class MissionStepTest {
                 .body("size()", is(0));
     }
 
+    @DisplayName("데이터 베이스 연결에 성공하고 카탈로그와 테이블 이름을 확인 한다.")
     @Test
     void 사단계() {
         try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
@@ -100,5 +107,49 @@ public class MissionStepTest {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @DisplayName("DB 데이터 추가와 /reservations 경로에 GET 요청을 보내 조회된 결과 수를 확인한다.")
+    @Test
+    void 오단계() {
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
+
+        List<Reservation> reservations = RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200).extract()
+                .jsonPath().getList(".", Reservation.class);
+
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+
+        assertThat(reservations.size()).isEqualTo(count);
+    }
+
+    @DisplayName("POST 요청에 대한 응답 헤더를 확인하고 DELETE 요청으로 데이터가 제거되었는지 확인한다.")
+    @Test
+    void 육단계() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201)
+                .header("Location", "/reservations/1");
+
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+        assertThat(count).isEqualTo(1);
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(204);
+
+        Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+        assertThat(countAfterDelete).isEqualTo(0);
     }
 }

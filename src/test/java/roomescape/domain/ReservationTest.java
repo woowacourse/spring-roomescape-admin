31f43f3dee896e8a2894f8e53ab6b472cc5203e0
donghoony <aru0504@naver.com>
2024-04-20 23:43:34 +0900

    style: 컨벤션 적용

--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -10,7 +10,7 @@ class ReservationTest {
     @Test
     @DisplayName("같은 ID를 가진다면, 서로 동등하다.")
     void equalsOnSameIdTest() {
-        Reservation actual = new Reservation(1L,"웨지", "2024-04-20", new ReservationTime("13:00"));
+        Reservation actual = new Reservation(1L, "웨지", "2024-04-20", new ReservationTime("13:00"));
         Reservation expected = new Reservation(1L, "웨지", "2024-04-21", new ReservationTime("12:00"));
         assertThat(actual).isEqualTo(expected);
     }

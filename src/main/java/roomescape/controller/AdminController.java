f9a34b6e4f0f19732a10a74f717497f37b9611ae
yoonseo choi <choiyoonseo02@gmail.com>
2024-04-20 16:34:23 +0900

    refactor: FrontController 네이밍 변경

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class FrontController {
+public class AdminController {
 
     @GetMapping("/admin")
     public String admin() {

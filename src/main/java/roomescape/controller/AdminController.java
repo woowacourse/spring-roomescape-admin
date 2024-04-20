296bcfcd4d7c60cfde843d0e8265fed0930b9272
reddevilmidzy <midzy0228@gmail.com>
2024-04-20 20:04:00 +0900

    style: 와일드카드 import 제거

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -1,7 +1,8 @@
 package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")

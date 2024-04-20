32aec9dffcd40e7447f26e97cb02852a1778396e
hjk0761 <hjk0761@naver.com>
2024-04-21 01:07:45 +0900

    style: 불필요한 개행 수정

---
+++ src/test/java/roomescape/LayeredTest.java
@@ -0,0 +1,32 @@
+package roomescape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import roomescape.controller.ReservationController;
+
+import java.lang.reflect.Field;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class LayeredTest {
+
+    @Autowired
+    private ReservationController reservationController;
+
+    @Test
+    void 구단계() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
+    }
+}

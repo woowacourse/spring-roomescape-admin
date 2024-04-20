43bab5562f9d5f2a0a7792b733c098a781650836
김민수 MINSOO KIM <alstn113@gmail.com>
2024-04-20 11:00:15 +0900

    refactor: 예약 응답에서 예약 시간 응답 관련 수정

--- src/main/java/roomescape/dto/ReservationResponse.java
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -2,18 +2,17 @@ package roomescape.dto;
 
 import java.time.LocalDate;
 import roomescape.domain.Reservation;
-import roomescape.domain.ReservationTime;
 
 public record ReservationResponse(Long id,
                                   String name,
                                   LocalDate date,
-                                  ReservationTime time) {
+                                  ReservationTimeResponse time) {
 
     public static ReservationResponse from(Reservation reservation) {
         return new ReservationResponse(
                 reservation.getId(),
                 reservation.getName(),
                 reservation.getDate(),
-                reservation.getTime());
+                ReservationTimeResponse.from(reservation.getTime()));
     }
 }

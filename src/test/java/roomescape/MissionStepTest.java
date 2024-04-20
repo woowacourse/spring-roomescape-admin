ff8968881adde4b584c0c2cb7c1ca228c5cb526d
이상진 <priceelees@gmail.com>
2024-04-20 22:31:07 +0900

    refactor: js 와 테스트에서의 상태 코드를 컨트롤러의 반환값에 맞게 수정

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -59,7 +59,7 @@ public class MissionStepTest {
                 .body(params)
                 .when().post("/reservations")
                 .then().log().all()
-                .statusCode(200)
+                .statusCode(201)
                 .body("id", is(1));
 
         RestAssured.given().log().all()
@@ -71,7 +71,7 @@ public class MissionStepTest {
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(204);
 
         RestAssured.given().log().all()
                 .when().get("/reservations")

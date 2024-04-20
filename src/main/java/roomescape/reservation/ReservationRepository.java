aef36af8232b51fccf1c0688fd3d1a97660ca9d7
Hyeon0208 <dmsgktn0208@gmail.com>
2024-04-20 18:22:49 +0900

    feat : 예약 레포지토리 추가

---
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -0,0 +1,69 @@
+package roomescape.reservation;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReservationRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationRepository(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Reservation save(final ReservationRequest reservationRequest) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    "insert into reservation(name, date, time) values (?, ? ,?)", new String[]{"id"});
+            ps.setString(1, reservationRequest.getName());
+            ps.setString(2, reservationRequest.getDate());
+            ps.setString(3, reservationRequest.getTime());
+            return ps;
+        }, keyHolder);
+
+        Long id = keyHolder.getKey().longValue();
+        return new Reservation(id, reservationRequest.getName(), reservationRequest.getDate(),
+                reservationRequest.getTime());
+    }
+
+    public Optional<Reservation> findById(final Long id) {
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject("select * from reservation where id = ?",
+                    (resultSet, rowNum) -> {
+                        Reservation reservation = new Reservation(
+                                resultSet.getLong("id"),
+                                resultSet.getString("name"),
+                                resultSet.getString("date"),
+                                resultSet.getString("time")
+                        );
+                        return reservation;
+                    }, id));
+        } catch (EmptyResultDataAccessException exception) {
+            return Optional.empty();
+        }
+    }
+
+    public List<Reservation> findAll() {
+        return jdbcTemplate.query("select * from reservation",
+                (resultSet, rowNum) -> {
+                    Reservation reservation = new Reservation(
+                            resultSet.getLong("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("date"),
+                            resultSet.getString("time")
+                    );
+                    return reservation;
+                });
+    }
+
+    public void deleteById(final Long id) {
+        jdbcTemplate.update("delete from reservation where id = ?", id);
+    }
+}

0d6ec2902b56e26db6d2ffbf4769364d74d87ae6
jongmee <ra123221@gmail.com>
2024-04-20 15:19:39 +0900

    test(ReservationTimeAcceptanceTest): 예약 시간 생성 API 테스트

---
+++ src/test/java/roomescape/acceptance/ReservationTimeAcceptanceTest.java
@@ -0,0 +1,27 @@
+package roomescape.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReservationTimeAcceptanceTest implements AcceptanceTest {
+
+    @Test
+    @DisplayName("[Step7] 예약 시간을 생성한다.")
+    void createReservationTime() {
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(201)
+                .header("Location", "/times/1");
+    }
+}

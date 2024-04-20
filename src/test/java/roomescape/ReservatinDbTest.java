0107cfeda2b038e0d7f22456c3466b2a15893a2f
hyeonjilee <skylar12200@gmail.com>
2024-04-20 13:11:57 +0900

    feat: db 연결

---
+++ src/test/java/roomescape/ReservatinDbTest.java
@@ -0,0 +1,28 @@
+package roomescape;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootTest
+public class ReservatinDbTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Test
+    void 사단계() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

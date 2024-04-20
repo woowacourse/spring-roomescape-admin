5a8ce3c207bed044053a9eba19d1b0ba6aaa28dc
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-04-20 16:33:12 +0900

    chore: H2 DB 연결

---
+++ src/test/java/roomescape/JdbcConnectionTest.java
@@ -0,0 +1,29 @@
+package roomescape;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootTest
+public class JdbcConnectionTest {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @DisplayName("DataSource 객체를 이용하여 Connection 검증한다.")
+    @Test
+    void connectionTest() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

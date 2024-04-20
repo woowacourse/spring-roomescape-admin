1cec37937d7fe526225f48b71c51219070f3202d
3juhwan <13selfesteem91@naver.com>
2024-04-20 16:44:24 +0900

    refactor: reservations URI를 클래스 단위로 맵핑

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 import roomescape.dto.ReservationRequestDto;
@@ -17,11 +18,12 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/reservations")
 public class ReservationController {
     private final List<Reservation> reservations = new ArrayList<>();
     private final AtomicLong index = new AtomicLong(0);
 
-    @GetMapping("/reservations")
+    @GetMapping
     public ResponseEntity<List<ReservationResponseDto>> findAll() {
         List<ReservationResponseDto> reservationResponseDtos = reservations.stream()
                 .map(ReservationResponseDto::from)
@@ -30,7 +32,7 @@ public class ReservationController {
         return ResponseEntity.ok(reservationResponseDtos);
     }
 
-    @PostMapping("/reservations")
+    @PostMapping
     public ResponseEntity<ReservationResponseDto> add(@RequestBody ReservationRequestDto reservationRequestDto) {
         Reservation newReservation = new Reservation(
                 index.incrementAndGet(),
@@ -44,7 +46,7 @@ public class ReservationController {
                 .body(ReservationResponseDto.from(newReservation));
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable long id) {
         reservations.stream()
                 .filter(reservation -> reservation.hasId(id))

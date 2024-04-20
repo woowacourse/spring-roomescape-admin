275a1ecfd251e21008485c9011f921a53f61edfe
kunsanglee <gosmdochee2@gmail.com>
2024-04-20 16:47:37 +0900

    refactor: ReservationController create, delete 요청에 대한 응답 HTTP 상태코드 변경

--- src/main/java/roomescape/presentation/ReservationController.java
+++ src/main/java/roomescape/presentation/ReservationController.java
@@ -1,5 +1,6 @@
 package roomescape.presentation;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,12 +31,12 @@ public class ReservationController {
     @PostMapping
     public ResponseEntity<ReservationResponse> create(@RequestBody ReservationRequest request) {
         ReservationResponse response = service.create(request);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.created(URI.create("/reservations/" + response.id())).body(response);
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.deleteById(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }

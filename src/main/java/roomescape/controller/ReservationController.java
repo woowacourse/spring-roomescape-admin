8fbaf558d2a6b8ec25aa7256c28252485e853a83
reddevilmidzy <midzy0228@gmail.com>
2024-04-21 00:30:34 +0900

    refactor: 예약 저장 시 ResponseEntity 제공 메서드 활용 및 Uri 변환 로직 변경

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,9 +1,8 @@
 package roomescape.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 import roomescape.dto.ReservationRequest;
 import roomescape.dto.ReservationResponse;
 import roomescape.model.Reservation;
@@ -40,10 +40,10 @@ public class ReservationController {
         final long id = reservationService.save(reservationRequest);
         final Reservation reservation = reservationRequest.toReservation(id);
         final ReservationResponse reservationResponse = ReservationResponse.from(reservation);
-        final HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.add(HttpHeaders.LOCATION, String.format("/reservations/%s", id));
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .headers(responseHeaders)
+        final URI uri = UriComponentsBuilder.fromPath("/reservations/{id}")
+                .buildAndExpand(id)
+                .toUri();
+        return ResponseEntity.created(uri)
                 .body(reservationResponse);
     }
 

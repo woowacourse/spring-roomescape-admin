64b51621be35b8ad3d9da79f88e199dc24b5d69f
jinwoo22 <jenny010925@naver.com>
2024-04-21 00:27:55 +0900

    refactor: 필요없는 테스트 코드 제거

--- src/test/java/roomescape/ReservationControllerTest.java
+++ src/test/java/roomescape/ReservationControllerTest.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -13,6 +14,16 @@ import static org.hamcrest.Matchers.is;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationControllerTest {
+
+    private static Map<String, String> params;
+
+    @BeforeAll
+    static void setUp() {
+        params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+    }
 
     @Test
     void getReservations() {
@@ -25,11 +36,6 @@ class ReservationControllerTest {
 
     @Test
     void postReservation() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
-
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
@@ -37,31 +43,13 @@ class ReservationControllerTest {
                 .then().log().all()
                 .statusCode(201)
                 .header("Location", "/reservations/1");
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
     }
-
 
     @Test
     void deleteReservation() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
-
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
                 .statusCode(204);
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
     }
 }

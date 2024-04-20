51458b9b3848261c71d9b5416aac3382f64843e6
jongmee <ra123221@gmail.com>
2024-04-21 02:33:40 +0900

    test(ReservationServiceTest): Mocking 적용

--- src/test/java/roomescape/business/ReservationServiceTest.java
+++ src/test/java/roomescape/business/ReservationServiceTest.java
@@ -2,28 +2,39 @@ package roomescape.business;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 import roomescape.dto.ReservationResponse;
-import roomescape.persistence.ReservationCollectionRepository;
 import roomescape.persistence.ReservationRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static roomescape.TestFixture.*;
 
+@ExtendWith(MockitoExtension.class)
 class ReservationServiceTest {
-    private final ReservationRepository reservationRepository = new ReservationCollectionRepository();
-    private final ReservationService reservationService = new ReservationService(reservationRepository);
+    @Mock
+    private ReservationRepository reservationRepository;
+
+    @InjectMocks
+    private ReservationService reservationService;
 
     @Test
     @DisplayName("예약을 생성한다.")
     void createReservation() {
         // given
-        Reservation reservation = MIA_RESERVATION(new ReservationTime(MIA_RESERVATION_TIME));
+        Reservation reservation = MIA_RESERVATION();
+
+        when(reservationRepository.save(reservation))
+                .thenReturn(new Reservation(1L, reservation));
 
         // when
         ReservationResponse savedReservation = reservationService.createReservation(reservation);
@@ -36,13 +47,13 @@ class ReservationServiceTest {
     @DisplayName("동일한 시간에 같은 사용자가 예약할 수 없다.")
     void createSameReservation() {
         // given
-        Reservation miaReservation = MIA_RESERVATION(new ReservationTime(MIA_RESERVATION_TIME));
-        reservationRepository.save(miaReservation);
+        Reservation miaReservation = MIA_RESERVATION();
 
-        Reservation newReservation = MIA_RESERVATION(new ReservationTime(MIA_RESERVATION_TIME));
+        when(reservationRepository.findAllByDateAndTime(any(), any()))
+                .thenReturn(List.of(miaReservation));
 
         // when & then
-        assertThatThrownBy(() -> reservationService.createReservation(newReservation))
+        assertThatThrownBy(() -> reservationService.createReservation(miaReservation))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -50,14 +61,13 @@ class ReservationServiceTest {
     @DisplayName("동일한 시간대에 최대 4팀이 예약할 수 있다. 초과되면 예외가 발생한다.")
     void createLimitedReservations() {
         // given
-        Reservation miaReservation = MIA_RESERVATION(new ReservationTime(MIA_RESERVATION_TIME));
+        Reservation miaReservation = new Reservation(USER_MIA, MIA_RESERVATION_DATE, new ReservationTime(MIA_RESERVATION_TIME));
         Reservation tommyReservation = new Reservation(USER_TOMMY, MIA_RESERVATION_DATE, new ReservationTime(MIA_RESERVATION_TIME));
         Reservation wonnyReservation = new Reservation("wonny", MIA_RESERVATION_DATE, new ReservationTime(MIA_RESERVATION_TIME));
         Reservation neoReservation = new Reservation("neo", MIA_RESERVATION_DATE, new ReservationTime(MIA_RESERVATION_TIME));
-        reservationRepository.save(miaReservation);
-        reservationRepository.save(tommyReservation);
-        reservationRepository.save(wonnyReservation);
-        reservationRepository.save(neoReservation);
+
+        when(reservationRepository.findAllByDateAndTime(any(), any()))
+                .thenReturn(List.of(miaReservation, tommyReservation, wonnyReservation, neoReservation));
 
         Reservation newReservation = new Reservation("new", MIA_RESERVATION_DATE, new ReservationTime(MIA_RESERVATION_TIME));
 
@@ -70,10 +80,11 @@ class ReservationServiceTest {
     @DisplayName("모든 예약 목록을 조회한다.")
     void getReservations() {
         // given
-        Reservation miaReservation = MIA_RESERVATION(new ReservationTime(MIA_RESERVATION_TIME));
+        Reservation miaReservation = MIA_RESERVATION();
         Reservation tommyReservation = TOMMY_RESERVATION();
-        reservationRepository.save(miaReservation);
-        reservationRepository.save(tommyReservation);
+
+        when(reservationRepository.findAll())
+                .thenReturn(List.of(miaReservation, tommyReservation));
 
         // when
         List<ReservationResponse> reservations = reservationService.getReservations();
@@ -88,15 +99,14 @@ class ReservationServiceTest {
     @DisplayName("예약을 삭제한다.")
     void deleteReservation() {
         // given
-        Reservation miaReservation = MIA_RESERVATION(new ReservationTime(MIA_RESERVATION_TIME));
-        reservationRepository.save(miaReservation);
+        Reservation miaReservation = MIA_RESERVATION();
 
-        // when
-        reservationService.deleteReservation(1L);
+        when(reservationRepository.findById(1L))
+                .thenReturn(Optional.of(miaReservation));
 
-        // then
-        Optional<Reservation> deleted = reservationRepository.findById(1L);
-        assertThat(deleted).isEmpty();
+        // when & then
+        assertThatCode(() -> reservationService.deleteReservation(1L))
+                .doesNotThrowAnyException();
     }
 
     @Test
@@ -104,6 +114,9 @@ class ReservationServiceTest {
     void deleteReservationNotExistingId() {
         // given
         Long notExistingId = 1L;
+
+        when(reservationRepository.findById(notExistingId))
+                .thenReturn(Optional.empty());
 
         // when & then
         assertThatThrownBy(() -> reservationService.deleteReservation(notExistingId))

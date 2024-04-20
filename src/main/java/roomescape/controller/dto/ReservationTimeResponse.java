fa67d4f6a8c35e115233ec7498d21618ce814a21
donghoony <aru0504@naver.com>
2024-04-20 18:55:01 +0900

    fix: 요구사항에 맞도록 시간 포맷 수정

--- src/main/java/roomescape/controller/dto/ReservationTimeResponse.java
+++ src/main/java/roomescape/controller/dto/ReservationTimeResponse.java
@@ -1,11 +1,16 @@
 package roomescape.controller.dto;
 
-import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import roomescape.domain.ReservationTime;
 
-public record ReservationTimeResponse(Long id, LocalTime startAt) {
+public record ReservationTimeResponse(Long id, String startAt) {
+
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
 
     public static ReservationTimeResponse from(ReservationTime reservationTime) {
-        return new ReservationTimeResponse(reservationTime.getId(), reservationTime.getTime());
+        return new ReservationTimeResponse(
+                reservationTime.getId(),
+                TIME_FORMATTER.format(reservationTime.getTime())
+        );
     }
 }

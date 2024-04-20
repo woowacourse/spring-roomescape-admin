00fa10606efd01efcd359a5ab43ac0799425bd0f
KIMGYUTAE <kiimwf0819@gmail.com>
2024-04-21 01:33:17 +0900

    refactor(AdminController): 메서드명 변경

--- src/main/java/roomescape/AdminController.java
+++ src/main/java/roomescape/AdminController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminController {
 
     @GetMapping("/admin")
-    public String main() {
+    public String home() {
         return "admin/index";
     }
 

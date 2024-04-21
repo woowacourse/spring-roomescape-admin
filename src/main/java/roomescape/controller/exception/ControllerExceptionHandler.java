e9444ad388e50503cae08e96dc7d3ab54a7ec115
HyungUk Ryu <hw0603@naver.com>
2024-04-22 00:34:22 +0900

    feat(controllerExceptionHandler): 요청의 날짜/시간 포맷이 잘못된 경우 오류 메시지 응답

--- src/main/java/roomescape/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/roomescape/controller/exception/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package roomescape.controller.exception;
 
+import java.time.format.DateTimeParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +11,13 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(IllegalArgumentException e) {
         ExceptionResponse response = new ExceptionResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+        return ResponseEntity.badRequest()
+                .body(response);
+    }
+
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<ExceptionResponse> handleDateTimeParseException() {
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.BAD_REQUEST, "날짜/시간 포맷이 잘못되었습니다.");
         return ResponseEntity.badRequest()
                 .body(response);
     }

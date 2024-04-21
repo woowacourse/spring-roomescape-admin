159f8a2d5923f7b87588d896ab95d91dfc351958
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-04-21 17:01:54 +0900

    refactor: 모델 객체 domain 패키지로 이동

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.Reservation;
 import roomescape.controller.request.ReservationRequest;
+import roomescape.model.Reservation;
 
 @RestController
 public class ReservationController {

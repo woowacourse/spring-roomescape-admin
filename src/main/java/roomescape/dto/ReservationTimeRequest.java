e8f0db26ff7ca47d748d467ca389e2e07e9ea683
이상진 <priceelees@gmail.com>
2024-04-21 17:06:32 +0900

    refactor(ReservationTimeRequest): 엔티티 변환 메서드 제거

--- src/main/java/roomescape/dto/ReservationTimeRequest.java
+++ src/main/java/roomescape/dto/ReservationTimeRequest.java
@@ -1,10 +1,4 @@
 package roomescape.dto;
 
-import roomescape.entity.ReservationTime;
-
 public record ReservationTimeRequest(String startAt) {
-
-    public ReservationTime toEntity(long id) {
-        return new ReservationTime(id, startAt);
-    }
 }

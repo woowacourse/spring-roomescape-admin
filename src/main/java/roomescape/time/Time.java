3a08d362c491732d963827d92ed35552edcbf539
Hyeon0208 <dmsgktn0208@gmail.com>
2024-04-21 17:25:36 +0900

    feat : 역직렬화를 위한 기본 생성자 추가

--- src/main/java/roomescape/time/Time.java
+++ src/main/java/roomescape/time/Time.java
@@ -1,8 +1,13 @@
 package roomescape.time;
 
+import java.util.Objects;
+
 public class Time {
     private Long id;
     private String startAt;
+
+    public Time() {
+    }
 
     public Time(final Long id, final String startAt) {
         this.id = id;
@@ -15,5 +20,22 @@ public class Time {
 
     public String getStartAt() {
         return startAt;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Time time = (Time) o;
+        return Objects.equals(id, time.id) && Objects.equals(startAt, time.startAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, startAt);
     }
 }

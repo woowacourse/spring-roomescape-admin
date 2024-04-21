953bb6b5a4ac2aa285d8f316865244acf3a834a0
이상진 <priceelees@gmail.com>
2024-04-21 17:05:28 +0900

    refactor(ReservationTime): id의 타입 변경(long -> Long)

--- src/main/java/roomescape/entity/ReservationTime.java
+++ src/main/java/roomescape/entity/ReservationTime.java
@@ -2,15 +2,15 @@ package roomescape.entity;
 
 public class ReservationTime {
 
-    private final long id;
+    private final Long id;
     private final String startAt;
 
-    public ReservationTime(long id, String startAt) {
+    public ReservationTime(Long id, String startAt) {
         this.id = id;
         this.startAt = startAt;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 

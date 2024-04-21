7af89bb1f6443eb34157e830937f215d60c5f3a3
이상진 <priceelees@gmail.com>
2024-04-21 12:03:35 +0900

    feat(ReservationTimeApiController): 예약 시간 조회 Api 구현

--- src/main/java/roomescape/controller/ReservationTimeApiController.java
+++ src/main/java/roomescape/controller/ReservationTimeApiController.java
@@ -1,10 +1,13 @@
 package roomescape.controller;
 
+import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,11 +29,25 @@ public class ReservationTimeApiController {
                 .usingGeneratedKeyColumns("id");
     }
 
+    @GetMapping
+    public List<ReservationTime> findAllReservationTimes() {
+        String sql = "select * from reservation_time";
+
+        return jdbcTemplate.query(sql, reservationTimeRowMapper());
+    }
+
     @PostMapping
     public ReservationTime addReservationTime(@RequestBody ReservationTimeRequest reservationTimeRequest) {
         SqlParameterSource parameters = new BeanPropertySqlParameterSource(reservationTimeRequest);
         long newId = insertActor.executeAndReturnKey(parameters).longValue();
 
         return reservationTimeRequest.toEntity(newId);
+    }
+
+    private RowMapper<ReservationTime> reservationTimeRowMapper() {
+        return (resultSet, rowNum) -> new ReservationTime(
+                resultSet.getLong("id"),
+                resultSet.getString("start_at")
+        );
     }
 }

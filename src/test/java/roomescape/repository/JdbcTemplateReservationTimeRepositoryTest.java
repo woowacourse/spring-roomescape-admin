3f6cf9f420fea7258608dacb016f2d02c0173cd6
le2sky <leehaneul990623@gmail.com>
2024-04-21 18:34:47 +0900

    test: reservation time repository 테스트 코드 작성

---
+++ src/test/java/roomescape/repository/JdbcTemplateReservationTimeRepositoryTest.java
@@ -0,0 +1,83 @@
+package roomescape.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalTime;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+import roomescape.domain.ReservationTime;
+import roomescape.domain.ReservationTimeRepository;
+
+@SpringBootTest
+@Transactional
+class JdbcTemplateReservationTimeRepositoryTest {
+
+    @Autowired
+    private ReservationTimeRepository repository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @DisplayName("신규 예약 시간을 저장할 수 있다.")
+    @Test
+    void save() {
+        LocalTime expectedTime = LocalTime.now();
+        ReservationTime reservationTime = new ReservationTime(expectedTime);
+
+        ReservationTime saved = repository.save(reservationTime);
+
+        ReservationTime found = findById(saved.getId());
+        assertThat(saved.getId()).isEqualTo(found.getId());
+        assertThat(saved.getStartAt()).isEqualTo(expectedTime);
+    }
+
+    @DisplayName("예약 시간을 삭제할 수 있다.")
+    @Test
+    void delete() {
+        save("12:30");
+
+        repository.deleteBy(1L);
+
+        assertThatThrownBy(() -> findById(1L))
+                .isInstanceOf(EmptyResultDataAccessException.class);
+    }
+
+    @DisplayName("모든 예약 시간을 불러올 수 있다.")
+    @Test
+    void findAll() {
+        save("12:30");
+        save("14:30");
+
+        List<ReservationTime> times = repository.findAll();
+
+        assertThat(times).hasSize(2);
+    }
+
+    @DisplayName("예약 시간이 존재하지 않으면 빈 리스트를 반환한다.")
+    @Test
+    void findAllWhenEmpty() {
+        List<ReservationTime> times = repository.findAll();
+
+        assertThat(times).isEmpty();
+    }
+
+    private void save(String startAt) {
+        jdbcTemplate.update("insert into reservation_time (start_at) values (?)", startAt);
+    }
+
+    private ReservationTime findById(long id) {
+        String sql = "select * from reservation_time where id = ?";
+
+        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new ReservationTime(
+                rs.getLong("id"),
+                rs.getTime("start_at").toLocalTime()
+        ), id);
+    }
+}

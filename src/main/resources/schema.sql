3677c5ea5b85dd5bb079a1489af76672cd25974a
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-04-21 12:03:56 +0900

    build : application.properties 수정, schema sql 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE reservation
+(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name varchar(255) not null,
+    date varchar(255) not null,
+    time varchar(255) not null,
+    primary key (id)
+);

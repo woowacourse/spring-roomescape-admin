bfc22612e3111b3e40f003d0f3800b13c6e2dbb5
yoonjuho <dbswn990@naver.com>
2024-04-22 01:15:32 +0900

    feat(ReservationController): JDBCTemplate 주입

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,15 +1,13 @@
 package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 import roomescape.domain.Reservation;
 import roomescape.dto.ReservationRequestDto;
 import roomescape.dto.ReservationResponseDto;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
@@ -18,34 +16,54 @@ import java.util.concurrent.atomic.AtomicLong;
 public class ReservationController {
     private final ConcurrentHashMap<Long, Reservation> reservations = new ConcurrentHashMap<>();
     private final AtomicLong index = new AtomicLong(1);
+    private final JdbcTemplate jdbcTemplate;
+
+    ReservationController(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @GetMapping("/reservations")
     public List<ReservationResponseDto> reservations() {
-        return reservations.values()
-                           .stream()
-                           .map(ReservationResponseDto::new)
-                           .toList();
+        final String sql = "select id, name, date, time from reservation";
+        final List<Reservation> reservationList = jdbcTemplate.query(sql, reservationRowMapper);
+        return reservationList.stream()
+                              .map(ReservationResponseDto::from)
+                              .toList();
+    }
+
+    @GetMapping("/reservations/{id}")
+    public ReservationResponseDto reservation(@PathVariable final Long id) {
+        final String sql = "select id, name, date, time from reservation where id = ?";
+        final Reservation reservation = jdbcTemplate.queryForObject(sql, reservationRowMapper, id);
+        return ReservationResponseDto.from(reservation);
     }
 
     @PostMapping("/reservations")
     public ReservationResponseDto create(@RequestBody final ReservationRequestDto request) {
-        final LocalTime time = LocalTime.parse(request.getTime(), DateTimeFormatter.ofPattern("HH:mm"));
-        final LocalDate date = LocalDate.parse(request.getDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        final LocalDateTime dateTime = LocalDateTime.of(date, time);
-
         final Long id = index.getAndIncrement();
-        final Reservation reservation = new Reservation(id, request.getName(), dateTime);
+        final Reservation reservation = new Reservation(id, request.getName(), request.getDate(), request.getTime());
         reservations.put(id, reservation);
-
-        return new ReservationResponseDto(reservation);
+        return ReservationResponseDto.from(reservation);
     }
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> delete(@PathVariable final Long id) {
         if (reservations.containsKey(id)) {
             reservations.remove(id);
-            return ResponseEntity.noContent().build();
+            return ResponseEntity.noContent()
+                                 .build();
         }
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.notFound()
+                             .build();
     }
+
+    private final RowMapper<Reservation> reservationRowMapper = (resultSet, rowNum) -> {
+        Reservation reservation = new Reservation(
+                resultSet.getLong("id"),
+                resultSet.getString("name"),
+                resultSet.getString("date"),
+                resultSet.getString("time")
+        );
+        return reservation;
+    };
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -9,12 +9,14 @@ import io.restassured.http.ContentType;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.dto.ReservationResponseDto;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -42,7 +44,7 @@ class MissionStepTest {
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
+                .body("size()", is(0));
     }
 
     @Test
@@ -60,11 +62,11 @@ class MissionStepTest {
                 .statusCode(200)
                 .body("id", is(1));
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
+//        RestAssured.given().log().all()
+//                .when().get("/reservations")
+//                .then().log().all()
+//                .statusCode(200)
+//                .body("size()", is(1));
 
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
@@ -92,5 +94,26 @@ class MissionStepTest {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Test
+    void 오단계() {
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
+
+        List<ReservationResponseDto> reservations = RestAssured.given().log().all()
+                                                               .when().get("/reservations")
+                                                               .then().log().all()
+                                                               .statusCode(200).extract()
+                                                               .jsonPath().getList(".", ReservationResponseDto.class);
+        ReservationResponseDto reservationResponseDto = RestAssured.given().log().all()
+                                                                   .when().get("/reservations/1")
+                                                                   .then().log().all()
+                                                                   .statusCode(200).extract()
+                                                                   .jsonPath().getObject(".", ReservationResponseDto.class);
+
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+
+        assertThat(reservations.size()).isEqualTo(count);
+        assertThat(reservationResponseDto.getId()).isEqualTo(1L);
     }
 }

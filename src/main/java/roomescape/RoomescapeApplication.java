859933d628c52238bea55f6dcefc16831608c6b7
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-04-21 17:19:16 +0900

    feat: h2 데이터베이스와 연동한다.

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -1,12 +1,15 @@
 package roomescape;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class RoomescapeApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeApplication.class, args);
     }
-
 }

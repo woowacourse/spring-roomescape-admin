e5d35b575f231b6f27585793ca8482f7787b6b91
jinwoo22 <jenny010925@naver.com>
2024-04-21 14:43:47 +0900

    style: 사용하지 않는 import문 제거

--- src/main/java/roomescape/dto/request/ReservationCreateRequest.java
+++ src/main/java/roomescape/dto/request/ReservationCreateRequest.java
@@ -2,7 +2,6 @@ package roomescape.dto.request;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import roomescape.domain.Reservation;
 
 public record ReservationCreateRequest(String name, LocalDate date, LocalTime time) {
 }

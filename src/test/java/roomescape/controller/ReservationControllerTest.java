0bfbd4656c0c561c25a063b2991b166451580e70
3juhwan <13selfesteem91@naver.com>
2024-04-21 17:36:18 +0900

    test: controller 슬라이스 테스트

---
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -0,0 +1,67 @@
+package roomescape.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import roomescape.domain.Reservation;
+import roomescape.repository.InMemoryReservationRepositoryImpl;
+import roomescape.repository.ReservationRepository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import(InMemoryReservationRepositoryImpl.class)
+@WebMvcTest(ReservationController.class)
+class ReservationControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DisplayName("[200] GET /reservations")
+    @Test
+    void findAll() throws Exception {
+        this.mvc.perform(get("/reservations"))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("[201] POST /reservations")
+    @Test
+    void add() throws Exception {
+        String reservation = objectMapper.writeValueAsString(new Reservation(
+                "비밥",
+                LocalDate.now().plusDays(1),
+                LocalTime.of(9, 0)));
+
+        this.mvc.perform(post("/reservations")
+                        .content(reservation)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    @DisplayName("[204] DELETE /reservations/{id}")
+    @Test
+    void deleteTest() throws Exception {
+        Reservation savedReservation = reservationRepository.save(new Reservation(
+                "비밥",
+                LocalDate.now().plusDays(1),
+                LocalTime.of(9, 0)));
+
+        this.mvc.perform(delete("/reservations/" + savedReservation.getId()))
+                .andExpect(status().isNoContent());
+    }
+}

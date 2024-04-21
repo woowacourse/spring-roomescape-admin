7affc3364703c4996540b9d2eb568d6c35af64fb
이상진 <priceelees@gmail.com>
2024-04-21 17:17:53 +0900

    style(AdminController): 인덱스 페이지 접속 메서드명 수정(admin -> index)

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 
     @GetMapping
-    public String admin() {
+    public String index() {
         return "/admin/index";
     }
 

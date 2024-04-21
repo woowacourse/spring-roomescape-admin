8f9c03eabff110cd809a4460cdaf75da6094ae2a
BurningFalls <sherlocky31415@gmail.com>
2024-04-21 11:50:03 +0900

    database: date, time 타입 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE reservation
 (
-    id      BIGINT          NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255)    NOT NULL,
-    date    VARCHAR(255)    NOT NULL,
-    time    VARCHAR(255)    NOT NULL,
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    date DATE         NOT NULL,
+    time TIME         NOT NULL,
     PRIMARY KEY (id)
 );

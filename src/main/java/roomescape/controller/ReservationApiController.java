82218e962c24ca8908ba570159295ee411283ff8
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-04-21 10:55:27 +0900

    refactor(ReservationDto) : 클래스 명 변경
    클래스명 : ReservationDto -> ReservationRequest
    사용하는 코드 반영

--- src/main/java/roomescape/controller/ReservationApiController.java
+++ src/main/java/roomescape/controller/ReservationApiController.java
@@ -1,7 +1,7 @@
 package roomescape.controller;
 
 import org.springframework.web.bind.annotation.*;
-import roomescape.dto.ReservationDto;
+import roomescape.dto.ReservationRequest;
 import roomescape.domain.Reservation;
 
 import java.util.ArrayList;
@@ -22,8 +22,8 @@ public class ReservationApiController {
     }
 
     @PostMapping
-    public Reservation createReservation(@RequestBody final ReservationDto reservationDto) {
-        Reservation reservation = Reservation.toEntity(index.getAndIncrement(), reservationDto);
+    public Reservation createReservation(@RequestBody final ReservationRequest reservationRequest) {
+        Reservation reservation = Reservation.toEntity(index.getAndIncrement(), reservationRequest);
         reservations.add(reservation);
         return reservation;
     }
---
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -0,0 +1,4 @@
+package roomescape.dto;
+
+public record ReservationRequest(String name, String date, String time) {
+}

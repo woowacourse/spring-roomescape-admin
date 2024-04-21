9c1964a09172181ebf82208f66646b7444d6d9ce
이상진 <priceelees@gmail.com>
2024-04-21 13:23:23 +0900

    refactor(ReservationRequest): 요구사항에 맞게 예약 Request 에 time_id를 넣도록 수정

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -1,10 +1,11 @@
 package roomescape.dto;
 
 import roomescape.entity.Reservation;
+import roomescape.entity.ReservationTime;
 
-public record ReservationRequest(String name, String date, String time) {
+public record ReservationRequest(String name, String date, long timeId) {
 
-    public Reservation toEntity(long id) {
-        return new Reservation(id, name, date, time);
+    public Reservation toEntity(long id, ReservationTime reservationTime) {
+        return new Reservation(id, name, date, reservationTime);
     }
 }

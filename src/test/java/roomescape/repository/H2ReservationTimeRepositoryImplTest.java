b122e2f9f35378472258319dbcd5a0beadde8e7a
3juhwan <13selfesteem91@naver.com>
2024-04-21 22:38:43 +0900

    test: 예약 시간 repository 테스트 추가

---
+++ src/test/java/roomescape/repository/H2ReservationTimeRepositoryImplTest.java
@@ -0,0 +1,51 @@
+package roomescape.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import roomescape.domain.ReservationTime;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+class H2ReservationTimeRepositoryImplTest {
+    @Autowired
+    private H2ReservationTimeRepositoryImpl reservationTimeRepository;
+
+    @DisplayName("예약 시간을 저장한다")
+    @Test
+    void save() {
+        ReservationTime savedReservation = reservationTimeRepository.save(new ReservationTime(
+                LocalTime.of(12, 0)));
+
+        assertThat(savedReservation.getStartAt())
+                .isEqualTo(LocalTime.of(12, 0));
+    }
+
+    @DisplayName("저장된 예약 시간을 조회한다")
+    @Test
+    void findAll() {
+        List<ReservationTime> reservationTimes = reservationTimeRepository.findAll();
+
+        assertThat(reservationTimes.size())
+                .isEqualTo(0);
+    }
+
+    @DisplayName("저장된 예약 시간을 삭제한다")
+    @Test
+    void deleteById() {
+        ReservationTime savedReservation = reservationTimeRepository.save(new ReservationTime(
+                LocalTime.of(12, 0)));
+
+        reservationTimeRepository.deleteById(savedReservation.getId());
+
+        assertThat(reservationTimeRepository.findAll().size())
+                .isEqualTo(0);
+    }
+}

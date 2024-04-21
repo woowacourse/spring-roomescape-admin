8f55df69076230a1ceab081d1f98545be4199209
이상진 <priceelees@gmail.com>
2024-04-21 12:03:46 +0900

    feat(ReservationTimeApiController): 예약 시간 삭제 Api 구현

--- src/main/java/roomescape/controller/ReservationTimeApiController.java
+++ src/main/java/roomescape/controller/ReservationTimeApiController.java
@@ -7,7 +7,9 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +44,12 @@ public class ReservationTimeApiController {
         long newId = insertActor.executeAndReturnKey(parameters).longValue();
 
         return reservationTimeRequest.toEntity(newId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReservationTime(@PathVariable long id) {
+        String sql = "delete from reservation_time where id = ?";
+        jdbcTemplate.update(sql, id);
     }
 
     private RowMapper<ReservationTime> reservationTimeRowMapper() {

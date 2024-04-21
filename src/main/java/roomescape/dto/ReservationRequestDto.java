d47c5aed2c474f57f995782ee969a5fa049341bd
3juhwan <13selfesteem91@naver.com>
2024-04-21 17:38:27 +0900

    feat: dto에 예약 정책 추가

--- src/main/java/roomescape/dto/ReservationRequestDto.java
+++ src/main/java/roomescape/dto/ReservationRequestDto.java
@@ -1,7 +1,53 @@
 package roomescape.dto;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Objects;
 
-public record ReservationRequestDto(String name, LocalDate date, LocalTime time) {
+public record ReservationRequestDto(
+        String name,
+        LocalDate date,
+        LocalTime time
+) {
+    private static final int NAME_LENGTH_MIN = 2;
+    private static final int NAME_LENGTH_MAX = 10;
+    private static final int RESERVATION_TIME_MIN = 9;
+    private static final int RESERVATION_TIME_MAX = 20;
+
+    public ReservationRequestDto {
+        validateName(name);
+        validateDate(date);
+        validateTime(time);
+        validateDateTime(date, time);
+    }
+
+    public static void validateName(String name) {
+        Objects.requireNonNull(name);
+        if (name.length() < NAME_LENGTH_MIN || NAME_LENGTH_MAX < name.length()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDate(LocalDate date) {
+        Objects.requireNonNull(date);
+        if (date.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateTime(LocalTime time) {
+        Objects.requireNonNull(time);
+        if (time.isBefore(LocalTime.of(RESERVATION_TIME_MIN, 0))
+                || time.isAfter(LocalTime.of(RESERVATION_TIME_MAX, 0))) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDateTime(LocalDate date, LocalTime time) {
+        LocalDateTime localDateTime = LocalDateTime.of(date, time);
+        if (localDateTime.isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException();
+        }
+    }
 }

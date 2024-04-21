1313ac5e06c2e0544bf5ff04bd0807507ec2ac02
이상진 <priceelees@gmail.com>
2024-04-21 17:05:05 +0900

    refactor(Reservation): 필드 타입 변경 및 생성자 추가
    - id의 타입을 long -> Long 으로 변경
    - 모든 필드를 final 로 수정
    - id와 다른 Reservation 객체를 이용하는 생성자 추가

--- src/main/java/roomescape/entity/Reservation.java
+++ src/main/java/roomescape/entity/Reservation.java
@@ -2,19 +2,23 @@ package roomescape.entity;
 
 public class Reservation {
 
-    private long id;
-    private String name;
-    private String date;
-    private ReservationTime time;
+    private final Long id;
+    private final String name;
+    private final String date;
+    private final ReservationTime time;
 
-    public Reservation(long id, String name, String date, ReservationTime time) {
+    public Reservation(Long id, String name, String date, ReservationTime time) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
     }
 
-    public long getId() {
+    public Reservation(Long id, Reservation reservation) {
+        this(id, reservation.getName(), reservation.getDate(), reservation.getTime());
+    }
+
+    public Long getId() {
         return id;
     }
 

f49cae53bd9291ef249ce2c2487fd0aad196d51a
haiseong <wjdgotjd9908@gmail.com>
2024-04-21 20:18:41 +0900

    feat(JdbcTemplateTest, schema.sql): 데이터 베이스 연결

---
+++ src/test/java/roomescape/admin/JdbcTemplateTest.java
@@ -0,0 +1,29 @@
+package roomescape.admin;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+public class JdbcTemplateTest {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void 사단계() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

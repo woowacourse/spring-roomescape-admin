e74c3e74b4e236bb2411c6a794de4b80ec78f8b0
yoonjuho <dbswn990@naver.com>
2024-04-22 01:12:17 +0900

    refactor(ReservationResponseDto): 정적 팩토리 메서드 추가

--- src/main/java/roomescape/dto/ReservationResponseDto.java
+++ src/main/java/roomescape/dto/ReservationResponseDto.java
@@ -1,21 +1,22 @@
 package roomescape.dto;
 
-import java.time.format.DateTimeFormatter;
 import roomescape.domain.Reservation;
 
 public class ReservationResponseDto {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-
     private final Long id;
     private final String name;
     private final String date;
     private final String time;
 
-    public ReservationResponseDto(final Reservation reservation) {
-        this.id = reservation.getId();
-        this.name = reservation.getName();
-        this.date = reservation.getDate().toString();
-        this.time = reservation.getTime().format(formatter);
+    private ReservationResponseDto(final Long id, final String name, final String date, final String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public static ReservationResponseDto from(final Reservation reservation) {
+        return new ReservationResponseDto(reservation.getId(), reservation.getName(), reservation.getDate().toString(), reservation.getTime().toString());
     }
 
     public Long getId() {

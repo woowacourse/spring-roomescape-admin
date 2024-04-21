859076aa43dfa39f70d62975dfb272854cd726ad
Hyeon0208 <dmsgktn0208@gmail.com>
2024-04-21 17:52:52 +0900

    refactor : import문 정리

--- src/main/java/roomescape/time/TimeApiController.java
+++ src/main/java/roomescape/time/TimeApiController.java
@@ -1,7 +1,6 @@
 package roomescape.time;
 
 import java.util.List;
-import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;

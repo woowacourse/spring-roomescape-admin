d127a597296c8dae245f26b265b2a243709da461
Hyeon0208 <dmsgktn0208@gmail.com>
2024-04-21 17:24:13 +0900

    feat : time 객체를 반환하도록 수정 및 조회 쿼리 추가

--- src/test/java/roomescape/time/TimeRepositoryTest.java
+++ src/test/java/roomescape/time/TimeRepositoryTest.java
@@ -12,9 +12,11 @@ import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 
 @DisplayName("시간 레포지토리")
 @JdbcTest
+@Sql(scripts = "classpath:reservation-test.sql")
 @Import(TimeRepository.class)
 class TimeRepositoryTest {
 
@@ -29,18 +31,18 @@ class TimeRepositoryTest {
                     TimeRequest timeRequest = new TimeRequest("10:00");
 
                     // when
-                    Long id = timeRepository.save(timeRequest);
+                    Time time = timeRepository.save(timeRequest);
 
                     // then
-                    assertThat(id).isEqualTo(1L);
+                    assertThat(time.getId()).isEqualTo(2L);
                 }),
                 DynamicTest.dynamicTest("id로 시간 정보를 조회한다.", () -> {
                     // given
-                    Optional<Time> reservation = timeRepository.findById(1L);
+                    Optional<Time> reservation = timeRepository.findById(2L);
 
                     // then
                     assertAll(
-                            () -> assertThat(reservation.get().getId()).isEqualTo(1L),
+                            () -> assertThat(reservation.get().getId()).isEqualTo(2L),
                             () -> assertThat(reservation.get().getStartAt()).isEqualTo("10:00")
                     );
                 }),
@@ -49,11 +51,22 @@ class TimeRepositoryTest {
                     List<Time> reservations = timeRepository.findAll();
 
                     // then
-                    assertThat(reservations).hasSize(1);
+                    assertThat(reservations).hasSize(2);
+                }),
+                DynamicTest.dynamicTest("time 기본키를 참조하는 reservation이 있는지 조회한다.", () -> {
+                    // given & when
+                    Optional<Time> time1 = timeRepository.findBySameReferId(1L);
+                    Optional<Time> time2 = timeRepository.findBySameReferId(2L);
+
+                    // then
+                    assertAll(
+                            () -> assertThat(time1.isPresent()).isTrue(),
+                            () -> assertThat(time2.isPresent()).isFalse()
+                    );
                 }),
                 DynamicTest.dynamicTest("id로 시간 정보를 제거한다.", () -> {
                     // given
-                    Long id = 1L;
+                    Long id = 2L;
 
                     // when
                     timeRepository.deleteById(id);

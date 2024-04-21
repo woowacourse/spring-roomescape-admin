8cd9a623d929733e58905147535a65c29b824843
zangsu <zangsu_@naver.com>
2024-04-22 02:04:38 +0900

    fix(ReservationTimeFindResponse) : 잘못된 필드 명 수정

--- src/main/java/roomescape/controller/dto/ReservationTimeFindResponse.java
+++ src/main/java/roomescape/controller/dto/ReservationTimeFindResponse.java
@@ -3,7 +3,7 @@ package roomescape.controller.dto;
 import roomescape.domain.ReservationTime;
 import roomescape.util.CustomDateTimeFormatter;
 
-public record ReservationTimeFindResponse(Long id, String startedAt) {
+public record ReservationTimeFindResponse(Long id, String startAt) {
     public static ReservationTimeFindResponse of(ReservationTime reservationTime) {
         return new ReservationTimeFindResponse(
                 reservationTime.getId(),

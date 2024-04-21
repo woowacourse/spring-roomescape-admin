39da20f673d568df2485fd4a8594f1a385ece317
yoonjuho <dbswn990@naver.com>
2024-04-22 01:13:13 +0900

    refactor(Reservation): 필드 분리 및 상수 추가

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,18 +1,26 @@
 package roomescape.domain;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Reservation {
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm");
     private final Long id;
     private final String name;
-    private final LocalDateTime dateTime;
+    private final LocalDate date;
+    private final LocalTime time;
 
-    public Reservation(final Long id, final String name, final LocalDateTime dateTime) {
+    public Reservation(final Long id, final String name, final LocalDate date, final LocalTime time) {
         this.id = id;
         this.name = name;
-        this.dateTime = dateTime;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Reservation(final Long id, final String name, final String date, final String time) {
+        this(id, name, LocalDate.parse(date, DATE_FORMAT), LocalTime.parse(time, TIME_FORMAT));
     }
 
     public Long getId() {
@@ -24,10 +32,10 @@ public class Reservation {
     }
 
     public LocalDate getDate() {
-        return dateTime.toLocalDate();
+        return date;
     }
 
     public LocalTime getTime() {
-        return dateTime.toLocalTime();
+        return time;
     }
 }

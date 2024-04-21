00e90865b9564efc1bc9cde18025a7c398106ea0
leegwichan <cnddkscndgus@naver.com>
2024-04-21 09:54:20 +0900

    refactor(ReservationTime): 시간 형식의 유효성 검사 기능 추가

--- src/main/java/roomescape/domain/ReservationTime.java
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -1,11 +1,35 @@
 package roomescape.domain;
 
-public record ReservationTime(
-        Long id,
-        String startAt) {
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class ReservationTime {
+
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
+    private final Long id;
+    private final LocalTime startAt;
+
+    public ReservationTime(Long id, String startAt) {
+        this.id = id;
+        this.startAt = LocalTime.parse(startAt, TIME_FORMATTER);
+    }
+
+    public ReservationTime(Long id) {
+        this.id = id;
+        this.startAt = null;
+    }
 
     public ReservationTime(String startAt) {
         this(null, startAt);
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public String startAt() {
+        return startAt.format(TIME_FORMATTER);
     }
 
 }

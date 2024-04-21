77f277b495f7d927733b2991e859e9c758b63793
Seokmyung Ham <gkatjraud1@gmail.com>
2024-04-21 19:40:53 +0900

    feat(ReservationDate): 예약 날짜를 포장하는 클래스 추가

---
+++ src/test/java/roomescape/domain/ReservationDateTest.java
@@ -0,0 +1,23 @@
+package roomescape.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ReservationDateTest {
+
+    @Test
+    @DisplayName("날짜가 같으면 같은 hash 값을 갖고 동일한 객체이다.")
+    void hash_equals_override_test() {
+        ReservationDate date1 = new ReservationDate(LocalDate.of(2024, 4, 21));
+        ReservationDate date2 = new ReservationDate(LocalDate.of(2024, 4, 21));
+
+        assertAll(
+                () -> assertThat(date1.hashCode()).isEqualTo(date2.hashCode()),
+                () -> assertThat(date1).isEqualTo(date2)
+        );
+    }
+}

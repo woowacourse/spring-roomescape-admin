0eb1f83973ae4ada938cd220c741e0a9deb21eb2
ehtjsv2 <ehtjsv2@naver.com>
2024-04-22 16:09:42 +0900

    fix: 페이지에서 예약 삭제 안되는 버그 수정

--- src/main/resources/static/js/reservation-legacy.js
+++ src/main/resources/static/js/reservation-legacy.js
@@ -133,7 +133,7 @@ function requestDelete(id) {
 
   return fetch(`${RESERVATION_API_ENDPOINT}/${id}`, requestOptions)
       .then(response => {
-        if (response.status !== 200) throw new Error('Delete failed');
+        if (response.status !== 204) throw new Error('Delete failed');
       });
 }
 

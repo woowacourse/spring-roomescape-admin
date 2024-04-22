5c1aa2ede888dedc09aaeba3522251c8c3f2d993
hoyeonyy <highoption@naver.com>
2024-04-22 16:08:36 +0900

    test(...) : RandomPort 변경

--- src/test/java/roomescape/admin/AdminControllerTest.java
+++ src/test/java/roomescape/admin/AdminControllerTest.java
@@ -1,14 +1,25 @@
 package roomescape.admin;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class AdminControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void beforeEach() {
+        RestAssured.port = port;
+    }
 
     @Test
     @DisplayName("root url 을 요청하면 welcome page 를 반환한다.")

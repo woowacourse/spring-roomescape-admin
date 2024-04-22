986a91e7db6970ea5b5e5b061333853824981884
Arachneee <jhg2819@naver.com>
2024-04-22 21:13:50 +0900

    feat: json 포맷 검증 추가

--- src/main/java/roomescape/controller/dto/ReservationRequest.java
+++ src/main/java/roomescape/controller/dto/ReservationRequest.java
@@ -1,10 +1,12 @@
 package roomescape.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import roomescape.service.dto.ReservationServiceRequest;
 
 public record ReservationRequest(
         String name,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
         LocalDate date,
         Long timeId
 ) {
--- src/main/java/roomescape/controller/dto/ReservationTimeRequest.java
+++ src/main/java/roomescape/controller/dto/ReservationTimeRequest.java
@@ -1,9 +1,11 @@
 package roomescape.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 import roomescape.service.dto.ReservationTimeServiceRequest;
 
 public record ReservationTimeRequest(
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm", timezone = "Asia/Seoul")
         LocalTime startAt
 ) {
 

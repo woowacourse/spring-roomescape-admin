f7c8d123d7303e239ab19ce06488bbd6b18cf564
fram1998 <koust6u@pusan.ac.kr>
2024-04-22 15:50:08 +0900

    feat: 데이터베이스 적용 전체 조회 기능 구현

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -1,7 +1,10 @@
 package roomescape.repository;
 
+
+import java.util.List;
 import roomescape.Reservation;
 
 public interface ReservationRepository {
     void save(final Reservation reservation);
+    List<Reservation> findAll();
 }
--- src/main/java/roomescape/repository/ReservationRepositoryImpl.java
+++ src/main/java/roomescape/repository/ReservationRepositoryImpl.java
@@ -1,6 +1,8 @@
 package roomescape.repository;
 
+import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import roomescape.Reservation;
 
@@ -19,4 +21,18 @@ public class ReservationRepositoryImpl implements ReservationRepository {
         jdbcTemplate.update(sql, reservation.getName(), reservation.getDate(), reservation.getTime());
     }
 
+    @Override
+    public List<Reservation> findAll() {
+        final var sql = "SELECT id, name, date, time FROM reservation";
+        return jdbcTemplate.query(sql, actorRowMapper());
+    }
+
+    private RowMapper<Reservation> actorRowMapper() {
+        return (resultSet, rowNum) -> new Reservation.Builder()
+                .id(resultSet.getLong("id"))
+                .name(resultSet.getString("name"))
+                .date(resultSet.getDate("date").toLocalDate())
+                .time(resultSet.getTime("time").toLocalTime())
+                .build();
+    }
 }

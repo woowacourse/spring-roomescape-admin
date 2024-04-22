f632a40de6a183ec8461ce878f76554eede51e92
hoyeonyy <highoption@naver.com>
2024-04-22 16:09:06 +0900

    feat(jdbcTest) : DB연결 테스트 및 스키마 작성

---
+++ src/test/java/roomescape/JdbcTest.java
@@ -0,0 +1,37 @@
+package roomescape;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class JdbcTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    @DisplayName("데이터베이스 연동 테스트")
+    void jdbcTest() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertAll(
+                    () -> assertThat(connection).isNotNull(),
+                    () -> assertThat(connection.getCatalog()).isEqualTo("DATABASE"),
+                    () -> assertThat(
+                            connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

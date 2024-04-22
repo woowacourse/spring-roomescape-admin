e043f3aae9db76052143f520eb423395fc57ea43
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-04-22 21:09:19 +0900

    build(schema) : reservation table 속성 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,17 @@
-CREATE TABLE reservation
-(
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
-    name varchar(255) not null,
-    date varchar(255) not null,
-    time varchar(255) not null,
-    primary key (id)
-);
 CREATE TABLE reservation_time
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE reservation
+(
+    id      BIGINT       NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(255) NOT NULL,
+    date    VARCHAR(255) NOT NULL,
+    time_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
+);
+

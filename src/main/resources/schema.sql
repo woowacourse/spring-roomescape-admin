3e37e61940bee9bae87f24d62f29c069808bd761
Nam Gi Beom <rlqja0523@naver.com>
2024-04-22 15:01:44 +0900

    chore: time schema 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,15 @@
+-- CREATE TABLE IF NOT EXISTS reservation
 CREATE TABLE reservation
 (
     id                    BIGINT       NOT NULL AUTO_INCREMENT,
     name                  VARCHAR(255) NOT NULL,
     reservation_date_time DATETIME     NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE reservation_time
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );

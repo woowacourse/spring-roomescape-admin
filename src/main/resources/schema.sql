54a4a0a2abc4b9635417051f3286b4bff535e219
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-04-22 19:43:13 +0900

    build(schema) : reservation_time 생성하는 sql 문 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,14 @@
 CREATE TABLE reservation
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
     name varchar(255) not null,
     date varchar(255) not null,
     time varchar(255) not null,
     primary key (id)
+);
+CREATE TABLE reservation_time
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );

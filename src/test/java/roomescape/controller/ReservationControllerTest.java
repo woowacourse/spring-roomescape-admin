304b23ee66d94ff0eada69ef16cb7a8b54f79930
ehtjsv2 <ehtjsv2@naver.com>
2024-04-22 18:03:16 +0900

    refactor: 중복되는 테스트 삭제

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -1,28 +1,19 @@
 package roomescape.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
-import roomescape.domain.Reservation;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ReservationControllerTest {
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     @DisplayName("/reservations get 요청 시 예약 목록을 응답할 수 있다")
     @Test
@@ -130,12 +121,19 @@ public class ReservationControllerTest {
                 .body("size()", is(1));
     }
 
-
-    // 이전 단계들 테스트
-    @DisplayName("/reservations post 요청 시 예약을 추가할 수 있다.")
-    @Disabled
+    @DisplayName("/reservations/{id} delete 요청 시 예약을 삭제할 수 있다.")
     @Test
-    void should_add_reservation_when_post_request_reservations_old() {
+    void should_remove_reservation_when_delete_request_reservations_id() {
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(200);
+
         Map<String, Object> reservation = new HashMap<>();
         reservation.put("name", "브라운");
         reservation.put("date", "2023-08-05");
@@ -149,30 +147,6 @@ public class ReservationControllerTest {
                 .statusCode(201);
 
         RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
-    }
-
-    @DisplayName("/reservations/{id} delete 요청 시 예약을 삭제할 수 있다.")
-    @Disabled
-    @Test
-    void should_remove_reservation_when_delete_request_reservations_id() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(201)
-                .body("id", is(1));
-
-        RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
                 .statusCode(204);
@@ -183,52 +157,4 @@ public class ReservationControllerTest {
                 .statusCode(200)
                 .body("size()", is(0));
     }
-
-    @DisplayName("reservation 테이블과 연결이 가능합니다.")
-    @Disabled
-    @Test
-    void should_equal_reservation_size_both_db_and_jdbc() {
-        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05",
-                "15:40");
-        List<Reservation> reservations = RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200).extract()
-                .jsonPath().getList(".", Reservation.class);
-
-        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
-
-        assertThat(reservations.size()).isEqualTo(count);
-    }
-
-    @DisplayName("db 테이블에 예약정보를 추가하고 삭제할 수 있습니다.")
-    @Disabled
-    @Test
-    void should_add_and_remove_reservation() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "10:00");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(201)
-                .header("Location", "/reservations/1");
-
-        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
-        assertThat(count).isEqualTo(1);
-
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
-                .then().log().all()
-                .statusCode(204);
-
-        Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
-        assertThat(countAfterDelete).isEqualTo(0);
-    }
-
-
 }

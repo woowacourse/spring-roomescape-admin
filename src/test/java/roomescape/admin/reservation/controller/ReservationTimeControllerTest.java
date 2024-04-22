becd23efce6ecb964aa57e7876f81c49e18fc745
haiseong <wjdgotjd9908@gmail.com>
2024-04-23 02:16:21 +0900

    refactor(ReservationTimeControllerTest): DisplayName 추가

--- src/test/java/roomescape/admin/reservation/controller/ReservationTimeControllerTest.java
+++ src/test/java/roomescape/admin/reservation/controller/ReservationTimeControllerTest.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -13,14 +14,16 @@ import org.springframework.test.annotation.DirtiesContext;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationTimeControllerTest {
+
     @Test
+    @DisplayName("예약시간을 추가하고 삭제합니다.")
     void testReservationTimeCRD() {
-        Map<String, String> params = new HashMap<>();
-        params.put("startAt", "10:00");
+        Map<String, String> reservationTimeParameters = new HashMap<>();
+        reservationTimeParameters.put("startAt", "10:00");
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
-                .body(params)
+                .body(reservationTimeParameters)
                 .when().post("/times")
                 .then().log().all()
                 .statusCode(200);

3f007dbb24023974234e8c53056a2da1d9b7a3be
BurningFalls <sherlocky31415@gmail.com>
2024-04-22 15:01:55 +0900

    refactor: @Controller -> @RestController

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,7 +2,6 @@ package roomescape.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import roomescape.dto.ReservationDto;
 import roomescape.entity.Reservation;
@@ -11,7 +10,7 @@ import roomescape.service.ReservationService;
 import java.net.URI;
 import java.util.List;
 
-@Controller
+@RestController
 public class ReservationController {
     private final ReservationService reservationService;
 
@@ -28,13 +27,6 @@ public class ReservationController {
         return ResponseEntity.created(URI.create("/reservations/" + id)).body(reservation);
     }
 
-    private Reservation makeReservation(ReservationDto reservationDto, long id) {
-        String name = reservationDto.name();
-        String date = reservationDto.date();
-        String time = reservationDto.time();
-        return new Reservation(id, name, date, time);
-    }
-
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> getAllReservations() {
         return ResponseEntity.ok(reservationService.getAllReservations());
@@ -44,5 +36,12 @@ public class ReservationController {
     public ResponseEntity<Void> deleteReservationById(@PathVariable("id") long id) {
         reservationService.deleteReservationWithId(id);
         return ResponseEntity.noContent().build();
+    }
+
+    private Reservation makeReservation(ReservationDto reservationDto, long id) {
+        String name = reservationDto.name();
+        String date = reservationDto.date();
+        String time = reservationDto.time();
+        return new Reservation(id, name, date, time);
     }
 }

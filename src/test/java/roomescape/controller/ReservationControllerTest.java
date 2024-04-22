009d8c65f30e8af7a78c6b2b733752ba69629e1b
Zeus6768 <akaps6768@gmail.com>
2024-04-22 22:09:47 +0900

    feat: db 연동 후 예약 삭제 API 구현
    존재하지 않는 예약을 삭제할 경우 404 not found 응답하는 로직 삭제.
      - 로직이 복잡해지므로 일단 Happy Case만 구현하는 방향으로 리팩터링

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -1,5 +1,6 @@
 package roomescape.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import java.time.LocalDate;
@@ -7,7 +8,9 @@ import java.time.LocalTime;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
 import io.restassured.RestAssured;
@@ -17,6 +20,9 @@ import roomescape.ReservationDto;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationControllerTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @DisplayName("예약 내역을 조회한다.")
     @Test
@@ -57,21 +63,18 @@ class ReservationControllerTest {
     @DisplayName("예약을 삭제한다.")
     @Test
     void delete() {
-        RestAssured.given()
-                .contentType(ContentType.JSON)
-                .body(reservationDto())
-                .post("/reservations");
+        jdbcTemplate.update(
+                "INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)",
+                "브라운", LocalDate.parse("2023-08-05"), LocalTime.parse("15:40")
+        );
 
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(204);
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
+        Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+        assertThat(countAfterDelete).isEqualTo(0);
     }
 
     static ReservationDto reservationDto() {

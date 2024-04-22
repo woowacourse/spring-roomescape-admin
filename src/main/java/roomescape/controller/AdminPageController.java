fa9eb9198416877939b6e0a09b86acc84aac0b14
tsulocalize <tsulocalize@gmail.com>
2024-04-22 15:07:20 +0900

    refactor(AdminPageController): URI 중복 제거

--- src/main/java/roomescape/controller/AdminPageController.java
+++ src/main/java/roomescape/controller/AdminPageController.java
@@ -2,21 +2,23 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminPageController {
 
-    @GetMapping("/admin")
+    @GetMapping
     public String getIndexPage() {
         return "admin/index";
     }
 
-    @GetMapping("/admin/reservation")
+    @GetMapping("/reservation")
     public String getReservationPage() {
         return "admin/reservation";
     }
 
-    @GetMapping("/admin/time")
+    @GetMapping("/time")
     public String getTimePage() {
         return "admin/time";
     }

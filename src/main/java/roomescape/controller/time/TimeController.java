f3b74cd29360d753d8ac628d1fdd8a5410d77bc6
Gyeongho Yang <chchch1213@naver.com>
2024-04-22 17:10:38 +0900

    feat: 컨트롤러에서의 사용자 요청에 따른 예약 시간 로직 처리

--- src/main/java/roomescape/controller/time/TimeController.java
+++ src/main/java/roomescape/controller/time/TimeController.java
@@ -1,7 +1,9 @@
 package roomescape.controller.time;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,11 +34,19 @@ public class TimeController {
     }
 
     @PostMapping
-    public ResponseEntity<TimeResponse> addTime(@RequestBody TimeRequest timeRequest) {
+    public ResponseEntity<TimeResponse> addTime(@RequestBody final TimeRequest timeRequest) {
         ReservationTime time = ReservationTimeMapper.map(timeRequest);
         ReservationTime savedTime = timeRepository.save(time);
         TimeResponse response = ReservationTimeMapper.map(savedTime);
 
         return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTime(@PathVariable final Long id) {
+        if (timeRepository.deleteById(id) == 0) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
     }
 }

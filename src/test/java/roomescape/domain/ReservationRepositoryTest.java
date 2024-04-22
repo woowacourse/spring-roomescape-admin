9544ca9d8eff1de7294babc366aaffd4283daa0b
hyun <osh123chm@gmail.com>
2024-04-22 15:21:35 +0900

    feat: h2 데이터베이스 연결

---
+++ src/test/java/roomescape/domain/ReservationRepositoryTest.java
@@ -0,0 +1,29 @@
+package roomescape.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootTest
+class ReservationRepositoryTest {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    @DisplayName("H2 데이터베이스에 연결한다.")
+    void connectH2Database() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

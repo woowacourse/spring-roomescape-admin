4b1deb6749a9d1314ff8baa5c67160d93193136f
tackyu <tack970913@gmail.com>
2024-04-23 01:43:29 +0900

    refactor: delete에서 발생 가능한 예외 종류 변경

--- src/main/java/roomescape/controller/ReservationsController.java
+++ src/main/java/roomescape/controller/ReservationsController.java
@@ -7,6 +7,7 @@ import roomescape.dto.ReservationResponse;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -34,7 +35,7 @@ public class ReservationsController {
         Reservation reservation = reservations.stream()
                 .filter(target -> Objects.equals(target.getId(), id))
                 .findFirst()
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(NoSuchElementException::new);
 
         reservations.remove(reservation);
     }

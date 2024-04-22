d80780614167baadfdb68ec9ce064a93e0698ae6
haiseong <wjdgotjd9908@gmail.com>
2024-04-22 23:26:34 +0900

    feat(ReservationTime): equals 메서드 오버라이딩

--- src/main/java/roomescape/admin/reservation/entity/ReservationTime.java
+++ src/main/java/roomescape/admin/reservation/entity/ReservationTime.java
@@ -1,6 +1,7 @@
 package roomescape.admin.reservation.entity;
 
 import java.time.LocalTime;
+import java.util.Objects;
 
 public class ReservationTime {
 
@@ -18,5 +19,22 @@ public class ReservationTime {
 
     public LocalTime getStartAt() {
         return startAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ReservationTime that = (ReservationTime) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }

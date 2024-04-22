93b3addd7d5035c0f68159578cb8e66d44fef04f
leetaehoon <xogns1514@gmail.com>
2024-04-22 15:48:52 +0900

    fix: Response 명세서에 맞게 수정

--- src/main/java/roomescape/reservation/dto/response/ReservationResponse.java
+++ src/main/java/roomescape/reservation/dto/response/ReservationResponse.java
@@ -3,16 +3,15 @@ package roomescape.reservation.dto.response;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import roomescape.reservation.domain.Reservation;
-import roomescape.reservation.domain.ReservationTime;
 
 public record ReservationResponse(Long id, String name,
                                   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-                                  LocalDate date, ReservationTime reservationTime) {
+                                  LocalDate date, ReservationTimeResponse time) {
 
     public static ReservationResponse from(Reservation reservation) {
         return new ReservationResponse(
                 reservation.getId(), reservation.getName(),
-                reservation.getDate(), reservation.getTime()
+                reservation.getDate(), ReservationTimeResponse.from(reservation.getTime())
         );
     }
 }

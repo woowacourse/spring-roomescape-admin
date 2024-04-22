adb85ad0d7990975566c9d6ec5ed6b36e4d7f00d
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-04-22 16:30:40 +0900

    refactor(Reservation, ReservationCreateRequest) : record로 변경

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,35 +1,8 @@
 package roomescape.domain;
 
-public class Reservation {
-    private final long id;
-    private final String name;
-    private final String date;
-    private final String time;
-
-    public Reservation(long id, String name, String date, String time) {
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
+public record Reservation(long id, String name, String date, String time) {
 
     public boolean checkSameId(long id) {
         return this.id == id;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getTime() {
-        return time;
     }
 }
--- src/main/java/roomescape/dto/ReservationCreateRequest.java
+++ src/main/java/roomescape/dto/ReservationCreateRequest.java
@@ -2,30 +2,9 @@ package roomescape.dto;
 
 import roomescape.domain.Reservation;
 
-public class ReservationCreateRequest {
-    private String name;
-    private String date;
-    private String time;
-
-    public ReservationCreateRequest(String name, String date, String time) {
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
+public record ReservationCreateRequest(String name, String date, String time) {
 
     public Reservation createReservation(long id) {
         return new Reservation(id, name, date, time);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getTime() {
-        return time;
     }
 }

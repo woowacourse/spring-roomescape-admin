0047bce50bb22889722d3bfde559a5622a1b308d
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-04-22 19:32:12 +0900

    refactor: 멤버 변수 순서 변경

--- src/main/java/roomescape/dto/app/ReservationAppRequest.java
+++ src/main/java/roomescape/dto/app/ReservationAppRequest.java
@@ -2,5 +2,5 @@ package roomescape.dto.app;
 
 import java.time.LocalDate;
 
-public record ReservationAppRequest(LocalDate date, String name, Long timeId) {
+public record ReservationAppRequest(Long timeId, LocalDate date, String name) {
 }

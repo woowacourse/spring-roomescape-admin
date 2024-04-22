b8edbc659160ed9c867bd51cc8212b48b81ae9f9
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-04-23 00:38:52 +0900

    test: 컨트롤러 DB 로직 분리 확인

--- src/test/java/roomescape/JdbcConnectionTest.java
+++ src/test/java/roomescape/JdbcConnectionTest.java
@@ -3,6 +3,7 @@ package roomescape;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,13 +15,17 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.reservation.controller.ReservationController;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class JdbcConnectionTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
+
+    @Autowired
+    private ReservationController reservationController;
+
     @LocalServerPort
     int port;
 
@@ -39,5 +44,20 @@ public class JdbcConnectionTest {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @DisplayName("ReservationController에 있던 데이터베이스 관련 로직 분리를 검증한다.")
+    @Test
+    void injectionTest() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 }

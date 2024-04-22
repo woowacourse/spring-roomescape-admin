20567e10912f40489c3493c4049a9d081716dffe
Minjoo <m0522inju@gmail.com>
2024-04-22 19:59:03 +0900

    add: 예약 시간 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,14 @@ CREATE TABLE reservation
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    date DATE NOT NULL,
-    time TIME NOT NULL,
+    date DATE         NOT NULL,
+    time TIME         NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE reservation_time
+(
+    id       BIGINT NOT NULL AUTO_INCREMENT,
+    start_at TIME   NOT NULL,
     PRIMARY KEY (id)
 );

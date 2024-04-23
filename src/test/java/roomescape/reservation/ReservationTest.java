ccd790ea7913537f83c7076c5c278a43d2962ce4
leetaehoon <xogns1514@gmail.com>
2024-04-23 20:44:58 +0900

    test: test용 port 지정

--- src/test/java/roomescape/reservation/ReservationTest.java
+++ src/test/java/roomescape/reservation/ReservationTest.java
@@ -6,10 +6,12 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import roomescape.reservation.dto.request.ReservationRequest;
@@ -17,6 +19,14 @@ import roomescape.reservation.dto.request.ReservationRequest;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
 
     @Autowired
     private JdbcTemplate jdbcTemplate;

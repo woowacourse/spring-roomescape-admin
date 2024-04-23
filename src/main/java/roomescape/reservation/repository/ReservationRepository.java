67860c6253056c406020081d893473ee7fca0191
hoyeonyy <highoption@naver.com>
2024-04-23 11:27:28 +0900

    feat(ReservationRepository) : save, findAll 생성

--- src/main/java/roomescape/reservation/repository/ReservationRepository.java
+++ src/main/java/roomescape/reservation/repository/ReservationRepository.java
@@ -1,4 +1,50 @@
 package roomescape.reservation.repository;
 
+import java.sql.PreparedStatement;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.reservation.domain.Reservation;
+
+@Repository
 public class ReservationRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Long save(Reservation reservation) {
+        String sql = "insert into reservation (id, name, date, time) values (?,?,?,?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(
+                    sql, new String[]{"id"}
+            );
+            ps.setString(1, String.valueOf(reservation.getId()));
+            ps.setString(2, reservation.getName());
+            ps.setString(3, String.valueOf(reservation.getDate()));
+            ps.setString(4, String.valueOf(reservation.getTime()));
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
+
+    public List<Reservation> findAll() {
+        String sql = "select * from reservation";
+        return jdbcTemplate.query(sql,
+                (rs, rowNum) -> {
+                    return new Reservation(
+                            rs.getLong("id"),
+                            rs.getString("name"),
+                            rs.getDate("date").toLocalDate(),
+                            rs.getTime("time").toLocalTime()
+                    );
+                });
+    }
 }
---
+++ src/test/java/roomescape/repository/ReservationRepositoryTest.java
@@ -0,0 +1,59 @@
+package roomescape.repository;
+
+import io.restassured.RestAssured;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import roomescape.reservation.domain.Reservation;
+import roomescape.reservation.repository.ReservationRepository;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReservationRepositoryTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void beforeEach() {
+        RestAssured.port = port;
+    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @Test
+    @DisplayName("DB 저장 테스트")
+    void saveTest() {
+        Reservation reservation = new Reservation(1L, "hogi", LocalDate.now(), LocalTime.now());
+        Long saveId = reservationRepository.save(reservation);
+
+        Assertions.assertThat(saveId).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("DB 조회 테스트")
+    void findAllTest() {
+        Reservation reservation1 = new Reservation(1L, "hogi", LocalDate.now(), LocalTime.now());
+        Reservation reservation2 = new Reservation(2L, "kaki", LocalDate.now(), LocalTime.now());
+        reservationRepository.save(reservation1);
+        reservationRepository.save(reservation2);
+
+        List<Reservation> reservations = reservationRepository.findAll();
+        Assertions.assertThat(reservations.size()).isEqualTo(2);
+    }
+}

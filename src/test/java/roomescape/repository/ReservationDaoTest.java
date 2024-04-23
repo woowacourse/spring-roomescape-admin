00aa82a3f7ca43d61bae6bbca78e56543e5a1900
reddevilmidzy <midzy0228@gmail.com>
2024-04-23 10:53:22 +0900

    test: db 연결 확인 테스트 추가

--- src/test/java/roomescape/repository/ReservationDaoTest.java
+++ src/test/java/roomescape/repository/ReservationDaoTest.java
@@ -3,14 +3,18 @@ package roomescape.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import roomescape.model.Reservation;
 
@@ -20,6 +24,21 @@ class ReservationDaoTest {
 
     @Autowired
     ReservationDao reservationDao;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @DisplayName("db 연결 확인")
+    @Test
+    void checkConnection() {
+        try (final Connection connection = Objects.requireNonNull(jdbcTemplate.getDataSource()).getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE_TEST");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (final SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
     @DisplayName("예약 저장")
     @Test
@@ -31,7 +50,8 @@ class ReservationDaoTest {
 
         assertAll(
                 () -> assertThat(beforeSaving).isEmpty(),
-                () -> assertThat(afterSaving).hasSize(1));
+                () -> assertThat(afterSaving).hasSize(1)
+        );
     }
 
     @DisplayName("예약 삭제")

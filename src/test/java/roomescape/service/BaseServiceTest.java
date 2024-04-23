d8ade42387846d3e856a33f2bb38943344f6d6da
김민수 MINSOO KIM <alstn113@gmail.com>
2024-04-23 10:47:36 +0900

    refactor: 서비스 테스트에서 @Transactional 제거

--- src/test/java/roomescape/service/BaseServiceTest.java
+++ src/test/java/roomescape/service/BaseServiceTest.java
@@ -1,8 +1,6 @@
 package roomescape.service;
 
-import org.springframework.transaction.annotation.Transactional;
 import roomescape.BaseTest;
 
-@Transactional
 public abstract class BaseServiceTest extends BaseTest {
 }

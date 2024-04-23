4ba177abf00dc2d851134a27ea3b46bb98b79fd6
woojong <dwax1324@gmail.com>
2024-04-23 20:13:49 +0900

    refactor: Reservation 레코드에서 클래스로 변경

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -3,7 +3,41 @@ package roomescape.domain;
 import java.time.LocalDate;
 import java.util.Objects;
 
-public record Reservation(Long id, String name, LocalDate date, ReservationTime reservationTime) {
+public class Reservation {
+    private Long id;
+    private final String name;
+    private final LocalDate date;
+    private final ReservationTime reservationTime;
+
+    public Reservation(final String name, final LocalDate date, final ReservationTime reservationTime) {
+        this.name = name;
+        this.date = date;
+        this.reservationTime = reservationTime;
+    }
+
+    public Reservation(Long id, String name, LocalDate date, ReservationTime reservationTime) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.reservationTime = reservationTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public ReservationTime getReservationTime() {
+        return reservationTime;
+    }
+
     @Override
     public boolean equals(final Object o) {
         if (this == o) {

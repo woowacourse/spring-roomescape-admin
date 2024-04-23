b69655c0bcc2e7f908862f08714c1cfcb92b1174
libienz <leekh10195@gmail.com>
2024-04-23 20:06:50 +0900

    test: 예약 지속 시간 테스트 추가

--- src/test/java/roomescape/entity/ReservationTimeTest.java
+++ src/test/java/roomescape/entity/ReservationTimeTest.java
@@ -2,6 +2,7 @@ package roomescape.entity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static roomescape.entity.ReservationTime.RESERVATION_DURATION_HOUR;
 
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +14,15 @@ class ReservationTimeTest {
     void reservationTimeCreationTestWithNull() {
         assertThatThrownBy(() -> new ReservationTime(null))
                 .isInstanceOf(NullPointerException.class);
+    }
+
+    @DisplayName("예약이 끝나는 시간은 시작 시간으로부터 지정된 기간 뒤 까지이다")
+    @Test
+    void reservationDurationTest() {
+        LocalDateTime startTime = LocalDateTime.of(2024, 4, 20, 12, 30);
+        ReservationTime reservationTime = new ReservationTime(startTime);
+
+        assertThat(reservationTime.getEndDateTime()).isEqualTo(startTime.plusHours(RESERVATION_DURATION_HOUR));
     }
 
     @DisplayName("다른 예약시간과 비교, 예약 시간이 겹치는 경우를 알 수 있다")

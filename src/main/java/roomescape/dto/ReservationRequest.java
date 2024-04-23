90166484592b14953f0e505679cf7836df97d412
eun-byeol <joeunbyeol98@gmail.com>
2024-04-23 15:46:52 +0900

    refactor: 정적 팩토리 메서드 삭제

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -1,16 +1,5 @@
 package roomescape.dto;
 
-import roomescape.model.Reservation;
-import roomescape.model.ReservationTime;
-
 public record ReservationRequest(String name, String date, Long timeId) {
 
-    public static Reservation of(ReservationRequest reservationRequest, ReservationTime reservationTime) {
-        return new Reservation(
-            null,
-            reservationRequest.name,
-            reservationRequest.date,
-            reservationTime
-        );
-    }
 }
--- src/main/java/roomescape/dto/ReservationTimeRequest.java
+++ src/main/java/roomescape/dto/ReservationTimeRequest.java
@@ -1,10 +1,5 @@
 package roomescape.dto;
 
-import roomescape.model.ReservationTime;
-
 public record ReservationTimeRequest(String startAt) {
 
-    public static ReservationTime from(ReservationTimeRequest reservationTimeRequest) {
-        return new ReservationTime(null, reservationTimeRequest.startAt);
-    }
 }

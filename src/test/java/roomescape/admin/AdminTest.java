36cc7ef232bef5a1f70cc48c63fffcd8f863c988
leetaehoon <xogns1514@gmail.com>
2024-04-24 00:10:49 +0900

    test: test용 port 지정

--- src/test/java/roomescape/admin/AdminTest.java
+++ src/test/java/roomescape/admin/AdminTest.java
@@ -1,12 +1,22 @@
 package roomescape.admin;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class AdminTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
 
     @DisplayName("어드민 메인 페이지 테스트")
     @Test

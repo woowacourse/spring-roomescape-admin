c793602c250f2352ee8cf269157af4ac4a22d392
KIMGYUTAE <kiimwf0819@gmail.com>
2024-04-23 16:27:11 +0900

    refactor(ReservationController): 중복되는 URL 매핑

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -9,19 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 import roomescape.domain.repository.ReservationRepository;
 import roomescape.dto.ReservationDto;
 
 @RestController
+@RequestMapping("/reservations")
 public class ReservationController {
 
     @Autowired
     private ReservationRepository reservationRepository;
 
-    @GetMapping("/reservations")
-    public ResponseEntity<List<ReservationDto>> getReservations() {
+    @GetMapping
+    public ResponseEntity<List<ReservationDto>> getAllReservations() {
         List<Reservation> reservations = reservationRepository.findAll();
         List<ReservationDto> reservationResponse = reservations.stream()
                 .map(ReservationDto::toDto)
@@ -30,21 +32,21 @@ public class ReservationController {
         return ResponseEntity.ok(reservationResponse);
     }
 
-    @GetMapping("/reservations/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<ReservationDto> getReservation(@PathVariable Long id) {
         Reservation reservation = reservationRepository.findById(id);
         ReservationDto reservationResponse = ReservationDto.toDto(reservation);
         return ResponseEntity.ok(reservationResponse);
     }
 
-    @PostMapping("/reservations")
+    @PostMapping
     public ResponseEntity<Void> addReservation(@RequestBody ReservationDto reservationRequest) {
         Long savedId = reservationRepository.save(reservationRequest.toEntity());
         String redirectUrl = "/reservations/" + savedId;
         return ResponseEntity.status(HttpStatus.CREATED).header("Location", redirectUrl).build();
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
         reservationRepository.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();

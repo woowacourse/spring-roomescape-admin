e756781a62d38fb1ff66f6dee1c20f75a3c41ff1
kunsanglee <gosmdochee2@gmail.com>
2024-04-23 22:19:46 +0900

    refactor: 사용하지 않는 InMemoryReservationRepository 제거

--- src/main/java/roomescape/domain/H2ReservationRepository.java
+++ src/main/java/roomescape/domain/H2ReservationRepository.java
@@ -4,7 +4,6 @@ import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import org.springframework.context.annotation.Primary;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,7 +11,6 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-@Primary
 @Repository
 public class H2ReservationRepository implements ReservationRepository {
     private final JdbcTemplate jdbcTemplate;

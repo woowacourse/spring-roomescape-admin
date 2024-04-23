516f033d410b1f96eea75c8b2eb0372d295374af
Nam Gi Beom <rlqja0523@naver.com>
2024-04-23 11:31:05 +0900

    chore: 스키마 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,18 @@
 -- CREATE TABLE IF NOT EXISTS reservation
-CREATE TABLE reservation
-(
-    id                    BIGINT       NOT NULL AUTO_INCREMENT,
-    name                  VARCHAR(255) NOT NULL,
-    reservation_date_time DATETIME     NOT NULL,
-    PRIMARY KEY (id)
-);
 
 CREATE TABLE reservation_time
 (
     id       BIGINT NOT NULL AUTO_INCREMENT,
     start_at TIME   NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE reservation
+(
+    id      BIGINT       NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(255) NOT NULL,
+    date    DATE         NOT NULL,
+    time_id BIGINT       NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
 );

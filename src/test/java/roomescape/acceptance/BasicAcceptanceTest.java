69e864f7d91f60f9c3d508926dad37bb17ce7f7a
3juhwan <13selfesteem91@naver.com>
2024-04-23 20:54:01 +0900

    refactor: 인수 테스트에 다이나믹 테스트 적용

--- src/test/java/roomescape/acceptance/BasicAcceptanceTest.java
+++ src/test/java/roomescape/acceptance/BasicAcceptanceTest.java
@@ -4,8 +4,10 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public abstract class BasicAcceptanceTest {
     @LocalServerPort
     private int port;
--- src/test/java/roomescape/acceptance/ReservationTimeAcceptanceTest.java
+++ src/test/java/roomescape/acceptance/ReservationTimeAcceptanceTest.java
@@ -3,16 +3,36 @@ package roomescape.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 public class ReservationTimeAcceptanceTest extends BasicAcceptanceTest {
     @DisplayName("예약 시간을 저장했다가 삭제한다")
-    @Test
-    void saveReservationTime_and_delete() {
+    @TestFactory
+    Stream<DynamicTest> saveReservationTime_and_delete() {
+        return Stream.of(
+                dynamicTest("예약 시간 관리 페이지에 접속한다", this::accessReservationTimePage),
+                dynamicTest("예약 시간을 저장한다", this::addReservationTime),
+                dynamicTest("예약 시간을 모두 조회한다 (총 1개)", () -> checkReservationCount(1)),
+                dynamicTest("예약 시간을 삭제한다", () -> deleteReservationTime(1)),
+                dynamicTest("예약 시간을 모두 조회한다 (총 0개)", () -> checkReservationCount(0))
+        );
+    }
+
+    private void accessReservationTimePage() {
+        RestAssured.given().log().all()
+                .when().get("/admin/time")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    private void addReservationTime() {
         Map<String, String> params = Map.of("startAt", "10:00");
 
         RestAssured.given().log().all()
@@ -21,15 +41,19 @@ public class ReservationTimeAcceptanceTest extends BasicAcceptanceTest {
                 .when().post("/times")
                 .then().log().all()
                 .statusCode(200);
+    }
 
+    private void checkReservationCount(int expectedCount) {
         RestAssured.given().log().all()
                 .when().get("/times")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(1));
+                .body("size()", is(expectedCount));
+    }
 
+    private void deleteReservationTime(final int reservationId) {
         RestAssured.given().log().all()
-                .when().delete("/times/1")
+                .when().delete("/times/" + reservationId)
                 .then().log().all()
                 .statusCode(204);
     }

edb74ac8f204208dce5d512794d97f5e0d80af70
3juhwan <13selfesteem91@naver.com>
2024-04-23 21:14:45 +0900

    feat: 예약 시간 요청에 대한 예외 처리

--- src/main/java/roomescape/controller/request/ReservationTimeRequest.java
+++ src/main/java/roomescape/controller/request/ReservationTimeRequest.java
@@ -3,8 +3,13 @@ package roomescape.controller.request;
 import roomescape.domain.ReservationTime;
 
 import java.time.LocalTime;
+import java.util.Objects;
 
 public record ReservationTimeRequest(LocalTime startAt) {
+    public ReservationTimeRequest {
+        Objects.requireNonNull(startAt);
+    }
+
     public ReservationTime toEntity() {
         return new ReservationTime(startAt);
     }

002d3741c281d7b7de70fbd1b9d2bb4614d7685f
Nam Gi Beom <rlqja0523@naver.com>
2024-04-23 20:35:58 +0900

    fix: in memory db 대체 이슈 해결

--- src/test/java/roomescape/database/ConnectionTest.java
+++ src/test/java/roomescape/database/ConnectionTest.java
@@ -1,16 +1,19 @@
 package roomescape.database;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @JdbcTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 class ConnectionTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;

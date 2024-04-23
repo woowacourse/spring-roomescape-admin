bdde130d6af2b9c901f701deece7edd623943424
juha <khabh@naver.com>
2024-04-23 15:35:42 +0900

    refactor: ReservationTime 검증 추가

--- src/main/java/roomescape/model/ReservationTime.java
+++ src/main/java/roomescape/model/ReservationTime.java
@@ -8,12 +8,19 @@ public class ReservationTime {
     private final LocalTime startAt;
 
     public ReservationTime(final Long id, final String startAt) {
-        this(id, LocalTime.parse(startAt));
+        this(id, parseTime(startAt));
     }
 
     public ReservationTime(final Long id, final LocalTime startAt) {
         this.id = id;
         this.startAt = startAt;
+    }
+
+    private static LocalTime parseTime(final String startAt) {
+        if (startAt.isEmpty()) {
+            throw new IllegalArgumentException("시간이 입력되지 않았습니다.");
+        }
+        return LocalTime.parse(startAt);
     }
 
     public Long getId() {

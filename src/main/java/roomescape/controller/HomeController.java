24974c509de1bac082de8812da8a0fb57a2b74ac
fram1998 <koust6u@pusan.ac.kr>
2024-04-23 13:31:49 +0900

    feat: 예약 정보 저장 후 리다이렉션 엔드포인트 기능 추가

--- src/main/java/roomescape/controller/HomeController.java
+++ src/main/java/roomescape/controller/HomeController.java
@@ -2,12 +2,18 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
     @GetMapping
     public String getWelcomePage() {
         return "redirect:/admin";
+    }
+
+    @GetMapping("/reservations/{id}")
+    public String getReservationPage(@PathVariable final String id) {
+        return "redirect:/admin/reservation";
     }
 
 }

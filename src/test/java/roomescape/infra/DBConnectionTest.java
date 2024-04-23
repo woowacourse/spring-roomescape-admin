4e47c32bbc6aaa848ab31af0d879166a17eab8e6
robinjoon <robin980108@naver.com>
2024-04-23 10:53:00 +0900

    fix: 데이터베이스 연동 테스트 코드에서 필요 없는 설정 삭제

--- src/test/java/roomescape/infra/DBConnectionTest.java
+++ src/test/java/roomescape/infra/DBConnectionTest.java
@@ -9,16 +9,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class DBConnectionTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Test
-    @DisplayName("데이터베이스 접속이 잘 되는지 확.")
+    @DisplayName("데이터베이스 접속이 잘 되는지 확인.")
     void connectToDB() {
         try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
             assertConnection(connection);

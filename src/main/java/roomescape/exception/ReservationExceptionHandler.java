df3ff9394e82cf0e4dcc31ae577f06959a3d0b66
kelly6bf <kelly6bf@gmail.com>
2024-04-23 21:56:46 +0900

    refactor: 에러 응답 API 객체 생성

--- src/main/java/roomescape/exception/ReservationExceptionHandler.java
+++ src/main/java/roomescape/exception/ReservationExceptionHandler.java
@@ -4,11 +4,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
+
 @RestControllerAdvice
 public class ReservationExceptionHandler {
 
     @ExceptionHandler
-    protected ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 }

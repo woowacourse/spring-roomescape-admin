6d29aa9a489f468fc823af3377435e5bcf0f671e
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-04-23 17:04:01 +0900

    test: AdminController 테스트 구현

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-
     @GetMapping
     public String readAdminPage() {
         return "admin/index";
---
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -0,0 +1,34 @@
+package roomescape.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class AdminControllerTest {
+    @Test
+    @DisplayName("홈 화면을 요청하면 200 OK을 응답한다.")
+    void adminPageTest() {
+        RestAssured.given().log().all()
+                .when().get("/admin")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("관리자 예약 화면을 요청하면 200 OK를 응답한다.")
+    void adminReservationPageTest() {
+        RestAssured.given().log().all()
+                .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+}

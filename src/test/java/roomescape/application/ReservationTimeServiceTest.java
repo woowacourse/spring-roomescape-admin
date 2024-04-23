3aa940382a81b5a867780c5a452833e31136104c
Nam Gi Beom <rlqja0523@naver.com>
2024-04-23 22:26:14 +0900

    test: 예약시간 서비스 테스트를 단위 테스트로 리팩토링

--- src/test/java/roomescape/application/ReservationTimeServiceTest.java
+++ src/test/java/roomescape/application/ReservationTimeServiceTest.java
@@ -2,22 +2,34 @@ package roomescape.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.time.LocalTime;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import roomescape.domain.time.ReservationTime;
 import roomescape.domain.time.repository.ReservationTimeRepository;
 import roomescape.dto.reservationtime.ReservationTimeRequest;
-import roomescape.support.FakeReservationTimeRepository;
 
+@ExtendWith(MockitoExtension.class)
 class ReservationTimeServiceTest {
-    private final ReservationTimeRepository reservationTimeRepository = new FakeReservationTimeRepository();
-    private final ReservationTimeService reservationTimeService = new ReservationTimeService(reservationTimeRepository);
+    @InjectMocks
+    private ReservationTimeService reservationTimeService;
+    @Mock
+    private ReservationTimeRepository reservationTimeRepository;
 
     @Test
     void 예약_시간을_성공적으로_등록한다() {
         LocalTime startAt = LocalTime.of(13, 0);
+        when(reservationTimeRepository.existsByStartAt(any())).thenReturn(false);
+        when(reservationTimeRepository.save(any())).thenReturn(new ReservationTime(1L, startAt));
         ReservationTimeRequest request = new ReservationTimeRequest(startAt);
 
         ReservationTime reservationTime = reservationTimeService.register(request);
@@ -27,13 +39,11 @@ class ReservationTimeServiceTest {
 
     @Test
     void 중복된_예약_시간이_있으면_등록을_실패한다() {
+        when(reservationTimeRepository.existsByStartAt(any())).thenReturn(true);
         LocalTime startAt = LocalTime.of(13, 0);
         ReservationTimeRequest request = new ReservationTimeRequest(startAt);
-        reservationTimeService.register(request);
 
-        ReservationTimeRequest duplicatedRequest = new ReservationTimeRequest(startAt);
-
-        assertThatThrownBy(() -> reservationTimeService.register(duplicatedRequest))
+        assertThatThrownBy(() -> reservationTimeService.register(request))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("이미 존재하는 예약 시간입니다.");
     }
@@ -41,19 +51,19 @@ class ReservationTimeServiceTest {
     @Test
     void 예약_시간을_삭제한다() {
         LocalTime startAt = LocalTime.of(13, 0);
-        ReservationTimeRequest request = new ReservationTimeRequest(startAt);
-        ReservationTime reservationTime = reservationTimeService.register(request);
+        ReservationTime reservationTime = new ReservationTime(1L, startAt);
+        when(reservationTimeRepository.findById(anyLong())).thenReturn(Optional.of(reservationTime));
 
         reservationTimeService.delete(reservationTime.getId());
 
-        Optional<ReservationTime> optionalReservationTime = reservationTimeRepository.findById(reservationTime.getId());
-        assertThat(optionalReservationTime).isEmpty();
+        verify(reservationTimeRepository).deleteById(reservationTime.getId());
     }
 
     @Test
     void 존재하지_않는_예약_시간을_삭제하면_예외가_발생한다() {
-        long nonExistentId = 0L;
-        assertThatThrownBy(() -> reservationTimeService.delete(nonExistentId))
+        when(reservationTimeRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> reservationTimeService.delete(1L))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("존재하지 않는 예약 시간입니다.");
     }

3c47d5259490ebee11d34ef15391c39448e6d365
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-04-23 13:52:06 +0900

    feat: time controller 계층 구현

---
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -0,0 +1,38 @@
+package roomescape.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import roomescape.domain.reservation.ReservationTime;
+import roomescape.dto.ReservationTimeRequestDto;
+import roomescape.dto.ReservationTimeResponseDto;
+import roomescape.service.ReservationTimeService;
+
+import java.util.Set;
+
+@RestController
+public class ReservationTimeController {
+    private final ReservationTimeService reservationTimeService;
+
+    public ReservationTimeController(ReservationTimeService reservationTimeService) {
+        this.reservationTimeService = reservationTimeService;
+    }
+
+    @PostMapping("/times")
+    public ResponseEntity<ReservationTimeResponseDto> addTime(@RequestBody ReservationTimeRequestDto timeRequestDto) {
+        ReservationTime time  = reservationTimeService.addTime(timeRequestDto.toEntity());
+        return ResponseEntity.ok(new ReservationTimeResponseDto(time));
+    }
+
+    @GetMapping("/times")
+    public ResponseEntity<Set<ReservationTimeResponseDto>> times() {
+        Set<ReservationTimeResponseDto> reservationTimes = ReservationTimeResponseDto.listOf(reservationTimeService.allReservationTimes());
+        return ResponseEntity.ok(reservationTimes);
+    }
+
+    @DeleteMapping("/times/{id}")
+    public ResponseEntity<Void> deleteTime(@PathVariable Long id) {
+        reservationTimeService.delete(id);
+        return ResponseEntity.ok().build();
+
+    }
+}

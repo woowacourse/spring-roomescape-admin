0c8d3aa1a6a8e0156a2cfaad24d701dbe5452b0b
woojong <dwax1324@gmail.com>
2024-04-23 20:14:13 +0900

    refactor: public 키워드 삭제

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -26,7 +26,7 @@ import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+class MissionStepTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;

ce819ea6865d9297ef0a8038edc73d6764848e07
3juhwan <13selfesteem91@naver.com>
2024-04-23 13:44:40 +0900

    refactor: reservation id 데이터 타입 변경

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -4,20 +4,20 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Reservation {
-    private final long id;
+    private final Long id;
     private final Name name;
     private final LocalDate date;
     private final ReservationTime time;
 
     public Reservation(String name, LocalDate date, LocalTime time) {
-        this(0L, new Name(name), date, new ReservationTime(time));
+        this(null, new Name(name), date, new ReservationTime(time));
     }
 
     public Reservation(String name, LocalDate date, ReservationTime time) {
-        this(0L, new Name(name), date, time);
+        this(null, new Name(name), date, time);
     }
 
-    public Reservation(long id, Name name, LocalDate date, ReservationTime time) {
+    public Reservation(Long id, Name name, LocalDate date, ReservationTime time) {
         this.id = id;
         this.name = name;
         this.date = date;

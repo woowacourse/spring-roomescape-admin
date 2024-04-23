06d0e36335c0da44e068eb2a90688348b7c959a9
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-04-23 17:18:32 +0900

    test: ReservationTimeController 테스트 구현

---
+++ src/test/java/roomescape/controller/ReservationTimeControllerTest.java
@@ -0,0 +1,59 @@
+package roomescape.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class ReservationTimeControllerTest {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    @DisplayName("시간 생성을 요청하면 200 OK를 응답한다.")
+    void postReservationTest() {
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("시간 읽기를 요청하면 200 OK를 응답한다.")
+    void getReservationTest() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .when().get("/times")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("시간 삭제를 요청하면 200 OK를 응답한다.")
+    void deleteReservationTest() {
+        String timeSql = "insert into reservation_time (start_at) values (?)";
+        jdbcTemplate.update(timeSql, "11:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .when().delete("/times/1")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+}

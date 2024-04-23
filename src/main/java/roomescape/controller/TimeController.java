0d404e613185909e563da9a7ecdca5ebbbe0c867
yoonseo choi <choiyoonseo02@gmail.com>
2024-04-23 15:51:09 +0900

    feat(TimeController): time delete

--- src/main/java/roomescape/controller/TimeController.java
+++ src/main/java/roomescape/controller/TimeController.java
@@ -1,12 +1,16 @@
 package roomescape.controller;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import roomescape.dao.TimeDAO;
 import roomescape.dto.TimeCreateRequestDto;
 import roomescape.model.Time;
@@ -29,5 +33,15 @@ public class TimeController {
     public ResponseEntity<Time> create(@RequestBody TimeCreateRequestDto timeCreateRequestDto) {
         Time time = timeDAO.insert(timeCreateRequestDto);
         return ResponseEntity.ok(time);
+    }
+
+    @DeleteMapping("/times/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        try {
+            timeDAO.delete(id);
+        } catch (Exception e) {
+            throw new ResponseStatusException(NOT_FOUND, "Unable to find resource");
+        }
+        return ResponseEntity.ok().build();
     }
 }

5724067af611060bc71ef3cc6e02b950e0937435
kelly6bf <kelly6bf@gmail.com>
2024-04-23 22:50:40 +0900

    test: 예약 & 예약 시간 삭제 예외 테스트 추가

--- src/test/java/roomescape/controller/ReservationApiControllerTest.java
+++ src/test/java/roomescape/controller/ReservationApiControllerTest.java
@@ -89,6 +89,16 @@ class ReservationApiControllerTest {
         assertThat(reservations.size()).isEqualTo(0);
     }
 
+    @DisplayName("존재하지 않는 예약 정보를 삭제하려고 하면 400코드가 응답된다.")
+    @Test
+    void deleteNoExistReservationTest() {
+        RestAssured.given().log().all()
+                .when().delete("/reservations/5")
+                .then().log().all()
+                .statusCode(400)
+                .body("message", is("해당 id의 예약이 존재하지 않습니다."));
+    }
+
     @DisplayName("전체 예약 시간 정보를 조회한다.")
     @Test
     void getReservationTimesTest() {
@@ -112,48 +122,6 @@ class ReservationApiControllerTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("id", is(2));
-    }
-
-    @DisplayName("예약 시간 정보를 삭제한다.")
-    @Test
-    void deleteReservationTimeTest() {
-        // 예약 정보 삭제
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
-                .then().log().all()
-                .statusCode(200);
-
-        // 예약 시간 정보 삭제
-        RestAssured.given().log().all()
-                .when().delete("/times/1")
-                .then().log().all()
-                .statusCode(200);
-
-        // 예약 시간 정보 조회
-        List<ReservationResponse> reservations = RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200).extract()
-                .jsonPath().getList(".", ReservationResponse.class);
-
-        assertThat(reservations.size()).isEqualTo(0);
-    }
-
-    @DisplayName("유효하지 않은 사용자 이름을 포함한 예약 저장 요청을 하면 400코드가 응답된다.")
-    @Test
-    void saveReservationWithInvalidName() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운운운운운운운운우눙누우웅ㅇ");
-        params.put("date", "2023-08-05");
-        params.put("timeId", "1");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(400)
-                .body("message", is("예약자 이름은 1글자 이상 5글자 이하여야 합니다."));
     }
 
     @DisplayName("존재하지 않는 예약 시간을 포함한 예약 저장 요청을 하면 400코드가 응답된다.")
@@ -188,5 +156,57 @@ class ReservationApiControllerTest {
                 .then().log().all()
                 .statusCode(400)
                 .body("message", is("현재 날짜보다 이전 날짜를 예약할 수 없습니다."));
+    }
+
+    @DisplayName("예약 시간 정보를 삭제한다.")
+    @Test
+    void deleteReservationTimeTest() {
+        // 예약 정보 삭제
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
+
+        // 예약 시간 정보 삭제
+        RestAssured.given().log().all()
+                .when().delete("/times/1")
+                .then().log().all()
+                .statusCode(200);
+
+        // 예약 시간 정보 조회
+        List<ReservationResponse> reservations = RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200).extract()
+                .jsonPath().getList(".", ReservationResponse.class);
+
+        assertThat(reservations.size()).isEqualTo(0);
+    }
+
+    @DisplayName("존재하지 않는 예약 시간 정보를 삭제하려고 하면 400코드가 응답된다.")
+    @Test
+    void deleteNoExistReservationTimeTest() {
+        RestAssured.given().log().all()
+                .when().delete("/times/5")
+                .then().log().all()
+                .statusCode(400)
+                .body("message", is("해당 id의 예약 시간이 존재하지 않습니다."));
+    }
+
+    @DisplayName("유효하지 않은 사용자 이름을 포함한 예약 저장 요청을 하면 400코드가 응답된다.")
+    @Test
+    void saveReservationWithInvalidName() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운운운운운운운운우눙누우웅ㅇ");
+        params.put("date", "2023-08-05");
+        params.put("timeId", "1");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400)
+                .body("message", is("예약자 이름은 1글자 이상 5글자 이하여야 합니다."));
     }
 }

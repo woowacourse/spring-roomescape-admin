cb6dd8da5ee3b3d5197360532f525001106f3114
SCY <helenason@naver.com>
2024-04-23 15:17:45 +0900

    feat: 엔티티의 id 범위 검증

--- src/main/java/roomescape/entity/Reservation.java
+++ src/main/java/roomescape/entity/Reservation.java
@@ -10,10 +10,17 @@ public class Reservation {
     private final ReservationTime time;
 
     public Reservation(long id, String name, LocalDate date, ReservationTime time) {
+        validateIdRange(id);
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    private void validateIdRange(long id) {
+        if (id <= 0) {
+            throw new RuntimeException("[ERROR] 데이터베이스 오류입니다. 관리자에게 문의해주세요.");
+        }
     }
 
     public long getId() {
--- src/main/java/roomescape/entity/ReservationTime.java
+++ src/main/java/roomescape/entity/ReservationTime.java
@@ -8,8 +8,15 @@ public class ReservationTime {
     private final LocalTime startAt;
 
     public ReservationTime(long id, LocalTime startAt) {
+        validateIdRange(id);
         this.id = id;
         this.startAt = startAt;
+    }
+
+    private void validateIdRange(long id) {
+        if (id <= 0) {
+            throw new RuntimeException("[ERROR] 데이터베이스 오류입니다. 관리자에게 문의해주세요.");
+        }
     }
 
     public long getId() {

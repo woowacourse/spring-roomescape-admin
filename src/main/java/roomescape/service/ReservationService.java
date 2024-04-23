6fcd39445c3c91f6a3b2b3a0e8902fa8ba0cd5a4
HyungHoKim00 <rlagudgh1109@gmail.com>
2024-04-23 11:23:21 +0900

    refactor: 불필요한 인터페이스 제거

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,13 +1,44 @@
 package roomescape.service;
 
 import java.util.List;
+import org.springframework.stereotype.Service;
+import roomescape.dao.ReservationDao;
+import roomescape.dao.ReservationTimeDao;
+import roomescape.domain.Reservation;
+import roomescape.domain.ReservationTime;
 import roomescape.dto.ReservationRequest;
 import roomescape.dto.ReservationResponse;
+import roomescape.idgenerator.AutoIncrementIdGenerator;
+import roomescape.idgenerator.IdGenerator;
 
-public interface ReservationService {
-    List<ReservationResponse> findAll();
+@Service
+public class ReservationService {
+    private final ReservationDao reservationDao;
+    private final ReservationTimeDao reservationTimeDao;
+    private final IdGenerator idGenerator = new AutoIncrementIdGenerator();
 
-    ReservationResponse save(ReservationRequest reservationRequest);
+    public ReservationService(ReservationDao reservationDao,
+                              ReservationTimeDao reservationTimeDao) {
+        this.reservationDao = reservationDao;
+        this.reservationTimeDao = reservationTimeDao;
+    }
 
-    boolean deleteById(long id);
+    public List<ReservationResponse> findAll() {
+        List<Reservation> reservations = reservationDao.findAll();
+        return reservations.stream()
+                .map(ReservationResponse::new)
+                .toList();
+    }
+
+    public ReservationResponse save(ReservationRequest reservationRequest) {
+        long timeId = reservationRequest.timeId();
+        ReservationTime time = reservationTimeDao.findById(timeId);
+        Reservation reservation = reservationRequest.toDomain(idGenerator.generateNewId(), time);
+        reservationDao.save(reservation);
+        return new ReservationResponse(reservation);
+    }
+
+    public boolean deleteById(long id) {
+        return reservationDao.deleteById(id);
+    }
 }
--- src/main/java/roomescape/service/ReservationTimeService.java
+++ src/main/java/roomescape/service/ReservationTimeService.java
@@ -1,13 +1,38 @@
 package roomescape.service;
 
 import java.util.List;
+import org.springframework.stereotype.Service;
+import roomescape.dao.ReservationTimeDao;
+import roomescape.domain.ReservationTime;
 import roomescape.dto.ReservationTimeRequest;
 import roomescape.dto.ReservationTimeResponse;
+import roomescape.idgenerator.AutoIncrementIdGenerator;
+import roomescape.idgenerator.IdGenerator;
 
-public interface ReservationTimeService {
-    List<ReservationTimeResponse> findAll();
+@Service
+public class ReservationTimeService {
+    private final ReservationTimeDao reservationTimeDao;
+    private final IdGenerator idGenerator = new AutoIncrementIdGenerator();
 
-    ReservationTimeResponse save(ReservationTimeRequest reservationTimeRequest);
+    public ReservationTimeService(ReservationTimeDao reservationTimeDao) {
+        this.reservationTimeDao = reservationTimeDao;
+    }
 
-    boolean deleteById(long id);
+    public List<ReservationTimeResponse> findAll() {
+        return reservationTimeDao.findAll()
+                .stream()
+                .map(ReservationTimeResponse::new)
+                .toList();
+    }
+
+    public ReservationTimeResponse save(ReservationTimeRequest reservationTimeRequest) {
+        ReservationTime reservationTime = reservationTimeRequest.toDomain(
+                idGenerator.generateNewId());
+        reservationTimeDao.save(reservationTime);
+        return new ReservationTimeResponse(reservationTime);
+    }
+
+    public boolean deleteById(long id) {
+        return reservationTimeDao.deleteById(id);
+    }
 }

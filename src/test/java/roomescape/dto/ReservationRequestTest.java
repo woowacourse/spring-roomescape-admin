b3777f316e26102f103dff00fcb45765766e59a0
tackyu <tack970913@gmail.com>
2024-04-23 14:58:32 +0900

    style: code formatting

--- src/test/java/roomescape/dto/ReservationRequestTest.java
+++ src/test/java/roomescape/dto/ReservationRequestTest.java
@@ -7,16 +7,15 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationRequestTest {
     @Test
-    void convert_Request_to_Reservation(){
-        ReservationRequest reservationRequest=new ReservationRequest("abc", LocalDate.of(2024,4,23)
-                , LocalTime.of(13,45));
+    void convert_Request_to_Reservation() {
+        ReservationRequest reservationRequest = new ReservationRequest("abc", LocalDate.of(2024, 4, 23)
+                , LocalTime.of(13, 45));
         Reservation reservation = reservationRequest.toReservation(1L);
-        Reservation target = new Reservation(1L,"abc", LocalDate.of(2024,4,23)
-                ,LocalTime.of(13,45));
+        Reservation target = new Reservation(1L, "abc", LocalDate.of(2024, 4, 23)
+                , LocalTime.of(13, 45));
 
         assertThat(reservation).usingRecursiveComparison().isEqualTo(target);
     }
--- src/test/java/roomescape/dto/ReservationResponseTest.java
+++ src/test/java/roomescape/dto/ReservationResponseTest.java
@@ -7,16 +7,15 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationResponseTest {
     @Test
-    void convert_Reservation_to_Response(){
-        Reservation reservation = new Reservation(1L,"abc", LocalDate.of(2024,4,23)
-                , LocalTime.of(13,45));
+    void convert_Reservation_to_Response() {
+        Reservation reservation = new Reservation(1L, "abc", LocalDate.of(2024, 4, 23)
+                , LocalTime.of(13, 45));
         ReservationResponse response = ReservationResponse.toDto(reservation);
-        ReservationResponse target = new ReservationResponse(1L,"abc", LocalDate.of(2024,4,23)
-                , LocalTime.of(13,45));
+        ReservationResponse target = new ReservationResponse(1L, "abc", LocalDate.of(2024, 4, 23)
+                , LocalTime.of(13, 45));
 
         assertThat(response).usingRecursiveComparison().isEqualTo(target);
     }

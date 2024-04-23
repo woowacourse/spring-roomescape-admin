ddf64f5798ea40348aa7a6f3801ae460067f428c
KIMGYUTAE <kiimwf0819@gmail.com>
2024-04-23 17:05:34 +0900

    feat(ReservationTime): 예약 시간 도메인 추가

---
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -0,0 +1,29 @@
+package roomescape.domain;
+
+import java.time.LocalTime;
+
+public class ReservationTime {
+
+    private Long id;
+    private LocalTime startAt;
+
+    public ReservationTime() {
+    }
+
+    public ReservationTime(Long id, LocalTime startAt) {
+        this.id = id;
+        this.startAt = startAt;
+    }
+
+    public ReservationTime(LocalTime startAt) {
+        this(null, startAt);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalTime getStartAt() {
+        return startAt;
+    }
+}

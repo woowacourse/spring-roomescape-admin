ef04e1004e2dd1cf2e20a0765a2eac08065c44d1
nayonsoso <panda0329@naver.com>
2024-04-23 15:20:53 +0900

    feat: 예약 시간 관리 데이터베이스 추가

--- src/test/java/roomescape/DataBaseConnectionTest.java
+++ src/test/java/roomescape/DataBaseConnectionTest.java
@@ -1,6 +1,7 @@
 package roomescape;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,13 +20,15 @@ class DataBaseConnectionTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @DisplayName("데이터베이스 연결")
     @Test
     void dataBaseConnection() {
         try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
             Assertions.assertAll(
                     () -> assertThat(connection).isNotNull(),
                     () -> assertThat(connection.getCatalog()).isEqualTo("DATABASE"),
-                    () -> assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue()
+                    () -> assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue(),
+                    () -> assertThat(connection.getMetaData().getTables(null, null, "RESERVATION_TIME", null).next()).isTrue()
             );
         } catch (SQLException e) {
             throw new RuntimeException(e);

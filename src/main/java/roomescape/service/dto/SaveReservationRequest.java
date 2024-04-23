c1b3b0ca67ecaa11fbeef6510314a9b1ceb874b5
KimSangHyeok <tkdgur0906@naver.com>
2024-04-23 20:09:41 +0900

    refactor: dto 불필요 필드 제거

--- src/main/java/roomescape/service/dto/SaveReservationRequest.java
+++ src/main/java/roomescape/service/dto/SaveReservationRequest.java
@@ -3,7 +3,7 @@ package roomescape.service.dto;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 
-public record SaveReservationRequest(String name, String date, Long timeId, String startAt) {
+public record SaveReservationRequest(String name, String date, Long timeId) {
 
     public static Reservation toEntity(SaveReservationRequest request, ReservationTime reservationTime) {
         return new Reservation(request.name(), request.date(), reservationTime);

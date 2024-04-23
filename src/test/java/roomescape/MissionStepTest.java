217a0b1176fb99e13a6adb6ada4565bc63df8eff
ashsty <qkralsdk991@naver.com>
2024-04-23 13:23:49 +0900

    feat: h2 데이터베이스 연결

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -3,21 +3,28 @@ package roomescape;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    @DisplayName("admin 페이지 URL 요청이 올바르게 연결된다.")
     @Test
     void 일단계() {
         RestAssured.given().log().all()
@@ -26,6 +33,7 @@ public class MissionStepTest {
                 .statusCode(200);
     }
 
+    @DisplayName("reservation 페이지 URL 요청과 조회 요청이 올바르게 연결된다.")
     @Test
     void 이단계() {
         RestAssured.given().log().all()
@@ -40,6 +48,7 @@ public class MissionStepTest {
                 .body("size()", is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
     }
 
+    @DisplayName("reservation 페이지에 새로운 예약 정보를 추가하거나 삭제할 수 있다.")
     @Test
     void 삼단계() {
         Map<String, String> params = new HashMap<>();
@@ -71,5 +80,17 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(0));
+    }
+
+    @DisplayName("h2 데이터베이스가 연결되었는지 확인한다.")
+    @Test
+    void 사단계() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

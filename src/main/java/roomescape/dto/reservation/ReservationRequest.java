53db1fd01bcb38f4267207104fddf9aa82692e0c
Nam Gi Beom <rlqja0523@naver.com>
2024-04-24 00:15:00 +0900

    feat: request null 검증

--- src/main/java/roomescape/dto/reservation/ReservationRequest.java
+++ src/main/java/roomescape/dto/reservation/ReservationRequest.java
@@ -1,6 +1,12 @@
 package roomescape.dto.reservation;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
-public record ReservationRequest(String name, LocalDate date, long timeId) { // timeid Long -> null 체크
+public record ReservationRequest(String name, LocalDate date, Long timeId) {
+    public ReservationRequest {
+        Objects.requireNonNull(name, "예약자명은 필수입니다.");
+        Objects.requireNonNull(date, "예약날짜는 필수입니다.");
+        Objects.requireNonNull(timeId, "예약시간은 필수입니다.");
+    }
 }
--- src/main/java/roomescape/dto/reservationtime/ReservationTimeRequest.java
+++ src/main/java/roomescape/dto/reservationtime/ReservationTimeRequest.java
@@ -1,6 +1,10 @@
 package roomescape.dto.reservationtime;
 
 import java.time.LocalTime;
+import java.util.Objects;
 
 public record ReservationTimeRequest(LocalTime startAt) {
+    public ReservationTimeRequest {
+        Objects.requireNonNull(startAt, "시작 시간은 필수입니다.");
+    }
 }

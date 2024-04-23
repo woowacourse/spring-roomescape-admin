cfee6ad44d24854fa597c0a34d70cd8421eba887
yoonseo choi <choiyoonseo02@gmail.com>
2024-04-23 15:49:25 +0900

    feat(TimeDAO): time insert

--- src/main/java/roomescape/dao/TimeDAO.java
+++ src/main/java/roomescape/dao/TimeDAO.java
@@ -1,10 +1,14 @@
 package roomescape.dao;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import roomescape.dto.TimeCreateRequestDto;
 import roomescape.model.Time;
 
 @Repository
@@ -24,5 +28,17 @@ public class TimeDAO {
                         resultSet.getLong("id"),
                         resultSet.getString("start_at")
                 ));
+    }
+
+    public Time insert(TimeCreateRequestDto timeCreateRequestDto) {
+        String sql = "INSERT INTO reservation_time (start_at) VALUES (?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    sql, new String[]{"id"});
+            ps.setString(1, timeCreateRequestDto.startAt());
+            return ps;
+        }, keyHolder);
+        return new Time(keyHolder.getKey().longValue(), timeCreateRequestDto.startAt());
     }
 }

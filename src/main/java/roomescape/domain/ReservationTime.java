b905097bc08ae75dbc22435ecfb648c1f2e9ebcc
takoyakimchi <c4nyou@gmail.com>
2024-04-23 20:11:27 +0900

    refactor: 시간 포맷 HH:mm으로 변경

--- src/main/java/roomescape/domain/ReservationTime.java
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -1,10 +1,13 @@
 package roomescape.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 
 public class ReservationTime {
 
     private final Long id;
+
+    @JsonFormat(pattern = "HH:mm")
     private final LocalTime startAt;
 
     public ReservationTime(Long id, LocalTime startAt) {

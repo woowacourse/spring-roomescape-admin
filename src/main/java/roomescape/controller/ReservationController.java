a740aa6ff0701eba87040b2d2260debf0483199c
KIMGYUTAE <kiimwf0819@gmail.com>
2024-04-23 10:48:15 +0900

    refactor: controller 패키지 추가 및 클래스 이동

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,4 +1,4 @@
-package roomescape;
+package roomescape.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.Reservation;
+import roomescape.ReservationDto;
+import roomescape.ReservationRepository;
 
 @RestController
 public class ReservationController {

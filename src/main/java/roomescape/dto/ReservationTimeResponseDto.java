199436570f55cb8b9ea51ee4bc4dbd8d9050e6c1
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-04-23 17:59:32 +0900

    feat: time table 반영

--- src/main/java/roomescape/dto/ReservationTimeResponseDto.java
+++ src/main/java/roomescape/dto/ReservationTimeResponseDto.java
@@ -4,17 +4,15 @@ import roomescape.domain.reservation.ReservationTime;
 
 import java.time.LocalTime;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public record ReservationTimeResponseDto(Long id, LocalTime time) {
     public ReservationTimeResponseDto(ReservationTime time){
         this(time.getId(), time.getTime());
     }
 
-    public static Set<ReservationTimeResponseDto> listOf(List<ReservationTime> reservationTimes){
+    public static List<ReservationTimeResponseDto> listOf(List<ReservationTime> reservationTimes){
         return reservationTimes.stream()
                 .map(ReservationTimeResponseDto::new)
-                .collect(Collectors.toSet());
+                .toList();
     }
 }

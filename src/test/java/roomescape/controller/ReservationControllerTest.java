c78741f1a7aacffd1e0cbd5b6800a2c7774ac714
KimSangHyeok <tkdgur0906@naver.com>
2024-04-23 17:06:13 +0900

    test: 계층화 리팩터링 테스트 추가

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import roomescape.domain.Reservation;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
@@ -192,5 +193,23 @@ public class ReservationControllerTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
+    }
+
+    @Autowired
+    private ReservationController reservationController;
+
+    @Test
+    @DisplayName("데이터베이스 관련 로직을 컨트롤러에서 분리하였다.")
+    void layerRefactoring() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 }

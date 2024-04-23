c5c6dafd62ead669ee4ab03dec6e8e150d6fcbc4
jinwoo22 <jenny010925@naver.com>
2024-04-23 13:52:02 +0900

    refactor: @RequestMapping 삭제

--- src/main/java/roomescape/controller/ReservationPageController.java
+++ src/main/java/roomescape/controller/ReservationPageController.java
@@ -2,18 +2,16 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
 public class ReservationPageController {
 
-    @GetMapping
+    @GetMapping("/admin")
     public String getAdminPage() {
         return "/admin/index";
     }
 
-    @GetMapping("/reservation")
+    @GetMapping("/admin/reservation")
     public String getReservationPage() {
         return "/admin/reservation-legacy";
     }

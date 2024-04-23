c7c31dbc36da64fd0782a8dfee96a8d57266d2a5
hjk0761 <hjk0761@naver.com>
2024-04-23 14:35:54 +0900

    test: 컨트롤러 테스트가 jdbcTemplate 에 의존하는 상황 수정
    테스트를 위한 더미 데이터를 data.sql을 통해 넣어주고, 컨트롤러 테스트에서 jdbcTemplate 의존 없이 응답만을 테스트 하도록 수정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO reservation_time(start_at) VALUES '01:00';
+
+INSERT INTO reservation(name, date, time_id) VALUES ('브라운', '2024-01-01', '1')
--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -1,33 +1,22 @@
 package roomescape.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
-import roomescape.domain.Reservation;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ReservationControllerTest {
-
-    @Autowired
-    private final JdbcTemplate jdbcTemplate;
-
-    public @Autowired ReservationControllerTest(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
 
     @LocalServerPort
     int port;
@@ -35,7 +24,6 @@ public class ReservationControllerTest {
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
-        jdbcTemplate.update("INSERT INTO reservation_time(id, start_at) VALUES (?, ?)", 1, "10:00");
     }
 
     @Test
@@ -49,34 +37,28 @@ public class ReservationControllerTest {
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
+                .body("size()", is(1));
     }
 
     @Test
     void reservationsTest() {
-        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)", "브라운", "2023-08-05", 1);
-
-        List<Reservation> reservations = RestAssured.given().log().all()
+        RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
-                .statusCode(200).extract()
-                .jsonPath().getList(".", Reservation.class);
-
-        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
-
-        assertThat(reservations.size()).isEqualTo(count);
+                .statusCode(200);
     }
 
     @Test
-    void insertTest() {
+    void insertTest() throws JsonProcessingException {
         Map<String, Object> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
         params.put("timeId", 1);
 
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestJson = objectMapper.writeValueAsString(params);
+
+        RestAssured.given().contentType("application/json").body(requestJson).log().all()
                 .when().post("/reservations")
                 .then().log().all()
                 .statusCode(200);
@@ -84,27 +66,9 @@ public class ReservationControllerTest {
 
     @Test
     void deleteByIdTest() {
-        Map<String, Object> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("timeId", 1);
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(200);
-
-        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
-        assertThat(count).isEqualTo(1);
-
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
                 .statusCode(204);
-
-        Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
-        assertThat(countAfterDelete).isEqualTo(0);
     }
 }

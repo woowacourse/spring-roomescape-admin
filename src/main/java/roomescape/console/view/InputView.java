d894c30d821992710f2a8720da75fb5490fd1005
Arachneee <jhg2819@naver.com>
2024-04-24 00:12:55 +0900

    refactor: 뷰 형식 변경

--- src/main/java/roomescape/console/view/InputView.java
+++ src/main/java/roomescape/console/view/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
 
     public ConsoleCommand readCommand() {
         System.out.println("""
-                원하는 기능을 형식에 맞게 입력하세요.
+                \n ****** 원하는 기능을 형식에 맞게 입력하세요. ******
                 1. 예약 조회 - ex) GET/reservations
                 2. 예약 추가 - ex) POST/reservations {name},{date},{timeId}
                 3. 예약 삭제 ID - ex) DELETE/reservation {id}

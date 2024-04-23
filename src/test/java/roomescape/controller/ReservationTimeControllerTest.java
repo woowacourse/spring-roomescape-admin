ed28f7d00b0e5b2da20506d16b6aa3a9845fc225
eun-byeol <joeunbyeol98@gmail.com>
2024-04-24 00:56:23 +0900

    test: 데이터베이스 관련 로직 분리 테스트 추가

--- src/test/java/roomescape/controller/ReservationTimeControllerTest.java
+++ src/test/java/roomescape/controller/ReservationTimeControllerTest.java
@@ -1,23 +1,30 @@
 package roomescape.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationTimeControllerTest {
 
+    @Autowired
+    private ReservationTimeController reservationTimeController;
+
     @DisplayName("시간 추가, 조회, 삭제")
     @Test
-    void insertAndSearchAndRemoveTime() {
+    void postAndGetAndDeleteTime() {
         Map<String, String> params = new HashMap<>();
         params.put("startAt", "10:00");
 
@@ -38,5 +45,18 @@ class ReservationTimeControllerTest {
             .when().delete("/times/1")
             .then().log().all()
             .statusCode(204);
+    }
+
+    @DisplayName("데이터베이스 관련 로직 분리")
+    @Test
+    void layeredArchitecture() {
+        boolean isJdbcTemplateInjected = false;
+        for (Field field : reservationTimeController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 }

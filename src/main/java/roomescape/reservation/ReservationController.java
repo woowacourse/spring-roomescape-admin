fd99b67e3cab9be8ccc2cdcb47bb1dd21d877d1d
gyungchan Jo <namejgc@naver.com>
2024-04-23 22:03:08 +0900

    feat: JdbcTemplate을 이용하여 예약 추가, 취소 로직 구현

--- src/main/java/roomescape/reservation/ReservationController.java
+++ src/main/java/roomescape/reservation/ReservationController.java
@@ -2,16 +2,18 @@ package roomescape.reservation;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.net.URI;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.Time;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class ReservationController {
-    private final List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong atomicLong = new AtomicLong();
     private final JdbcTemplate jdbcTemplate;
 
     public ReservationController(final JdbcTemplate jdbcTemplate) {
@@ -33,19 +35,32 @@ public class ReservationController {
 
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> create(@RequestBody ReservationRequest reservationRequest) {
-        Reservation reservation = new Reservation(
-                atomicLong.incrementAndGet(),
+        final String sql = "INSERT INTO reservation(name, date, time) VALUES(?, ?, ?)";
+        final KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            final PreparedStatement preparedStatement = con.prepareStatement(sql, new String[]{"id"});
+            preparedStatement.setString(1, reservationRequest.name());
+            preparedStatement.setDate(2, Date.valueOf(reservationRequest.date()));
+            preparedStatement.setTime(3, Time.valueOf(reservationRequest.time()));
+            return preparedStatement;
+        }, keyHolder);
+
+        final long id = keyHolder.getKey().longValue();
+        final Reservation reservation = new Reservation(
+                id,
                 reservationRequest.name(),
                 reservationRequest.date(),
-                reservationRequest.time());
+                reservationRequest.time()
+        );
 
-        reservations.add(reservation);
-        return ResponseEntity.ok(reservation);
+        return ResponseEntity.created(URI.create("/reservations/" + id)).body(reservation);
     }
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") long id) {
-        reservations.removeIf(reservation -> reservation.getId() == id);
-        return ResponseEntity.ok().build();
+        final String sql = "DELETE FROM reservation WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+
+        return ResponseEntity.noContent().build();
     }
 }

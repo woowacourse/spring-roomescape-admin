4d239acfc3afd879f613a8904da76c8ff6233a24
Zeus6768 <akaps6768@gmail.com>
2024-04-24 19:37:54 +0900

    refactor: dto 삭제 및 class -> record 변경

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -1,35 +1,8 @@
 package roomescape.reservation;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 
-public class Reservation {
+import roomescape.time.ReservationTime;
 
-    private final long id;
-    private final String name;
-    private final LocalDate date;
-    private final LocalTime time;
-
-    public Reservation(long id, String name, LocalDate date, LocalTime time) {
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
+public record Reservation(long id, String name, LocalDate date, ReservationTime time) {
 }

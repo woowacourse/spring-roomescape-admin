1f01aefb6f1853b42a189b29f6d63d975bd1a84e
libienz <leekh10195@gmail.com>
2024-04-24 19:22:31 +0900

    feat: TimeService 구현

---
+++ src/main/java/roomescape/service/TimeService.java
@@ -0,0 +1,23 @@
+package roomescape.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import roomescape.entity.ReservationTime;
+import roomescape.repository.TimeRepository;
+
+@Service
+public class TimeService {
+    private final TimeRepository timeRepository;
+
+    public TimeService(TimeRepository timeRepository) {
+        this.timeRepository = timeRepository;
+    }
+
+    public List<ReservationTime> readAll() {
+        return timeRepository.readAll();
+    }
+
+    public ReservationTime save(ReservationTime reservationTime) {
+        return timeRepository.save(reservationTime);
+    }
+}

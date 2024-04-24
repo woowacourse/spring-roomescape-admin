b7723ea7c8a441f0e753a51eeb324b5f4904ce2c
nhlee98 <e5slnh9876@gmail.com>
2024-04-24 16:11:15 +0900

    feat: 시간 삭제 API 구현

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -2,7 +2,9 @@ package roomescape.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +40,10 @@ public class ReservationTimeController {
         return ResponseEntity.ok()
                 .location(URI.create("/times/" + reservationTime.getId()))
                 .body(ReservationTimeResponse.from(reservationTime));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReservationTime(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }

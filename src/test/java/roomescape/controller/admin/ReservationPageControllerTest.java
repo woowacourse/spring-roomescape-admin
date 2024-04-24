4589555bb0c84a17df342a48e0dbc762ff3c8adf
devhoya97 <diajbh123@gmail.com>
2024-04-24 16:05:59 +0900

    test: 시간 관리 기능 테스트

--- src/test/java/roomescape/controller/admin/ReservationPageControllerTest.java
+++ src/test/java/roomescape/controller/admin/ReservationPageControllerTest.java
@@ -1,4 +1,4 @@
-package roomescape.controller;
+package roomescape.controller.admin;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
--- src/test/java/roomescape/controller/reservation/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/reservation/ReservationControllerTest.java
@@ -1,4 +1,4 @@
-package roomescape.controller;
+package roomescape.controller.reservation;
 
 import static org.hamcrest.Matchers.is;
 

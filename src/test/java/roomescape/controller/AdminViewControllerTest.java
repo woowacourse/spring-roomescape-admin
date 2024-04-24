0f8fbd24c0a33d959f97531f517cdf6fc25e9b14
libienz <leekh10195@gmail.com>
2024-04-25 00:19:15 +0900

    test: 리다이렉션 테스트 추가

--- src/test/java/roomescape/controller/AdminViewControllerTest.java
+++ src/test/java/roomescape/controller/AdminViewControllerTest.java
@@ -1,5 +1,7 @@
 package roomescape.controller;
 
+import static org.hamcrest.Matchers.equalTo;
+
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,5 +34,38 @@ class AdminViewControllerTest {
                 .when().get("/admin/time")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @DisplayName("메인 페이지를 요청하면 어드민 메인 페이지로 리다이렉트한다.")
+    @Test
+    void mainPageRedirectionTest() {
+        RestAssured.given()
+                .redirects().follow(false)
+                .when().get("http://localhost:8080")
+                .then()
+                .statusCode(302)
+                .header("Location", equalTo("http://localhost:8080/admin"));
+    }
+
+    @DisplayName("예약 페이지를 요청하면 어드민 예약 페이지로 리다이렉트한다.")
+    @Test
+    void reservationPageRedirectionTest() {
+        RestAssured.given()
+                .redirects().follow(false)
+                .when().get("http://localhost:8080/reservation")
+                .then()
+                .statusCode(302)
+                .header("Location", equalTo("http://localhost:8080/admin/reservation"));
+    }
+
+    @DisplayName("게임 시간 페이지를 요청하면 어드민 시간 페이지로 리다이렉트한다.")
+    @Test
+    void timePageRedirectionTest() {
+        RestAssured.given()
+                .redirects().follow(false)
+                .when().get("http://localhost:8080/time")
+                .then()
+                .statusCode(302)
+                .header("Location", equalTo("http://localhost:8080/admin/time"));
     }
 }

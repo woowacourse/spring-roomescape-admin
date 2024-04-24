9cbe2b0919c9bf752ce4e53b73a49547864db26f
J-I-H-O <jeongjiho0731@gmail.com>
2024-04-24 16:40:34 +0900

    refactor: record로 변경
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/roomescape/dto/reservation/ReservationRequest.java
+++ src/main/java/roomescape/dto/reservation/ReservationRequest.java
@@ -4,31 +4,9 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import roomescape.domain.reservation.Reservation;
 
-public class ReservationRequest {
-
-    private final String name;
-    private final LocalDate date;
-    private final LocalTime time;
-
-    public ReservationRequest(String name, LocalDate date, LocalTime time) {
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
+public record ReservationRequest(String name, LocalDate date, LocalTime time) {
 
     public Reservation toReservation() {
         return new Reservation(this.name, this.date, this.time);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getTime() {
-        return time;
     }
 }
--- src/main/java/roomescape/dto/time/TimeRequest.java
+++ src/main/java/roomescape/dto/time/TimeRequest.java
@@ -1,24 +1,11 @@
 package roomescape.dto.time;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalTime;
 import roomescape.domain.time.Time;
 
-public class TimeRequest {
-
-    private final LocalTime startAt;
-
-    @JsonCreator
-    public TimeRequest(@JsonProperty("startAt") LocalTime startAt) {
-        this.startAt = startAt;
-    }
+public record TimeRequest(LocalTime startAt) {
 
     public Time toTime() {
         return new Time(this.startAt);
-    }
-
-    public LocalTime getStartAt() {
-        return startAt;
     }
 }

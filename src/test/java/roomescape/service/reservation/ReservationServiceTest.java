b98292d1cd99354bf7427b86f2ea27be3bb69a8c
Seokmyung Ham <gkatjraud1@gmail.com>
2024-04-24 17:30:48 +0900

    refactor: 의존 객체와 상호작용 행위를 검증하도록 테스트 리팩토링

--- src/test/java/roomescape/service/reservation/ReservationServiceTest.java
+++ src/test/java/roomescape/service/reservation/ReservationServiceTest.java
@@ -2,7 +2,9 @@ package roomescape.service.reservation;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -25,41 +27,38 @@ class ReservationServiceTest {
     @InjectMocks
     private ReservationService reservationService;
 
-    @DisplayName("모든 예약 정보를 dto 리스트로 반환한다.")
+    @DisplayName("모든 예약 정보 조회 및 의존 객체 상호작용 테스트")
     @Test
     void find_all_reservations_test() {
         Reservation reservation1 = new Reservation(1L, "안돌", "2024-09-08", 1L, "00:00");
         Reservation reservation2 = new Reservation(2L, "재즈", "2024-11-30", 1L, "00:00");
-        List<Reservation> reservations = List.of(
-                reservation1,
-                reservation2
-        );
+        List<Reservation> reservations = List.of(reservation1, reservation2);
+        given(reservationRepository.findAllReservations()).willReturn(reservations);
+
+        List<ReservationResponseDto> results = reservationService.findAllReservations();
+        verify(reservationRepository, times(1)).findAllReservations();
+
         List<ReservationResponseDto> responseDtos = List.of(
                 new ReservationResponseDto(reservation1),
                 new ReservationResponseDto(reservation2)
         );
-
-        when(reservationRepository.findAllReservations()).thenReturn(reservations);
-
-        List<ReservationResponseDto> results = reservationService.findAllReservations();
-
         assertAll(
                 () -> assertThat(results.size()).isEqualTo(responseDtos.size()),
                 () -> assertThat(results.get(0).getId()).isEqualTo(responseDtos.get(0).getId())
         );
     }
 
-    @DisplayName("예약을 저장하고 예약 응답 dto를 반환한다.")
+    @DisplayName("예약 저장 및 의존 객체 상호작용 테스트")
     @Test
     void create_reservation_test() {
         ReservationRequestDto requestDto = new ReservationRequestDto("재즈", "2024-04-21", 1L);
         Reservation reservation = new Reservation(1L, "재즈", "2024-04-21", 1L, "15:30");
-        ReservationResponseDto responseDto = new ReservationResponseDto(reservation);
-
-        when(reservationRepository.insertReservation(requestDto.toReservation())).thenReturn(reservation);
+        given(reservationRepository.insertReservation(requestDto.toReservation())).willReturn(reservation);
 
         ReservationResponseDto result = reservationService.createReservation(requestDto);
+        verify(reservationRepository, times(1)).insertReservation(requestDto.toReservation());
 
+        ReservationResponseDto responseDto = new ReservationResponseDto(reservation);
         assertAll(
                 () -> assertThat(result.getId()).isEqualTo(responseDto.getId()),
                 () -> assertThat(result.getName()).isEqualTo(responseDto.getName()),
--- src/test/java/roomescape/service/time/ReservationTimeServiceTest.java
+++ src/test/java/roomescape/service/time/ReservationTimeServiceTest.java
@@ -2,7 +2,9 @@ package roomescape.service.time;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -25,42 +27,39 @@ class ReservationTimeServiceTest {
     @InjectMocks
     private ReservationTimeService reservationTimeService;
 
-    @DisplayName("모든 예약 시간 정보를 응답 dto로 반환한다.")
+    @DisplayName("모든 예약 시간 정보 조회 및 의존 객체 상호작용 테스트")
     @Test
     void find_all_reservation_times() {
         ReservationTime time1 = new ReservationTime(1L, "11:30");
         ReservationTime time2 = new ReservationTime(2L, "23:59");
-        List<ReservationTime> reservationTimes = List.of(
-                time1,
-                time2
-        );
+        List<ReservationTime> reservationTimes = List.of(time1, time2);
+        given(reservationTimeRepository.findAllReservationTimes()).willReturn(reservationTimes);
+
+        List<ReservationTimeResponseDto> results = reservationTimeService.findAllReservationTimes();
+        verify(reservationTimeRepository, times(1)).findAllReservationTimes();
+
         List<ReservationTimeResponseDto> responseDtos = List.of(
                 new ReservationTimeResponseDto(time1),
                 new ReservationTimeResponseDto(time2)
         );
-
-        when(reservationTimeRepository.findAllReservationTimes()).thenReturn(reservationTimes);
-
-        List<ReservationTimeResponseDto> results = reservationTimeService.findAllReservationTimes();
-
         assertAll(
                 () -> assertThat(results.size()).isEqualTo(responseDtos.size()),
                 () -> assertThat(results.get(0).getId()).isEqualTo(responseDtos.get(0).getId())
         );
     }
 
-    @DisplayName("예약 시간을 저장하고 예약 시간 응답 dto를 반환한다.")
+    @DisplayName("예약 시간 저장 및 의존 객체 상호작용 테스트")
     @Test
     void create_reservation_time_test() {
         ReservationTimeRequestDto requestDto = new ReservationTimeRequestDto("11:30");
         ReservationTime reservationTime = new ReservationTime(1L, "11:30");
-        ReservationTimeResponseDto responseDto = new ReservationTimeResponseDto(reservationTime);
-
-        when(reservationTimeRepository.insertReservationTime(requestDto.toReservationTime())).thenReturn(
+        given(reservationTimeRepository.insertReservationTime(requestDto.toReservationTime())).willReturn(
                 reservationTime);
 
         ReservationTimeResponseDto result = reservationTimeService.createReservationTime(requestDto);
+        verify(reservationTimeRepository, times(1)).insertReservationTime(requestDto.toReservationTime());
 
+        ReservationTimeResponseDto responseDto = new ReservationTimeResponseDto(reservationTime);
         assertAll(
                 () -> assertThat(result.getId()).isEqualTo(responseDto.getId()),
                 () -> assertThat(result.getStartAt()).isEqualTo(responseDto.getStartAt())

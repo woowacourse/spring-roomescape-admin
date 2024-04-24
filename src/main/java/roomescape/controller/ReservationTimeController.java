0fa3171d4bd7b9040c15a8a3b3de7c9d632d14ba
nhlee98 <e5slnh9876@gmail.com>
2024-04-24 16:04:09 +0900

    feat: 시간 추가 API 구현

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -1,12 +1,18 @@
 package roomescape.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.ReservationTime;
+import roomescape.dto.ReservationTimeRequest;
 import roomescape.dto.ReservationTimeResponse;
 import roomescape.repository.H2ReservationTimeRepository;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,5 +30,13 @@ public class ReservationTimeController {
         return repository.findAll().stream()
                 .map(ReservationTimeResponse::from)
                 .toList();
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationTimeResponse> addReservationTime(@RequestBody ReservationTimeRequest reservationTimeRequest) {
+        ReservationTime reservationTime = repository.save(reservationTimeRequest.toReservationTime());
+        return ResponseEntity.ok()
+                .location(URI.create("/times/" + reservationTime.getId()))
+                .body(ReservationTimeResponse.from(reservationTime));
     }
 }
--- src/main/java/roomescape/dto/ReservationTimeResponse.java
+++ src/main/java/roomescape/dto/ReservationTimeResponse.java
@@ -3,6 +3,7 @@ package roomescape.dto;
 import roomescape.domain.ReservationTime;
 
 public record ReservationTimeResponse(Long id, String startAt) {
+
     public static ReservationTimeResponse from(ReservationTime reservationTime) {
         return new ReservationTimeResponse(reservationTime.getId(), reservationTime.getStartAt());
     }

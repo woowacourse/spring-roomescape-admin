56dc6063b2e5495da920d039a4746eaefe270a66
haiseong <wjdgotjd9908@gmail.com>
2024-04-24 14:47:06 +0900

    refactor(ReservationRequest, ReservationTimeRequest): toEntity 메서드 생성

--- src/main/java/roomescape/admin/reservation/controller/dto/request/ReservationTimeRequest.java
+++ src/main/java/roomescape/admin/reservation/controller/dto/request/ReservationTimeRequest.java
@@ -1,6 +1,10 @@
 package roomescape.admin.reservation.controller.dto.request;
 
 import java.time.LocalTime;
+import roomescape.admin.reservation.entity.ReservationTime;
 
 public record ReservationTimeRequest(LocalTime startAt) {
+    public ReservationTime toReservationTime() {
+        return new ReservationTime(startAt);
+    }
 }

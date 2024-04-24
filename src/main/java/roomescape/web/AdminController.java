71633f9e30f182e174785b4a319261216b64da0b
Choo <choo000407@naver.com>
2024-04-24 16:47:45 +0900

    feat: `/admin/reservation` 요청 시 리뉴얼된 예약 관리 페이지를 응답한다

--- src/main/java/roomescape/web/AdminController.java
+++ src/main/java/roomescape/web/AdminController.java
@@ -14,7 +14,7 @@ public class AdminController {
 
     @GetMapping("/reservation")
     public String getReservationPage() {
-        return "admin/reservation-legacy";
+        return "admin/reservation";
     }
 
     @GetMapping("/time")

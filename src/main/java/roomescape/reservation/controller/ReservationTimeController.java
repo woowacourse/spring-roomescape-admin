a28d04d7acee3295cf112a5bc990cad803bc9d3a
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-04-24 13:21:45 +0900

    refactor: `@Controller`로 변경

--- src/main/java/roomescape/reservation/controller/ReservationTimeController.java
+++ src/main/java/roomescape/reservation/controller/ReservationTimeController.java
@@ -2,18 +2,18 @@ package roomescape.reservation.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import roomescape.reservation.dto.ReservationTimeRequest;
 import roomescape.reservation.dto.ReservationTimeResponse;
 import roomescape.reservation.service.ReservationTimeService;
 
-@RestController
+@Controller
 @RequestMapping("/times")
 public class ReservationTimeController {
     private final ReservationTimeService reservationTimeService;

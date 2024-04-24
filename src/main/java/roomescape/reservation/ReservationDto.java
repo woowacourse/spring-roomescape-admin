d7d3785a9bf1980706e1fae427a26ddeba94773c
Zeus6768 <akaps6768@gmail.com>
2024-04-24 17:12:24 +0900

    refactor: dto id 타입 Long -> long 변경

--- src/main/java/roomescape/reservation/ReservationDto.java
+++ src/main/java/roomescape/reservation/ReservationDto.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public record ReservationDto(Long id, String name, LocalDate date, LocalTime time) {
+public record ReservationDto(long id, String name, LocalDate date, LocalTime time) {
 
     public static ReservationDto from(Reservation reservation) {
         return new ReservationDto(reservation.getId(), reservation.getName(), reservation.getDate(),

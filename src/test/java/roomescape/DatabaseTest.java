16e3698a4e20d48a3840a6f2d3c352b48c689804
tackyu <tack970913@gmail.com>
2024-04-24 17:07:58 +0900

    test: 데이터베이스 연결 확인 테스트 코드 추가

---
+++ src/test/java/roomescape/DatabaseTest.java
@@ -0,0 +1,32 @@
+package roomescape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.function.Consumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class DatabaseTest {
+    
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void database_Connection_Test() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

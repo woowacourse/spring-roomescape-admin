a1b7ef5ed1449462ed45d0b09c2d428e4a4cbcd8
Minjoo <m0522inju@gmail.com>
2024-04-24 09:57:57 +0900

    style: 코드 포맷팅

--- src/main/java/roomescape/dto/ReservationDto.java
+++ src/main/java/roomescape/dto/ReservationDto.java
@@ -6,7 +6,9 @@ import roomescape.domain.reservation.Reservation;
 public record ReservationDto(Long id, String name, LocalDate date, ReservationTimeDto time) {
     public static ReservationDto from(Reservation reservation) {
         return new ReservationDto(
-                reservation.getId(), reservation.getName(), reservation.getDate(),
+                reservation.getId(),
+                reservation.getName(),
+                reservation.getDate(),
                 ReservationTimeDto.from(reservation.getTime())
         );
     }

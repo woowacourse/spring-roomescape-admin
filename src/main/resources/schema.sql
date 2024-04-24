7c407875528514ca365cbce6b1c2a8420466da61
Choo <choo000407@naver.com>
2024-04-24 16:52:37 +0900

    feat: 예약 테이블과 예약 시간 테이블 간 다:1 관계를 추가한다

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,11 @@
 CREATE TABLE reservation
 (
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    date VARCHAR(255) NOT NULL,
-    time VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    id      BIGINT       NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(255) NOT NULL,
+    date    VARCHAR(255) NOT NULL,
+    time_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
 );
 
 CREATE TABLE reservation_time

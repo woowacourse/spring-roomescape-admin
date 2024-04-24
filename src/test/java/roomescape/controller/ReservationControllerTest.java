5ff9061795c837c45e59818378af0b0998e225e3
zangsu <zangsu_@naver.com>
2024-04-24 22:19:25 +0900

    test(/test/.../controller) : 컨트롤러 통합 테스트 구현

---
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -0,0 +1,74 @@
+package roomescape.controller;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+class ReservationControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setupPort() {
+        RestAssured.port = this.port;
+    }
+
+    @DisplayName("저장된 예약을 전체 조회할 수 있다.")
+    @Test
+    void findReservationTest() {
+        RestAssured.given().log().all()
+                .when().get("/reservations").then()
+                .statusCode(200)
+                .body("size()", is(3));
+    }
+
+    @DisplayName("예약을 정상적으로 생성할 수 있다.")
+    @Test
+    void createReservationTest() {
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(Map.of(
+                        "name", "미르",
+                        "date", "2024-04-24",
+                        "timeId", "1"
+                ))
+                .when().post("/reservations")
+                .then()
+                .statusCode(200)
+                .header("Location", "/reservations/4")
+                .body("id", equalTo(4),
+                        "name", equalTo("미르"),
+                        "date", equalTo("2024-04-24"),
+                        "time.id", equalTo(1),
+                        "time.startAt", equalTo("10:00")
+                );
+    }
+
+    @DisplayName("id를 활용해 예약을 삭제할 수 있다.")
+    @Test
+    void deleteReservationTest() {
+        RestAssured.given()
+                .when().delete("/reservations/3")
+                .then()
+                .statusCode(204);
+
+        RestAssured.given()
+                .when().get("/reservations")
+                .then()
+                .body("size()", is(2));
+    }
+}
---
+++ src/test/java/roomescape/controller/ReservationTimeControllerTest.java
@@ -0,0 +1,77 @@
+package roomescape.controller;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+class ReservationTimeControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setupPort() {
+        RestAssured.port = this.port;
+    }
+
+    @DisplayName("저장된 예약 시간을 전체 조회할 수 있다.")
+    @Test
+    void findReservationTest() {
+        RestAssured.given().log().all()
+                .when().get("/times").then()
+                .statusCode(200)
+                .body("size()", is(2));
+    }
+
+    @DisplayName("예약 시간을 정상적으로 생성할 수 있다.")
+    @Test
+    void createReservationTest() {
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(Map.of(
+                        "startAt", "18:00"
+                ))
+                .when().post("/times")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(3),
+                        "startAt", equalTo("18:00")
+                );
+    }
+
+    @DisplayName("id를 활용해 예약이 없는 예약 시간을 삭제할 수 있다.")
+    @Test
+    void deleteReservationTest() {
+        RestAssured.given()
+                .when().delete("/times/2")
+                .then()
+                .statusCode(204);
+
+        RestAssured.given()
+                .when().get("/times")
+                .then()
+                .body("size()", is(1));
+    }
+
+    @DisplayName("예약이 있는 예약 시간은 삭제할 수 없다.")
+    @Test
+    void cantDeleteWhenReservationTimeUsedTest() {
+        RestAssured.given()
+                .when().delete("/times/1")
+                .then()
+                .statusCode(500);
+    }
+}

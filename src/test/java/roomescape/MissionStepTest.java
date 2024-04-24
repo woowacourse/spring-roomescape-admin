58f4436d01f97c461815d338508c467d64799894
DobbyMac <kimdobby@g.skku.edu>
2024-04-24 16:24:52 +0900

    test: 9단계 통합 테스트 코드 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
@@ -17,6 +18,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.controller.ReservationController;
 import roomescape.model.Reservation;
 import roomescape.model.ReservationDto;
 import roomescape.model.ReservationTime;
@@ -30,6 +32,9 @@ class MissionStepTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private ReservationController reservationController;
 
     private final RowMapper<ReservationDto> reservationDtoMapper = (resultSet, rowNum) ->
             new ReservationDto(
@@ -201,6 +206,21 @@ class MissionStepTest {
                 .when().delete("/times/1")
                 .then().log().all()
                 .statusCode(204);
+    }
+
+    @Test
+    @DisplayName("9단계- 기존 ReservationController의 데이터베이스 관련 로직을 다른 클래스로 분리한 것을 확인한다.")
+    void reservationControllerTest_ShouldAssertFalse_WhenChecked() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 
 }

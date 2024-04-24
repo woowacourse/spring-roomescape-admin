539b66ba89f8e57f997217978b248e22f52146e6
Seokmyung Ham <gkatjraud1@gmail.com>
2024-04-24 20:58:53 +0900

    refactor: Mock을 사용하지않고 RowMapper를 테스트하도록 변경

--- src/test/java/roomescape/repository/reservation/ReservationRowMapperTest.java
+++ src/test/java/roomescape/repository/reservation/ReservationRowMapperTest.java
@@ -2,49 +2,37 @@ package roomescape.repository.reservation;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.BDDMockito.given;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import org.junit.jupiter.api.BeforeEach;
+import java.sql.Types;
+import org.h2.tools.SimpleResultSet;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import roomescape.domain.Reservation;
 
-@ExtendWith(MockitoExtension.class)
 class ReservationRowMapperTest {
 
-    private ReservationRowMapper rowMapper;
-
-    @BeforeEach
-    void setUp() {
-        rowMapper = new ReservationRowMapper();
-    }
-
-    @Mock
-    private ResultSet resultSet;
-
-    @DisplayName("RowMapper는 ResultSet을 이용하여 Reservation을 매핑해서 반환한다.")
+    @DisplayName("RowMapper는 ResultSet을 이용하여 Reservation을 매핑한다.")
     @Test
     void return_reservation_by_map_row() throws SQLException {
-        given(resultSet.getLong("reservation_id")).willReturn(1L);
-        given(resultSet.getString("name")).willReturn("재즈");
-        given(resultSet.getString("date")).willReturn("2024-04-23");
-        given(resultSet.getLong("time_id")).willReturn(1L);
-        given(resultSet.getString("time_value")).willReturn("11:12");
+        SimpleResultSet resultSet = new SimpleResultSet();
+        resultSet.addColumn("reservation_id", Types.BIGINT, 10, 0);
+        resultSet.addColumn("name", Types.VARCHAR, 255, 0);
+        resultSet.addColumn("date", Types.VARCHAR, 255, 0);
+        resultSet.addColumn("time_id", Types.BIGINT, 10, 0);
+        resultSet.addColumn("time_value", Types.VARCHAR, 255, 0);
+        resultSet.addRow(1, "재즈", "2024-04-24", 1, "20:00");
+        resultSet.next();
 
+        ReservationRowMapper rowMapper = new ReservationRowMapper();
         Reservation reservation = rowMapper.mapRow(resultSet, 1);
 
         assertAll(
-                () -> assertThat(reservation.getId()).isEqualTo(1L),
+                () -> assertThat(reservation.getId()).isEqualTo(1),
                 () -> assertThat(reservation.getName()).isEqualTo("재즈"),
-                () -> assertThat(reservation.getDate()).isEqualTo("2024-04-23"),
-                () -> assertThat(reservation.getTimeId()).isEqualTo(1L),
-                () -> assertThat(reservation.getTimeStartAt()).isEqualTo("11:12")
+                () -> assertThat(reservation.getDate()).isEqualTo("2024-04-24"),
+                () -> assertThat(reservation.getTimeId()).isEqualTo(1),
+                () -> assertThat(reservation.getTimeStartAt()).isEqualTo("20:00")
         );
     }
-
 }
--- src/test/java/roomescape/repository/time/ReservationTimeRowMapperTest.java
+++ src/test/java/roomescape/repository/time/ReservationTimeRowMapperTest.java
@@ -2,42 +2,31 @@ package roomescape.repository.time;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.BDDMockito.given;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import org.junit.jupiter.api.BeforeEach;
+import java.sql.Types;
+import org.h2.tools.SimpleResultSet;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import roomescape.domain.ReservationTime;
 
-@ExtendWith(MockitoExtension.class)
 class ReservationTimeRowMapperTest {
 
-    private ReservationTimeRowMapper rowMapper;
-
-    @BeforeEach
-    void setUp() {
-        rowMapper = new ReservationTimeRowMapper();
-    }
-
-    @Mock
-    private ResultSet resultSet;
-
-    @DisplayName("RowMapper는 ResultSet을 이용하여 ReservationTime을 매핑해서 반환한다.")
+    @DisplayName("RowMapper는 ResultSet을 이용하여 ReservationTime을 매핑한다.")
     @Test
-    void return_reservation_time_by_rowMapper() throws SQLException {
-        given(resultSet.getLong("id")).willReturn(1L);
-        given(resultSet.getString("start_at")).willReturn("17:14");
+    void return_reservation_by_map_row() throws SQLException {
+        SimpleResultSet resultSet = new SimpleResultSet();
+        resultSet.addColumn("id", Types.BIGINT, 10, 0);
+        resultSet.addColumn("start_at", Types.VARCHAR, 255, 0);
+        resultSet.addRow(1, "20:00");
+        resultSet.next();
 
+        ReservationTimeRowMapper rowMapper = new ReservationTimeRowMapper();
         ReservationTime reservationTime = rowMapper.mapRow(resultSet, 1);
 
         assertAll(
-                () -> assertThat(reservationTime.getId()).isEqualTo(1L),
-                () -> assertThat(reservationTime.getStartAt()).isEqualTo("17:14")
+                () -> assertThat(reservationTime.getId()).isEqualTo(1),
+                () -> assertThat(reservationTime.getStartAt()).isEqualTo("20:00")
         );
     }
 }

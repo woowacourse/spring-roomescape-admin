4bfaedb8e68888e7fcc76924fe26733a91eff762
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-04-24 16:30:41 +0900

    refactor : URI 값 바로 반환하게 변경

--- src/main/java/roomescape/dto/response/ReservationCreateResponse.java
+++ src/main/java/roomescape/dto/response/ReservationCreateResponse.java
@@ -3,16 +3,18 @@ package roomescape.dto.response;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import roomescape.domain.Reservation;
 
+import java.net.URI;
+
 public record ReservationCreateResponse(long id, String name, String date,
                                         ReservationTimeCreateResponse time) {
     @JsonIgnore
-    public String getLocationHeaderValue() {
-        return "/reservations/" + id;
+    public URI getLocationHeaderValue() {
+        return URI.create("/reservations/" + id);
     }
 
     public static ReservationCreateResponse from(long id, Reservation reservation) {
         final ReservationTimeCreateResponse reservationTimeCreateResponse = new ReservationTimeCreateResponse(reservation.getTime()
                                                                                                                          .getId(), reservation.getStartAtAsString());
-        return new ReservationCreateResponse(id, reservation.getName(), reservation.getDate(), reservationTimeCreateResponse);
+        return new ReservationCreateResponse(id, reservation.getNameAsString(), reservation.getDateAsString(), reservationTimeCreateResponse);
     }
 }

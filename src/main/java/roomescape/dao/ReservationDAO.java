7c51e566e0b69535f8449edd0054c4b10d15e657
gyungchan Jo <namejgc@naver.com>
2024-04-25 17:33:53 +0900

    refactor: 예약 추가시 SimpleJdbcInsert를 사용하도록 변경

--- src/main/java/roomescape/dao/ReservationDAO.java
+++ src/main/java/roomescape/dao/ReservationDAO.java
@@ -1,42 +1,40 @@
 package roomescape.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
+import javax.sql.DataSource;
 import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public class ReservationDAO {
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert jdbcInsert;
 
-    public ReservationDAO(final JdbcTemplate jdbcTemplate) {
+    public ReservationDAO(final JdbcTemplate jdbcTemplate, final DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
+        this.jdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("reservation")
+                .usingGeneratedKeyColumns("id");
     }
 
     public Reservation insert(final Reservation reservation) {
-        final String sql = "INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)";
-        final KeyHolder keyHolder = new GeneratedKeyHolder();
-
         final String name = reservation.getName();
         final LocalDate date = reservation.getDate();
         final ReservationTime time = reservation.getTime();
 
-        jdbcTemplate.update(con -> {
-            final PreparedStatement preparedStatement = con.prepareStatement(sql, new String[]{"id"});
-            preparedStatement.setString(1, name);
-            preparedStatement.setDate(2, Date.valueOf(date));
-            preparedStatement.setLong(3, time.getId());
-            return preparedStatement;
-        }, keyHolder);
+        final SqlParameterSource parameterSource = new MapSqlParameterSource()
+                .addValue("name", name)
+                .addValue("date", date)
+                .addValue("time_id", time.getId());
 
-        final long id = keyHolder.getKey().longValue();
+        final long id = jdbcInsert.executeAndReturnKey(parameterSource).longValue();
         return new Reservation(id, name, date, time);
     }
 
--- src/main/java/roomescape/dao/ReservationTimeDAO.java
+++ src/main/java/roomescape/dao/ReservationTimeDAO.java
@@ -1,37 +1,35 @@
 package roomescape.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.ReservationTime;
 
-import java.sql.PreparedStatement;
-import java.sql.Time;
+import javax.sql.DataSource;
 import java.time.LocalTime;
 import java.util.List;
 
 @Repository
 public class ReservationTimeDAO {
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert jdbcInsert;
 
-    public ReservationTimeDAO(final JdbcTemplate jdbcTemplate) {
+    public ReservationTimeDAO(final JdbcTemplate jdbcTemplate, final DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
+        this.jdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("reservation_time")
+                .usingGeneratedKeyColumns("id");
     }
 
     public ReservationTime insert(final ReservationTime reservationTime) {
-        final String sql = "INSERT INTO reservation_time (start_at) VALUES (?)";
-        final KeyHolder keyHolder = new GeneratedKeyHolder();
-
         final LocalTime startAt = reservationTime.getStartAt();
 
-        jdbcTemplate.update(con -> {
-            final PreparedStatement preparedStatement = con.prepareStatement(sql, new String[]{"id"});
-            preparedStatement.setTime(1, Time.valueOf(startAt));
-            return preparedStatement;
-        }, keyHolder);
+        final SqlParameterSource parameterSource = new MapSqlParameterSource()
+                .addValue("start_at", startAt);
 
-        final long id = keyHolder.getKey().longValue();
+        final long id = jdbcInsert.executeAndReturnKey(parameterSource).longValue();
         return new ReservationTime(id, startAt);
     }
 

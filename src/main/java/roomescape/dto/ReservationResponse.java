93fe007e94228f03873ce24428eb6c02fde98f89
linirini <2001yerin@naver.com>
2024-04-25 20:06:03 +0900

    refactor: 불필요한 JsonProperty 제거

--- src/main/java/roomescape/dto/ReservationResponse.java
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -1,13 +1,12 @@
 package roomescape.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import roomescape.domain.Reservation;
 
 public record ReservationResponse(
         long id,
         String name,
         String date,
-        @JsonProperty("time") ReservationTimeResponse time) {
+        ReservationTimeResponse time) {
     public ReservationResponse(final Reservation reservation) {
         this(reservation.getId(), reservation.getName(), reservation.getDate(), new ReservationTimeResponse(reservation.getReservationTime()));
     }

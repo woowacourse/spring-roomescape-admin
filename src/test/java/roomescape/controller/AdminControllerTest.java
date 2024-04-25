5e16b9b7a9a65350cfba943c9ccfc8314c428f6d
Minjoo <m0522inju@gmail.com>
2024-04-25 10:42:18 +0900

    refactor: 불필요한 어노테이션 삭제

--- src/test/java/roomescape/controller/AdminControllerTest.java
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -7,10 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class AdminControllerTest {
 
     @LocalServerPort

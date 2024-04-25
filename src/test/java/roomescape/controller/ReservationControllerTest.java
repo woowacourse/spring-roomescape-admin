90cb9fe5d5cb9716f9f65459c67faa4ff46b0e13
ehtjsv2 <ehtjsv2@naver.com>
2024-04-25 15:42:20 +0900

    refactor: ReservationController test들 다른 테스트에 종속적이지 않도록 수정

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -6,16 +6,35 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import roomescape.dto.ReservationAddRequest;
 import roomescape.dto.ReservationTimeAddRequest;
+import roomescape.repository.ReservationDaoImpl;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ReservationControllerTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        jdbcTemplate.update("insert into reservation_time values(1,'10:00')");
+        jdbcTemplate.update("insert into reservation (name, date, time_id) values('브라운','2020-12-12',1)");
+    }
+
+    @AfterEach
+    void setDown() {
+        jdbcTemplate.update("delete from reservation");
+    }
 
     @DisplayName("예약 목록을 불러올 수 있다.")
     @Test
@@ -24,63 +43,12 @@ public class ReservationControllerTest {
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(0));
-    }
-
-
-    @DisplayName("delete 요청 시 id값이 존재하지 않으면 500 Internel Server Error를 응답한다.")
-    @Test
-    void should_response_bad_request_when_nonExist_id() {
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
-                .then().log().all()
-                .statusCode(500);
-    }
-
-
-    @DisplayName("예약을 추가를 성공할 시, 200 ok를 응답한다,")
-    @Test
-    void should_add_reservation_when_post_request_reservations() {
-        ReservationTimeAddRequest reservationTimeAddRequest = new ReservationTimeAddRequest(LocalTime.of(10, 0));
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(reservationTimeAddRequest)
-                .when().post("/times")
-                .then().log().all()
-                .statusCode(200);
-
-        ReservationAddRequest reservationAddRequest = new ReservationAddRequest(
-                LocalDate.of(2023, 8, 5),
-                "브라운",
-                1L);
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(reservationAddRequest)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(201);
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
                 .body("size()", is(1));
     }
 
-    @DisplayName("존재하는 리소스에 대한 삭제 요청시, 200 ok를 응답한다.")
+    @DisplayName("예약을 추가를 성공할 시, 201 ok를 응답한다,")
     @Test
-    void should_remove_reservation_when_delete_request_reservations_id() {
-        ReservationTimeAddRequest reservationTimeAddRequest = new ReservationTimeAddRequest(LocalTime.of(10, 0));
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(reservationTimeAddRequest)
-                .when().post("/times")
-                .then().log().all()
-                .statusCode(200);
-
+    void should_add_reservation_when_post_request_reservations() {
         ReservationAddRequest reservationAddRequest = new ReservationAddRequest(
                 LocalDate.of(2023, 8, 5),
                 "브라운",
@@ -92,16 +60,23 @@ public class ReservationControllerTest {
                 .when().post("/reservations")
                 .then().log().all()
                 .statusCode(201);
+    }
 
+    @DisplayName("존재하는 리소스에 대한 삭제 요청시, 204 ok를 응답한다.")
+    @Test
+    void should_remove_reservation_when_delete_request_reservations_id() {
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
                 .statusCode(204);
+    }
 
+    @DisplayName("존재하지 않는 리소스에 대한 삭제 요청시, 500 Internel Server Error를 응답한다.")
+    @Test
+    void should_response_bad_request_when_nonExist_id() {
         RestAssured.given().log().all()
-                .when().get("/reservations")
+                .when().delete("/reservations/2")
                 .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
+                .statusCode(500);
     }
 }

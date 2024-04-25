7b097348c940f4b5224a5d2f8f6ddb43af643a30
Gyeongho Yang <chchch1213@naver.com>
2024-04-25 18:39:18 +0900

    test: 엔티티 부여 메서드에 대한 테스트 추가

--- src/test/java/roomescape/entity/ReservationTest.java
+++ src/test/java/roomescape/entity/ReservationTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ReservationTest {
 
     @Test
-    @DisplayName("예약 엔티티에 아이디와 예약 시간을 부여한다.")
+    @DisplayName("예약에 아이디를 부여한다.")
     void assignId() {
         // given
         Reservation reservation = new Reservation(
@@ -27,6 +27,29 @@ class ReservationTest {
 
         // when
         Reservation actual = reservation.assignId(2L);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("예약에 시간을 부여한다.")
+    void assignTime() {
+        // given
+        ReservationTime reservationTime = new ReservationTime(1L, LocalTime.of(10, 0));
+        Reservation reservation = new Reservation(
+                2L,
+                "seyang",
+                LocalDate.of(2024, 4, 24),
+                reservationTime);
+        Reservation expected = new Reservation(
+                2L,
+                "seyang",
+                LocalDate.of(2024, 4, 24),
+                reservationTime);
+
+        // when
+        Reservation actual = reservation.assignTime(reservationTime);
 
         // then
         assertThat(actual).isEqualTo(expected);
--- src/test/java/roomescape/entity/ReservationTimeTest.java
+++ src/test/java/roomescape/entity/ReservationTimeTest.java
@@ -3,16 +3,23 @@ package roomescape.entity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ReservationTimeTest {
 
     @Test
-    @DisplayName("예약 시간 엔티티에 아이디를 부여한다.")
+    @DisplayName("예약 시간에 아이디를 부여한다.")
     void assignId() {
         // given
-
+        ReservationTime time = new ReservationTime(null, LocalTime.of(10, 0));
+        ReservationTime expected = new ReservationTime(2L, LocalTime.of(10, 0));
 
         // when
+        ReservationTime actual = time.assignId(2L);
 
         // then
+        assertThat(actual).isEqualTo(expected);
     }
 }

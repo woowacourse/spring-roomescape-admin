8f0067b16d23203ee054207480b1c8ecc1eb066a
Nam Gi Beom <rlqja0523@naver.com>
2024-04-25 17:50:44 +0900

    test: 고전파 테스트 추가

---
+++ src/test/java/roomescape/application/classical/ReservationServiceTest.java
@@ -0,0 +1,93 @@
+package roomescape.application.classical;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+import static roomescape.fixture.ClockFixture.fixedClock;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import roomescape.application.ReservationService;
+import roomescape.domain.reservation.Reservation;
+import roomescape.domain.time.ReservationTime;
+import roomescape.domain.time.repository.ReservationTimeRepository;
+import roomescape.dto.reservation.ReservationRequest;
+import roomescape.support.extension.TableTruncateExtension;
+
+@SpringBootTest
+@ExtendWith(TableTruncateExtension.class)
+public class ReservationServiceTest {
+    private final ReservationTime time = new ReservationTime(1L, LocalTime.of(10, 0));
+
+    @Autowired
+    private ReservationService reservationService;
+    @Autowired
+    private ReservationTimeRepository reservationTimeRepository;
+    @SpyBean
+    private Clock clock;
+
+    @BeforeEach
+    void setUp() {
+        reservationTimeRepository.save(time);
+        when(clock.instant()).thenReturn(fixedClock(LocalDate.of(2024, 4, 20)).instant());
+
+    }
+
+    @Test
+    void 예약을_성공한다() {
+        LocalDate date = LocalDate.of(2024, 4, 21);
+        ReservationRequest request = new ReservationRequest("prin", date, 1L);
+
+        Reservation reservation = reservationService.reserve(request);
+
+        assertThat(reservation.getName()).isEqualTo("prin");
+        assertThat(reservation.getDate()).isEqualTo(date);
+        assertThat(reservation.getTime()).isEqualTo(time.getStartAt());
+    }
+
+    @Test
+    void 최소_1일_전에_예약하지_않으면_예약을_실패한다() {
+        LocalDate invalidDate = LocalDate.of(2024, 4, 20);
+        ReservationRequest reservationRequest = new ReservationRequest("liv", invalidDate, 1L);
+
+        assertThatThrownBy(() -> reservationService.reserve(reservationRequest))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("예약은 최소 1일 전에 해야합니다.");
+    }
+
+    @Test
+    void 중복된_예약이_있으면_예약을_실패한다() {
+        LocalDate date = LocalDate.of(2024, 4, 21);
+        ReservationRequest reservationRequest = new ReservationRequest("sudal", date, 1L);
+        reservationService.reserve(reservationRequest);
+
+        assertThatThrownBy(() -> reservationService.reserve(reservationRequest))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이미 예약된 날짜, 시간입니다.");
+    }
+
+    @Test
+    void 예약을_취소한다() {
+        LocalDate date = LocalDate.of(2024, 4, 21);
+        ReservationRequest request = new ReservationRequest("prin", date, 1L);
+        Reservation reservation = reservationService.reserve(request);
+
+        reservationService.cancel(reservation.getId());
+
+        assertThat(reservationService.findReservations()).isEmpty();
+    }
+
+    @Test
+    void 존재하지_않는_예약을_취소하면_예외가_발생한다() {
+        assertThatThrownBy(() -> reservationService.cancel(1L))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("존재하지 않는 예약입니다.");
+    }
+}
---
+++ src/test/java/roomescape/application/classical/ReservationTimeServiceTest.java
@@ -0,0 +1,60 @@
+package roomescape.application.classical;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalTime;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import roomescape.application.ReservationTimeService;
+import roomescape.domain.time.ReservationTime;
+import roomescape.dto.reservationtime.ReservationTimeRequest;
+import roomescape.support.extension.TableTruncateExtension;
+
+@SpringBootTest
+@ExtendWith(TableTruncateExtension.class)
+public class ReservationTimeServiceTest {
+    @Autowired
+    private ReservationTimeService reservationTimeService;
+
+    @Test
+    void 예약_시간을_성공적으로_등록한다() {
+        LocalTime startAt = LocalTime.of(13, 0);
+        ReservationTimeRequest request = new ReservationTimeRequest(startAt);
+
+        ReservationTime reservationTime = reservationTimeService.register(request);
+
+        assertThat(reservationTime.getStartAt()).isEqualTo(startAt);
+    }
+
+    @Test
+    void 중복된_예약_시간이_있으면_등록을_실패한다() {
+        LocalTime startAt = LocalTime.of(13, 0);
+        ReservationTimeRequest request = new ReservationTimeRequest(startAt);
+        reservationTimeService.register(request);
+
+        assertThatThrownBy(() -> reservationTimeService.register(request))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이미 존재하는 예약 시간입니다.");
+    }
+
+    @Test
+    void 예약_시간을_삭제한다() {
+        LocalTime startAt = LocalTime.of(13, 0);
+        ReservationTimeRequest request = new ReservationTimeRequest(startAt);
+        ReservationTime reservationTime = reservationTimeService.register(request);
+
+        reservationTimeService.delete(reservationTime.getId());
+
+        assertThat(reservationTimeService.findReservationTimes()).isEmpty();
+    }
+
+    @Test
+    void 존재하지_않는_예약_시간을_삭제하면_예외가_발생한다() {
+        assertThatThrownBy(() -> reservationTimeService.delete(1L))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("존재하지 않는 예약 시간입니다.");
+    }
+}

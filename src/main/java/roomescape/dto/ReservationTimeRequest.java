b12df9145edc396f7e96faf1d52911dfe4fb6343
KIMGYUTAE <kiimwf0819@gmail.com>
2024-04-25 09:54:03 +0900

    refactor(ReservationTimeRequest): 필드명 변경

--- src/main/java/roomescape/dto/ReservationTimeRequest.java
+++ src/main/java/roomescape/dto/ReservationTimeRequest.java
@@ -4,10 +4,10 @@ import java.time.LocalTime;
 import roomescape.domain.ReservationTime;
 
 public record ReservationTimeRequest(
-        String time
+        String startAt
 ) {
 
     public ReservationTime toEntity() {
-        return new ReservationTime(LocalTime.parse(time));
+        return new ReservationTime(LocalTime.parse(startAt));
     }
 }

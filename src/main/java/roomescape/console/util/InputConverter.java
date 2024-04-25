605462c29611f9424c8aeec0ac6568c021c35057
leetaehoon <xogns1514@gmail.com>
2024-04-25 20:35:04 +0900

    refactor(InputConverter): 날짜, 시간 변환 변경

--- src/main/java/roomescape/console/util/InputConverter.java
+++ src/main/java/roomescape/console/util/InputConverter.java
@@ -7,18 +7,17 @@ import roomescape.reservation.dto.request.ReservationTimeRequest;
 
 public class InputConverter {
 
+    public static final String DELIMITER = ",";
+
     private InputConverter() {
     }
 
     public static ReservationRequest toReservationRequest(String body) {
-        // 검증 추가
-        String[] arguments = body.split(",");
-
+        String[] arguments = body.split(DELIMITER);
         return new ReservationRequest(arguments[0].trim(), toLocalDate(arguments[1].trim()), toId(arguments[2].trim()));
     }
 
     public static ReservationTimeRequest toReservationTimeRequest(String body) {
-        // 검증 추가
         return new ReservationTimeRequest(toTime(body));
     }
 
@@ -27,12 +26,10 @@ public class InputConverter {
     }
 
     private static LocalDate toLocalDate(String date) {
-        String[] split = date.split("-");
-        return LocalDate.of(Integer.parseInt(split[0]), Integer.parseInt(split[1]), Integer.parseInt(split[2]));
+        return LocalDate.parse(date);
     }
 
     private static LocalTime toTime(String time) {
-        String[] split = time.split(":");
-        return LocalTime.of(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+        return LocalTime.parse(time);
     }
 }

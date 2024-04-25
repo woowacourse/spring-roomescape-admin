4bd01b37715fe1782bb103751f216372095565ef
Zeus6768 <akaps6768@gmail.com>
2024-04-25 19:50:00 +0900

    refactor: 예약 id 타입 long -> Long 변경

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -4,8 +4,5 @@ import java.time.LocalDate;
 
 import roomescape.time.ReservationTime;
 
-public record Reservation(long id, String name, LocalDate date, ReservationTime time) {
-    public Reservation(String name, LocalDate date, ReservationTime time) {
-        this(0, name, date, time);
-    }
+public record Reservation(Long id, String name, LocalDate date, ReservationTime time) {
 }

c3be68aff5dfeb240d258a407d55d22b77ddfff6
Eugene Jang <kyunellroll@gmail.com>
2024-04-25 11:35:41 +0900

    style(ReservationRepository): 개행 삭제

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import roomescape.domain.Reservation;
 
 public interface ReservationRepository {
-
     Reservation saveReservation(Reservation reservation);
 
     List<Reservation> findAllReservation();

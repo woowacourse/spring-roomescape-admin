f89d16c3d39ad7649b5ecfcd749a9dc6e1dd126f
Minjoo <m0522inju@gmail.com>
2024-04-25 10:55:48 +0900

    refactor: 예측되는 사용자 API 이용 방식에 맞춰 테스트 코드 개

--- src/test/java/roomescape/ReservationFlowTest.java
+++ src/test/java/roomescape/ReservationFlowTest.java
@@ -12,18 +12,15 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.DirtiesContext;
 import roomescape.dto.ReservationRequest;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationFlowTest {
 
     @Autowired
@@ -35,21 +32,6 @@ class ReservationFlowTest {
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
-    }
-
-    @Test
-    @DisplayName("저장된 모든 reservation을 조회한다.")
-    void findAllReservationsFromDatabase() {
-        jdbcTemplate.update(
-                "INSERT INTO reservation_time (start_at) VALUES (?)", "15:40");
-        jdbcTemplate.update(
-                "INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)", "브라운", "2023-08-05", 1);
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
     }
 
     @TestFactory
@@ -73,6 +55,13 @@ class ReservationFlowTest {
                     Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
                     assertThat(count).isEqualTo(1);
                 }),
+                dynamicTest("저장된 모든 예약을 조회한다.", () -> {
+                    RestAssured.given().log().all()
+                            .when().get("/reservations")
+                            .then().log().all()
+                            .statusCode(200)
+                            .body("size()", is(1));
+                }),
                 dynamicTest("예약을 삭제한다.", () -> {
                     RestAssured.given().log().all()
                             .when().delete("/reservations/1")
@@ -82,16 +71,13 @@ class ReservationFlowTest {
                     Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation",
                             Integer.class);
                     assertThat(countAfterDelete).isEqualTo(0);
+                }),
+                dynamicTest("존재하지 않는 예약을 삭제하려고 시도하면 404 status를 반환한다.", () -> {
+                    RestAssured.given().log().all()
+                            .when().delete("/reservations/1")
+                            .then().log().all()
+                            .statusCode(404);
                 })
         );
-    }
-
-    @Test
-    @DisplayName("존재하지 않는 예약을 삭제하려고 시도하면 404 status를 반환한다.")
-    void deleteReservationByNotExistId() {
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
-                .then().log().all()
-                .statusCode(404);
     }
 }
--- src/test/java/roomescape/ReservationReservationTimeFlowTest.java
+++ src/test/java/roomescape/ReservationReservationTimeFlowTest.java
@@ -1,6 +1,7 @@
 package roomescape;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 import io.restassured.RestAssured;
@@ -11,19 +12,15 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.DirtiesContext;
-import roomescape.domain.ReservationTime;
 import roomescape.dto.ReservationTimeRequest;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationReservationTimeFlowTest {
 
     @Autowired
@@ -35,23 +32,6 @@ class ReservationReservationTimeFlowTest {
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
-    }
-
-    @Test
-    @DisplayName("저장된 모든 시간을 조회한다.")
-    void findAllTimes() {
-        jdbcTemplate.update(
-                "INSERT INTO reservation_time (start_at) VALUES (?)", "10:00"
-        );
-
-        List<ReservationTime> reservationTimes = RestAssured.given().log().all()
-                .when().get("/times")
-                .then().log().all()
-                .statusCode(200).extract()
-                .jsonPath().getList(".", ReservationTime.class);
-
-        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(1) from reservation_time", Integer.class);
-        assertThat(reservationTimes.size()).isEqualTo(count);
     }
 
     @TestFactory
@@ -72,14 +52,28 @@ class ReservationReservationTimeFlowTest {
                     Integer count = jdbcTemplate.queryForObject("SELECT COUNT(1) from reservation_time", Integer.class);
                     assertThat(count).isEqualTo(1);
                 }),
+                dynamicTest("저장된 모든 시간을 조회한다.", () -> {
+                    RestAssured.given().log().all()
+                            .when().get("/times")
+                            .then().log().all()
+                            .statusCode(200)
+                            .body("size()", is(1));
+                }),
                 dynamicTest("시간을 삭제한다.", () -> {
                     RestAssured.given().log().all()
                             .when().delete("/times/1")
                             .then().log().all()
                             .statusCode(204);
 
-                    Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT COUNT(1) from reservation_time", Integer.class);
+                    Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT COUNT(1) from reservation_time",
+                            Integer.class);
                     assertThat(countAfterDelete).isEqualTo(0);
+                }),
+                dynamicTest("존재하지 않는 시간을 삭제하려고 시도하면 404 status를 반환한다.", () -> {
+                    RestAssured.given().log().all()
+                            .when().delete("/times/1")
+                            .then().log().all()
+                            .statusCode(404);
                 })
         );
     }

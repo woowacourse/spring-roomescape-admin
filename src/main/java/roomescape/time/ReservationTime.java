82042939e5f7afbeab5c4aae68c52920f2aff04f
Zeus6768 <akaps6768@gmail.com>
2024-04-25 18:51:43 +0900

    refactor: 테스트코드에서만 사용되는 생성자 삭제

--- src/main/java/roomescape/time/ReservationTime.java
+++ src/main/java/roomescape/time/ReservationTime.java
@@ -5,9 +5,6 @@ import java.time.LocalTime;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 public record ReservationTime(long id, LocalTime startAt) {
-    public ReservationTime(LocalTime startAt) {
-        this(0, startAt);
-    }
     @JsonFormat(pattern = "HH:mm")
     public LocalTime startAt() {
         return startAt;

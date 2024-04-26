131aa254649ef40f06769ad14191487b59f79b95
HyungHoKim00 <rlagudgh1109@gmail.com>
2024-04-26 16:32:18 +0900

    fix: console에서 database가 아닌 memory를 사용하도록 수정

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -1,22 +1,39 @@
 package roomescape;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import roomescape.console.ConsoleController;
+import roomescape.console.fakedao.FakeReservationDao;
+import roomescape.console.fakedao.FakeReservationDb;
+import roomescape.console.fakedao.FakeReservationTimeDao;
+import roomescape.console.fakedao.FakeReservationTimeDb;
+import roomescape.service.ReservationService;
+import roomescape.service.ReservationTimeService;
 
 @SpringBootApplication
 public class RoomescapeApplication implements CommandLineRunner {
-    @Autowired
-    private ConsoleController consoleController;
-
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeApplication.class, args);
     }
 
     @Override
     public void run(String... args) {
+        ConsoleController consoleController = initConsoleController();
         consoleController.run();
+    }
+
+    private ConsoleController initConsoleController() {
+        FakeReservationDb fakeReservationDb = new FakeReservationDb();
+        FakeReservationTimeDb fakeReservationTimeDb = new FakeReservationTimeDb();
+        FakeReservationDao fakeReservationDao
+                = new FakeReservationDao(fakeReservationDb, fakeReservationTimeDb);
+        FakeReservationTimeDao fakeReservationTimeDao
+                = new FakeReservationTimeDao(fakeReservationDb, fakeReservationTimeDb);
+        ReservationService reservationService
+                = new ReservationService(fakeReservationDao, fakeReservationTimeDao);
+        ReservationTimeService reservationTimeService
+                = new ReservationTimeService(fakeReservationTimeDao);
+        return new ConsoleController(reservationService, reservationTimeService);
     }
 }

049725bf7f2148c0d78a1eca26444e520b261d99
leetaehoon <xogns1514@gmail.com>
2024-04-26 15:56:47 +0900

    feat(ServiceFactory): console용 ServiceFactory 생성

---
+++ src/main/java/roomescape/console/config/ServiceFactory.java
@@ -0,0 +1,35 @@
+package roomescape.console.config;
+
+import roomescape.console.dao.MemoryReservationDao;
+import roomescape.console.dao.MemoryReservationTimeDao;
+import roomescape.reservation.service.ReservationService;
+import roomescape.reservation.service.ReservationTimeService;
+
+public class ServiceFactory {
+
+    private static final ServiceFactory serviceFactory = new ServiceFactory();
+    private static final ReservationService reservationService;
+    private static final ReservationTimeService reservationTimeService;
+
+    static {
+        MemoryReservationDao memoryReservationDao = new MemoryReservationDao();
+        MemoryReservationTimeDao memoryReservationTimeDao = new MemoryReservationTimeDao();
+
+        reservationService = new ReservationService(memoryReservationDao, memoryReservationTimeDao);
+        reservationTimeService = new ReservationTimeService(memoryReservationTimeDao);
+    }
+
+    private ServiceFactory() {}
+
+    public static ServiceFactory getInstance() {
+        return serviceFactory;
+    }
+
+    public ReservationService getReservationService() {
+        return reservationService;
+    }
+
+    public ReservationTimeService getReservationTimeService() {
+        return reservationTimeService;
+    }
+}

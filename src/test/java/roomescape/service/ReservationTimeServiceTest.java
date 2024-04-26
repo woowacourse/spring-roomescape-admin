b9b827ef059db662204f92181545dbd7513afedf
SungKyum Kim <sungkyum1@naver.com>
2024-04-26 14:27:53 +0900

    test: 에러 메시지 변경에 따른 테스트 수정

--- src/test/java/roomescape/service/ReservationTimeServiceTest.java
+++ src/test/java/roomescape/service/ReservationTimeServiceTest.java
@@ -31,9 +31,10 @@ class ReservationTimeServiceTest {
     void 예약이_등록되어_있는_예약시간은_삭제할_수_없다() {
         jdbcTemplate.update("insert into reservation_time (start_at) values ('10:00')");
         jdbcTemplate.update("insert into reservation (name, date, time_id) values ('조앤', '2023-10-23', 1)");
+        long timeId = 1;
 
-        Assertions.assertThatCode(() -> reservationTimeService.deleteTime(1))
+        Assertions.assertThatCode(() -> reservationTimeService.deleteTime(timeId))
                 .isInstanceOf(ReservationExistsForTimeException.class)
-                .hasMessage("해당 예약시간에 예약이 등록되어 있습니다.");
+                .hasMessage("해당 예약시간에 예약이 등록되어 있습니다 (timeId : " + timeId + ")");
     }
 }

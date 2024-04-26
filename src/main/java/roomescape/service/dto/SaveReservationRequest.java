2aac795e1832c9e49a7fba26ea6565b1e1c7d175
KimSangHyeok <tkdgur0906@naver.com>
2024-04-26 21:57:53 +0900

    fix: 요청 json 형식과 dto의 형식 일치하게 수정

--- src/main/java/roomescape/service/dto/SaveReservationRequest.java
+++ src/main/java/roomescape/service/dto/SaveReservationRequest.java
@@ -5,7 +5,7 @@ import roomescape.domain.ReservationTime;
 
 import java.time.LocalDate;
 
-public record SaveReservationRequest(String name, LocalDate date, Long reservationTimeId) {
+public record SaveReservationRequest(String name, LocalDate date, Long timeId) {
 
     public static Reservation toEntity(SaveReservationRequest request, ReservationTime reservationTime) {
         return new Reservation(request.name(), request.date(), reservationTime);

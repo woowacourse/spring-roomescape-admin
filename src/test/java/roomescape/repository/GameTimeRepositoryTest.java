64bf7696dcfa02efffb9d417852497e8b6bff59d
libienz <leekh10195@gmail.com>
2024-04-26 16:28:23 +0900

    refactor: 테스트 코드의 불필요한 애노테이션 제거 개선

--- src/test/java/roomescape/repository/GameTimeRepositoryTest.java
+++ src/test/java/roomescape/repository/GameTimeRepositoryTest.java
@@ -13,13 +13,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import roomescape.entity.GameTime;
 
 @SpringBootTest
 @Transactional
-@Rollback
 class GameTimeRepositoryTest {
 
     @Autowired
--- src/test/java/roomescape/repository/ReservationRepositoryTest.java
+++ src/test/java/roomescape/repository/ReservationRepositoryTest.java
@@ -12,14 +12,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import roomescape.entity.GameTime;
 import roomescape.entity.Reservation;
 
 @SpringBootTest
 @Transactional
-@Rollback
 class ReservationRepositoryTest {
 
     @Autowired

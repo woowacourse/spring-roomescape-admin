03da20161e66de49bd05fe9765d2e22136f187d0
ikjo39 <chi4321@naver.com>
2024-04-26 22:45:42 +0900

    feat: view 로직 추가

--- src/main/java/roomescape/controller/console/RoomEscapeConsoleController.java
+++ src/main/java/roomescape/controller/console/RoomEscapeConsoleController.java
@@ -1,7 +1,15 @@
 package roomescape.controller.console;
 
+import static roomescape.view.Command.READ_ALL_RESERVATIONS;
+
+import java.util.List;
+import roomescape.dto.reservation.ReservationCreateRequest;
+import roomescape.dto.reservation.ReservationResponse;
+import roomescape.dto.reservationtime.ReservationTimeCreateRequest;
+import roomescape.dto.reservationtime.ReservationTimeResponse;
 import roomescape.service.ReservationService;
 import roomescape.service.ReservationTimeService;
+import roomescape.view.Command;
 import roomescape.view.InputView;
 import roomescape.view.OutputView;
 
@@ -23,6 +31,51 @@ public class RoomEscapeConsoleController {
     }
 
     public void run() {
+        Command command = READ_ALL_RESERVATIONS;
+        while (!command.isEnd()) {
+            command = inputView.readCommand();
+            switch (command) {
+                case READ_ALL_RESERVATIONS -> printReservations();
+                case READ_ALL_RESERVATIONS_TIMES -> printReservationTimes();
+                case CREATE_RESERVATION -> createReservation();
+                case CREATE_RESERVATIONS_TIME -> createReservationTime();
+                case DELETE_RESERVATION -> deleteReservation();
+                case DELETE_RESERVATIONS_TIME -> deleteReservationTime();
+            }
+        }
+    }
 
+    private void printReservations() {
+        List<ReservationResponse> results = reservationService.findAll();
+        outputView.printReservations(results);
+    }
+
+    private void printReservationTimes() {
+        List<ReservationTimeResponse> results = reservationTimeService.findAll();
+        outputView.printReservationTimes(results);
+    }
+
+    private void createReservation() {
+        ReservationCreateRequest createRequest = inputView.createReservationCreatRequest();
+        ReservationResponse result = reservationService.add(createRequest);
+        outputView.printReservation(result);
+    }
+
+    private void createReservationTime() {
+        ReservationTimeCreateRequest createRequest = inputView.createReservationTimeCreatRequest();
+        ReservationTimeResponse result = reservationTimeService.add(createRequest);
+        outputView.printReservationTime(result);
+    }
+
+    private void deleteReservation() {
+        Long id = inputView.readReservationId();
+        reservationService.delete(id);
+        outputView.printSuccessDeleted();
+    }
+
+    private void deleteReservationTime() {
+        Long id = inputView.readReservationTimeId();
+        reservationTimeService.delete(id);
+        outputView.printSuccessDeleted();
     }
 }

c5b649795e1dd3492cd9cc48603f65f5d1e7a8ed
leetaehoon <xogns1514@gmail.com>
2024-04-26 15:44:33 +0900

    feat: memory 저장용 dao 구현체 생성

---
+++ src/main/java/roomescape/console/dao/MemoryReservationDao.java
@@ -0,0 +1,42 @@
+package roomescape.console.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import roomescape.reservation.dao.ReservationDao;
+import roomescape.reservation.domain.Reservation;
+
+public class MemoryReservationDao implements ReservationDao {
+
+    private final AtomicLong index;
+    private final List<Reservation> reservations;
+
+    public MemoryReservationDao() {
+        index = new AtomicLong(0);
+        reservations = new ArrayList<>();
+    }
+
+    @Override
+    public Reservation save(Reservation reservation) {
+        long id = index.incrementAndGet();
+        Reservation saveReservation = new Reservation(id, reservation.getName(), reservation.getDate(), reservation.getTime());
+        reservations.add(saveReservation);
+
+        return saveReservation;
+    }
+
+    @Override
+    public List<Reservation> findAllReservations() {
+        return reservations;
+    }
+
+    @Override
+    public void delete(Long id) {
+        Reservation findReservation = reservations.stream()
+                .filter(reservation -> reservation.getId().equals(id))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("해당 예약을 찾을 수 없습니다."));
+
+        reservations.remove(findReservation);
+    }
+}
---
+++ src/main/java/roomescape/console/dao/MemoryReservationTimeDao.java
@@ -0,0 +1,51 @@
+package roomescape.console.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import roomescape.reservation.dao.ReservationTimeDao;
+import roomescape.reservation.domain.ReservationTime;
+
+public class MemoryReservationTimeDao implements ReservationTimeDao {
+
+    private final AtomicLong index;
+    private final List<ReservationTime> reservationTimes;
+
+    public MemoryReservationTimeDao() {
+        this.index = new AtomicLong(0);
+        this.reservationTimes = new ArrayList<>();
+    }
+
+    @Override
+    public ReservationTime save(ReservationTime reservationTime) {
+        long id = index.incrementAndGet();
+        ReservationTime saveReservationTime = new ReservationTime(id, reservationTime.getStartAt());
+        reservationTimes.add(saveReservationTime);
+
+        return saveReservationTime;
+    }
+
+    @Override
+    public List<ReservationTime> findAllReservationTimes() {
+        return reservationTimes;
+    }
+
+    @Override
+    public Optional<ReservationTime> findById(Long id) {
+        ReservationTime findReservation = reservationTimes.stream()
+                .filter(reservationTime -> reservationTime.getId().equals(id))
+                .findAny()
+                .orElse(null);
+
+        return Optional.ofNullable(findReservation);
+    }
+
+    @Override
+    public void delete(Long id) {
+        ReservationTime findReservationTime = findById(id)
+                .orElseThrow(() -> new IllegalStateException("존재하지 않는 예약 시간입니다."));
+
+        reservationTimes.remove(findReservationTime);
+    }
+}

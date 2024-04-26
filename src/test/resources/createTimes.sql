294b6e5968390c9f401d6a6af18243b759160298
reddevilmidzy <midzy0228@gmail.com>
2024-04-26 14:15:26 +0900

    refactor: 시간 생성 쿼리문 수정

--- src/test/resources/createTimes.sql
+++ src/test/resources/createTimes.sql
@@ -1,3 +1,4 @@
-INSERT INTO reservation_time (start_at) values ('12:00:00');
-INSERT INTO reservation_time (start_at) values ('13:00:00');
-INSERT INTO reservation_time (start_at) values ('14:00:00');
+INSERT INTO reservation_time (start_at)
+values ('12:00:00'),
+       ('13:00:00'),
+       ('14:00:00');

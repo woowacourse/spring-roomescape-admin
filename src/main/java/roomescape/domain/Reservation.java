2b21920039c0a152676bcf2478c787774f4109d7
HoeSeong123 <ghltjd3@gmail.com>
2024-04-26 14:24:12 +0900

    refactor : null 값이 들어오는 경우에 대한 예외처리

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -9,11 +9,18 @@ public class Reservation {
     private LocalDate date;
     private ReservationTime time;
 
-    public Reservation(Long id, String name, LocalDate date, ReservationTime time) {
+    public Reservation(Long id, String name, LocalDate date, ReservationTime time) throws NullPointerException {
+        validate(name, date, time);
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    private void validate(String name, LocalDate date, ReservationTime time) {
+        if(name == null || date == null || time == null) {
+            throw new NullPointerException();
+        }
     }
 
     public Long getId() {
--- src/main/java/roomescape/domain/ReservationTime.java
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -6,9 +6,16 @@ public class ReservationTime {
     private long id;
     private LocalTime startAt;
 
-    public ReservationTime(long id, LocalTime startAt) {
+    public ReservationTime(long id, LocalTime startAt) throws NullPointerException {
+        validate(startAt);
         this.id = id;
         this.startAt = startAt;
+    }
+
+    private void validate(LocalTime startAt) {
+        if(startAt == null) {
+            throw new NullPointerException();
+        }
     }
 
     public long getId() {

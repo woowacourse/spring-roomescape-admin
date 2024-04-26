3760231b9c644af46cf2eef90cc9ba93ff8d5974
Eugene Jang <kyunellroll@gmail.com>
2024-04-26 15:50:09 +0900

    style(ReservationIntegrationTest): 어노테이션 설정 작성 방식 통일

--- src/test/java/roomescape/integration/ReservationIntegrationTest.java
+++ src/test/java/roomescape/integration/ReservationIntegrationTest.java
@@ -10,10 +10,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ReservationIntegrationTest {
     @LocalServerPort
     private int port;

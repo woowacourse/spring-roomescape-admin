a3b7528be03ff7ea88a4064dc40afb0e95854de6
KimSangHyeok <tkdgur0906@naver.com>
2024-04-26 13:44:51 +0900

    test: 예약, 예약시간 실패 단위테서트 수정

--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -3,6 +3,9 @@ package roomescape.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ReservationTest {
@@ -10,8 +13,8 @@ class ReservationTest {
     @Test
     @DisplayName("성공 : id를 통해 동일한 예약인지 판별한다.")
     void checkSameReservation_Success() {
-        Reservation reservation1 = new Reservation(1L, "capy", "2024-04-25", new ReservationTime("10:00"));
-        Reservation reservation2 = new Reservation(1L, "capy", "2024-04-25", new ReservationTime("10:00"));
+        Reservation reservation1 = new Reservation(1L, "capy", LocalDate.of(2024, 4, 25), new ReservationTime(LocalTime.of(10, 0)));
+        Reservation reservation2 = new Reservation(1L, "capy", LocalDate.of(2024, 4, 25), new ReservationTime(LocalTime.of(10, 0)));
 
         assertThat(reservation1.isSameReservation(reservation2.getId())).isTrue();
     }
@@ -19,8 +22,8 @@ class ReservationTest {
     @Test
     @DisplayName("실패 : id를 통해 동일한 예약인지 판별한다.")
     void checkSameReservation_Failure() {
-        Reservation reservation1 = new Reservation(1L, "capy", "2024-04-25", new ReservationTime("10:00"));
-        Reservation reservation2 = new Reservation(2L, "capy", "2024-04-25", new ReservationTime("11:00"));
+        Reservation reservation1 = new Reservation(1L, "capy", LocalDate.of(2024, 4, 25), new ReservationTime(LocalTime.of(10, 0)));
+        Reservation reservation2 = new Reservation(2L, "capy", LocalDate.of(2024, 4, 25), new ReservationTime(LocalTime.of(11, 0)));
 
         assertThat(reservation1.isSameReservation(reservation2.getId())).isFalse();
     }
--- src/test/java/roomescape/domain/ReservationTimeTest.java
+++ src/test/java/roomescape/domain/ReservationTimeTest.java
@@ -3,6 +3,8 @@ package roomescape.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ReservationTimeTest {
@@ -10,8 +12,8 @@ class ReservationTimeTest {
     @Test
     @DisplayName("성공 : id를 통해 동일한 예약 시간인지 판별한다.")
     void checkSameReservationTime_Success() {
-        ReservationTime reservationTime1 = new ReservationTime(1L, "10:00");
-        ReservationTime reservationTime2 = new ReservationTime(1L, "10:00");
+        ReservationTime reservationTime1 = new ReservationTime(1L, LocalTime.of(10, 0));
+        ReservationTime reservationTime2 = new ReservationTime(1L, LocalTime.of(10, 0));
 
         assertThat(reservationTime1.isSameReservationTime(reservationTime2.getId())).isTrue();
     }
@@ -19,8 +21,8 @@ class ReservationTimeTest {
     @Test
     @DisplayName("실패 : id를 통해 동일한 예약 시간인지 판별한다.")
     void checkSameReservationTime_Failure() {
-        ReservationTime reservationTime1 = new ReservationTime(1L, "10:00");
-        ReservationTime reservationTime2 = new ReservationTime(2L, "11:00");
+        ReservationTime reservationTime1 = new ReservationTime(1L, LocalTime.of(10, 0));
+        ReservationTime reservationTime2 = new ReservationTime(2L, LocalTime.of(10, 0));
 
         assertThat(reservationTime1.isSameReservationTime(reservationTime2.getId())).isFalse();
     }

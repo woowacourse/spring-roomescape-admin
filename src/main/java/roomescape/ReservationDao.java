5dd212a26f0d7334810f1ad67b4a6a505a30af8b
tackyu <tack970913@gmail.com>
2024-04-26 20:44:37 +0900

    feat: 데이터베이스 연동 데이터 추가 기능 구현

--- src/main/java/roomescape/ReservationDao.java
+++ src/main/java/roomescape/ReservationDao.java
@@ -2,9 +2,14 @@ package roomescape;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.Reservation;
+import roomescape.dto.ReservationRequest;
 
+import javax.sql.DataSource;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
@@ -12,21 +17,31 @@ import java.util.List;
 @Repository
 public class ReservationDao {
 
-    private  JdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert simpleJdbcInsert;
 
-    public ReservationDao(JdbcTemplate jdbcTemplate) {
+    public ReservationDao(JdbcTemplate jdbcTemplate, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("reservation")
+                .usingGeneratedKeyColumns("id");
     }
 
-    private final RowMapper<Reservation> reservationRowMapper=(resultSet,rowNum)-> new Reservation(
+    private final RowMapper<Reservation> reservationRowMapper = (resultSet, rowNum) -> new Reservation(
             resultSet.getLong("id"),
             resultSet.getString("name"),
             LocalDate.parse(resultSet.getString("date")),
             LocalTime.parse(resultSet.getString("time"))
     );
 
-    public List<Reservation> findAll(){
-        String sql="select * from reservation";
-        return jdbcTemplate.query(sql,reservationRowMapper);
+    public List<Reservation> findAll() {
+        String sql = "select * from reservation";
+        return jdbcTemplate.query(sql, reservationRowMapper);
+    }
+
+    public Reservation insert(ReservationRequest reservationRequest) {
+        SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(reservationRequest);
+        long id = simpleJdbcInsert.executeAndReturnKey(parameterSource).longValue();
+        return reservationRequest.toReservation(id);
     }
 }
--- src/main/java/roomescape/controller/ReservationsController.java
+++ src/main/java/roomescape/controller/ReservationsController.java
@@ -4,19 +4,12 @@ import org.springframework.web.bind.annotation.*;
 import roomescape.ReservationDao;
 import roomescape.domain.Reservation;
 import roomescape.dto.ReservationRequest;
-import roomescape.dto.ReservationResponse;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationsController {
-    private final List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong id = new AtomicLong(1);
     private final ReservationDao reservationDao;
 
     public ReservationsController(ReservationDao reservationDao) {
@@ -33,6 +26,8 @@ public class ReservationsController {
         Reservation newReservation = reservationRequest.toReservation(id.getAndIncrement());
         reservations.add(newReservation);
         return ReservationResponse.toDto(newReservation);
+    public Reservation create(@RequestBody ReservationRequest reservationRequest) {
+        return reservationDao.insert(reservationRequest);
     }
 
     @DeleteMapping("/{id}")

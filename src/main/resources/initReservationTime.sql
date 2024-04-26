702504c172bf9ebecc66175cc0f06ffd5b619c1c
jinwoo22 <jenny010925@naver.com>
2024-04-26 13:12:20 +0900

    refactor: dao 테스트 정리

---
+++ src/main/resources/initReservationTime.sql
@@ -0,0 +1,1 @@
+insert into reservation_time(start_at) values('10:00');
--- src/test/java/roomescape/dao/ReservationDaoTest.java
+++ src/test/java/roomescape/dao/ReservationDaoTest.java
@@ -9,42 +9,42 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
 import roomescape.domain.Reservation;
+import roomescape.domain.ReservationTime;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@Sql("classpath:initReservationTime.sql")
 class ReservationDaoTest {
 
     @Autowired
     private ReservationDao reservationDao;
 
-    private final Reservation request = new Reservation(
-            null,
-            "브라운",
-            LocalDate.of(2023, 8, 5),
-            LocalTime.of(10, 0));
+    @Test
+    void save() {
+        Long savedId = reservationDao.save(createTestReservation());
+        assertThat(savedId).isEqualTo(1);
+    }
 
     @Test
     void findAll() {
-        reservationDao.save(request);
-
+        reservationDao.save(createTestReservation());
         List<Reservation> reservations = reservationDao.findAll();
 
         assertThat(reservations).size().isEqualTo(1);
     }
 
     @Test
-    void save() {
-        Long savedId = reservationDao.save(request);
-
-        assertThat(savedId).isEqualTo(1);
-    }
-
-    @Test
     void delete() {
-        Long savedId = reservationDao.save(request);
+        Long savedId = reservationDao.save(createTestReservation());
         reservationDao.delete(savedId);
 
         assertThat(reservationDao.findAll()).isEmpty();
+    }
+
+    private Reservation createTestReservation() {
+        ReservationTime reservationTime = new ReservationTime(1L, LocalTime.of(10, 0));
+        return new Reservation(null, "브라운", LocalDate.of(2023, 8, 5), reservationTime);
     }
 }

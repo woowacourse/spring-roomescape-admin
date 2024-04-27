c85c4f86e23fe97286c807af697c72ea10a9d885
3juhwan <13selfesteem91@naver.com>
2024-04-27 20:33:52 +0900

    refactor(console): 빈 수동 등록으로 변경

--- src/main/java/roomescape/console/AppConfig.java
+++ src/main/java/roomescape/console/AppConfig.java
@@ -5,11 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import roomescape.console.controller.ConsoleController;
 import roomescape.console.controller.ReservationTimeController;
 import roomescape.console.utils.Command;
+import roomescape.console.utils.ControllerMapper;
 
 import java.util.Map;
 
 @Configuration
 public class AppConfig {
+    @Bean
+    public ControllerMapper controllerMapper(ReservationTimeController reservationTimeController) {
+        return new ControllerMapper(controllers(reservationTimeController));
+    }
+
     @Bean
     public Map<Command, ConsoleController> controllers(ReservationTimeController reservationTimeController) {
         return Map.of(Command.TIMES, reservationTimeController);
--- src/main/java/roomescape/console/utils/ControllerMapper.java
+++ src/main/java/roomescape/console/utils/ControllerMapper.java
@@ -1,12 +1,10 @@
 package roomescape.console.utils;
 
-import org.springframework.stereotype.Component;
 import roomescape.console.controller.ConsoleController;
 import roomescape.console.view.ConsoleRequest;
 
 import java.util.Map;
 
-@Component
 public class ControllerMapper {
     private final Map<Command, ConsoleController> controllers;
 

b9b90ed061aa07f47eb38e6255d30831e291eb6d
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-04-27 14:51:01 +0900

    feat(ConsoleInputView): 예외 처리 추가 및 메서드 순서 변경

--- src/main/java/roomescape/console/view/ConsoleInputView.java
+++ src/main/java/roomescape/console/view/ConsoleInputView.java
@@ -1,5 +1,6 @@
 package roomescape.console.view;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -30,9 +31,9 @@ public class ConsoleInputView {
         System.out.println("ex) 2023-08-10, 브리, 1");
 
         List<String> reservation = List.of(inputString().split(DELIMITER));
-        LocalDate date = LocalDate.parse(reservation.get(0).trim(), DATE_FORMATTER);
+        LocalDate date = toLocalDate(reservation.get(0));
         String name = reservation.get(1).trim();
-        Long timeId = Long.parseLong(reservation.get(2).trim());
+        Long timeId = toId(reservation.get(2));
 
         return new ReservationRequest(date, name, timeId);
     }
@@ -43,23 +44,67 @@ public class ConsoleInputView {
         System.out.println("ex) 09:50");
 
         String reservationTime = inputString();
-        LocalTime startAt = LocalTime.parse(reservationTime.trim(), TIME_FORMATTER);
+        LocalTime startAt = toLocalTime(reservationTime);
 
         return new ReservationTimeRequest(startAt);
+    }
+
+    private String inputString() {
+        return SCANNER.nextLine();
+    }
+
+    private LocalDate toLocalDate(String string) {
+        try {
+            return LocalDate.parse(string.trim(), DATE_FORMATTER);
+        } catch (DateTimeException exception) {
+            throw new IllegalArgumentException("날짜 형식이 일치하지 않습니다.", exception);
+        }
+    }
+
+    private LocalTime toLocalTime(String string) {
+        try {
+            return LocalTime.parse(string.trim(), TIME_FORMATTER);
+        } catch (DateTimeException exception) {
+            throw new IllegalArgumentException("시간 형식이 일치하지 않습니다.", exception);
+        }
+    }
+
+    private Long toId(String string) {
+        try {
+            return Long.parseLong(string);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("id 형식(양수)을 맞추어 입력해야 합니다.", exception);
+        }
     }
 
     public DomainCommand chooseDomain() {
         System.out.println();
         System.out.println("도메인을 선택해주세요.");
         System.out.println("1 - 예약 관리, 2 - 예약 시간 관리, 3 - 프로그램 종료");
-        return INT_TO_DOMAIN_COMMAND.get(inputInt());
+        return inputCommandBy(INT_TO_DOMAIN_COMMAND);
     }
 
     public FunctionCommand chooseFunction() {
         System.out.println();
         System.out.println("기능을 선택해주세요");
         System.out.println("1 - 전체 조회, 2 - 생성, 3 - 삭제");
-        return INT_TO_FUNCTION_COMMAND.get(inputInt());
+        return inputCommandBy(INT_TO_FUNCTION_COMMAND);
+    }
+
+    private <T> T inputCommandBy(Map<Integer, T> commandMap) {
+        T command = commandMap.get(inputInt());
+        if (command == null) {
+            throw new IllegalArgumentException("입력 형식이 일치하지 않습니다.");
+        }
+        return command;
+    }
+
+    private int inputInt() {
+        try {
+            return Integer.parseInt(SCANNER.nextLine());
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("정수 만을 입력해야 합니다.", exception);
+        }
     }
 
     public Long inputDeletingId() {
@@ -68,16 +113,12 @@ public class ConsoleInputView {
         return inputId();
     }
 
-    private String inputString() {
-        return SCANNER.nextLine();
-    }
-
-    private int inputInt() {
-        return Integer.parseInt(SCANNER.nextLine());
-    }
-
     private Long inputId() {
-        return Long.parseLong(SCANNER.nextLine());
+        try {
+            return Long.parseLong(SCANNER.nextLine());
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("id 형식(양수)을 맞추어 입력해야 합니다.", exception);
+        }
     }
 
 }

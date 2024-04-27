0692cf2ab9794ec855b1346cb6d23cac87e3b08d
3juhwan <13selfesteem91@naver.com>
2024-04-27 20:28:59 +0900

    refactor(console): 불필요한 형식 지정자 삭제

--- src/main/java/roomescape/console/controller/response/ReservationTimeResponse.java
+++ src/main/java/roomescape/console/controller/response/ReservationTimeResponse.java
@@ -1,13 +1,12 @@
 package roomescape.console.controller.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import roomescape.core.domain.ReservationTime;
 
 import java.time.LocalTime;
 
 public record ReservationTimeResponse(
         Long id,
-        @JsonFormat(pattern = "HH:mm") LocalTime startAt
+        LocalTime startAt
 ) {
     public static ReservationTimeResponse from(ReservationTime reservationTime) {
         return new ReservationTimeResponse(

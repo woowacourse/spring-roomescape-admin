da2a4800808b351dc6c3520f5ac746a6173930ee
HyungUk Ryu <hw0603@naver.com>
2024-04-27 15:15:41 +0900

    refactor(exceptionHandler): 클래스명 변경 - ControllerExceptionHandler -> ExceptionHandler

--- src/main/java/roomescape/exception/ExceptionHandler.java
+++ src/main/java/roomescape/exception/ExceptionHandler.java
@@ -4,18 +4,17 @@ import java.time.format.DateTimeParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ControllerExceptionHandler {
-    @ExceptionHandler(IllegalArgumentException.class)
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(IllegalArgumentException e) {
         ExceptionResponse response = new ExceptionResponse(HttpStatus.BAD_REQUEST, e.getMessage());
         return ResponseEntity.badRequest()
                 .body(response);
     }
 
-    @ExceptionHandler(DateTimeParseException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(DateTimeParseException.class)
     public ResponseEntity<ExceptionResponse> handleDateTimeParseException() {
         ExceptionResponse response = new ExceptionResponse(HttpStatus.BAD_REQUEST, "날짜/시간 포맷이 잘못되었습니다.");
         return ResponseEntity.badRequest()

7be00c3f8fb59f697fc25fa2f79083005d2576b0
3juhwan <13selfesteem91@naver.com>
2024-04-28 00:54:11 +0900

    feat(console): 콘솔 예약 API controller 어댑터 빈 등록

--- src/main/java/roomescape/console/AppConfig.java
+++ src/main/java/roomescape/console/AppConfig.java
@@ -3,6 +3,7 @@ package roomescape.console;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import roomescape.console.controller.ControllerAdapter;
+import roomescape.console.controller.ReservationControllerAdapter;
 import roomescape.console.controller.ReservationTimeControllerAdapter;
 import roomescape.console.utils.ConsoleCommand;
 import roomescape.console.utils.ControllerAdapterMapper;
@@ -12,12 +13,21 @@ import java.util.Map;
 @Configuration
 public class AppConfig {
     @Bean
-    public ControllerAdapterMapper controllerMapper(ReservationTimeControllerAdapter reservationTimeControllerAdapter) {
-        return new ControllerAdapterMapper(controllers(reservationTimeControllerAdapter));
+    public ControllerAdapterMapper controllerAdapterMapper(
+            ReservationTimeControllerAdapter reservationTimeControllerAdapter,
+            ReservationControllerAdapter reservationControllerAdapter
+    ) {
+        return new ControllerAdapterMapper(controllers(
+                reservationTimeControllerAdapter,
+                reservationControllerAdapter));
     }
 
     @Bean
-    public Map<ConsoleCommand, ControllerAdapter> controllers(ReservationTimeControllerAdapter reservationTimeControllerAdapter) {
-        return Map.of(ConsoleCommand.TIMES, reservationTimeControllerAdapter);
+    public Map<ConsoleCommand, ControllerAdapter> controllers(
+            ReservationTimeControllerAdapter reservationTimeControllerAdapter,
+            ReservationControllerAdapter reservationControllerAdapter
+    ) {
+        return Map.of(ConsoleCommand.TIMES, reservationTimeControllerAdapter,
+                ConsoleCommand.RESERVATIONS, reservationControllerAdapter);
     }
 }

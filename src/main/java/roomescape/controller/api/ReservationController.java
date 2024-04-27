78952ebd6e31ddb26019bc5a3678ad535011958f
hyxrxn <gpfls981220@gmail.com>
2024-04-27 15:37:54 +0900

    feat(ReservationDao): reservation 테이블 접근 책임 분리

--- src/main/java/roomescape/controller/api/ReservationController.java
+++ src/main/java/roomescape/controller/api/ReservationController.java
@@ -1,13 +1,7 @@
 package roomescape.controller.api;
 
-import java.sql.Date;
-import java.time.LocalTime;
 import java.util.List;
-import java.util.Map;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,44 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import roomescape.dao.ReservationDao;
 import roomescape.domain.Reservation;
-import roomescape.domain.ReservationTime;
 import roomescape.dto.reservation.ReservationRequest;
 import roomescape.dto.reservation.ReservationResponse;
-import roomescape.dto.reservationtime.ReservationTimeResponse;
 
 @RequestMapping("/reservations")
 @Controller
 public class ReservationController {
 
-    private static final RowMapper<Reservation> RESERVATION_ROW_MAPPER = (resultSet, rowNum) -> {
-        Long timeId = resultSet.getLong("time_id");
-        LocalTime time = resultSet.getTime("start_at").toLocalTime();
+    ReservationDao reservationDao;
 
-        return new Reservation(
-                resultSet.getLong("id"),
-                resultSet.getString("name"),
-                resultSet.getString("date"),
-                new ReservationTime(timeId, time));
-    };
-    private static final RowMapper<ReservationTime> RESERVATION_TIME_ROW_MAPPER = (resultSet, rowNum) ->
-            new ReservationTime(
-                    resultSet.getLong("id"),
-                    resultSet.getTime("start_at").toLocalTime());
-
-    private JdbcTemplate jdbcTemplate;
-    private SimpleJdbcInsert simpleJdbcInsert;
-
-    public ReservationController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate.getDataSource())
-                .withTableName("reservation")
-                .usingGeneratedKeyColumns("id");
+    public ReservationController(ReservationDao reservationDao) {
+        this.reservationDao = reservationDao;
     }
 
     @GetMapping("")
     public ResponseEntity<List<ReservationResponse>> reservations() {
-        List<ReservationResponse> reservationResponses = getReservations().stream()
+        List<ReservationResponse> reservationResponses = reservationDao.findAllReservations()
+                .stream()
                 .map(ReservationResponse::from)
                 .toList();
 
@@ -61,48 +36,17 @@ public class ReservationController {
 
     @PostMapping("")
     public ResponseEntity<ReservationResponse> create(@RequestBody ReservationRequest reservationRequest) {
-        Long id = simpleJdbcInsert.executeAndReturnKey(
-                        Map.of(
-                                "name", reservationRequest.name(),
-                                "date", Date.valueOf(reservationRequest.date()),
-                                "time_id", reservationRequest.timeId()
-                        ))
-                .longValue();
-
-        ReservationTime reservationTime = jdbcTemplate.queryForObject(
-                "select id, start_at from reservation_time where id = ?",
-                RESERVATION_TIME_ROW_MAPPER,
-                reservationRequest.timeId());
-
-        ReservationResponse reservationResponse = new ReservationResponse(
-                id,
-                reservationRequest.name(),
-                reservationRequest.date().toString(),
-                ReservationTimeResponse.from(reservationTime));
+        Long id = reservationDao.saveReservation(reservationRequest);
+        Reservation reservation = reservationDao.findReservation(id);
+        ReservationResponse reservationResponse = ReservationResponse.from(reservation);
 
         return ResponseEntity.ok(reservationResponse);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        jdbcTemplate.update("delete from reservation where id = ?", id);
+        reservationDao.deleteReservation(id);
 
         return ResponseEntity.ok().build();
-    }
-
-    private List<Reservation> getReservations() {
-        String sql = """
-                select
-                    r.id as reservation_id,
-                    r.name,
-                    r.date,
-                    t.id as time_id,
-                    t.start_at as time_value
-                from reservation as r
-                inner join reservation_time as t
-                on r.time_id = t.id
-                """;
-
-        return jdbcTemplate.query(sql, RESERVATION_ROW_MAPPER);
     }
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.controller.api.ReservationController;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -21,6 +23,8 @@ class MissionStepTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private ReservationController reservationController;
 
     @Test
     void 일단계() {
@@ -108,5 +112,19 @@ class MissionStepTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
+    }
+
+    @Test
+    void 구단계() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 }

4cf5fb0b937f9815497e0a24446c02cc4627b6d1
Eugene Jang <kyunellroll@gmail.com>
2024-04-27 14:49:49 +0900

    refactor(...Repository): 인터페이스가 아닌 구현체만 사용하도록 변경

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -1,12 +1,33 @@
 package roomescape.repository;
 
 import java.util.List;
+import org.springframework.stereotype.Component;
+import roomescape.dao.ReservationDaoImpl;
+import roomescape.dao.ReservationTimeDao;
 import roomescape.domain.Reservation;
+import roomescape.domain.Time;
 
-public interface ReservationRepository {
-    void saveReservation(Reservation reservation);
+@Component
+public class ReservationRepository {
+    private final ReservationDaoImpl reservationDao;
+    private final ReservationTimeDao reservationTimeDao;
 
-    List<Reservation> findAllReservation();
+    public ReservationRepository(ReservationDaoImpl reservationDao, ReservationTimeDao reservationTimeDao) {
+        this.reservationDao = reservationDao;
+        this.reservationTimeDao = reservationTimeDao;
+    }
 
-    void deleteReservationById(long reservationId);
+    public void saveReservation(Reservation reservation) {
+        Time time = reservationTimeDao.findById(reservation.getReservationTime().getId());
+        reservation.setTime(time);
+        reservationDao.save(reservation);
+    }
+
+    public List<Reservation> findAllReservation() {
+        return reservationDao.findAll();
+    }
+
+    public void deleteReservationById(long reservationId) {
+        reservationDao.deleteById(reservationId);
+    }
 }
--- src/main/java/roomescape/repository/ReservationTimeRepository.java
+++ src/main/java/roomescape/repository/ReservationTimeRepository.java
@@ -1,12 +1,27 @@
 package roomescape.repository;
 
 import java.util.List;
-import roomescape.domain.ReservationTime;
+import org.springframework.stereotype.Component;
+import roomescape.dao.ReservationTimeDao;
+import roomescape.domain.Time;
 
-public interface ReservationTimeRepository {
-    void saveReservationTime(ReservationTime reservationTime);
+@Component
+public class ReservationTimeRepository {
+    private final ReservationTimeDao reservationTimeDao;
 
-    List<ReservationTime> findAllReservationTimes();
+    public ReservationTimeRepository(ReservationTimeDao reservationTimeDao) {
+        this.reservationTimeDao = reservationTimeDao;
+    }
 
-    void deleteReservationTimeById(long reservationTimeId);
+    public void saveReservationTime(Time reservationTime) {
+        reservationTimeDao.save(reservationTime);
+    }
+
+    public List<Time> findAllReservationTimes() {
+        return reservationTimeDao.findAll();
+    }
+
+    public void deleteReservationTimeById(long reservationTimeId) {
+        reservationTimeDao.deleteById(reservationTimeId);
+    }
 }

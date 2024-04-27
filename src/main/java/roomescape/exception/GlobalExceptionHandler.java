aedda2a261f39e57d846f4a22d63edddf237ea9a
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-04-27 18:54:52 +0900

    fix: 서버 에러 노출 해결

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package roomescape.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(new ExceptionResponse(e.getMessage()));
@@ -13,6 +17,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleException(Exception e) {
-        return ResponseEntity.internalServerError().body(new ExceptionResponse(e.getMessage()));
+        log.error(e.getMessage());
+        return ResponseEntity.internalServerError()
+                .body(new ExceptionResponse(ErrorType.UNEXPECTED_SERVER_ERROR.getMessage()));
     }
 }

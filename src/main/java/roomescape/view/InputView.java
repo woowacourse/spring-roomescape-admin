674ef7c18e0434bd5385e133d7bc2054942aab76
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-04-28 04:38:08 +0900

    refactor: 간단한 안내메시지를 inputView에서 출력하도록 수정

--- src/main/java/roomescape/view/InputView.java
+++ src/main/java/roomescape/view/InputView.java
@@ -1,5 +1,7 @@
 package roomescape.view;
 
+import roomescape.domain.reservation.ReservationTime;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeParseException;
@@ -8,12 +10,19 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public String readOptions() {
-        return SCANNER.nextLine();
+    public int readOptions() {
+        try {
+            return Integer.parseInt(SCANNER.nextLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식의 옵션입니다.");
+        }
     }
 
     public LocalTime readTime() {
         try {
+            System.out.println("[INFO] 원하는 예약 시간을 입력해주세요 ex) 19:00:00");
+            System.out.println("[INFO] 방탈출 예약 가능 시간은" + ReservationTime.startTime() +
+                    "부터 " + ReservationTime.endTime() + "까지입니다.");
             return LocalTime.parse(SCANNER.nextLine());
         } catch (DateTimeParseException e) {
             throw new IllegalArgumentException("[ERROR] 잘못된 형식의 시간입니다.");
@@ -22,6 +31,9 @@ public class InputView {
 
     public LocalDate readDate() {
         try {
+            System.out.println("[INFO] 원하는 예약 시간을 입력해주세요 ex) 19:00:00");
+            System.out.println("[INFO] 방탈출 예약 가능 시간은" + ReservationTime.startTime() +
+                    "부터 " + ReservationTime.endTime() + "까지입니다.");
             return LocalDate.parse(SCANNER.nextLine());
         } catch (DateTimeParseException e) {
             throw new IllegalArgumentException("[ERROR] 잘못된 형식의 시간입니다.");
@@ -30,13 +42,15 @@ public class InputView {
 
     public Long readId() {
         try {
-            return SCANNER.nextLong();
+            System.out.println("[INFO] 해당 id를 입력해주세요 ex) 1");
+            return Long.valueOf(SCANNER.nextLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 잘못된 형식의 id입니다.");
         }
     }
 
     public String readName() {
+        System.out.println("[INFO] 예약자 성명을 입력해주세요 ex) sudal");
         return SCANNER.nextLine();
     }
 }

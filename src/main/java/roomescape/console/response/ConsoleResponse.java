5c3077adcbc90ac6ae37572771465d5cc148aa12
3juhwan <13selfesteem91@naver.com>
2024-04-27 18:44:16 +0900

    feat(console): 콘솔 응답을 담는 dto 추가

---
+++ src/main/java/roomescape/console/response/ConsoleResponse.java
@@ -0,0 +1,34 @@
+package roomescape.console.response;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ConsoleResponse {
+    private final String content;
+
+    private ConsoleResponse(final String content) {
+        this.content = content;
+    }
+
+    public static ConsoleResponse empty() {
+        return new ConsoleResponse("");
+    }
+
+    public static ConsoleResponse from(List<ReservationTimeResponse> reservationTimeResponses) {
+        String content = reservationTimeResponses.stream()
+                .map(ConsoleResponse::from)
+                .map(reservationTimeResponse -> reservationTimeResponse.content)
+                .collect(Collectors.joining("\n"));
+        return new ConsoleResponse(content);
+    }
+
+    public static ConsoleResponse from(ReservationTimeResponse reservationTimeResponse) {
+        String idInfo = "id - " + reservationTimeResponse.id();
+        String startAtInfo = "startAt - " + reservationTimeResponse.startAt();
+        return new ConsoleResponse(String.join(", ", idInfo, startAtInfo));
+    }
+
+    public String getContent() {
+        return content;
+    }
+}

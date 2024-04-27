fe090a1085ad4738400f69300a59f4ab4181f2ab
tackyu <tack970913@gmail.com>
2024-04-27 15:33:03 +0900

    feat: 시간 관리 dao 추가

---
+++ src/main/java/roomescape/dao/TimeManagementDao.java
@@ -0,0 +1,35 @@
+package roomescape.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.Time;
+
+import javax.sql.DataSource;
+import java.time.LocalTime;
+import java.util.List;
+
+@Repository
+public class TimeManagementDao {
+    private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert simpleJdbcInsert;
+
+    public TimeManagementDao(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert=new SimpleJdbcInsert(dataSource)
+                .withTableName("reservation_time")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    private final RowMapper<Time> timeRowMapper=(resultSet,rowNum)->new Time(
+            resultSet.getLong("id"),
+            LocalTime.parse(resultSet.getString("start_at"))
+    );
+
+    public List<Time> findAll(){
+        String sql="select * from reservation_time";
+        return jdbcTemplate.query(sql,timeRowMapper);
+    }
+
+}

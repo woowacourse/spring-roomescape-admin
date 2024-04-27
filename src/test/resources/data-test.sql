897355f2db1f77e6a23e3f33cf3c3132425e5481
Eugene Jang <kyunellroll@gmail.com>
2024-04-27 14:35:33 +0900

    build(schema-test): 테스트용 데이터베이스, 스키마 정의

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,4 +1,10 @@
-INSERT INTO reservation (name, date, time)
-VALUES ('폴라', '2023-08-05', '15:40'),
-       ('구구', '2023-06-05', '13:40');
+INSERT INTO reservation_time (start_at)
+VALUES ('15:40'),
+       ('13:40'),
+       ('17:40'),
+       ('13:20');
+
+INSERT INTO reservation (name, date, time_id)
+VALUES ('폴라', '2023-08-05', 1),
+       ('구구', '2023-06-05', 2);
 

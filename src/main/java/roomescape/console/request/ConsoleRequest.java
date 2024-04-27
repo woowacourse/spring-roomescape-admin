70c8e9981a59e6b1f681e42da64ae24d1657a95e
3juhwan <13selfesteem91@naver.com>
2024-04-27 16:20:00 +0900

    feat(console): 콘솔 요청을 담는 DTO 추가

---
+++ src/main/java/roomescape/console/request/ConsoleRequest.java
@@ -0,0 +1,32 @@
+package roomescape.console.request;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ConsoleRequest {
+    private final String method;
+    private final String command;
+    private final List<String> contents;
+
+    public ConsoleRequest(String[] inputs) {
+        this(inputs[0], inputs[1], List.of(Arrays.copyOfRange(inputs, 2, inputs.length)));
+    }
+
+    private ConsoleRequest(String method, String command, List<String> contents) {
+        this.method = method;
+        this.command = command;
+        this.contents = contents;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public List<String> getContents() {
+        return contents;
+    }
+}

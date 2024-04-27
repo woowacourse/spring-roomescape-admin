a0271cba7a978b04adfc60d152c8fcec4b59f0c3
J-I-H-O <jeongjiho0731@gmail.com>
2024-04-27 14:11:46 +0900

    refactor: 시간 응답에 DTO를 사용하도록 변경

--- src/main/java/roomescape/dto/reservation/ReservationResponse.java
+++ src/main/java/roomescape/dto/reservation/ReservationResponse.java
@@ -1,14 +1,19 @@
 package roomescape.dto.reservation;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import roomescape.domain.reservation.Reservation;
-import roomescape.domain.time.Time;
+import roomescape.dto.time.TimeResponse;
 
 import java.time.LocalDate;
 
-public record ReservationResponse(Long id, String name, LocalDate date, Time time) {
+public record ReservationResponse(Long id, String name, LocalDate date,
+                                  @JsonProperty("time") TimeResponse timeResponse) {
 
     public static ReservationResponse from(Reservation reservation) {
-        return new ReservationResponse(reservation.getId(), reservation.getName(),
-                reservation.getDate(), reservation.getTime());
+        return new ReservationResponse(
+                reservation.getId(),
+                reservation.getName(),
+                reservation.getDate(),
+                TimeResponse.from(reservation.getTime()));
     }
 }

eab0223439a568e5ddb48c4f2ff247bc52380cbe
3juhwan <13selfesteem91@naver.com>
2024-04-27 15:39:19 +0900

    refactor: 웹과 콘솔 어플리케이션 메인 클래스를 각 패키지로 분리

--- src/main/java/roomescape/console/RoomescapeConsoleApplication.java
+++ src/main/java/roomescape/console/RoomescapeConsoleApplication.java
@@ -1,9 +1,11 @@
-package roomescape;
+package roomescape.console;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"roomescape.console", "roomescape.core"})
 public class RoomescapeConsoleApplication {
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeConsoleApplication.class, args);
--- src/main/java/roomescape/web/RoomescapeWebApplication.java
+++ src/main/java/roomescape/web/RoomescapeWebApplication.java
@@ -1,8 +1,10 @@
-package roomescape;
+package roomescape.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"roomescape.web", "roomescape.core"})
 @SpringBootApplication
 public class RoomescapeWebApplication {
     public static void main(String[] args) {

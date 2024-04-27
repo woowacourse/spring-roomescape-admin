2d9b247a10a7fd8e1f0e603119dcb82e9f32fb35
tackyu <tack970913@gmail.com>
2024-04-27 15:26:24 +0900

    refactor: RequestMapping 추가

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -4,15 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
-    @GetMapping("admin")
+    @GetMapping
     public String admin() {
         return "/admin/index";
     }
 
-    @GetMapping("admin/reservation")
+    @GetMapping("/reservation")
     public String reservation() {
         return "/admin/reservation-legacy";
     }
 
+    @GetMapping("/time")
+    public String manageTime(){
+        return "admin/time";
+    }
 }

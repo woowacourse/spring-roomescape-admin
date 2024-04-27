91ae50dce099bff797a3f81cac6daf4d5e5d5c91
3juhwan <13selfesteem91@naver.com>
2024-04-27 16:20:34 +0900

    feat(console): Command과 CommandMapper 추가

---
+++ src/main/java/roomescape/console/Command.java
@@ -0,0 +1,21 @@
+package roomescape.console;
+
+import java.util.Arrays;
+
+public enum Command {
+    TIMES("times"),
+    RESERVATIONS("reservations");
+
+    private final String value;
+
+    Command(final String value) {
+        this.value = value;
+    }
+
+    public static Command from(String value) {
+        return Arrays.stream(Command.values())
+                .filter(command -> command.value.equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("'" + value + "'는 존재하지 않는 명령어입니다."));
+    }
+}
---
+++ src/main/java/roomescape/console/ControllerMapper.java
@@ -0,0 +1,18 @@
+package roomescape.console;
+
+import roomescape.console.request.ConsoleRequest;
+
+import java.util.Map;
+
+public class ControllerMapper {
+    private final Map<Command, ConsoleController> controllers;
+
+    public ControllerMapper(Map<Command, ConsoleController> controllers) {
+        this.controllers = controllers;
+    }
+
+    public ConsoleController getHandler(ConsoleRequest request) {
+        Command command = Command.from(request.getCommand());
+        return controllers.get(command);
+    }
+}

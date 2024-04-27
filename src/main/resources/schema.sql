301bde3890e977b8eb3d959bcebe49ee83de6940
Eugene Jang <kyunellroll@gmail.com>
2024-04-27 14:36:58 +0900

    build(schema): 데이터베이스 초기 구조 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,8 +3,9 @@ CREATE TABLE IF NOT EXISTS reservation
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     date VARCHAR(255) NOT NULL,
-    time VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    time_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
 );
 
 CREATE TABLE IF NOT EXISTS reservation_time

23b5bc6af09541af60548f339bc1c5eceee394ac
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-04-27 20:14:19 +0900

    feat: inputView 구현

---
+++ src/main/java/roomescape/view/InputView.java
@@ -0,0 +1,33 @@
+package roomescape.view;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public String readOptions() {
+        return SCANNER.nextLine();
+    }
+
+    public LocalTime readTime() {
+        try {
+            return LocalTime.parse(SCANNER.nextLine());
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식의 시간입니다.");
+        }
+    }
+
+    public static LocalDate readDate() {
+        try {
+            return LocalDate.parse(SCANNER.nextLine());
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 형식의 시간입니다.");
+        }    }
+
+    public String readName() {
+        return SCANNER.nextLine();
+    }
+}

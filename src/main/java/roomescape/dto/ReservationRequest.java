7a00bec6c8f82d2634b7b4416393223d814a7eb0
hyun <osh123chm@gmail.com>
2024-04-28 19:49:04 +0900

    fix: 예약 생성 및 도메인 전환 로직 수정

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -6,7 +6,7 @@ import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 
 public record ReservationRequest(String name, String date, Long timeId) {
-    public Reservation toDomain() {
-        return new Reservation(new Name(name), LocalDate.parse(date), new ReservationTime(timeId));
+    public Reservation toDomain(ReservationTime reservationTime) {
+        return new Reservation(new Name(name), LocalDate.parse(date), reservationTime);
     }
 }

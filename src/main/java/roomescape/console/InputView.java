bc68b036b9e4ad2a292b8b10b4fee5447ff87194
HyungHoKim00 <rlagudgh1109@gmail.com>
2024-04-28 14:56:16 +0900

    style: 불필요한 개행 제거

--- src/main/java/roomescape/console/InputView.java
+++ src/main/java/roomescape/console/InputView.java
@@ -15,9 +15,8 @@ public class InputView {
     }
 
     public static ReservationRequest inputReservationRequest() {
-        System.out.println(
-                "input reservation request(name,date,timeId)."
-                        + " (ex - harry" + DELIMITER + "2024-05-11" + DELIMITER + "1)");
+        System.out.println("input reservation request(name,date,timeId)."
+                + " (ex - harry" + DELIMITER + "2024-05-11" + DELIMITER + "1)");
         String input = SCANNER.nextLine();
         String[] split = input.split(DELIMITER);
         if (split.length == 3) {

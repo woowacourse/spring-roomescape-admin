832e9ea67bd78ca285caded73c5bdf08a1a4025a
tackyu <tack970913@gmail.com>
2024-04-29 01:06:02 +0900

    refactor: test용 datasource 설정 추가

--- src/test/java/roomescape/DatabaseTest.java
+++ src/test/java/roomescape/DatabaseTest.java
@@ -21,7 +21,7 @@ public class DatabaseTest {
     void Database_Connection_Test() {
         try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
             assertThat(connection).isNotNull();
-            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getCatalog()).isEqualTo("TEST");
             assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
             assertThat(connection.getMetaData().getTables(null, null, "RESERVATION_TIME", null).next()).isTrue();
         } catch (SQLException e) {

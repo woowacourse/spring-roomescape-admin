016490d86f8a61760fe3b6d70261d6bc671bda5e
jinwoo22 <jenny010925@naver.com>
2024-04-28 15:06:54 +0900

    refactor: @JsonFormat으로 형식지정해서 값 반환

--- src/main/java/roomescape/controller/dto/response/ReservationTimeResponse.java
+++ src/main/java/roomescape/controller/dto/response/ReservationTimeResponse.java
@@ -1,9 +1,13 @@
 package roomescape.controller.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 import roomescape.domain.ReservationTime;
 
-public record ReservationTimeResponse(Long id, LocalTime startAt) {
+public record ReservationTimeResponse(
+        Long id,
+        @JsonFormat(pattern = "HH:mm")
+        LocalTime startAt) {
 
     public static ReservationTimeResponse fromReservationTime(ReservationTime time) {
         return new ReservationTimeResponse(time.getId(), time.getStartAt());

f5b03d5ab953530ee17ddaf4267d6a224eacf86e
lemone <lejinhy@gmail.com>
2024-04-28 14:59:28 +0900

    feat: 기본 시간 데이터 넣기

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,3 +14,6 @@ CREATE TABLE reservation
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id)
 );
+
+INSERT INTO reservation_time (start_at) VALUES ('10:00');
+INSERT INTO reservation_time (start_at) VALUES ('11:00');

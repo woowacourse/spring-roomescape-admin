3778781261cb9a8225e417b8bb304cfae04477b4
jinwoo22 <jenny010925@naver.com>
2024-04-28 15:47:09 +0900

    refactor: 예약시간에 대한 테스트코드 ReservationTimeController로 분리 및 DisplayName 추가

--- src/test/java/roomescape/controller/ReservationTimeControllerTest.java
+++ src/test/java/roomescape/controller/ReservationTimeControllerTest.java
@@ -1,5 +1,7 @@
 package roomescape.controller;
 
+import static org.hamcrest.Matchers.is;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
@@ -11,51 +13,47 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 
-import static org.hamcrest.Matchers.is;
-
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-@Sql("classpath:initReservation.sql")
-class ReservationControllerTest {
+@Sql("classpath:initReservationTime.sql")
+class ReservationTimeControllerTest {
 
-    private final Map<String, Object> reservation = createReservationRequest();
+    private final Map<String, String> times = createReservationTimeRequest();
 
-    @DisplayName("예약을 생성한다.")
+    @DisplayName("예약시간을 생성한다.")
     @Test
-    void postReservation() {
+    void postTime() {
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
-                .body(reservation)
-                .when().post("/reservations")
+                .body(times)
+                .when().post("/times")
                 .then().log().all()
                 .statusCode(200);
     }
 
-    @DisplayName("예약을 전부 조회한다.")
+    @DisplayName("예약시간을 전부 조회한다.")
     @Test
-    void getReservations() {
+    void getReservationTimes() {
         RestAssured.given().log().all()
-                .when().get("/reservations")
+                .when().get("/times")
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
     }
 
-    @DisplayName("예약을 삭제한다.")
+    @DisplayName("예약시간을 삭제한다.")
     @Test
     void delete() {
         RestAssured.given().log().all()
-                .when().delete("/reservations/1")
+                .when().delete("/times/2")
                 .then().log().all()
                 .statusCode(200);
     }
 
-    private Map<String, Object> createReservationRequest() {
-        Map<String, Object> reservation = new HashMap<>();
-        reservation.put("name", "브라운");
-        reservation.put("date", "2023-08-05");
-        reservation.put("timeId", 1);
+    private Map<String, String> createReservationTimeRequest() {
+        Map<String, String> times = new HashMap<>();
+        times.put("startAt", "10:00");
 
-        return reservation;
+        return times;
     }
 }

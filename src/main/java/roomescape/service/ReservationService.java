2f12c710de002fc2950388a5eccf84243d5a0792
reddevilmidzy <midzy0228@gmail.com>
2024-04-28 23:56:28 +0900

    refactor: 서비스의 단건 조회 메서드 제거

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,7 +1,6 @@
 package roomescape.service;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.stereotype.Service;
 import roomescape.dto.CreateReservationRequest;
 import roomescape.dto.ReservationResponse;
@@ -37,12 +36,8 @@ public class ReservationService {
                 .toList();
     }
 
-    public Optional<Reservation> findById(final long id) {
-        return reservationDao.findById(id);
-    }
-
     public void remove(final long id) {
-        final Reservation findReservation = findById(id)
+        final Reservation findReservation = reservationDao.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException(String.format("id: %s는 존재하지 않는 id 입니다.", id)));
         reservationDao.remove(findReservation.getId());
     }

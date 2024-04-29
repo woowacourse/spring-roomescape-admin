6c8931ff5c782e1c7bbcede4489b341bcd123603
kelly6bf <kelly6bf@gmail.com>
2024-04-29 16:42:18 +0900

    test: 불필요한 어노테이션 제거

--- src/test/java/roomescape/repository/ReservationRepositoryTest.java
+++ src/test/java/roomescape/repository/ReservationRepositoryTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 import roomescape.domain.ClientName;
 import roomescape.domain.Reservation;
@@ -17,11 +16,9 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
 @SpringBootTest
-@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 @Sql(value = "/schema.sql", executionPhase = BEFORE_TEST_METHOD)
 class ReservationRepositoryTest {
 

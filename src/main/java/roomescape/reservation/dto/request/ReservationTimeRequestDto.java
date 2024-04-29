9c537f95c74e7e2377616066c030aaa50c5a6d96
PgmJun <chltmdwns96@gmail.com>
2024-04-29 16:30:09 +0900

    feat: Request 시, null 검증 기능 구현

--- src/main/java/roomescape/reservation/dto/request/ReservationTimeRequestDto.java
+++ src/main/java/roomescape/reservation/dto/request/ReservationTimeRequestDto.java
@@ -1,10 +1,12 @@
 package roomescape.reservation.dto.request;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.lang.NonNull;
 
 import java.time.LocalTime;
 
 public record ReservationTimeRequestDto(
         @DateTimeFormat(pattern = "kk:mm")
+        @NonNull
         LocalTime startAt) {
 }

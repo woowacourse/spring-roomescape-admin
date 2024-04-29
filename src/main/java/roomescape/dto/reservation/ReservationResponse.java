372fbb9f061f0a549ece5dc3d0b4fe54be57d098
J-I-H-O <jeongjiho0731@gmail.com>
2024-04-29 12:02:18 +0900

    refactor: 코드 재정렬
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/roomescape/dto/reservation/ReservationResponse.java
+++ src/main/java/roomescape/dto/reservation/ReservationResponse.java
@@ -1,10 +1,9 @@
 package roomescape.dto.reservation;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDate;
 import roomescape.domain.reservation.Reservation;
 import roomescape.dto.time.TimeResponse;
-
-import java.time.LocalDate;
 
 public record ReservationResponse(Long id, String name, LocalDate date,
                                   @JsonProperty("time") TimeResponse timeResponse) {
@@ -14,6 +13,7 @@ public record ReservationResponse(Long id, String name, LocalDate date,
                 reservation.getId(),
                 reservation.getName(),
                 reservation.getDate(),
-                TimeResponse.from(reservation.getTime()));
+                TimeResponse.from(reservation.getTime())
+        );
     }
 }

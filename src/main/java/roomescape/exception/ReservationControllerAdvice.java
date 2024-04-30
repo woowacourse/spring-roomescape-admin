b114b2a828e619389c2b46660aac93db7a1c88bb
fram1998 <koust6u@pusan.ac.kr>
2024-04-30 11:37:58 +0900

    refactor: 불필요한 코드 분리 다시 병합

--- src/main/java/roomescape/exception/ReservationControllerAdvice.java
+++ src/main/java/roomescape/exception/ReservationControllerAdvice.java
@@ -4,13 +4,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.controller.ReservationController;
+import roomescape.controller.ReservationTimeController;
 
-@RestControllerAdvice(assignableTypes = ReservationController.class)
+@RestControllerAdvice(assignableTypes = {ReservationController.class, ReservationTimeController.class})
 public class ReservationControllerAdvice {
 
     @ExceptionHandler(value = IllegalArgumentException.class)
     public ResponseEntity<String> invokeIllegalArgument() {
         return ResponseEntity.notFound()
                 .build();
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> invokeIllegalStatement(final IllegalStateException e) {
+        return ResponseEntity.badRequest()
+                .body(e.getMessage());
     }
 }

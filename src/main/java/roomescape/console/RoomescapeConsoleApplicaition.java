df004131dd7d5128eda3ce7858f5b4d8bece4795
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-05-01 13:58:43 +0900

    refactor: 스프링부트 웹 서버 실행 옵션 설정

--- src/main/java/roomescape/console/RoomescapeConsoleApplicaition.java
+++ src/main/java/roomescape/console/RoomescapeConsoleApplicaition.java
@@ -1,16 +1,26 @@
 package roomescape.console;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import roomescape.console.controller.MainController;
 import roomescape.console.repository.ReservationMemoryRepository;
 import roomescape.console.repository.ReservationTimeMemoryRepository;
 import roomescape.core.service.ReservationService;
 import roomescape.core.service.ReservationTimeService;
 
+@SpringBootApplication(scanBasePackages = {"roomescape.console", "roomescape.core"})
 public class RoomescapeConsoleApplicaition {
     public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(RoomescapeConsoleApplicaition.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
+
         MainController mainController = new MainController(
                 new ReservationService(new ReservationMemoryRepository()),
                 new ReservationTimeService(new ReservationTimeMemoryRepository()));
         mainController.run();
     }
 }
+
+

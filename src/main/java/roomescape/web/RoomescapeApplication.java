0cd6f1bd22acd29fc21bf81ed0d4ad9036a98077
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-05-01 13:49:40 +0900

    refactor: 정의 방법 변경

--- src/main/java/roomescape/web/RoomescapeApplication.java
+++ src/main/java/roomescape/web/RoomescapeApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"roomescape.web", "roomescape.core"})
+@SpringBootApplication(scanBasePackages = {"roomescape.web", "roomescape.core"})
 public class RoomescapeApplication {
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeApplication.class, args);

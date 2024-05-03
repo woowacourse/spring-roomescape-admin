9a34f38a6b31e3ca3f7c307fbfee6a7cd7c06f87
lemone <lejinhy@gmail.com>
2024-05-03 21:24:23 +0900

    refactor: schema.sql의 DML 제거 후 applicationRunner로 초기화 코드 추가

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -1,11 +1,30 @@
 package roomescape;
 
+import java.time.LocalTime;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import roomescape.domain.ReservationTime;
+import roomescape.repository.ReservationTimeRepository;
 
 @SpringBootApplication
 public class RoomescapeApplication {
+    private final ReservationTimeRepository reservationTimeRepository;
+
+    public RoomescapeApplication(ReservationTimeRepository reservationTimeRepository) {
+        this.reservationTimeRepository = reservationTimeRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner initData() {
+        return args -> {
+            System.out.println("init data");
+            reservationTimeRepository.insert(new ReservationTime(LocalTime.of(9, 0)));
+        };
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE reservation_time
 (
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
     start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -10,10 +10,7 @@ CREATE TABLE reservation
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
     date    VARCHAR(255) NOT NULL,
-    time_id    BIGINT,
+    time_id BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id)
 );
-
-INSERT INTO reservation_time (start_at) VALUES ('10:00');
-INSERT INTO reservation_time (start_at) VALUES ('11:00');

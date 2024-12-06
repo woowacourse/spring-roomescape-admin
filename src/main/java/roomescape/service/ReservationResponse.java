21ac04ea981c0a5ca65d4f045b288eb0b782f0c4
devhoya97 <diajbh123@gmail.com>
2024-12-06 23:14:50 +0900

    refactor: 예약 응답시 시간을 분 단위까지만 표현

--- src/main/java/roomescape/service/ReservationResponse.java
+++ src/main/java/roomescape/service/ReservationResponse.java
@@ -2,11 +2,20 @@ package roomescape.service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import roomescape.domain.Reservation;
 
-public record ReservationResponse(long id, String name, LocalDate date, LocalTime time) {
+public record ReservationResponse(long id, String name, LocalDate date, String time) {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
+    public ReservationResponse(long id, String name, LocalDate date, LocalTime time) {
+        this(id, name, date, time.format(FORMATTER));
+    }
 
     public ReservationResponse(Reservation reservation) {
-        this(reservation.getId(), reservation.getName(), reservation.getDate(), reservation.getTime());
+        this(reservation.getId(), reservation.getName(), reservation.getDate(),
+                reservation.getTime().format(FORMATTER));
     }
+
 }

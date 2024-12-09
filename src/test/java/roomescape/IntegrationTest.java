1322f2fbee23d939371c676d037cf3a226af6779
devhoya97 <diajbh123@gmail.com>
2024-12-09 23:08:49 +0900

    test: 통합 테스트 추가

---
+++ src/test/java/roomescape/IntegrationTest.java
@@ -0,0 +1,70 @@
+package roomescape;
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Sql(scripts = "/truncate.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/test_data.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+public class IntegrationTest {
+
+    @Test
+    @DisplayName("관리자 페이지를 응답한다.")
+    void adminPage() {
+        RestAssured.given().log().all()
+                .when().get("/admin")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("관리자 예약 페이지를 응답한다.")
+    void adminReservationPage() {
+        RestAssured.given().log().all()
+                .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("모든 예약을 조회한다.")
+    void findAllReservations() {
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(3));
+    }
+
+    @Test
+    @DisplayName("예약을 추가한다.")
+    void createReservation() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(4));
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(4));
+    }
+}
---
+++ src/test/resources/test_data.sql
@@ -0,0 +1,6 @@
+insert into reservation (name, date, time)
+values ('r1', '2100-12-01', '10:00');
+insert into reservation (name, date, time)
+values ('r2', '2100-12-02', '10:00');
+insert into reservation (name, date, time)
+values ('r3', '2100-12-03', '11:00');

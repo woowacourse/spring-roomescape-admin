f005d291dbc1986cbd50dc08d6e728c0b8a044dc
V4N1LLA <bubble1549@gmail.com>
2024-12-13 17:01:30 +0900

    feat: Reservation 클래스 생성
    - 예약 데이터 모델 클래스

---
+++ src/main/java/roomescape/model/Reservation.java
@@ -0,0 +1,50 @@
+package roomescape.model;
+
+public class Reservation {
+
+    private int id;
+    private String name;
+    private String date;
+    private String time;
+
+    public Reservation(int id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
+

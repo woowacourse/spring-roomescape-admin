b0fcdb8d6d2981fb87829a6b2272e84763e6260b
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-04-15 21:30:11 +0900

    feat: 예약 조회 기능 구현

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,12 +1,25 @@
 package roomescape;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ReservationController {
+
+    private final List<Reservation> reservations = new ArrayList<>();
+
     @GetMapping("/admin/reservation")
-    public String reservation(){
+    public String reservation() {
         return "./admin/reservation-legacy";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> searchReservations() {
+        return ResponseEntity.ok().body(reservations);
     }
 }

62d423f2efdee16bb45d0753037cef2dcdbd37df
yesjuhee <juhee0924@g.skku.edu>
2025-04-15 16:02:41 +0900

    feat: 예약 취소 API 추가

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -4,10 +4,13 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -42,7 +45,6 @@ public class ReservationController {
                 reservationRequest.date(),
                 reservationRequest.time()
         );
-
         reservations.add(reservation);
 
         return ResponseEntity.ok(
@@ -53,5 +55,17 @@ public class ReservationController {
                         reservation.getTime()
                 )
         );
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
+        System.out.println();
+        Reservation reservation = reservations.stream()
+                .filter(value -> Objects.equals(value.getId(), id))
+                .findFirst()
+                .orElseThrow(() ->  new IllegalArgumentException("아이디 없음"));
+        reservations.remove(reservation);
+
+        return ResponseEntity.ok().build();
     }
 }

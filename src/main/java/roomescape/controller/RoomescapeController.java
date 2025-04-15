db2001148e04758ad6db4594132ee427f6f34931
rladmstn <oz33177351@gmail.com>
2025-04-15 18:02:01 +0900

    feat: repository 추가

--- src/main/java/roomescape/controller/RoomescapeController.java
+++ src/main/java/roomescape/controller/RoomescapeController.java
@@ -1,9 +1,6 @@
 package roomescape.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,11 +10,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import roomescape.dto.CreateReservationDto;
 import roomescape.entity.Reservation;
+import roomescape.repository.ReservationRepository;
 
 @Controller
 public class RoomescapeController {
-    private final List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong index = new AtomicLong(1);
+
+    private final ReservationRepository reservationRepository;
+
+    public RoomescapeController(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
 
     @GetMapping("/admin")
     public String admin(){
@@ -31,25 +33,19 @@ public class RoomescapeController {
 
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> getReservations(){
+        List<Reservation> reservations = reservationRepository.findAll();
         return ResponseEntity.ok().body(reservations);
     }
 
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> createReservation(@RequestBody CreateReservationDto createReservationDto) {
-        Reservation reservation = createReservationDto.toEntity(index.getAndIncrement());
-        reservations.add(reservation);
+        Reservation reservation = reservationRepository.add(createReservationDto);
         return ResponseEntity.ok().body(reservation);
     }
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
-        Reservation reservation = reservations.stream()
-                .filter(item -> item.getId().equals(id))
-                .findFirst()
-                .orElseThrow(RuntimeException::new);
-
-        reservations.remove(reservation);
-
+        reservationRepository.deleteById(id);
         return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -0,0 +1,34 @@
+package roomescape.repository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Repository;
+import roomescape.dto.CreateReservationDto;
+import roomescape.entity.Reservation;
+
+@Repository
+public class ReservationRepository {
+    private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(1);
+
+    public List<Reservation> findAll() {
+        return Collections.unmodifiableList(reservations);
+    }
+
+    public Reservation add(CreateReservationDto createReservationDto) {
+        Reservation reservation = createReservationDto.toEntity(index.getAndIncrement());
+        reservations.add(reservation);
+        return reservation;
+    }
+
+    public void deleteById(Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(item -> item.getId().equals(id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+
+        reservations.remove(reservation);
+    }
+}

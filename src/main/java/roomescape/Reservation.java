876a4d1736b8cf450334dd641392f0e2ba2b39ff
ryan-dia <cjfdnjs158@naver.com>
2025-04-15 16:21:33 +0900

    feat: 예약 조회 구현
    Co-authored-by: choidongjun0830 <adong0808@naver.com>

---
+++ src/main/java/roomescape/Reservation.java
@@ -0,0 +1,19 @@
+package roomescape;
+
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+    private Long id;
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
+
+    public Reservation(final Long id, final String name, final LocalDate date, final LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+}

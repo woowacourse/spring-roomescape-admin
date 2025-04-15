1953591dcabcf0a1eb191e1d104ab7e1a1662639
seaniiio <cnc4750@naver.com>
2025-04-15 17:23:47 +0900

    feat: 메인 페이지 경로 수정

--- src/main/java/roomescape/controller/MainController.java
+++ src/main/java/roomescape/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("/admin")
     public String showAdminMainPage() {
         return "/admin/index";
     }

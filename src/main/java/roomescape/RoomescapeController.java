61418cdedcc9d30d458b1f22eaa02396b2a5f26a
ryan-dia <cjfdnjs158@naver.com>
2025-04-15 16:21:33 +0900

    feat: 예약 조회 구현
    Co-authored-by: choidongjun0830 <adong0808@naver.com>

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,13 +1,30 @@
 package roomescape;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class RoomescapeController {
+
+    private final List<Reservation> reservations = new ArrayList<>();
 
     @GetMapping("/admin")
     public String getAdminPage() {
         return "admin/index";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String getReservationPage(Model model) {
+        model.addAttribute("reservations", reservations);
+        return "admin/reservation-legacy";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> getReservations() {
+        return ResponseEntity.ok(reservations);
     }
 }

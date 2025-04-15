34d9174cd8b507386f583f3465a1f7e1aed381a9
juno <wnsgh732@gmail.com>
2025-04-15 16:42:41 +0900

    feat: 웰컴 페이지 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,6 +1,7 @@
 package roomescape;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -8,6 +9,14 @@ import org.springframework.test.annotation.DirtiesContext;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
+    @Test
+    @DisplayName("/ 요청에 응답한다")
+    void welcome_page() {
+        RestAssured.given().log().all()
+            .when().get("/")
+            .then().log().all()
+            .statusCode(200);
+    }
 
     @Test
     void 일단계() {
@@ -16,5 +25,4 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200);
     }
-
 }

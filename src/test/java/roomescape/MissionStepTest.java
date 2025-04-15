22c8191413ede93419a4f693719156b32d93828d
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-04-15 21:10:07 +0900

    feat: 홈 페이지 응답 기능 구현

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -12,7 +12,7 @@ public class MissionStepTest {
     @Test
     void 일단계() {
         RestAssured.given().log().all()
-                .when().get("/")
+                .when().get("/admin")
                 .then().log().all()
                 .statusCode(200);
     }

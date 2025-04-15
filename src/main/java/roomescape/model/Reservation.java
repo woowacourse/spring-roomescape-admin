436e8fd709f9223ad0c1f9319399ff8022d48822
YuyoungRhee <bbclara@naver.com>
2025-04-15 15:47:36 +0900

    feat(domain): Reservation 정의

---
+++ src/main/java/roomescape/model/Reservation.java
@@ -0,0 +1,27 @@
+package roomescape.model;
+
+import java.time.LocalDateTime;
+
+public class Reservation {
+    private final Long id;
+    private final String name;
+    private final LocalDateTime reservationTime;
+
+    public Reservation(Long id, String name, LocalDateTime reservationTime) {
+        this.id = id;
+        this.name = name;
+        this.reservationTime = reservationTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getReservationTime() {
+        return reservationTime;
+    }
+}

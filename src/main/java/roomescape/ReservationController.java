bb62b7ae833f6ad0b07723c9cfb768385f6ff582
yesjuhee <juhee0924@g.skku.edu>
2025-04-15 15:42:29 +0900

    feat: 예약 추가 API 추가

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -4,23 +4,24 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ReservationController {
 
-    private List<Reservation> reservations = new ArrayList<>(List.of(
-            new Reservation(1L, "hotteok", LocalDate.now(), LocalTime.now()),
-            new Reservation(2L, "norang", LocalDate.now(), LocalTime.now()),
-            new Reservation(3L, "neo", LocalDate.now(), LocalTime.now())
-    ));
+    private List<Reservation> reservations = new ArrayList<>();
+
+    private AtomicLong index = new AtomicLong(0);
 
     @GetMapping("/reservations")
-    public ResponseEntity<List<ReservationDto>> getReservations() {
-        List<ReservationDto> reservationDtos = reservations.stream()
-                .map(reservation -> new ReservationDto(
+    public ResponseEntity<List<ReservationResponse>> getReservations() {
+        List<ReservationResponse> reservationResponses = reservations.stream()
+                .map(reservation -> new ReservationResponse(
                         reservation.getId(),
                         reservation.getName(),
                         reservation.getDate(),
@@ -28,6 +29,29 @@ public class ReservationController {
                 ))
                 .toList();
 
-        return ResponseEntity.ok(reservationDtos);
+        return ResponseEntity.ok(reservationResponses);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<ReservationResponse> createReservation(
+            @RequestBody ReservationRequest reservationRequest
+    ) {
+        Reservation reservation = new Reservation(
+                index.incrementAndGet(),
+                reservationRequest.name(),
+                reservationRequest.date(),
+                reservationRequest.time()
+        );
+
+        reservations.add(reservation);
+
+        return ResponseEntity.ok(
+                new ReservationResponse(
+                        reservation.getId(),
+                        reservation.getName(),
+                        reservation.getDate(),
+                        reservation.getTime()
+                )
+        );
     }
 }

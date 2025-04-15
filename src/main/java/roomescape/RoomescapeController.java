db3beadbc8a416d0115d7376c65d63420666ac83
ryan-dia <cjfdnjs158@naver.com>
2025-04-15 16:44:20 +0900

    fix: Controller 어노테이션 변경
    Co-authored-by: choidongjun0830 <adong0808@naver.com>

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class RoomescapeController {
 
     private final List<Reservation> reservations = new ArrayList<>();
@@ -27,11 +28,13 @@ public class RoomescapeController {
         return "admin/reservation-legacy";
     }
 
+    @ResponseBody
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> getReservations() {
         return ResponseEntity.ok(reservations);
     }
 
+    @ResponseBody
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> createReservation(@RequestBody ReservationRequest request) {
         final long reservationId = index.incrementAndGet();

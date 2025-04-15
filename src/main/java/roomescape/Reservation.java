9eaab9ae97ee86c35cadd79f171c81cecbd1c063
sonjh919 <kingjh1125@gmail.com>
2025-04-15 17:07:34 +0900

    feat: 예약 추가 기능 구현

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -1,4 +1,35 @@
 package roomescape;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Reservation {
+
+    private final Long id;
+    private final String name;
+    private final String date;
+    private final String time;
+
+    public Reservation(Long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTime() {
+        return time;
+    }
 }
--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -2,17 +2,23 @@ package roomescape;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ReservationController {
 
     private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
 
     @GetMapping("/admin/reservation")
-    public String reservation(){
+    public String reservation() {
         return "/admin/reservation-legacy.html";
     }
 
@@ -20,5 +26,15 @@ public class ReservationController {
     @ResponseBody
     public List<Reservation> getReservations() {
         return reservations;
+    }
+
+    @PostMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<Reservation> create(@RequestBody Map<String, String> reservationRequest) {
+        Reservation reservation = new Reservation(index.getAndIncrement(), reservationRequest.get("name"),
+                reservationRequest.get("date"), reservationRequest.get("time"));
+
+        reservations.add(reservation);
+        return ResponseEntity.ok().body(reservation);
     }
 }

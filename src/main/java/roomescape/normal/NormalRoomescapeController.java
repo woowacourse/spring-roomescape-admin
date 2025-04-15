542419ad38b9e928642f227bca48c49a007fe0a2
KimDaehyeon <daehyeon3351@gmail.com>
2025-04-15 15:11:54 +0900

    refactor: Controller 를 admin 과 normal 로 분기

--- src/main/java/roomescape/normal/NormalRoomescapeController.java
+++ src/main/java/roomescape/normal/NormalRoomescapeController.java
@@ -1,18 +1,13 @@
-package roomescape;
+package roomescape.normal;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class RoomescapeController {
+public class NormalRoomescapeController {
 
     @GetMapping("/")
     public String getDefaultPage() {
         return "main/index";
-    }
-
-    @GetMapping("/admin")
-    public String getAdminPage() {
-        return "admin/index";
     }
 }

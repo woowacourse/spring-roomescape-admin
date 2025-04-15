0b8f7dbfdb356185ef136b7474a7221750d12c96
KimDaehyeon <daehyeon3351@gmail.com>
2025-04-15 15:54:31 +0900

    feat: 예약 추가, 취소 기능 구현

--- src/main/java/roomescape/controller/normal/NormalRoomescapeController.java
+++ src/main/java/roomescape/controller/normal/NormalRoomescapeController.java
@@ -1,23 +1,23 @@
 package roomescape.controller.normal;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import roomescape.controller.normal.dto.ReservationCreateRequest;
 import roomescape.domain.Reservation;
 
 @Controller
 public class NormalRoomescapeController {
-    private final List<Reservation> reservations = new ArrayList<>();
 
-    public NormalRoomescapeController() {
-        reservations.add(new Reservation(1, "브라운", LocalDate.of(2023,1,1), LocalTime.of(10,0)));
-        reservations.add(new Reservation(2, "브라운", LocalDate.of(2023,1,2), LocalTime.of(11,0)));
-        reservations.add(new Reservation(3, "브라운", LocalDate.of(2023,1,3), LocalTime.of(11,0)));
-    }
+    private final AtomicLong atomicLong = new AtomicLong(1);
+    private final List<Reservation> reservations = new ArrayList<>();
 
     @GetMapping("/")
     public String getDefaultPage() {
@@ -27,5 +27,29 @@ public class NormalRoomescapeController {
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> getReservations() {
         return ResponseEntity.ok(reservations);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> createReservation(@RequestBody ReservationCreateRequest request) {
+        Reservation reservation = new Reservation(
+            atomicLong.getAndIncrement(),
+            request.getName(),
+            request.getDate(),
+            request.getTime()
+        );
+
+        reservations.add(reservation);
+        return ResponseEntity.ok(reservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable long id) {
+        Reservation reservationToDelete = reservations.stream()
+            .filter(reservation -> reservation.getId() == id)
+            .findFirst()
+            .orElseThrow(RuntimeException::new);
+
+        reservations.remove(reservationToDelete);
+        return ResponseEntity.ok().build();
     }
 }

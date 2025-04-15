be805e3c494c671761d06a93b5e0b7d78e620b2b
rladmstn <oz33177351@gmail.com>
2025-04-15 15:32:37 +0900

    feat: 예약 조회 기능 추가

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,12 +1,29 @@
 package roomescape;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RoomescapeController {
+    private List<Reservation> reservations = new ArrayList<>();
+
     @GetMapping("/admin")
     public String admin(){
         return "index";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String adminReservation(Model model){
+        model.addAttribute("reservations", reservations);
+        return "admin/reservation-legacy";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> getReservations(){
+        return ResponseEntity.ok().body(reservations);
     }
 }

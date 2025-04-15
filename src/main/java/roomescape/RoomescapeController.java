63566c82a32b1890195eea7145b59d593b44c06e
ryan-dia <cjfdnjs158@naver.com>
2025-04-15 16:38:54 +0900

    feat: 예약 생성 기능 구현
    Co-authored-by: choidongjun0830 <adong0808@naver.com>

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -2,15 +2,19 @@ package roomescape;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RoomescapeController {
 
     private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong();
 
     @GetMapping("/admin")
     public String getAdminPage() {
@@ -26,5 +30,13 @@ public class RoomescapeController {
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> getReservations() {
         return ResponseEntity.ok(reservations);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> createReservation(@RequestBody ReservationRequest request) {
+        final long reservationId = index.incrementAndGet();
+        final Reservation reservation = new Reservation(reservationId, request.name(), request.date(), request.time());
+        reservations.add(reservation);
+        return ResponseEntity.ok(reservation);
     }
 }

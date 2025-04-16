7bb799cdd54abec3eebf607c058039cf296f448c
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-04-16 11:44:03 +0900

    feat: 예약 조회를 위한 Reservation 객체 구현

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -3,8 +3,9 @@ package roomescape;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +21,9 @@ public class ReservationController {
 
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> searchReservations() {
+        reservations.add(new Reservation(1L,"브라운", LocalDate.of(2023,1,1), LocalTime.of(10,0)));
+        reservations.add(new Reservation(2L,"하루", LocalDate.of(2023,2,1), LocalTime.of(11,0)));
+        reservations.add(new Reservation(3L,"히포", LocalDate.of(2023,3,3), LocalTime.of(13,0)));
         return ResponseEntity.ok().body(reservations);
     }
 }

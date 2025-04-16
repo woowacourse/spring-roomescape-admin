8ec3ebe44760032fde50f03c228f1bf69c09e7af
DongchannN <ldc991104@gmail.com>
2025-04-16 14:40:37 +0900

    refactor: Controller와 저장소 분리

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,9 +1,6 @@
 package roomescape;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,36 +13,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ReservationController {
 
-    private final List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong idx = new AtomicLong(1);
+    private final ReservationRepository reservationRepository;
+
+    public ReservationController(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
 
     @GetMapping("/reservations")
     @ResponseBody
+
     public ResponseEntity<List<Reservation>> getReservations() {
+        List<Reservation> reservations = reservationRepository.findAll();
         return ResponseEntity.ok(reservations);
     }
 
     @PostMapping("/reservations")
     @ResponseBody
     public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
-        Reservation createdReservation = new Reservation(
-                idx.getAndIncrement(),
-                reservation.getName(),
-                reservation.getDate(),
-                reservation.getTime());
-        reservations.add(createdReservation);
-        return ResponseEntity.ok(createdReservation);
+        Reservation saved = reservationRepository.save(reservation);
+        return ResponseEntity.ok(saved);
     }
 
     @DeleteMapping("/reservations/{id}")
     @ResponseBody
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
         try {
-            Reservation foundReservation = reservations.stream()
-                    .filter(reservation -> reservation.getId().equals(id))
-                    .findFirst()
+            Reservation found = reservationRepository.findById(id)
                     .orElseThrow(() -> new IllegalArgumentException("예약을 찾을 수 없습니다."));
-            reservations.remove(foundReservation);
+            reservationRepository.delete(found);
             return ResponseEntity.ok().build();
         } catch (IllegalArgumentException e) {
             return ResponseEntity.notFound().build();

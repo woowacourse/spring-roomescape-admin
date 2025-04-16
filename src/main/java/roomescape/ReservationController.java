688ad13660323b85c3126e7987e4295e55ed9ed5
kjyyjk <rlawnsdud920@gmail.com>
2025-04-16 11:26:11 +0900

    style: 메서드명 수정
    - Controller 클래스 명에 도메인이 명시되어있으므로
    메서드의 도메인 명시 부분을 제거한다

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -10,22 +10,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/reservations")
 public class ReservationController {
     private final AtomicLong index = new AtomicLong(1);
     private List<Reservation> reservations = new ArrayList<>();
 
-    @GetMapping("/reservations")
-    public ResponseEntity<List<ReservationResponse>> getReservations() {
+    @GetMapping
+    public ResponseEntity<List<ReservationResponse>> getAll() {
         List<ReservationResponse> reservationResponses = reservations.stream()
             .map(ReservationResponse::from)
             .toList();
         return ResponseEntity.ok().body(reservationResponses);
     }
 
-    @PostMapping("/reservations")
-    public ResponseEntity<ReservationResponse> createReservation(
+    @PostMapping
+    public ResponseEntity<ReservationResponse> create(
         @RequestBody ReservationRequest request) {
         Reservation reservation = new Reservation(index.getAndIncrement(), request.name(),
             request.date(), request.time());
@@ -33,8 +35,8 @@ public class ReservationController {
         return ResponseEntity.ok().body(ReservationResponse.from(reservation));
     }
 
-    @DeleteMapping("/reservations/{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable(name = "id") Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable(name = "id") Long id) {
         boolean isRemoved = reservations.removeIf(reservation -> reservation.getId() == id);
         if (isRemoved) {
             return ResponseEntity.ok().build();

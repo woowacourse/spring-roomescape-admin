4c8c852793a622cdc28cf5ce5ff1c4a26b4418c0
Yeongjae Kim <sees77770@gmail.com>
2025-04-16 16:42:12 +0900

    refactor: ApiController, VIewController 분리

--- src/main/java/roomescape/ReservationApiController.java
+++ src/main/java/roomescape/ReservationApiController.java
@@ -7,32 +7,27 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class RoomescapeController {
+@RestController
+public class ReservationApiController {
 
     private final List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong atomicLong = new AtomicLong();
-
-    @GetMapping("/admin")
-    public String getAdmin() {
-        return "redirect:/";
-    }
-
-    @GetMapping("/admin/reservation")
-    public String getAdminReservation() {
-        return "admin/reservation-legacy";
-    }
+    private final AtomicLong reservationId = new AtomicLong();
 
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> getReservations() {
         return ResponseEntity.ok(reservations);
+    }
+
+    @GetMapping("/reservations/test")
+    public List<Reservation> getTestReservations() {
+        return reservations;
     }
 
     @PostMapping("/reservations")
@@ -42,7 +37,7 @@ public class RoomescapeController {
         String name = newReservation.get("name");
         LocalTime time = LocalTime.parse(newReservation.get("time"));
 
-        Reservation created = new Reservation(atomicLong.incrementAndGet(), name, date, time);
+        Reservation created = new Reservation(reservationId.incrementAndGet(), name, date, time);
         reservations.add(created);
 
         return ResponseEntity.ok(created);

da66f1220fb28849d5bda8be134678ec32281d69
MinJun Choi <mj043000@naver.com>
2025-04-16 11:13:37 +0900

    refactor:페이지 응답과 API 응답 컨트롤러 분리
    Co-authored-by: eoehd1ek <eoehd2ek@gmail.com>

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -4,41 +4,28 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class RoomescapeController {
+@RestController
+public class ReservationController {
 
     private final Reservations reservations;
     private final AtomicLong index = new AtomicLong(1);
 
-    public RoomescapeController() {
+    public ReservationController() {
         this.reservations = new Reservations(List.of());
     }
 
-    @GetMapping("/admin")
-    public String admin() {
-        return "admin/index.html";
-    }
-
-    @GetMapping("/admin/reservation")
-    public String reservation() {
-        return "admin/reservation-legacy.html";
-    }
-
-    @ResponseBody
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> reservations() {
         return new ResponseEntity<>(reservations.getReservations(), HttpStatus.OK);
     }
 
-    @ResponseBody
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> createReservation(@RequestBody final ReservationDto reservationDto) {
         final Reservation reservation = new Reservation(index.getAndIncrement(), reservationDto.name(),
@@ -48,7 +35,6 @@ public class RoomescapeController {
         return new ResponseEntity<>(reservation, HttpStatus.OK);
     }
 
-    @ResponseBody
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable("id") final Long id) {
         reservations.deleteBy(id);

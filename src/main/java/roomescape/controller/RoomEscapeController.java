61f0005a5a360795fa25becd446e1a150e613386
ppparkta <pppppark2@gmail.com>
2025-04-16 15:02:06 +0900

    feat: 예약 삭제 기능 구현

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -3,7 +3,9 @@ package roomescape.controller;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -43,5 +45,15 @@ public class RoomEscapeController {
         Reservation reservation = roomEscapeService.convertReservation(reservationDto);
         reservations.add(reservation);
         return reservation;
+    }
+
+    @DeleteMapping("reservations/{reservationId}")
+    @ResponseBody
+    public void delete(@PathVariable("reservationId") Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(v -> v.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 예약 번호입니다:" + id));
+        reservations.remove(reservation);
     }
 }

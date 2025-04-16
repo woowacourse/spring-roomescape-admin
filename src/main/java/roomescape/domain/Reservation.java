dffcac8a51add461698ba66a039f1d751975ec7e
CheChe903 <cheche903@naver.com>
2025-04-16 13:43:19 +0900

    remove(Reservation): 미사용 메서드 삭제

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -10,21 +10,11 @@ public class Reservation {
     private final LocalDate date;
     private final LocalTime time;
 
-    public Reservation(final String name, final LocalDate date, final LocalTime time) {
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
-
     public Reservation(final Long id, final String name, final LocalDate date, final LocalTime time) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
-    }
-
-    public static Reservation toEntity(Reservation reservation, Long id) {
-        return new Reservation(id, reservation.name, reservation.date, reservation.time);
     }
 
     public Long getId() {

5acf30737ef600bc435fc585828bb2d36973a7a7
DongchannN <ldc991104@gmail.com>
2025-04-16 14:12:03 +0900

    feat: 홈 화면 반환

---
+++ src/main/java/roomescape/AdminController.java
@@ -0,0 +1,15 @@
+package roomescape;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin")
+@Controller
+public class AdminController {
+
+    @GetMapping
+    public String getAdminPage() {
+        return "admin/index";
+    }
+}

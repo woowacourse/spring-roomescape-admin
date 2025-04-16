696014770f7b06894befb264b415a1d565cdd94d
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-04-16 15:55:48 +0900

    feat: 예약 추가 및 삭제 기능 추가

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,28 +2,44 @@ package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import roomescape.Reservation;
-import roomescape.ReservationResponse;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import roomescape.dto.ReservationRequest;
+import roomescape.dto.ReservationResponse;
+import roomescape.service.ReservationService;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ReservationController {
 
-    private final List<Reservation> reservations = new ArrayList<>(
-            List.of(
-                    new Reservation(1, "강산", LocalDate.now(), LocalTime.now()),
-                    new Reservation(2, "저스틴", LocalDate.now(), LocalTime.now()),
-                    new Reservation(3, "네오", LocalDate.now(), LocalTime.now())
-            )
-    );
+    private final ReservationService reservationService;
+
+    public ReservationController(final ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
 
     @GetMapping("/reservations")
     public ResponseEntity<List<ReservationResponse>> getReservations() {
-        return ResponseEntity.ok(ReservationResponse.from(reservations));
+        List<ReservationResponse> reservations = reservationService.getReservations();
+        System.out.println("조회");
+        return ResponseEntity.ok(reservations);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<ReservationResponse> createReservation(@RequestBody ReservationRequest reservationRequest) {
+        ReservationResponse reservationResponse = reservationService.createReservation(reservationRequest);
+        System.out.println("삽입");
+        return ResponseEntity.ok(reservationResponse);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<String> deleteReservation(@PathVariable long id) {
+        reservationService.delete(id);
+        System.out.println("삭제");
+        return ResponseEntity.ok(null);
     }
 }

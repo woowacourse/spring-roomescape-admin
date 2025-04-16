1ed58bda23bd478a49ab20ee877d8ac8c42e3428
juno <wnsgh732@gmail.com>
2025-04-16 11:04:00 +0900

    chore: 시간 형식 변경

---
+++ src/main/java/roomescape/ReservationResponse.java
@@ -0,0 +1,13 @@
+package roomescape;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record ReservationResponse(long id, String name, LocalDate date,
+                                  @JsonFormat(pattern = "HH:mm") LocalTime time) {
+    public static ReservationResponse from(Reservation reservation) {
+        return new ReservationResponse(reservation.getId(), reservation.getName(),
+            reservation.getDate(), reservation.getTime());
+    }
+}

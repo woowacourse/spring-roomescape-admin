9397d64adf415044108604fec297c5182942ea68
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-04-16 11:33:11 +0900

    refactor: 컨트롤러 분리
    - 페이지 반환 책임의 PageController 분리

---
+++ src/main/java/roomescape/PageController.java
@@ -0,0 +1,17 @@
+package roomescape;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/admin")
+    public String getMainPage() {
+        return "admin/index";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String getReservationPage() {
+        return "admin/reservation-legacy";
+    }
+}

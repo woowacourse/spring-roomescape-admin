de58f958f955194c38c4261fc43afd0bf99fd74a
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-04-16 14:59:17 +0900

    refactor(controller) : 예약 추가/취소 기능 리팩터링
    - post : 생성자 오버로딩 활용

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -1,19 +1,27 @@
 package roomescape;
 
 public class Reservation {
-    private int id;
+    private Long id;
     private String name;
     private String date;
     private String time;
 
-    public Reservation(int id, String name, String date, String time) {
+    public Reservation() {}
+
+    public Reservation(Long id, String name, String date, String time) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
     }
 
-    public int getId() {
+    public Reservation(String name, String date, String time) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Long getId() {
         return id;
     }
     public String getName() {
--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -2,15 +2,22 @@ package roomescape;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class RoomescapeController {
 
     private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
 
     @GetMapping("/admin")
     public String admin() {
@@ -23,8 +30,23 @@ public class RoomescapeController {
     }
 
     @GetMapping("/reservations")
-    @ResponseBody
-    public ResponseEntity<List<Reservation>> adminReservations() {
+    public ResponseEntity<List<Reservation>> reservations() {
         return ResponseEntity.ok(reservations);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) {
+        Reservation newReservation = new Reservation(index.getAndIncrement(), reservation.getName(), reservation.getDate(),
+                reservation.getTime());
+        reservations.add(newReservation);
+        return ResponseEntity.ok().body(newReservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Reservation> deleteReservation(@PathVariable long id) {
+        Reservation reservation1 = reservations.stream().filter(reservation -> reservation.getId() == id).findFirst()
+                .orElseThrow(() ->new IllegalArgumentException("해당 ID 없음"));
+        reservations.remove(reservation1);
+        return ResponseEntity.ok().body(reservation1);
     }
 }

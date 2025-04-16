1b7b78bf7d8714ad53a0211f812b1153748d877b
junhaa <2171326@hansung.ac.kr>
2025-04-16 16:58:06 +0900

    feat: 날짜, 시간 형식에 맞게 직렬화하도록 변경

--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -1,9 +1,10 @@
 package roomescape.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public record Reservation(Long id, String name, LocalDate date, LocalTime time) {
+public record Reservation(Long id, String name, @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul") LocalDate date, @JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul") LocalTime time) {
 
     public static Reservation injectId(Reservation reservation, Long id){
         return new Reservation(id, reservation.name, reservation.date, reservation.time);

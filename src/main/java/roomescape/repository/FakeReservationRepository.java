996941f6b5e3106dfe8092f801c32a4e18ecb2c9
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-04-16 15:54:23 +0900

    feat: 예약 리포지토리 추상체, 구현체 추가
    - 전체 조회 기능
    - 저장 기능
    - 삭제 기능

---
+++ src/main/java/roomescape/repository/FakeReservationRepository.java
@@ -0,0 +1,39 @@
+package roomescape.repository;
+
+import org.springframework.stereotype.Repository;
+import roomescape.entity.ReservationEntity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class FakeReservationRepository implements ReservationRepository {
+
+    private static final int START_ID_NUMBER = 1;
+
+    private final AtomicLong idGenerator = new AtomicLong(START_ID_NUMBER);
+
+    private final Map<Long, ReservationEntity> reservations = new HashMap();
+
+    @Override
+    public List<ReservationEntity> findAll() {
+        return reservations.values()
+                .stream()
+                .toList();
+    }
+
+    @Override
+    public ReservationEntity save(final ReservationEntity reservationEntity) {
+        reservationEntity.setId(idGenerator.getAndIncrement());
+        reservations.put(reservationEntity.getId(), reservationEntity);
+        return reservationEntity;
+    }
+
+    @Override
+    public void delete(final long id) {
+        reservations.remove(id);
+    }
+}
---
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -0,0 +1,14 @@
+package roomescape.repository;
+
+import roomescape.entity.ReservationEntity;
+
+import java.util.List;
+
+public interface ReservationRepository {
+
+    List<ReservationEntity> findAll();
+
+    ReservationEntity save(ReservationEntity reservationEntity);
+
+    void delete(long id);
+}

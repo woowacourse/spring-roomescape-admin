baf843d2303581aefc2a436cdb2ecee204e5fa07
changuii <rhljh7410@gmail.com>
2025-04-16 15:00:53 +0900

    test: 예약 api들을 분리해서 테스트 작성
    - 생성, 조회, 삭제 기능들을 각각 분리해서 호출 테스트

--- src/test/java/roomescape/reservation/ReservationControllerTest.java
+++ src/test/java/roomescape/reservation/ReservationControllerTest.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,15 @@ import org.springframework.test.annotation.DirtiesContext;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ReservationControllerTest {
+
+    private static final Map<String, String> RESERVATION_BODY = new HashMap<>();
+
+    @BeforeAll
+    static void initParams(){
+        RESERVATION_BODY.put("name", "브라운");
+        RESERVATION_BODY.put("date", "2023-08-05");
+        RESERVATION_BODY.put("time", "15:40");
+    }
 
     @DisplayName("/reservations로 요청이 들어오면 예약 정보들을 응답한다.")
     @Test
@@ -25,17 +35,34 @@ public class ReservationControllerTest {
                 .body("size()", is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
     }
 
-    @DisplayName("예약을 생성하고 삭제한다.")
+    @DisplayName("예약을 생성하고, 200 OK를 응답")
     @Test
-    void 삼단계() {
-        final Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
-
+    void post() {
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
-                .body(params)
+                .body(RESERVATION_BODY)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+    }
+
+    @DisplayName("예약이 존재하지 않는다면 200 OK와 빈 컬렉션 응답")
+    @Test
+    void get() {
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @DisplayName("존재하는 모든 예약과 200 OK를 응답")
+    @Test
+    void get1() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(RESERVATION_BODY)
                 .when().post("/reservations")
                 .then().log().all()
                 .statusCode(200)
@@ -46,22 +73,28 @@ public class ReservationControllerTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
+    }
+
+    @DisplayName("주어진 아이디에 해당하는 예약이 있다면 200 OK 응답")
+    @Test
+    void remove1() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(RESERVATION_BODY)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
 
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
                 .statusCode(200);
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
     }
 
     @DisplayName("주어진 아이디에 해당하는 예약이 없다면 404로 응답한다.")
     @Test
-    void Naming_Test() {
+    void remove2() {
         RestAssured.given().log().all()
                 .when().delete("/reservations/3")
                 .then().log().all()

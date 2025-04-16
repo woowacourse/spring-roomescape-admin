6442df9fbdd66ef1b4b284c052ecaf998a8a2db9
Yehyeok Bang <qkddpgur318@gmail.com>
2025-04-16 14:24:36 +0900

    feat: 관리자 홈 화면 구현
    Co-authored-by: saera-yook <yook.saera@gmail.com>

--- src/test/java/roomescape/AdminControllerTest.java
+++ src/test/java/roomescape/AdminControllerTest.java
@@ -1,5 +1,7 @@
 package roomescape;
 
+import static org.hamcrest.Matchers.containsString;
+
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,14 +10,17 @@ import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+class AdminControllerTest {
 
-    @DisplayName("1단계 - 관리자 홈 화면 응답 성공")
+    @DisplayName("관리자 홈 화면 핵심 태그 존재 여부를 확인한다.")
     @Test
-    void step1() {
+    void containsCoreTagsInAdmin() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body(containsString("<title>방탈출 어드민</title>"))
+                .body(containsString("<a class=\"nav-link\" href=\"/reservation\">Reservation</a>"))
+                .body(containsString("<a class=\"nav-link\" href=\"/time\">Time</a>"));
     }
 }

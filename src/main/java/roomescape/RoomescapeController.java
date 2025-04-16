97508af9679d1f46c06aba536ee6d9a07b96e611
ppparkta <pppppark2@gmail.com>
2025-04-16 14:09:58 +0900

    feat: reservation 조회 기능 구현

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,12 +1,33 @@
 package roomescape;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RoomescapeController {
+
+    private List<Reservation> reservations = new ArrayList<>();
+
     @GetMapping("/admin")
     public String home() {
         return "/admin/index";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String reservation() {
+        return "/admin/reservation-legacy";
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> readReservations() {
+        Reservation reservation = new Reservation(1L, "브라운", LocalDate.of(2023, 1, 1), LocalTime.of(11, 10, 10));
+        reservations.add(reservation);
+        return reservations;
     }
 }

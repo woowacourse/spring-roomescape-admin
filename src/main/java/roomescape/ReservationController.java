07ef4a06480c1ad9e506b29fea611037ccdff395
egaeng09 <egaeng09@gmail.com>
2025-04-16 14:59:21 +0900

    feat(ReservationController, ReservationDto): 예약 추가 및 취소 기능 구현

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,20 +1,23 @@
 package roomescape;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ReservationController {
 
-    private List<Reservation> reservations = List.of(
-            new Reservation(1, "브라운", LocalDate.of(2023, 1, 1), LocalTime.of(10, 0)),
-            new Reservation(2, "브라운", LocalDate.of(2023, 1, 2), LocalTime.of(11, 0))
-    );
+    private final AtomicLong index = new AtomicLong(1L);
+    private List<Reservation> reservations = new ArrayList<>();
 
     @GetMapping("/admin/reservation")
     public String getReservationPage(Model model) {
@@ -27,4 +30,23 @@ public class ReservationController {
         return reservations;
     }
 
+
+    @PostMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<Reservation> addReservation(@RequestBody ReservationDto reservationDto) {
+        Reservation reservation = new Reservation(index.getAndIncrement(), reservationDto.name(), reservationDto.date(),
+                reservationDto.time());
+        reservations.add(reservation);
+        return ResponseEntity.ok(reservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> getReservations(@PathVariable("id") long id) {
+        boolean isDeleted = reservations.removeIf((reservation) -> reservation.getId() == id);
+        if (isDeleted) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
---
+++ src/main/java/roomescape/ReservationDto.java
@@ -0,0 +1,14 @@
+package roomescape;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record ReservationDto(
+        String name,
+        LocalDate date,
+        LocalTime time
+) {
+    public Reservation toEntity(long id) {
+        return new Reservation(id, name, date, time);
+    }
+}

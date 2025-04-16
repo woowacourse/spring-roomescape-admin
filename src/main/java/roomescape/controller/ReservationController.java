452d710ec9a999a0a3b156311be7c53d68767621
kimdev <kim__dev@hufs.ac.kr>
2025-04-16 11:13:22 +0900

    refactor: 메서드명 및 일부 코드 컨벤션 리팩토링

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -4,11 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import roomescape.dto.CreateReservationDto;
 import roomescape.model.Reservation;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
@@ -18,38 +19,38 @@ public class ReservationController {
     private final AtomicLong index = new AtomicLong(1);
 
     @GetMapping("/admin")
-    public String home() {
+    public String getAdminHomepage() {
         return "admin/index.html";
     }
 
     @GetMapping("/admin/reservation")
-    public String reservationPage() {
+    public String getAdminReservationPage() {
         return "admin/reservation-legacy.html";
     }
 
     @GetMapping("/reservations")
     @ResponseBody
     public ResponseEntity<List<Reservation>> getAllReservations() {
-        return ResponseEntity.ok()
+        return ResponseEntity
+                .status(HttpStatus.OK)
                 .body(reservations);
     }
 
     @PostMapping("/reservations")
     @ResponseBody
-    public ResponseEntity<Reservation> addReservation(@RequestBody CreateReservationDto reservationDto){
-        Reservation reservation = new Reservation(index.getAndIncrement(), reservationDto.name(),reservationDto.date(),reservationDto.time());
+    public ResponseEntity<Reservation> addReservation(@RequestBody CreateReservationDto reservationDto) {
+        Reservation reservation = new Reservation(index.getAndIncrement(), reservationDto.name(), reservationDto.date(), reservationDto.time());
         reservations.add(reservation);
-        return ResponseEntity.ok().body(reservation);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(reservation);
     }
 
     @DeleteMapping("/reservations/{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id){
-        Reservation deleteReservation = reservations.stream()
-                .filter(reservation -> reservation.id() == id)
-                .findAny()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        reservations.remove(deleteReservation);
-
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
+        reservations.removeIf(reservation -> Objects.equals(reservation.id(), id));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .build();
     }
 }
--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -4,5 +4,4 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record Reservation(Long id, String name, LocalDate date, LocalTime time) {
-
 }

1f95d36a28f2a0ad7a09ed68a80aff5c35ae710b
ryan-dia <cjfdnjs158@naver.com>
2025-04-16 14:59:34 +0900

    refactor: 불필요한 어노테이션 제거
    Co-authored-by: choidongjun0830 <adong0808@naver.com>

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RoomescapeController {
@@ -31,13 +30,11 @@ public class RoomescapeController {
         return "admin/reservation-legacy";
     }
 
-    @ResponseBody
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> getReservations() {
         return ResponseEntity.ok(reservations);
     }
 
-    @ResponseBody
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> createReservation(@RequestBody ReservationRequest request) {
         final long reservationId = index.incrementAndGet();
@@ -46,7 +43,6 @@ public class RoomescapeController {
         return ResponseEntity.ok(reservation);
     }
 
-    @ResponseBody
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
         final Reservation reservation = reservations.stream()

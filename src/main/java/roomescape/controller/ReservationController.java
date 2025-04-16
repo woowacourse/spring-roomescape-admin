3364b51fb22e4a224b70f07228f32218c34a7ef9
황재웅 <wodnd0131@naver.com>
2025-04-16 14:24:13 +0900

    faet: admin으로 요청시 메인 페이지 응답

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,14 @@
+package roomescape.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReservationController {
+
+    @GetMapping("/admin")
+    public String getMainPage() {
+        return "admin/index";
+    }
+
+}
--- src/test/java/roomescape/ReservationControllerTest.java
+++ src/test/java/roomescape/ReservationControllerTest.java
@@ -1,18 +1,20 @@
 package roomescape;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+public class ReservationControllerTest {
 
     @Test
-    void 일단계() {
+    @DisplayName("/admin으로 요청시 메인 페이지 응답")
+    void getMainPage() {
         RestAssured.given().log().all()
-                .when().get("/")
+                .when().get("/admin")
                 .then().log().all()
                 .statusCode(200);
     }

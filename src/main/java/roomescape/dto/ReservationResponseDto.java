a032fa951a85992cbda93bb9e216658f62d93262
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-04-16 14:28:58 +0900

    fix: 예약 시간 표기 오류 수정 (format 형식 변경으로 해결)

--- src/main/java/roomescape/dto/ReservationResponseDto.java
+++ src/main/java/roomescape/dto/ReservationResponseDto.java
@@ -4,12 +4,13 @@ import java.time.format.DateTimeFormatter;
 import roomescape.domain.Reservation;
 
 public record ReservationResponseDto(long id, String name, String date, String time) {
-    public static ReservationResponseDto from(Reservation reservation){
+
+    public static ReservationResponseDto from(Reservation reservation) {
         return new ReservationResponseDto(
             reservation.getId(),
             reservation.getPerson().getName(),
             reservation.getReservationTime().getDate().toString(),
-            reservation.getReservationTime().getTime().format(DateTimeFormatter.ofPattern("mm:ss"))
+            reservation.getReservationTime().getTime().format(DateTimeFormatter.ofPattern("HH:mm"))
         );
     }
 }

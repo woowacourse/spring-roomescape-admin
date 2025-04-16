d3eb5af7f5f87f1c7ff604b79be3027ed1c97052
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-04-16 15:51:17 +0900

    feat: DB 엔티티 추가

---
+++ src/main/java/roomescape/entity/ReservationEntity.java
@@ -0,0 +1,60 @@
+package roomescape.entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ReservationEntity {
+    private long id;
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
+
+    public ReservationEntity(final long id, final String name, final LocalDate date, final LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public ReservationEntity(final String name, final LocalDate date, final LocalTime time) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public static ReservationEntity createWithoutId(String name, LocalDate date, LocalTime time){
+        return new ReservationEntity(name,date,time);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setId(final long id) {
+        this.id = id;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public void setDate(final LocalDate date) {
+        this.date = date;
+    }
+
+    public void setTime(final LocalTime time) {
+        this.time = time;
+    }
+}

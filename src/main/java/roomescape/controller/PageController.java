d35d70403221e6354f95df6bb113870582c71b21
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-04-16 12:01:46 +0900

    feat: 웰컴페이지 접속 시 /admin으로 리다이렉트 처리

--- src/main/java/roomescape/controller/PageController.java
+++ src/main/java/roomescape/controller/PageController.java
@@ -1,10 +1,21 @@
 package roomescape.controller;
 
+import java.net.URI;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    @GetMapping("/")
+    public ResponseEntity<Void> getWelcomePage() {
+        HttpHeaders header = new HttpHeaders();
+        header.setLocation(URI.create("/admin"));
+        return new ResponseEntity<>(header, HttpStatus.PERMANENT_REDIRECT);
+    }
+
     @GetMapping("/admin")
     public String getMainPage() {
         return "admin/index";

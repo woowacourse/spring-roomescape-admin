8ac56fdf688d25d5172d08d041bb95ab6291aac2
MinJun Choi <mj043000@naver.com>
2025-04-16 17:29:55 +0900

    refactor: dto class에서 record로 변경
    Co-authored-by: eoehd1ek <eoehd2ek@gmail.com>

--- src/main/java/roomescape/dto/ReservationRequestDto.java
+++ src/main/java/roomescape/dto/ReservationRequestDto.java
@@ -4,31 +4,9 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import roomescape.domain.Reservation;
 
-public class ReservationRequestDto {
-
-    private final String name;
-    private final LocalDate date;
-    private final LocalTime time;
-
-    public ReservationRequestDto(String name, LocalDate date, LocalTime time) {
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
+public record ReservationRequestDto(String name, LocalDate date, LocalTime time) {
 
     public Reservation toEntity(final long id) {
         return new Reservation(id, name, date, time);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getTime() {
-        return time;
     }
 }

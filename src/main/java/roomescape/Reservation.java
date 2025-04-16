76f9edde52f1ff86e232ad950b8106a2eba42f61
BWY <quantumjump10@gmail.com>
2025-04-16 14:14:37 +0900

    feat: Reservation 필드 검증 기능 추가

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -11,6 +11,9 @@ public class Reservation {
     private LocalTime time;
 
     public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+        validateName(name);
+        validateDate(date);
+        validateTime(time);
         this.id = id;
         this.name = name;
         this.date = date;
@@ -35,5 +38,23 @@ public class Reservation {
 
     public LocalTime getTime() {
         return time;
+    }
+
+    private void validateName(String value) {
+        if (value.isBlank()) {
+            throw new IllegalArgumentException("이름을 입력해주세요.");
+        }
+    }
+
+    private void validateDate(LocalDate date) {
+        if (date == null) {
+            throw new IllegalArgumentException("날짜를 입력해주세요.");
+        }
+    }
+
+    private void validateTime(LocalTime time) {
+        if (time == null) {
+            throw new IllegalArgumentException("시간을 입력해주세요.");
+        }
     }
 }

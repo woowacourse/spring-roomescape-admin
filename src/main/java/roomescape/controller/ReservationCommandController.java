32b6fe74f3811a42e7489686d7760c9b6ced30cb
ppparkta <pppppark2@gmail.com>
2025-04-17 15:07:02 +0900

    chore: 불필요한 import 제거

--- src/main/java/roomescape/controller/ReservationCommandController.java
+++ src/main/java/roomescape/controller/ReservationCommandController.java
@@ -3,7 +3,6 @@ package roomescape.controller;
 import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;

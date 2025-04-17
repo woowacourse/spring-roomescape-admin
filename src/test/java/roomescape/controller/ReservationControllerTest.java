2b83f4e9f726f333361710724dc6ab06710fb292
황재웅 <wodnd0131@naver.com>
2025-04-17 17:59:57 +0900

    test: RestAssuredMockMvc 추가 및 테스트

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -1,82 +1,56 @@
 package roomescape.controller;
 
 import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import java.util.HashMap;
-import java.util.Map;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import roomescape.entity.Reservation;
+import roomescape.repository.ReservationRepository;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@WebMvcTest(ReservationController.class)
 public class ReservationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ReservationRepository reservationRepository;
+
+    @BeforeEach
+    void setUp() {
+        RestAssuredMockMvc.mockMvc(mockMvc);
+    }
 
     @Test
     @DisplayName("/admin/reservation 요청 시 예약 관리 페이지 응답")
     void readReservation() {
-        RestAssured.given().log().all()
+        Reservation reservation1 = new Reservation(1L, "브라운", LocalDate.now().plusDays(1), LocalTime.now());
+        Reservation reservation2 = new Reservation(2L, "네오", LocalDate.now().plusDays(1), LocalTime.now());
+
+        List<Reservation> reservations = List.of(
+                reservation1, reservation2
+        );
+
+        given(reservationRepository.findAll()).willReturn(reservations);
+
+        RestAssuredMockMvc.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(0));
-    }
-
-    @Test
-    @DisplayName("예약 관리 페이지 내에서 예약 추가")
-    void postReservation() {
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(getTestParamsWithReservation())
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("name", is("브라운"));
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
-    }
-
-    @Test
-    @DisplayName("예약 관리 페이지 내에서 예약 삭제")
-    void deleteReservation() {
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(getTestParamsWithReservation())
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("id", is(1));
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
-
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
-                .then().log().all()
-                .statusCode(200);
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
-    }
-
-    private Map<String, String> getTestParamsWithReservation() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
-        return params;
+                .body("size()", is(2))
+                .body("[0].id", is(1))
+                .body("[0].name", is("브라운"))
+                .body("[1].id", is(2))
+                .body("[1].name", is("네오"));
     }
 }

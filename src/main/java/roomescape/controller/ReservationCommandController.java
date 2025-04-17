bee250c479ff34ff014a104e41a7cf6421c0f27a
ppparkta <pppppark2@gmail.com>
2025-04-17 14:56:11 +0900

    refactor: 대문자 수정

--- src/main/java/roomescape/controller/ReservationCommandController.java
+++ src/main/java/roomescape/controller/ReservationCommandController.java
@@ -3,6 +3,7 @@ package roomescape.controller;
 import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,6 +33,6 @@ public class ReservationCommandController {
 
     @DeleteMapping("reservations/{reservationId}")
     public void delete(@PathVariable("reservationId") Long id) {
-       reservationService.delete(id);
+        reservationService.delete(id);
     }
 }

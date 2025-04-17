78c8e9dcb2c7dfc8ba4daff164eecd778a055268
supernovaMK <minkikim2002@gmail.com>
2025-04-17 19:11:17 +0900

    feat: 예약 삭제 예외 처리 관련 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,9 +1,7 @@
 package roomescape.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,34 +9,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.dto.ReservationRequest;
+import roomescape.dto.ReservationResponse;
 import roomescape.model.Reservation;
+import roomescape.model.Reservations;
 
 @RequestMapping("/reservations")
 @RestController
 public class ReservationController {
 
-    private final List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong atomicLong = new AtomicLong();
+    private final Reservations reservations = new Reservations();
 
-    @GetMapping("")
-    public List<Reservation> reservations() {
-        return reservations;
+    @GetMapping
+    public List<ReservationResponse> reservations() {
+        return reservations.getReservations();
     }
 
-    @PostMapping("")
-    public Reservation addReservation(@RequestBody Reservation input) {
-        Reservation reservation = Reservation.injectId(input, atomicLong.incrementAndGet());
-        reservations.add(reservation);
+    @PostMapping
+    public ReservationResponse addReservation(@RequestBody ReservationRequest request) {
+        Reservation reservation = request.toEntity();
 
-        return reservation;
+        return reservations.createReservation(reservation);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteReservation(@PathVariable("id") Long id) {
-        Reservation targetReservation = reservations.stream()
-                .filter(reservation -> Objects.equals(reservation.id(), id))
-                .findAny()
-                .orElseThrow(RuntimeException::new);
-        reservations.remove(targetReservation);
+    public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
+        try {
+            reservations.deleteReservation(id);
+            return ResponseEntity.ok().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }

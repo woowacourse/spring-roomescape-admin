acce4c2e91b0239ab4f0129dfd27a899817b847c
RIWON <121172065+awrion3@users.noreply.github.com>
2025-04-17 15:20:40 +0900

    refactor: use require non null for reservation parameter

--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -2,8 +2,16 @@ package roomescape.model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Objects;
 
 public record Reservation(long id, String name, LocalDate date, LocalTime time) {
+    private static final String INVALID_INPUT = "[ERROR] 널 값은 입력될 수 없습니다.";
+
+    public Reservation {
+        Objects.requireNonNull(name, INVALID_INPUT);
+        Objects.requireNonNull(date, INVALID_INPUT);
+        Objects.requireNonNull(time, INVALID_INPUT);
+    }
 
     public Reservation copyWithId(long id) {
         return new Reservation(id, name, date, time);

9c4b198827137feed71c83598ba21ce1f8eaad01
mimi <110226866+hongkikii@users.noreply.github.com>
2025-04-17 15:05:43 +0900

    refactor: 클래스별 책임 분리 및 reservation 패키지 생성

--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -1,20 +1,10 @@
 package roomescape;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class RoomescapeController {
-    private final List<Reservation> reservations;
-
-    public RoomescapeController() {
-        this.reservations = new ArrayList<>();
-    }
-
     @GetMapping("/admin")
     public String showAdminWelcome() {
         return "admin/index";
@@ -23,30 +13,5 @@ public class RoomescapeController {
     @GetMapping("/admin/reservation")
     public String showAllReservation() {
         return "admin/reservation-legacy";
-    }
-
-    @GetMapping("/reservations")
-    public ResponseEntity<List<Reservation>> getAllReservation() {
-        return ResponseEntity.ok().body(reservations);
-    }
-
-    @PostMapping("/reservations")
-    public ResponseEntity<Reservation> createReservation(@RequestBody ReservationDto reservationDto) {
-        Reservation newReservation = new Reservation(
-                reservationDto.name(),
-                reservationDto.date(),
-                reservationDto.time());
-        reservations.add(newReservation);
-        return ResponseEntity.ok(newReservation);
-    }
-
-    @DeleteMapping("/reservations/{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
-        Reservation findReservation = reservations.stream()
-                .filter(reservation -> reservation.hasSame(id))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 id 입니다."));
-        reservations.remove(findReservation);
-        return ResponseEntity.ok().build();
     }
 }

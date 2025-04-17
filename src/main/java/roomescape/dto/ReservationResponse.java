2bd3b2db251e501c23ca83944543a2cf83adf84c
junhaa <2171326@hansung.ac.kr>
2025-04-17 15:19:17 +0900

    refactor: 요청, 응답 관련 DTO 추가

---
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -0,0 +1,12 @@
+package roomescape.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import roomescape.model.Reservation;
+
+public record ReservationResponse(Long id, String name, @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul") LocalDate date, @JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul") LocalTime time){
+    public static ReservationResponse toDto(Long id, Reservation reservation){
+        return new ReservationResponse(id, reservation.getName(), reservation.getDate(), reservation.getTime());
+    }
+}
--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -1,12 +1,29 @@
 package roomescape.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public record Reservation(Long id, String name, @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul") LocalDate date, @JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul") LocalTime time) {
+public class Reservation {
 
-    public static Reservation injectId(Reservation reservation, Long id){
-        return new Reservation(id, reservation.name, reservation.date, reservation.time);
+    private final String name;
+    private final LocalDate date;
+    private final LocalTime time;
+
+    public Reservation(String name, LocalDate date, LocalTime time) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
     }
 }

963ab1bd3176dc42ef14153686ded24743467288
supernovaMK <minkikim2002@gmail.com>
2025-04-17 19:15:09 +0900

    refactor: dto의 JsonFormat 추가

--- src/main/java/roomescape/dto/ReservationResponse.java
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -1,10 +1,14 @@
 package roomescape.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import roomescape.model.Reservation;
 
-public record ReservationResponse(Long id, String name, LocalDate localDate, LocalTime localTime) {
+public record ReservationResponse(Long id,
+                                  String name,
+                                  @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul") LocalDate date,
+                                  @JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul") LocalTime time) {
 
     public static ReservationResponse toDto(Long id, Reservation reservation) {
         return new ReservationResponse(id, reservation.getName(), reservation.getDate(), reservation.getTime());

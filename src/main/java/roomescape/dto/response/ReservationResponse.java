5f2296e3e0d6805cb5790f38b17103afd49dae4b
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-04-17 14:14:40 +0900

    refactor: 시간 포맷 변경

--- src/main/java/roomescape/dto/response/ReservationResponse.java
+++ src/main/java/roomescape/dto/response/ReservationResponse.java
@@ -1,16 +1,19 @@
 package roomescape.dto.response;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import roomescape.Reservation;
 
 public record ReservationResponse(
         Long id,
         String name,
         LocalDate date,
-        LocalTime time
+        String time
 ) {
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
     public static ReservationResponse from(Reservation reservation) {
-        return new ReservationResponse(reservation.id(), reservation.name(), reservation.date(), reservation.time());
+        String time = TIME_FORMATTER.format(reservation.time());
+        return new ReservationResponse(reservation.id(), reservation.name(), reservation.date(), time);
     }
 }

8d8ca3d488a02cb011eb8f211626367a741ec314
jeyong <joon6093@naver.com>
2025-04-17 11:47:59 +0900

    feat: Reservation의 검증 기능 추가

--- src/main/java/roomescape/user/domain/Reservation.java
+++ src/main/java/roomescape/user/domain/Reservation.java
@@ -3,17 +3,35 @@ package roomescape.user.domain;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Objects;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class Reservation {
 
     private final Long id;
     private final String name;
     private final LocalDate date;
     private final LocalTime time;
+
+    public Reservation(final Long id, final String name, final LocalDate date, final LocalTime time) {
+        validateNotNull(name, date, time);
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    private void validateNotNull(final String name, final LocalDate date, final LocalTime time) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or blank");
+        }
+        if (date == null) {
+            throw new IllegalArgumentException("Date cannot be null");
+        }
+        if (time == null) {
+            throw new IllegalArgumentException("Time cannot be null");
+        }
+    }
 
     @Override
     public boolean equals(final Object object) {
@@ -23,7 +41,7 @@ public class Reservation {
         if (!(object instanceof Reservation that)) {
             return false;
         }
-        
+
         return Objects.equals(id, that.id);
     }
 

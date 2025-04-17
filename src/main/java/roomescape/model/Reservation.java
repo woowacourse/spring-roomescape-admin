1eb025d1115d7141c7ead0f8171a5c713837b3b8
Han <jumdo12@gmail.com>
2025-04-17 11:20:26 +0900

    feat: Reservation 유효성 검사 추가

--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -2,7 +2,24 @@ package roomescape.model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Objects;
 
 public record Reservation(Long id, String name, LocalDate date, LocalTime time) {
 
+    public Reservation {
+        validateNotNull(id, name, date, time);
+        validateName(name);
+    }
+
+    private void validateNotNull(Long id, String name, LocalDate date, LocalTime time) {
+        if (Objects.isNull(id) || Objects.isNull(name) || Objects.isNull(date) || Objects.isNull(time)) {
+            throw new IllegalArgumentException("Invalid reservation");
+        }
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank() || name.isEmpty()) {
+            throw new IllegalArgumentException("Invalid name");
+        }
+    }
 }

3044f501f7d32d6889ec5f572bfb7b3398aecb0b
BWY <quantumjump10@gmail.com>
2025-04-17 11:30:33 +0900

    refactor: ReservationController의 애노테이션을 RestController로 변경

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,16 +1,16 @@
 package roomescape;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class ReservationController {
 
     private final Reservations reservations = new Reservations();

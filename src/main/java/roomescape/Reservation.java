baa1c1486b0ee202c6d16445377769452ae87d0e
mimi <110226866+hongkikii@users.noreply.github.com>
2025-04-17 14:44:34 +0900

    refactor: reservationIndex 생성 위치 변경
    - Controller -> Reservation

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -2,6 +2,45 @@ package roomescape;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 
-public record Reservation(Long id, String name, LocalDate date, LocalTime time) {
+public final class Reservation {
+    private static final AtomicLong reservationIndex = new AtomicLong(1);
+
+    private final Long id;
+    private final String name;
+    private final LocalDate date;
+    private final LocalTime time;
+
+    public Reservation(String name, LocalDate date, LocalTime time) {
+        this.id = reservationIndex.getAndIncrement();
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public boolean hasSame(Long targetId) {
+        return id.equals(targetId);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (other == null || other.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (Reservation) other;
+        return Objects.equals(this.id, that.id) &&
+                Objects.equals(this.name, that.name) &&
+                Objects.equals(this.date, that.date) &&
+                Objects.equals(this.time, that.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, date, time);
+    }
 }
--- src/main/java/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/RoomescapeController.java
@@ -6,12 +6,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class RoomescapeController {
     private final List<Reservation> reservations;
-    private final AtomicLong reservationIndex = new AtomicLong(1);
 
     public RoomescapeController() {
         this.reservations = new ArrayList<>();
@@ -35,7 +33,6 @@ public class RoomescapeController {
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> createReservation(@RequestBody ReservationDto reservationDto) {
         Reservation newReservation = new Reservation(
-                reservationIndex.getAndIncrement(),
                 reservationDto.name(),
                 reservationDto.date(),
                 reservationDto.time());
@@ -46,7 +43,7 @@ public class RoomescapeController {
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
         Reservation findReservation = reservations.stream()
-                .filter(reservation -> reservation.id().equals(id))
+                .filter(reservation -> reservation.hasSame(id))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 id 입니다."));
         reservations.remove(findReservation);

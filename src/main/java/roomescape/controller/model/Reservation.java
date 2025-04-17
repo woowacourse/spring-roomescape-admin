010dfa2b6fb751ef338db34ecc3b05612ee0980c
songsunkook <songsunkook@gmail.com>
2025-04-17 13:42:24 +0900

    refactor:  예약 필수 정보 누락 시 예외처리

--- src/main/java/roomescape/controller/model/Reservation.java
+++ src/main/java/roomescape/controller/model/Reservation.java
@@ -10,6 +10,30 @@ public record Reservation(
     LocalTime time
 ) {
 
+    public Reservation {
+        validateName(name);
+        validateDate(date);
+        validateTime(time);
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR]  예약 필수 정보가 누락되었습니다. name: " + name);
+        }
+    }
+
+    private void validateDate(LocalDate date) {
+        if (date == null) {
+            throw new IllegalArgumentException("[ERROR]  예약 필수 정보가 누락되었습니다. date: " + date);
+        }
+    }
+
+    private void validateTime(LocalTime time) {
+        if (time == null) {
+            throw new IllegalArgumentException("[ERROR]  예약 필수 정보가 누락되었습니다. time: " + time);
+        }
+    }
+
     public static Reservation of(Long id, Reservation reservation) {
         return new Reservation(id, reservation.name, reservation.date, reservation.time);
     }

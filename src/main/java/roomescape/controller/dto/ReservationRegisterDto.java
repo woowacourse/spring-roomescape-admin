6f12006b57f81baadf97dbbd415ed6f4f24a0544
Jin409 <sualng123@gmail.com>
2025-04-17 11:26:28 +0900

    feat(ReservationRegisterDto): DTO 내부의 날짜 관련 검증 로직 추가

--- src/main/java/roomescape/controller/dto/ReservationRegisterDto.java
+++ src/main/java/roomescape/controller/dto/ReservationRegisterDto.java
@@ -1,18 +1,23 @@
 package roomescape.controller.dto;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 import roomescape.domain.Reservation;
 
 public record ReservationRegisterDto(
+        @Future
         @DateTimeFormat(pattern = "yyyy-MM-dd")
         LocalDate date,
 
         @NotBlank
+        @Length(min = 1, max = 4)
         String name,
 
+        @Future
         @DateTimeFormat(pattern = "HH:mm")
         LocalTime time
 ) {

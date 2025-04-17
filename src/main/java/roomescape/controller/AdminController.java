c5d8e90d6a69ffc72f5a763ae9182f1338fd0fa9
yush <yush2202@gmail.com>
2025-04-17 11:44:23 +0900

    test: 잘못된 테스트 수정

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -2,6 +2,7 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AdminController {

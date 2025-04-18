5d9f752c9cff068beaf95c4e053279f59b230e9a
byesol <bsj5967@naver.com>
2025-04-19 02:35:17 +0900

    refactor: 클래스명 dto로 수정

--- src/main/java/roomescape/ReservationDto.java
+++ src/main/java/roomescape/ReservationDto.java
@@ -3,7 +3,7 @@ package roomescape;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public record ReservationRequest(String name, LocalDate date, LocalTime time) {
+public record ReservationDto(String name, LocalDate date, LocalTime time) {
 
     public Reservation toReservation(final long id) {
         return new Reservation(id, name, date, time);

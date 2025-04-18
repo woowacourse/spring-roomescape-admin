110e90eeee384ab3a9851b29d06196d8194736c5
ryan-dia <cjfdnjs158@naver.com>
2025-04-18 16:34:16 +0900

    refactor: 예약 테스트시 기본 설정값 추가

--- src/test/java/roomescape/ReservationsTest.java
+++ src/test/java/roomescape/ReservationsTest.java
@@ -5,34 +5,36 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ReservationsTest {
 
-    @Test
-    void 예약_추가_실패() {
-        // given
-        final Reservations reservations = new Reservations();
-        final Long id = 1L;
-        final Reservation reservation = new Reservation(id, "테스트2", LocalDate.of(2026, 4, 16), LocalTime.of(13, 44));
+    final Reservations reservations = new Reservations();
+    final Long id = 1L;
+    final Reservation reservation = new Reservation(id, "테스트2", LocalDate.of(2026, 4, 16), LocalTime.of(13, 44));
 
+    @BeforeEach
+    void setUp() {
+        reservations.add(reservation);
+    }
+
+    @Test
+    void 동일_ID_예약_추가_시_예외_발생() {
+        // given
         // when
-        reservations.add(new Reservation(id, "테스트", LocalDate.of(2025, 4, 16), LocalTime.of(12, 44)));
 
         // then
         assertThatThrownBy(() -> reservations.add(reservation)).isInstanceOf(IllegalStateException.class);
     }
 
     @Test
-    void 예약_추가_성공() {
+    void 다른_ID_예약_추가_성공() {
         // given
-        final Reservations reservations = new Reservations();
-        final Reservation reservation = new Reservation(2L, "테스트2", LocalDate.of(2026, 4, 16), LocalTime.of(13, 44));
+        final Reservation reservationTest = new Reservation(2L, "테스트", LocalDate.of(2025, 4, 16), LocalTime.of(12, 44));
 
         // when
-        reservations.add(new Reservation(1L, "테스트", LocalDate.of(2025, 4, 16), LocalTime.of(12, 44)));
-
         // then
-        assertThatCode(() -> reservations.add(reservation)).doesNotThrowAnyException();
+        assertThatCode(() -> reservations.add(reservationTest)).doesNotThrowAnyException();
     }
 }

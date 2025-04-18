f27e00a6acc5741707b46d707a12bf2f0da33a69
julie <7jujubuilds@gmail.com>
2025-04-18 14:22:01 +0900

    test: 예약 생성, 삭제 기능의 예외 테스트 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -2,13 +2,12 @@ package roomescape;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.hamcrest.Matchers.is;
 
@@ -82,5 +81,30 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(0));
+    }
+
+    @Test
+    @DisplayName("예약을 추가할 수 없으면 오류 상태코드를 반환한다")
+    void createReservationException() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(500);
+    }
+
+    @Test
+    @DisplayName("예약을 삭제할 수 없으면 오류 상태코드를 반환한다")
+    void deleteReservationException() {
+        RestAssured.given().log().all()
+                .when().delete("/reservations/100")
+                .then().log().all()
+                .statusCode(500);
     }
 }

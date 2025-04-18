22658971357f714cf32492f5814503259c9536de
7i.w__k <tizmfns1218@Naver.com>
2025-04-18 21:04:30 +0900

    refactor: 테스트 간 의존성 분리

--- src/test/java/roomescape/reservation/ReservationApiTest.java
+++ src/test/java/roomescape/reservation/ReservationApiTest.java
@@ -4,19 +4,27 @@ import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import roomescape.reservation.entity.Reservation;
+import roomescape.reservation.repository.ReservationRepository;
 
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationApiTest {
+
+    @Autowired
+    private ReservationRepository reservationRepository;
 
     @DisplayName("어드민 페이지로 접근할 수 있다.")
     @Test
@@ -39,20 +47,37 @@ class ReservationApiTest {
     @DisplayName("모든 예약 정보를 반환한다.")
     @Test
     void test3() {
+        LocalDateTime now = LocalDateTime.now();
+
+        List<Reservation> reservations = List.of(
+                new Reservation(1, "꾹", now),
+                new Reservation(2, "꾹", now),
+                new Reservation(3, "꾹", now)
+        );
+
+        for (Reservation reservation : reservations) {
+            reservationRepository.save(reservation);
+        }
+
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(0));
+                .body("size()", is(3));
     }
 
     @DisplayName("예약 정보를 추가한다.")
     @Test
     void test4() {
+
+        String name = "브라운";
+        String date = "2023-08-05";
+        String time = "15:40";
+
         Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
+        params.put("name", name);
+        params.put("date", date);
+        params.put("time", time);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -60,13 +85,11 @@ class ReservationApiTest {
                 .when().post("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("id", is(1));
-
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
+                .body("id", is(1),
+                        "name", is(name),
+                        "date", is(date),
+                        "time", is(time)
+                );
     }
 
     @DisplayName("예약 삭제")
@@ -76,6 +99,12 @@ class ReservationApiTest {
         @DisplayName("예약을 삭제한다.")
         @Test
         void test1() {
+            long id = 1L;
+
+            // TODO get Reservation 경우에는 데이터 3개가 문제없이 저장되었지만, delete를 할 때는 repository에 존재하지 않음
+//            Reservation reservation = new Reservation(id, "브라운", LocalDateTime.now());
+//            reservationRepository.save(reservation);
+
             Map<String, String> params = new HashMap<>();
             params.put("name", "브라운");
             params.put("date", "2023-08-05");
@@ -87,18 +116,13 @@ class ReservationApiTest {
                     .when().post("/reservations")
                     .then().log().all()
                     .statusCode(200)
-                    .body("id", is(1));
+                    .body("id", is((int) id));
+
 
             RestAssured.given().log().all()
-                    .when().delete("/reservations/1")
+                    .when().delete("/reservations/" + id)
                     .then().log().all()
                     .statusCode(204);
-
-            RestAssured.given().log().all()
-                    .when().get("/reservations")
-                    .then().log().all()
-                    .statusCode(200)
-                    .body("size()", is(0));
         }
 
         @DisplayName("존재하지 않는 예약을 삭제할 경우 NOT_FOUND 반환")

5e2681666f68e2c030a45ad4c097aa51cb1065b9
7i.w__k <tizmfns1218@Naver.com>
2025-04-18 19:48:59 +0900

    refactor: mockMvc로 Controller 검증

--- src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
@@ -1,9 +1,13 @@
 package roomescape.reservation.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -13,29 +17,31 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import roomescape.reservation.dto.ReservationRequestDto;
-import roomescape.reservation.dto.ReservationResponseDto;
 import roomescape.reservation.entity.Reservation;
-import roomescape.reservation.exception.EntityNotFoundException;
 import roomescape.reservation.repository.ReservationRepository;
 import roomescape.reservation.repository.ReservationRepositoryImpl;
 
 class ReservationControllerTest {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
     private ReservationRepository reservationRepository;
-    private ReservationController reservationController;
+    private MockMvc mockMvc;
 
     @BeforeEach
     void init() {
         reservationRepository = new ReservationRepositoryImpl();
-        reservationController = new ReservationController(reservationRepository);
+        ReservationController reservationController = new ReservationController(reservationRepository);
+        mockMvc = MockMvcBuilders.standaloneSetup(reservationController).build();
+        objectMapper.registerModule(new JavaTimeModule());
     }
 
     @DisplayName("모든 예약 정보를 가져온다.")
     @Test
-    void test1() {
+    void test1() throws Exception {
         // given
         LocalDateTime now = LocalDateTime.now();
 
@@ -45,63 +51,58 @@ class ReservationControllerTest {
                 new Reservation(3, "꾹", now)
         );
 
-        List<ReservationResponseDto> expected = reservations.stream()
-                .map(ReservationResponseDto::toDto)
-                .toList();
-
         for (Reservation reservation : reservations) {
             reservationRepository.save(reservation);
         }
 
-        // when
-        ResponseEntity<List<ReservationResponseDto>> result = reservationController.readAllReservations();
-
-        // then
-        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(result.getBody()).isEqualTo(expected);
+        // when & then
+        mockMvc.perform(get("/reservations"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(3))
+                .andExpect(jsonPath("$[0].name").value("꾹"));
     }
 
     @DisplayName("예약 정보를 추가한다.")
     @ParameterizedTest
     @CsvSource(value = {"꾹,2025-04-17,10:00"}, delimiter = ',')
-    void test2(String name, String date, String time) {
+    void test2(String name, String date, String time) throws Exception {
         // given
         LocalDate localDate = LocalDate.parse(date);
         LocalTime localTime = LocalTime.parse(time);
 
         ReservationRequestDto requestDto = new ReservationRequestDto(name, localDate, localTime);
-        ReservationResponseDto expected = new ReservationResponseDto(1L, name, localDate, localTime);
+        String json = objectMapper.writeValueAsString(requestDto);
 
-        // when
-        ResponseEntity<ReservationResponseDto> result = reservationController.add(requestDto);
-
-        // then
-        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(result.getBody()).isEqualTo(expected);
+        // when & then
+        mockMvc.perform(post("/reservations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)
+        ).andExpect(status().isOk());
     }
 
     @DisplayName("예약 정보를 삭제한다.")
     @Test
-    void test3() {
+    void test3() throws Exception {
         // given
         long id = 1L;
         Reservation reservation = new Reservation(id, "꾹", LocalDateTime.now());
         reservationRepository.save(reservation);
 
         // when & then
-        assertThatCode(() -> reservationController.delete(id))
-                .doesNotThrowAnyException();
+
+        mockMvc.perform(delete("/reservations/{id}", id))
+                .andExpect(status().isNoContent());
     }
 
     @DisplayName("삭제할 예약 정보가 없다면 404 에러 코드를 반환한다")
     @Test
-    void test4() {
+    void test4() throws Exception {
         // given
         long id = 1L;
 
         // when & then
-        assertThatThrownBy(() -> reservationController.delete(id))
-                .isInstanceOf(EntityNotFoundException.class);
+        mockMvc.perform(delete("/reservations/{id}", id))
+                .andExpect(status().isNotFound());
     }
 
 }

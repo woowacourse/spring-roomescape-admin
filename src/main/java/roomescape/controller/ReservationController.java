9bacbb5e6e6895b4238701d27a69d7c23bdbcaa2
CheChe903 <cheche903@naver.com>
2025-04-18 15:28:11 +0900

    refactor(ReservationController): 예약 응답 DTO 생성

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 import roomescape.dto.request.ReservationCreateRequest;
+import roomescape.dto.response.ReservationResponse;
 
 @RestController
 @RequestMapping("/reservations")
@@ -24,12 +26,13 @@ public class ReservationController {
     private final AtomicLong index = new AtomicLong(1);
 
     @GetMapping()
-    public ResponseEntity<List<Reservation>> findAll() {
-        return ResponseEntity.ok().body(reservations);
+    public ResponseEntity<List<ReservationResponse>> findAll() {
+        return ResponseEntity.ok().body(createReservationResponses());
     }
 
     @PostMapping()
-    public ResponseEntity<Reservation> create(@Valid @RequestBody ReservationCreateRequest reservationCreateRequest) {
+    public ResponseEntity<ReservationResponse> create(
+            @Valid @RequestBody ReservationCreateRequest reservationCreateRequest) {
 
         Reservation reservation = new Reservation(index.getAndIncrement(),
                 reservationCreateRequest.name(),
@@ -37,7 +40,7 @@ public class ReservationController {
                 reservationCreateRequest.time()
         );
         reservations.add(reservation);
-        return ResponseEntity.ok().body(reservation);
+        return ResponseEntity.ok().body(createReservationResponse(reservation));
     }
 
     @DeleteMapping("{id}")
@@ -50,5 +53,19 @@ public class ReservationController {
         reservations.remove(reservation);
 
         return ResponseEntity.noContent().build();
+    }
+
+    private List<ReservationResponse> createReservationResponses() {
+        return reservations.stream()
+                .map(this::createReservationResponse)
+                .collect(Collectors.toList());
+    }
+
+    private ReservationResponse createReservationResponse(Reservation reservation) {
+        return new ReservationResponse(
+                reservation.getId(),
+                reservation.getName(),
+                reservation.getDate(),
+                reservation.getTime());
     }
 }

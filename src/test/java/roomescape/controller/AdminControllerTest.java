4bf2b462d372f2ae928dc12b45424dbee9ad55c8
Chaeyoung714 <202102760@hufs.ac.kr>
2025-04-18 13:53:29 +0900

    fix : fix delete test setting error

--- src/test/java/roomescape/controller/AdminControllerTest.java
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -68,6 +68,8 @@ public class AdminControllerTest {
     @Test
     @DisplayName("예약을 삭제한다.")
     void deleteReservation() {
+        createReservation();
+
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()

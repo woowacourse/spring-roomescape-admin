d153b61f718de469c327e548c72cbced1163206f
YuyoungRhee <bbclara@naver.com>
2025-04-18 13:44:11 +0900

    refactor(controller): deleteReservation() 메서드도 @ResponseBody를 통해 응답을 반환하도록 변경

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -1,7 +1,6 @@
 package roomescape.controller;
 
 import java.util.List;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,10 +42,9 @@ public class RoomEscapeController {
         return ReservationResponse.reservationToDto(repository.findById(id));
     }
 
-
+    @ResponseBody
     @DeleteMapping("/reservations/{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
+    public void deleteReservation(@PathVariable("id") Long id) {
         repository.deleteById(id);
-        return ResponseEntity.ok().build();
     }
 }

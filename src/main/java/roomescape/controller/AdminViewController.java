2e75b07538a09b3d18c5d399eaffa8e938593298
ljhee92 <weilainib7@gmail.com>
2025-04-18 13:34:11 +0900

    refactor: ViewController -> AdminViewController 클래스명 구체화, @RequestMapping 추가
    - AdminViewController 내부에서 공유되는 mapping을 표현할 수 있도록 클래스 레벨에 @RequestMapping 추가

--- src/main/java/roomescape/controller/AdminViewController.java
+++ src/main/java/roomescape/controller/AdminViewController.java
@@ -2,16 +2,18 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ViewController {
+@RequestMapping("/admin")
+public class AdminViewController {
 
-    @GetMapping("/admin")
+    @GetMapping
     public String admin(){
         return "admin/index";
     }
 
-    @GetMapping("/admin/reservation")
+    @GetMapping("/reservation")
     public String adminReservation(){
         return "admin/reservation-legacy";
     }

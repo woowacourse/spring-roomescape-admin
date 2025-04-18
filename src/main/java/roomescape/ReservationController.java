51f6bf393fcf0548c410b9864cef4c2d3336afee
BWY <quantumjump10@gmail.com>
2025-04-18 17:20:49 +0900

    refactor: ReservationController에 @RequestMapping 추가

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -6,28 +6,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/reservations")
 public class ReservationController {
 
     private final Reservations reservations = new Reservations();
 
-    @GetMapping("/reservations")
+    @GetMapping
     public ResponseEntity<List<Reservation>> readAll() {
         return ResponseEntity.ok(reservations.getAllReservations());
     }
 
-    @PostMapping("/reservations")
+    @PostMapping
     public ResponseEntity<Reservation> create(@RequestBody ReservationReqDto dto) {
         Reservation newReservation = dto.toReservation(reservations.generateId());
         reservations.add(newReservation);
         return ResponseEntity.ok(newReservation);
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         reservations.deleteById(id);
         return ResponseEntity.ok().build();

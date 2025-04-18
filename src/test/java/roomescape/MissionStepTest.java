77087908bdbcf7ba0bca05e4dd38436340694ee3
LeeJaeHoon <dlwogns3413@naver.com>
2025-04-18 15:49:35 +0900

    refactor: 테스트 명 더 명확히 변경

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -2,24 +2,21 @@ package roomescape;
 
 import static org.hamcrest.Matchers.is;
 
-import java.io.IOException;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.annotation.DirtiesContext;
-
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
     @Test
-    void 일단계() {
+    void admin_경로로_요청을_보내면_어드민_메인_페이지를_응답할_수_있다() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
@@ -27,21 +24,24 @@ public class MissionStepTest {
     }
 
     @Test
-    void 이단계() {
+    void 예약_관리_페이지를_응답할_수_있다() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
+    }
 
+    @Test
+    void 예약_목록을_조회할_수_있다() {
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
+                .body("size()", is(0));
     }
 
     @Test
-    void 삼단계() {
+    void 예약을_추가_또는_삭제_할_수_있다() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");

60c27412214389f4f95bec16451842d22a2043ab
CheChe903 <cheche903@naver.com>
2025-04-18 15:19:02 +0900

    refactor(Controller): ViewController, RestController 분리

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -6,32 +6,29 @@ import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 import roomescape.dto.request.ReservationCreateRequest;
 
-@Controller
+@RestController
+@RequestMapping("/reservations")
 public class ReservationController {
 
     private final List<Reservation> reservations = new ArrayList<>();
     private final AtomicLong index = new AtomicLong(1);
 
-    @GetMapping("/admin/reservation")
-    public String getReservationPage() {
-        return "admin/reservation-legacy";
-    }
-
-    @GetMapping("/reservations")
+    @GetMapping()
     public ResponseEntity<List<Reservation>> findAll() {
         return ResponseEntity.ok().body(reservations);
     }
 
-    @PostMapping("/reservations")
+    @PostMapping()
     public ResponseEntity<Reservation> create(@Valid @RequestBody ReservationCreateRequest reservationCreateRequest) {
 
         Reservation reservation = new Reservation(index.getAndIncrement(),
@@ -43,7 +40,7 @@ public class ReservationController {
         return ResponseEntity.ok().body(reservation);
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         Reservation reservation = reservations.stream()
                 .filter(it -> Objects.equals(it.getId(), id))
--- src/main/java/roomescape/controller/ViewController.java
+++ src/main/java/roomescape/controller/ViewController.java
@@ -1,14 +1,18 @@
 package roomescape.controller;
 
-import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class RoomescapeController {
+public class ViewController {
 
     @GetMapping("admin")
     public String home() {
         return "admin/index";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String getReservationPage() {
+        return "admin/reservation-legacy";
     }
 }

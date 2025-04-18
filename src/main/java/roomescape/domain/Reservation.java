de87f7eae00e35c4dc28720b6b6ac133c4a0c0ff
seaniiio <cnc4750@naver.com>
2025-04-19 04:12:23 +0900

    feat(Reservation): 유효성 검사 로직 추가

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,18 +1,20 @@
 package roomescape.domain;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 public class Reservation {
 
+    private static final int MAX_NAME_LENGTH = 10;
     private final Long id;
     private final String name;
     private final LocalDateTime dateTime;
 
     public Reservation(final Long id, final String name, final LocalDateTime dateTime) {
-        this.id = Objects.requireNonNull(id, "Id는 null이 될 수 없습니다.");
-        this.name = Objects.requireNonNull(name, "Name은 null이 될 수 없습니다.");
-        this.dateTime = Objects.requireNonNull(dateTime, "DateTime은 null이 될 수 없습니다.");
+        validate(id, name, dateTime);
+        this.id = id;
+        this.name = name;
+        this.dateTime = dateTime;
     }
 
     public boolean isSameId(final Long givenId) {
@@ -29,5 +31,37 @@ public class Reservation {
 
     public LocalDateTime getDateTime() {
         return dateTime;
+    }
+
+    private void validate(final Long id, final String name, final LocalDateTime dateTime) {
+        validateId(id);
+        validateName(name);
+        validateDateTime(dateTime);
+    }
+
+    private void validateId(final Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Id는 null이 될 수 없습니다.");
+        }
+    }
+
+    private void validateName(final String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Name은 null이 될 수 없습니다.");
+        }
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("Name은 10글자를 초과할 수 없습니다.");
+        }
+    }
+
+    private void validateDateTime(final LocalDateTime dateTime) {
+        if (dateTime == null) {
+            throw new IllegalArgumentException("DateTime은 null이 될 수 없습니다.");
+        }
+
+        boolean isPast = dateTime.toLocalDate().isBefore(LocalDate.now());
+        if (isPast) {
+            throw new IllegalArgumentException("과거 날짜에 대한 예약을 할 수 없습니다.");
+        }
     }
 }
--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -1,29 +1,47 @@
 package roomescape.domain;
 
-import java.time.LocalDateTime;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 class ReservationTest {
 
     @Test
     void 아이디가_null인_경우_예외를_발생시킨다() {
         Assertions.assertThatThrownBy(() -> new Reservation(null, "밍트", LocalDateTime.now()))
-                .isInstanceOf(NullPointerException.class)
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("Id는 null이 될 수 없습니다.");
     }
 
     @Test
     void 이름이_null인_경우_예외를_발생시킨다() {
         Assertions.assertThatThrownBy(() -> new Reservation(1L, null, LocalDateTime.now()))
-                .isInstanceOf(NullPointerException.class)
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("Name은 null이 될 수 없습니다.");
+    }
+
+    @Test
+    void 이름이_10글자_초과인_경우_예외를_발생시킨다() {
+        Assertions.assertThatThrownBy(() -> new Reservation(1L, "잠실에사는비행기데코피크민", LocalDateTime.now()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Name은 10글자를 초과할 수 없습니다.");
     }
 
     @Test
     void 날짜가_null인_경우_예외를_발생시킨다() {
         Assertions.assertThatThrownBy(() -> new Reservation(2L, "메이", null))
-                .isInstanceOf(NullPointerException.class)
+                .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("DateTime은 null이 될 수 없습니다.");
+    }
+
+    @Test
+    void 날짜가_과거인_경우_예외를_발생시킨다() {
+        // given
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+
+        Assertions.assertThatThrownBy(() -> new Reservation(2L, "메이", yesterday))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("과거 날짜에 대한 예약을 할 수 없습니다.");
     }
 }

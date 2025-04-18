f7104cccc2951de02ca6841f1c1e47faec86df4d
kjungw <sagakkb@gmail.com>
2025-04-18 09:40:59 +0900

    refactor: 패키지명 수정

--- src/main/java/roomescape/controller/RoomescapeController.java
+++ src/main/java/roomescape/controller/RoomescapeController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import roomescape.model.Reservation;
-import roomescape.model.Reservations;
+import roomescape.domain.Reservation;
+import roomescape.domain.Reservations;
 import roomescape.vo.ReservationCreateVo;
 
 @Controller

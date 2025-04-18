73e0a6b1daa455d8bb26a7be8808d711c25d0962
ljhee92 <weilainib7@gmail.com>
2025-04-18 16:47:05 +0900

    refactor: 동시성 보장을 위해 List -> ConcurrentMap 변경
    - AtomicLong을 사용한 이유인 동시성 보장과 동일한 이유로 List를 ConcurrentMap으로 변경

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -1,35 +1,36 @@
 package roomescape.repository;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Repository;
 import roomescape.dto.ReservationRequest;
 import roomescape.entity.Reservation;
 import roomescape.exception.InvalidReservationException;
 
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Repository
 public class ReservationRepository {
-    private final List<Reservation> reservations = new ArrayList<>();
+    private final ConcurrentMap<Long, Reservation> reservations = new ConcurrentHashMap<>();
     private final AtomicLong index = new AtomicLong(1);
 
     public List<Reservation> findAll() {
-        return Collections.unmodifiableList(reservations);
+        return reservations.keySet().stream()
+                .map(reservations::get)
+                .toList();
     }
 
-    public Reservation add(ReservationRequest reservationRequest) {
-        Reservation reservation = reservationRequest.toEntity(index.getAndIncrement());
-        reservations.add(reservation);
-        return reservation;
+    public Reservation add(final ReservationRequest reservationRequest) {
+        Reservation newReservation = reservationRequest.toEntity(index.getAndIncrement());
+        reservations.put(newReservation.getId(), newReservation);
+        return newReservation;
     }
 
-    public void deleteById(Long id) {
-        Reservation reservation = reservations.stream()
-                .filter(item -> item.getId().equals(id))
-                .findFirst()
-                .orElseThrow(InvalidReservationException::new);
-
-        reservations.remove(reservation);
+    public void deleteById(final Long id) {
+        if (!reservations.containsKey(id)) {
+            throw new InvalidReservationException();
+        }
+        reservations.remove(id);
     }
 }

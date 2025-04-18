8332c5c922aadf7dd4e0676aa0893f633957b9a4
soeun2537 <soeun2537@gmail.com>
2025-04-18 17:09:10 +0900

    test: 예약 생성 검증 테스트 작성

--- src/test/java/roomescape/ReservationTest.java
+++ src/test/java/roomescape/ReservationTest.java
@@ -16,52 +16,100 @@ import org.springframework.test.annotation.DirtiesContext;
 public class ReservationTest {
 
     @Test
-    @DisplayName("모든 예약을 조회한다.")
-    void getAllReservations() {
-        RestAssured.given().log().all()
-                .when().get("/admin/reservation")
+    @DisplayName("예약에 대한 생성, 조회, 삭제를 진행한다.")
+    void createAndReadAndDeleteReservation() {
+        var params = Map.of(
+                "name", "브라운",
+                "date", "2023-08-05",
+                "time", "15:40"
+        );
+
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when()
+                .post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+
+        RestAssured
+                .given().log().all()
+                .when()
+                .get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+
+        RestAssured
+                .given().log().all()
+                .when()
+                .delete("/reservations/1")
                 .then().log().all()
                 .statusCode(200);
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
+        RestAssured
+                .given().log().all()
+                .when()
+                .get("/reservations")
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(0));
     }
 
     @Test
-    @DisplayName("예약에 대한 생성, 조회, 삭제를 진행한다.")
-    void reservationCrd() {
-        Map<String, String> params = Map.of(
-                "name", "브라운",
+    @DisplayName("예약 생성시 이름이 비어있으면 예외가 발생한다.")
+    void cannotCreateReservationWhenNameIsBlank() {
+        var params = Map.of(
+                "name", "",
                 "date", "2023-08-05",
                 "time", "15:40"
         );
 
-        RestAssured.given().log().all()
+        RestAssured
+                .given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
-                .when().post("/reservations")
+                .when()
+                .post("/reservations")
                 .then().log().all()
-                .statusCode(200)
-                .body("id", is(1));
+                .statusCode(400);
+    }
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
+    @Test
+    @DisplayName("예약 생성시 날짜가 null이면 예외가 발생한다.")
+    void cannotCreateReservationWhenDateIsNull() {
+        var params = new HashMap<String, String>();
+        params.put("name", "브라운");
+        params.put("date", null);
+        params.put("time", "15:40");
 
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when()
+                .post("/reservations")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(400);
+    }
 
-        RestAssured.given().log().all()
-                .when().get("/reservations")
+    @Test
+    @DisplayName("예약 생성시 시간이 null이면 예외가 발생한다.")
+    void cannotCreateReservationWhenTimeIsNull() {
+        var params = new HashMap<String, String>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", null);
+
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when()
+                .post("/reservations")
                 .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
+                .statusCode(400);
     }
 }

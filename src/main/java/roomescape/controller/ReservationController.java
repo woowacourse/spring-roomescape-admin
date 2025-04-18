709043864b30ff1d3b871d8709d1a28232e2fab4
soeun2537 <soeun2537@gmail.com>
2025-04-18 16:01:09 +0900

    refactor: @Controller + @ResponseBody를 @RestController 어노테이션으로 변경

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,18 +3,17 @@ package roomescape.controller;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.dto.ReservationRequest;
 import roomescape.entity.Reservation;
 import roomescape.repository.ReservationRepository;
-import roomescape.dto.ReservationRequest;
 
-@Controller
+@RestController
 public class ReservationController {
 
     private final ReservationRepository reservationRepository;
@@ -24,14 +23,12 @@ public class ReservationController {
     }
 
     @GetMapping("/reservations")
-    @ResponseBody
     public ResponseEntity<List<Reservation>> getReservations() {
         List<Reservation> reservations = reservationRepository.findAll();
         return ResponseEntity.ok(reservations);
     }
 
     @PostMapping("/reservations")
-    @ResponseBody
     public ResponseEntity<Reservation> createReservation(@RequestBody @Valid ReservationRequest request) {
         Reservation reservation = request.toReservation(reservationRepository.getNextId());
         Reservation saved = reservationRepository.save(reservation);
@@ -39,7 +36,6 @@ public class ReservationController {
     }
 
     @DeleteMapping("/reservations/{id}")
-    @ResponseBody
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
         try {
             Reservation found = reservationRepository.findById(id)

bb7519368d6d746a9ff1a92f2b5cb903e0b72e2e
minSsan <msp1125@hanyang.ac.kr>
2025-04-18 09:52:40 +0900

    feat: id 생성 책임을 Entity로 변경

--- src/main/java/roomescape/ReservationEntity.java
+++ src/main/java/roomescape/ReservationEntity.java
@@ -2,6 +2,42 @@ package roomescape;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.concurrent.atomic.AtomicLong;
 
-public record ReservationEntity(Long id, String name, LocalDate date, LocalTime time) {
+public class ReservationEntity {
+    private static final AtomicLong index = new AtomicLong(1);
+    private final Long id;
+    private final String name;
+    private final LocalDate date;
+    private final LocalTime time;
+
+    public ReservationEntity(Long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public ReservationEntity(String name, LocalDate date, LocalTime time) {
+        this.id = index.getAndIncrement();
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public LocalDate date() {
+        return date;
+    }
+
+    public LocalTime time() {
+        return time;
+    }
 }

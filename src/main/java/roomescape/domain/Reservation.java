e1b40c61c6dbd7a82eb81d6e421d86a757efa8f3
juno <wnsgh732@gmail.com>
2025-04-18 16:29:03 +0900

    chore: Reservation 필드를 불변으로 수정

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -5,9 +5,9 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Reservation {
-    private long id;
-    private String name;
-    private LocalDateTime dateTime;
+    private final long id;
+    private final String name;
+    private final LocalDateTime dateTime;
 
     public Reservation(long id, String name, LocalDateTime dateTime) {
         this.id = id;

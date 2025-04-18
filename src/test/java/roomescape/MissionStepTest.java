b002c4df6a68d285c763c1a6dcd61545774c81b1
KimDaehyeon <daehyeon3351@gmail.com>
2025-04-18 14:40:26 +0900

    test: 존재하지 않는 예약 삭제 시 404 응답 테스트 작성

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -85,10 +85,16 @@ public class MissionStepTest {
             .statusCode(200);
 
         RestAssured.given().log().all()
+            .when().delete("/reservations/0")
+            .then().log().all()
+            .statusCode(404);
+
+        RestAssured.given().log().all()
             .when().get("/reservations")
             .then().log().all()
             .statusCode(200)
             .body("size()", is(0));
+
     }
 
     @Test

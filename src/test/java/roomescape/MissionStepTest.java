fcdd04d2c5a58bbca51a9a4034ce639520d20bb6
KimDaehyeon <daehyeon3351@gmail.com>
2025-04-18 15:31:01 +0900

    refactor: 테스트를 독립적으로 분리

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -15,7 +15,7 @@ import org.springframework.test.annotation.DirtiesContext;
 public class MissionStepTest {
 
     @Test
-    void 일단계() {
+    void 홈_화면을_응답할_수_있다() {
         RestAssured.given().log().all()
             .when().get("/")
             .then().log().all()
@@ -23,12 +23,15 @@ public class MissionStepTest {
     }
 
     @Test
-    void 이단계() {
+    void 예약_조회_화면을_응답할_수_있다() {
         RestAssured.given().log().all()
             .when().get("/admin/reservation")
             .then().log().all()
             .statusCode(200);
+    }
 
+    @Test
+    void 예약_목록_데이터를_조회할_수_있다() {
         RestAssured.given().log().all()
             .when().get("/reservations")
             .then().log().all()
@@ -37,7 +40,7 @@ public class MissionStepTest {
     }
 
     @Test
-    void 삼단계() {
+    void 예약을_생성할_수_있다() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
@@ -56,6 +59,22 @@ public class MissionStepTest {
             .then().log().all()
             .statusCode(200)
             .body("size()", is(1));
+    }
+
+    @Test
+    void 예약을_삭제할_수_있다() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+            .contentType(ContentType.JSON)
+            .body(params)
+            .when().post("/reservations")
+            .then().log().all()
+            .statusCode(200)
+            .body("id", is(1));
 
         RestAssured.given().log().all()
             .when().delete("/reservations/1")
@@ -63,16 +82,18 @@ public class MissionStepTest {
             .statusCode(200);
 
         RestAssured.given().log().all()
-            .when().delete("/reservations/0")
-            .then().log().all()
-            .statusCode(404);
-
-        RestAssured.given().log().all()
             .when().get("/reservations")
             .then().log().all()
             .statusCode(200)
             .body("size()", is(0));
+    }
 
+    @Test
+    void 존재하지_않는_예약을_삭제할_수_없다() {
+        RestAssured.given().log().all()
+            .when().delete("/reservations/0")
+            .then().log().all()
+            .statusCode(404);
     }
 }
 

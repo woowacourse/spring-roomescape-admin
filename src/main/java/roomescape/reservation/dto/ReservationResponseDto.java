fa2bb97472b512812c291a907ddc277489cfd959
Firedrago95 <yonghwa95@naver.com>
2025-04-18 13:34:16 +0900

    refactor: 변수 분리

--- src/main/java/roomescape/reservation/dto/ReservationResponseDto.java
+++ src/main/java/roomescape/reservation/dto/ReservationResponseDto.java
@@ -1,14 +1,16 @@
 package roomescape.reservation.dto;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import roomescape.reservation.entity.Reservation;
 
 public record ReservationResponseDto(Long id, String name, LocalDate date, LocalTime time) {
 
     public static ReservationResponseDto toDto(Reservation reservation) {
-        LocalDate localDate = reservation.getDateTime().toLocalDate();
-        LocalTime localTime = reservation.getDateTime().toLocalTime();
+        LocalDateTime dateTime = reservation.getDateTime();
+        LocalDate localDate = dateTime.toLocalDate();
+        LocalTime localTime = dateTime.toLocalTime();
 
         return new ReservationResponseDto(reservation.getId(), reservation.getName(), localDate, localTime);
     }

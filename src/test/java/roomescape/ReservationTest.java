bb34b2312e7017aa062aed5a0fdbad2f65d8f49d
Yeongjae Kim <sees77770@gmail.com>
2025-04-18 23:41:36 +0900

    refactor: 테스트 메서드 명 추가

--- src/test/java/roomescape/ReservationTest.java
+++ src/test/java/roomescape/ReservationTest.java
@@ -12,10 +12,10 @@ import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+public class ReservationTest {
 
     @Test
-    void 일단계() {
+    void 관리자_페이지를_응답한다() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
@@ -23,12 +23,15 @@ public class MissionStepTest {
     }
 
     @Test
-    void 이단계() {
+    void 방탈출_예약_페이지를_응답한다() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
+    }
 
+    @Test
+    void 방탈출_예약_목록을_응답한다() {
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
@@ -37,7 +40,7 @@ public class MissionStepTest {
     }
 
     @Test
-    void 삼단계() {
+    void 방탈출_예약을_생성_조회_삭제한다() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");

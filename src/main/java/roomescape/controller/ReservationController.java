b572abe04113a7ed8ed4ec29919a998024555c0c
(minjae8563) <(minjae8563@naver.com)>
2025-04-18 15:44:49 +0900

    refactor: ReservationController 가 필드로 Reservations 를 갖도록 수정

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,10 +2,10 @@ package roomescape.controller;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,18 +14,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.entity.Reservation;
+import roomescape.entity.Reservations;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
 
-    private List<Reservation> reservations = new ArrayList<>();
+    private final Reservations reservations;
     private AtomicLong index = new AtomicLong(1);
+
+    @Autowired
+    public ReservationController(Reservations reservations) {
+        this.reservations = reservations;
+    }
 
     @GetMapping()
     public List<Reservation> getReservations(
     ) {
-        return reservations;
+        return reservations.findAllReservations();
     }
 
     @PostMapping()
@@ -36,20 +42,21 @@ public class ReservationController {
     ) {
         Reservation reservation = new Reservation(index.getAndIncrement(), name, date, time);
 
-        reservations.add(reservation);
+        reservations.validateSameDateTime(reservation);
+        reservations.saveReservation(reservation);
         return ResponseEntity.ok().body(reservation);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<List<Reservation>> deleteReservation(
+    public ResponseEntity<Reservations> deleteReservation(
             @PathVariable Long id
     ) {
-        Reservation reservation = reservations.stream()
+        Reservation reservation = reservations.findAllReservations().stream()
                 .filter(it -> Objects.equals(it.getId(), id))
                 .findFirst()
                 .orElseThrow(RuntimeException::new);
 
-        reservations.remove(reservation);
+        reservations.deleteReservation(reservation);
 
         return ResponseEntity.ok().body(reservations);
     }

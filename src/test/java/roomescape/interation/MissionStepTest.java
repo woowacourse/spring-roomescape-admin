bc96d0b5bab4ea063857a6fe5b975e6897a5760b
kjungw <sagakkb@gmail.com>
2025-04-18 15:57:07 +0900

    test: 통합테스트 정리

--- src/test/java/roomescape/interation/MissionStepTest.java
+++ src/test/java/roomescape/interation/MissionStepTest.java
@@ -1,4 +1,4 @@
-package roomescape;
+package roomescape.interation;
 
 import static org.hamcrest.Matchers.is;
 
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -14,30 +15,37 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
+    @DisplayName("메인페이지를 응답할 수 있다")
     @Test
-    void 일단계() {
+    void canResponseMainPage() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
                 .statusCode(200);
     }
 
+    @DisplayName("예약페이지를 응답할 수 있다")
     @Test
-    void 이단계() {
+    void canResponseReservationPage() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
+    }
 
+    @DisplayName("저장된 예약들을 응답할 수 있다")
+    @Test
+    void canResponseReservations() {
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
+                .body("size()", is(0));
     }
 
+    @DisplayName("예약을 추가할 수 있다")
     @Test
-    void 삼단계() {
+    void canCreateReservation() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
@@ -56,6 +64,23 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
+    }
+
+    @DisplayName("예약을 삭제할 수 있다")
+    @Test
+    void canDeleteReservation() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201)
+                .body("id", is(1));
 
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
@@ -67,13 +92,5 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(0));
-    }
-
-    @Test
-    void 존재하지_않는_id의_예약_삭제시_404_반환() {
-        RestAssured.given().log().all()
-                .when().delete("/reservations/77")
-                .then().log().all()
-                .statusCode(404);
     }
 }

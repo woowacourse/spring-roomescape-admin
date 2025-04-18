940daefff60e494e61cf45302dee66f4f79f459b
AppleMint98 <applemint9821@gmail.com>
2025-04-18 18:00:37 +0900

    refactor(controller): 반환 형식에 따른 controller 분리

---
+++ src/main/java/roomescape/controller/AdminPageController.java
@@ -0,0 +1,19 @@
+package roomescape.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminPageController {
+
+    @GetMapping("/admin")
+    public String getAdminPage() {
+        return "admin/index";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String getReservationPage() {
+        return "admin/reservation-legacy";
+    }
+
+}
--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,19 +2,18 @@ package roomescape.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 import roomescape.dto.ReservationRequest;
 import roomescape.dto.ReservationResponse;
 import roomescape.service.RoomescapeService;
 
-@Controller
+@RestController
 public class ReservationController {
 
     private final RoomescapeService roomescapeService;
@@ -23,18 +22,7 @@ public class ReservationController {
         this.roomescapeService = roomescapeService;
     }
 
-    @GetMapping("/admin")
-    public String getAdminPage() {
-        return "admin/index";
-    }
-
-    @GetMapping("/admin/reservation")
-    public String getReservationPage() {
-        return "admin/reservation-legacy";
-    }
-
     @GetMapping("/reservations")
-    @ResponseBody
     public ResponseEntity<List<ReservationResponse>> reservationList() {
         List<Reservation> reservations = roomescapeService.findReservations();
         List<ReservationResponse> responses = reservations.stream().map(ReservationResponse::of).toList();
@@ -42,7 +30,6 @@ public class ReservationController {
     }
 
     @PostMapping("/reservations")
-    @ResponseBody
     public ResponseEntity<ReservationResponse> reservationAdd(@RequestBody ReservationRequest request) {
         try {
             Reservation savedReservation = roomescapeService.addReservation(request.toReservation());

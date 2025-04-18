75778869616085466f069395e953ec045a62c643
yush <yush2202@gmail.com>
2025-04-18 16:42:30 +0900

    refactor: @RequestMapping 어노테이션 적용

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -2,17 +2,19 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping("/admin")
+    @GetMapping("")
     public String admin() {
         return "admin/index";
     }
 
-    @GetMapping("/admin/reservation")
+    @GetMapping("/reservation")
     public String reservation() {
         return "admin/reservation-legacy";
     }
--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Person;
 import roomescape.domain.Reservation;
@@ -17,32 +18,30 @@ import roomescape.dto.ReservationRequestDto;
 import roomescape.dto.ReservationResponseDto;
 
 @RestController
+@RequestMapping("/reservations")
 public class ReservationController {
 
     private final Reservations reservations = new Reservations();
     private final AtomicLong reservationIndex = new AtomicLong(1);
     private final AtomicLong personIndex = new AtomicLong(1);
 
-    @GetMapping("/reservations")
+    @GetMapping("")
     public List<ReservationResponseDto> readReservations() {
         return reservations.getReservations().stream()
-            .map(ReservationResponseDto::from)
-            .toList();
+                .map(ReservationResponseDto::from)
+                .toList();
     }
 
-    @PostMapping("/reservations")
+    @PostMapping("")
     public Reservation createReservations(
-        @RequestBody ReservationRequestDto reservationRequestDto) {
+            @RequestBody ReservationRequestDto reservationRequestDto) {
         Person person = new Person(personIndex.getAndIncrement(), reservationRequestDto.name());
         ReservationTime reservationTime = new ReservationTime(
-            LocalDateTime.of(reservationRequestDto.date(), reservationRequestDto.time()));
-        Reservation reservation = new Reservation(reservationIndex.getAndIncrement(), person,
-            reservationTime);
-        reservations.save(reservation);
-        return reservation;
+                LocalDateTime.of(reservationRequestDto.date(), reservationRequestDto.time()));
+        return reservations.save(person, reservationTime);
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("/{id}")
     public void deleteReservation(@PathVariable(name = "id") Long id) {
         reservations.deleteById(id);
     }

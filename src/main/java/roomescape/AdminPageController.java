853c9937228398ed2eb4a3db17c4a422178d02fe
BWY <quantumjump10@gmail.com>
2025-04-18 16:09:02 +0900

    refactor: 관리자 페이지 라우팅을 다루는 컨트롤러의 이름을 AdminPageController로 변경

--- src/main/java/roomescape/AdminPageController.java
+++ src/main/java/roomescape/AdminPageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class RoomEscapeController {
+public class AdminPageController {
 
     @GetMapping("/admin")
     public String getMainPage() {

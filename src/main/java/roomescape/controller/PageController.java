2278fd15ab72a8808c864515309fbc998a166fea
MinJun Choi <mj043000@naver.com>
2025-04-18 16:31:28 +0900

    refactor: 컨트롤러명 변경

--- src/main/java/roomescape/controller/PageController.java
+++ src/main/java/roomescape/controller/PageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/admin")
 @Controller
-public class AdminController {
+public class PageController {
 
     @GetMapping
     public String admin() {

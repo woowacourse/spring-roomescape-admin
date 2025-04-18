4429badf40ffab01168628cadf6795ea994adac1
Han <jumdo12@gmail.com>
2025-04-18 17:16:13 +0900

    refactor(Controller): Reservation 저장 책임 분리

---
+++ src/main/java/roomescape/model/ReservationGroup.java
@@ -0,0 +1,36 @@
+package roomescape.model;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ReservationGroup {
+
+    private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(1);
+
+    public Long getIndexAndIncrement() {
+        return index.getAndIncrement();
+    }
+
+    public void addReservation(Reservation reservation) {
+        reservations.add(reservation);
+    }
+
+    public void deleteReservationById(Long id) {
+        Reservation deleteReservation = reservations.stream()
+                .filter(reservation -> Objects.equals(reservation.id(), id))
+                .findAny()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        reservations.remove(deleteReservation);
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+}

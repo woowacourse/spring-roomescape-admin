457264a6b7a0dc05900e1b9fed528cc327e63a93
Han <jumdo12@gmail.com>
2025-04-18 17:15:34 +0900

    feat: Dto가 domain으로 변환할 수 있도록 기능 추거

--- src/main/java/roomescape/dto/CreateReservationDto.java
+++ src/main/java/roomescape/dto/CreateReservationDto.java
@@ -1,7 +1,13 @@
 package roomescape.dto;
 
+import roomescape.model.Reservation;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record CreateReservationDto(String name, LocalDate date, LocalTime time) {
+
+    public Reservation convertToEntity(Long id) {
+        return new Reservation(id, name, date, time);
+    }
 }

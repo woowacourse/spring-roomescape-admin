ed1e2ad0eeef1912545f071dc02d35d79297a85f
geonwoo <kysub99@naver.com>
2025-04-19 17:52:01 +0900

    test: 테스트 목적별 분리 및 이름 변경

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -14,8 +15,22 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
+    private void 예약_생성(String name, String date, String time) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("date", date);
+        params.put("time", time);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200);
+    }
+
     @Test
-    void 일단계() {
+    void 관리_페이지_접근시_정상응답() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
@@ -23,7 +38,7 @@ public class MissionStepTest {
     }
 
     @Test
-    void 이단계() {
+    void 예약이_없을경우_목록이_비어있음() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
@@ -35,21 +50,21 @@ public class MissionStepTest {
                 .statusCode(200)
                 .body("size()", is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
     }
+
     @Test
-    void 삼단계() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
+    void 예약을_생성하면_목록에_포함() {
+        예약_생성("브라운", "2023-08-05", "15:40");
 
         RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-
-                .when().post("/reservations")
+                .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("id", is(1));
+                .body("size()", is(1));
+    }
+
+    @Test
+    void 예약을_생성하고_삭제_후_목록이_비어있음을_확인() {
+        예약_생성("브라운", "2023-08-05", "15:40");
 
         RestAssured.given().log().all()
                 .when().get("/reservations")

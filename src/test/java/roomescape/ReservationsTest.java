ae7efc03c2a82a2ff7dffee75dc203195228a7a8
ChoiDongjun <adong0808@naver.com>
2025-04-19 19:29:40 +0900

    test: 예약 조회, 삭제 테스트 추가

--- src/test/java/roomescape/ReservationsTest.java
+++ src/test/java/roomescape/ReservationsTest.java
@@ -1,23 +1,45 @@
 package roomescape;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 class ReservationsTest {
+
+    public Reservations createTestReservations() {
+        final Reservations reservations = new Reservations();
+        reservations.add(new Reservation(1L, "테스트", LocalDate.of(2025, 4, 16), LocalTime.of(12, 44)));
+
+        return reservations;
+    }
+
+    @Test
+    void 예약_조회_성공() {
+        //given, when
+        Reservations reservations = createTestReservations();
+
+        //then
+        assertThat(reservations.findById(1L)).isEqualTo(new Reservation(1L, "테스트", LocalDate.of(2025, 4, 16), LocalTime.of(12, 44)));
+    }
+
+    @Test
+    void 예약_조회_실패() {
+        //given, when
+        Reservations reservations = createTestReservations();
+
+        //then
+        assertThatThrownBy(() -> reservations.findById(2L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 예약입니다.");
+    }
 
     @Test
     void 예약_추가_실패() {
-        // given
-        final Reservations reservations = new Reservations();
-        final Long id = 1L;
-        final Reservation reservation = new Reservation(id, "테스트2", LocalDate.of(2026, 4, 16), LocalTime.of(13, 44));
-
-        // when
-        reservations.add(new Reservation(id, "테스트", LocalDate.of(2025, 4, 16), LocalTime.of(12, 44)));
+        //given, when
+        Reservations reservations = createTestReservations();
+        final Reservation reservation = new Reservation(1L, "테스트2", LocalDate.of(2026, 4, 16), LocalTime.of(13, 44));
 
         // then
         assertThatThrownBy(() -> reservations.add(reservation)).isInstanceOf(IllegalStateException.class);
@@ -25,14 +47,32 @@ class ReservationsTest {
 
     @Test
     void 예약_추가_성공() {
-        // given
-        final Reservations reservations = new Reservations();
-        final Reservation reservation = new Reservation(2L, "테스트2", LocalDate.of(2026, 4, 16), LocalTime.of(13, 44));
-
-        // when
-        reservations.add(new Reservation(1L, "테스트", LocalDate.of(2025, 4, 16), LocalTime.of(12, 44)));
+        //given, when
+        Reservations reservations = createTestReservations();
+        reservations.add(new Reservation(2L, "테스트2", LocalDate.of(2026, 4, 16), LocalTime.of(13, 44)));
 
         // then
-        assertThatCode(() -> reservations.add(reservation)).doesNotThrowAnyException();
+        assertThat(reservations.getReservations().size()).isEqualTo(2);
+    }
+
+    @Test
+    void 예약_삭제_성공() {
+        //given, when
+        Reservations reservations = createTestReservations();
+        reservations.removeById(1L);
+
+        //then
+        assertThat(reservations.getReservations().size()).isEqualTo(0);
+    }
+
+    @Test
+    void 예약_삭제_실패() {
+        //given, when
+        Reservations reservations = createTestReservations();
+
+        //then
+        assertThatThrownBy(() -> reservations.removeById(2L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재하지 않는 예약입니다.");
     }
 }

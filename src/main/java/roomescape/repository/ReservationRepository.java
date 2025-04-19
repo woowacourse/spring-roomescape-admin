18c752d4d08f209b66063abc7aa7524d64cf926e
soeun2537 <soeun2537@gmail.com>
2025-04-19 17:22:17 +0900

    refactor: ReservationRepository 인터페이스 구현

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -1,39 +1,16 @@
 package roomescape.repository;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.stereotype.Repository;
 import roomescape.entity.Reservation;
 
-@Repository
-public class ReservationRepository {
+public interface ReservationRepository {
 
-    private final List<Reservation> reservations = Collections.synchronizedList(new ArrayList<>());
-    private final AtomicLong index = new AtomicLong(1L);
+    List<Reservation> findAll();
 
-    public long getNextId() {
-        return index.getAndIncrement();
-    }
+    Optional<Reservation> findById(Long id);
 
-    public List<Reservation> findAll() {
-        return new ArrayList<>(reservations);
-    }
+    Reservation save(Reservation reservation);
 
-    public Optional<Reservation> findById(Long id) {
-        return reservations.stream()
-                .filter(reservation -> reservation.getId().equals(id))
-                .findFirst();
-    }
-
-    public Reservation save(Reservation reservation) {
-        reservations.add(reservation);
-        return reservation;
-    }
-
-    public void delete(Reservation reservation) {
-        reservations.remove(reservation);
-    }
+    void delete(Reservation reservation);
 }
---
+++ src/main/java/roomescape/repository/ReservationRepositoryImpl.java
@@ -0,0 +1,42 @@
+package roomescape.repository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Repository;
+import roomescape.entity.Reservation;
+
+@Repository
+public class ReservationRepositoryImpl implements ReservationRepository {
+
+    private final List<Reservation> reservations = Collections.synchronizedList(new ArrayList<>());
+    private final AtomicLong index = new AtomicLong(1L);
+
+    @Override
+    public List<Reservation> findAll() {
+        return new ArrayList<>(reservations);
+    }
+
+    @Override
+    public Optional<Reservation> findById(Long id) {
+        return reservations.stream()
+                .filter(reservation -> reservation.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public Reservation save(Reservation reservation) {
+        if (reservation.getId() == null) {
+            reservation.setId(index.getAndIncrement());
+        }
+        reservations.add(reservation);
+        return reservation;
+    }
+
+    @Override
+    public void delete(Reservation reservation) {
+        reservations.remove(reservation);
+    }
+}

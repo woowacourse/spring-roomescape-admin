cc91e09a54f34997ecb6a6993563401c3bc923ea
Han <jumdo12@gmail.com>
2025-04-20 08:44:24 +0900

    refactor: 메서드 시그니쳐 변경

--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -6,11 +6,11 @@ import java.time.LocalTime;
 public record Reservation(Long id, String name, LocalDate date, LocalTime time) {
 
     public Reservation {
-        validateNotNull(id, name, date, time);
+        validateReservation(id, name, date, time);
         validateName(name);
     }
 
-    private void validateNotNull(Long id, String name, LocalDate date, LocalTime time) {
+    private void validateReservation(Long id, String name, LocalDate date, LocalTime time) {
         if (id == null || name == null || date == null || time == null) {
             throw new IllegalArgumentException("Invalid reservation");
         }

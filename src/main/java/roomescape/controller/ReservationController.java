110120de7d33008e00267ada5a61eb469edac4bf
eunseongu <rndmstjs75@gmail.com>
2025-04-19 22:24:37 +0900

    refactor: @Controller를 @RestController로 변경하여 REST 응답 처리

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.reservation.Reservation;
 
-@Controller
+@RestController
 @RequestMapping("/reservations")
 public class ReservationController {
 

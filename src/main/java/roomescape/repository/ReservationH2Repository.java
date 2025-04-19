b193ad48cb759e1d640be3da4e05d3102762da85
Jin409 <sualng123@gmail.com>
2025-04-19 23:03:56 +0900

    feat(ReservationH2Repository): h2 데이터베이스 기반 저장 및 삭제 기능 구현

--- src/main/java/roomescape/repository/ReservationH2Repository.java
+++ src/main/java/roomescape/repository/ReservationH2Repository.java
@@ -1,8 +1,14 @@
 package roomescape.repository;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Time;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.Reservation;
 
@@ -14,7 +20,19 @@ public class ReservationH2Repository implements ReservationRepository {
 
     @Override
     public long save(final Reservation reservation) {
-        return 0L;
+        String query = "INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, reservation.getName());
+            ps.setDate(2, Date.valueOf(reservation.getDate()));
+            ps.setTime(3, Time.valueOf(reservation.getTime()));
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
     }
 
     @Override
@@ -33,5 +51,7 @@ public class ReservationH2Repository implements ReservationRepository {
 
     @Override
     public void deleteById(final long id) {
+        String query = "DELETE FROM reservation WHERE id = ?";
+        jdbcTemplate.update(query, id);
     }
 }

5c57fcefd8a9076ec6c979a3df2202673efa118e
2jin2.1031 <2jin2.1031@gmail.com>
2025-04-19 22:24:21 +0900

    test: 잘못된 예약 번호를 찾을 시의 예외 처리 단위 테스트 추가

---
+++ src/test/java/roomescape/database/ReservationDatabaseImplTest.java
@@ -0,0 +1,40 @@
+package roomescape.database;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import roomescape.domain.Reservation;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+class ReservationDatabaseImplTest {
+
+    @DisplayName("존재하지 않는 예약 ID로 조회하면 예외가 발생한다.")
+    @Test
+    void findById_throwsExceptionByNonExistentId() {
+        // given
+        String dummyName1 = "kali";
+        LocalDateTime dummyDateTime1 = LocalDateTime.now().plusDays(1);
+
+
+        String dummyName2 = "pobi";
+        LocalDateTime dummyDateTime2 = LocalDateTime.now().plusDays(2);
+
+        List<Reservation> reservations = List.of(
+                Reservation.of(dummyName1, dummyDateTime1.toLocalDate(), dummyDateTime1.toLocalTime()),
+                Reservation.of(dummyName2, dummyDateTime2.toLocalDate(), dummyDateTime2.toLocalTime())
+        );
+
+        ReservationDatabaseImpl db = new ReservationDatabaseImpl();
+        for (Reservation reservation : reservations) {
+            db.add(reservation);
+        }
+
+        // when & then
+        Assertions.assertThatCode(
+                () -> db.findById(3L)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}

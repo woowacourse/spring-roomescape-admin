783b715ad39b11eb000dbbc2c4d71b6b0985558a
kimdev <kim__dev@hufs.ac.kr>
2025-04-19 18:27:57 +0900

    refactor: 모든 Reservation을 반환할 때 방어적 복사 적용

--- src/main/java/roomescape/repository/MemoryReservationRepository.java
+++ src/main/java/roomescape/repository/MemoryReservationRepository.java
@@ -13,7 +13,7 @@ public class MemoryReservationRepository implements ReservationRepository {
 
     @Override
     public List<Reservation> findAll() {
-        return reservations;
+        return new ArrayList<>(reservations);
     }
 
     @Override

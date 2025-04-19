dffc63b39c84c0a697c01e4a680432392680ad28
seaniiio <cnc4750@naver.com>
2025-04-20 03:22:36 +0900

    refactor(Reservations): 동기화되는 자료구조 사용하도록 수정

--- src/main/java/roomescape/domain/Reservations.java
+++ src/main/java/roomescape/domain/Reservations.java
@@ -1,22 +1,21 @@
 package roomescape.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Reservations {
 
-    private final List<Reservation> reservations;
+    private final Queue<Reservation> reservations;
 
     public Reservations() {
-        this.reservations = new ArrayList<>();
+        this.reservations = new ConcurrentLinkedQueue<>();
     }
 
-    public synchronized void add(final Reservation reservation) {
+    public void add(final Reservation reservation) {
         reservations.add(reservation);
     }
 
-    public synchronized void deleteById(final Long id) {
+    public void deleteById(final Long id) {
         Reservation reservation = findById(id);
         reservations.remove(reservation);
     }
@@ -28,7 +27,7 @@ public class Reservations {
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 id에 대한 예약 기록이 존재하지 않습니다."));
     }
 
-    public synchronized List<Reservation> getReservations() {
-        return Collections.unmodifiableList(reservations);
+    public Queue<Reservation> getReservations() {
+        return new ConcurrentLinkedQueue<>(reservations);
     }
 }

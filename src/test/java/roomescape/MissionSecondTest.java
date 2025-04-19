dd84870a83b95014c183bc1c5ea0b15530f19d7e
Minuring <mw9505@gmail.com>
2025-04-19 17:06:31 +0900

    test: assert DB Catalog

--- src/test/java/roomescape/MissionSecondTest.java
+++ src/test/java/roomescape/MissionSecondTest.java
@@ -7,16 +7,12 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-@JdbcTest(
-    properties = {
-        "spring.datasource.generate-unique-name=false",
-        "spring.datasource.url=jdbc:h2:mem:database",
-        "spring.datasource.name=database"
-    }
-)
+@JdbcTest(properties = "spring.datasource.url=jdbc:h2:mem:database")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class MissionSecondTest {
 
     @Autowired
@@ -27,9 +23,8 @@ public class MissionSecondTest {
         try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
             assertAll(
                 () -> assertThat(connection).isNotNull(),
-//                () -> assertThat(connection.getCatalog()).isEqualTo("DATABASE"),
-                () -> assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null)
-                    .next()).isTrue()
+                () -> assertThat(connection.getCatalog()).isEqualTo("DATABASE"),
+                () -> assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue()
             );
         } catch (SQLException e) {
             throw new RuntimeException(e);

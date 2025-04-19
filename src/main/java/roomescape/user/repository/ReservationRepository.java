9423033acc41d1ccc95b7a8d6b8407ecf468dc3f
jeyong <joon6093@naver.com>
2025-04-19 13:07:58 +0900

    feat: Repository의 효율을 위해 HashMap을 사용하도록 변경

--- src/main/java/roomescape/user/repository/ReservationRepository.java
+++ src/main/java/roomescape/user/repository/ReservationRepository.java
@@ -1,8 +1,9 @@
 package roomescape.user.repository;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Repository;
 import roomescape.user.domain.Reservation;
@@ -10,31 +11,29 @@ import roomescape.user.domain.Reservation;
 @Repository
 public class ReservationRepository {
 
-    private final List<Reservation> reservations = new ArrayList<>();
+    private final Map<Long, Reservation> reservations = new ConcurrentHashMap<>();
     private final AtomicLong id = new AtomicLong(1);
 
     public Long save(final Reservation reservation) {
         final Long currentId = id.getAndIncrement();
-        
         final Reservation created = new Reservation(
                 currentId, reservation.getName(), reservation.getDate(), reservation.getTime());
-        reservations.add(created);
 
-        return created.getId();
+        reservations.put(currentId, created);
+        
+        return currentId;
     }
 
     public Optional<Reservation> findById(final Long id) {
-        return reservations.stream()
-                .filter(reservation -> reservation.getId().equals(id))
-                .findFirst();
+        return Optional.ofNullable(reservations.get(id));
     }
 
     public List<Reservation> findAll() {
-        return List.copyOf(reservations);
+        return List.copyOf(reservations.values());
     }
 
     public void delete(final Reservation reservation) {
-        reservations.remove(reservation);
+        reservations.remove(reservation.getId());
     }
 
     public void clear() {

cedc9bd0b53b52d048081f8bef6894d7aa16e399
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-04-19 19:59:56 +0900

    refactor: RoomescapeController 클래스명 변경

--- src/main/java/roomescape/controller/AdminController.java
+++ src/main/java/roomescape/controller/AdminController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class RoomescapeController {
+public class AdminController {
 
     @GetMapping("/admin")
     public String welcomeHome() {

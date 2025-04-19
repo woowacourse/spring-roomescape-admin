4c473c1dbeab6a2d808ec39962f4e16e7364cca5
2jin2.1031 <2jin2.1031@gmail.com>
2025-04-19 22:39:55 +0900

    test: 메서드명과 @DisplayName 내용 직관적으로 수정

--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -4,20 +4,22 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationTest {
 
     @Nested
-    @DisplayName("예약 시점이 현재보다 과거이면 예외가 발생해야한다.")
+    @DisplayName("예약 시점이 현재보다 과거이면 예외가 발생해야 한다.")
     class isPastTense {
 
         @DisplayName("예약 시점이 과거이면 예외가 발생한다.")
         @Test
-        void isPastTense_exception() {
+        void isPastTense_throwsExceptionByPastTime() {
             // given
             LocalDateTime pastDateTime = LocalDateTime.now().minusHours(1);
 
@@ -33,7 +35,7 @@ class ReservationTest {
 
         @DisplayName("예약 시점이 미래이면 예외를 발생하지 않는다.")
         @Test
-        void isPastTense_success() {
+        void isPastTense_doesNotThrowExceptionByFutureTime() {
             // given
             LocalDateTime futureDateTime = LocalDateTime.now().plusHours(1);
 

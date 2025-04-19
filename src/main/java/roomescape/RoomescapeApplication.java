8cbda5578489022b85fd4394dfced4b6f140184c
Jin409 <sualng123@gmail.com>
2025-04-19 22:24:51 +0900

    chore: h2 데이터베이스 세팅

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -1,12 +1,25 @@
 package roomescape;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class RoomescapeApplication {
+public class RoomescapeApplication implements CommandLineRunner {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+
     }
 
 }

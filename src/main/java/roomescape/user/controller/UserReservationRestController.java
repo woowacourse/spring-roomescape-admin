1800cca2c5cad3346ed93211502b0f0a62e51a16
jeyong <joon6093@naver.com>
2025-04-19 12:35:46 +0900

    refactor: RequestMapping을 이용하여 공통 uri를 관리하도록 변경

--- src/main/java/roomescape/user/controller/UserReservationRestController.java
+++ src/main/java/roomescape/user/controller/UserReservationRestController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.user.domain.Reservation;
 import roomescape.user.repository.ReservationRepository;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/reservations")
 public class UserReservationRestController {
 
     private final ReservationRepository reservationRepository;
@@ -25,14 +27,14 @@ public class UserReservationRestController {
         return ResponseEntity.badRequest().build();
     }
 
-    @GetMapping("/reservations")
+    @GetMapping
     public ResponseEntity<List<Reservation>> retrieveReservations() {
         final List<Reservation> reservations = reservationRepository.findAll();
 
         return ResponseEntity.ok(reservations);
     }
 
-    @PostMapping("/reservations")
+    @PostMapping
     public ResponseEntity<Reservation> persistReservation(@RequestBody final Reservation reservation) {
         final Long id = reservationRepository.save(reservation);
         final Optional<Reservation> found = reservationRepository.findById(id);
@@ -41,7 +43,7 @@ public class UserReservationRestController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable final Long id) {
         final Optional<Reservation> found = reservationRepository.findById(id);
         if (found.isEmpty()) {

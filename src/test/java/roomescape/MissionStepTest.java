7fc49c422c91cf860b1c07922445fc0c18791c3f
kjyyjk <rlawnsdud920@gmail.com>
2025-04-20 17:52:43 +0900

    test: 6단계 api 테스트 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -22,8 +22,8 @@ public class MissionStepTest {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    @Test
     @DisplayName("/ GET 요청에 응답한다")
+    @Test
     void welcome_page() {
         RestAssured.given().log().all()
             .when().get("/")
@@ -31,8 +31,8 @@ public class MissionStepTest {
             .statusCode(200);
     }
 
-    @Test
     @DisplayName("/admin GET 요청에 응답한다")
+    @Test
     void admin_page() {
         RestAssured.given().log().all()
             .when().get("/admin")
@@ -40,8 +40,8 @@ public class MissionStepTest {
             .statusCode(200);
     }
 
-    @Test
     @DisplayName("/admin/reservation GET 요청에 응답한다")
+    @Test
     void admin_reservation_page() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
@@ -49,18 +49,18 @@ public class MissionStepTest {
                 .statusCode(200);
     }
 
-    @Test
     @DisplayName("/reservations GET 요청에 정상적으로 응답한다")
+    @Test
     void reservations_api() {
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
+                .body("size()", is(0));
     }
 
-    @Test
     @DisplayName("/reservations POST 요청에 정상적으로 응답한다")
+    @Test
     void reservation_post_api() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
@@ -82,8 +82,8 @@ public class MissionStepTest {
             .body("size()", is(1));
     }
 
-    @Test
     @DisplayName("/reservations DELETE 요청에 정상적으로 응답한다")
+    @Test
     void reservation_delete_api() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
@@ -110,8 +110,8 @@ public class MissionStepTest {
             .body("size()", is(0));
     }
 
-    @Test
     @DisplayName("/reservations DELETE 요청에 존재하지 않는 자원이면 404로 응답한다")
+    @Test
     void reservation_delete_api_not_found() {
         RestAssured.given().log().all()
             .when().delete("/reservations/1")
@@ -119,8 +119,9 @@ public class MissionStepTest {
             .statusCode(404);
     }
 
+    @DisplayName("/reservations GET api는 DB로부터 데이터를 조회한다")
     @Test
-    void 오단계() {
+    void step_five() {
         String sql = "INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)";
         jdbcTemplate.update(sql, "브라운", "2023-08-05", "15:40");
 
@@ -133,5 +134,32 @@ public class MissionStepTest {
         Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
 
         assertThat(reservations.size()).isEqualTo(count);
+    }
+
+    @DisplayName("/reservations POST, DELETE api는 DB로부터 데이터를 삽입, 삭제한다")
+    @Test
+    void step_six() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200);
+
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+        assertThat(count).isEqualTo(1);
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
+
+        Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+        assertThat(countAfterDelete).isEqualTo(0);
     }
 }

4399bb185aa0dee1f09454a1f254018c46ff6b23
taek2222 <oot309@daum.net>
2025-04-20 20:31:42 +0900

    test: DB 테이블 레코드 수와 API 응답 크기 일치 테스트 작성

--- src/test/java/roomescape/controller/ReservationApiControllerTest.java
+++ src/test/java/roomescape/controller/ReservationApiControllerTest.java
@@ -1,17 +1,26 @@
 package roomescape.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.dto.ReservationResponse;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationApiControllerTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     private static void createAndSendReservation() {
         Map<String, String> params = createReservationData();
@@ -104,5 +113,20 @@ class ReservationApiControllerTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(0));
+    }
+
+    @Test
+    void DB_테이블_레코드_수와_API_응답_크기가_일치한다() {
+        jdbcTemplate.update("INSERT INTO reservation (name, datetime) VALUES (?, ?)", "브라운", "2023-08-05T15:40");
+
+        List<ReservationResponse> reservations = RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200).extract()
+                .jsonPath().getList(".", ReservationResponse.class);
+
+        Integer count = jdbcTemplate.queryForObject("SELECT count(*) from reservation", Integer.class);
+
+        assertThat(reservations.size()).isEqualTo(count);
     }
 }

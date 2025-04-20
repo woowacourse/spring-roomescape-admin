85fc75d6030f3eef8b3d06ab38b3e65b4c815201
mimi <110226866+hongkikii@users.noreply.github.com>
2025-04-20 17:48:35 +0900

    [1-3단계 - 방탈출 예약 관리] 미미(홍향미) 미션 제출합니다. (#249)
    * feat: 어드민 홈 화면 조회 기능 구현
    
    * feat: 예약 관리 페이지 반환
    
    * feat: 예약 추가/취소 기능 구현
    
    * chore: 미사용 라이브러리 import 삭제
    
    * refactor: @ResponseBody 어노테이션 제거
    
    * style: 메서드 네이밍 수정
    
    - reservations, allReservation -> allReservation으로 통일
    
    * refactor: reservationIndex 생성 위치 변경
    
    - Controller -> Reservation
    
    * style: 불필요 개행 제거
    
    * refactor: getter 추가
    
    * refactor: 클래스별 책임 분리 및 reservation 패키지 생성
    
    * refactor: 클래스 네이밍 수정 및 공통 경로 추출
    
    * refactor: 예외 처리 핸들러 추가
    
    * refactor: 불필요 코드 제거
    
    ---------
    
    Co-authored-by: minSsan <msp1125@hanyang.ac.kr>

---
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -0,0 +1,62 @@
+package roomescape.reservation;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+public final class Reservation {
+    private static final AtomicLong reservationIndex = new AtomicLong(1);
+
+    private final Long id;
+    private final String name;
+    private final LocalDate date;
+    private final LocalTime time;
+
+    public Reservation(String name, LocalDate date, LocalTime time) {
+        this.id = reservationIndex.getAndIncrement();
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public boolean hasSame(Long targetId) {
+        return id.equals(targetId);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (other == null || other.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (Reservation) other;
+        return Objects.equals(this.id, that.id) &&
+                Objects.equals(this.name, that.name) &&
+                Objects.equals(this.date, that.date) &&
+                Objects.equals(this.time, that.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, date, time);
+    }
+}
---
+++ src/main/java/roomescape/reservation/ReservationApiController.java
@@ -0,0 +1,37 @@
+package roomescape.reservation;
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationApiController {
+    private final Reservations reservations;
+
+    public ReservationApiController() {
+        this.reservations = new Reservations();
+    }
+
+    @PostMapping
+    public ResponseEntity<Reservation> add(@RequestBody ReservationDto reservationDto) {
+        return ResponseEntity.ok(reservations.add(reservationDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Reservation>> getAll() {
+        return ResponseEntity.ok(reservations.getAll());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        reservations.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}

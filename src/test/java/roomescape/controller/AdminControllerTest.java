835cb321a3010c98860c78bf9746af3fcd2d9ec7
Chaeyoung714 <202102760@hufs.ac.kr>
2025-04-20 23:43:24 +0900

    remove : remove unusing annotation

--- src/test/java/roomescape/controller/AdminControllerTest.java
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -10,12 +10,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import roomescape.RoomescapeApplication;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-@ContextConfiguration(classes = RoomescapeApplication.class)
 public class AdminControllerTest {
 
     @Test

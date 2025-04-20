9322356718c388e68dd96250cd15f634a6200700
taek2222 <oot309@daum.net>
2025-04-20 20:37:07 +0900

    refactor: 예약 전체 목록 API DB 연결로 변경

--- src/main/java/roomescape/controller/ReservationApiController.java
+++ src/main/java/roomescape/controller/ReservationApiController.java
@@ -1,9 +1,12 @@
 package roomescape.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,9 +26,24 @@ public class ReservationApiController {
     private final Reservations reservations = new Reservations();
     private final AtomicLong reservationId = new AtomicLong();
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping
     public List<ReservationResponse> getReservations() {
-        return reservations.getReservations().stream()
+        String sql = "select * from reservation";
+        List<Reservation> reservations = jdbcTemplate.query(
+                sql,
+                (resultSet, row) -> {
+                    return new Reservation(
+                            resultSet.getLong("id"),
+                            resultSet.getString("name"),
+                            resultSet.getObject("datetime", LocalDateTime.class)
+                    );
+                }
+        );
+
+        return reservations.stream()
                 .map(ReservationResponse::new)
                 .toList();
     }

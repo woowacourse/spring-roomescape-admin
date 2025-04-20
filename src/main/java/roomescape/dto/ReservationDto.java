338264c57983c1581492059029ea9a6d013181f6
jye326 <jye326@naver.com>
2025-04-20 23:21:20 +0900

    rename(model) : AddReservationDto

--- src/main/java/roomescape/dto/ReservationDto.java
+++
@@ -1,6 +0,0 @@
-package roomescape.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record ReservationDto(@JsonProperty(value = "name", defaultValue = "name") String name,@JsonProperty("date") String date,@JsonProperty("time") String time) {
-}

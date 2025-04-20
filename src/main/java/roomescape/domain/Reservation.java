19ed1d04db7c4663e7695cc85a4ba6a700dcf18d
kjungw <sagakkb@gmail.com>
2025-04-20 11:46:32 +0900

    refactor: 도메인의 입력값 검증 제거

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -3,7 +3,6 @@ package roomescape.domain;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Objects;
-import roomescape.exception.ValidationExceptionMessage;
 
 public final class Reservation {
 
@@ -13,7 +12,6 @@ public final class Reservation {
     private final LocalTime time;
 
     public Reservation(Long id, String name, LocalDate date, LocalTime time) {
-        validate(name, date, time);
         this.id = id;
         this.name = name;
         this.date = date;
@@ -34,30 +32,6 @@ public final class Reservation {
 
     public LocalTime getTime() {
         return time;
-    }
-
-    private void validate(String name, LocalDate date, LocalTime time) {
-        validateName(name);
-        validateDate(date);
-        validateTime(time);
-    }
-
-    private void validateName(String name) {
-        if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException(ValidationExceptionMessage.NULL_OR_BLANK_NAME.getContent());
-        }
-    }
-
-    private void validateDate(LocalDate date) {
-        if (date == null) {
-            throw new IllegalArgumentException(ValidationExceptionMessage.NULL_DATE.getContent());
-        }
-    }
-
-    private void validateTime(LocalTime time) {
-        if (time == null) {
-            throw new IllegalArgumentException(ValidationExceptionMessage.NULL_TIME.getContent());
-        }
     }
 
     @Override

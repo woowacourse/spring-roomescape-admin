ad392e9b7cccfbebd5327ff8791fa12fe98d22a7
rladmstn <oz33177351@gmail.com>
2025-04-20 16:54:10 +0900

    test: h2 연결 테스트 작성

---
+++ src/test/java/roomescape/JdbcConnectionTest.java
@@ -0,0 +1,26 @@
+package roomescape;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@JdbcTest
+public class JdbcConnectionTest {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void 사단계() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

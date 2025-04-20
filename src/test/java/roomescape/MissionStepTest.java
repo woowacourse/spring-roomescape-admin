ebf60d38a9b43eb72169460e463540e0b2ced228
ljhee92 <weilainib7@gmail.com>
2025-04-20 18:11:32 +0900

    refactor: 테스트 코드 테스트명 구체화

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,9 +1,8 @@
 package roomescape;
 
-import static org.hamcrest.Matchers.is;
-
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -11,12 +10,15 @@ import org.springframework.test.annotation.DirtiesContext;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.Matchers.is;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
     @Test
-    void 일단계() {
+    @DisplayName("GET /admin 요청시 어드민 메인 페이지를 응답한다")
+    void admin_요청시_어드민_메인_페이지를_응답한다() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
@@ -24,21 +26,27 @@ public class MissionStepTest {
     }
 
     @Test
-    void 이단계() {
+    @DisplayName("GET /admin/reservation 요청시 예약관리 페이지를 응답한다")
+    void admin_reservation_요청시_예약관리_페이지를_응답한다() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
+    }
 
+    @Test
+    @DisplayName("GET /reservations 요청시 모든 예약을 응답한다")
+    void get_reservations_요청시_모든_예약을_응답한다() {
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()",is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
+                .body("size()",is(0));
     }
 
     @Test
-    void 삼단계() {
+    @DisplayName("POST /reservations 요청시 예약을 추가한다")
+    void post_reservations_요청시_예약을_추가한다() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
@@ -57,6 +65,23 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
+    }
+
+    @Test
+    @DisplayName("DELETE /reservations/{id} 요청시 예약을 삭제한다")
+    void delete_reservations_요청시_예약을_추가하고_삭제한다() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
 
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")

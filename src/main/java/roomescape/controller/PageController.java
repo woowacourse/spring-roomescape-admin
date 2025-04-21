eb9c55e913f1e1cbe1fe8e60241aae31d8b771c8
jhpark1227 <junhyeok0246@naver.com>
2025-04-22 00:22:32 +0900

    feat: 방탈출 예약 페이지의 템플릿 변경

--- src/main/java/roomescape/controller/PageController.java
+++ src/main/java/roomescape/controller/PageController.java
@@ -13,7 +13,7 @@ public class PageController {
 
     @GetMapping("/admin/reservation")
     public String reservation() {
-        return "admin/reservation-legacy";
+        return "admin/reservation";
     }
 
     @GetMapping("/admin/time")

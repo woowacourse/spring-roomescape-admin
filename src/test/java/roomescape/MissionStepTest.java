debebd36b795fb7d582da7318c5bc1b8e6df0666
MinJun Choi <mj043000@naver.com>
2025-04-21 21:38:03 +0900

    refactor: 테스트메서드명 변경

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -78,6 +78,14 @@ public class MissionStepTest {
     }
 
     @Test
+    void 삼단계_없는예약_삭제하기() {
+        RestAssured.given().log().all()
+                .when().delete("/reservations/123")
+                .then().log().all()
+                .statusCode(404);
+    }
+
+    @Test
     void 사단계() {
         try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
             assertThat(connection).isNotNull();

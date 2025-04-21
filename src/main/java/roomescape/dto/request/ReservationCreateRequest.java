867bace5df15bb52df8c6432807c5cc1ca268bef
Dompoo <dlckdrms0517@gmail.com>
2025-04-21 13:00:37 +0900

    fix: 예약 생성 요청 수정

--- src/main/java/roomescape/dto/request/ReservationCreateRequest.java
+++ src/main/java/roomescape/dto/request/ReservationCreateRequest.java
@@ -3,13 +3,12 @@ package roomescape.dto.request;
 import org.springframework.util.ObjectUtils;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.Map;
 
 public record ReservationCreateRequest(
         String name,
         LocalDate date,
-        LocalTime time
+        long timeId
 ) {
     public ReservationCreateRequest {
         if (ObjectUtils.isEmpty(name)) {
@@ -18,16 +17,13 @@ public record ReservationCreateRequest(
         if (date == null) {
             throw new IllegalArgumentException("날짜는 필수값입니다.");
         }
-        if (time == null) {
-            throw new IllegalArgumentException("시간은 필수값입니다.");
-        }
     }
 
     public Map<String, ?> dataMap() {
         return Map.of(
                 "name", name,
                 "date", date,
-                "time", time
+                "time_id", timeId
         );
     }
 }

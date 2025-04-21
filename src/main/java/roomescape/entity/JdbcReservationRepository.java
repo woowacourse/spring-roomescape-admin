1280e41b7a323da7ef49430744ed5092f75c5f43
(minjae8563) <(minjae8563@naver.com)>
2025-04-21 14:06:10 +0900

    feat: Jdbc 를 연동하여 데이터를 CRUD 하는 객체 기능 구현

---
+++ src/main/java/roomescape/entity/JdbcReservationRepository.java
@@ -0,0 +1,62 @@
+package roomescape.entity;
+
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class JdbcReservationRepository implements ReservationRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcReservationRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void saveReservation(Reservation wantToSaveReservation) {
+        String query = "INSERT INTO RESERVATION (name, date, time) values (?,?,?)";
+        jdbcTemplate.update(query, wantToSaveReservation.getName(), wantToSaveReservation.getDate(), wantToSaveReservation.getTime());
+    }
+
+    @Override
+    public int deleteReservation(Long wantToDeleteId) {
+        String query = "DELETE FROM RESERVATION WHERE id = ?";
+        return jdbcTemplate.update(query, Long.valueOf(wantToDeleteId));
+    }
+
+    @Override
+    public Reservation findReservationById(Long wandToFindId) {
+        String query = "String select id, name, date, time FROM RESERVATION";
+        return jdbcTemplate.queryForObject(
+                query,
+                (rs, rowNum) -> {
+                    Reservation reservation = new Reservation(
+                            rs.getLong("id"),
+                            rs.getString("name"),
+                            rs.getDate("date").toLocalDate(),
+                            rs.getTime("time").toLocalTime()
+                    );
+                    return reservation;
+                }
+                ,wandToFindId
+        );
+    }
+
+    @Override
+    public List<Reservation> findAllReservations() {
+        String sql = "SELECT id, name, date, time FROM RESERVATION";
+
+        return jdbcTemplate.query(
+                sql,
+                (result, rowNum) -> {
+                    Reservation reservation = new Reservation(
+                            result.getLong("id"),
+                            result.getString("name"),
+                            result.getDate("date").toLocalDate(),
+                            result.getTime("time").toLocalTime()
+                    );
+                    return reservation;
+                }
+        );
+    }
+
+}
---
+++ src/main/java/roomescape/entity/ReservationRepository.java
@@ -0,0 +1,10 @@
+package roomescape.entity;
+
+import java.util.List;
+
+public interface ReservationRepository {
+    public void saveReservation(Reservation wantToSaveReservation);
+    public int deleteReservation(Long wantToDeleteId);
+    public Reservation findReservationById(Long wandToFindId);
+    public List<Reservation> findAllReservations();
+}

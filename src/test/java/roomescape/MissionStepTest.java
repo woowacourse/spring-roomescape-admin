854a69f15d8dbdd13f67f70646629606b2d45f48
yesjuhee <juhee0924@g.skku.edu>
2025-04-21 22:31:44 +0900

    feat: h2 데이터베이스 적용

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,21 +1,29 @@
 package roomescape;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
-    @DisplayName("홈페이지에 접근할 수 있다.")
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @DisplayName("1단계: 홈페이지에 접근할 수 있다.")
     @Test
     void testHomePage() {
         RestAssured.given().log().all()
@@ -24,7 +32,7 @@ public class MissionStepTest {
                 .statusCode(200);
     }
 
-    @DisplayName("예약을 조회할 수 있다.")
+    @DisplayName("2단계: 예약을 조회할 수 있다.")
     @Test
     void testGetReservationPage() {
         RestAssured.given().log().all()
@@ -39,7 +47,7 @@ public class MissionStepTest {
                 .body("size()", is(0));
     }
 
-    @DisplayName("예약을 추가하고 취소할 수 있다.")
+    @DisplayName("3단계: 예약을 추가하고 취소할 수 있다.")
     @Test
     void testCreateDeleteReservation() {
         Map<String, String> params = new HashMap<>();
@@ -71,5 +79,18 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(0));
+    }
+
+
+    @DisplayName("4단계: 데이터베이스를 적용할 수 있다.")
+    @Test
+    void testDatabaseConnection() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

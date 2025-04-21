b474acbbb3de5c6b18b714bd28b982d67a73677c
DongchannN <96824025+DongchannN@users.noreply.github.com>
2025-04-21 15:58:34 +0900

    [1-3단계 - 방탈출 예약 관리] 차니(이동찬) 미션 제출합니다. (#256)
    * feat: 홈 화면 반환
    
    * feat: 얘약 기능 구현
    
    * refactor: Controller와 저장소 분리
    
    * refactor: 테스트명 추가
    
    * refactor: 공백 삭제
    
    * feat: 입력 검증 추가
    
    * docs(README.md): README 작성
    
    * refactor: 불필요한 에노테이션 제거
    
    * refactor: 어노테이션 수정 및 추가
    
    * test: 예약 관련 테스트 케이스 추가

---
+++ src/main/java/roomescape/AdminController.java
@@ -0,0 +1,20 @@
+package roomescape;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin")
+@Controller
+public class AdminController {
+
+    @GetMapping
+    public String getAdminPage() {
+        return "admin/index";
+    }
+
+    @GetMapping("/reservation")
+    public String getReservation() {
+        return "admin/reservation-legacy";
+    }
+}
---
+++ src/test/java/roomescape/ReservationTest.java
@@ -0,0 +1,188 @@
+package roomescape;
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReservationTest {
+
+    @Test
+    @DisplayName("예약을 성공적으로 추가한다")
+    void createReservationTest() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        // when, then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1));
+    }
+
+    @Test
+    @DisplayName("필드의 형식이 맞지 않다면 예외가 발생한다")
+    void reservationFormatDoesntMatchTest() {
+        // given
+        Map<String, String> invalidDate = new HashMap<>();
+        invalidDate.put("name", "브라운");
+        invalidDate.put("date", "20230-08-05");
+        invalidDate.put("time", "15:40");
+
+        Map<String, String> invalidTime = new HashMap<>();
+        invalidTime.put("name", "브라운");
+        invalidTime.put("date", "2023-08-05");
+        invalidTime.put("time", "15:40.");
+
+        // when, then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(invalidDate)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(invalidTime)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400);
+    }
+
+    @Test
+    @DisplayName("예약 생성 시 필드가 비어있다면 예외가 발생한다")
+    void reservationNameBlankTest() {
+        // given
+        Map<String, String> blankName = new HashMap<>();
+        blankName.put("name", " ");
+        blankName.put("date", "2023-08-05");
+        blankName.put("time", "15:40");
+
+        Map<String, String> blankDate = new HashMap<>();
+        blankDate.put("name", "브라운");
+        blankDate.put("date", "");
+        blankDate.put("time", "15:40");
+
+        Map<String, String> blankTime = new HashMap<>();
+        blankTime.put("name", "브라운");
+        blankTime.put("date", "2023-08-05");
+        blankTime.put("time", "");
+
+        // when, then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(blankName)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(blankDate)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(blankTime)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400);
+    }
+
+    @Test
+    @DisplayName("예약 생성 시 필드가 없다면 예외가 발생한다")
+    void reservationDateOrTimeNullTest() {
+        // given
+        Map<String, String> noName = new HashMap<>();
+        noName.put("date", "2023-08-05");
+        noName.put("time", "15:40");
+
+        Map<String, String> noDate = new HashMap<>();
+        noDate.put("name", "브라운");
+        noDate.put("time", "15:40");
+
+        Map<String, String> noTime = new HashMap<>();
+        noTime.put("name", "브라운");
+        noName.put("date", "2023-08-05");
+
+        // when, then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(noName)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(noDate)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(noTime)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400);
+    }
+
+    @Test
+    @DisplayName("모든 예약을 조회한다.")
+    void getAllReservations() {
+        RestAssured.given().log().all()
+                .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @Test
+    @DisplayName("예약을 삭제한다")
+    void deleteReservationTest() {
+        // given
+        createReservationTest();
+
+        // when, then
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 예약 삭제 시 예외가 발생한다")
+    void deleteNonExistReservationTest() {
+        // given
+        createReservationTest();
+
+        // when, then
+        RestAssured.given().log().all()
+                .when().delete("/reservations2")
+                .then().log().all()
+                .statusCode(404);
+    }
+}

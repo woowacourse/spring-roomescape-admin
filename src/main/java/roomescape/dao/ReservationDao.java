3b85b5e91c5af6c97a7ce140dfea67f6605b25bb
LJM\eueo8 <eueo8259@gmail.com>
2025-04-22 00:02:51 +0900

    feat : id로 reservation 찾는 메서드 구현

--- src/main/java/roomescape/dao/ReservationDao.java
+++ src/main/java/roomescape/dao/ReservationDao.java
@@ -1,10 +1,15 @@
 package roomescape.dao;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.Time;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import roomescape.model.Reservation;
 import roomescape.model.ReservationDateTime;
@@ -32,15 +37,28 @@ public class ReservationDao {
         return jdbcTemplate.query(sql, actorRowMapper);
     }
 
-    public void saveReservation(Reservation reservation) {
-        String sql = "INSERT INTO reservation (name, date, time) value(?,?,?)";
-        jdbcTemplate.update(sql, reservation.getName(), reservation.getDate(), reservation.getTime());
+    public Long saveReservation(Reservation reservation) {
+        String sql = "INSERT INTO reservation (name, date, time) values (?,?,?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, reservation.getName());
+            ps.setDate(2, Date.valueOf(reservation.getDate()));
+            ps.setTime(3, Time.valueOf(reservation.getTime()));
+            return ps;
+        }, keyHolder);
+        return keyHolder.getKey().longValue();
     }
 
-    public Reservation findByNameAndDateTime(Reservation reservation) {
-        String sql = "SELECT * FROM reservation WHERE (name, date, time) value(?,?,?)";
-        Reservation findReservation = jdbcTemplate.queryForObject(
-                sql, actorRowMapper,reservation.getName(), reservation.getDate(), reservation.getTime());
-        return findReservation;
+    public Reservation findReservationById(Long id) {
+        String sql = "SELECT * FROM reservation WHERE id = ?";
+        Reservation reservation = jdbcTemplate.queryForObject(sql, actorRowMapper, id);
+        return reservation;
+    }
+
+    public void deleteById(Long id) {
+        String sql = "DELETE FROM reservation WHERE id = ?";
+        jdbcTemplate.update(sql, id);
     }
 }

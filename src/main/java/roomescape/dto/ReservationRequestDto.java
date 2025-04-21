5f1122bd35290394b38b9a8f06616cd3ea63a7d8
LJM\eueo8 <eueo8259@gmail.com>
2025-04-21 18:05:18 +0900

    feat: Reservation으로 전환하는 메서드 추가

--- src/main/java/roomescape/dto/ReservationRequestDto.java
+++ src/main/java/roomescape/dto/ReservationRequestDto.java
@@ -1,10 +1,20 @@
 package roomescape.dto;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import roomescape.model.Reservation;
+import roomescape.model.ReservationDateTime;
 
-public record ReservationRequestDto (
+public record ReservationRequestDto(
         String name,
         LocalDate date,
         LocalTime time
-){}
+) {
+    public Reservation convertToReservation() {
+        return new Reservation(
+                this.name,
+                new ReservationDateTime(LocalDateTime.of(this.date, this.time))
+        );
+    }
+}

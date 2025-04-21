5869eefcfc6576306ec3f85e295f313c23688963
(minjae8563) <(minjae8563@naver.com)>
2025-04-21 14:06:52 +0900

    feat: ReservationRepository 빈에 등록 로직 구현

--- src/main/java/roomescape/AppConfig.java
+++ src/main/java/roomescape/AppConfig.java
@@ -1,16 +1,28 @@
 package roomescape;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import roomescape.entity.JdbcReservationRepository;
+import roomescape.entity.ReservationRepository;
 import roomescape.entity.Reservations;
 
 @Configuration
 public class AppConfig {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Bean
     public Reservations reservations() {
         return new Reservations(List.of());
+    }
+
+    @Bean
+    public ReservationRepository reservationRepository() {
+        return new JdbcReservationRepository(jdbcTemplate);
     }
 
 }

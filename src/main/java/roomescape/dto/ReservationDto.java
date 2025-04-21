94f36a6ee836b0a319352cff73302c450c8a3c7e
jhpark1227 <junhyeok0246@naver.com>
2025-04-22 00:21:38 +0900

    fix: ReservationDto 필드명 수정

--- src/main/java/roomescape/dto/ReservationDto.java
+++ src/main/java/roomescape/dto/ReservationDto.java
@@ -1,16 +1,15 @@
 package roomescape.dto;
 
-import java.time.format.DateTimeFormatter;
 import roomescape.domain.Reservation;
 
-public record ReservationDto(Long id, String name, String date, String time) {
+public record ReservationDto(Long id, String name, String date, ReservationTimeDto time) {
 
     public static ReservationDto from(final Reservation reservation) {
         return new ReservationDto(
                 reservation.getId(),
                 reservation.getName(),
                 reservation.getDate().toString(),
-                reservation.getReservationTime().getStartAt().format(DateTimeFormatter.ofPattern("HH:mm"))
+                ReservationTimeDto.from(reservation.getReservationTime())
         );
     }
 }

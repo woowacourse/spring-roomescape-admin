150b1f554f0d5c019a5058ee8092c8be30e5e120
Yeongjae Kim <sees77770@gmail.com>
2025-04-21 15:43:47 +0900

    test: 예약자 이름 검증 테스트 추가

--- src/test/java/roomescape/reservation/domain/ReservationTest.java
+++ src/test/java/roomescape/reservation/domain/ReservationTest.java
@@ -6,9 +6,21 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import roomescape.reservation.domain.exception.PastReservationException;
+import roomescape.reservation.domain.exception.ReserverNameEmptyException;
 
 public class ReservationTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" "})
+    void 예약자_이름은_비어있거나_null일_수_없다(String input) {
+        assertThatThrownBy(() -> new ReserverName(input))
+                .isInstanceOf(ReserverNameEmptyException.class);
+    }
 
     @Test
     void 과거_시간으로_예약할_수_없다() {

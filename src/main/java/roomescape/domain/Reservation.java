7605cf94d6ee1f658aee2d1dab8877148d9513fc
taek2222 <oot309@daum.net>
2025-04-22 00:24:43 +0900

    feat: 이름 길이 검증 추가

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -12,20 +12,27 @@ public class Reservation {
     private final LocalDateTime dateTime;
 
     public Reservation(Long id, String name, LocalDateTime dateTime) {
+        validateNameLength(name);
         this.id = id;
         this.name = name;
         this.dateTime = dateTime;
     }
 
     public Reservation(Long id, String name, LocalDate date, LocalTime time) {
-        LocalDateTime dateTime = LocalDateTime.of(date, time);
-        this.id = id;
-        this.name = name;
-        this.dateTime = dateTime;
+        this(id, name, LocalDateTime.of(date, time));
     }
 
     public String formatDateTime(DateTimeFormatter formatter) {
         return dateTime.format(formatter);
+    }
+
+    private void validateNameLength(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 빈칸이거나 NULL일 수 없습니다.");
+        }
+        if (name.length() > 255) {
+            throw new IllegalArgumentException("이름의 길이는 " + 255 + " 초과할 수 없습니다.");
+        }
     }
 
     public Long getId() {

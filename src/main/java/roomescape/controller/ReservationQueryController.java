15a8430fbfd507becdb6baca8948c84e4f4916dd
ppparkta <pppppark2@gmail.com>
2025-04-21 14:19:00 +0900

    feat: welcome 페이지 추가

--- src/main/java/roomescape/controller/ReservationQueryController.java
+++ src/main/java/roomescape/controller/ReservationQueryController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ReservationQueryController {
 
-    @GetMapping("admin")
+    @GetMapping("")
     public String home() {
+        return "home/index";
+    }
+
+    @GetMapping("admin")
+    public String admin() {
         return "admin/index";
     }
 

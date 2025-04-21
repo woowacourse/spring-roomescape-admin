1edda1537fcb63e0b9597e2dfd0e4ee38953467b
Dompoo <dlckdrms0517@gmail.com>
2025-04-21 11:23:34 +0900

    feat: ReservationTimeResponse 구현

--- src/main/java/roomescape/dto/response/ReservationTimeResponse.java
+++ src/main/java/roomescape/dto/response/ReservationTimeResponse.java
@@ -3,16 +3,21 @@ package roomescape.dto.response;
 import roomescape.ReservationTime;
 import roomescape.dto.request.ReservationTimeCreateRequest;
 
+import java.time.format.DateTimeFormatter;
+
 public record ReservationTimeResponse(
         Long id,
         String startAt
 ) {
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
 
     public static ReservationTimeResponse from(final ReservationTime times) {
-        return null;
+        String startTime = TIME_FORMATTER.format(times.startTime());
+        return new ReservationTimeResponse(times.id(), startTime);
     }
 
     public static ReservationTimeResponse from(final ReservationTimeCreateRequest request, final long savedId) {
-        return null;
+        String startTime = TIME_FORMATTER.format(request.startAt());
+        return new ReservationTimeResponse(savedId, startTime);
     }
 }

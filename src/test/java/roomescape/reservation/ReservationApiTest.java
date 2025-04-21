2139e64509ccd68d2cb3e68a371cdf48daae1e68
Firedrago95 <yonghwa95@naver.com>
2025-04-21 14:24:53 +0900

    refactor: 테스트명 수정

--- src/test/java/roomescape/reservation/ReservationApiTest.java
+++ src/test/java/roomescape/reservation/ReservationApiTest.java
@@ -1,16 +1,22 @@
 package roomescape.reservation;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
@@ -18,27 +24,27 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 class ReservationApiTest {
 
-    @DisplayName("어드민 페이지로 접근할 수 있다.")
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
-    void test1() {
+    void 어드민_페이지로_접근할_수_있다() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
                 .statusCode(200);
     }
 
-    @DisplayName("어드민이 예약 관리 페이지에 접근한다.")
     @Test
-    void test2() {
+    void 어드민이_예약_관리_페이지에_접근한다() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
     }
 
-    @DisplayName("모든 예약 정보를 반환한다.")
     @Test
-    void test3() {
+    void 모든_예약_정보를_반환한다() {
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
@@ -46,9 +52,8 @@ class ReservationApiTest {
                 .body("size()", is(0));
     }
 
-    @DisplayName("예약 정보를 추가한다.")
     @Test
-    void test4() {
+    void 예약_정보를_추가한다() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
@@ -72,9 +77,8 @@ class ReservationApiTest {
     @DisplayName("예약 삭제")
     @Nested
     class delete{
-        @DisplayName("예약을 삭제한다.")
         @Test
-        void test1() {
+        void 예약을_삭제한다() {
             Map<String, String> params = new HashMap<>();
             params.put("name", "브라운");
             params.put("date", "2023-08-05");
@@ -100,9 +104,9 @@ class ReservationApiTest {
                     .body("size()", is(0));
         }
 
-        @DisplayName("존재하지 않는 예약을 삭제할 경우 NOT_FOUND 반환")
+
         @Test
-        void test2(){
+        void 존재하지_않는_예약을_삭제할_경우_NOT_FOUND_반환(){
             RestAssured.given().log().all()
                     .when().delete("/reservations/4")
                     .then().log().all()

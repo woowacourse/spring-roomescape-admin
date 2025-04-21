6580e257d689247af0f08e2b75e7c773f3ea88a1
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-04-21 16:16:38 +0900

    [1-3단계 - 방탈출 예약 관리] 띠용(주용은) 미션 제출합니다. (#222)

---
+++ src/main/java/roomescape/controller/RoomescapeController.java
@@ -0,0 +1,60 @@
+package roomescape.controller;
+
+import java.util.List;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import roomescape.dto.AddReservationDto;
+import roomescape.model.Reservation;
+import roomescape.model.Reservations;
+
+@Controller
+public class RoomescapeController {
+    private final Reservations reservations;
+    private AtomicLong index = new AtomicLong(1);
+
+
+    public RoomescapeController(Reservations reservations) {
+        this.reservations = reservations;
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin/index";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String adminReservation() {
+        return "admin/reservation-legacy";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> reservations() {
+        return ResponseEntity.ok(reservations.getReservations());
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> addReservation(@RequestBody AddReservationDto addReservationDto) {
+        Reservation newReservation = AddReservationDto.toEntity(getIndexAndIncrement(), addReservationDto);
+        reservations.addReservation(newReservation);
+        return ResponseEntity.ok().body(newReservation);
+    }
+
+    public Long getIndexAndIncrement() {
+        return index.getAndIncrement();
+    }
+
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Reservation> deleteReservation(@PathVariable long id) {
+        Reservation oldReservation = reservations.deleteReservation(id);
+        return ResponseEntity.ok().body(oldReservation);
+    }
+
+}

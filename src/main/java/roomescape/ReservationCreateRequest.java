a627343328730fe96262fdd1535c60c26f4c9f1f
ChoiDongjun <adong0808@naver.com>
2025-04-21 22:40:48 +0900

    feat: ReservationCreateRequest에서 Reservation 엔티티로 매핑 기능 추가

--- src/main/java/roomescape/ReservationCreateRequest.java
+++ src/main/java/roomescape/ReservationCreateRequest.java
@@ -2,8 +2,14 @@ package roomescape;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import roomescape.entity.Reservation;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record ReservationCreateRequest(@NotBlank String name, @NotNull LocalDate date, @NotNull LocalTime time) {
+
+    public Reservation mapToReservation(final Long id) {
+        return new Reservation(id, name, date, time);
+    }
 }

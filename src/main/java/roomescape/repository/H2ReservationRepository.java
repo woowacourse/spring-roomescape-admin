5fd5bcc7a8ba46ef00ddc1c68b0f834eb2896623
soeun2537 <soeun2537@gmail.com>
2025-04-21 16:55:48 +0900

    feat: H2ReservationRepository 구현 및 빈 등록

---
+++ src/main/java/roomescape/repository/H2ReservationRepository.java
@@ -0,0 +1,77 @@
+package roomescape.repository;
+
+import java.sql.PreparedStatement;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.Reservation;
+
+@Repository
+public class H2ReservationRepository implements ReservationRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public H2ReservationRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Reservation> findAll() {
+        String sql = "select id, name, date, time from reservation";
+        return jdbcTemplate.query(
+                sql,
+                (resultSet, rowNum) -> new Reservation(
+                        resultSet.getLong("id"),
+                        resultSet.getString("name"),
+                        LocalDate.parse(resultSet.getString("date")),
+                        LocalTime.parse(resultSet.getString("time"))
+                ));
+    }
+
+    @Override
+    public Optional<Reservation> findById(Long id) {
+        String sql = "select id, name, date, time from reservation";
+        return Optional.ofNullable(
+                jdbcTemplate.queryForObject(
+                        sql,
+                        (resultSet, rowNum) -> new Reservation(
+                                resultSet.getLong("id"),
+                                resultSet.getString("name"),
+                                LocalDate.parse(resultSet.getString("date")),
+                                LocalTime.parse(resultSet.getString("time"))
+                        )
+                ));
+    }
+
+    @Override
+    public Reservation save(Reservation reservation) {
+        String sql = "insert into reservation (name, date, time) values (?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    sql,
+                    new String[]{"id"});
+            ps.setString(1, reservation.getName());
+            ps.setString(2, reservation.getDate().toString());
+            ps.setString(3, reservation.getTime().toString());
+            return ps;
+        }, keyHolder);
+
+        long id = keyHolder.getKey().longValue();
+
+        return new Reservation(id, reservation.getName(), reservation.getDate(), reservation.getTime());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        String sql = "delete from reservation where id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+}
---
+++ src/test/java/roomescape/H2ReservationRepositoryTest.java
@@ -0,0 +1,112 @@
+package roomescape;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import roomescape.domain.Reservation;
+import roomescape.repository.H2ReservationRepository;
+import roomescape.repository.ReservationRepository;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class H2ReservationRepositoryTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    private ReservationRepository reservationRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        reservationRepository = new H2ReservationRepository(jdbcTemplate);
+    }
+
+    @Test
+    void connectDatabase() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @DisplayName("예약 저장을 테스트한다.")
+    void save() {
+        // given
+        Reservation reservation = new Reservation(
+                null,
+                "미소",
+                LocalDate.of(2025, 4, 21),
+                LocalTime.of(10, 0)
+        );
+
+        // when
+        Reservation saved = reservationRepository.save(reservation);
+
+        // then
+        Assertions.assertThat(saved.getName()).isEqualTo(reservation.getName());
+        Assertions.assertThat(saved.getDate()).isEqualTo(reservation.getDate());
+        Assertions.assertThat(saved.getTime()).isEqualTo(reservation.getTime());
+    }
+
+    @Test
+    @DisplayName("모든 예약을 조회한다.")
+    void findAll() {
+        // given
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)",
+                "브라운", "2023-08-05", "15:40");
+
+        // when
+        List<Reservation> reservations = reservationRepository.findAll();
+
+        // then
+        Reservation expected = new Reservation(1L, "브라운", LocalDate.of(2023, 8, 5), LocalTime.of(15, 40));
+        Assertions.assertThat(reservations.size()).isEqualTo(1);
+        Assertions.assertThat(reservations.getFirst()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("id로 예약을 조회한다.")
+    void findById() {
+        // given
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)",
+                "브라운", "2023-08-05", "15:40");
+
+        // when
+        Optional<Reservation> reservation = reservationRepository.findById(1L);
+
+        // then
+        Reservation expected = new Reservation(1L, "브라운", LocalDate.of(2023, 8, 5), LocalTime.of(15, 40));
+        Assertions.assertThat(reservation.get()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("id로 예약을 삭제한다.")
+    void deleteById() {
+        // given
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)",
+                "브라운", "2023-08-05", "15:40");
+
+        // when
+        reservationRepository.deleteById(1L);
+
+        // then
+        List<Reservation> reservations = reservationRepository.findAll();
+        Assertions.assertThat(reservations).isEmpty();
+    }
+}

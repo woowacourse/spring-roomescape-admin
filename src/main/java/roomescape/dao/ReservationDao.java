46d114719c6ff976be0f3287a263db031a58addb
LJM\eueo8 <eueo8259@gmail.com>
2025-04-21 18:02:28 +0900

    feat: ReservationDao 생성 및 findAll메서드 및 saveReservation메서드 구현

---
+++ src/main/java/roomescape/dao/ReservationDao.java
@@ -0,0 +1,34 @@
+package roomescape.dao;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import roomescape.model.Reservation;
+import roomescape.model.ReservationDateTime;
+
+@Repository
+public class ReservationDao {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<Reservation> findAll() {
+        String sql = "SELECT * FROM reservation";
+        return jdbcTemplate.query(sql,(resultSet, rowNum) -> {
+            Reservation reservation = new Reservation(
+                    resultSet.getLong("id"),
+                    resultSet.getString("name"),
+                    new ReservationDateTime(LocalDateTime.of(
+                            resultSet.getDate("date").toLocalDate(),
+                            resultSet.getTime("time").toLocalTime()
+                    )));
+            return reservation;
+        });
+    }
+
+    public void saveReservation(Reservation reservation){
+        String sql = "INSERT INTO reservation (name, date, time) value(?,?,?)";
+        jdbcTemplate.update(sql,reservation.getName(), reservation.getDate(), reservation.getTime());
+    }
+}

84ba9a73c01e0dbe2a121a9a14b38dbe48617ca1
jhpark1227 <junhyeok0246@naver.com>
2025-04-21 20:22:37 +0900

    feat: 방탈출 시간표 페이지 구현

--- src/main/java/roomescape/controller/PageController.java
+++ src/main/java/roomescape/controller/PageController.java
@@ -15,4 +15,9 @@ public class PageController {
     public String reservation() {
         return "admin/reservation-legacy";
     }
+
+    @GetMapping("/admin/time")
+    public String timePage() {
+        return "admin/time";
+    }
 }

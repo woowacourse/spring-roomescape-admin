e00c34856354b9c255d55b7dfc9d841834fa601f
soeun2537 <soeun2537@gmail.com>
2025-04-21 16:57:09 +0900

    refactor: ReservationControllerTest 단위 테스트로 분리

--- src/test/java/roomescape/ReservationControllerTest.java
+++ src/test/java/roomescape/ReservationControllerTest.java
@@ -1,23 +1,51 @@
 package roomescape;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.domain.Reservation;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class ReservationTest {
+public class ReservationControllerTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Test
-    @DisplayName("예약에 대한 생성, 조회, 삭제를 진행한다.")
-    void createAndReadAndDeleteReservation() {
+    @DisplayName("예약을 조회하는 API를 요청한다.")
+    void getReservations() {
+        // given
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)",
+                "브라운", "2023-08-05", "15:40");
+
+        // when
+        List<Reservation> reservations = RestAssured
+                .given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200).extract()
+                .jsonPath().getList(".", Reservation.class);
+
+        // then
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+        assertThat(reservations.size()).isEqualTo(count);
+    }
+
+    @Test
+    @DisplayName("예약을 생성하는 API를 요청한다.")
+    void createReservations() {
         var params = Map.of(
                 "name", "브라운",
                 "date", "2023-08-05",
@@ -28,20 +56,20 @@ public class ReservationTest {
                 .given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
-                .when()
-                .post("/reservations")
+                .when().post("/reservations")
                 .then().log().all()
                 .statusCode(200)
                 .body("id", is(1));
+    }
 
-        RestAssured
-                .given().log().all()
-                .when()
-                .get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
+    @Test
+    @DisplayName("예약을 삭제하는 API를 요청한다.")
+    void createAndReadAndDeleteReservation() {
+        // given
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)",
+                "브라운", "2023-08-05", "15:40");
 
+        // when
         RestAssured
                 .given().log().all()
                 .when()
@@ -49,13 +77,9 @@ public class ReservationTest {
                 .then().log().all()
                 .statusCode(200);
 
-        RestAssured
-                .given().log().all()
-                .when()
-                .get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
+        // then
+        Integer count = jdbcTemplate.queryForObject("SELECT count(*) from reservation", Integer.class);
+        assertThat(count).isEqualTo(0);
     }
 
     @Test

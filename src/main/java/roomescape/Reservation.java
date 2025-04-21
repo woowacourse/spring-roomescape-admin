7e81b4196a0abe23701dcf0ad3f5e35c880198f2
Dompoo <dlckdrms0517@gmail.com>
2025-04-21 12:54:16 +0900

    refactor: 예약 시간에 대하여 외래키 사용

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -1,13 +1,12 @@
 package roomescape;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 
 public record Reservation(
         long id,
         String name,
         LocalDate date,
-        LocalTime time
+        ReservationTime time
 ) {
     public Reservation {
         if (name == null) {

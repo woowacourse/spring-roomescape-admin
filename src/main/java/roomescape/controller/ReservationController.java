90fc8cce996382fb0c901740d297d38535fd524a
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-04-21 17:15:33 +0900

    refactor: RoomescapeController -> ReservationController 네이밍 변경

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -9,34 +9,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import roomescape.dto.ReservationCreateDto;
 import roomescape.dto.ReservationReadDto;
 import roomescape.model.Reservation;
 import roomescape.repository.ReservationRepository;
 
 @Controller
-public class RoomescapeController {
+@RequestMapping("reservations")
+public class ReservationController {
     private final ReservationRepository reservationRepository;
 
-    public RoomescapeController(ReservationRepository reservationRepository) {
+    public ReservationController(ReservationRepository reservationRepository) {
         this.reservationRepository = reservationRepository;
     }
 
-    @GetMapping("/reservations")
+    @GetMapping
     public ResponseEntity<List<ReservationReadDto>> getReservations() {
         return ResponseEntity.ok().body(reservationRepository.findAll());
     }
 
-    @PostMapping("/reservations")
+    @PostMapping
     public ResponseEntity<ReservationReadDto> createReservation(@RequestBody ReservationCreateDto dto) {
         Reservation newReservation = new Reservation(dto.getName(), dto.getDate(), dto.getTime());
         ReservationReadDto readDto = reservationRepository.add(newReservation);
         return ResponseEntity.created(URI.create("reservations/" + readDto.getId())).body(readDto);
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
         reservationRepository.deleteBy(id);
         return ResponseEntity.ok().build();
     }
+
+
 }

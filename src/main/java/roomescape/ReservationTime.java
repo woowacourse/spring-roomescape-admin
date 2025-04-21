5d891923b37e368e8d76ebdcfa1abec6276c0085
Dompoo <dlckdrms0517@gmail.com>
2025-04-21 14:37:24 +0900

    refactor: ReservationTime 유효성 검사 추가

--- src/main/java/roomescape/ReservationTime.java
+++ src/main/java/roomescape/ReservationTime.java
@@ -6,4 +6,9 @@ public record ReservationTime(
         long id,
         LocalTime startTime
 ) {
+    public ReservationTime {
+        if (startTime == null) {
+            throw new IllegalArgumentException("시작 시간은 null이 될 수 없습니다.");
+        }
+    }
 }
--- src/main/java/roomescape/dto/request/ReservationTimeCreateRequest.java
+++ src/main/java/roomescape/dto/request/ReservationTimeCreateRequest.java
@@ -6,6 +6,12 @@ import java.util.Map;
 public record ReservationTimeCreateRequest(
         LocalTime startAt
 ) {
+    public ReservationTimeCreateRequest {
+        if (startAt == null) {
+            throw new IllegalArgumentException("시작 시간은 필수값입니다.");
+        }
+    }
+
     public Map<String, ?> dataMap() {
         return Map.of("start_at", startAt);
     }

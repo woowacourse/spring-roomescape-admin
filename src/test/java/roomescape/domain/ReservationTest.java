dfbf0ddf298845ce64f7e236b1d95768f448b8c8
eoehd1ek <eoehd2ek@gmail.com>
2025-04-22 00:08:28 +0900

    refactor: Fixture 사용하도록 수정

--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -2,10 +2,9 @@ package roomescape.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import roomescape.domain.fixture.ReservationFixture;
 
 class ReservationTest {
 
@@ -14,10 +13,7 @@ class ReservationTest {
     void should_return_true_when_id_is_same() {
         // given
         Long id = 1L;
-        String name = "miku";
-        LocalDate date = LocalDate.of(2025, 4, 21);
-        LocalTime time = LocalTime.of(10, 0);
-        Reservation reservation = new Reservation(id, name, date, time);
+        Reservation reservation = ReservationFixture.RESERVATION_1_KIM_2025_04_21_10_00;
 
         // when
         final boolean result = reservation.isSameId(id);
@@ -32,10 +28,7 @@ class ReservationTest {
         // given
         Long id = 1L;
         Long otherId = 2L;
-        String name = "miku";
-        LocalDate date = LocalDate.of(2025, 4, 21);
-        LocalTime time = LocalTime.of(10, 0);
-        Reservation reservation = new Reservation(id, name, date, time);
+        Reservation reservation = ReservationFixture.RESERVATION_1_KIM_2025_04_21_10_00;
 
         // when
         final boolean result = reservation.isSameId(otherId);

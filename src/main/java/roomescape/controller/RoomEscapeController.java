cdd34d1702fe663e45108b9e6d7b90fd4b7a1bbd
julie <7jujubuilds@gmail.com>
2025-04-22 17:57:01 +0900

    feat: 시간 관리 페이지 구현

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -17,4 +17,9 @@ public class RoomEscapeController {
     public String getReservationPage() {
         return "/admin/reservation-legacy";
     }
+
+    @GetMapping("/time")
+    public String getReservationTimePage() {
+        return "/admin/time.html";
+    }
 }

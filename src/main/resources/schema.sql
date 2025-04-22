09fb2686a631a1f64a882f2578257b61dddf49ea
threepebbles <threepebbles@naver.com>
2025-04-22 19:20:55 +0900

    chore: 예약 시간 테이블이 존재하지 않는 경우만 생성하도록 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS reservations
     PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation_times
+CREATE TABLE IF NOT EXISTS reservation_times
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     start_at VARCHAR(255) NOT NULL,

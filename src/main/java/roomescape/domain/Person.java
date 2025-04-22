cf415e219c00f72eb5d63e6a27a6dc27acfbb41f
yush <yush2202@gmail.com>
2025-04-22 21:20:58 +0900

    feat: Person 예외 추가
    비어있는 이름이라면 예외가 발생함.

--- src/main/java/roomescape/domain/Person.java
+++ src/main/java/roomescape/domain/Person.java
@@ -1,5 +1,13 @@
 package roomescape.domain;
 
 public record Person(String name) {
+    public Person {
+        isInvalidName(name);
+    }
 
+    private void isInvalidName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("비어 있는 이름은 입력 불가합니다.");
+        }
+    }
 }
---
+++ src/test/java/roomescape/domain/PersonTest.java
@@ -0,0 +1,16 @@
+package roomescape.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+
+class PersonTest {
+    @DisplayName("비어있는 이름이라면 예외를 발생시킵니다.")
+    @ParameterizedTest
+    @EmptySource
+    void isInvalidNameTest(String name) {
+        assertThatCode(() -> new Person(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+}

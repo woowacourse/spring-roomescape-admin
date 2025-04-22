bd1888bea50c9a7da15d55e82753ca805d3accfc
Jin409 <sualng123@gmail.com>
2025-04-22 20:02:07 +0900

    refactor(RoomescapeController): 공통 URI /reservations를 @RequestMapping으로 추출

--- src/main/java/roomescape/controller/RoomescapeController.java
+++ src/main/java/roomescape/controller/RoomescapeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.ReservationService;
@@ -17,6 +18,7 @@ import roomescape.controller.dto.ReservationRegisterDto;
 import roomescape.repository.ReservationRepository;
 
 @RestController
+@RequestMapping("/reservations")
 public class RoomescapeController {
 
     private final ReservationRepository reservationRepository;
@@ -27,14 +29,14 @@ public class RoomescapeController {
         this.reservationService = reservationService;
     }
 
-    @GetMapping("/reservations")
+    @GetMapping
     public List<ReservationDto> getReservations() {
         return reservationRepository.findAll().stream()
                 .map(ReservationDto::new)
                 .toList();
     }
 
-    @PostMapping("/reservations")
+    @PostMapping
     @ResponseBody
     public ReservationDto registerReservation(
             @RequestBody @Valid final ReservationRegisterDto reservationRegisterDto) {
@@ -42,7 +44,7 @@ public class RoomescapeController {
         return reservationService.findReservationById(savedId);
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteReservation(@PathVariable(name = "id") final long id) {
         try {
             reservationRepository.deleteById(id);

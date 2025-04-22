794e55558aa6802660502ac563ce990cec5fc194
KimDaehyeon <daehyeon3351@gmail.com>
2025-04-22 14:26:52 +0900

    refactor: 컨트롤러 패키지 구조 변경

--- src/main/java/roomescape/controller/reservation/ReservationController.java
+++ src/main/java/roomescape/controller/reservation/ReservationController.java
@@ -1,4 +1,4 @@
-package roomescape.controller;
+package roomescape.controller.reservation;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/roomescape/controller/roomescape/RoomescapeController.java
+++ src/main/java/roomescape/controller/roomescape/RoomescapeController.java
@@ -1,4 +1,4 @@
-package roomescape.controller;
+package roomescape.controller.roomescape;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

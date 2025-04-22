5b093e2ba6ea465cda6cb09bab3c747b40c3f39c
DEV_WOONG <ywcsuwon@gmail.com>
2025-04-22 15:37:51 +0900

    test(red): 모든 예약을 가져온다.

--- src/test/java/roomescape/repository/ReservationRepostoryTest.java
+++ src/test/java/roomescape/repository/ReservationRepostoryTest.java
@@ -2,7 +2,9 @@ package roomescape.repository;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,13 +18,14 @@ public class ReservationRepostoryTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    @Autowired
-    private ReservationRepository reservationRepository;
+    @Autowired private ReservationRepository reservationRepository;
 
     @BeforeEach
     void setUp() {
         jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "Lemon", "2025-04-22",
                 "16:22");
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "DDinghwa", "2025-06-03",
+                "13:00");
     }
 
     @Test
@@ -43,5 +46,22 @@ public class ReservationRepostoryTest {
         });
     }
 
+    @Test
+    @DisplayName("모든 에약을 가져온다.")
+    void findALLReservations() {
+        //given
+        //when
+        List<Reservation> reservations = reservationRepository.findAll();
 
+        //then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(reservations).hasSize(2);
+            softAssertions.assertThat(reservations.getFirst().getName()).isEqualTo("Lemon");
+            softAssertions.assertThat(reservations.getFirst().getDate()).isEqualTo(LocalDate.of(2025, 4, 22));
+            softAssertions.assertThat(reservations.getFirst().getTime()).isEqualTo(LocalTime.of(16, 22));
+            softAssertions.assertThat(reservations.getLast().getName()).isEqualTo("DDinghwa");
+            softAssertions.assertThat(reservations.getFirst().getDate()).isEqualTo(LocalDate.of(2025, 6, 3));
+            softAssertions.assertThat(reservations.getFirst().getTime()).isEqualTo(LocalTime.of(13, 0));
+        });
+    }
 }

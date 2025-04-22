028fec5fa0678cf72ed88e897029dc7739a28f1c
DongchannN <ldc991104@gmail.com>
2025-04-22 09:07:33 +0900

    test: DB 연결 테스트 케이스 작성

---
+++ src/test/java/roomescape/DatabaseTest.java
@@ -0,0 +1,30 @@
+package roomescape;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootTest
+public class DatabaseTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @DisplayName("데이터 베이스 커넥션 및 디비, 테이블 이름 확인")
+    @Test
+    void connectionTest() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

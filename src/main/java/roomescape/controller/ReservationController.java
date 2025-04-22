c369ea0c82078da60fe930d66c27cf15a1a2cf76
supernovaMK <minkikim2002@gmail.com>
2025-04-22 13:24:52 +0900

    feat: DB를 사용하는 api 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -9,36 +9,53 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.dao.ReservationDao;
 import roomescape.dto.ReservationRequest;
 import roomescape.dto.ReservationResponse;
 import roomescape.model.Reservation;
-import roomescape.model.Reservations;
 
 @RequestMapping("/reservations")
 @RestController
 public class ReservationController {
 
-    private final Reservations reservations = new Reservations();
+    private final ReservationDao reservationDao;
+
+    public ReservationController(ReservationDao reservationDao) {
+        this.reservationDao = reservationDao;
+    }//생성자 책임을 분리한다. 싱글톤 객체 주입을 통해서.
 
     @GetMapping
-    public List<ReservationResponse> reservations() {
-        return reservations.getReservations();
+    public ResponseEntity<List<ReservationResponse>> reservations() {
+
+        List<Reservation> reservations = reservationDao.selectAllReservation();
+
+        List<ReservationResponse> reservationResponses = reservations.stream()
+                .map(ReservationResponse::toDto).toList();
+
+        return ResponseEntity.ok().body(reservationResponses);
     }
 
     @PostMapping
-    public ReservationResponse addReservation(@RequestBody ReservationRequest request) {
+    public ResponseEntity<ReservationResponse> addReservation(@RequestBody ReservationRequest request) {
+
         Reservation reservation = request.toEntity();
 
-        return reservations.createReservation(reservation);
+        Reservation addedReservation = reservationDao.addReservation(reservation);
+
+        ReservationResponse reservationResponse = ReservationResponse.toDto(addedReservation);
+
+        return ResponseEntity.ok().body(reservationResponse);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
-        try {
-            reservations.deleteReservation(id);
+
+        int effectedRow = reservationDao.deleteReservationById(id);
+
+        if (effectedRow == 1) {
             return ResponseEntity.ok().build();
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.notFound().build();
         }
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,16 +1,23 @@
 package roomescape;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.model.Reservation;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -89,6 +96,38 @@ class MissionStepTest {
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
-                .statusCode(404);
+                .statusCode(204);
+    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @DisplayName("데이터베이스 생성 테스트")
+    @Test
+    void dataBase_created_test() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @DisplayName("데이터베이스 데이터 조회 테스트")
+    @Test
+    void dataBase_select_test() {
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05",
+                "15:40");
+
+        List<Reservation> reservations = RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200).extract()
+                .jsonPath().getList(".", Reservation.class);
+
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+
+        assertThat(reservations.size()).isEqualTo(count);
     }
 }

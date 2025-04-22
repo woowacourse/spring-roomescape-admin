795896dee8350f56c44c46593fcf1c74d4875019
threepebbles <threepebbles@naver.com>
2025-04-22 20:29:45 +0900

    chore: reservation 테이블의 time 컬럼과 reservation_times 테이블의 start_at 컬럼 외래 키 관계 설정
    - 예약 시간 중복 방지 목적

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -3,13 +3,15 @@ CREATE TABLE IF NOT EXISTS reservations
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
     date    VARCHAR(255) NOT NULL,
-    time    VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    time_id BIGINT       NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_times (id)
     );
 
 CREATE TABLE IF NOT EXISTS reservation_times
 (
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
     start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
-    );
+);

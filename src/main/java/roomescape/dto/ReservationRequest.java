04e90dbf0523aa9d5e4fbd2eb2ea36169a4c247e
yush <yush2202@gmail.com>
2025-04-22 16:53:53 +0900

    refactor(reservation을 dto 내부에서 객체로 생성하도록 변경):

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -4,15 +4,21 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import roomescape.domain.Person;
+import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 
 public record ReservationRequest(String name, LocalDate date, LocalTime time) {
 
-    public ReservationTime toReservationTime() {
+
+    public Reservation toReservation() {
+        return new Reservation(toPerson(), toReservationTime());
+    }
+
+    private ReservationTime toReservationTime() {
         return new ReservationTime(LocalDateTime.of(date, time));
     }
 
-    public Person toPerson() {
+    private Person toPerson() {
         return new Person(name);
     }
 }

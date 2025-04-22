67756f227c1f2eb0957a9ee9e4532cbc70492955
CheChe903 <cheche903@naver.com>
2025-04-22 21:27:39 +0900

    test(ReservationTimeTest): 예약 시간 단위 테스트

---
+++ src/test/java/roomescape/domain/ReservationTime/ReservationTimeTest.java
@@ -0,0 +1,25 @@
+package roomescape.domain.ReservationTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class ReservationTimeTest {
+
+    @DisplayName("아이디가 같으면 true를, 다르면 false를 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = "1, 1, true, 1, 2, false")
+    void equalIdTest(final long firstId, final long secondId, boolean result) {
+
+        // given
+        ReservationTime reservationTime = new ReservationTime(1L, LocalTime.of(10, 0));
+
+        // when
+
+        // then
+        assertThat(reservationTime.isEqualId(secondId)).isEqualTo(result);
+    }
+}

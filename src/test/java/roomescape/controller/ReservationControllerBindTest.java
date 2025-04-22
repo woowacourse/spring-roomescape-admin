bdc6871c87c0fc8b70c6c2f7fa575f73858cd12a
Minuring <mw9505@gmail.com>
2025-04-22 15:11:16 +0900

    test: 불필요한 SpringBoot 테스트 경량화

--- src/test/java/roomescape/controller/ReservationControllerBindTest.java
+++ src/test/java/roomescape/controller/ReservationControllerBindTest.java
@@ -1,34 +1,39 @@
 package roomescape.controller;
 
-import io.restassured.RestAssured;
-import java.util.Map;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.annotation.DirtiesContext;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import roomescape.repository.ReservationFakeRepository;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@WebMvcTest(ReservationController.class)
+@Import(ReservationFakeRepository.class)
 public class ReservationControllerBindTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @DisplayName("잘못된 형식의 요청으로 파라미터 바인딩 실패 시 400 Bad Request")
     @ParameterizedTest
-    @MethodSource("invalidRequestParameters")
-    void badRequestWhenRequestInvalid(Map<String, String> request) {
-        RestAssured
-            .given().contentType("application/json")
-            .when().body(request).post("/reservations")
-            .then().statusCode(HttpStatus.BAD_REQUEST.value());
-    }
-
-    public static Stream<Arguments> invalidRequestParameters() {
-        return Stream.of(
-            Arguments.of(Map.of("name", "brown", "date", "2-0-2-3", "timeId", "1")),
-            Arguments.of(Map.of("name", "brown", "date", "2023-01-01", "timeId", "abc"))
-        );
+    @ValueSource(
+        strings = {
+            """
+                {"name" : "brown", "date" : "2-0-2-3", "timeId" : "1"}
+            """,
+            """
+                {"name" : "brown", "date" : "2023-01-01", "timeId" : "abc"}
+            """
+        }
+    )
+    void badRequestWhenRequestInvalid(String jsonBody) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/reservations")
+                .contentType("application/json")
+                .content(jsonBody))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 }

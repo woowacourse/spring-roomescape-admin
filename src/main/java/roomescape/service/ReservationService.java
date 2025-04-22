116df16be1c23906caeb14875549dc38e6f994a1
YuyoungRhee <bbclara@naver.com>
2025-04-22 14:08:29 +0900

    feat(service): 날짜 형식에 대한 유효성 검증 추가

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,5 +1,7 @@
 package roomescape.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import roomescape.dto.request.ReservationRequest;
@@ -22,11 +24,21 @@ public class ReservationService {
     }
 
     public ReservationResponse registerReservation(ReservationRequest request) {
+        validateDateFormat(request.date());
+
         ReservationTime reservationTime = timeRepository.findById(request.timeId());
         Reservation reservation = ReservationMapper.toDomain(request, reservationTime);
         Long id = reservationRepository.save(reservation);
 
         return ReservationMapper.toDto(Reservation.withId(id, reservation));
+    }
+
+    private void validateDateFormat(String date) {
+        try {
+            LocalDate.parse(date);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("유효하지 않은 날짜입니다: " + date);
+        }
     }
 
     public List<ReservationResponse> getAllReservations() {

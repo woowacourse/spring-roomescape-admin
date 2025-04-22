c1197e618f2f87bd0c11a383c9983b540df3d162
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-04-22 19:41:47 +0900

    feat(controller) : TimeController 추가

--- src/main/java/roomescape/controller/TimeController.java
+++ src/main/java/roomescape/controller/TimeController.java
@@ -1,8 +1,16 @@
 package roomescape.controller;
 
+import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import roomescape.TimeRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import roomescape.dto.AddTimeDto;
+import roomescape.model.Time;
+import roomescape.repository.TimeRepository;
 
 @Controller
 public class TimeController {
@@ -15,6 +23,21 @@ public class TimeController {
     @GetMapping("admin/time")
     public String adminTime() {
         return "admin/time";
+    }
+
+    @PostMapping("/times")
+    public ResponseEntity<Time> addTime(@RequestBody AddTimeDto addTimeDto) {
+        return ResponseEntity.ok(timeRepository.addTime(addTimeDto));
+    }
+
+    @GetMapping("/times")
+    public ResponseEntity<List<Time>> addTime() {
+        return ResponseEntity.ok(timeRepository.getAllTime());
+    }
+
+    @DeleteMapping("/times/{id}")
+    public ResponseEntity<Integer> deleteTime(@PathVariable Long id) {
+        return ResponseEntity.ok(timeRepository.deleteTime(id));
     }
 
 }

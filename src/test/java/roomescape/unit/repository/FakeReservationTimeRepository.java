3f31fea4979e4408881a0940eec94b2d2479c694
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-04-22 14:34:32 +0900

    test: ReservationTimeService 테스트 추가

--- src/test/java/roomescape/unit/repository/FakeReservationTimeRepository.java
+++ src/test/java/roomescape/unit/repository/FakeReservationTimeRepository.java
@@ -1,23 +1,36 @@
 package roomescape.unit.repository;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 import roomescape.domain.ReservationTime;
 import roomescape.repository.ReservationTimeRepository;
 
 public class FakeReservationTimeRepository implements ReservationTimeRepository {
-    
+
+    AtomicLong index = new AtomicLong(1L);
+    List<ReservationTime> reservationTimes = new ArrayList<>();
+
     @Override
     public long add(ReservationTime reservationTime) {
-        return 0;
+        long id = index.getAndIncrement();
+        reservationTimes.add(new ReservationTime(id, reservationTime.getTime()));
+        return id;
     }
 
     @Override
     public List<ReservationTime> findAll() {
-        return List.of();
+        return Collections.unmodifiableList(reservationTimes);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        Optional<ReservationTime> findReservationTimes = reservationTimes.stream()
+                .filter(reservation -> reservation.getId().equals(id))
+                .findAny();
+        findReservationTimes.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 id입니다." + id));
+        reservationTimes.remove(findReservationTimes.get());
     }
 }
--- src/test/java/roomescape/unit/service/ReservationTimeServiceTest.java
+++ src/test/java/roomescape/unit/service/ReservationTimeServiceTest.java
@@ -2,13 +2,11 @@ package roomescape.unit.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 import roomescape.service.ReservationTimeService;
 import roomescape.unit.repository.FakeReservationTimeRepository;
@@ -25,19 +23,17 @@ class ReservationTimeServiceTest {
     }
 
     @Test
-    void 예약을_추가하고_조회할_수_있다() {
+    void 예약시간을_추가하고_조회할_수_있다() {
         assertThat(reservationTimeService.allReservationTimes().size()).isEqualTo(0);
-        reservationTimeService.addReservationTime(new ReservationTime(1, LocalTime.now()));
-        reservationService.addReservation(new Reservation(null, "praisebak", LocalDate.now(), LocalTime.now()));
-        assertThat(reservationService.allReservations().size()).isEqualTo(1);
+        reservationTimeService.addReservationTime(new ReservationTime(1L, LocalTime.now()));
+        assertThat(reservationTimeService.allReservationTimes().size()).isEqualTo(1);
     }
 
     @Test
-    void 예약을_삭제하고_조회할_수_있다() {
-        long id = reservationService.addReservation(
-                new Reservation(null, "praisebak", LocalDate.now(), LocalTime.now()));
-        assertThat(reservationService.allReservations().size()).isEqualTo(1);
-        reservationService.deleteReservation(id);
-        assertThat(reservationService.allReservations().size()).isEqualTo(0);
+    void 예약시간을_삭제하고_조회할_수_있다() {
+        long id = reservationTimeService.addReservationTime(new ReservationTime(null, LocalTime.now().plusHours(1L)));
+        assertThat(reservationTimeService.allReservationTimes().size()).isEqualTo(1);
+        reservationTimeService.deleteReservationTime(id);
+        assertThat(reservationTimeService.allReservationTimes().size()).isEqualTo(0);
     }
 }

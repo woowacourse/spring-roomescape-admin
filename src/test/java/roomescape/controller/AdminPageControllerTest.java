84c47e458e950b3c8e40cd015f70a7138fbfd621
saera <yook.saera@gmail.com>
2025-04-22 20:02:32 +0900

    refactor: 불필요한 애노테이션 삭제

--- src/test/java/roomescape/controller/AdminPageControllerTest.java
+++ src/test/java/roomescape/controller/AdminPageControllerTest.java
@@ -6,10 +6,8 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class AdminPageControllerTest {
 
     @DisplayName("관리자 홈 화면 핵심 태그 존재 여부를 확인한다.")

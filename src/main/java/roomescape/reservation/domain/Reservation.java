6dcc013b566b39872b7747aa0d85159fa8798afd
bowook <mbosx316@naver.com>
2025-04-22 16:36:09 +0900

    refactor(Reservation) : Reservation 수정

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -1,15 +1,12 @@
 package roomescape.reservation.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Reservation {
     private final Long id;
     private final String name;
-    @JsonFormat(pattern = "yyyy-MM-dd")
     private final LocalDate date;
-    @JsonFormat(pattern = "HH:mm")
     private final LocalTime time;
 
     public Reservation(final long id, final String name, final LocalDate date, final LocalTime time) {
@@ -19,19 +16,19 @@ public class Reservation {
         this.time = time;
     }
 
-    public static Reservation toEntity(Reservation reservation, Long id) {
-        return new Reservation(id, reservation.name, reservation.date, reservation.time);
-    }
-
-    public boolean isSameId(final long id) {
-        return this.id == id;
-    }
-
     public Long getId() {
         return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
     }
 }

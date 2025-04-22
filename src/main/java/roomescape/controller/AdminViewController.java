13720e9bc04e46e67dead9c72b6517eb7f201570
ljhee92 <weilainib7@gmail.com>
2025-04-22 22:36:10 +0900

    feat: 예약 페이지 매핑 수정

--- src/main/java/roomescape/controller/AdminViewController.java
+++ src/main/java/roomescape/controller/AdminViewController.java
@@ -15,7 +15,7 @@ public class AdminViewController {
 
     @GetMapping("/reservation")
     public String adminReservation(){
-        return "admin/reservation-legacy";
+        return "admin/reservation";
     }
 
     @GetMapping("/time")

ef07f9be6792530bfca293fd73a24690296047c3
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-04-22 20:01:24 +0900

    chore: 스키마 변경, reservation_time 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,15 @@
 CREATE TABLE IF NOT EXISTS reservation
 (
-    id        BIGINT       NOT NULL AUTO_INCREMENT,
-    name      VARCHAR(255) NOT NULL,
-    date_time TIMESTAMP    NOT NULL,
+    id      BIGINT       NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(255) NOT NULL,
+    date    DATE         NOT NULL,
+    time_id BIGINT       NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS reservation_time
+(
+    id       BIGINT NOT NULL AUTO_INCREMENT,
+    start_at TIME   NOT NULL,
     PRIMARY KEY (id)
 );

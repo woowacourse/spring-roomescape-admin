48c6d5170c77b3285167c9758a046191f1cc4b0f
(minjae8563) <(minjae8563@naver.com)>
2025-04-22 13:07:16 +0900

    refactor: ReservationService 를 참조하도록 변경, AtomicLong 필드에서 제거

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,8 +2,8 @@ package roomescape.controller;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,40 +12,38 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import roomescape.reservation.Reservation;
-import roomescape.reservation.ReservationRepository;
+import roomescape.reservation.ReservationService;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
 
-    private final ReservationRepository reservationRepository;
-    private AtomicLong index = new AtomicLong(1);
+    private final ReservationService reservationService;
 
     @Autowired
-    public ReservationController(ReservationRepository reservationRepository) {
-        this.reservationRepository = reservationRepository;
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
     }
 
     @GetMapping()
     public List<Reservation> getReservations(
     ) {
-        return reservationRepository.findAllReservations();
+        return reservationService.findAllReservations();
     }
 
     @PostMapping()
     public ResponseEntity<Reservation> createReservation(
             @RequestBody Reservation reservation
     ) {
-        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
-
-        /*try{
-            reservationRepository.validateReservationTimeAvailability(newReservation);
+        try{
+            reservationService.validateReservationTimeAvailability(reservation);
         } catch (IllegalArgumentException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }*/
+        }
 
-        reservationRepository.saveReservation(newReservation);
+        Reservation newReservation = reservationService.saveReservation(reservation);
         return ResponseEntity.ok().body(newReservation);
     }
 
@@ -53,14 +51,14 @@ public class ReservationController {
     public ResponseEntity<List<Reservation>> deleteReservation(
             @PathVariable Long id
     ) {
-        Reservation reservation = reservationRepository.findAllReservations().stream()
+        Reservation reservation = reservationService.findAllReservations().stream()
                 .filter(it -> Objects.equals(it.getId(), id))
                 .findFirst()
                 .orElseThrow(RuntimeException::new);
 
-        reservationRepository.deleteReservation(reservation.getId());
+        reservationService.deleteReservation(reservation.getId());
 
-        return ResponseEntity.ok().body(reservationRepository.findAllReservations());
+        return ResponseEntity.ok().body(reservationService.findAllReservations());
     }
 
 }

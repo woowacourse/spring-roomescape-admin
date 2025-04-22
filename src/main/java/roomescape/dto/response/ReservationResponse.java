d97995e3cbead9197dac5eb949c7580ef3ecf2e7
seaniiio <cnc4750@naver.com>
2025-04-22 16:13:17 +0900

    feat: 시간 직렬화할 때 HH:mm 포맷팅

--- src/main/java/roomescape/dto/response/ReservationResponse.java
+++ src/main/java/roomescape/dto/response/ReservationResponse.java
@@ -1,5 +1,6 @@
 package roomescape.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import roomescape.domain.Reservation;
 
 import java.time.LocalDate;
@@ -7,7 +8,12 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
-public record ReservationResponse(Long id, String name, LocalDate date, LocalTime time) {
+public record ReservationResponse(
+        Long id,
+        String name,
+        LocalDate date,
+        @JsonFormat(pattern = "HH:mm")
+        LocalTime time) {
 
     public static List<ReservationResponse> from(final List<Reservation> reservations) {
         return reservations.stream()

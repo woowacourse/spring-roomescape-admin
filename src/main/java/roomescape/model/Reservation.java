3564ab85008a17ecd196127b790365f672930c36
Han <jumdo12@gmail.com>
2025-04-22 15:15:24 +0900

    feat(Reservation): id 없이 생성 할 수 있도록 정적 팩토리 메서드 추가

--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -6,21 +6,34 @@ import java.time.LocalTime;
 public record Reservation(Long id, String name, LocalDate date, LocalTime time) {
 
     public Reservation {
-        validateReservation(id, name, date, time);
+        validateId(id);
         validateName(name);
+        validateDateAndTime(date, time);
+    }
+
+    public static Reservation withoutId(String name, LocalDate date, LocalTime time) {
+        validateName(name);
+        validateDateAndTime(date, time);
+        return new Reservation(null, name, date, time);
     }
 
     public boolean isSameId(Long id) {
         return this.id == id;
     }
 
-    private void validateReservation(Long id, String name, LocalDate date, LocalTime time) {
-        if (id == null || name == null || date == null || time == null) {
+    private void validateId(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Id cannot be null");
+        }
+    }
+
+    private static void validateDateAndTime(LocalDate date, LocalTime time) {
+        if (date == null || time == null) {
             throw new IllegalArgumentException("Invalid reservation");
         }
     }
 
-    private void validateName(String name) {
+    private static void validateName(String name) {
         if (name.isBlank() || name.isEmpty()) {
             throw new IllegalArgumentException("Invalid name");
         }

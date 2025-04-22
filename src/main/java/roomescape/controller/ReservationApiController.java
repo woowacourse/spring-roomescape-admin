d6c736f66718bea0fc226bc0adec4f6e1c9cc4c9
Han <jumdo12@gmail.com>
2025-04-22 15:55:10 +0900

    feat(Controller): save 및 delete 추가

--- src/main/java/roomescape/controller/ReservationApiController.java
+++ src/main/java/roomescape/controller/ReservationApiController.java
@@ -1,51 +1,42 @@
 package roomescape.controller;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+import roomescape.dao.ReservationDao;
 import roomescape.dto.CreateReservationRequestDto;
 import roomescape.dto.ReservationResponseDto;
 import roomescape.model.Reservation;
-import roomescape.model.ReservationGroup;
 
 import java.util.List;
 
 @RestController
 public class ReservationApiController {
 
-    ReservationGroup reservations = new ReservationGroup();
+    private final ReservationDao reservationDao;
+
+    @Autowired
+    public ReservationApiController(ReservationDao reservationDao) {
+        this.reservationDao = reservationDao;
+    }
 
     @GetMapping("/reservations")
     public List<ReservationResponseDto> getAllReservations() {
-        return reservations.getReservations().stream()
-                .map(reservation -> new ReservationResponseDto(
-                        reservation.id(),
-                        reservation.name(),
-                        reservation.date(),
-                        reservation.time()
-                ))
-                .toList();
+        return reservationDao.findAllReservations();
     }
 
     @PostMapping("/reservations")
-    public ReservationResponseDto addReservation(@RequestBody CreateReservationRequestDto reservationDto) {
-        try {
-            Reservation newReservation = reservationDto.toEntity(reservations.getIndexAndIncrement());
-            reservations.addReservation(newReservation);
-
-            ReservationResponseDto responseDto = new ReservationResponseDto(
-                    newReservation.id(),
-                    newReservation.name(),
-                    newReservation.date(),
-                    newReservation.time());
-            return responseDto;
-        } catch (IllegalArgumentException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
+    public ReservationResponseDto addReservation(@RequestBody final CreateReservationRequestDto reservationDto) {
+        Reservation noIdReservation = reservationDto.toEntity();
+        Long id = reservationDao.saveAndReturnId(noIdReservation);
+        return new ReservationResponseDto(
+                id,
+                noIdReservation.name(),
+                noIdReservation.date(),
+                noIdReservation.time());
     }
 
     @DeleteMapping("/reservations/{id}")
-    public void deleteReservation(@PathVariable("id") Long id) {
-        reservations.deleteReservationById(id);
+    public void deleteReservation(@PathVariable("id") final Long id) {
+        reservationDao.delete(id);
     }
 }

e9d6777fb1a07b9012ee19d9217d941fed03d28b
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-04-22 20:06:28 +0900

    refactor: 롬복 적용

--- src/main/java/roomescape/reservation/domain/ReserverName.java
+++ src/main/java/roomescape/reservation/domain/ReserverName.java
@@ -1,29 +1,27 @@
 package roomescape.reservation.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReserverName {
 
     private final String value;
 
-    private ReserverName(final String value) {
-        validate(value);
-        this.value = value;
-    }
-
     public static ReserverName from(final String name) {
+        validate(name);
         return new ReserverName(name);
     }
 
-    private void validate(final String value) {
+    private static void validate(final String value) {
         validateBlank(value);
     }
 
-    private void validateBlank(final String value) {
+    private static void validateBlank(final String value) {
         if (value == null || value.isBlank()) {
             throw new IllegalArgumentException("예약자 이름은 빈 칸일 수 없습니다.");
         }
-    }
-
-    public String getValue() {
-        return value;
     }
 }

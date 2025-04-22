a57132904b21596fa4df43eed889e3888e8490da
KimDaehyeon <daehyeon3351@gmail.com>
2025-04-22 13:42:35 +0900

    feat: 예약 id 추가

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -2,22 +2,41 @@ package roomescape.domain;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Objects;
 
 public class Reservation {
-    private long id;
-    private String name;
-    private LocalDate date;
-    private LocalTime time;
 
-    public Reservation(long id, String name, LocalDate date, LocalTime time) {
+    private final String name;
+    private final LocalDate date;
+    private final LocalTime time;
+    private Long id;
+
+    public Reservation(
+        final Long id,
+        final String name,
+        final LocalDate date,
+        final LocalTime time
+    ) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
     }
 
+    public Reservation(
+        final String name,
+        final LocalDate date,
+        final LocalTime time
+    ) {
+        this(null, name, date, time);
+    }
+
     public long getId() {
         return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -30,5 +49,29 @@ public class Reservation {
 
     public LocalTime getTime() {
         return time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Reservation that)) {
+            return false;
+        }
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name)
+            && Objects.equals(date, that.date) && Objects.equals(time, that.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, date, time);
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", date=" + date +
+            ", time=" + time +
+            '}';
     }
 }

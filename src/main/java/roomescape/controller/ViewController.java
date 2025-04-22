3f43fc755b32b14d7f20d028531b289ed88a333d
CheChe903 <cheche903@naver.com>
2025-04-22 13:22:33 +0900

    feat(ViewController): time 뷰 추가 및 미션 테스트 추가

--- src/main/java/roomescape/controller/ViewController.java
+++ src/main/java/roomescape/controller/ViewController.java
@@ -2,17 +2,24 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin/")
 public class ViewController {
 
-    @GetMapping("admin")
+    @GetMapping()
     public String getHomePage() {
         return "admin/index";
     }
 
-    @GetMapping("/admin/reservation")
+    @GetMapping("reservation")
     public String getReservationPage() {
         return "admin/reservation-legacy";
+    }
+
+    @GetMapping("time")
+    public String getTimePage() {
+        return "admin/time";
     }
 }

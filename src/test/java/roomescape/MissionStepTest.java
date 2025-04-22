1231419c3cb6e027763986b08b5c5780a2211d1b
kjungw <sagakkb@gmail.com>
2025-04-22 12:17:12 +0900

    test: 테스트 패키지 정리

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,4 +1,4 @@
-package roomescape.interation;
+package roomescape;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
--- src/test/java/roomescape/domain/ReservationTest.java
+++
@@ -1,5 +0,0 @@
-package roomescape.domain;
-
-class ReservationTest {
-
-}

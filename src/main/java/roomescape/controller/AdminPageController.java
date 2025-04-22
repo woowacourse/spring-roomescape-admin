92fd8c9d23f6f4a5349c6e9b9b8521708beb96b5
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-04-22 17:45:33 +0900

    refactor(controller) : html 페이지 반환 controller 분리, RequestMapping

---
+++ src/main/java/roomescape/controller/AdminPageController.java
@@ -0,0 +1,17 @@
+package roomescape.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminPageController {
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin/index";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String adminReservation() {
+        return "admin/reservation-legacy";
+    }
+}
--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,45 +1,38 @@
 package roomescape.controller;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.ReservationRepository;
 import roomescape.dto.AddReservationDto;
 import roomescape.model.Reservation;
 
-@Controller
-public class RoomescapeController {
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+    private final ReservationRepository reservationRepository;
 
-    @Autowired
-    ReservationRepository reservationRepository;
-
-    @GetMapping("/admin")
-    public String admin() {
-        return "admin/index";
+    public ReservationController(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
     }
 
-    @GetMapping("/admin/reservation")
-    public String adminReservation() {
-        return "admin/reservation-legacy";
-    }
-
-    @GetMapping("/reservations")
+    @GetMapping()
     public ResponseEntity<List<Reservation>> reservations() {
         return ResponseEntity.ok(reservationRepository.getAllReservations());
     }
 
-    @PostMapping("/reservations")
+    @PostMapping()
     public ResponseEntity<Reservation> addReservation(@RequestBody AddReservationDto addReservationDto) {
         return ResponseEntity.ok(reservationRepository.addReservation(addReservationDto));
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Integer> deleteReservation(@PathVariable long id) {
         return ResponseEntity.ok().body(reservationRepository.deleteReservation(id));
     }

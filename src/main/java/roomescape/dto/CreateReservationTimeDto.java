3c10299a535be2212794b834de3bc0b417e557ab
jhpark1227 <junhyeok0246@naver.com>
2025-04-22 23:55:10 +0900

    feat: DTO 유효성 검사 추가

--- src/main/java/roomescape/dto/CreateReservationTimeDto.java
+++ src/main/java/roomescape/dto/CreateReservationTimeDto.java
@@ -1,9 +1,10 @@
 package roomescape.dto;
 
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 import roomescape.domain.ReservationTime;
 
-public record CreateReservationTimeDto(LocalTime startAt) {
+public record CreateReservationTimeDto(@NotNull LocalTime startAt) {
 
     public ReservationTime toReservationTime() {
         return new ReservationTime(null, startAt);

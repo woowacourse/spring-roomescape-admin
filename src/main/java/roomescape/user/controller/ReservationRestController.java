ef2c1818a5fba3a5e737569a15de7390ea0a4fe3
threepebbles <threepebbles@naver.com>
2025-04-22 16:31:41 +0900

    refactor: RequestMapping 사용해서 중복 코드 제거

--- src/main/java/roomescape/user/controller/ReservationRestController.java
+++ src/main/java/roomescape/user/controller/ReservationRestController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.exception.DataNotFoundException;
 import roomescape.user.domain.Reservation;
 import roomescape.user.repository.ReservationRepository;
 
 @RestController
+@RequestMapping("/reservations")
 public class ReservationRestController {
 
     private final ReservationRepository reservationRepository;
@@ -34,21 +36,21 @@ public class ReservationRestController {
         return ResponseEntity.notFound().build();
     }
 
-    @PostMapping("/reservations")
+    @PostMapping
     public ResponseEntity<Reservation> persistReservation(@RequestBody final Reservation reservation) {
         final Long id = reservationRepository.save(reservation);
         final Reservation found = reservationRepository.getOneById(id);
         return ResponseEntity.ok(found);
     }
 
-    @GetMapping("/reservations")
+    @GetMapping
     public ResponseEntity<List<Reservation>> retrieveReservations() {
         final List<Reservation> reservations = reservationRepository.findAll();
 
         return ResponseEntity.ok(reservations);
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable final Long id) {
         final Reservation found = reservationRepository.getOneById(id);
 

2b90c34a4daa9b8952f4239ced1e61f949046539
Yehyeok Bang <qkddpgur318@gmail.com>
2025-04-22 19:46:27 +0900

    fix: 이동 버튼의 잘못된 경로 수정 및 테스트 추가

--- src/test/java/roomescape/controller/AdminPageControllerTest.java
+++ src/test/java/roomescape/controller/AdminPageControllerTest.java
@@ -6,10 +6,8 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class AdminPageControllerTest {
 
     @DisplayName("관리자 홈 화면 핵심 태그 존재 여부를 확인한다.")
@@ -21,6 +19,30 @@ class AdminPageControllerTest {
                 .statusCode(200)
                 .body(containsString("<title>방탈출 어드민</title>"))
                 .body(containsString("<a class=\"nav-link\" href=\"/admin/reservation\">Reservation</a>"))
-                .body(containsString("<a class=\"nav-link\" href=\"/time\">Time</a>"));
+                .body(containsString("<a class=\"nav-link\" href=\"/admin/time\">Time</a>"));
+    }
+
+    @DisplayName("예약 관리 화면 핵심 태그 존재 여부를 확인한다.")
+    @Test
+    void containsCoreTagsInReservationTest() {
+        RestAssured.given().log().all()
+                .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200)
+                .body(containsString("<h2 class=\"content-container-title\">방탈출 예약 페이지</h2>"))
+                .body(containsString("<a class=\"nav-link\" href=\"/admin/reservation\">Reservation</a>"))
+                .body(containsString("<a class=\"nav-link\" href=\"/admin/time\">Time</a>"));
+    }
+
+    @DisplayName("예약 시간 관리 화면 핵심 태그 존재 여부를 확인한다.")
+    @Test
+    void containsCoreTagsInTimeTest() {
+        RestAssured.given().log().all()
+                .when().get("/admin/time")
+                .then().log().all()
+                .statusCode(200)
+                .body(containsString("<h2 class=\"content-container-title\">시간 관리 페이지</h2>"))
+                .body(containsString("<a class=\"nav-link\" href=\"/admin/reservation\">Reservation</a>"))
+                .body(containsString("<a class=\"nav-link\" href=\"/admin/time\">Time</a>"));
     }
 }

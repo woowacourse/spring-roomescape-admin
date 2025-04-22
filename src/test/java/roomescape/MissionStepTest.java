77bf9fe630dc7d21bd6430660d7e42ae47f07795
DEV_WOONG <ywcsuwon@gmail.com>
2025-04-22 11:40:11 +0900

    test: MissionStepTest 5단계 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -90,4 +90,21 @@ public class MissionStepTest {
             throw new RuntimeException(e);
         }
     }
+
+
+    @Test
+    void 오단계() {
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
+
+        List<Reservation> reservations = RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200).extract()
+                .jsonPath().getList(".", Reservation.class);
+
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+
+        assertThat(reservations.size()).isEqualTo(count);
+    }
+
 }

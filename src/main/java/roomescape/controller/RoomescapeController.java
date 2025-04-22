1722b603a1d23e60fbba1aae902f91fe4995a5ab
ryan-dia <cjfdnjs158@naver.com>
2025-04-22 14:22:51 +0900

    refactor: requestMapping 적용

--- src/main/java/roomescape/controller/RoomescapeController.java
+++ src/main/java/roomescape/controller/RoomescapeController.java
@@ -10,30 +10,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.controller.dto.ReservationRequest;
 import roomescape.service.ReservationService;
 import roomescape.service.dto.ReservationResponse;
 
 @RestController
+@RequestMapping("/reservations")
 @RequiredArgsConstructor
 public class RoomescapeController {
 
     private final ReservationService reservationService;
 
-    @GetMapping("/reservations")
+    @GetMapping
     public ResponseEntity<List<ReservationResponse>> getReservations() {
         final List<ReservationResponse> reservationResponses = reservationService.getReservations();
         return ResponseEntity.ok(reservationResponses);
     }
 
-    @PostMapping("/reservations")
+    @PostMapping
     public ResponseEntity<ReservationResponse> createReservation(@Valid @RequestBody ReservationRequest request) {
         final ReservationResponse reservationResponse = reservationService.saveReservation(request);
         return ResponseEntity.ok(reservationResponse);
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
         reservationService.deleteReservation(id);
         return ResponseEntity.ok().build();

1d9e84104f2d06b4ebe5d3197e0043aa3c8514cd
kjungw <sagakkb@gmail.com>
2025-04-23 14:54:30 +0900

    refactor: 운영과 테스트 DB 분리

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -139,7 +139,7 @@ public class MissionStepTest {
     void canConnectDatabase() {
         try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
             assertThat(connection).isNotNull();
-            assertThat(connection.getCatalog()).isEqualTo("ROOMESCAPE_DATABASE");
+            assertThat(connection.getCatalog()).isEqualTo("TEST_ROOMESCAPE_DATABASE");
             assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
         } catch (SQLException e) {
             throw new RuntimeException(e);

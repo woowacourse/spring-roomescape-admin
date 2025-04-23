f47ed8023396e97ebcf41f5b3e98e63fe95a9006
Han <jumdo12@gmail.com>
2025-04-23 11:10:49 +0900

    feat: ReservationTime 유효성 검사

--- src/main/java/roomescape/model/ReservationTime.java
+++ src/main/java/roomescape/model/ReservationTime.java
@@ -4,4 +4,13 @@ import java.time.LocalTime;
 
 public record ReservationTime(Long id, LocalTime startAt) {
 
+    public ReservationTime {
+        validateStartAt(startAt);
+    }
+
+    void validateStartAt(LocalTime startAt) {
+        if (startAt == null) {
+            throw new IllegalArgumentException("startAt cannot be null");
+        }
+    }
 }

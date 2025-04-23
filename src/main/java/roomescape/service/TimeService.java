7132abf282fe3e32144bee1870e4fd38dde39d59
LJM\eueo8 <eueo8259@gmail.com>
2025-04-23 11:56:54 +0900

    feat : 모든 Time 정보를 가져오는 getAllTimes 메서드 구현

--- src/main/java/roomescape/service/TimeService.java
+++ src/main/java/roomescape/service/TimeService.java
@@ -1,7 +1,21 @@
 package roomescape.service;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
+import roomescape.dao.TimeDao;
+import roomescape.model.Time;
 
 @Service
 public class TimeService {
+
+    private final TimeDao timeDao;
+
+    public TimeService(TimeDao timeDao) {
+        this.timeDao = timeDao;
+    }
+
+    public List<Time> getAllTimes() {
+        return timeDao.findAll();
+    }
+
 }

be705340e6f87f455b007b933bb8367fbe6c2497
ryan-dia <cjfdnjs158@naver.com>
2025-04-23 14:52:24 +0900

    refactor: ReservationTime 대신 dto를 반환하도록 변경

--- src/main/java/roomescape/service/dto/ReservationResponse.java
+++ src/main/java/roomescape/service/dto/ReservationResponse.java
@@ -2,15 +2,14 @@ package roomescape.service.dto;
 
 import java.time.LocalDate;
 import roomescape.Reservation;
-import roomescape.ReservationTime;
 
-public record ReservationResponse(Long id, String name, LocalDate date, ReservationTime time) {
+public record ReservationResponse(Long id, String name, LocalDate date, ReservationTimeResponse time) {
     public ReservationResponse(Reservation reservation) {
         this(
                 reservation.getId(),
                 reservation.getName(),
                 reservation.getDate(),
-                reservation.getTime()
+                new ReservationTimeResponse(reservation.getTime())
         );
     }
 }

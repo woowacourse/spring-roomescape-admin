aeedc95c58abb9b19e7b1a0a6db1b25fa96d2645
ryan-dia <cjfdnjs158@naver.com>
2025-04-23 10:50:40 +0900

    test: 8단계 테스트 세팅

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -19,7 +19,7 @@ import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStepTest {
+class MissionStepTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -56,7 +56,7 @@ public class MissionStepTest {
         final LocalDate localDate = now.plusDays(1);
         params.put("name", "브라운");
         params.put("date", localDate.toString());
-        params.put("time", "15:40");
+        params.put("reservationTime", "15:40");
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -167,5 +167,26 @@ public class MissionStepTest {
                 .when().delete("/times/1")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @Test
+    void 팔단계() {
+        Map<String, Object> reservation = new HashMap<>();
+        reservation.put("name", "브라운");
+        reservation.put("date", "2023-08-05");
+        reservation.put("timeId", 1);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservation)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
     }
 }

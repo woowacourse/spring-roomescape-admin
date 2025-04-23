073c3c9bb4039ed2126053e265dae2d9c16302c3
Han <jumdo12@gmail.com>
2025-04-23 11:14:41 +0900

    test: Reservation Page, Time Page 테스트

--- src/test/java/roomescape/controller/AdminPageControllerTest.java
+++ src/test/java/roomescape/controller/AdminPageControllerTest.java
@@ -16,4 +16,22 @@ class AdminPageControllerTest {
                 .then().log().all()
                 .statusCode(200);
     }
+
+    @DisplayName("Reservation Page 테스트")
+    @Test
+    void reservationPageTest() {
+        RestAssured.given().log().all()
+                .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @DisplayName("Time Page 테스트")
+    @Test
+    void reservationTimePageTest() {
+        RestAssured.given().log().all()
+                .when().get("/admin/time")
+                .then().log().all()
+                .statusCode(200);
+    }
 }

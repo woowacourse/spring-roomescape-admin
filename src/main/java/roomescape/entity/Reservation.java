a7bf58cf6b9851cf29e38deda2542f22860dafc3
ChoiDongjun <adong0808@naver.com>
2025-04-23 11:53:58 +0900

    refactor: time 필드를 ReservationTime 타입으로 수정

--- src/main/java/roomescape/entity/Reservation.java
+++ src/main/java/roomescape/entity/Reservation.java
@@ -2,16 +2,14 @@ package roomescape.entity;
 
 
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Objects;
 
 public class Reservation {
     private final Long id;
     private final String name;
     private final LocalDate date;
-    private final LocalTime time;
+    private final ReservationTime time;
 
-    public Reservation(final Long id, final String name, final LocalDate date, final LocalTime time) {
+    public Reservation(final Long id, final String name, final LocalDate date, final ReservationTime time) {
         if (LocalDate.now().isAfter(date)) {
             throw new IllegalArgumentException("예약은 미래 날짜여야 합니다.");
         }
@@ -33,19 +31,7 @@ public class Reservation {
         return date;
     }
 
-    public LocalTime getTime() {
+    public ReservationTime getTime() {
         return time;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Reservation that = (Reservation) o;
-        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(date, that.date) && Objects.equals(time, that.time);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, date, time);
     }
 }

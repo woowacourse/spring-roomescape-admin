0289b56d05fc1b0fbf1ab094d894cdf9f89d8995
taek2222 <oot309@daum.net>
2025-04-23 21:42:27 +0900

    test: 예약 시간 화면 요청 테스트 작성

--- src/test/java/roomescape/controller/AdminPageControllerTest.java
+++ src/test/java/roomescape/controller/AdminPageControllerTest.java
@@ -28,4 +28,12 @@ class AdminPageControllerTest {
                 .then().log().all()
                 .statusCode(200);
     }
+
+    @Test
+    void admin_예약_시간_화면_요청을_성공한다() {
+        RestAssured.given().log().all()
+                .when().get("/admin/time")
+                .then().log().all()
+                .statusCode(200);
+    }
 }

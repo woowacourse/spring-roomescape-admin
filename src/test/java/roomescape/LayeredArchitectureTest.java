153e7282f153ad6861a80334917ad8dbf48eb9cf
changuii <rhljh7410@gmail.com>
2025-04-23 17:58:34 +0900

    test: 9단계 테스트 추가

---
+++ src/test/java/roomescape/LayeredArchitectureTest.java
@@ -0,0 +1,32 @@
+package roomescape;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Field;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import roomescape.reservation.ReservationController;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class LayeredArchitectureTest {
+
+    @Autowired
+    private ReservationController reservationController;
+
+    @Test
+    void 구단계() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
+    }
+}

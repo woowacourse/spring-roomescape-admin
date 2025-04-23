65f5cec5970926d969ab8322c174ef014b202527
ChoiDongjun <adong0808@naver.com>
2025-04-23 09:50:08 +0900

    feat: 예약 시간 관리를 위한 ReservationTime 추가

---
+++ src/main/java/roomescape/entity/ReservationTime.java
@@ -0,0 +1,18 @@
+package roomescape.entity;
+
+import java.time.LocalTime;
+
+public class ReservationTime {
+
+    private Long id;
+
+    private LocalTime time;
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+}

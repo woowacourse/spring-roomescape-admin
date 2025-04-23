5bad0e307418264d03920bd9cd0f61f9731dd0fd
rladmstn <oz33177351@gmail.com>
2025-04-23 21:30:09 +0900

    refactor: 주석 제거

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -19,12 +19,6 @@ public class ReservationService {
         return reservationRepository.findAll();
     }
 
-    /*
-    TODO: dto를 repository에 넘겨주면 왜 안될까? -> 난 dto가 view와 근접한 객체라고 생각했음
-    근데 repository에서 컬럼 별 필드 매핑 해줄거면 repository에 객체 말고 dto 넘기는게 오히려 일 덜하는거 아닌가?
-    service는 by-pass만 해주면 되니까
-    또 service 계층에선 reservation id를 못만드네 생각해보니까
-     */
     public Reservation createReservation(CreateReservationDto createReservationDto) {
         Long id = reservationRepository.addAndGetId(createReservationDto);
         return reservationRepository.findById(id);

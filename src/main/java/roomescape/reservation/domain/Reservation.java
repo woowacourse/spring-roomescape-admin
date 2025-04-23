0696587b29362369ed5bb96e9c96b4df3fbff2e5
Yeongjae Kim <sees77770@gmail.com>
2025-04-24 01:44:20 +0900

    feat: 현재 이후 시간에만 예약 가능 검사 추가

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -1,7 +1,9 @@
 package roomescape.reservation.domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import roomescape.reservation.domain.exception.PastReservationException;
 import roomescape.time.domain.ReservationTime;
 
 public class Reservation {
@@ -20,6 +22,16 @@ public class Reservation {
 
     public Reservation(String reserverName, LocalDate date, ReservationTime reservationTime) {
         this(null, reserverName, date, reservationTime);
+        validatePast();
+    }
+
+    private void validatePast() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime reservationDateTime = LocalDateTime.of(reservationDate.getDate(), reservationTime.getStartAt());
+
+        if (reservationDateTime.isBefore(now) || reservationDateTime.isEqual(now)) {
+            throw new PastReservationException("[ERROR] 현재 시간 이후로 예약할 수 있습니다.");
+        }
     }
 
     public Long getId() {
@@ -45,5 +57,5 @@ public class Reservation {
     public Long getTimeId() {
         return reservationTime.getId();
     }
-    
+
 }
--- src/test/java/roomescape/reservation/domain/ReservationTest.java
+++ src/test/java/roomescape/reservation/domain/ReservationTest.java
@@ -1,11 +1,17 @@
 package roomescape.reservation.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
+import roomescape.reservation.domain.exception.PastReservationException;
 import roomescape.reservation.domain.exception.ReserverNameEmptyException;
+import roomescape.time.domain.ReservationTime;
 
 public class ReservationTest {
 
@@ -16,4 +22,25 @@ public class ReservationTest {
         assertThatThrownBy(() -> new ReserverName(input))
                 .isInstanceOf(ReserverNameEmptyException.class);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1})
+    void 현재나_현재_이전_시간에는_예약할_수_없다(int minuteOffset) {
+        LocalDate today = LocalDate.now();
+        LocalTime time = LocalTime.now().minusMinutes(minuteOffset);
+        ReservationTime reservationTime = new ReservationTime(time);
+
+        assertThatThrownBy(() -> new Reservation("홍길동", today, reservationTime))
+                .isInstanceOf(PastReservationException.class);
+    }
+
+    @Test
+    void 현재_이후_시간에는_예약이_성공한다() {
+        LocalDate today = LocalDate.now();
+        ReservationTime futureTime = new ReservationTime(LocalTime.now().plusMinutes(10));
+
+        assertThatCode(() -> new Reservation("홍길동", today, futureTime))
+                .doesNotThrowAnyException();
+    }
+
 }

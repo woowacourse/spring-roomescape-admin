864c7123a47651b33bfe2660728f8526548398dc
LJM\eueo8 <eueo8259@gmail.com>
2025-04-23 11:53:50 +0900

    feat : 모든 Time 정보를 가져오는 findAll 메서드 구현

--- src/main/java/roomescape/dao/TimeDao.java
+++ src/main/java/roomescape/dao/TimeDao.java
@@ -1,7 +1,29 @@
 package roomescape.dao;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import roomescape.model.Time;
 
 @Repository
 public class TimeDao {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<Time> actorRowMapper = (resultSet, rowNum) -> {
+        Time time = new Time(
+                resultSet.getLong("id"),
+                resultSet.getTime("time").toLocalTime()
+        );
+        return time;
+    };
+
+    public List<Time> findAll() {
+        String sql = "SELECT * FROM time";
+        return jdbcTemplate.query(sql, actorRowMapper);
+    }
+
 }

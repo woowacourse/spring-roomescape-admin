05ba5a4dd76e89afa2a2e0c827246786bb89ac12
(minjae8563) <(minjae8563@naver.com)>
2025-04-23 10:59:24 +0900

    feat: ReservationTimeRepository, ReservationTimeService 빈에 등록

--- src/main/java/roomescape/AppConfig.java
+++ src/main/java/roomescape/AppConfig.java
@@ -1,13 +1,16 @@
 package roomescape;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import roomescape.reservation.JdbcReservationRepository;
+import roomescape.reservationTime.JdbcReservationTimeRepository;
 import roomescape.reservation.ReservationRepository;
 import roomescape.reservation.ReservationService;
 import roomescape.reservation.ReservationServiceImpl;
+import roomescape.reservationTime.ReservationTimeRepository;
+import roomescape.reservationTime.ReservationTimeService;
+import roomescape.reservationTime.ReservationTimeServiceImpl;
 
 @Configuration
 public class AppConfig {
@@ -20,6 +23,16 @@ public class AppConfig {
     @Bean
     public ReservationService reservationService(ReservationRepository reservationRepository) {
         return new ReservationServiceImpl(reservationRepository);
+    }
+
+    @Bean
+    public ReservationTimeRepository reservationTimeRepository(JdbcTemplate jdbcTemplate) {
+        return new JdbcReservationTimeRepository(jdbcTemplate);
+    }
+
+    @Bean
+    public ReservationTimeService reservationTimeService(JdbcTemplate jdbcTemplate) {
+        return new ReservationTimeServiceImpl(reservationTimeRepository(jdbcTemplate));
     }
 
 }

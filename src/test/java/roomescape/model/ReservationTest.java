52a49fe7ca8a40f0e7fb2f4f9ad8144c9dcef268
Han <jumdo12@gmail.com>
2025-04-23 14:47:11 +0900

    test: 테스트 ReservationTime 반영

--- src/test/java/roomescape/model/ReservationTest.java
+++ src/test/java/roomescape/model/ReservationTest.java
@@ -12,17 +12,17 @@ import java.util.stream.Stream;
 
 class ReservationTest {
 
-    @DisplayName("Date와 Time이 존재하지 않으면 생성 불가능하다")
+    @DisplayName("Date 와 ReservationTime 이 존재하지 않으면 생성 불가능하다")
     @ParameterizedTest
     @MethodSource("invalidReservationArguments")
-    void invalidReservationDateTimeTest(Long id, String name, LocalDate date, LocalTime time) {
-        Assertions.assertThatThrownBy(() -> new Reservation(id, name, date, time))
+    void invalidReservationDateTimeTest(Long id, String name, LocalDate date, ReservationTime reservationTime) {
+        Assertions.assertThatThrownBy(() -> new Reservation(id, name, date, reservationTime))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     static Stream<Arguments> invalidReservationArguments() {
         return Stream.of(
-                Arguments.of(1L, "가이온", null, LocalTime.now()),
+                Arguments.of(1L, "가이온", null, new ReservationTime(1L, LocalTime.now())),
                 Arguments.of(1L, "가이온", LocalDate.now(), null)
         );
     }
@@ -32,10 +32,10 @@ class ReservationTest {
     @MethodSource("invalidNames")
     void invalidReservationNameTest(String reservationName) {
         Long id = 1L;
-        LocalDate localDate = LocalDate.now();
-        LocalTime localTime = LocalTime.now();
+        LocalDate date = LocalDate.now();
+        ReservationTime reservationTime = new ReservationTime(1L, LocalTime.now());
 
-        Assertions.assertThatThrownBy(() -> new Reservation(id, reservationName, localDate, localTime))
+        Assertions.assertThatThrownBy(() -> new Reservation(id, reservationName, date, reservationTime))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
--- src/test/java/roomescape/model/ReservationTimeTest.java
+++ src/test/java/roomescape/model/ReservationTimeTest.java
@@ -4,16 +4,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalTime;
-
 class ReservationTimeTest {
 
     @DisplayName("예약 시간이 null 일 수 없다.")
     @Test
     void reservationTimeTest() {
-        LocalTime nullLocalTime = null;
-
-        Assertions.assertThatThrownBy(() -> new ReservationTime(1L, nullLocalTime))
+        Assertions.assertThatThrownBy(() -> new ReservationTime(1L, null))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

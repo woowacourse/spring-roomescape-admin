f327e76f8615cad0017841687984461adafae7fe
Han <jumdo12@gmail.com>
2025-04-23 16:44:53 +0900

    test: 미션 테스트 수정

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
-import roomescape.model.Reservation;
+import roomescape.dto.ReservationResponseDto;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -58,10 +58,20 @@ public class MissionStepTest {
 
         @Test
         void 삼단계() {
-            Map<String, String> params = new HashMap<>();
+            Map<String, Object> reservationTime = new HashMap<>();
+            reservationTime.put("startAt", "10:00");
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(reservationTime)
+                    .when().post("/times")
+                    .then().log().all()
+                    .statusCode(200);
+
+            Map<String, Object> params = new HashMap<>();
             params.put("name", "브라운");
             params.put("date", "2023-08-05");
-            params.put("time", "15:40");
+            params.put("timeId", 1);
 
             RestAssured.given().log().all()
                     .contentType(ContentType.JSON)
@@ -116,15 +126,32 @@ public class MissionStepTest {
 
         @Test
         void 오단계() {
-            jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
+            Map<String, Object> reservationTime = new HashMap<>();
+            reservationTime.put("startAt", "10:00");
 
-            List<Reservation> reservations = RestAssured.given().log().all()
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(reservationTime)
+                    .when().post("/times")
+                    .then().log().all()
+                    .statusCode(200);
+
+            jdbcTemplate.update("INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)", "브라운", "2023-08-05", 1);
+
+            List<ReservationResponseDto> reservations = RestAssured.given().log().all()
                     .when().get("/reservations")
                     .then().log().all()
                     .statusCode(200).extract()
-                    .jsonPath().getList(".", Reservation.class);
+                    .jsonPath().getList(".", ReservationResponseDto.class);
 
-            Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+            String sql = """
+                    SELECT count(1)
+                    from reservation as r
+                    inner join reservation_time as rt
+                    on rt.id = r.time_id
+                    """;
+
+            Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
 
             assertThat(reservations.size()).isEqualTo(count);
         }
@@ -138,10 +165,20 @@ public class MissionStepTest {
 
         @Test
         void 육단계() {
-            Map<String, String> params = new HashMap<>();
+            Map<String, Object> reservationTime = new HashMap<>();
+            reservationTime.put("startAt", "10:00");
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(reservationTime)
+                    .when().post("/times")
+                    .then().log().all()
+                    .statusCode(200);
+
+            Map<String, Object> params = new HashMap<>();
             params.put("name", "브라운");
             params.put("date", "2023-08-05");
-            params.put("time", "10:00");
+            params.put("timeId", 1);
 
             RestAssured.given().log().all()
                     .contentType(ContentType.JSON)
@@ -150,7 +187,14 @@ public class MissionStepTest {
                     .then().log().all()
                     .statusCode(200);
 
-            Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+            String sql = """
+                    SELECT count(1)
+                    from reservation as r
+                    inner join reservation_time as rt
+                    on rt.id = r.time_id
+                    """;
+
+            Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
             assertThat(count).isEqualTo(1);
 
             RestAssured.given().log().all()
@@ -158,7 +202,7 @@ public class MissionStepTest {
                     .then().log().all()
                     .statusCode(200);
 
-            Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+            Integer countAfterDelete = jdbcTemplate.queryForObject(sql, Integer.class);
             assertThat(countAfterDelete).isEqualTo(0);
         }
     }
@@ -188,6 +232,42 @@ public class MissionStepTest {
                     .when().delete("/times/1")
                     .then().log().all()
                     .statusCode(200);
+        }
+    }
+
+    @Nested
+    class Step8 {
+
+        @Test
+        void 팔단계() {
+            Map<String, Object> reservationTime = new HashMap<>();
+            reservationTime.put("startAt", "10:00");
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(reservationTime)
+                    .when().post("/times")
+                    .then().log().all()
+                    .statusCode(200);
+
+            Map<String, Object> reservation = new HashMap<>();
+            reservation.put("name", "브라운");
+            reservation.put("date", "2023-08-05");
+            reservation.put("timeId", 1);
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(reservation)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(200);
+
+
+            RestAssured.given().log().all()
+                    .when().get("/reservations")
+                    .then().log().all()
+                    .statusCode(200)
+                    .body("size()", is(1));
         }
     }
 }

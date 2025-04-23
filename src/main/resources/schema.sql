3146f10e7692f5bd00cca040c04f0acbdef15a4d
LJM\eueo8 <eueo8259@gmail.com>
2025-04-23 11:38:34 +0900

    chore : time 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,10 @@ CREATE TABLE reservation
     time    VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE time
+(
+    id      BIGINT       NOT NULL AUTO_INCREMENT,
+    time    VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);

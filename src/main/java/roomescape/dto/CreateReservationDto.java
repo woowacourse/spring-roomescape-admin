152667e832faffeb6ce852412982769c86fa42c6
rladmstn <oz33177351@gmail.com>
2025-04-23 14:10:10 +0900

    refactor: dto 리팩토링

--- src/main/java/roomescape/dto/CreateReservationDto.java
+++ src/main/java/roomescape/dto/CreateReservationDto.java
@@ -1,14 +1,4 @@
 package roomescape.dto;
 
-import roomescape.entity.Reservation;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-public record CreateReservationDto(String name, LocalDate date, LocalTime time) {
-
-    public Reservation toEntity(Long id) {
-        return new Reservation(id, name, LocalDateTime.of(date, time));
-    }
+public record CreateReservationDto(String name, String date, Long timeId) {
 }
--- src/main/java/roomescape/dto/ReservationResponseDto.java
+++ src/main/java/roomescape/dto/ReservationResponseDto.java
@@ -2,21 +2,15 @@ package roomescape.dto;
 
 import roomescape.entity.Reservation;
 
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-
 public record ReservationResponseDto(Long id, String name, String date, String time) {
-
-    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.KOREA);
-    private static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm", Locale.KOREA);
 
     public static ReservationResponseDto from(Reservation reservation) {
 
-         return new ReservationResponseDto(
-                 reservation.getId(),
-                 reservation.getName(),
-                 reservation.getDateTime().toLocalDate().format(dateFormatter),
-                 reservation.getDateTime().toLocalTime().format(timeFormatter)
-         );
+        return new ReservationResponseDto(
+                reservation.getId(),
+                reservation.getName(),
+                reservation.getDate(),
+                reservation.getTime().getStartAt()
+        );
     }
 }

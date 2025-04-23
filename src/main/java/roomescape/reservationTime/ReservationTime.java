fe2e144ea195c777b6c89bd3e59f59f47434998a
(minjae8563) <(minjae8563@naver.com)>
2025-04-23 10:58:23 +0900

    feat: ReservationTime 클래스 구현

---
+++ src/main/java/roomescape/reservationTime/ReservationTime.java
@@ -0,0 +1,33 @@
+package roomescape.reservationTime;
+
+import java.time.LocalTime;
+
+public class ReservationTime {
+
+    private Long id;
+    private LocalTime startAt;
+
+    public ReservationTime() {
+    }
+
+    public ReservationTime(LocalTime startAt) {
+        this.startAt = startAt;
+    }
+
+    public ReservationTime(Long id, LocalTime startAt) {
+        this.id = id;
+        this.startAt = startAt;
+    }
+
+    public static ReservationTime toEntity(Long id, ReservationTime reservationTime) {
+        return new ReservationTime(id, reservationTime.getStartAt());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalTime getStartAt() {
+        return startAt;
+    }
+}

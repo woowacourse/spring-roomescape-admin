7de57935da4529dac919780e5504257cff82447a
DEV_WOONG <ywcsuwon@gmail.com>
2025-04-23 14:00:06 +0900

    feat(green): 시간을 저장한다

--- src/main/java/roomescape/repository/H2ReservationTimeRepository.java
+++ src/main/java/roomescape/repository/H2ReservationTimeRepository.java
@@ -1,12 +1,39 @@
 package roomescape.repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Time;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import roomescape.entity.ReservationTime;
 
 @Repository
 public class H2ReservationTimeRepository implements ReservationTimeRepository{
+
+      private final JdbcTemplate jdbcTemplate;
+
+    public H2ReservationTimeRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
-    public ReservationTime save(ReservationTime time) {
-        return null;
+    public ReservationTime save(ReservationTime reservationTime) {
+        final String sql = "INSERT INTO reservation_time (start_at) VALUES (?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setTime(1, Time.valueOf(reservationTime.getTime()));
+                    return ps;
+        },keyHolder);
+        long generatedId = keyHolder.getKey().longValue();
+
+        return new ReservationTime(
+                generatedId,
+                reservationTime.getTime()
+        );
     }
 }

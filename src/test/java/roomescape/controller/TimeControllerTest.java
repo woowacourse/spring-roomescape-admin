be32f6afa92a0729339f387ee282c25b00a1bf67
(minjae8563) <(minjae8563@naver.com)>
2025-04-23 10:57:40 +0900

    test: TimeController 테스트 추가

---
+++ src/test/java/roomescape/controller/TimeControllerTest.java
@@ -0,0 +1,65 @@
+package roomescape.controller;
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class TimeControllerTest {
+
+    Map<String, String> params = new HashMap<>();
+
+    @BeforeEach
+    void setUp() {
+        params.put("startAt", "10:00");
+    }
+
+    @Test
+    @DisplayName("시간 추가 테스트")
+    void saveReservationTimeTest() {
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/times")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+    }
+
+    @Test
+    @DisplayName("시간 삭제 테스트")
+    void deleteReservationTimeTest() {
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().delete("/times/1")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @AfterEach
+    void afterEach() {
+        params.clear();
+    }
+
+}

26c9dd2ef60a0bbd50711f8e72a0e1148bd6db26
CheChe903 <cheche903@naver.com>
2025-04-23 15:54:48 +0900

    test(ReservationTimeCreateRequest): DTO 단위 테스트

---
+++ src/test/java/roomescape/dto/request/ReservationTimeCreateRequestTest.java
@@ -0,0 +1,22 @@
+package roomescape.dto.request;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ReservationTimeCreateRequestTest {
+
+    @DisplayName("시간이 비어있으면 예외가 발생한다.")
+    @Test
+    void validateTimeNullThrowExceptionTest() {
+
+        // given
+
+        // when & then
+        assertThatThrownBy(() -> new ReservationTimeCreateRequest(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("빈 값으로 예약할 수 없습니다.");
+    }
+
+}

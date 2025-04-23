a8b4149369e0ecaf55841a0b9d93f24b3f02c085
threepebbles <threepebbles@naver.com>
2025-04-23 09:40:24 +0900

    remove: 사용하지 않는 기능 제거
    - 요청 dto를 도메인 객체로 변환하는 기능 제거

--- src/main/java/roomescape/admin/controller/dto/ReservationTimeRequest.java
+++ src/main/java/roomescape/admin/controller/dto/ReservationTimeRequest.java
@@ -1,13 +1,9 @@
 package roomescape.admin.controller.dto;
 
 import java.time.LocalTime;
-import roomescape.admin.domain.ReservationTime;
 
 public record ReservationTimeRequest(
         LocalTime startAt
 ) {
-
-    public ReservationTime toReservationTime() {
-        return new ReservationTime(null, startAt);
-    }
+    
 }
--- src/main/java/roomescape/user/controller/dto/ReservationRequest.java
+++ src/main/java/roomescape/user/controller/dto/ReservationRequest.java
@@ -2,15 +2,11 @@ package roomescape.user.controller.dto;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import roomescape.user.domain.Reservation;
 
 public record ReservationRequest(
         String name,
         LocalDate date,
         LocalTime time
 ) {
-
-    public Reservation toReservation() {
-        return new Reservation(name, date, time);
-    }
+    
 }

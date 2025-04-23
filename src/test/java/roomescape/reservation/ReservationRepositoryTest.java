8087e826b080992574421c677a4516aa92266618
gabean13 <choegabin171@gmail.com>
2025-04-23 15:53:50 +0900

    test: reservation test 코드 추가

---
+++ src/test/java/roomescape/reservation/ReservationRepositoryTest.java
@@ -0,0 +1,88 @@
+package roomescape.reservation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import roomescape.reservation.model.Reservation;
+import roomescape.reservation.model.ReservationDetails;
+import roomescape.reservation.repository.ReservationRepository;
+
+@JdbcTest(properties = "application-test.properties")
+@Import(ReservationRepository.class)
+class ReservationRepositoryTest {
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @DisplayName("전체 예약 리스트 불러온다.")
+    @Test
+    void findAll() {
+        // given
+        ReservationDetails reservationDetails = new ReservationDetails("test", LocalDate.of(2024, 12, 1),
+                LocalTime.of(12, 1));
+        reservationRepository.insertReservation(reservationDetails);
+
+        // when
+        List<Reservation> reservations = reservationRepository.findAll();
+
+        // then
+        assertThat(reservations).hasSize(1);
+        assertThat(reservations.getFirst())
+                .hasFieldOrPropertyWithValue("name", "test")
+                .hasFieldOrPropertyWithValue("date", LocalDate.of(2024, 12, 1))
+                .hasFieldOrPropertyWithValue("time", LocalTime.of(12, 1));
+    }
+
+    @DisplayName("예약을 추가한다.")
+    @Test
+    void insertReservation() {
+        // given
+        ReservationDetails reservationDetails = new ReservationDetails("test", LocalDate.of(2024, 12, 1),
+                LocalTime.of(12, 1));
+
+        // when
+        Reservation reservation = reservationRepository.insertReservation(reservationDetails);
+
+        // then
+        assertThat(reservation)
+                .hasFieldOrPropertyWithValue("name", "test")
+                .hasFieldOrPropertyWithValue("date", LocalDate.of(2024, 12, 1))
+                .hasFieldOrPropertyWithValue("time", LocalTime.of(12, 1));
+    }
+
+    @DisplayName("id가 일치하는 예약을 삭제하면 true를 반환한다.")
+    @Test
+    void deleteReservationById_existId() {
+        // given
+        ReservationDetails reservationDetails = new ReservationDetails("test", LocalDate.of(2024, 12, 1),
+                LocalTime.of(12, 1));
+        reservationRepository.insertReservation(reservationDetails);
+        long id = 1L;
+
+        // when
+        boolean isDeleted = reservationRepository.deleteReservationById(id);
+
+        // then
+        assertThat(isDeleted).isTrue();
+    }
+
+    @DisplayName("id가 존재하지 않는 예약을 삭제 요청하면 false를 반환한다.")
+    @Test
+    void deleteReservationById_notExistId() {
+        // given
+        long id = 1L;
+
+        // when
+        boolean isDeleted = reservationRepository.deleteReservationById(id);
+
+        // then
+        assertThat(isDeleted).isFalse();
+    }
+}

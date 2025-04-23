9ef2f15cc49124450bcc2ba749750675c4abd563
AppleMint98 <applemint9821@gmail.com>
2025-04-23 16:41:44 +0900

    refactor(controller): 반환 페이지 변경

--- src/main/java/roomescape/controller/AdminPageController.java
+++ src/main/java/roomescape/controller/AdminPageController.java
@@ -13,9 +13,11 @@ public class AdminPageController {
 
     @GetMapping("/admin/reservation")
     public String getReservationPage() {
-        return "admin/reservation-legacy";
+        return "admin/reservation";
     }
 
     @GetMapping("/admin/time")
-    public String getTimePage() { return "admin/time"; }
+    public String getTimePage() {
+        return "admin/time";
+    }
 }
--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.domain.Reservation;
 import roomescape.dto.ReservationRequest;
 import roomescape.dto.ReservationResponse;
 import roomescape.service.RoomescapeService;

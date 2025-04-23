d74f71e99ba6c5de021369f1701168b56ac8af62
bowook <mbosx316@naver.com>
2025-04-23 16:58:05 +0900

    feat(AdminController) : /admin/time 페이지 view 반환

--- src/main/java/roomescape/admin/controller/AdminController.java
+++ src/main/java/roomescape/admin/controller/AdminController.java
@@ -17,4 +17,9 @@ public class AdminController {
     public String getReservationPage() {
         return "admin/reservation-legacy";
     }
+
+    @GetMapping("/time")
+    public String getTimePage() {
+        return "admin/time";
+    }
 }

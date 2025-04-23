6926e30b5daf2c5bbda98081de7d8700eaf3f5e1
BWY <quantumjump10@gmail.com>
2025-04-23 11:24:32 +0900

    style: AdminPageController 패키지 이동

--- src/test/java/roomescape/admin/controller/AdminPageControllerTest.java
+++ src/test/java/roomescape/admin/controller/AdminPageControllerTest.java
@@ -1,12 +1,10 @@
-package roomescape;
+package roomescape.admin.controller;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)

0b2b1b4cf30d1a49b0ef0838e9dc16e52f2896df
ChoiDongjun <adong0808@naver.com>
2025-04-23 11:02:44 +0900

    feat: ReservationTime 생성자, final 키워드 추가

--- src/main/java/roomescape/entity/ReservationTime.java
+++ src/main/java/roomescape/entity/ReservationTime.java
@@ -4,9 +4,14 @@ import java.time.LocalTime;
 
 public class ReservationTime {
 
-    private Long id;
+    private final Long id;
 
-    private LocalTime time;
+    private final LocalTime time;
+
+    public ReservationTime(final Long id, final LocalTime time) {
+        this.id = id;
+        this.time = time;
+    }
 
     public Long getId() {
         return id;

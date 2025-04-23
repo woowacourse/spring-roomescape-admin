0ec08cf6447ffb30b4bc738b7cfcc7b03e0dd30d
taek2222 <oot309@daum.net>
2025-04-24 00:31:17 +0900

    chore: 예약 테이블의 날짜 DATE 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE reservation
 (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
-    date    VARCHAR(255) NOT NULL,
+    date    DATE         NOT NULL,
     time_id BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id)

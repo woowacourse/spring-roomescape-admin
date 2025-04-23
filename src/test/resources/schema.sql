d1a68b11c726b14f48292ee331732ed9199ec3a1
junhaa <2171326@hansung.ac.kr>
2025-04-23 13:27:43 +0900

    chore: 시간 관련 테이블 쿼리 SQL 추가

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,9 +1,18 @@
 DROP TABLE IF EXISTS reservation;
+DROP TABLE IF EXISTS reservation_time;
+
 CREATE TABLE reservation
 (
     id   BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     date VARCHAR(255) NOT NULL,
     time VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE reservation_time
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );

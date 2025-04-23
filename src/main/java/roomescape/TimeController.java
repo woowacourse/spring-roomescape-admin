dfc7e66498cd17c6e3ce26f2a2c09b22bded754f
byesol <bsj5967@naver.com>
2025-04-23 22:17:52 +0900

    feat: 7단계 시간 삭제 API 구현

--- src/main/java/roomescape/TimeController.java
+++ src/main/java/roomescape/TimeController.java
@@ -8,7 +8,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -49,7 +51,13 @@ public class TimeController {
                     LocalTime.parse(resultSet.getString("start_at")));
         });
         return ResponseEntity.ok(reservationTimeResponseDtos);
+    }
 
+    @DeleteMapping("/times/{id}")
+    public ResponseEntity<Void> deleteReservationTime(@PathVariable long id) {
+        String sql = "delete from reservation_time where id = ?";
+        jdbcTemplate.update(sql, id);
+        return ResponseEntity.ok().build();
     }
 
 }

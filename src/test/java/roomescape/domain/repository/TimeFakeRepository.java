016d2e107c16d8f95d2301541726c7ef3a2a221d
junhaa <2171326@hansung.ac.kr>
2025-04-24 21:46:08 +0900

    test: service 계층 테스트 코드 작성
    - Fake Repository를 이용하여 Service 계층 테스트

---
+++ src/test/java/roomescape/domain/repository/TimeFakeRepository.java
@@ -0,0 +1,41 @@
+package roomescape.domain.repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import roomescape.reservation.domain.Time;
+import roomescape.reservation.domain.repository.TimeRepository;
+
+public class TimeFakeRepository implements TimeRepository {
+
+    private final Map<Long, Time> times = new ConcurrentHashMap<>();
+    private final AtomicLong idGenerator = new AtomicLong();
+
+    @Override
+    public Long save(Time time) {
+        Long id = idGenerator.incrementAndGet();
+        times.put(id, time.withId(id));
+        return id;
+    }
+
+    @Override
+    public List<Time> findAll() {
+        return times.values().stream().toList();
+    }
+
+    @Override
+    public int deleteById(Long id) {
+        if (times.containsKey(id)) {
+            times.remove(id);
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public Time findById(Long id) {
+        return times.get(id);
+    }
+
+}

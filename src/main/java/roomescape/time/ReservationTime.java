81f9678c7cb6daa54051ac5a5104a2941d8325e3
kjyyjk <rlawnsdud920@gmail.com>
2025-04-24 15:05:30 +0900

    feat: ReservationTime의 시간 Null 검증 추가

--- src/main/java/roomescape/time/ReservationTime.java
+++ src/main/java/roomescape/time/ReservationTime.java
@@ -7,8 +7,15 @@ public class ReservationTime {
     private final LocalTime startAt;
 
     public ReservationTime(Long id, LocalTime startAt) {
+        validateTime(startAt);
         this.id = id;
         this.startAt = startAt;
+    }
+
+    private void validateTime(LocalTime startAt) {
+        if (startAt == null) {
+            throw new IllegalArgumentException("시간 정보가 존재하지 않습니다.");
+        }
     }
 
     public Long getId() {

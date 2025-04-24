bf54da1ed404e251bcab9c6c8ec7e8d8054d934c
eunseongu <rndmstjs75@gmail.com>
2025-04-24 14:46:44 +0900

    feat: 시간 삭제 기능 구현

--- src/main/java/roomescape/controller/ReservationTimeAPIController.java
+++ src/main/java/roomescape/controller/ReservationTimeAPIController.java
@@ -8,7 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,5 +50,12 @@ public class ReservationTimeAPIController {
                         LocalTime.parse(rs.getString("start_at"))
                 ));
         return ResponseEntity.ok(times);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTime(@PathVariable Long id) {
+        String sql = "delete from reservation_time where id = ?";
+        jdbcTemplate.update(sql, id);
+        return ResponseEntity.ok().build();
     }
 }

975187d7991edefcd0df3535d01c48fa64a26db1
Han <jumdo12@gmail.com>
2025-04-24 14:02:40 +0900

    test: 미션 9단계 테스트 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.controller.ReservationController;
 import roomescape.dto.ReservationResponseDto;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -268,6 +270,27 @@ public class MissionStepTest {
                     .then().log().all()
                     .statusCode(200)
                     .body("size()", is(1));
+        }
+    }
+
+    @Nested
+    class Step9 {
+
+        @Autowired
+        private ReservationController reservationController;
+
+        @Test
+        void 구단계() {
+            boolean isJdbcTemplateInjected = false;
+
+            for (Field field : reservationController.getClass().getDeclaredFields()) {
+                if (field.getType().equals(JdbcTemplate.class)) {
+                    isJdbcTemplateInjected = true;
+                    break;
+                }
+            }
+
+            assertThat(isJdbcTemplateInjected).isFalse();
         }
     }
 }

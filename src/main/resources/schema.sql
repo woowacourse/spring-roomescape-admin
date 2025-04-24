164ce365cad6b6647254315b8b5fa0a620646586
ppparkta <pppppark2@gmail.com>
2025-04-24 16:30:02 +0900

    feat: Database 설정 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+create table reservation
+(
+    id   bigint       not null auto_increment,
+    name varchar(255) not null,
+    date varchar(255) not null,
+    time varchar(255) not null,
+    primary key (id)
+);

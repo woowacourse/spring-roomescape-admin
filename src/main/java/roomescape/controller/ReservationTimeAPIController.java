8b1d8326f44bc83ffde4b0086d502d933b43557f
eunseongu <rndmstjs75@gmail.com>
2025-04-24 13:29:13 +0900

    feat: 시간 추가 기능 구현

---
+++ src/main/java/roomescape/controller/ReservationTimeAPIController.java
@@ -0,0 +1,38 @@
+package roomescape.controller;
+
+import java.sql.PreparedStatement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.ReservationTime;
+import roomescape.dto.ReservationTimeRequest;
+
+@RestController
+@RequestMapping("/times")
+public class ReservationTimeAPIController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @PostMapping
+    public ResponseEntity<ReservationTime> addTime(@RequestBody ReservationTimeRequest reservationTimeRequest) {
+        String sql = "insert into reservation_time(start_at) values (?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, reservationTimeRequest.getStartAt().toString());
+            return ps;
+        }, keyHolder);
+        ReservationTime reservationTime = new ReservationTime(
+                keyHolder.getKey().longValue(),
+                reservationTimeRequest.getStartAt()
+        );
+        return ResponseEntity.ok(reservationTime);
+    }
+}

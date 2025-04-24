c03c105db5ac121006c1f93b61a40d5da0f2d813
민경윤 <ruddbs3308@naver.com>
2025-04-24 15:54:13 +0900

    feat: 방탈출 시간을 삭제하는 기능 구현

--- src/main/java/roomescape/presentation/controller/TimeController.java
+++ src/main/java/roomescape/presentation/controller/TimeController.java
@@ -2,7 +2,9 @@ package roomescape.presentation.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +37,16 @@ public class TimeController {
         final List<TimeResponse> timeResponses = timeService.findAll();
 
         return ResponseEntity.ok(timeResponses);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") final Long id) {
+        try {
+            timeService.remove(id);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().build();
     }
 }

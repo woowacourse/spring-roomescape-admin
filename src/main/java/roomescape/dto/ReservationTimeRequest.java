f54cea7c07966502e82d6ef0d679e4ee157cb1f9
supernovaMK <minkikim2002@gmail.com>
2025-04-24 14:32:28 +0900

    feat: 예약 시간 추가 request Dto 구현

---
+++ src/main/java/roomescape/dto/ReservationTimeRequest.java
@@ -0,0 +1,13 @@
+package roomescape.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalTime;
+import roomescape.model.ReservationTime;
+
+public record ReservationTimeRequest(@JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul")LocalTime startAt) {
+
+    public ReservationTime toEntity() {
+
+        return new ReservationTime(null, startAt);
+    }
+}

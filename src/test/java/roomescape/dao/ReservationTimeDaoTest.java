50acdf88806d5f66b44d2d73806826f49182b590
dltmddus <tmddus4671@gmail.com>
2025-04-24 19:17:06 +0900

    test: ReservationTimeDao 테스트 추가

--- src/test/java/roomescape/dao/ReservationTimeDaoTest.java
+++ src/test/java/roomescape/dao/ReservationTimeDaoTest.java
@@ -2,22 +2,18 @@ package roomescape.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 @SpringBootTest
-class ReservationDaoTest {
-    @Autowired
-    private ReservationDao reservationDao;
+class ReservationTimeDaoTest {
 
     @Autowired
     private ReservationTimeDao reservationTimeDao;
@@ -27,7 +23,6 @@ class ReservationDaoTest {
 
     @BeforeEach
     void setUp() {
-        jdbcTemplate.update("delete from reservation");
         jdbcTemplate.update("delete from reservation_time");
     }
 
@@ -35,30 +30,26 @@ class ReservationDaoTest {
     void 예약_시간을_추가할_수_있다() {
         // given
         ReservationTime reservationTime = new ReservationTime(LocalTime.of(10, 0));
-        Long id = reservationTimeDao.create(reservationTime);
-        Reservation reservation = new Reservation("포라", LocalDate.now(), new ReservationTime(id, reservationTime.getStartAt()));
 
         // when
-        reservationDao.create(reservation);
-        List<Reservation> reservationDaoAll = reservationDao.findAll();
+        reservationTimeDao.create(reservationTime);
+        List<ReservationTime> reservationTimeDaoAll = reservationTimeDao.findAll();
 
         // then
-        assertThat(reservationDaoAll.size()).isEqualTo(1);
+        assertThat(reservationTimeDaoAll.size()).isEqualTo(1);
     }
 
     @Test
     void 예약_시간을_조회할_수_있다() {
         // given
         ReservationTime reservationTime = new ReservationTime(LocalTime.of(10, 0));
-        Long id = reservationTimeDao.create(reservationTime);
-        Reservation reservation = new Reservation("포라", LocalDate.now(), new ReservationTime(id, reservationTime.getStartAt()));
-        reservationDao.create(reservation);
 
         // when
-        List<Reservation> reservationDaoAll = reservationDao.findAll();
+        reservationTimeDao.create(reservationTime);
+        List<ReservationTime> reservationTimeDaoAll = reservationTimeDao.findAll();
 
         // then
-        assertThat(reservationDaoAll.getFirst().getName()).isEqualTo("포라");
+        assertThat(reservationTimeDaoAll.getFirst().getStartAt()).isEqualTo(LocalTime.of(10, 0));
     }
 
     @Test
@@ -66,13 +57,11 @@ class ReservationDaoTest {
         // given
         ReservationTime reservationTime = new ReservationTime(LocalTime.of(10, 0));
         Long id = reservationTimeDao.create(reservationTime);
-        Reservation reservation = new Reservation("포라", LocalDate.now(), new ReservationTime(id, reservationTime.getStartAt()));
-        reservationDao.create(reservation);
-        int beforeSize = reservationDao.findAll().size();
+        int beforeSize = reservationTimeDao.findAll().size();
 
         // when
-        reservationDao.delete(id);
-        int afterSize = reservationDao.findAll().size();
+        reservationTimeDao.delete(id);
+        int afterSize = reservationTimeDao.findAll().size();
 
         // then
         assertThat(beforeSize).isEqualTo(1);

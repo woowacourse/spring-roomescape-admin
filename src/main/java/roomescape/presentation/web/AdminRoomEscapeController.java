529b62a34714921199831dd29ad84d3cbbbf3308
Dompoo <dlckdrms0517@gmail.com>
2025-04-24 11:28:50 +0900

    feat: admin/time 매핑 추가

--- src/main/java/roomescape/presentation/web/AdminRoomEscapeController.java
+++ src/main/java/roomescape/presentation/web/AdminRoomEscapeController.java
@@ -15,4 +15,9 @@ public class AdminRoomEscapeController {
     public String reservation() {
         return "/admin/reservation";
     }
+
+    @GetMapping("/admin/time")
+    public String time() {
+        return "/admin/time";
+    }
 }

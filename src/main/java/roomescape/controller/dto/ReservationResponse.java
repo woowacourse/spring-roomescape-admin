bc1e42b37441b366ee220f24f005bb5f0b309a15
yesjuhee <juhee0924@g.skku.edu>
2025-04-24 17:34:52 +0900

    fix: 응답 JSON 형식 오류 수정

--- src/main/java/roomescape/controller/dto/ReservationResponse.java
+++ src/main/java/roomescape/controller/dto/ReservationResponse.java
@@ -7,7 +7,7 @@ public record ReservationResponse(
         Long id,
         String name,
         LocalDate date,
-        ReservationTimeResponse reservationTimeResponse
+        ReservationTimeResponse time
 ) {
 
     public ReservationResponse(final Reservation reservation) {

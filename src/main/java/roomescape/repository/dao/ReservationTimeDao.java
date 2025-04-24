65f9d7bafae2faedd1e8999b6875e4a472741087
sung-june27 <kmymy27@gmail.com>
2025-04-24 15:11:43 +0900

    refactor(ReservationTimeDao): 길이로 인한 줄바꿈 시행

--- src/main/java/roomescape/repository/dao/ReservationTimeDao.java
+++ src/main/java/roomescape/repository/dao/ReservationTimeDao.java
@@ -20,10 +20,12 @@ public class ReservationTimeDao implements ReservationTimeRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    private final RowMapper<ReservationTime> reservationTimeRowMapper = (resultSet, rowNum) -> new ReservationTime(
-            resultSet.getLong("id"),
-            LocalTime.parse(resultSet.getString("start_at"))
-    );
+    private final RowMapper<ReservationTime> reservationTimeRowMapper = (resultSet, rowNum) -> {
+        return new ReservationTime(
+                resultSet.getLong("id"),
+                LocalTime.parse(resultSet.getString("start_at"))
+        );
+    };
 
     public ReservationTime save(ReservationTime reservationTime) {
         KeyHolder keyHolder = new GeneratedKeyHolder();

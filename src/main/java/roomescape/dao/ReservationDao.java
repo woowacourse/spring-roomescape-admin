d4e0c1e9e3c508f8c148348dc8485d0a187a6f89
taek2222 <oot309@daum.net>
2025-04-24 11:23:27 +0900

    refactor: 요구 사항 추가로, 예약 DAO 전체 수정
    - reservation_time FK 키 등록으로 인한 조회 SQL문 변경했습니다.
    
    - 객체 필드의 변경으로 불러오는 과정의 Mapper 형식을 변경했습니다.
    
    - 가독성 향상을 위해 save 비즈니스 로직을 분리했습니다.

--- src/main/java/roomescape/dao/ReservationDao.java
+++ src/main/java/roomescape/dao/ReservationDao.java
@@ -1,25 +1,40 @@
 package roomescape.dao;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Map;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 import roomescape.domain.Reservation;
+import roomescape.domain.ReservationTime;
 import roomescape.domain.Reservations;
 
 @Component
 public class ReservationDao {
 
-    private static final String FIND_ALL_SQL = "select * from reservation";
+    private static final String FIND_ALL_SQL = """
+            select
+                r.id as reservation_id,
+                r.name,
+                r.date,
+                t.id as time_id,
+                t.start_at as time_value
+            from reservation as r
+            inner join reservation_time as t
+            on r.time_id = t.id
+            """;
     private static final String DELETE_BY_ID_SQL = "delete from reservation where id = ?";
     private static final RowMapper<Reservation> RESERVATION_ROW_MAPPER = (resultSet, row) ->
             new Reservation(
                     resultSet.getLong("id"),
                     resultSet.getString("name"),
-                    resultSet.getObject("datetime", LocalDateTime.class)
+                    resultSet.getObject("date", LocalDate.class),
+                    new ReservationTime(
+                            resultSet.getLong("id"),
+                            resultSet.getObject("start_at", LocalTime.class)
+                    )
             );
 
     private final JdbcTemplate jdbcTemplate;
@@ -36,21 +51,29 @@ public class ReservationDao {
     }
 
     public Reservation save(Reservation reservation) {
-        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName("reservation")
-                .usingGeneratedKeyColumns("id");
-
-        Map<String, Object> params = new HashMap<>(2);
-        params.put("name", reservation.getName());
-        params.put("datetime", reservation.getDateTime());
+        SimpleJdbcInsert jdbcInsert = createJdbcInsert();
+        Map<String, Object> params = convertToParams(reservation);
 
         Long id = jdbcInsert.executeAndReturnKey(params).longValue();
-
         return new Reservation(id, reservation);
     }
 
     public boolean deleteById(Long id) {
         int updatedRows = jdbcTemplate.update(DELETE_BY_ID_SQL, id);
         return updatedRows > 0;
+    }
+
+    private SimpleJdbcInsert createJdbcInsert() {
+        return new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("reservation")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    private Map<String, Object> convertToParams(final Reservation reservation) {
+        return Map.of(
+                "name", reservation.getName(),
+                "date", reservation.getDate(),
+                "time_id", reservation.getTime().getId()
+        );
     }
 }

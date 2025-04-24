db2861365e76da361b6fb3117af6772397f75d6c
minSsan <msp1125@hanyang.ac.kr>
2025-04-24 18:08:39 +0900

    test(ReservationTimeEntity): 운영 시간 검증 테스트 추가

--- src/test/java/roomescape/entity/ReservationTimeEntityTest.java
+++ src/test/java/roomescape/entity/ReservationTimeEntityTest.java
@@ -2,8 +2,12 @@ package roomescape.entity;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalTime;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,5 +42,28 @@ class ReservationTimeEntityTest {
 
         // then
         assertThat(isDuplicated).isFalse();
+    }
+
+    @DisplayName("예약 시간이 운영 시간에 포함되는지 판단할 수 있다.")
+    @ParameterizedTest
+    @MethodSource
+    void checkIsAvailable(LocalTime startAt, final boolean expected) {
+        // given
+        ReservationTimeEntity timeEntity = new ReservationTimeEntity(null, startAt);
+
+        // when
+        final boolean actual = timeEntity.isAvailable();
+
+        // then
+        assertThat(actual).isSameAs(expected);
+    }
+
+    private static Stream<Arguments> checkIsAvailable() {
+        return Stream.of(
+                Arguments.of(LocalTime.of(9, 59), false),
+                Arguments.of(LocalTime.of(10, 0), true),
+                Arguments.of(LocalTime.of(22, 1), false),
+                Arguments.of(LocalTime.of(22, 0), true)
+        );
     }
 }

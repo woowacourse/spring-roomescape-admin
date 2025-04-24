d3f6a956eba464efb41ef41a4da022ac859f96e5
ryan-dia <cjfdnjs158@naver.com>
2025-04-24 11:43:52 +0900

    test: 테스트를 위한 stub 레포지토리 추가

--- src/test/java/roomescape/StubReservationRepository.java
+++ src/test/java/roomescape/StubReservationRepository.java
@@ -2,44 +2,60 @@ package roomescape;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 import roomescape.repository.ReservationRepository;
 
 public class StubReservationRepository implements ReservationRepository {
 
-    private final boolean existsReservation;
-    private final Reservation createResult;
-    private final List<Reservation> reservations;
+    private final List<Reservation> data = new ArrayList<>();
+    private final AtomicLong atomicLong = new AtomicLong();
 
-    public StubReservationRepository(boolean existsReservation, Reservation createResult,
-                                     List<Reservation> reservations) {
-        this.existsReservation = existsReservation;
-        this.createResult = createResult;
-        this.reservations = reservations;
+    public StubReservationRepository(final Reservation... inputReservations) {
+        data.addAll(List.of(inputReservations));
+        long maxId = data.stream()
+                .mapToLong(Reservation::getId)
+                .max()
+                .orElse(0L);
+        atomicLong.set(maxId);
     }
 
     @Override
     public List<Reservation> findAll() {
-        return reservations;
+        return List.copyOf(data);
     }
 
     @Override
     public boolean existsByDateAndTime(final LocalDate date, final LocalTime time) {
-        return existsReservation;
+        return data.stream()
+                .anyMatch(reservation -> reservation.getDate().equals(date) && reservation.getTime().getStartAt()
+                        .equals(time));
     }
 
     @Override
     public Reservation save(final String name, final LocalDate date, final Long timeId, final LocalTime time) {
-        return createResult;
+        long newId = atomicLong.incrementAndGet();
+        Reservation reservation = new Reservation(
+                newId,
+                name,
+                date,
+                new ReservationTime(timeId, time)
+        );
+        data.add(reservation);
+        return reservation;
     }
 
     @Override
     public void remove(final Long id) {
+        data.removeIf(r -> r.getId().equals(id));
     }
 
     @Override
     public Optional<Reservation> findById(final Long id) {
-        return Optional.empty();
+        return data.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst();
     }
 }

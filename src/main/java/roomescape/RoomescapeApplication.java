3b295efbc483fb04c46bd05522ae5e7aa3aa303d
sung-june27 <kmymy27@gmail.com>
2025-04-24 14:36:42 +0900

    feat: 콘솔 관련 작업 내용 삭제

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -4,19 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoomescapeApplication /*implements CommandLineRunner*/ {
+public class RoomescapeApplication {
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeApplication.class, args);
     }
-
-//    @Override
-//    public void run(String... args) throws Exception {
-//        Reservations reservations = new Reservations();
-//        ReservationTimes reservationTimes = new ReservationTimes();
-//        ConsoleController controller = new ConsoleController(
-//                new ReservationService(reservations, reservationTimes),
-//                new ReservationTimeService(reservationTimes)
-//        );
-//        controller.run();
-//    }
 }

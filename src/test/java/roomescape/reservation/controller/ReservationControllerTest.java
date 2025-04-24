2d7239100a960d6ec5558af737c4dafc34166978
BWY <quantumjump10@gmail.com>
2025-04-24 22:23:49 +0900

    refactor: ReservationControllerTest 테스트 구조 수정

--- src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
@@ -17,43 +17,33 @@ import static org.hamcrest.Matchers.is;
 public class ReservationControllerTest {
 
     @Test
-    @DisplayName("예약 목록을 조회한다")
-    void readReservations() {
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(0));
-    }
-
-    @Test
     @DisplayName("예약을 추가하면 응답에 ID가 포함되고, 전체 조회 시 포함된다.")
     void createReservation() {
-        createReservationTime();
-
-        Map<String, Object> reservation = new HashMap<>();
-        reservation.put("name", "브라운");
-        reservation.put("date", "2023-08-05");
-        reservation.put("timeId", 1);
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(reservation)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(200);
+        setUpReservation();
 
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
+
+    }
+
+    @Test
+    @DisplayName("예약 ID를 통해 해당 예약을 조회한다.")
+    void getReservationById() {
+        setUpReservation();
+
+        RestAssured.given().log().all()
+                .when().get("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
     }
 
     @Test
     @DisplayName("예약 삭제 후, 조회 결과에 나타나지 않아야 한다.")
-    void deleteReservationBy() {
-        createReservation();
+    void deleteReservationById() {
+        setUpReservation();
 
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
@@ -67,6 +57,11 @@ public class ReservationControllerTest {
                 .body("size()", is(0));
     }
 
+    private void setUpReservation() {
+        createReservationTime();
+        createNewReservation();
+    }
+
     private void createReservationTime() {
         Map<String, Object> reservationTime = new HashMap<>();
         reservationTime.put("startAt", "10:00");
@@ -75,6 +70,20 @@ public class ReservationControllerTest {
                 .contentType(ContentType.JSON)
                 .body(reservationTime)
                 .when().post("/times")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    private void createNewReservation() {
+        Map<String, Object> reservation = new HashMap<>();
+        reservation.put("name", "브라운");
+        reservation.put("date", "2023-08-05");
+        reservation.put("timeId", 1);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservation)
+                .when().post("/reservations")
                 .then().log().all()
                 .statusCode(200);
     }

2bd59ca149f5053fee8f7b67310ebec9bfac4462
ryan-dia <cjfdnjs158@naver.com>
2025-04-24 13:29:14 +0900

    feat: 응답 상태 어노테이션 추가

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,12 +3,14 @@ package roomescape.controller;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.controller.dto.ReservationRequest;
 import roomescape.service.ReservationService;
@@ -27,11 +29,13 @@ public class ReservationController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ReservationResponse createReservation(@Valid @RequestBody ReservationRequest request) {
         return reservationService.saveReservation(request);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteReservation(@PathVariable Long id) {
         reservationService.deleteReservation(id);
     }
--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -2,12 +2,14 @@ package roomescape.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.controller.dto.CreateTimeRequest;
 import roomescape.service.ReservationTimeService;
@@ -21,6 +23,7 @@ public class ReservationTimeController {
     private final ReservationTimeService reservationTimeService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ReservationTimeResponse saveTime(@RequestBody CreateTimeRequest createTimeRequest) {
         return reservationTimeService.saveTime(createTimeRequest.startAt());
     }
@@ -31,6 +34,7 @@ public class ReservationTimeController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         reservationTimeService.delete(id);
     }

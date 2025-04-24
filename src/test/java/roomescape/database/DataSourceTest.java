98b5cb26f50892d13164ff25dfc9d8b18bc15671
changuii <rhljh7410@gmail.com>
2025-04-24 13:40:32 +0900

    test: 읽기 쉽도록 주석 추가

--- src/test/java/roomescape/database/DataSourceTest.java
+++ src/test/java/roomescape/database/DataSourceTest.java
@@ -51,24 +51,27 @@ public class DataSourceTest {
     @DisplayName("데이터베이스에 예약 하나 추가 후 예약 조회 API를 통해 조회한 예약 수와 데이터베이스 쿼리를 통해 조회한 예약 수가 같은지 비교")
     @Test
     void 오단계() {
+        // given
         jdbcTemplate.update("INSERT INTO RESERVATION_TIME (start_at) VALUES (?)", "10:00");
         jdbcTemplate.update("INSERT INTO RESERVATION (name, date, time_id) VALUES (?, ?, ?)", "브라운", "2023-08-05",
                 "1");
+        final Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
 
+        // when
         final List<ReservationResponse> reservations = RestAssured.given().port(port).log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200).extract()
                 .jsonPath().getList(".", ReservationResponse.class);
 
-        final Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
-
+        // then
         assertThat(reservations.size()).isEqualTo(count);
     }
 
     @DisplayName("reservation 삽입, 삭제 검증")
     @Test
     void 육단계() {
+        // given
         Map<String, String> RESERVATION_BODY = new HashMap<>();
         RESERVATION_BODY.put("name", "브라운");
         RESERVATION_BODY.put("date", "2023-08-05");
@@ -77,6 +80,7 @@ public class DataSourceTest {
         Map<String, String> TIME_BODY = new HashMap<>();
         TIME_BODY.put("startAt", "10:00");
 
+        // when
         RestAssured.given().port(port).log().all()
                 .contentType(ContentType.JSON)
                 .body(TIME_BODY)
@@ -91,9 +95,11 @@ public class DataSourceTest {
                 .then().log().all()
                 .statusCode(200);
 
+        // then
         Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
         assertThat(count).isEqualTo(1);
 
+        // given & when & then
         RestAssured.given().port(port).log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
@@ -106,6 +112,7 @@ public class DataSourceTest {
     @DisplayName("time과 reservation 연결 테스트")
     @Test
     void 팔단계() {
+        // given
         Map<String, Object> reservation = new HashMap<>();
         reservation.put("name", "브라운");
         reservation.put("date", "2023-08-05");
@@ -129,6 +136,7 @@ public class DataSourceTest {
                 .statusCode(200);
 
 
+        // when & then
         RestAssured.given().port(port).log().all()
                 .when().get("/reservations")
                 .then().log().all()

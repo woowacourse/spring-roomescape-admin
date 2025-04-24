6b867cd5dd4dd88b9d1e48e3e00e433f45009f86
DEV_WOONG <ywcsuwon@gmail.com>
2025-04-24 16:43:01 +0900

    docs: schema.sql, data.sql 문법 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,20 @@
-CREATE TABLE reservation_time
-(
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL,
+-- 테이블이 이미 존재할 경우를 대비해 삭제
+DROP TABLE IF EXISTS reservation;
+DROP TABLE IF EXISTS reservation_time;
+
+-- 예약 시간 테이블
+CREATE TABLE reservation_time (
+    id        BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at  VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation
-(
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    date VARCHAR(255) NOT NULL,
-    time_id BIGINT,                           -- 컬럼 수정
+-- 예약 테이블
+CREATE TABLE reservation (
+    id      BIGINT       NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(255) NOT NULL,
+    date    VARCHAR(255) NOT NULL,
+    time_id BIGINT,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id) -- 외래키 추가
+    FOREIGN KEY (time_id) REFERENCES reservation_time(id)
 );

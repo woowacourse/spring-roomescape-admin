c19b449ec3b1c0493b958084330357621c851e4a
sung-june27 <kmymy27@gmail.com>
2025-04-24 15:00:15 +0900

    refactor: Controller 예외 처리 시, ExceptionHandler 사용

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,9 +3,11 @@ package roomescape.controller;
 import java.net.URI;
 import java.util.List;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +26,12 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<String> notFoundInDB() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("DB에서 데이터를 찾을 수 없습니다.");
+    }
+
     @GetMapping
     public ResponseEntity<List<ReservationResponse>> getReservations() {
         List<ReservationResponse> responses = reservationService.findAll();
@@ -33,13 +41,9 @@ public class ReservationController {
 
     @GetMapping("{id}")
     public ResponseEntity<ReservationResponse> getReservation(@PathVariable long id) {
-        try {
-            ReservationResponse response = reservationService.findById(id);
+        ReservationResponse response = reservationService.findById(id);
 
-            return ResponseEntity.ok(response);
-        } catch (EmptyResultDataAccessException e) {
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping

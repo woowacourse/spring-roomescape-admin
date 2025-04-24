2c6ac5f3ed2c47330a98f6e16e01b69bb0b1bb04
threepebbles <threepebbles@naver.com>
2025-04-24 10:41:50 +0900

    test: 컨트롤러에 JdbcTemplate 타입의 필드가 존재하는지 검증하는 테스트 추가

--- src/test/java/roomescape/admin/controller/ReservationTimeRestControllerTest.java
+++ src/test/java/roomescape/admin/controller/ReservationTimeRestControllerTest.java
@@ -1,18 +1,25 @@
 package roomescape.admin.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ReservationTimeRestControllerTest {
+
+    @Autowired
+    private ReservationTimeRestController reservationTimeRestController;
 
     @Test
     void 예약_가능한_시간을_목록에_추가한다() {
@@ -36,5 +43,19 @@ class ReservationTimeRestControllerTest {
                 .when().delete("/times/1")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @Test
+    void 컨트롤러는_JdbcTemplate_타입의_필드를_갖고_있지_않다() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationTimeRestController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 }
--- src/test/java/roomescape/user/controller/ReservationRestControllerTest.java
+++ src/test/java/roomescape/user/controller/ReservationRestControllerTest.java
@@ -1,23 +1,30 @@
 package roomescape.user.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ReservationRestControllerTest {
+
+    @Autowired
+    private ReservationRestController reservationRestController;
 
     @BeforeEach
     void setUp() {
@@ -130,5 +137,19 @@ class ReservationRestControllerTest {
                 .when().delete("/reservations/1")
                 .then().log().all()
                 .statusCode(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    void 컨트롤러는_JdbcTemplate_타입의_필드를_갖고_있지_않다() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationRestController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 }

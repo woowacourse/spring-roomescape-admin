513ecbf4fed3014566bf1d1818123ad63876fab6
(minjae8563) <(minjae8563@naver.com)>
2025-04-24 15:58:29 +0900

    refactor: ReservationService 생성자에 인자 추가

--- src/main/java/roomescape/AppConfig.java
+++ src/main/java/roomescape/AppConfig.java
@@ -21,8 +21,8 @@ public class AppConfig {
     }
 
     @Bean
-    public ReservationService reservationService(ReservationRepository reservationRepository) {
-        return new ReservationServiceImpl(reservationRepository);
+    public ReservationService reservationService(ReservationRepository reservationRepository, ReservationTimeRepository reservationTimeRepository) {
+        return new ReservationServiceImpl(reservationRepository, reservationTimeRepository);
     }
 
     @Bean

eb2f25326170cb37f1cb7149b25e62a45117f43f
Yeongjae Kim <sees77770@gmail.com>
2025-04-24 18:13:11 +0900

    refactor: ReserverName 테스트 분리

--- src/test/java/roomescape/reservation/domain/ReservationTest.java
+++ src/test/java/roomescape/reservation/domain/ReservationTest.java
@@ -7,30 +7,21 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import roomescape.reservation.domain.exception.PastReservationException;
-import roomescape.reservation.domain.exception.ReserverNameEmptyException;
 import roomescape.time.domain.ReservationTime;
 
 public class ReservationTest {
 
     @ParameterizedTest
-    @NullAndEmptySource
-    @ValueSource(strings = {" "})
-    void 예약자_이름은_비어있거나_null일_수_없다(String input) {
-        assertThatThrownBy(() -> new ReserverName(input))
-                .isInstanceOf(ReserverNameEmptyException.class);
-    }
-
-    @ParameterizedTest
     @ValueSource(ints = {0, 1})
-    void 현재나_현재_이전_시간에는_예약할_수_없다(int minuteOffset) {
+    void 현재나_이전_시간에는_예약할_수_없다(int minuteOffset) {
         LocalDate today = LocalDate.now();
         LocalTime time = LocalTime.now().minusMinutes(minuteOffset);
         ReservationTime reservationTime = new ReservationTime(time);
 
-        assertThatThrownBy(() -> new Reservation("홍길동", today, reservationTime))
+        assertThatThrownBy(() -> new Reservation(
+                "홍길동", today, reservationTime))
                 .isInstanceOf(PastReservationException.class);
     }
 
@@ -39,7 +30,8 @@ public class ReservationTest {
         LocalDate today = LocalDate.now();
         ReservationTime futureTime = new ReservationTime(LocalTime.now().plusMinutes(10));
 
-        assertThatCode(() -> new Reservation("홍길동", today, futureTime))
+        assertThatCode(() -> new Reservation(
+                "홍길동", today, futureTime))
                 .doesNotThrowAnyException();
     }
 

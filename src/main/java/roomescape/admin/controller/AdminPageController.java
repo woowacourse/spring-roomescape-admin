589ee0d9375ec76dbbe81b72b2abbe34949c1263
BWY <quantumjump10@gmail.com>
2025-04-24 13:46:23 +0900

    refactor: 예약 페이지 파일 수정

--- src/main/java/roomescape/admin/controller/AdminPageController.java
+++ src/main/java/roomescape/admin/controller/AdminPageController.java
@@ -15,7 +15,7 @@ public class AdminPageController {
 
     @GetMapping("/reservation")
     public String getReservationPage() {
-        return "/admin/reservation-legacy";
+        return "/admin/reservation";
     }
 
     @GetMapping("/time")

da27a636e7a77541c74166889128b5ec236d09bb
(minjae8563) <(minjae8563@naver.com)>
2025-04-24 11:14:07 +0900

    refactor: 필드 LocalTime 을 ReservationTime 수정

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -1,33 +1,29 @@
 package roomescape.reservation;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
+import roomescape.reservationTime.ReservationTime;
 
 public class Reservation {
 
     private Long id;
     private String name;
     private LocalDate date;
-    private LocalTime time;
+    private ReservationTime time;
 
     public Reservation() {
     }
 
-    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+    public Reservation(Long id, String name, LocalDate date, ReservationTime time) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
     }
 
-    public Reservation(String name, LocalDate date, LocalTime time) {
+    public Reservation(String name, LocalDate date, ReservationTime time) {
         this.name = name;
         this.date = date;
         this.time = time;
-    }
-
-    public boolean isSameDateTime(Reservation reservation) {
-        return reservation.date.isEqual(this.date) && reservation.time.equals(this.time);
     }
 
     public static Reservation toEntity(Reservation reservation, Long id) {
@@ -46,7 +42,7 @@ public class Reservation {
         return id;
     }
 
-    public LocalTime getTime() {
+    public ReservationTime getTime() {
         return time;
     }
 }

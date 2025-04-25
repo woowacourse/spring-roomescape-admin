6fdd05c90989e13224aef6eb592aba71b39ab203
bowook <mbosx316@naver.com>
2025-04-25 20:10:49 +0900

    refactor(ReservationService) : 인터페이스를 주입받도록 수정

--- src/main/java/roomescape/reservation/service/ReservationService.java
+++ src/main/java/roomescape/reservation/service/ReservationService.java
@@ -2,30 +2,30 @@ package roomescape.reservation.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
-import roomescape.reservation.dao.ReservationDAO;
+import roomescape.reservation.dao.ReservationDao;
 import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.dto.ReservationRequest;
 import roomescape.reservation.dto.ReservationResponse;
-import roomescape.time.dao.TimeDAO;
+import roomescape.time.dao.ReservationTimeDao;
 import roomescape.time.domain.ReservationTime;
 import roomescape.time.dto.TimeResponse;
 
 @Service
 public class ReservationService {
-    private final ReservationDAO reservationDAO;
-    private final TimeDAO timeDAO;
+    private final ReservationDao reservationDao;
+    private final ReservationTimeDao reservationTimeDao;
 
-    public ReservationService(final ReservationDAO reservationDAO, final TimeDAO timeDAO) {
-        this.reservationDAO = reservationDAO;
-        this.timeDAO = timeDAO;
+    public ReservationService(final ReservationDao reservationDao, final ReservationTimeDao reservationTimeDao) {
+        this.reservationDao = reservationDao;
+        this.reservationTimeDao = reservationTimeDao;
     }
 
     public ReservationResponse createReservation(final ReservationRequest reservationRequest) {
-        ReservationTime reservationTime = timeDAO.findReservationTimeById(reservationRequest.timeId());
+        ReservationTime reservationTime = reservationTimeDao.findReservationTimeById(reservationRequest.timeId());
         Reservation notSavedreservation = new Reservation(
                 null, reservationRequest.name(), reservationRequest.date(), reservationTime
         );
-        Reservation savedReservation = reservationDAO.insertReservation(notSavedreservation);
+        Reservation savedReservation = reservationDao.insertReservation(notSavedreservation);
         return new ReservationResponse(
                 savedReservation.getId(),
                 savedReservation.getName(),
@@ -35,11 +35,11 @@ public class ReservationService {
     }
 
     public void removeReservation(final long id) {
-        reservationDAO.removeReservation(id);
+        reservationDao.removeReservation(id);
     }
 
     public List<ReservationResponse>  findAllReservations() {
-        List<Reservation> reservations = reservationDAO.findAllReservations();
+        List<Reservation> reservations = reservationDao.findAllReservations();
 
         return reservations.stream()
                 .map(reservation -> new ReservationResponse(

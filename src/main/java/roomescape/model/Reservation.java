f36b16a68c775cbd51ae039876bb2d75f460034d
saera <yook.saera@gmail.com>
2025-04-25 17:40:09 +0900

    refactor: 불필요한 메서드 삭제

--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -46,10 +46,6 @@ public final class Reservation {
         }
     }
 
-    public static Reservation toEntity(Long id, Reservation reservation) {
-        return new Reservation(id, reservation.name, reservation.date, reservation.time);
-    }
-
     public Long getId() {
         return id;
     }

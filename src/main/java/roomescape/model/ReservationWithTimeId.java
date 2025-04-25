8d5bbd9dfcc2e29afb44adc1e9b40d9d3cc65143
supernovaMK <minkikim2002@gmail.com>
2025-04-25 14:11:13 +0900

    feat: dto를 담을 Model ReservationWithTimeId 구현

---
+++ src/main/java/roomescape/model/ReservationWithTimeId.java
@@ -0,0 +1,34 @@
+package roomescape.model;
+
+import java.time.LocalDate;
+
+public class ReservationWithTimeId {
+
+    private final Long id;
+    private final String name;
+    private final LocalDate date;
+    private final Long timeId;
+
+    public ReservationWithTimeId(Long id, String name, LocalDate date, Long timeId) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.timeId = timeId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Long getTimeId() {
+        return timeId;
+    }
+}

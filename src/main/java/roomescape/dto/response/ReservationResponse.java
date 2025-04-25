19b2fe9be754fd2034a9999c12e1e341f08651c2
YuyoungRhee <bbclara@naver.com>
2025-04-25 14:38:28 +0900

    fix(response): api명세와 형식이 다른 부분 @JsonFormat으로 수정

--- src/main/java/roomescape/dto/response/ReservationResponse.java
+++ src/main/java/roomescape/dto/response/ReservationResponse.java
@@ -1,11 +1,18 @@
 package roomescape.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 
 public record ReservationResponse(
+
         Long id,
+
         String name,
+
+        @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate date,
+
+        @JsonFormat(pattern = "HH:mm")
         TimeResponse time
 ) {
 }
--- src/main/java/roomescape/dto/response/TimeResponse.java
+++ src/main/java/roomescape/dto/response/TimeResponse.java
@@ -1,9 +1,13 @@
 package roomescape.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 
 public record TimeResponse(
+
         Long id,
+
+        @JsonFormat(pattern = "HH:mm")
         LocalTime startAt
 ) {
 }

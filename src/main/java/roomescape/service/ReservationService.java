a8a28332f5de957e3100b8c4198f52762df19b49
supernovaMK <minkikim2002@gmail.com>
2025-04-25 14:11:28 +0900

    feat: 예약 Service 구현

---
+++ src/main/java/roomescape/service/ReservationService.java
@@ -0,0 +1,42 @@
+package roomescape.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import roomescape.dao.ReservationDao;
+import roomescape.dao.ReservationTimeDao;
+import roomescape.entity.Reservation;
+import roomescape.entity.ReservationTime;
+import roomescape.model.ReservationWithTimeId;
+
+@Service
+public class ReservationService {
+
+    private final ReservationDao reservationDao;
+    private final ReservationTimeDao reservationTimeDao;
+
+    public ReservationService(ReservationDao reservationDao, ReservationTimeDao reservationTimeDao) {
+        this.reservationDao = reservationDao;
+        this.reservationTimeDao = reservationTimeDao;
+    }
+
+    public List<Reservation> selectAllReservation() {
+
+        List<Reservation> reservations = reservationDao.selectAllReservation();
+
+        return reservations;
+    }
+
+    public Reservation addReservation(ReservationWithTimeId reservationWithTimeId) {
+
+        Long reservationId = reservationDao.addReservation(reservationWithTimeId);
+
+        ReservationTime reservationTime = reservationTimeDao.findTimeById(reservationWithTimeId.getTimeId());
+
+        return new Reservation(reservationId, reservationWithTimeId.getName(), reservationWithTimeId.getDate(),
+                reservationTime);
+    }
+
+    public int deleteReservationById(Long id) {
+        return reservationDao.deleteReservationById(id);
+    }
+}

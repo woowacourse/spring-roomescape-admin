26fccd5bd6d2b34bad9bbffa94371d6992ac94e4
민경윤 <ruddbs3308@naver.com>
2025-04-25 19:05:54 +0900

    refactor: 검증 로직 메서드 분리

--- src/main/java/roomescape/business/service/ReservationService.java
+++ src/main/java/roomescape/business/service/ReservationService.java
@@ -38,6 +38,10 @@ public class ReservationService {
     public void remove(final Long id) {
         final int rowNum = reservationDao.remove(id);
 
+        validateIdExists(rowNum);
+    }
+
+    private static void validateIdExists(final int rowNum) {
         if (rowNum == 0) {
             throw new IllegalArgumentException("해당하는 id가 없습니다.");
         }

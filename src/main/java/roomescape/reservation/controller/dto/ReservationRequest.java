7cbda1515bf50b6ac55e24121c8b50057c6f0a5b
junhaa <2171326@hansung.ac.kr>
2025-04-25 15:03:50 +0900

    feat: 요청 값에 대한 검증 기능 구현

--- src/main/java/roomescape/reservation/controller/dto/ReservationRequest.java
+++ src/main/java/roomescape/reservation/controller/dto/ReservationRequest.java
@@ -8,9 +8,33 @@ import roomescape.reservation.domain.Time;
 public record ReservationRequest(String name,
                                  @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul") LocalDate date,
                                  Long timeId) {
+    public ReservationRequest {
+        validateName(name);
+        validateDate(date);
+        validateTimeId(timeId);
+    }
 
     public Reservation newReservation(Time time) {
         return new Reservation(null, name, date, time);
     }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 필수입니다.");
+        }
+    }
+
+    private void validateDate(LocalDate date) {
+        if (date == null) {
+            throw new IllegalArgumentException("예약 날짜는 필수입니다.");
+        }
+    }
+
+    private void validateTimeId(Long timeId) {
+        if (timeId == null) {
+            throw new IllegalArgumentException("예약 시간 ID는 필수입니다.");
+        }
+    }
+
 }
 
--- src/main/java/roomescape/reservation/controller/dto/TimeRequest.java
+++ src/main/java/roomescape/reservation/controller/dto/TimeRequest.java
@@ -4,9 +4,20 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 import roomescape.reservation.domain.Time;
 
-public record TimeRequest (@JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul") LocalTime startAt) {
+public record TimeRequest(@JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul") LocalTime startAt) {
 
-    public Time newTime(){
+    public TimeRequest {
+        validateStartAt(startAt);
+    }
+
+    public Time newTime() {
         return new Time(null, startAt);
     }
+
+    private void validateStartAt(LocalTime startAt) {
+        if (startAt == null) {
+            throw new IllegalArgumentException("시작 시각은 필수입니다.");
+        }
+    }
+
 }

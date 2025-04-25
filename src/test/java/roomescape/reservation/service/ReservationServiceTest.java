79c6379f7a1c59507965d53bdaf082bf21424149
bowook <mbosx316@naver.com>
2025-04-25 20:11:15 +0900

    test(ReservationServiceTest) : 예약 서비스 테스트 추가

---
+++ src/test/java/roomescape/reservation/service/ReservationServiceTest.java
@@ -0,0 +1,90 @@
+package roomescape.reservation.service;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import roomescape.reservation.dao.ReservationDao;
+import roomescape.reservation.dao.fake.FakeReservationJdbcDao;
+import roomescape.reservation.dao.fake.FakeReservationTimeDao;
+import roomescape.reservation.domain.Reservation;
+import roomescape.reservation.dto.ReservationRequest;
+import roomescape.reservation.dto.ReservationResponse;
+import roomescape.time.dao.ReservationTimeDao;
+import roomescape.time.domain.ReservationTime;
+import roomescape.time.dto.TimeRequest;
+
+class ReservationServiceTest {
+
+    private final ReservationService reservationService;
+    private final ReservationDao reservationDao;
+    private final ReservationTimeDao reservationTimeDao;
+
+    public ReservationServiceTest() {
+        this.reservationDao = new FakeReservationJdbcDao();
+        this.reservationTimeDao = new FakeReservationTimeDao();
+        this.reservationService = new ReservationService(reservationDao, reservationTimeDao);
+    }
+
+    @AfterEach
+    void setUp() {
+        reservationDao.removeReservation(1L);
+    }
+
+    @DisplayName("요청받은 Reservation에 대해서 create 테스트")
+    @Test
+    void test1() {
+        //given
+        TimeRequest timeRequest = new TimeRequest(LocalTime.of(17,48));
+        reservationTimeDao.insertTime(timeRequest);
+
+        ReservationRequest reservationRequest = new ReservationRequest(
+                "피케이",
+                LocalDate.of(2025,4,25),
+                1L
+        );
+
+        //when
+        ReservationResponse insertedReservation = reservationService.createReservation(reservationRequest);
+
+        //then
+        Assertions.assertThat(insertedReservation.id()).isNotNull();
+    }
+
+    @DisplayName("id를 통한 Reservation 삭제 테스트")
+    @Test
+    void test2() {
+        //given
+        Reservation reservation = reservationDao.insertReservation(
+                new Reservation(
+                        1L,
+                        "피케이",
+                        LocalDate.of(2025,4,25),
+                        new ReservationTime(1L, LocalTime.of(19,35)))
+        );
+        //when
+        reservationService.removeReservation(reservation.getId());
+
+        //then
+        Assertions.assertThat(reservationDao.findAllReservations()).isEmpty();
+    }
+
+    @DisplayName("모든 예약 정보 조회")
+    @Test
+    void test3() {
+        //given
+        reservationDao.insertReservation(
+                new Reservation(
+                        1L,
+                        "피케이",
+                        LocalDate.of(2025,4,25),
+                        new ReservationTime(1L, LocalTime.of(19,35)))
+        );
+
+        //when & then
+        Assertions.assertThat(reservationService.findAllReservations().size()).isOne();
+    }
+
+}

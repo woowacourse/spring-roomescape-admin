30a1b3d5b6bbae53421c2063126cdc66f11b92e0
bowook <mbosx316@naver.com>
2025-04-25 20:08:58 +0900

    refactor(ReservationTimeJdbcDao) : RowMapper를 사용해서 중복된 코드 추출

--- src/main/java/roomescape/time/dao/ReservationTimeJdbcDao.java
+++ src/main/java/roomescape/time/dao/ReservationTimeJdbcDao.java
@@ -5,37 +5,43 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import roomescape.time.domain.ReservationTime;
 import roomescape.time.dto.TimeRequest;
 
 @Repository
-public class TimeDAO {
+public class ReservationTimeJdbcDao implements ReservationTimeDao{
 
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert simpleJdbcInsert;
-    public TimeDAO(final JdbcTemplate jdbcTemplate) {
+
+    public ReservationTimeJdbcDao(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
         this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
                 .withTableName("reservation_time")
                 .usingGeneratedKeyColumns("id");
     }
 
+    private final RowMapper<ReservationTime> reservationTimeRowMapper = (resultSet, rowNum) -> {
+        ReservationTime reservationTime = new ReservationTime(
+                resultSet.getLong("id"),
+                resultSet.getTime("start_at").toLocalTime()
+        );
+        return reservationTime;
+    };
+
+    @Override
     public List<ReservationTime> findAllTimes() {
         String sql = "SELECT * from reservation_time";
-        List<ReservationTime> reservationTimes = jdbcTemplate.query(
+        return jdbcTemplate.query(
                 sql,
-                (resultSet, rowNum) -> {
-                    ReservationTime reservationTime = new ReservationTime(
-                            resultSet.getLong("id"),
-                            resultSet.getTime("start_at").toLocalTime()
-                    );
-                    return reservationTime;
-                });
-        return reservationTimes;
+                reservationTimeRowMapper
+        );
     }
 
+    @Override
     public ReservationTime insertTime(final TimeRequest timeRequest) {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("start_at", Time.valueOf(timeRequest.startAt()));
@@ -44,21 +50,19 @@ public class TimeDAO {
         return new ReservationTime(insertedId.longValue(), timeRequest.startAt());
     }
 
+    @Override
     public void deleteTime(final Long id) {
         String sql = "DELETE from reservation_time where id = ?";
         jdbcTemplate.update(sql, id);
     }
 
+    @Override
     public ReservationTime findReservationTimeById(final Long id) {
         String sql = "SELECT id, start_at from reservation_time where id = ?";
         return jdbcTemplate.queryForObject(
                 sql,
-                (resultset, rowNum) -> {
-                    ReservationTime reservationTime = new ReservationTime(
-                            resultset.getLong("id"),
-                            resultset.getTime("start_at").toLocalTime()
-                    );
-                    return reservationTime;
-                }, id);
+                reservationTimeRowMapper,
+                id
+        );
     }
 }

42cddc0e72c7c14f3cfbfd8a26986564a94ebca8
supernovaMK <minkikim2002@gmail.com>
2025-04-25 14:11:37 +0900

    feat: 예약 Dto 구현

--- src/main/java/roomescape/dao/ReservationDao.java
+++ src/main/java/roomescape/dao/ReservationDao.java
@@ -7,7 +7,9 @@ import java.util.Map;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
-import roomescape.model.Reservation;
+import roomescape.entity.Reservation;
+import roomescape.entity.ReservationTime;
+import roomescape.model.ReservationWithTimeId;
 
 @Repository
 public class ReservationDao {
@@ -15,33 +17,37 @@ public class ReservationDao {
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert simpleJdbcInsert;
 
-    public ReservationDao(JdbcTemplate jdbcTemplate) {
+    public ReservationDao(JdbcTemplate jdbcTemplate, ReservationTimeDao reservationTimeDao) {
         this.jdbcTemplate = jdbcTemplate;
         this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate).withTableName("reservation")
                 .usingGeneratedKeyColumns("id");
     }
 
     public List<Reservation> selectAllReservation() {
-        String sql = "SELECT * from reservation";
+        String sql = "SELECT r.id as reservation_id, r.name, r.date, t.id as time_id, t.start_at as time_value FROM reservation as r inner join reservation_time as t on r.time_id = t.id";
 
-        List<Reservation> reservationList = jdbcTemplate.query(sql, (resultSet, rowNum) -> {
-            Reservation reservation = new Reservation(resultSet.getLong("id"), resultSet.getString("name"),
-                    LocalDate.parse(resultSet.getString("date")), LocalTime.parse(resultSet.getString("time")));
+        List<Reservation> reservations = jdbcTemplate.query(sql, (resultSet, rowNum) -> {
+            Reservation reservation = new Reservation(resultSet.getLong("id"),
+                    resultSet.getString("name"),
+                    LocalDate.parse(resultSet.getString("date")),
+                    new ReservationTime(resultSet.getLong("time_id"),
+                            LocalTime.parse(resultSet.getString("time_value"))));
+
             return reservation;
         });
 
-        return reservationList;
+        return reservations;
     }
 
-    public Reservation addReservation(Reservation reservation) {
-        final Map<String, String> parameters = Map.of(
+    public Long addReservation(ReservationWithTimeId reservation) {
+        final Map<String, Object> parameters = Map.of(
                 "name", reservation.getName(),
-                "date", reservation.getDate().toString(),
-                "time", reservation.getTime().toString());
+                "date", reservation.getDate(),
+                "time_id", reservation.getTimeId());
 
         Long id = simpleJdbcInsert.executeAndReturnKey(parameters).longValue();
 
-        return new Reservation(id, reservation.getName(), reservation.getDate(), reservation.getTime());
+        return id;
     }
 
     public int deleteReservationById(Long id) {

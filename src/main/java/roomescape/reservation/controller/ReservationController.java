263ce8c10a497b8da22c17c3d6cbbe7d5331d523
bowook <mbosx316@naver.com>
2025-04-25 21:19:00 +0900

    refactor(ReservationController) : 예외를 받은 후 404 상태 반환

--- src/main/java/roomescape/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/reservation/controller/ReservationController.java
@@ -2,6 +2,7 @@ package roomescape.reservation.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,10 +31,15 @@ public class ReservationController {
     }
 
     @PostMapping
-    public ReservationResponse createReservation(
+    public ResponseEntity<ReservationResponse> createReservation(
             @RequestBody ReservationRequest reservationRequest
     ) {
-        return reservationService.createReservation(reservationRequest);
+        try {
+            ReservationResponse reservationResponse = reservationService.createReservation(reservationRequest);
+            return ResponseEntity.ok(reservationResponse);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/{id}")

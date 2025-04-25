15358b4e48a167df00fc7354d0a10943711f446f
Han <jumdo12@gmail.com>
2025-04-25 19:57:14 +0900

    refactor: 에러 메시지 수정

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -12,19 +12,19 @@ public record Reservation(Long id, String name, LocalDate date, ReservationTime 
 
     private static void validateName(String name) {
         if (name == null || name.isBlank() || name.isEmpty()) {
-            throw new IllegalArgumentException("Invalid name");
+            throw new IllegalArgumentException("[ERROR] 예약자의 이름은 1글자 이상으로 이루어져야 합니다. ");
         }
     }
 
     private static void validateDate(LocalDate date) {
         if (date == null) {
-            throw new IllegalArgumentException("Invalid date");
+            throw new IllegalArgumentException("[ERROR] 예약 날짜는 반드시 입력해야 합니다. 예시) YYYY-MM-DD");
         }
     }
 
     private static void validateReservationTime(ReservationTime reservationTime) {
         if (reservationTime == null) {
-            throw new IllegalArgumentException("Invalid reservation");
+            throw new IllegalArgumentException("[ERROR] 예약 시간을 반드시 입력해야 합니다.");
         }
     }
 }

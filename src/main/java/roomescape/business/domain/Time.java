950faca12139573a409b4e8b7f6161fce9b7f15f
민경윤 <ruddbs3308@naver.com>
2025-04-25 19:01:50 +0900

    feat: Time의 인자로 null이 들어오면 예외가 발생하는 기능 구현

--- src/main/java/roomescape/business/domain/Time.java
+++ src/main/java/roomescape/business/domain/Time.java
@@ -1,6 +1,7 @@
 package roomescape.business.domain;
 
 import java.time.LocalTime;
+import java.util.Objects;
 
 public class Time {
 
@@ -9,12 +10,23 @@ public class Time {
     private final LocalTime startAt;
 
     public Time(final LocalTime startAt) {
+        validateNonNull(startAt);
         this.startAt = startAt;
     }
 
     public Time(final Long id, final LocalTime startAt) {
+        validateNonNull(id, startAt);
         this.id = id;
         this.startAt = startAt;
+    }
+
+    private void validateNonNull(final LocalTime startAt) {
+        Objects.requireNonNull(startAt);
+    }
+
+    private void validateNonNull(final Long id, final LocalTime startAt) {
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(startAt);
     }
 
     public Long getId() {

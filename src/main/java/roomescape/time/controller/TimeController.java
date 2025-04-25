28a5c1d5952c3a4173f483e53e2b688b684ea979
bowook <mbosx316@naver.com>
2025-04-25 20:09:26 +0900

    refactor(TimeController) : timeDAO 클래스 명 변경에 따른 변경

--- src/main/java/roomescape/time/controller/TimeController.java
+++ src/main/java/roomescape/time/controller/TimeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.time.dao.TimeDAO;
+import roomescape.time.dao.ReservationTimeJdbcDao;
 import roomescape.time.domain.ReservationTime;
 import roomescape.time.dto.TimeRequest;
 import roomescape.time.dto.TimeResponse;
@@ -17,15 +17,15 @@ import roomescape.time.dto.TimeResponse;
 @RequestMapping("/times")
 public class TimeController {
 
-    private final TimeDAO timeDAO;
+    private final ReservationTimeJdbcDao reservationTimeJdbcDao;
 
-    public TimeController(final TimeDAO timeDAO) {
-        this.timeDAO = timeDAO;
+    public TimeController(final ReservationTimeJdbcDao reservationTimeJdbcDao) {
+        this.reservationTimeJdbcDao = reservationTimeJdbcDao;
     }
 
     @GetMapping
     public List<TimeResponse> getTime() {
-        List<ReservationTime> reservationTimes = timeDAO.findAllTimes();
+        List<ReservationTime> reservationTimes = reservationTimeJdbcDao.findAllTimes();
 
         return reservationTimes.stream()
                 .map(reservationTime -> new TimeResponse(reservationTime.getId(), reservationTime.getStartAt()))
@@ -34,7 +34,7 @@ public class TimeController {
 
     @PostMapping
     public TimeResponse createTime(@RequestBody TimeRequest timeRequest) {
-        ReservationTime reservationTime = timeDAO.insertTime(timeRequest);
+        ReservationTime reservationTime = reservationTimeJdbcDao.insertTime(timeRequest);
         return new TimeResponse(
                 reservationTime.getId(),
                 reservationTime.getStartAt()
@@ -43,6 +43,6 @@ public class TimeController {
 
     @DeleteMapping("/{id}")
     public void deleteTime(@PathVariable("id") long id) {
-        timeDAO.deleteTime(id);
+        reservationTimeJdbcDao.deleteTime(id);
     }
 }

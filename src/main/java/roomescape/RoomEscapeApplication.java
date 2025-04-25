e2ecc0de08d0f5ba5282a0d5794f4c893bc2613f
byesol <bsj5967@naver.com>
2025-04-25 16:33:37 +0900

    refactor: 클래스 위치 변경

--- src/main/java/roomescape/RoomEscapeApplication.java
+++ src/main/java/roomescape/RoomEscapeApplication.java
@@ -1,4 +1,4 @@
-package roomescape.usecase.Reservation;
+package roomescape;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

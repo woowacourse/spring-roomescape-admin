c91cdd251acdf71bddcae67e016b73416e14b9a6
민경윤 <ruddbs3308@naver.com>
2025-04-25 19:01:50 +0900

    feat: Time의 인자로 null이 들어오면 예외가 발생하는 기능 구현

---
+++ src/test/java/roomescape/business/domain/TimeTest.java
@@ -0,0 +1,23 @@
+package roomescape.business.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.time.LocalTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TimeTest {
+
+    @DisplayName("생성자1로 null은 들어올 수 없다.")
+    @Test
+    void validateNonNull() {
+        // given & when & then
+        assertAll(
+                () -> assertThatThrownBy(() -> new Time(null))
+                        .isInstanceOf(NullPointerException.class),
+                () -> assertThatThrownBy(() -> new Time(null, LocalTime.MAX))
+                        .isInstanceOf(NullPointerException.class)
+        );
+    }
+}

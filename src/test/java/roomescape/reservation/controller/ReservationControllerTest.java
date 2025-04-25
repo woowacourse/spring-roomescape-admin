72ba89384eafa6c8d2fc5818983c3441a84f6d88
BWY <quantumjump10@gmail.com>
2025-04-25 15:50:19 +0900

    feat: DB 관련 로직이 컨트롤러에서 분리되어 있는지 확인하는 테스트 추가

--- src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
@@ -4,17 +4,24 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ReservationControllerTest {
+
+    @Autowired
+    private ReservationController reservationController;
 
     @Test
     @DisplayName("예약을 추가하면 응답에 ID가 포함되고, 전체 조회 시 포함된다.")
@@ -86,5 +93,20 @@ public class ReservationControllerTest {
                 .when().post("/reservations")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("데이터베이스 관련 로직이 컨트롤러로부터 분리되어 있는지 확인한다.")
+    void isSeperatedDBLogic() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 }

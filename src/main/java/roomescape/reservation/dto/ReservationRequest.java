1bb22685063d1f1c674d171fb4b04d56140a4aac
bowook <mbosx316@naver.com>
2025-04-25 16:43:15 +0900

    refactor(ReservationDto) : 요청 및 반환 수정

--- src/main/java/roomescape/reservation/dto/ReservationRequest.java
+++ src/main/java/roomescape/reservation/dto/ReservationRequest.java
@@ -1,11 +1,10 @@
-package roomescape.reservation.dto.request;
+package roomescape.reservation.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
-import java.time.LocalTime;
 
 public record ReservationRequest(
         String name,
         @JsonFormat(pattern = "yyyy-MM-dd") LocalDate date,
-        @JsonFormat(pattern = "HH:mm") LocalTime time) {
+        Long timeId) {
 }

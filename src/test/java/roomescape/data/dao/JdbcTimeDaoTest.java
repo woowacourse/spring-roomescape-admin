e1063986b0ae038b84dbabe8b14d3d1372412c06
민경윤 <ruddbs3308@naver.com>
2025-04-26 21:34:45 +0900

    test: 방탈출 시간 DB 저장 기능 검증

---
+++ src/test/java/roomescape/data/dao/JdbcTimeDaoTest.java
@@ -0,0 +1,57 @@
+package roomescape.data.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import roomescape.business.domain.Time;
+import roomescape.data.entity.TimeEntity;
+
+@JdbcTest
+class JdbcTimeDaoTest {
+
+    private TimeDao timeDao;
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    JdbcTimeDaoTest(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @BeforeEach
+    void setUp() {
+        timeDao = new JdbcTimeDao(jdbcTemplate);
+        jdbcTemplate.execute("DROP TABLE IF EXISTS reservation_time CASCADE");
+        jdbcTemplate.execute("""
+                CREATE TABLE reservation_time
+                (
+                    id SERIAL,
+                    start_at VARCHAR(255) NOT NULL,
+                    PRIMARY KEY (id)
+                );
+                """);
+
+    }
+
+    @DisplayName("데이터베이스에 방탈출 시간을 저장한다.")
+    @Test
+    void save() {
+        // given & when
+        final Long id = timeDao.save(new Time(LocalTime.of(10, 10)));
+        final TimeEntity actual = jdbcTemplate.queryForObject(
+                "SELECT id, start_at FROM reservation_time WHERE id = ?",
+                TimeEntity.getDefaultRowMapper(), id
+        );
+
+        // then
+        assertThat(actual).isEqualTo(new TimeEntity(1L, "10:10"));
+    }
+}

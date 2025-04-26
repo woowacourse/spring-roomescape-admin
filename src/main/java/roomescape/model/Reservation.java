866a588729eafe9bf89057dd82810dbe0f6b2132
supernovaMK <minkikim2002@gmail.com>
2025-04-26 14:49:06 +0900

    refactor: null 값에 관한 Reservation 예외 처리

--- src/main/java/roomescape/model/Reservation.java
+++ src/main/java/roomescape/model/Reservation.java
@@ -11,10 +11,24 @@ public class Reservation {
     private final ReservationTime time;
 
     public Reservation(Long id, String name, LocalDate date, ReservationTime time) {
+        validate(name, date, time);
+
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    private void validate(String name, LocalDate date, ReservationTime time) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 null이거나 비어 있을 수 없습니다.");
+        }
+        if (date == null) {
+            throw new IllegalArgumentException("예약 날짜는 null일 수 없습니다.");
+        }
+        if (time == null) {
+            throw new IllegalArgumentException("예약 시간은 null일 수 없습니다.");
+        }
     }
 
     public Long getId() {

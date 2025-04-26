244b4bc0e4d35d920f50dfc0083303e23f385153
supernovaMK <minkikim2002@gmail.com>
2025-04-26 14:08:38 +0900

    refactor: 필수적인 값이 없을 경우에 관한 dto 예외처리

--- src/main/java/roomescape/dto/ReservationRequest.java
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -1,13 +1,21 @@
 package roomescape.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import roomescape.entity.ReservationWithTimeId;
 
 public record ReservationRequest(
-                                 String name,
-                                 @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul") LocalDate date,
-                                 Long timeId) {
+        @NotBlank(message = "이름은 필수입니다.")
+        String name,
+
+        @NotNull(message = "예약 날짜는 필수입니다.")
+        @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+        LocalDate date,
+
+        @NotNull(message = "예약 시간 ID는 필수입니다.")
+        Long timeId) {
 
     public ReservationWithTimeId toReservationWithId() {
         return new ReservationWithTimeId(null, name, date, timeId);
--- src/main/java/roomescape/dto/ReservationTimeRequest.java
+++ src/main/java/roomescape/dto/ReservationTimeRequest.java
@@ -1,10 +1,15 @@
 package roomescape.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 import roomescape.entity.ReservationTime;
 
-public record ReservationTimeRequest(@JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul")LocalTime startAt) {
+public record ReservationTimeRequest(@NotNull(message = "예약 시간 입력은 필수입니다.") @JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul")LocalTime startAt) {
+
+
 
     public ReservationTime toEntity() {
 

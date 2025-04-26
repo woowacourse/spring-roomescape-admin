4d20c8bf6f9c114da8d090999c9621e695844242
민경윤 <ruddbs3308@naver.com>
2025-04-27 01:27:11 +0900

    refactor: 코드 블럭 정렬

--- src/main/java/roomescape/data/dao/JdbcReservationDao.java
+++ src/main/java/roomescape/data/dao/JdbcReservationDao.java
@@ -39,16 +39,16 @@ public class JdbcReservationDao implements ReservationDao {
     public List<Reservation> findAll() {
         final String sql =
                 """
-                         SELECT
-                         r.id as reservation_id,
-                         r.name,
-                         r.date,
-                         t.id as time_id,
-                         t.start_at as time_value
-                         FROM reservation as r
-                         inner join reservation_time as t
-                         on r.time_id = t.id
-                        """;
+                SELECT
+                r.id as reservation_id,
+                r.name,
+                r.date,
+                t.id as time_id,
+                t.start_at as time_value
+                FROM reservation as r
+                inner join reservation_time as t
+                on r.time_id = t.id
+                """;
 
         return jdbcTemplate.query(sql, ReservationEntity.getDefaultRowMapper()).stream()
                 .map(ReservationEntity::toDomain)

61848f778169bb9d9017e193707322efb0da7f43
YuyoungRhee <bbclara@naver.com>
2025-04-27 02:41:32 +0900

    test(MissonStepTest): @DirtiesContext 삭제
    @BeforeEach에서의 db 초기화가 이미 이루어지고 있으므로 삭제함

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -20,14 +20,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.DirtiesContext;
 import roomescape.controller.ReservationController;
 import roomescape.controller.dto.response.ReservationResponse;
 import roomescape.repository.ReservationRepository;
 import roomescape.repository.TimeRepository;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
     @LocalServerPort

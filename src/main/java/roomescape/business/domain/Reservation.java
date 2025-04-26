0e22fb08bb88015191621363e05f229198a3b130
민경윤 <ruddbs3308@naver.com>
2025-04-26 23:37:38 +0900

    test: 모든 방탈출 예약 DB 조회 기능 검증

--- src/main/java/roomescape/business/domain/Reservation.java
+++ src/main/java/roomescape/business/domain/Reservation.java
@@ -66,4 +66,19 @@ public class Reservation {
     public Time getTime() {
         return time;
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Reservation that = (Reservation) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name)
+               && Objects.equals(date, that.date) && Objects.equals(time, that.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, date, time);
+    }
 }

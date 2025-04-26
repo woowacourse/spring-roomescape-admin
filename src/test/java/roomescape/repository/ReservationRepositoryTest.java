792ed1eb72d31de7094c7196b6e411f92f553e88
DEV_WOONG <ywcsuwon@gmail.com>
2025-04-27 03:07:26 +0900

    refactor: 미사용 어노테이션 삭제

--- src/test/java/roomescape/repository/ReservationRepositoryTest.java
+++ src/test/java/roomescape/repository/ReservationRepositoryTest.java
@@ -12,12 +12,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.DirtiesContext;
 import roomescape.entity.Reservation;
 import roomescape.entity.ReservationTime;
 
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class ReservationRepositoryTest {
 
     @Autowired

48eace19cc751e83ca08d9a5103fdde1c5e73c4c
eunseongu <rndmstjs75@gmail.com>
2025-04-26 15:23:48 +0900

    feat: 예약 기능에서 시간을 선택하는 기능 구현
    - 기존에 구현한 예약 기능에서 시간을 시간 테이블에 저장된 값만 선택할 수 있도록 수정

--- src/main/java/roomescape/controller/ReservationTimeAPIController.java
+++ src/main/java/roomescape/controller/ReservationTimeAPIController.java
@@ -27,7 +27,7 @@ public class ReservationTimeAPIController {
 
     @PostMapping
     public ResponseEntity<ReservationTime> addTime(@RequestBody ReservationTimeRequest reservationTimeRequest) {
-        String sql = "insert into reservation_time(start_at) values (?)";
+        String sql = "INSERT INTO reservation_time (start_at) VALUES (?)";
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(connection -> {
             PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
@@ -43,7 +43,7 @@ public class ReservationTimeAPIController {
 
     @GetMapping
     public ResponseEntity<List<ReservationTime>> searchTime() {
-        String sql = "select id, start_at from reservation_time";
+        String sql = "SELECT id, start_at FROM reservation_time";
         List<ReservationTime> times = jdbcTemplate.query(sql,
                 (rs, rowNum) -> new ReservationTime(
                         rs.getLong("id"),
@@ -54,7 +54,7 @@ public class ReservationTimeAPIController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTime(@PathVariable Long id) {
-        String sql = "delete from reservation_time where id = ?";
+        String sql = "DELETE FROM reservation_time WHERE id = ?";
         jdbcTemplate.update(sql, id);
         return ResponseEntity.ok().build();
     }

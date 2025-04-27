745b165be293904f4cbcc2624bc3e8ea4ba261fd
jhan0121 <jhan0121@gmail.com>
2025-04-27 16:19:39 +0900

    refactor: RoomescapeAdminController 로 클래스 명 변경

--- src/main/java/roomescape/controller/RoomescapeAdminController.java
+++ src/main/java/roomescape/controller/RoomescapeAdminController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/admin")
 @Controller
-public class RoomescapeController {
+public class RoomescapeAdminController {
 
     @GetMapping
     public String getAdminPage() {

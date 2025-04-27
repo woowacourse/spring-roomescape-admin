fca7bca1e68966ef3b39a520ebaa25c68ad8e8ed
7i.w__k <tizmfns1218@Naver.com>
2025-04-27 15:21:31 +0900

    refactor: RestController 활용

--- src/main/java/roomescape/domain/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/domain/reservation/controller/ReservationController.java
@@ -3,17 +3,17 @@ package roomescape.domain.reservation.controller;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.reservation.dto.ReservationRequest;
 import roomescape.domain.reservation.dto.ReservationResponse;
 import roomescape.domain.reservation.service.ReservationService;
 
-@Controller
+@RestController
 public class ReservationController {
 
     private final ReservationService reservationService;
--- src/main/java/roomescape/domain/reservation/controller/ReservationTimeController.java
+++ src/main/java/roomescape/domain/reservation/controller/ReservationTimeController.java
@@ -3,17 +3,17 @@ package roomescape.domain.reservation.controller;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.reservation.dto.ReservationTimeRequest;
 import roomescape.domain.reservation.dto.ReservationTimeResponse;
 import roomescape.domain.reservation.service.ReservationTimeService;
 
-@Controller
+@RestController
 public class ReservationTimeController {
 
     private final ReservationTimeService reservationTimeService;

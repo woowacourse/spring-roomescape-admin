6122ce1db2c86ecd8aa4e31b6a0e525b30e11650
KimDaehyeon <daehyeon3351@gmail.com>
2025-04-27 15:32:50 +0900

    refactor: 예약 시간 도메인 추가에 따른 dao 변경

--- src/main/java/roomescape/repository/reservation/H2ReservationDao.java
+++ src/main/java/roomescape/repository/reservation/H2ReservationDao.java
@@ -2,7 +2,6 @@ package roomescape.repository.reservation;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.Time;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.dao.support.DataAccessUtils;
@@ -11,17 +10,22 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
-import roomescape.domain.Reservation;
+import roomescape.domain.reservation.Reservation;
+import roomescape.domain.time.ReservationTime;
 
 @Component
 public class H2ReservationDao implements ReservationDao {
 
     private final JdbcTemplate jdbcTemplate;
+
     private final RowMapper<Reservation> reservationRowMapper = ((rs, rowNum) -> new Reservation(
-        rs.getLong("id"),
+        rs.getLong("reservation_id"),
         rs.getString("name"),
         rs.getDate("date").toLocalDate(),
-        rs.getTime("time").toLocalTime()
+        new ReservationTime(
+            rs.getLong("time_id"),
+            rs.getTime("time_value").toLocalTime()
+        )
     ));
 
     public H2ReservationDao(JdbcTemplate jdbcTemplate) {
@@ -30,13 +34,13 @@ public class H2ReservationDao implements ReservationDao {
 
     @Override
     public void save(final Reservation reservation) {
-        String sql = "INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)";
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(connection -> {
             PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
             ps.setString(1, reservation.getName());
             ps.setDate(2, Date.valueOf(reservation.getDate()));
-            ps.setTime(3, Time.valueOf(reservation.getTime()));
+            ps.setLong(3, reservation.getTime().getId());
             return ps;
         }, keyHolder);
 
@@ -45,7 +49,17 @@ public class H2ReservationDao implements ReservationDao {
 
     @Override
     public Optional<Reservation> findById(final long id) {
-        String sql = "SELECT * FROM reservation WHERE id = ?";
+        String sql = """
+            SELECT
+                r.id AS reservation_id,
+                r.name,
+                r.date,
+                t.id AS time_id,
+                t.start_at AS time_value
+            FROM reservation AS r
+            INNER JOIN reservation_time AS t ON r.time_id = t.id
+            WHERE r.id = ?
+            """;
         List<Reservation> results = jdbcTemplate.query(sql, reservationRowMapper, id);
 
         return Optional.ofNullable(DataAccessUtils.singleResult(results));
@@ -53,7 +67,17 @@ public class H2ReservationDao implements ReservationDao {
 
     @Override
     public List<Reservation> findAll() {
-        String sql = "SELECT * FROM reservation";
+        String sql = """
+            SELECT
+                r.id as reservation_id,
+                r.name,
+                r.date,
+                t.id as time_id,
+                t.start_at as time_value
+            FROM reservation as r
+            inner join reservation_time as t
+            on r.time_id = t.id
+            """;
 
         return jdbcTemplate.query(sql, reservationRowMapper);
     }

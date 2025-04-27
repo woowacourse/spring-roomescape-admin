18b839974eb81bfadc13842e4f057c6f6bc42dfa
minSsan <msp1125@hanyang.ac.kr>
2025-04-27 14:49:51 +0900

    refactor: AdminController -> ReservationViewController 클래스 네이밍 변경

--- src/main/java/roomescape/controller/ReservationViewController.java
+++ src/main/java/roomescape/controller/ReservationViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
-public class AdminController {
+public class ReservationViewController {
 
     @GetMapping
     public String showAdminWelcome() {

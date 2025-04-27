0f54ab7e70b5695631a84fa9076dcf28f0f18dc4
LJM\eueo8 <eueo8259@gmail.com>
2025-04-27 21:43:13 +0900

    test : FakeReservationDao 생성 후 ReservationService 테스트들 구현

---
+++ src/test/java/roomescape/dao/FakeReservationDao.java
@@ -0,0 +1,42 @@
+package roomescape.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.jdbc.core.JdbcTemplate;
+import roomescape.model.Reservation;
+import roomescape.model.ReservationTime;
+
+public class FakeReservationDao extends ReservationDao{
+
+    private final Map<Long, Reservation> database = new HashMap<>();
+    private Long nextId = 1L;
+
+    public FakeReservationDao(JdbcTemplate jdbcTemplate) {
+        super(jdbcTemplate);
+    }
+
+    @Override
+    public Long saveReservation(Reservation reservation) {
+        Long id = nextId++;
+        Reservation savedReservation = new Reservation(
+                id,
+                reservation.getName(),
+                reservation.getDate(),
+                new ReservationTime(reservation.getTimeId())
+        );
+        database.put(id, savedReservation);
+        return id;
+    }
+
+    @Override
+    public List<Reservation> findAll() {
+        return new ArrayList<>(database.values());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        database.remove(id);
+    }
+}

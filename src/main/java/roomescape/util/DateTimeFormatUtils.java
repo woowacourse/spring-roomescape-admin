1c170f350dc8e248e172d264558aa0ac9a20e138
rladmstn <oz33177351@gmail.com>
2025-04-27 19:02:44 +0900

    feat: 일시 관련 util 메서드 추가

--- src/main/java/roomescape/util/DateTimeFormatUtils.java
+++ src/main/java/roomescape/util/DateTimeFormatUtils.java
@@ -1,12 +1,22 @@
 package roomescape.util;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public final class DateTimeFormatUtils {
 
-    public static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    public static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
 
     private DateTimeFormatUtils() {
+    }
+
+    public static LocalDate formatDateFrom(String input) {
+        return LocalDate.parse(input, DATE_FORMATTER);
+    }
+
+    public static LocalTime formatTimeFrom(String input) {
+        return LocalTime.parse(input, TIME_FORMATTER);
     }
 }

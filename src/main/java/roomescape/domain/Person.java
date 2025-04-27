d36e3f5c803df60b05da475caf5348dae044dfe3
yush <yush2202@gmail.com>
2025-04-27 20:38:46 +0900

    feat: 사용자 이름이 7자를 초과하면 예외를 발생시킴

--- src/main/java/roomescape/domain/Person.java
+++ src/main/java/roomescape/domain/Person.java
@@ -1,13 +1,28 @@
 package roomescape.domain;
 
 public record Person(String name) {
+
+    private static final int MAX_NAME_LENGTH = 7;
+
     public Person {
-        invalidName(name);
+        validateEmptyName(name);
+        validateNameLength(name);
     }
 
-    private void invalidName(String name) {
+    private void validateEmptyName(String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("비어 있는 이름은 입력 불가합니다.");
         }
+    }
+
+    private void validateNameLength(String name) {
+        if (overMaxLength(name)) {
+            throw new IllegalArgumentException(String.format("이름은 "
+                    + "%d자 이하여야 합니다.", MAX_NAME_LENGTH));
+        }
+    }
+
+    private boolean overMaxLength(String name) {
+        return name.length() > MAX_NAME_LENGTH;
     }
 }

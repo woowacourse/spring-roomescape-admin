d8afb2ac76a4a1651bd222d8357fc24f399722e1
eoehd1ek <eoehd2ek@gmail.com>
2025-04-28 03:09:40 +0900

    feat: db - time 조회, 삽입, 삭제 기능 추가

---
+++ src/main/java/roomescape/dao/TimeDAOImpl.java
@@ -0,0 +1,49 @@
+package roomescape.dao;
+
+import java.sql.PreparedStatement;
+import java.time.LocalTime;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.Time;
+
+@Repository
+public class TimeDAOImpl implements TimeDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public TimeDAOImpl(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Time> findAllTime() {
+        final String sql = "select id, start_at from reservation_time";
+        return jdbcTemplate.query(sql, (resultSet, rowNum) -> {
+            final Long id = resultSet.getLong("id");
+            final LocalTime startAt = LocalTime.parse(resultSet.getString("start_at"));
+            return new Time(id, startAt);
+        });
+    }
+
+    public Long insertTime(final Time time) {
+        final String sql = "insert into reservation_time (start_at) values (?)";
+        final KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            final PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, time.getStartAt().toString());
+            return ps;
+        }, keyHolder);
+        if (keyHolder.getKey() == null) {
+            return -1L;
+        }
+        return keyHolder.getKey()
+                .longValue();
+    }
+
+    public int deleteTimeById(final Long id) {
+        final String sql = "delete from reservation_time where id = ?";
+        return jdbcTemplate.update(sql, id);
+    }
+}
---
+++ src/test/java/roomescape/dao/TimeDAOImplTest.java
@@ -0,0 +1,61 @@
+package roomescape.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalTime;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import roomescape.domain.Time;
+
+@JdbcTest
+class TimeDAOImplTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    @DisplayName("모든 time 을 조회한다")
+    void findAllTime() {
+        // given
+        TimeDAOImpl timeDAOImpl = new TimeDAOImpl(jdbcTemplate);
+
+        // when
+        List<Time> times = timeDAOImpl.findAllTime();
+
+        // then
+        assertThat(times).isEmpty();
+    }
+
+    @Test
+    @DisplayName("reservation 을 추가한다")
+    void insertTime() {
+        // given
+        TimeDAOImpl timeDAOImpl = new TimeDAOImpl(jdbcTemplate);
+        Time time = new Time(LocalTime.of(12, 0));
+
+        // when
+        Long id = timeDAOImpl.insertTime(time);
+
+        // then
+        assertThat(id).isNotEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("reservation 을 삭제한다")
+    void deleteTimeById() {
+        // given
+        TimeDAOImpl timeDAOImpl = new TimeDAOImpl(jdbcTemplate);
+        Time time = new Time(LocalTime.of(12, 0));
+        Long id = timeDAOImpl.insertTime(time);
+
+        // when
+        int deletedCount = timeDAOImpl.deleteTimeById(id);
+
+        // then
+        assertThat(deletedCount).isEqualTo(1);
+    }
+}

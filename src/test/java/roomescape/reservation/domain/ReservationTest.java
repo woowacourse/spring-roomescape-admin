2de3735794ce054ce9a35de406309c6091b5eea2
threepebbles <threepebbles@naver.com>
2025-04-27 19:44:27 +0900

    test: 예약 정보 객체 생성 시 이름이 빈 문자열인 경우 예외 발생 테스트 추가

--- src/test/java/roomescape/reservation/domain/ReservationTest.java
+++ src/test/java/roomescape/reservation/domain/ReservationTest.java
@@ -22,6 +22,19 @@ class ReservationTest {
     }
 
     @Test
+    void 이름이_빈_문자열이면_예외가_발생한다() {
+        // given
+        final String name = "";
+        final LocalDate date = LocalDate.of(2025, 4, 24);
+        final ReservationTime reservationTime = new ReservationTime(LocalTime.of(10, 0));
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> {
+            new Reservation(name, date, reservationTime);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     void 날짜가_null이면_예외가_발생한다() {
         // given
         final String name = "피케이";

0721ebfbcf0f85578dded44c80114e599c9f252c
KimDaehyeon <daehyeon3351@gmail.com>
2025-04-27 15:32:12 +0900

    chore: 도메인, dao 적절한 패키지 분리

--- src/main/java/roomescape/repository/reservation/ReservationDao.java
+++ src/main/java/roomescape/repository/reservation/ReservationDao.java
@@ -1,8 +1,8 @@
-package roomescape.repository;
+package roomescape.repository.reservation;
 
 import java.util.List;
 import java.util.Optional;
-import roomescape.domain.Reservation;
+import roomescape.domain.reservation.Reservation;
 
 public interface ReservationDao {
 
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -4,15 +4,27 @@ import static org.hamcrest.core.Is.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.domain.reservation.Reservation;
+import roomescape.domain.time.ReservationTime;
+import roomescape.repository.reservation.ReservationDao;
+import roomescape.repository.time.ReservationTimeDao;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
+
+    @Autowired
+    private ReservationTimeDao reservationTimeDao;
+    @Autowired
+    private ReservationDao reservationDao;
 
     @Test
     void 홈_화면을_응답할_수_있다() {
@@ -41,10 +53,12 @@ public class MissionStepTest {
 
     @Test
     void 예약을_생성할_수_있다() {
-        Map<String, String> params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
-        params.put("time", "15:40");
+        params.put("timeId", 1);
+
+        reservationTimeDao.save(new ReservationTime(LocalTime.now().withNano(0)));
 
         RestAssured.given().log().all()
             .contentType(ContentType.JSON)
@@ -61,23 +75,14 @@ public class MissionStepTest {
             .body("[0].id", is(1))
             .body("[0].name", is("브라운"))
             .body("[0].date", is("2023-08-05"))
-            .body("[0].time", is("15:40"));
+            .body("[0].time.id", is(1));
     }
 
     @Test
     void 예약을_삭제할_수_있다() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-05");
-        params.put("time", "15:40");
-
-        RestAssured.given().log().all()
-            .contentType(ContentType.JSON)
-            .body(params)
-            .when().post("/reservations")
-            .then().log().all()
-            .statusCode(200)
-            .body("id", is(1));
+        ReservationTime reservationTime = new ReservationTime(LocalTime.now().withNano(0));
+        reservationTimeDao.save(reservationTime);
+        reservationDao.save(new Reservation("r1", LocalDate.now(), reservationTime));
 
         RestAssured.given().log().all()
             .when().delete("/reservations/1")
@@ -104,5 +109,53 @@ public class MissionStepTest {
             .then().log().all()
             .statusCode(404);
     }
+
+    @Test
+    void 칠단계() {
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10:00");
+
+        RestAssured.given().log().all()
+            .contentType(ContentType.JSON)
+            .body(params)
+            .when().post("/times")
+            .then().log().all()
+            .statusCode(200);
+
+        RestAssured.given().log().all()
+            .when().get("/times")
+            .then().log().all()
+            .statusCode(200)
+            .body("size()", is(1));
+
+        RestAssured.given().log().all()
+            .when().delete("/times/1")
+            .then().log().all()
+            .statusCode(200);
+    }
+
+    @Test
+    void 팔단계() {
+        Map<String, Object> reservation = new HashMap<>();
+        reservation.put("name", "브라운");
+        reservation.put("date", "2023-08-05");
+        reservation.put("timeId", 1);
+
+        reservationTimeDao.save(new ReservationTime(LocalTime.now().withNano(0)));
+
+        RestAssured.given().log().all()
+            .contentType(ContentType.JSON)
+            .body(reservation)
+            .when().post("/reservations")
+            .then().log().all()
+            .statusCode(200);
+
+        RestAssured.given().log().all()
+            .when().get("/reservations")
+            .then().log().all()
+            .statusCode(200)
+            .body("size()", is(1));
+    }
+
 }
 

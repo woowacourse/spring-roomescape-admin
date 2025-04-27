9685e353c0e7a4f841e4840f9a5dc600e7a0adda
ykmxxi <ykmxxi97@gmail.com>
2025-04-27 16:42:44 +0900

    refactor: 코드 가독성을 위한 메서드 분리

--- src/main/java/roomescape/repository/ReservationDao.java
+++ src/main/java/roomescape/repository/ReservationDao.java
@@ -8,6 +8,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
@@ -36,14 +38,19 @@ public class ReservationDao {
                 inner join reservation_time as t
                 on r.time_id = t.id
                 """;
-        return jdbcTemplate.query(findAllSql, (resultSet, rowNum) -> new Reservation(
+
+        return jdbcTemplate.query(findAllSql, getReservationRowMapper());
+    }
+
+    private RowMapper<Reservation> getReservationRowMapper() {
+        return (resultSet, rowNum) -> new Reservation(
                 resultSet.getLong("reservation_id"),
                 resultSet.getString("name"),
                 LocalDate.parse(resultSet.getString("date")),
                 new ReservationTime(
                         resultSet.getLong("time_id"),
-                        LocalTime.parse(resultSet.getString("time_value")))
-        ));
+                        LocalTime.parse(resultSet.getString("time_value"))
+                ));
     }
 
     public Reservation insert(final Reservation reservation) {
@@ -52,16 +59,23 @@ public class ReservationDao {
                 INSERT INTO reservation (name, date, time_id)
                 VALUES (?, ?, ?)
                 """;
-        jdbcTemplate.update(connection -> {
+        jdbcTemplate.update(getPreparedStatementCreator(reservation, insertSql), generatedKeyHolder);
+
+        return new Reservation(generatedKeyHolder.getKey().longValue(), reservation.getName(),
+                reservation.getDate(), reservation.getTime());
+    }
+
+    private PreparedStatementCreator getPreparedStatementCreator(
+            final Reservation reservation, final String insertSql
+    ) {
+        return connection -> {
             PreparedStatement preparedStatement = connection.prepareStatement(insertSql,
                     Statement.RETURN_GENERATED_KEYS);
             preparedStatement.setString(1, reservation.getName());
             preparedStatement.setString(2, reservation.getDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
             preparedStatement.setLong(3, reservation.getTime().getId());
             return preparedStatement;
-        }, generatedKeyHolder);
-        return new Reservation(generatedKeyHolder.getKey().longValue(), reservation.getName(),
-                reservation.getDate(), reservation.getTime());
+        };
     }
 
     public int deleteById(final Long id) {

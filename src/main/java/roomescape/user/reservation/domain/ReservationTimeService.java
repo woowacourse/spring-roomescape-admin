60aa995f7e9cb57c1fef8e9a40d3c128bd909d40
jeyong <joon6093@naver.com>
2025-04-28 04:50:11 +0900

    chore: @Transactional을 ReservationTimeService에 명시하도록 변경

--- src/main/java/roomescape/user/reservation/domain/ReservationTimeService.java
+++ src/main/java/roomescape/user/reservation/domain/ReservationTimeService.java
@@ -3,6 +3,7 @@ package roomescape.user.reservation.domain;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -10,19 +11,23 @@ public class ReservationTimeService {
 
     private final ReservationTimeRepository reservationTimeRepository;
 
+    @Transactional
     public Long saveReservationTime(final ReservationTime reservationTime) {
         return reservationTimeRepository.save(reservationTime);
     }
 
+    @Transactional(readOnly = true)
     public ReservationTime findReservationTime(final Long reservationTimeId) {
         return reservationTimeRepository.findById(reservationTimeId)
                 .orElseThrow(() -> new IllegalStateException("Reservation time not found"));
     }
 
+    @Transactional(readOnly = true)
     public List<ReservationTime> findAllReservationTimes() {
         return reservationTimeRepository.findAll();
     }
 
+    @Transactional
     public void deleteReservationTime(final Long id) {
         reservationTimeRepository.deleteById(id);
     }

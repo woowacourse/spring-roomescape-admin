db0cfed0ed7eec31deae10aded1e4dcd43b6dce3
2jin2.1031 <2jin2.1031@gmail.com>
2025-04-27 15:52:11 +0900

    refactor: dto을 class에서 record로 변경

--- src/main/java/roomescape/reservationTime/domain/dto/ReservationTimeResDto.java
+++ src/main/java/roomescape/reservationTime/domain/dto/ReservationTimeResDto.java
@@ -1,19 +1,6 @@
 package roomescape.reservationTime.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.time.LocalTime;
 
-public class ReservationTimeResDto {
-
-    @JsonProperty
-    private final Long id;
-
-    @JsonProperty
-    private final LocalTime startAt;
-
-    public ReservationTimeResDto(Long id, LocalTime startAt) {
-        this.id = id;
-        this.startAt = startAt;
-    }
+public record ReservationTimeResDto(Long id, LocalTime startAt) {
 }

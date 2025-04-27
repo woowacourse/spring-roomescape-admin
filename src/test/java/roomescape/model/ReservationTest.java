b15f9552c6f8a6daf45d2d9b7b1f53a56cd1e3cf
LJM\eueo8 <eueo8259@gmail.com>
2025-04-27 21:50:54 +0900

    refactor : given-when-then 형식 추가하여 가독성 개선

--- src/test/java/roomescape/model/ReservationTest.java
+++ src/test/java/roomescape/model/ReservationTest.java
@@ -10,61 +10,73 @@ class ReservationTest {
 
     @DisplayName("이름에 null값이 들어오면 예외처리되도록 한다.")
     @Test
-    void test1(){
+    void test1() {
+        // given
         LocalDateTime dateTime = LocalDateTime.now().plusDays(1);
-        assertThatThrownBy(()->
+
+        // when & then
+        assertThatThrownBy(() ->
                 new Reservation(
                         1L,
                         null,
                         dateTime.toLocalDate(),
-                        new ReservationTime(dateTime.toLocalTime())))
+                        new ReservationTime(dateTime.toLocalTime()))
+        )
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 null이거나 공백일 수 없습니다");
     }
 
     @DisplayName("이름에 공백값 들어오면 예외처리되도록 한다.")
     @Test
-    void test2(){
+    void test2() {
+        // given
         LocalDateTime dateTime = LocalDateTime.now().plusDays(1);
-        assertThatThrownBy(()->
+
+        // when & then
+        assertThatThrownBy(() ->
                 new Reservation(
                         1L,
                         "",
                         dateTime.toLocalDate(),
-                        new ReservationTime(dateTime.toLocalTime())))
+                        new ReservationTime(dateTime.toLocalTime()))
+        )
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 null이거나 공백일 수 없습니다");
     }
 
     @DisplayName("당일 예약은 예외처리되도록 한다.")
     @Test
-    void test3(){
+    void test3() {
+        // given
         LocalDateTime dateTime = LocalDateTime.now();
-        assertThatThrownBy(()->
+
+        // when & then
+        assertThatThrownBy(() ->
                 new Reservation(
                         1L,
                         "히로",
                         dateTime.toLocalDate(),
-                        new ReservationTime(dateTime.toLocalTime())))
+                        new ReservationTime(dateTime.toLocalTime()))
+        )
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("과거 및 당일 예약은 불가능합니다.");
     }
 
     @DisplayName("오늘보다 과거로 예약하려고 할 경우 예외처리되도록 한다.")
     @Test
-    void test4(){
+    void test4() {
+        // given
         LocalDateTime dateTime = LocalDateTime.now().minusDays(1);
-        assertThatThrownBy(()->
+
+        // when & then
+        assertThatThrownBy(() ->
                 new Reservation(
                         1L,
                         "히로",
                         dateTime.toLocalDate(),
-                        new ReservationTime(dateTime.toLocalTime())))
+                        new ReservationTime(dateTime.toLocalTime()))
+        )
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("과거 및 당일 예약은 불가능합니다.");
     }
-
-
-
-
 }

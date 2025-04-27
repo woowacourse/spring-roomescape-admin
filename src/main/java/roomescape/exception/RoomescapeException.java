c057d55c47e6e17bedfd70d70b33ce77dbf5605b
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-04-27 14:56:04 +0900

    refactor: RoomescapeException의 생성자 및 메서드 protected로 캡슐화

--- src/main/java/roomescape/exception/RoomescapeException.java
+++ src/main/java/roomescape/exception/RoomescapeException.java
@@ -5,12 +5,12 @@ import org.springframework.http.HttpStatus;
 public abstract class RoomescapeException extends RuntimeException {
     private final HttpStatus httpStatus;
 
-    public RoomescapeException(HttpStatus httpStatus, String message) {
+    protected RoomescapeException(HttpStatus httpStatus, String message) {
         super(message);
         this.httpStatus = httpStatus;
     }
 
-    public HttpStatus getHttpStatus() {
+    protected HttpStatus getHttpStatus() {
         return httpStatus;
     }
 }

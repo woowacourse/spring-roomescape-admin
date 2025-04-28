e2433c1df27e95b755677a305ffe22f78394c32a
yush <yush2202@gmail.com>
2025-04-28 21:19:38 +0900

    refactor: 이전 검증방식으로 되돌림

--- src/main/java/roomescape/domain/Person.java
+++ src/main/java/roomescape/domain/Person.java
@@ -5,30 +5,30 @@ public record Person(String name) {
     private static final int MAX_NAME_LENGTH = 7;
 
     public Person {
-        validateNullName();
-        validateEmptyName();
-        validateNameLength();
+        validateNullName(name);
+        validateEmptyName(name);
+        validateNameLength(name);
     }
 
-    private void validateNullName() {
+    private void validateNullName(String name) {
         if (name == null) {
             throw new IllegalArgumentException("이름은 없을 수 없습니다.");
         }
     }
 
-    private void validateEmptyName() {
+    private void validateEmptyName(String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("비어 있는 이름은 입력 불가합니다.");
         }
     }
 
-    private void validateNameLength() {
-        if (overMaxLength()) {
+    private void validateNameLength(String name) {
+        if (overMaxLength(name)) {
             throw new IllegalArgumentException(String.format("이름은 %d자 이하여야 합니다.", MAX_NAME_LENGTH));
         }
     }
 
-    private boolean overMaxLength() {
+    private boolean overMaxLength(String name) {
         return name.length() > MAX_NAME_LENGTH;
     }
 }

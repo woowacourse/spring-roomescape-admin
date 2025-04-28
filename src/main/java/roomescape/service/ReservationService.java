e63c8a64483b0b973faf272d87959287e7d7c504
eoehd1ek <eoehd2ek@gmail.com>
2025-04-28 15:24:02 +0900

    feat(ReservationService): 인터페이스 정의

---
+++ src/main/java/roomescape/service/ReservationService.java
@@ -0,0 +1,14 @@
+package roomescape.service;
+
+import java.util.List;
+import roomescape.dto.ReservationRequest;
+import roomescape.dto.ReservationResponse;
+
+public interface ReservationService {
+
+    List<ReservationResponse> findAllReservations();
+
+    ReservationResponse createReservation(ReservationRequest reservationRequest);
+
+    int deleteReservationById(Long id);
+}

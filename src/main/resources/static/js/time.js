9fbfc98f6a2368c18e67b150b034903e7d356f8a
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-04-28 15:54:54 +0900

    refactor(model) : 필드명 스타일 변경
    - camel Case 적용 : ReservationTime.start_at -> startAt, ReservationTimeRequestDto.time_id -> timeId

--- src/main/resources/static/js/time.js
+++ src/main/resources/static/js/time.js
@@ -1,6 +1,6 @@
 let isEditing = false;
 const API_ENDPOINT = '/times';
-const cellFields = ['id', 'start_at'];
+const cellFields = ['id', 'startAt'];
 const createCellFields = ['', createInput()];
 function createBody(inputs) {
   return {

0f76ad652f9c0e9b65982b1129a3e0d726a85cf7
yush <yush2202@gmail.com>
2025-04-28 17:36:38 +0900

    refactor: record 검증 방식 변경

--- src/main/java/roomescape/domain/Person.java
+++ src/main/java/roomescape/domain/Person.java
@@ -5,23 +5,30 @@ public record Person(String name) {
     private static final int MAX_NAME_LENGTH = 7;
 
     public Person {
-        validateEmptyName(name);
-        validateNameLength(name);
+        validateNullName();
+        validateEmptyName();
+        validateNameLength();
     }
 
-    private void validateEmptyName(String name) {
+    private void validateNullName() {
+        if (name == null) {
+            throw new IllegalArgumentException("이름은 없을 수 없습니다.");
+        }
+    }
+
+    private void validateEmptyName() {
         if (name.isBlank()) {
             throw new IllegalArgumentException("비어 있는 이름은 입력 불가합니다.");
         }
     }
 
-    private void validateNameLength(String name) {
-        if (overMaxLength(name)) {
+    private void validateNameLength() {
+        if (overMaxLength()) {
             throw new IllegalArgumentException(String.format("이름은 %d자 이하여야 합니다.", MAX_NAME_LENGTH));
         }
     }
 
-    private boolean overMaxLength(String name) {
+    private boolean overMaxLength() {
         return name.length() > MAX_NAME_LENGTH;
     }
 }

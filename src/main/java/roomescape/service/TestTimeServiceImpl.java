1ec285019ee677fb63b3645bbdf13af3a7cd38db
eoehd1ek <eoehd2ek@gmail.com>
2025-04-28 16:20:32 +0900

    refactor: 줄일 수 있는 코드 줄이기

--- src/main/java/roomescape/service/TestTimeServiceImpl.java
+++ src/main/java/roomescape/service/TestTimeServiceImpl.java
@@ -2,7 +2,6 @@ package roomescape.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import roomescape.domain.Time;
 import roomescape.dto.TimeRequest;
 import roomescape.dto.TimeResponse;
@@ -15,7 +14,7 @@ public class TestTimeServiceImpl implements TimeService {
     public List<TimeResponse> findAllTime() {
         return times.stream()
                 .map(TimeResponse::from)
-                .collect(Collectors.toUnmodifiableList());
+                .toList();
     }
 
     @Override
@@ -28,9 +27,8 @@ public class TestTimeServiceImpl implements TimeService {
 
     @Override
     public int deleteTimeById(final Long id) {
-        int count = (int) times.stream()
+        return (int) times.stream()
                 .filter(time -> time.getId().equals(id))
                 .count();
-        return count;
     }
 }

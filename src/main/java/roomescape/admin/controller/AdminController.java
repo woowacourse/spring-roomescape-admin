863b9f9e52cf5dd7a10a00f570f5369ea820bffa
bowook <mbosx316@naver.com>
2025-04-28 16:44:52 +0900

    refactor(AdminController) : 각 화면에 대응하는 페이지를 매핑시킬 수 있도록 수정

--- src/main/java/roomescape/admin/controller/AdminController.java
+++ src/main/java/roomescape/admin/controller/AdminController.java
@@ -2,13 +2,11 @@ package roomescape.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping
+    @GetMapping("/admin")
     public String getHomepage() {
         return "admin/index";
     }

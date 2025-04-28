c8b26e6e1f2c084f1994fae873df1ad3f4d2fd7f
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-04-28 17:46:27 +0900

    test: validator 테스트 gwt 추가

--- src/test/java/roomescape/common/validate/ValidatorTest.java
+++ src/test/java/roomescape/common/validate/ValidatorTest.java
@@ -11,8 +11,11 @@ class ValidatorTest {
     @Test
     @DisplayName("필드가 null이면 IllegalArgumentException이 발생한다")
     void validateNotNullFieldThrowsException() {
+        // given
         final Validator validator = Validator.of(SampleClass.class);
 
+        // when
+        // then
         assertThatThrownBy(() -> validator.notNullField("sampleField", null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("SampleClass.sampleField 은(는) null일 수 없습니다.");
@@ -21,16 +24,22 @@ class ValidatorTest {
     @Test
     @DisplayName("필드가 null이 아니면 예외가 발생하지 않는다")
     void validateNotNullFieldPasses() {
+        // given
         final Validator validator = Validator.of(SampleClass.class);
 
+        // when
+        // then
         assertThat(validator.notNullField("sampleField", "")).isNotNull();
     }
 
     @Test
     @DisplayName("필드가 null이거나 blank이면 IllegalArgumentException이 발생한다")
     void validateNotBlankFieldThrowsException() {
+        // given
         final Validator validator = Validator.of(SampleClass.class);
 
+        // when
+        // then
         assertThatThrownBy(() -> validator.notBlankField("sampleField", null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("SampleClass.sampleField 은(는) 비어있을 수 없습니다.");
@@ -43,8 +52,11 @@ class ValidatorTest {
     @Test
     @DisplayName("필드가 blank가 아니면 예외가 발생하지 않는다")
     void validateNotBlankFieldPasses() {
+        // given
         final Validator validator = Validator.of(SampleClass.class);
 
+        // when
+        // then
         assertThat(validator.notBlankField("sampleField", "content")).isNotNull();
     }
 

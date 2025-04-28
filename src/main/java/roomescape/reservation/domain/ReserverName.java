0f25078879996464dec691905e3fb2cc8c3e9eae
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-04-28 17:33:45 +0900

    feat: 예약자 명 동등성 비교 추가

--- src/main/java/roomescape/reservation/domain/ReserverName.java
+++ src/main/java/roomescape/reservation/domain/ReserverName.java
@@ -2,6 +2,7 @@ package roomescape.reservation.domain;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.experimental.FieldNameConstants;
 import roomescape.common.validate.Validator;
@@ -9,6 +10,7 @@ import roomescape.common.validate.Validator;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @FieldNameConstants(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode
 public class ReserverName {
 
     private final String value;

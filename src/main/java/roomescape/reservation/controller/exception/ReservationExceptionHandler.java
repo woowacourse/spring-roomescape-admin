e1e4946b786dfe6bbf2b8819d870b562dd1362a4
Yeongjae Kim <sees77770@gmail.com>
2025-04-28 15:05:47 +0900

    refactor: 예외 핸들러 패키지 분리

--- src/main/java/roomescape/reservation/controller/exception/ReservationExceptionHandler.java
+++ src/main/java/roomescape/reservation/controller/exception/ReservationExceptionHandler.java
@@ -1,4 +1,4 @@
-package roomescape.reservation.controller;
+package roomescape.reservation.controller.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;

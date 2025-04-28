32d17c63a2080fbabb958d5df8cae70a0aec6309
BWY <quantumjump10@gmail.com>
2025-04-28 22:44:13 +0900

    refactor: 예외 메시지 상수화

--- src/main/java/roomescape/reservation/exception/ReservationTimeNotFoundException.java
+++ src/main/java/roomescape/reservation/exception/ReservationTimeNotFoundException.java
@@ -2,11 +2,13 @@ package roomescape.reservation.exception;
 
 public class ReservationTimeNotFoundException extends RuntimeException {
 
+    private static final String MESSAGE = "해당 ID의 예약 시간이 존재하지 않습니다. id = ";
+
     public ReservationTimeNotFoundException(Long id) {
-        super("해당 ID의 예약 시간이 존재하지 않습니다. id = " + id);
+        super(MESSAGE + id);
     }
 
     public ReservationTimeNotFoundException(Long id, Throwable e) {
-        super("해당 ID의 예약 시간이 존재하지 않습니다. id = " + id);
+        super(MESSAGE + id);
     }
 }

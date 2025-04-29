c07b2145219a124abb8ced58b0ca245218a1e94c
민경윤 <ruddbs3308@naver.com>
2025-04-29 13:59:16 +0900

    refactor: 하나의 기능에 대해서만 테스트를 하도록 변경

--- src/test/java/roomescape/business/domain/PlayTimeTest.java
+++ src/test/java/roomescape/business/domain/PlayTimeTest.java
@@ -1,7 +1,6 @@
 package roomescape.business.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
@@ -9,20 +8,26 @@ import org.junit.jupiter.api.Test;
 
 class PlayTimeTest {
 
-    @DisplayName("객체 생성시 null을 들어올 수 없다.")
+    @DisplayName("생성자로 null은 들어올 수 없다.")
     @Test
     void validateNonNull() {
         // given
         final LocalTime invalidStartAt = null;
-        final LocalTime validStartAt = LocalTime.MAX;
-        final Long invalidId = null;
 
         // when & then
-        assertAll(
-                () -> assertThatThrownBy(() -> new PlayTime(invalidStartAt))
-                        .isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> PlayTime.createWithId(invalidId, validStartAt))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertThatThrownBy(() -> new PlayTime(invalidStartAt))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("id를 포함하여 생성할 때 id에 null은 들어올 수 없다.")
+    @Test
+    void createWithId() {
+        // given
+        final Long invalidId = null;
+        final LocalTime validStartAt = LocalTime.MAX;
+
+        // when & then
+        assertThatThrownBy(() -> PlayTime.createWithId(invalidId, validStartAt))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }

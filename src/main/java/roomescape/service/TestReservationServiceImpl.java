8cd48de3cee5476bbe6c63ca45f7d7767753eb3a
eoehd1ek <eoehd2ek@gmail.com>
2025-05-06 19:10:24 +0900

    fix: 페이크 객체를 원본 객체와 유사하게 동작하도록 수정

--- src/main/java/roomescape/service/TestReservationServiceImpl.java
+++ src/main/java/roomescape/service/TestReservationServiceImpl.java
@@ -2,6 +2,7 @@ package roomescape.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import roomescape.domain.Reservation;
 import roomescape.dto.ReservationRequest;
 import roomescape.dto.ReservationResponse;
@@ -9,6 +10,7 @@ import roomescape.dto.ReservationResponse;
 public class TestReservationServiceImpl implements ReservationService {
 
     private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong atomicLong = new AtomicLong(1L);
 
     @Override
     public List<ReservationResponse> findAllReservations() {
@@ -20,15 +22,17 @@ public class TestReservationServiceImpl implements ReservationService {
     @Override
     public ReservationResponse createReservation(final ReservationRequest reservationRequest) {
         final Reservation reservation = reservationRequest.toEntity();
+        reservation.setId(atomicLong.getAndIncrement());
         reservations.add(reservation);
-        reservation.setId(reservations.size() + 1L);
         return ReservationResponse.from(reservation);
     }
 
     @Override
     public int deleteReservationById(final Long id) {
-        return (int) reservations.stream()
-                .filter(reservation -> reservation.getId().equals(id))
-                .count();
+        int beforeSize = reservations.size();
+        reservations.removeIf(reservation -> reservation.getId()
+                .equals(id));
+        int afterSize = reservations.size();
+        return beforeSize - afterSize;
     }
 }

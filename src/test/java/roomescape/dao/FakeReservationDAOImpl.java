f789a77594f337aeba619eee029f95f8d3986333
eoehd1ek <eoehd2ek@gmail.com>
2025-05-06 21:42:55 +0900

    refactor: FakeReservationDAOImpl 실제 객체와 유사하게 동작 수정

--- src/test/java/roomescape/dao/FakeReservationDAOImpl.java
+++ src/test/java/roomescape/dao/FakeReservationDAOImpl.java
@@ -3,11 +3,13 @@ package roomescape.dao;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import roomescape.domain.Reservation;
 
 public class FakeReservationDAOImpl implements ReservationDAO {
 
     final List<Reservation> reservations = new ArrayList<>();
+    final AtomicLong atomicLong = new AtomicLong(1L);
 
     @Override
     public List<Reservation> findAllReservation() {
@@ -16,16 +18,18 @@ public class FakeReservationDAOImpl implements ReservationDAO {
 
     @Override
     public Long insertReservation(final Reservation reservation) {
+        final long id = atomicLong.getAndIncrement();
+        reservation.setId(id);
         reservations.add(reservation);
-        return (long) reservations.size();
+        return id;
     }
 
     @Override
     public int deleteReservationById(final Long id) {
-        final long idMatchedCount = reservations.stream()
-                .filter(reservation -> reservation.getId().equals(id))
-                .count();
-        return (int) idMatchedCount;
+        final int beforeSize = reservations.size();
+        reservations.removeIf(reservation -> reservation.getId().equals(id));
+        final int afterSize = reservations.size();
+        return beforeSize - afterSize;
     }
 
     @Override

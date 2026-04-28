3afac46d0a3544f02ed728303b1f7424440d7ac7
haeseon lee <haeseon4816@gmail.com>
2026-04-28 21:07:13 +0900

    feat: Reservation 객체 구현

---
+++ src/main/java/roomescape/Reservation.java
@@ -0,0 +1,48 @@
+package roomescape;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+
+    private Long id;
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
+
+    public Reservation() {
+    }
+
+    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public static Reservation toEntity(Reservation reservation, Long id) {
+        return new Reservation(id, reservation.getName(), reservation.getDate(), reservation.getTime());
+    }
+
+    public void update(Reservation newReservation) {
+        this.name = newReservation.name;
+        this.date = newReservation.date;
+        this.time = newReservation.time;
+    }
+}

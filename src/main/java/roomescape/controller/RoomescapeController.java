794a74947604e9c8296290344fab3c24f1a1b1ca
ghkdtjfxkd <syt1128@gmail.com>
2026-04-28 15:53:52 +0900

    chore: setup api skeleton

---
+++ src/main/java/roomescape/controller/RoomescapeController.java
@@ -0,0 +1,39 @@
+package roomescape.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.Reservation;
+import roomescape.dto.response.ReservationResponse;
+import roomescape.dto.request.ReservationRequest;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reservations")
+public class RoomescapeController {
+
+    private final List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(0L);
+
+    @GetMapping
+    public ResponseEntity<List<ReservationResponse>> getAllReservations() {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationResponse> createReservation(ReservationRequest request) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation() {
+        return null;
+    }
+}

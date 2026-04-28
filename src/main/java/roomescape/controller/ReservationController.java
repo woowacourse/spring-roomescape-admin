c8f15f16540f59963b00aacf79ab2801f92a2167
nn98 <jkllhgb@gmail.com>
2026-04-28 18:00:01 +0900

    feat: 컨트롤러 세팅

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,20 +3,23 @@ package roomescape.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 
-@Controller
+@RestController
 public class ReservationController {
 
     private List<Reservation> reservations = new ArrayList<>();
     private AtomicLong index = new AtomicLong(1);
 //    reservations.add(new Reservation(index.incrementAndGet(), "브라운", "2023-01-01", "10:00"));
 
-    @RequestMapping("/reservations")
-    public String reservations(Model model) {
+    @GetMapping("/reservations")
+    public  reservations(Model model) {
         model.addAttribute("reservations", reservations);
         return "reservations";
     }

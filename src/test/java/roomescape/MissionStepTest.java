7c64c3df28046f09a582065d293d8b6d6b88a336
이동훈 <higgs95@naver.com>
2026-04-28 21:50:22 +0900

    test: 예약 조회/생성/삭제 기능 테스트 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,17 +1,21 @@
 package roomescape;
 
+import static org.hamcrest.Matchers.is;
+
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-
-import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
     @Test
+    @DisplayName("예약을 조회한다.")
     void 예약_조회() {
         RestAssured.given().log().all()
                 .when().get("/reservations")
@@ -20,4 +24,40 @@ public class MissionStepTest {
                 .body("size()", is(0)); // 아직 생성 요청이 없으니 0개
     }
 
+    @Test
+    @DisplayName("예약을 추가한다.")
+    void 예약_추가() {
+        Map<String, String> params = Map.of(
+                "name", "Ace",
+                "date", "2026-04-28",
+                "time", "10:00"
+        );
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("예약을 삭제한다.")
+    void 예약_삭제() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(Map.of("name", "Ace", "date", "2026-04-29", "time", "10:00")).
+                when().post("/reservations")
+                .then().statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(204);
+
+        RestAssured.given()
+                .when().get("/reservations")
+                .then()
+                .body("size()", is(0));
+    }
 }

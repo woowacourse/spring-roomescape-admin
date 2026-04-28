679ecab1f59e7b0cff4752bea2c0e6df4911c7c4
toctoce <toctoce@naver.com>
2026-04-28 17:45:37 +0900

    feat: 예약 생성 기능 구현

---
+++ src/main/java/roomescape/ReservationController.java
@@ -0,0 +1,24 @@
+package roomescape;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import roomescape.domain.Reservation;
+
+@Controller
+public class ReservationController {
+    private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
+
+    @PostMapping("/reservations")
+    @ResponseBody
+    public Reservation create(@RequestBody Reservation reservation) {
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        reservations.add(newReservation);
+        return newReservation;
+    }
+}

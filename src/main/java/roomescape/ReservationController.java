e00886fdf150460b5205c4ec4a9f0533d25926dd
Jihyun3478 <leejihyun3478@gmail.com>
2026-04-28 21:47:53 +0900

    feat: 방탈출 예약 조회 기능 구현

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -2,7 +2,11 @@ package roomescape;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +24,10 @@ public class ReservationController {
         reservations.add(reservation);
 
         return reservation;
+    }
+
+    @GetMapping
+    public List<Reservation> getReservations() {
+        return reservations;
     }
 }

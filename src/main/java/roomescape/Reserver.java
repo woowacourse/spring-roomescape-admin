1a107265e5c363e7035b5fb455e7e186ae6f27ff
snow___shower <wonsb25@gmail.com>
2026-04-28 18:01:08 +0900

    refactor: time 타입 LoaclTime으로 수정
    Signed-off-by: snow___shower <wonsb25@gmail.com>

--- src/main/java/roomescape/Reserver.java
+++ src/main/java/roomescape/Reserver.java
@@ -1,24 +1,24 @@
 package roomescape;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Reserver {
     private Long id;
     private String name;
     private LocalDate date;
-    private LocalDateTime time;
+    private LocalTime time;
 
     public Reserver() {
     }
 
-    public Reserver(String name, LocalDate date, LocalDateTime time) {
+    public Reserver(String name, LocalDate date, LocalTime time) {
         this.name = name;
         this.date = date;
         this.time = time;
     }
 
-    public Reserver(Long id, String name, LocalDate date, LocalDateTime time) {
+    public Reserver(Long id, String name, LocalDate date, LocalTime time) {
         this.id = id;
         this.name = name;
         this.date = date;
@@ -37,7 +37,7 @@ public class Reserver {
         return date;
     }
 
-    public LocalDateTime getTime() {
+    public LocalTime getTime() {
         return time;
     }
 

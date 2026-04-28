8fef146cc257d65e0ab202cb982c297276bd82fd
koreaioi <koreaioi@gachon.ac.kr>
2026-04-29 00:52:16 +0900

    feat(ReservationController): 예약 생성 API 추가
    - Reqeust Body의 데이터를 매핑하기 위한ReservationSaveDto 생성

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,9 +2,13 @@ package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.Reservation;
 import roomescape.dao.ReservationDao;
 import roomescape.dto.ReservationDetailDto;
+import roomescape.dto.ReservationSaveDto;
 
 import java.util.List;
 
@@ -23,6 +27,13 @@ public class ReservationController {
                 .stream()
                 .map(ReservationDetailDto::from)
                 .toList();
+        return ResponseEntity.ok(responseData);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<ReservationDetailDto> createReservation(@RequestBody ReservationSaveDto dto) {
+        Reservation savedReservation = reservationDao.insert(dto.name(), dto.date(), dto.time());
+        ReservationDetailDto responseData = ReservationDetailDto.from(savedReservation);
         return ResponseEntity.ok(responseData);
     }
 

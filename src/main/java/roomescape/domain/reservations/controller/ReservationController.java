f0b46ace359edc7c7806129c41077f8337ec813e
jyt6640 <jyt6640@gmail.com>
2026-04-28 16:05:18 +0900

    feat: 예약 컨트롤러 추가
    - 예약 저장 컨트롤러 구현
    - 저장된 예약 리스트 조회 컨트롤러 구현
    - 예약 삭제 컨트롤러 구현

---
+++ src/main/java/roomescape/domain/reservations/controller/ReservationController.java
@@ -0,0 +1,44 @@
+package roomescape.domain.reservations.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.reservations.entity.Reservation;
+
+@RestController
+public class ReservationController {
+    private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
+
+    @PostMapping("/reservations")
+    public Reservation addReservation(
+            @RequestBody Reservation request
+    ) {
+        Reservation reservation = new Reservation(
+                index.getAndIncrement(),
+                request.getName(),
+                request.getDate().toString(),
+                request.getTime().toString()
+        );
+        reservations.add(reservation);
+        return reservation;
+    }
+
+    @GetMapping("/reservations")
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public void deleteReservation(
+            @PathVariable Long id
+    ) {
+        reservations.removeIf(reservation -> reservation.getId().equals(id));
+    }
+}

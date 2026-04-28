b77f81376f5bd617f7d8c4a641784c27cdda64d3
xxbeann <yjb4945@gmail.com>
2026-04-28 22:56:08 +0900

    refactor: make reservation fields final

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -5,12 +5,12 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Reservation {
-    private Long id;
-    private String name;
+    private final Long id;
+    private final String name;
     @JsonFormat(pattern = "yyyy-MM-dd")
-    private LocalDate date;
+    private final LocalDate date;
     @JsonFormat(pattern = "HH:mm")
-    private LocalTime time;
+    private final LocalTime time;
 
     public Reservation(Long id, String name, LocalDate date, LocalTime time) {
         this.id = id;

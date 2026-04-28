db2f4a39e8ff8d24f886a3da910731f223b05332
kangrae <kangrae0501@gmail.com>
2026-04-28 15:50:52 +0900

    feat: 예약 추가 기능 api 구현
    - request, response, entity 분리
    - 원본 entity 객체가 돌아다니지 않기 위함
    - request, response는 불변으로 하기위해 record로 설계
    - 자연스럽게 직렬화 가능한 객체로 선언됨

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -10,11 +10,36 @@ public class Reservation {
     private final LocalDate date;
     private final LocalTime time;
 
-    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+    private Reservation(Long id, String name, LocalDate date, LocalTime time) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    public static Reservation toEntity(Long index, ReservationRequest request) {
+        return new Reservation(
+                index,
+                request.name(),
+                request.date(),
+                request.time()
+        );
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
     }
 
 }
--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -2,14 +2,29 @@ package roomescape;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public class ReservationController {
 
     private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(0);
+
+    @PostMapping("/reservations")
+    public ResponseEntity<?> postReservation(@RequestBody ReservationRequest request) {
+        Reservation reservation = Reservation.toEntity(index.incrementAndGet(), request);
+        reservations.add(reservation);
+        
+        ReservationResponse response = ReservationResponse.from(reservation);
+        return ResponseEntity.ok().body(response);
+    }
 
     @GetMapping("/reservations")
     public ResponseEntity<?> getAllReservations() {

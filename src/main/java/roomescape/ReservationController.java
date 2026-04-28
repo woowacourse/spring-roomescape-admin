6e0838c55a224039304105435dd96dbb845513b3
Chocoding1 <galmeagi2@naver.com>
2026-04-28 20:16:45 +0900

    refactor: @RestController 어노테이션 추가

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class ReservationController {
 
     private final ReservationRepository reservationRepository;

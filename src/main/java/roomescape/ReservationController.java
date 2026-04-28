6153c18f0094756601107edde9e6f802e8253e00
kangrae <kangrae0501@gmail.com>
2026-04-28 16:42:08 +0900

    refactor: @ResponseBody 삭제
    - ResponseEntity로 return하기 때문에 body에 잘 매핑된다.
    - 이후에 명시적으로 해주기 위해서라면 @RestController를 사용하도록 한다.

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -12,10 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 public class ReservationController {
 
     private final List<Reservation> reservations = new ArrayList<>();

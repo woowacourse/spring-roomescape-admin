5f03d9b8dbbfcf4b52619776f9ec756ca68dc908
Soojin6943 <sjhan0711@gmail.com>
2026-04-28 17:45:34 +0900

    fix(reservation) : 예약 날짜, 시간 매핑 안되던 오류 해결

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -6,27 +6,27 @@ import java.time.LocalTime;
 public class Reservation {
     private Long id;
     private String name;
-    private LocalDate reservationDate;
-    private LocalTime reservationTime;
+    private LocalDate date;
+    private LocalTime time;
 
     public Reservation() {
     }
 
-    public Reservation(Long id, String name, LocalDate reservationDate, LocalTime reservationTime) {
+    public Reservation(Long id, String name, LocalDate date, LocalTime reservationTime) {
         this.id = id;
         this.name = name;
-        this.reservationDate = reservationDate;
-        this.reservationTime = reservationTime;
+        this.date = date;
+        this.time = reservationTime;
     }
 
-    public Reservation(String name, LocalDate reservationDate, LocalTime reservationTime) {
+    public Reservation(String name, LocalDate date, LocalTime reservationTime) {
         this.name = name;
-        this.reservationDate = reservationDate;
-        this.reservationTime = reservationTime;
+        this.date = date;
+        this.time = reservationTime;
     }
 
     public static Reservation toEntity(Long id, Reservation reservation) {
-        return new Reservation(id, reservation.name, reservation.reservationDate, reservation.reservationTime);
+        return new Reservation(id, reservation.name, reservation.date, reservation.time);
     }
 
     public Long getId() {
@@ -37,11 +37,11 @@ public class Reservation {
         return this.name;
     }
 
-    public LocalDate getReservationDate() {
-        return reservationDate;
+    public LocalDate getDate() {
+        return date;
     }
 
-    public LocalTime getReservationTime() {
-        return reservationTime;
+    public LocalTime getTime() {
+        return time;
     }
 }

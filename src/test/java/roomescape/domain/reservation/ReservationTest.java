2f5caf2aec235fdc0f952d21663bbca6d992e11c
Sumin <new.sumyang@gmail.com>
2026-04-28 21:35:17 +0900

    test: 테스트 추가

---
+++ src/test/java/roomescape/domain/reservation/ReservationTest.java
@@ -0,0 +1,72 @@
+package roomescape.domain.reservation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.junit.jupiter.api.Test;
+
+class ReservationTest {
+
+    @Test
+    void id가_없는_예약을_생성한다() {
+        // given
+        String name = "보예";
+        LocalDate date = LocalDate.of(2023, 8, 5);
+        LocalTime time = LocalTime.of(15, 40);
+
+        // when
+        Reservation reservation = Reservation.createWithoutId(name, date, time);
+
+        // then
+        assertAll(
+            () -> assertThat(reservation.getId()).isNull(),
+            () -> assertThat(reservation.getName()).isEqualTo(name),
+            () -> assertThat(reservation.getDate()).isEqualTo(date),
+            () -> assertThat(reservation.getTime()).isEqualTo(time)
+        );
+    }
+
+    @Test
+    void id를_부여한_예약을_생성한다() {
+        // given
+        Reservation reservation = Reservation.createWithoutId(
+            "보예",
+            LocalDate.of(2023, 8, 5),
+            LocalTime.of(15, 40)
+        );
+
+        // when
+        Reservation reservationWithId = Reservation.createWithId(1L, reservation);
+
+        // then
+        assertAll(
+            () -> assertThat(reservationWithId.getId()).isEqualTo(1L),
+            () -> assertThat(reservationWithId.getName()).isEqualTo("보예"),
+            () -> assertThat(reservationWithId.getDate()).isEqualTo(LocalDate.of(2023, 8, 5)),
+            () -> assertThat(reservationWithId.getTime()).isEqualTo(LocalTime.of(15, 40))
+        );
+    }
+
+    @Test
+    void DB에서_조회한_예약을_생성한다() {
+        // given
+        long id = 1L;
+        String name = "보예";
+        LocalDate date = LocalDate.of(2023, 8, 5);
+        LocalTime time = LocalTime.of(15, 40);
+
+        // when
+        Reservation reservation = Reservation.of(id, name, date, time);
+
+        // then
+        assertAll(
+            () -> assertThat(reservation.getId()).isEqualTo(id),
+            () -> assertThat(reservation.getName()).isEqualTo(name),
+            () -> assertThat(reservation.getDate()).isEqualTo(date),
+            () -> assertThat(reservation.getTime()).isEqualTo(time)
+        );
+    }
+}

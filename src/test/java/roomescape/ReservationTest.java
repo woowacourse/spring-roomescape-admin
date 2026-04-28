c9954af074f684497c326b300850b359a974936a
frombunny <rucy011121@gmail.com>
2026-04-28 18:23:57 +0900

    test(reservation): 중복 예약 여부 테스트 작성

--- src/test/java/roomescape/ReservationTest.java
+++ src/test/java/roomescape/ReservationTest.java
@@ -3,9 +3,11 @@ package roomescape;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.naming.spi.ResolveResult;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ReservationTest {
@@ -40,5 +42,15 @@ class ReservationTest {
     void 예약_생성_시_예약_시간이_null이면_예외가_발생한다() {
         assertThatThrownBy(() -> new Reservation("브라운", date, null)).isInstanceOf(
                 IllegalArgumentException.class);
+    }
+
+    @Test
+    void 날짜와_시간이_동일한_예약인지_반환한다() {
+        Reservation neoReservation = new Reservation("네오", date, time);
+        Reservation brownReservation = new Reservation("브라운", date, time);
+        Reservation pobiReservation = new Reservation("포비", date, LocalTime.of(14, 0));
+
+        assertThat(neoReservation.isSameDateTime(brownReservation)).isTrue();
+        assertThat(neoReservation.isSameDateTime(pobiReservation)).isFalse();
     }
 }

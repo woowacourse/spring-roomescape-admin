3f5f62db2ee6390a4cc1ae7513f237b68bc50ce1
snow___shower <wonsb25@gmail.com>
2026-04-28 17:00:47 +0900

    test: 예약 조회 API 단위 테스트 추가
    Signed-off-by: snow___shower <wonsb25@gmail.com>

--- src/test/java/roomescape/ReservationControllerTest.java
+++ src/test/java/roomescape/ReservationControllerTest.java
@@ -8,24 +8,38 @@ import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReservationControllerTest {
     private ReservationController controller;
+    private Reserver reserver;
+    private ResponseEntity<Void> createResponse;
 
     @BeforeEach
     void setUp() {
         controller = new ReservationController();
+        reserver = new Reserver("user1", LocalDate.of(2026, 4, 28),
+                LocalDateTime.of(2026, 4, 28, 15, 0, 0));
+        createResponse = controller.create(reserver);
     }
 
     @Test
     @DisplayName("예약자를 추가하면 201 코드를 반환한다.")
     void return201Created_When_AddReserver() {
-        Reserver reserver = new Reserver("user1", LocalDate.of(2026, 4, 28),
-                LocalDateTime.of(2026, 4, 28, 15, 0, 0));
-        ResponseEntity<Void> createResponse = controller.create(reserver);
-
         assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
+
+    @Test
+    @DisplayName("예약자를 조회하면 추가한 예약자의 정보를 반환한다.")
+    void returnReserverInfo_When_AddReserver() {
+        ResponseEntity<List<Reserver>> readResponse = controller.read();
+        List<Reserver> reservers = readResponse.getBody();
+
+        assertThat(reservers).isNotNull();
+        assertThat(reservers).hasSize(1);
+        assertThat(reservers.getFirst().getName()).isEqualTo("user1");
+
     }
 }

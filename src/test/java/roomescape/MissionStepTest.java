feda129710315e5abe88ee737c194f71c6432355
kangrae <kangrae0501@gmail.com>
2026-04-28 22:57:35 +0900

    chore: 테스트용 mem db 설정
    - 테스트와 운영 db를 분리하여 테스트를 실행해도 안전하도록 한다.

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -27,7 +27,7 @@ public class MissionStepTest {
     void 데이터베이스_연동() {
         try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
             assertThat(connection).isNotNull();
-            assertThat(connection.getCatalog()).isEqualTo("ROOMESCAPE");
+            assertThat(connection.getCatalog()).isEqualTo("ROOMESCAPE-TEST");
             assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
         } catch (SQLException e) {
             throw new RuntimeException(e);

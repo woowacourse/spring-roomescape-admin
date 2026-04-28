7d3175738d8a4bbbad2aa5d6c332402f2c756688
toctoce <toctoce@naver.com>
2026-04-28 23:47:53 +0900

    refactor: Controller를 RestController로 변경

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -3,29 +3,35 @@ package roomescape;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 
-@Controller
+@RestController
+@RequestMapping("/reservations")
 public class ReservationController {
     private List<Reservation> reservations = new ArrayList<>();
     private AtomicLong index = new AtomicLong(1);
 
-    @PostMapping("/reservations")
-    @ResponseBody
+    @PostMapping
     public Reservation create(@RequestBody Reservation reservation) {
         Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
         reservations.add(newReservation);
         return newReservation;
     }
 
-    @GetMapping("/reservations")
-    @ResponseBody
+    @GetMapping
     public List<Reservation> read() {
         return reservations;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        reservations.removeIf(reservation -> reservation.getId() == id);
     }
 }

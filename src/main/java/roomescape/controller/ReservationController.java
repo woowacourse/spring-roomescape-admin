4ed96d752e375a04623ffb8034646c0728a9355f
EveryPine <kenton.lim@gmail.com>
2026-04-28 16:36:55 +0900

    feat(reservations): 예약 삭제 기능 추가

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,15 +1,17 @@
 package roomescape.controller;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.ReservationRequestDto;
 import roomescape.dto.ReservationResponseDto;
@@ -21,21 +23,27 @@ import roomescape.entity.Reservation;
 public class ReservationController {
 
     private final List<Reservation> reservations;
-    private final AtomicLong index = new AtomicLong(1);
+    private final AtomicLong index = new AtomicLong(0);
 
     @GetMapping
-    @ResponseBody
-    public List<Reservation> getReservations() {
-        return Collections.unmodifiableList(reservations);
+    public ResponseEntity<List<Reservation>> getReservations() {
+        return new ResponseEntity<>(Collections.unmodifiableList(reservations), HttpStatus.OK);
     }
 
     @PostMapping
-    @ResponseBody
-    public ReservationResponseDto add(
+    public ResponseEntity<ReservationResponseDto> add(
         @RequestBody ReservationRequestDto reservationRequestDto) {
-        final Reservation reservation = Reservation.from(index.incrementAndGet(), reservationRequestDto);
+        final Reservation reservation = Reservation.from(index.incrementAndGet(),
+            reservationRequestDto);
         reservations.add(reservation);
 
-        return ReservationResponseDto.from(reservation);
+        return new ResponseEntity<>(ReservationResponseDto.from(reservation), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable final long id) {
+        reservations.removeIf(reservation -> reservation.getId() == id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }

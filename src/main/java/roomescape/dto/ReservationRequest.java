a43556b609ad4363a7de93f514a11036bc4beb6f
haeyoon1 <hynam1220@naver.com>
2026-04-28 16:46:54 +0900

    feat: reservation 요청 응답에 대한 dto 생성

---
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -0,0 +1,12 @@
+package roomescape.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record ReservationRequest(
+    String name,
+    LocalDate date,
+    LocalTime time
+) {
+
+}

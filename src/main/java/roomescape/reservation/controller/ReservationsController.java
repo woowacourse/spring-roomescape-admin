66146007822bfea7193b1b0b9afac4c0473bc1e8
quddaz <quddnddl35@naver.com>
2026-04-28 20:20:31 +0900

    feat(ReservationController) : DB 도입으로 인한 Controller 변경

--- src/main/java/roomescape/reservation/controller/ReservationsController.java
+++ src/main/java/roomescape/reservation/controller/ReservationsController.java
@@ -1,14 +1,20 @@
 package roomescape.reservation.controller;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import javax.swing.plaf.basic.BasicTreeUI.KeyHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +28,10 @@ import roomescape.reservation.entity.Reservation;
 @RestController
 @RequestMapping("/reservations")
 public class ReservationsController {
+
     private static final String SELECT_ALL = "SELECT * FROM reservation";
+    private static final String INSERT = "INSERT INTO reservation(name, date, time) VALUES (?, ?, ?);";
+    private static final String DELETE = "DELETE FROM reservation WHERE id = ?";
 
     @Autowired
     private final JdbcTemplate jdbcTemplate;
@@ -48,12 +57,32 @@ public class ReservationsController {
     }
 
     @PostMapping
-    public ResponseEntity<Reservation> add(@RequestBody ReservationRequestDto reservationRequestDto){
-        return null;
+    public ResponseEntity<Reservation> add(@RequestBody ReservationRequestDto reservationRequestDto) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(INSERT, new String[]{"id"});
+            ps.setString(1, reservationRequestDto.name());
+            ps.setDate(2, Date.valueOf(reservationRequestDto.date()));
+            ps.setTime(3, Time.valueOf(reservationRequestDto.time()));
+            return ps;
+        }, keyHolder);
+
+        Long id = keyHolder.getKey().longValue();
+
+        Reservation reservation = new Reservation(
+                id,
+                reservationRequestDto.name(),
+                reservationRequestDto.date(),
+                reservationRequestDto.time()
+        );
+
+        return ResponseEntity.ok(reservation);
     }
 
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id){
+        jdbcTemplate.update(DELETE, id);
     }
 }

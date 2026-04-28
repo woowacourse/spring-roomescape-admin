c5c7a753754c63e838d7efe2b68bd738076df17e
yItWorks <poiu7825@pusan.ac.kr>
2026-04-28 19:37:37 +0900

    fix: JSON 매핑 기능 추가

--- src/main/java/roomescape/domain/Name.java
+++ src/main/java/roomescape/domain/Name.java
@@ -1,9 +1,18 @@
 package roomescape.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public class Name {
     private String name;
 
+    @JsonCreator
     public Name(String name) {
         this.name = name;
+    }
+
+    @JsonValue
+    public String getName() {
+        return name;
     }
 }

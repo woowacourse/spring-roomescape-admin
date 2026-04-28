8a31b3a4e746ca212d9774aa983dfa436623e72c
JYL35 <154980427+JYL35@users.noreply.github.com>
2026-04-28 23:57:39 +0900

    feat: 예약 응답 객체 추가

---
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -0,0 +1,24 @@
+package roomescape.dto;
+
+import roomescape.entity.Reservation;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public record ReservationResponse(
+        Long id,
+        String name,
+        LocalDate date,
+        LocalTime time
+) {
+    public static ReservationResponse from(Reservation reservation) {
+        return new ReservationResponse(reservation.id(), reservation.name(), reservation.date(), reservation.time());
+    }
+
+    public static List<ReservationResponse> from(List<Reservation> reservations) {
+        return reservations.stream()
+                .map(ReservationResponse::from)
+                .toList();
+    }
+}

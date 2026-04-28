fb2147fc569e0fad352c6dd98068f0d317e4f0a2
Chocoding1 <galmeagi2@naver.com>
2026-04-28 19:22:28 +0900

    refactor: 테스트 코드 리팩토링(공통 지역 변수를 클래스 필드로 변경)

--- src/test/java/roomescape/ReservationRepositoryTest.java
+++ src/test/java/roomescape/ReservationRepositoryTest.java
@@ -1,5 +1,6 @@
 package roomescape;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,12 +14,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ReservationRepositoryTest {
 
+    private ReservationRepository reservationRepository;
+
+    @BeforeEach
+    void init() {
+        reservationRepository = new ReservationRepository();
+    }
+
     @Test
     @DisplayName("예약 조회 시 불변 리스트를 반환한다.")
     void findAll_then_return_unmodifiableList() {
-        // given
-        ReservationRepository reservationRepository = new ReservationRepository();
-
         // when
         List<Reservation> result = reservationRepository.findAll();
 
@@ -30,9 +35,6 @@ public class ReservationRepositoryTest {
     @Test
     @DisplayName("예약 추가 시 id가 생성되고 Reservation 객체가 생성되어 리스트에 추가된다.")
     void save_reservation() {
-        // given
-        ReservationRepository reservationRepository = new ReservationRepository();
-
         // when
         Reservation savedReservation = reservationRepository.save("홍길동", LocalDate.of(2024, 1, 1), LocalTime.of(10, 0));
 
@@ -44,9 +46,6 @@ public class ReservationRepositoryTest {
     @Test
     @DisplayName("에약 추가 시 id는 1씩 증가한다.")
     void save_then_id_is_incremented_by_1() {
-        // given
-        ReservationRepository reservationRepository = new ReservationRepository();
-
         // when
         Reservation savedReservation1 = reservationRepository.save("홍길동", LocalDate.of(2024, 1, 1), LocalTime.of(10, 0));
         Reservation savedReservation2 = reservationRepository.save("김철수", LocalDate.of(2024, 1, 2), LocalTime.of(11, 0));
@@ -60,7 +59,6 @@ public class ReservationRepositoryTest {
     @DisplayName("예약 삭제 시 해당 id의 예약이 리스트에서 삭제된다.")
     void delete_reservation_by_id_success() {
         // given
-        ReservationRepository reservationRepository = new ReservationRepository();
         Reservation savedReservation = reservationRepository.save("홍길동", LocalDate.of(2024, 1, 1), LocalTime.of(10, 0));
         Long id = savedReservation.getId();
 
@@ -74,9 +72,6 @@ public class ReservationRepositoryTest {
     @Test
     @DisplayName("존재하지 않는 id로 삭제 시 예외를 발생시킨다.")
     void delete_reservation_by_id_fail_when_reservation_not_found() {
-        // given
-        ReservationRepository reservationRepository = new ReservationRepository();
-
         // when & then
         assertThatThrownBy(() -> reservationRepository.deleteById(999L))
                 .isInstanceOf(NoSuchElementException.class)

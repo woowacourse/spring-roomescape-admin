30ee470f7ebeeec8e44ab1b8d2d035808c57418c
toctoce <toctoce@naver.com>
2026-04-28 18:17:31 +0900

    feat: 예약 리스트 조회 기능 구현

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,5 +21,11 @@ public class ReservationController {
         Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
         reservations.add(newReservation);
         return newReservation;
+    }
+
+    @GetMapping("/reservations")
+    @ResponseBody
+    public List<Reservation> read() {
+        return reservations;
     }
 }

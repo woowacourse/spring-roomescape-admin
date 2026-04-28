950fe408acc37291da41d693ce00d427e0e22029
kangrae <kangrae0501@gmail.com>
2026-04-28 22:42:55 +0900

    chore: schema.sql이 항상 실행되도록 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,8 @@
-CREATE TABLE reservation (
-                             id      BIGINT       NOT NULL AUTO_INCREMENT,
-                             name    VARCHAR(255) NOT NULL,
-                             date    VARCHAR(255) NOT NULL,
-                             time    VARCHAR(255) NOT NULL,
-                             PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS reservation
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    date DATE         NOT NULL,
+    time TIME         NOT NULL,
+    PRIMARY KEY (id)
 );

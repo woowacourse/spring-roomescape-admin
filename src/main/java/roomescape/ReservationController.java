a470b6ad01157e18a09ffd317d04bbf7df29d043
kangrae <kangrae0501@gmail.com>
2026-04-28 16:06:52 +0900

    feat: 예약 삭제 기능 api 구현

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -2,10 +2,13 @@ package roomescape;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,7 +24,7 @@ public class ReservationController {
     public ResponseEntity<?> postReservation(@RequestBody ReservationRequest request) {
         Reservation reservation = Reservation.toEntity(index.incrementAndGet(), request);
         reservations.add(reservation);
-        
+
         ReservationResponse response = ReservationResponse.from(reservation);
         return ResponseEntity.ok().body(response);
     }
@@ -29,6 +32,18 @@ public class ReservationController {
     @GetMapping("/reservations")
     public ResponseEntity<?> getAllReservations() {
         return ResponseEntity.ok().body(reservations);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<?> deleteReservation(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+
+        reservations.remove(reservation);
+
+        return ResponseEntity.ok().build(); // 요구사항에 맞춰서 noContent대신 ok를 return한다.
     }
 
 }

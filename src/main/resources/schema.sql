ca533b8db20b4715d84075ce5672e37c251e9fd1
ghkdtjfxkd <syt1128@gmail.com>
2026-04-28 23:11:46 +0900

    chore: 스키마에 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,9 @@
+CREATE TABLE reservation_time (
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE reservation (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,

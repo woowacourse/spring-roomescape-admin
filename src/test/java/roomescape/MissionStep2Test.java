310d79964aa7b79a22641f772b65a3ce94270895
mvg01 <dkms625@naver.com>
2026-04-28 19:41:19 +0900

    test: jdbc 테스트 코드 수정

--- src/test/java/roomescape/MissionStep2Test.java
+++ src/test/java/roomescape/MissionStep2Test.java
@@ -15,7 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStep2Test {
     @Autowired

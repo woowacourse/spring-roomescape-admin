a84ebaa87b21838f1f1bf83c9f937b9de67edf9a
HyoYoonNam <dev.rudevico@gmail.com>
2026-04-28 19:34:48 +0900

    feat: 예약 추가, 삭제 구현

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -5,15 +5,32 @@ import java.time.LocalTime;
 
 public class Reservation {
 
-    private final Long id;
+    private Long id;
     private final String name;
     private final LocalDate date;
     private final LocalTime time;
 
-    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
-        this.id = id;
+    public Reservation(String name, LocalDate date, LocalTime time) {
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date=" + date +
+                ", time=" + time +
+                '}';
     }
 }
--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,8 +3,13 @@ package roomescape.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import roomescape.Reservation;
 
@@ -12,11 +17,31 @@ import roomescape.Reservation;
 public class ReservationController {
 
     private final List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong index = new AtomicLong(1);
+    private final AtomicLong index = new AtomicLong(0);
 
     @GetMapping("/reservations")
     @ResponseBody
     public List<Reservation> readAll() {
         return List.copyOf(reservations);
+    }
+
+    @PostMapping("/reservations")
+    @ResponseBody
+    public ResponseEntity<Reservation> create(@RequestBody Reservation reservation) {
+        reservation.setId(index.incrementAndGet());
+        reservations.add(reservation);
+        return ResponseEntity.ok(reservation);
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> cancel(@PathVariable Long id) {
+        Reservation foundReservation = reservations.stream()
+                .filter(reservation -> reservation.getId().equals(id))
+                .findAny()
+                .orElseThrow();
+
+        reservations.remove(foundReservation);
+        return ResponseEntity.ok().build();
     }
 }

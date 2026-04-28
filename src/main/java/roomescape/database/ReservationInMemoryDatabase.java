790d77366af09900e94082d320a3709466804553
koreaioi <koreaioi@gachon.ac.kr>
2026-04-29 00:41:31 +0900

    feat(InMemoryDatabase): Insert 메서드 추가
    - autoIncrement 적용
    - API명세에 따라 Reservation 반환

--- src/main/java/roomescape/database/ReservationInMemoryDatabase.java
+++ src/main/java/roomescape/database/ReservationInMemoryDatabase.java
@@ -3,6 +3,8 @@ package roomescape.database;
 import org.springframework.stereotype.Component;
 import roomescape.Reservation;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -16,6 +18,12 @@ public class ReservationInMemoryDatabase {
 
     public List<Reservation> selectAll() {
         return Collections.unmodifiableList(reservations);
+    }
+
+    public Reservation insert(String name, LocalDate date, LocalTime time) {
+        Reservation reservation = new Reservation(autoIncrement.incrementAndGet(), name, date, time);
+        reservations.add(reservation);
+        return reservation;
     }
 
 }

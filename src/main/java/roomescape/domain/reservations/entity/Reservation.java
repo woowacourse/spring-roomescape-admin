660b472c976c3329ef6846575d6edc354edd1ae1
jyt6640 <jyt6640@gmail.com>
2026-04-28 16:04:18 +0900

    refactor: getter 및 id 필드 추가

--- src/main/java/roomescape/domain/reservations/entity/Reservation.java
+++ src/main/java/roomescape/domain/reservations/entity/Reservation.java
@@ -5,11 +5,13 @@ import java.time.LocalTime;
 
 public class Reservation {
 
+    private Long id;
     private String name;
     private LocalDate date;
     private LocalTime time;
 
-    public Reservation(String name, String date, String time) {
+    public Reservation(Long id, String name, String date, String time) {
+        this.id = id;
         this.name = name;
         this.date = parseLocalDate(date);
         this.time = parseTime(time);
@@ -21,5 +23,21 @@ public class Reservation {
 
     public LocalTime parseTime(String time) {
         return LocalTime.parse(time);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
     }
 }

6e8b8f3757bd7ce0342471f03efc36c57dff922a
kdongsu5509 <kod66170@gmail.com>
2026-04-28 17:57:38 +0900

    test : add test for id initiailze
    ReservationEntity 가 불변 객체임을 보장하는 테스트 추가

---
+++ src/test/java/roomescape/repository/ReservationEntityTest.java
@@ -0,0 +1,26 @@
+package roomescape.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ReservationEntityTest {
+
+    @Test
+    @DisplayName("ID를 설정하면 기존과는 다른 인스턴스를 반환한다")
+    void initializeId_return_another_instance() {
+        Long testId = 1L;
+        ReservationEntity noIdEntity = new ReservationEntity(
+                null,
+                "라티",
+                "2026-08-06",
+                "17:49"
+        );
+        int prevHashCode = noIdEntity.hashCode();
+
+        ReservationEntity withIdEntity = noIdEntity.initializeId(testId);
+        int afterHashCode = withIdEntity.hashCode();
+
+        Assertions.assertNotEquals(prevHashCode, afterHashCode);
+    }
+}

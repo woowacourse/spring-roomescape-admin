a511f6b7d08261f172ab6e27d78599db100437a4
symflee <symflee@gmail.com>
2026-04-28 21:29:03 +0900

    feat: 예약 삭제 기능 추가

--- src/main/java/roomescape/AdminController.java
+++ src/main/java/roomescape/AdminController.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,5 +40,18 @@ public class AdminController {
 
         return ResponseEntity.ok().body(reservation);
     }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findAny()
+                .orElseThrow();
+
+        reservations.remove(reservation);
+
+        return ResponseEntity.ok().build();
+    }
+
 
 }

8d9fbe3d2e00a9b32ede4b79f8e5c7e1ddba3a59
alstj2384 <ass91160@naver.com>
2026-04-28 21:18:32 +0900

    feat: 예약 삭제 기능 구현

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/reservations")
 public class ReservationController {
     private List<Reservation> reservations = new ArrayList<>();
-    private AtomicLong index = new AtomicLong(1);
+    private AtomicLong index = new AtomicLong(0);
 
     @GetMapping
     public ResponseEntity<List<Reservation>> get() {
@@ -28,6 +30,24 @@ public class ReservationController {
         String time = reservationCreateDto.getTime();
 
         reservations.add(new Reservation(index.incrementAndGet(), name, date, time));
-        return ResponseEntity.ok(reservations.get(index.intValue()));
+
+        Reservation create = reservations.stream()
+                .filter(reservation -> reservation.getId() == index.get())
+                .findAny()
+                .orElseThrow(() ->
+                        new IllegalArgumentException("요청한 번호를 찾을 수 없습니다."));
+
+        return ResponseEntity.ok(create);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Reservation delete = reservations.stream()
+                .filter(reservation -> reservation.getId() == id)
+                .findAny()
+                .orElseThrow(() ->
+                        new IllegalArgumentException("요청한 번호를 찾을 수 없습니다."));
+        reservations.remove(delete);
+        return ResponseEntity.ok().build();
     }
 }

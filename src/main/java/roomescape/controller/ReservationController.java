6226c47d237c6fec70a417c07dad011c773c47f3
Kim, Joon <kjoon418@naver.com>
2026-04-28 12:04:50 +0900

    feat: 예약 조회 기능 구현

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,23 @@
+package roomescape.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.Reservation;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+
+    private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(1);
+
+    @GetMapping
+    public ResponseEntity<List<Reservation>> findAll() {
+        return ResponseEntity.ok(reservations);
+    }
+}

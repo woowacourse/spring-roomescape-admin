56ae2fe3e6969f822d9206bd3a3efff71ddbc68f
symflee <symflee@gmail.com>
2026-04-28 20:49:53 +0900

    feat: 예약 조회 기능 추가

--- src/main/java/roomescape/AdminController.java
+++ src/main/java/roomescape/AdminController.java
@@ -2,9 +2,12 @@ package roomescape;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -21,4 +24,20 @@ public class AdminController {
 
         return ResponseEntity.ok(newReservation);
     }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> readAll() {
+        return ResponseEntity.ok().body(reservations);
+    }
+
+    @GetMapping("/reservations/{id}")
+    public ResponseEntity<Reservation> read(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findAny()
+                .orElseThrow();
+
+        return ResponseEntity.ok().body(reservation);
+    }
+
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -42,6 +42,30 @@ public class MissionStepTest {
     }
 
     @Test
+    void 예약_개별_조회() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("time", "15:40");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations/1")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", is(1))
+                .body("name", is("브라운"))
+                .body("date", is("2023-08-05"))
+                .body("time", is("15:40:00"));
+    }
+
+    @Test
     void 예약_추가_및_삭제() {
         Map<String, String> params = new HashMap<>();
         params.put("name", "브라운");

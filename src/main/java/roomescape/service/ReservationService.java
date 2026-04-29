d7b3051478216480cb68bd6e8f37c77be8ab8134
kdongsu5509 <kod66170@gmail.com>
2026-04-29 15:32:53 +0900

    fix : add missing annotation

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -2,10 +2,12 @@ package roomescape.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.domain.Reservation;
 import roomescape.repository.ReservationDao;
 
 @Service
+@Transactional
 public class ReservationService {
 
     private final ReservationDao reservationDao;

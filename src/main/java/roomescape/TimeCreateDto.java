77ece0c1e52366b409c3dd6a8cef63e08805fd3e
alstj2384 <ass91160@naver.com>
2026-04-29 20:50:09 +0900

    feat: 시간 관리 생성, 조회, 삭제 기능 추가

---
+++ src/main/java/roomescape/TimeCreateDto.java
@@ -0,0 +1,13 @@
+package roomescape;
+
+public class TimeCreateDto {
+    private final String startAt;
+
+    public TimeCreateDto(String startAt) {
+        this.startAt = startAt;
+    }
+
+    public String getStartAt() {
+        return startAt;
+    }
+}

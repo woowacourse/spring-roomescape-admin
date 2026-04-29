3d596b33fae0b9fd15c83d458fba8eefd5fa02db
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-29 21:55:11 +0900

    feat: 예약 도메인 객체 구현

---
+++ src/main/java/roomescape/Reservation.java
@@ -0,0 +1,46 @@
+package roomescape;
+
+public class Reservation {
+    private Long id;
+    private String name;
+    private String date;
+    private String time;
+
+    public Reservation() {
+
+    }
+
+    public Reservation(String name, String date, String time) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Reservation(Long id, String name, String date, String time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public static Reservation toEntity(Reservation reservation, Long id) {
+        return new Reservation(id, reservation.name, reservation.date, reservation.time);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+}

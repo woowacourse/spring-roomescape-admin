e8c7c491235adb06ab8ce4bd1104d577f2b2fbe5
2Jaeheon <jaeheon0826@jbnu.ac.kr>
2026-04-29 15:32:37 +0900

    test: 시간 검증 테스트에 비정상 파라미터 추가
    Signed-off-by: 2Jaeheon <jaeheon0826@jbnu.ac.kr>

--- src/test/java/roomescape/domain/ReservationTimeTest.java
+++ src/test/java/roomescape/domain/ReservationTimeTest.java
@@ -5,26 +5,22 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ReservationTimeTest {
-    @Test
-    @DisplayName("시간이 주어진 형식이 아닌 경우 예외를 발생한다.")
-    void throwException_When_IllegalTimeFormat() {
-        assertThatThrownBy(() -> new ReservationTime("9:00"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 
-    @Test
-    @DisplayName("시간 범위를 벗어나면 예외를 발생한다.")
-    void throwException_When_OutOfHourRange() {
-        assertThatThrownBy(() -> new ReservationTime("25:00"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("분 범위를 벗어나면 예외를 발생한다.")
-    void throwException_When_OutOfMinuteRange() {
-        assertThatThrownBy(() -> new ReservationTime("12:64"))
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "9:00",
+            "25:00",
+            "12:64",
+            "12-30",
+            "12:3",
+    })
+    @DisplayName("시간이 정상 형태가 아닌 경우 예외를 발생한다.")
+    void throwException_When_TimeIllegalFormat(String input) {
+        assertThatThrownBy(() -> new ReservationTime(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

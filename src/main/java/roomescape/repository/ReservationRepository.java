ba9ad4318312ae572f12ccc3b1fc3d28b0f21f14
armd482 <armd482@naver.com>
2026-04-29 21:24:44 +0900

    refactor: ReservationRepository add 시간정보 가져오는 방식으로 변경

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -2,17 +2,25 @@ package roomescape.repository;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 import roomescape.dao.ReservationDao;
+import roomescape.dao.ReservationTimeDao;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationCommand;
+import roomescape.domain.ReservationTime;
 
 @Repository
 public class ReservationRepository {
-    private final ReservationDao reservationDao;
+    private static final String INVALID_RESERVATION_TIME_ID = "유효하지 않은 시간 id입니다.";
 
-    public ReservationRepository(ReservationDao reservationDao) {
+    private final ReservationDao reservationDao;
+    private final ReservationTimeDao reservationTimeDao;
+
+    public ReservationRepository(ReservationDao reservationDao, ReservationTimeDao reservationTimeDao) {
         this.reservationDao = reservationDao;
+        this.reservationTimeDao = reservationTimeDao;
     }
 
     public List<Reservation> getAllReservation() {
@@ -20,9 +28,15 @@ public class ReservationRepository {
     }
 
     public Reservation addReservation(ReservationCommand reservationCommand) {
+        Optional<ReservationTime> optionalReservationTime = reservationTimeDao.getReservationTime(reservationCommand.timeId());
+
+        if(optionalReservationTime.isEmpty()) {
+            throw new NoSuchElementException(INVALID_RESERVATION_TIME_ID);
+        }
+
         long id = reservationDao.insertReservation(reservationCommand);
 
-        return new Reservation(id, reservationCommand.name(), reservationCommand.date(), null);
+        return new Reservation(id, reservationCommand.name(), reservationCommand.date(), optionalReservationTime.get());
     }
 
     public int deleteReservation(long id) {

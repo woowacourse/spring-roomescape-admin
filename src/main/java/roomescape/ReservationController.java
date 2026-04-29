c46259536736d4477f40eda51e001ab2e4bffd8e
toctoce <toctoce@naver.com>
2026-04-29 23:10:55 +0900

    feat(DB): DB 연동 구현

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,8 +1,6 @@
 package roomescape;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,23 +13,27 @@ import roomescape.domain.Reservation;
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
-    private List<Reservation> reservations = new ArrayList<>();
-    private AtomicLong index = new AtomicLong(1);
+
+    private final ReservationRepository reservationRepository;
+
+    public ReservationController(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
 
     @PostMapping
     public Reservation create(@RequestBody Reservation reservation) {
-        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
-        reservations.add(newReservation);
-        return newReservation;
+        System.out.println("reservation = " + reservation);
+        Long id = reservationRepository.insert(reservation);
+        return Reservation.toEntity(reservation, id);
     }
 
     @GetMapping
     public List<Reservation> read() {
-        return reservations;
+        return reservationRepository.findAllReservations();
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable long id) {
-        reservations.removeIf(reservation -> reservation.getId() == id);
+        reservationRepository.delete(id);
     }
 }
---
+++ src/main/java/roomescape/ReservationRepository.java
@@ -0,0 +1,58 @@
+package roomescape;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.Reservation;
+
+@Repository
+public class ReservationRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Long insert(Reservation reservation) {
+
+        String sql = "insert into reservation (name, date, time) values (?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    sql,
+                    new String[]{"id"});
+            ps.setString(1, reservation.getName());
+            ps.setString(2, reservation.getDate());
+            ps.setString(3, reservation.getTime());
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
+
+    public int delete(Long id) {
+        String sql = "delete from reservation where id = ?";
+        return jdbcTemplate.update(sql, Long.valueOf(id));
+    }
+
+    public List<Reservation> findAllReservations() {
+        String sql = "select id, name, date, time from reservation";
+        return jdbcTemplate.query(
+                sql,
+                (resultSet, rowNum) -> {
+                    Reservation reservation = new Reservation(
+                            resultSet.getLong("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("date"),
+                            resultSet.getString("time")
+                    );
+                    return reservation;
+                });
+    }
+
+}

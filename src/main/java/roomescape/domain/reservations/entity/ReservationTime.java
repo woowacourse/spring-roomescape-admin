eb4fd3b8a1714b0bf3bb6b01a32c3e01c0e40f66
jyt6640 <jyt6640@gmail.com>
2026-04-29 16:42:11 +0900

    feat: 예약 시간 관리를 위한 엔티티 추가

---
+++ src/main/java/roomescape/domain/reservations/entity/ReservationTime.java
@@ -0,0 +1,26 @@
+package roomescape.domain.reservations.entity;
+
+import java.time.LocalTime;
+
+public class ReservationTime {
+
+    private Long id;
+    private LocalTime startAt;
+
+    private ReservationTime(Long id, LocalTime startAt) {
+        this.id = id;
+        this.startAt = startAt;
+    }
+
+    public static ReservationTime of(Long id, LocalTime startAt) {
+        return new ReservationTime(id, startAt);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalTime getStartAt() {
+        return startAt;
+    }
+}

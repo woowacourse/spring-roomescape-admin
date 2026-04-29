cc41a9a5eac63a50f5277cc367651dbc44b69a7b
koreaioi <koreaioi@gachon.ac.kr>
2026-04-29 16:55:05 +0900

    feat: ReservationDao를 Interface로 추상화 #step2
    - 강결합을 완화하기 위함.

---
+++ src/main/java/roomescape/dao/InMemoryReservationDao.java
@@ -0,0 +1,37 @@
+package roomescape.dao;
+
+import org.springframework.stereotype.Repository;
+import roomescape.Reservation;
+import roomescape.database.ReservationInMemoryDatabase;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Repository
+public class InMemoryReservationDao implements ReservationDao{
+
+    private final ReservationInMemoryDatabase inMemoryDatabase;
+
+    public InMemoryReservationDao(ReservationInMemoryDatabase inMemoryDatabase) {
+        this.inMemoryDatabase = inMemoryDatabase;
+    }
+
+    public Reservation select(Long id) {
+        return inMemoryDatabase.select(id)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public List<Reservation> selectAll() {
+        return inMemoryDatabase.selectAll();
+    }
+
+    public Reservation insert(String name, LocalDate date, LocalTime time) {
+        return inMemoryDatabase.insert(name, date, time);
+    }
+
+    public void delete(Long id) {
+        inMemoryDatabase.delete(select(id));
+    }
+
+}
--- src/main/java/roomescape/dao/ReservationDao.java
+++ src/main/java/roomescape/dao/ReservationDao.java
@@ -1,37 +1,19 @@
 package roomescape.dao;
 
-import org.springframework.stereotype.Repository;
 import roomescape.Reservation;
-import roomescape.database.ReservationInMemoryDatabase;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
-@Repository
-public class ReservationDao {
+public interface ReservationDao {
 
-    private final ReservationInMemoryDatabase inMemoryDatabase;
+    Reservation select(Long id);
 
-    public ReservationDao(ReservationInMemoryDatabase inMemoryDatabase) {
-        this.inMemoryDatabase = inMemoryDatabase;
-    }
+    List<Reservation> selectAll();
 
-    public Reservation select(Long id) {
-        return inMemoryDatabase.select(id)
-                .orElseThrow(IllegalArgumentException::new);
-    }
+    Reservation insert(String name, LocalDate date, LocalTime time);
 
-    public List<Reservation> selectAll() {
-        return inMemoryDatabase.selectAll();
-    }
-
-    public Reservation insert(String name, LocalDate date, LocalTime time) {
-        return inMemoryDatabase.insert(name, date, time);
-    }
-
-    public void delete(Long id) {
-        inMemoryDatabase.delete(select(id));
-    }
+    void delete(Long id);
 
 }

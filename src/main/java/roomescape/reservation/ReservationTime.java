2674de0bc04e53f27c5c092b79f9527d12e36ad6
kangrae <kangrae0501@gmail.com>
2026-04-29 15:07:40 +0900

    feat: 예약 시간 객체 및 스키마 정의

---
+++ src/main/java/roomescape/reservation/ReservationTime.java
@@ -0,0 +1,15 @@
+package roomescape.reservation;
+
+import java.time.LocalTime;
+
+public class ReservationTime {
+
+    private final Long id;
+    private final LocalTime startAt;
+
+    public ReservationTime(Long id, LocalTime startAt) {
+        this.id = id;
+        this.startAt = startAt;
+    }
+    
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,11 @@ CREATE TABLE IF NOT EXISTS reservation
     time TIME         NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS reservation_time
+(
+    id       BIGINT NOT NULL AUTO_INCREMENT,
+    start_at TIME   NOT NULL,
+    PRIMARY KEY (id)
+);
+

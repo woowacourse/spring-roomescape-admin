6fcd78531d56db76033a1fbb0b391526f67cce28
choubung <choubung@sookmyung.ac.kr>
2026-04-29 16:06:41 +0900

    chore: 스키마 변경에 따른 sql 문서 업데이트

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -3,13 +3,12 @@ package roomescape.domain;
 import roomescape.domain.vo.Name;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 
 public class Reservation {
     private final Long id;
     private final Name name;
     private final LocalDate date;
-    private final LocalTime time;
+    private final ReservationTime time;
 
     public Reservation() {
         this.id = null;
@@ -18,23 +17,17 @@ public class Reservation {
         this.time = null;
     }
 
-    public Reservation(Long id, String name, String date, String time) {
+    public Reservation(Long id, String name, String date, ReservationTime time) {
         this.id = id;
         this.name = new Name(name);
         this.date = translateDate(date);
-        this.time = translateTime(time);
+        this.time = time;
     }
 
     private LocalDate translateDate(String date) {
         // TODO : 검증 로직
 
         return LocalDate.parse(date);
-    }
-
-    private LocalTime translateTime(String time) {
-        // TODO : 검증 로직
-
-        return LocalTime.parse(time);
     }
 
     public Long getId() {
@@ -49,7 +42,7 @@ public class Reservation {
         return date;
     }
 
-    public LocalTime getTime() {
+    public ReservationTime getTime() {
         return time;
     }
 }

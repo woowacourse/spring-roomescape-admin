0854ca559ea46c8f8abe388b5660aae458a11333
koreaioi <koreaioi@gachon.ac.kr>
2026-04-30 02:08:20 +0900

    feat(ReservationTime): ReservationTime 추가 #step3

---
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -0,0 +1,22 @@
+package roomescape.domain;
+
+import java.time.LocalTime;
+
+public class ReservationTime {
+
+    private Long id;
+    private LocalTime startAt;
+
+    public ReservationTime(LocalTime startAt) {
+        this.startAt = startAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalTime getStartAt() {
+        return startAt;
+    }
+
+}

1940dbbc76f9808ad38727026f4bfff082ac8d47
haeyoon1 <hynam1220@naver.com>
2026-04-29 22:30:11 +0900

    refactor: controller 어노테이션 변경

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -2,17 +2,17 @@ package roomescape.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.TimeRequest;
 import roomescape.dto.TimeResponse;
 import roomescape.service.ReservationTimeService;
 
-@Controller
+@RestController
 public class ReservationTimeController {
 
     private final ReservationTimeService reservationTimeService;

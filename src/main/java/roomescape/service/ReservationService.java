e8290530e3b4dfeb5f9aa0e0255299792d19358e
Kim, Joon <kjoon418@naver.com>
2026-04-29 21:32:28 +0900

    style: 부적절한 개행 수정

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,48 +1,48 @@
-    package roomescape.service;
+package roomescape.service;
 
-    import java.util.List;
-    import org.springframework.stereotype.Service;
-    import org.springframework.transaction.annotation.Transactional;
-    import roomescape.domain.Reservation;
-    import roomescape.domain.ReservationTime;
-    import roomescape.repository.ReservationRepository;
-    import roomescape.service.command.ReservationCreateCommand;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import roomescape.domain.Reservation;
+import roomescape.domain.ReservationTime;
+import roomescape.repository.ReservationRepository;
+import roomescape.service.command.ReservationCreateCommand;
 
-    @Service
-    public class ReservationService {
+@Service
+public class ReservationService {
 
-        private final ReservationRepository reservationRepository;
-        private final ReservationTimeService timeService;
+    private final ReservationRepository reservationRepository;
+    private final ReservationTimeService timeService;
 
-        public ReservationService(
-                ReservationRepository reservationRepository,
-                ReservationTimeService timeService
-        ) {
-            this.reservationRepository = reservationRepository;
-            this.timeService = timeService;
-        }
-
-        @Transactional
-        public Reservation create(
-                ReservationCreateCommand createCommand
-        ) {
-            ReservationTime time = timeService.findById(createCommand.timeId());
-            Reservation reservation = Reservation.create(
-                    createCommand.name(),
-                    createCommand.date(),
-                    time
-            );
-
-            return reservationRepository.create(reservation);
-        }
-
-        @Transactional
-        public List<Reservation> findAll() {
-            return reservationRepository.findAll();
-        }
-
-        @Transactional
-        public void delete(long id) {
-            reservationRepository.delete(id);
-        }
+    public ReservationService(
+            ReservationRepository reservationRepository,
+            ReservationTimeService timeService
+    ) {
+        this.reservationRepository = reservationRepository;
+        this.timeService = timeService;
     }
+
+    @Transactional
+    public Reservation create(
+            ReservationCreateCommand createCommand
+    ) {
+        ReservationTime time = timeService.findById(createCommand.timeId());
+        Reservation reservation = Reservation.create(
+                createCommand.name(),
+                createCommand.date(),
+                time
+        );
+
+        return reservationRepository.create(reservation);
+    }
+
+    @Transactional
+    public List<Reservation> findAll() {
+        return reservationRepository.findAll();
+    }
+
+    @Transactional
+    public void delete(long id) {
+        reservationRepository.delete(id);
+    }
+}

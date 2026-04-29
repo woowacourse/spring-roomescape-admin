aa0f78e5f16f84f1361a0fdf2035227f2f668657
jyt6640 <jyt6640@gmail.com>
2026-04-29 14:48:06 +0900

    refactor: SimpleJdbcInsert를 생성자 주입으로 변경

--- src/main/java/roomescape/domain/reservations/infrastructure/ReservationJdbcTemplateRepository.java
+++ src/main/java/roomescape/domain/reservations/infrastructure/ReservationJdbcTemplateRepository.java
@@ -1,15 +1,11 @@
 package roomescape.domain.reservations.infrastructure;
 
-import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.reservations.entity.Reservation;
 
@@ -17,56 +13,23 @@ import roomescape.domain.reservations.entity.Reservation;
 public class ReservationJdbcTemplateRepository implements ReservationRepository {
 
     private final JdbcTemplate jdbcTemplate;
-    private final DataSource dataSource;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
     public ReservationJdbcTemplateRepository(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.dataSource = dataSource;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("reservation")
+                .usingGeneratedKeyColumns("id");
     }
-
-//    @Override
-//    public Reservation save(Reservation reservation) {
-//        KeyHolder keyHolder = new GeneratedKeyHolder();
-//
-//        jdbcTemplate.update(connection -> {
-//            PreparedStatement ps = connection.prepareStatement(
-//                    "INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)",
-//                    Statement.RETURN_GENERATED_KEYS
-//            );
-//
-//            ps.setString(1, reservation.getName());
-//            ps.setString(2, reservation.getDate().toString());
-//            ps.setString(3, reservation.getTime().toString());
-//
-//            return ps;
-//        }, keyHolder);
-//
-//        Number key = keyHolder.getKey();
-//        if (key == null) {
-//            throw new IllegalStateException("ID 생성 실패");
-//        }
-//
-//        Long id = key.longValue();
-//
-//        return Reservation.of(
-//                id,
-//                reservation.getName(),
-//                reservation.getDate(),
-//                reservation.getTime()
-//        );
-//    }
 
     @Override
     public Reservation save(Reservation reservation) {
-        SimpleJdbcInsert insert = new SimpleJdbcInsert(dataSource)
-                .withTableName("reservation")
-                .usingGeneratedKeyColumns("id");
         Map<String, Object> params = Map.of(
                 "name", reservation.getName(),
                 "date", reservation.getDate().toString(),
                 "time", reservation.getTime().toString()
         );
-        Long id = insert.executeAndReturnKey(params).longValue();
+        Long id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
         return Reservation.of(
                 id,
                 reservation.getName(),

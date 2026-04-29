a87a4c917a00674440e5d5445fe16c5e84bc6820
Yoonhojoon <dbsghwns1209@gmail.com>
2026-04-29 15:28:57 +0900

    feat: schema.sql 수정 및 ReservationTime 엔티티 추가

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -4,13 +4,13 @@ public class Reservation {
     private final Long id;
     private final String name;
     private final String date;
-    private final String time;
+    private final ReservationTime time;
 
-    public Reservation(String name, String date, String time) {
+    public Reservation(String name, String date, ReservationTime time) {
         this(null, name, date, time);
     }
 
-    private Reservation(Long id, String name, String date, String time) {
+    private Reservation(Long id, String name, String date, ReservationTime time) {
         this.id = id;
         this.name = name;
         this.date = date;
@@ -33,7 +33,7 @@ public class Reservation {
         return date;
     }
 
-    public String getTime() {
+    public ReservationTime getTime() {
         return time;
     }
 }

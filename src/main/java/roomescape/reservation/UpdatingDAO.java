b207b6a49e46e39e4c3cbc31dc827d2910ffab61
haeseon lee <haeseon4816@gmail.com>
2026-04-30 00:24:54 +0900

    refactor: JdbcTemplate 필드 final로 변경

--- src/main/java/roomescape/reservation/UpdatingDAO.java
+++ src/main/java/roomescape/reservation/UpdatingDAO.java
@@ -10,7 +10,7 @@ import java.sql.PreparedStatement;
 @Repository
 public class UpdatingDAO {
 
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     public UpdatingDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;

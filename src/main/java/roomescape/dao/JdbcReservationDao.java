be3d30edce71327f249ec41df5adf60d41ac851c
koreaioi <koreaioi@gachon.ac.kr>
2026-04-29 22:16:23 +0900

    feat(JdbcReservationDao): delete 구현 #step2

--- src/main/java/roomescape/dao/JdbcReservationDao.java
+++ src/main/java/roomescape/dao/JdbcReservationDao.java
@@ -56,7 +56,8 @@ public class JdbcReservationDao implements ReservationDao{
 
     @Override
     public void delete(Long id) {
-
+        String sql = "delete from reservation where id = ?";
+        jdbcTemplate.update(sql, id);
     }
 
 }

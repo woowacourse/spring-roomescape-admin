f493cbae121a6a06e822e6cd4cb81b526aff1494
snowshower <wonsb25@gmail.com>
2026-04-29 23:02:46 +0900

    feat: 시간 조회, 추가, 삭제 dao 추가
    Signed-off-by: snowshower <wonsb25@gmail.com>

---
+++ src/main/java/roomescape/ReservationTimeDAO.java
@@ -0,0 +1,70 @@
+package roomescape;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Time;
+import java.util.List;
+import java.util.Objects;
+
+@Repository
+public class ReservationTimeDAO {
+    private JdbcTemplate jdbcTemplate;
+
+    public ReservationTimeDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<ReservationTime> rowMapper = (resultSet, rowNum) -> {
+        ReservationTime reservationTime = new ReservationTime(
+                resultSet.getLong("id"),
+                resultSet.getTime("start_at").toLocalTime()
+        );
+        return reservationTime;
+    };
+
+    public int count() {
+        String sql = "SELECT COUNT(*) FROM reservation_time";
+        return jdbcTemplate.queryForObject(sql, Integer.class);
+    }
+
+    public ReservationTime findReservationTimeById(Long id) {
+        String sql = "SELECT id, start_at FROM reservation_time WHERE  id=?";
+        return jdbcTemplate.queryForObject(sql, rowMapper, id);
+    }
+
+    public List<ReservationTime> findAllReservationTime() {
+        String sql = "SELECT id, start_at FROM reservation_time";
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public void insert(ReservationTime reservationTime) {
+        String sql = "INSERT INTO reservation_time (start_at) VALUES (?)";
+        jdbcTemplate.update(sql, reservationTime.getStartAt());
+    }
+
+    public Long insertWithKeyHolder(ReservationTime reservationTime) {
+        String sql = "INSERT INTO reservation_time (start_at) VALUES (?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    sql, new String[]{"id"});
+            preparedStatement.setTime(1, Time.valueOf(reservationTime.getStartAt()));
+            return preparedStatement;
+        }, keyHolder);
+
+        return Objects.requireNonNull(keyHolder.getKey()).longValue();
+    }
+
+    public int delete(Long id) {
+        String sql = "DELETE FROM reservation_time WHERE id=?";
+        return jdbcTemplate.update(sql, Long.valueOf(id));
+    }
+
+
+}

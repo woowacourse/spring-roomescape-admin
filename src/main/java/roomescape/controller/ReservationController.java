d2bab196ee937c3596e438fdc37261f99f46cc5b
JYL35 <154980427+JYL35@users.noreply.github.com>
2026-04-29 23:45:28 +0900

    feat: 예약 시간 CRUD API 구현 (인메모리)
    - Update 제외 Create, Read, Delete API 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -5,13 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import roomescape.dto.ReservationCreateRequest;
 import roomescape.dto.ReservationResponse;
+import roomescape.dto.ReservationTimeCreateRequest;
+import roomescape.dto.ReservationTimeResponse;
+import roomescape.entity.ReservationTime;
 import roomescape.service.ReservationService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ReservationController {
     private final ReservationService reservationService;
+    private List<ReservationTime> reservationTimes = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
 
     public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
@@ -32,5 +40,27 @@ public class ReservationController {
         reservationService.deleteReservation(id);
         return ResponseEntity.ok()
                 .build();
+    }
+
+    @PostMapping("/times")
+    public ResponseEntity<ReservationTimeResponse> createReservationTime(@RequestBody ReservationTimeCreateRequest request) {
+        ReservationTime reservationTime = request.toEntity(index.getAndIncrement());
+        reservationTimes.add(reservationTime);
+        return ResponseEntity.ok(ReservationTimeResponse.fromEntity(reservationTime));
+    }
+
+    @GetMapping("/times")
+    public ResponseEntity<List<ReservationTimeResponse>> readAllReservationTime() {
+        return ResponseEntity.ok(ReservationTimeResponse.fromEntities(reservationTimes));
+    }
+
+    @DeleteMapping("/times/{id}")
+    public ResponseEntity<Void> deleteReservationTime(@PathVariable Long id) {
+        ReservationTime reservationTime = reservationTimes.stream()
+                .filter(it -> Objects.equals(it.id(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+        reservationTimes.remove(reservationTime);
+        return ResponseEntity.ok().build();
     }
 }

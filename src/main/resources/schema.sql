bfeee0c4cb4cf3aacda07d009249a59df599ebe6
JinGoo <zzgg3040@naver.com>
2026-04-29 21:17:19 +0900

    refactor: 정렬 및 스키마 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,15 @@
-CREATE TABLE reservation_time
-(
-    id   BIGINT NOT NULL AUTO_INCREMENT,
-    start_at TIME   NOT NULL,
+CREATE TABLE reservation_time(
+    id          BIGINT NOT NULL AUTO_INCREMENT,
+    start_at    TIME   NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation
-(
+CREATE TABLE reservation(
     id                  BIGINT       NOT NULL AUTO_INCREMENT,
     name                VARCHAR(255) NOT NULL,
     date                DATE         NOT NULL,
     time                TIME         NOT NULL,
-    reservation_time_id BIGINT,
+    time_id BIGINT,
     PRIMARY KEY (id),
-    CONSTRAINT fk_reservation_time
-        FOREIGN KEY (reservation_time_id)
-            REFERENCES reservation_time (id)
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
 );

03fc988cc18fc0e834bda4f0a0161cade1317ada
Chocoding1 <galmeagi2@naver.com>
2026-04-29 19:08:37 +0900

    refactor: 테스트 케이스에 맞춰 필드명 수정

--- src/main/java/roomescape/ReservationSaveDto.java
+++ src/main/java/roomescape/ReservationSaveDto.java
@@ -3,5 +3,5 @@ package roomescape;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public record ReservationSaveDto(String name, LocalDate reservationDate, LocalTime reservationTime) {
+public record ReservationSaveDto(String name, LocalDate date, LocalTime time) {
 }

d37f92d2d8fc90d637c0785b985e6d8a1ff1cc00
alstj2384 <ass91160@naver.com>
2026-04-29 22:49:21 +0900

    refactor(Reservation): Controller와 Repository 계층 분리

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,11 +1,7 @@
 package roomescape;
 
-import java.sql.PreparedStatement;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,69 +13,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
-    private final JdbcTemplate jdbcTemplate;
     private final TimeRepository timeRepository;
+    private final ReservationRepository reservationRepository;
 
-//    public ReservationController(JdbcTemplate jdbcTemplate) {
-//        this.jdbcTemplate = jdbcTemplate;
-//    }
-
-    public ReservationController(JdbcTemplate jdbcTemplate, TimeRepository timeRepository) {
-        this.jdbcTemplate = jdbcTemplate;
+    public ReservationController(TimeRepository timeRepository, ReservationRepository reservationRepository) {
         this.timeRepository = timeRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     @GetMapping
-    public ResponseEntity<List<Reservation>> get() {
-        String sql = "select r.id as reservation_id, r.name, r.date, rt.id as time_id, rt.start_at from reservation r inner join reservation_time rt on r.time_id = rt.id";
-        List<Reservation> find = jdbcTemplate.query(
-                sql,
-                (resultSet, rowNum) -> new Reservation(
-                        resultSet.getLong("reservation_id"),
-                        resultSet.getString("name"),
-                        resultSet.getString("date"),
-                        new ReservationTime(resultSet.getLong("time_id"), resultSet.getString("start_at"))));
+    public ResponseEntity<List<Reservation>> findAll() {
+        List<Reservation> find = reservationRepository.findAll();
         return ResponseEntity.ok(find);
     }
 
     @PostMapping
     public ResponseEntity<Reservation> create(@RequestBody ReservationCreateDto reservationCreateDto) {
-
         ReservationTime find = timeRepository.findById(reservationCreateDto.getTimeId())
                 .orElseThrow(() -> new IllegalArgumentException("조회된 타임 슬롯이 없습니다."));
 
-        String sql = "insert into reservation(name, date, time_id) values (?, ?, ?)";
-
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            PreparedStatement pstmt = connection.prepareStatement(
-                    sql,
-                    new String[]{"id"});
-
-            pstmt.setString(1, reservationCreateDto.getName());
-            pstmt.setString(2, reservationCreateDto.getDate());
-            pstmt.setLong(3, find.getId());
-            return pstmt;
-        }, keyHolder);
-
-        Reservation reservation = jdbcTemplate.queryForObject(
-                "select id, name, date, time_id from reservation where id = ?",
-                (resultSet, rowNum) -> {
-                    return new Reservation(
-                            resultSet.getLong("id"),
-                            resultSet.getString("name"),
-                            resultSet.getString("date"),
-                            new ReservationTime(find.getId(), find.getStartAt()));
-                },
-                keyHolder.getKey().longValue());
+        Reservation reservation = reservationRepository.save(reservationCreateDto, find);
         return ResponseEntity.ok(reservation);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
+        reservationRepository.deleteById(id);
 
-        String sql = "delete from reservation where id = ?";
-        jdbcTemplate.update(sql, id);
         return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/roomescape/ReservationRepository.java
@@ -0,0 +1,50 @@
+package roomescape;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReservationRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Reservation> findAll() {
+        String sql = "select r.id as reservation_id, r.name, r.date, rt.id as time_id, rt.start_at from reservation r inner join reservation_time rt on r.time_id = rt.id";
+        return jdbcTemplate.query(
+                sql,
+                (resultSet, rowNum) -> new Reservation(
+                        resultSet.getLong("reservation_id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("date"),
+                        new ReservationTime(resultSet.getLong("time_id"), resultSet.getString("start_at"))));
+    }
+
+    public Reservation save(ReservationCreateDto dto, ReservationTime find) {
+        String sql = "insert into reservation(name, date, time_id) values (?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement pstmt = connection.prepareStatement(
+                    sql,
+                    new String[]{"id"});
+
+            pstmt.setString(1, dto.getName());
+            pstmt.setString(2, dto.getDate());
+            pstmt.setLong(3, find.getId());
+            return pstmt;
+        }, keyHolder);
+        return new Reservation(keyHolder.getKey().longValue(), dto.getName(), dto.getDate(), find);
+    }
+
+    public void deleteById(Long id) {
+        String sql = "delete from reservation where id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+}

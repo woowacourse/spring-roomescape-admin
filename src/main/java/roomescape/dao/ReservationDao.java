bcc63a8e8070d5948263ea8e4d5d77f8e9befc27
armd482 <armd482@naver.com>
2026-04-29 15:38:35 +0900

    feat: ReservationDao 구현

---
+++ src/main/java/roomescape/dao/ReservationDao.java
@@ -0,0 +1,65 @@
+package roomescape.dao;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.Reservation;
+
+@Repository
+public class ReservationDao {
+    private static final RowMapper<Reservation> MAPPER = (rs, rowNumber) -> new Reservation(
+            rs.getLong("id"),
+            rs.getString("name"),
+            rs.getString("date"),
+            rs.getString("time")
+    );
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public Optional<Reservation> getReservation(long id) {
+        String sql = "SELECT id, name, date, time FROM reservation WHERE id = ?";
+        return jdbcTemplate.query(sql, MAPPER, id).stream()
+                .findFirst();
+    }
+
+    public List<Reservation> getAllReservation() {
+        String sql = "SELECT id, name, date, time FROM reservation";
+        return jdbcTemplate.query(sql, MAPPER);
+    }
+
+    public Reservation insertReservation(Reservation reservation) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        String sql = "INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)";
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement statement = connection.prepareStatement(sql, new String[] { "id" });
+            statement.setString(1, reservation.name());
+            statement.setString(2, reservation.date());
+            statement.setString(3, reservation.time());
+            return statement;
+        }, keyHolder);
+
+        Number key = keyHolder.getKey();
+
+        if(key == null) {
+            throw new RuntimeException("ID 생성에 실패하였습니다.");
+        }
+
+        return reservation.update(key.longValue());
+    }
+
+    public void deleteReservation(long id) {
+        String sql = "DELETE FROM reservation WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+}
--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,4 +1,7 @@
 package roomescape.domain;
 
 public record Reservation(long id, String name, String date, String time) {
+    public Reservation update(long id) {
+        return new Reservation(id, name, date, time);
+    }
 }

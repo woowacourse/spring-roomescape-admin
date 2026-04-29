a9b9b3d4a6178b99c130d34ca11f92b56f1d4af1
dahxxn <dahyeonhan4529@gmail.com>
2026-04-29 09:12:01 +0900

    feat(repository): ReservationRepository 조회/추가/삭제/아이디생성 구현

---
+++ src/main/java/roomescape/reservation/repository/ReservationRepository.java
@@ -0,0 +1,44 @@
+package roomescape.reservation.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Repository;
+import roomescape.reservation.domain.Reservation;
+
+@Repository
+public class ReservationRepository {
+    private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(0);
+
+    private static final long DELTA = 1;
+
+    public List<Reservation> findAll() {
+        return new ArrayList<>(reservations);
+    }
+
+    public Optional<Reservation> findById(Long id) {
+        return reservations.stream()
+                .filter(reservations -> reservations.getId().equals(id)).findAny();
+    }
+
+    public Reservation save(Reservation reservation) {
+        if (!findById(reservation.getId()).isEmpty()) {
+            throw new IllegalStateException("중복된 예약 id가 존재합니다.");
+        }
+        reservations.add(reservation);
+        return reservation;
+    }
+
+    public void delete(Long id) {
+        Reservation reservation = findById(id)
+                .orElseThrow(() -> new IllegalStateException("존재하지 않는 예약입니다."));
+
+        reservations.remove(reservation);
+    }
+
+    public Long generateId() {
+        return index.addAndGet(DELTA);
+    }
+}

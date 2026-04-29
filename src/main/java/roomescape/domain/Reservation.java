a3b421d2a1ffa66b429e7c7980cdb8953953059b
minzzun99 <skg06123@naver.com>
2026-04-29 21:19:20 +0900

    refactor: Reservation 필드 final 추가 및 getter 수정, 패키지 이동

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,25 +1,18 @@
-package roomescape;
+package roomescape.domain;
 
 import java.time.LocalDate;
 
 public class Reservation {
-    private Long id;
-    private String name;
-    private LocalDate date;
-    private ReservationTime time;
-
-    private Reservation() {
-    }
+    private final Long id;
+    private final String name;
+    private final LocalDate date;
+    private final ReservationTime time;
 
     public Reservation(Long id, String name, LocalDate date, ReservationTime time) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
-    }
-
-    public static Reservation toEntity(Reservation reservation, Long id) {
-        return new Reservation(id, reservation.name, reservation.date, reservation.time);
     }
 
     public Long getId() {
@@ -30,8 +23,8 @@ public class Reservation {
         return name;
     }
 
-    public String getDate() {
-        return date.toString();
+    public LocalDate getDate() {
+        return date;
     }
 
     public ReservationTime getTime() {

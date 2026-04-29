569aee14084af9eafbfe71324e3afaf95a65b537
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-04-29 15:04:36 +0900

    feat: 2단계 요구사항 구현

--- src/main/java/roomescape/reservation/ReservationController.java
+++ src/main/java/roomescape/reservation/ReservationController.java
@@ -1,6 +1,9 @@
 package roomescape.reservation;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,31 +14,54 @@ import org.springframework.web.bind.annotation.RestController;
 import roomescape.reservation.dto.ReservationRequestDto;
 import roomescape.reservation.dto.ReservationResponseDto;
 
-import java.util.ArrayList;
+import java.sql.PreparedStatement;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
-    private final List<Reservation> reservations = new ArrayList<>();
-    private AtomicLong id = new AtomicLong(1);
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @GetMapping
-    public ResponseEntity<List<Reservation>> getReservations() {
-        return ResponseEntity.ok(List.copyOf(this.reservations));
+    public ResponseEntity<List<ReservationResponseDto>> getReservations() {
+        List<ReservationResponseDto> reservations =
+                jdbcTemplate.query("select * from reservation",
+                        (resultSet, rowNum) -> ReservationResponseDto.from(Reservation.of(
+                                resultSet.getLong("id"),
+                                resultSet.getString("name"),
+                                resultSet.getString("date"),
+                                resultSet.getString("time")
+                        ))
+                );
+        return ResponseEntity.ok(reservations);
     }
 
     @PostMapping
     public ResponseEntity<ReservationResponseDto> createReservation(@RequestBody ReservationRequestDto request) {
-        Reservation newReservation = request.toEntity(id.getAndIncrement());
-        this.reservations.add(newReservation);
-        return ResponseEntity.ok().body(ReservationResponseDto.from(newReservation));
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    "insert into reservation(name, date, time) values (?, ?, ?)",
+                    new String[]{"id"}
+            );
+            ps.setString(1, request.name());
+            ps.setString(2, request.date());
+            ps.setString(3, request.time());
+            return ps;
+        }, keyHolder);
+
+        Long id = keyHolder.getKey().longValue();
+        return ResponseEntity.ok().body(ReservationResponseDto.from(Reservation.of(id, request.name(), request.date(), request.time())));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
-        this.reservations.removeIf(reservation -> reservation.getId().equals(id));
+        jdbcTemplate.update("delete from reservation where id = ?", id);
         return ResponseEntity.ok().build();
     }
 }

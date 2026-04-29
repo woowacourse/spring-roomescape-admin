932aadc515479192d375563bbb9437c2ccf4eb6d
Chocoding1 <galmeagi2@naver.com>
2026-04-29 20:58:33 +0900

    feat: ReservationTime 객체 생성

---
+++ src/main/java/roomescape/ReservationTime.java
@@ -0,0 +1,14 @@
+package roomescape;
+
+import java.time.LocalTime;
+
+public class ReservationTime {
+
+    private final Long id;
+    private final LocalTime startAt;
+
+    public ReservationTime(Long id, LocalTime startAt) {
+        this.id = id;
+        this.startAt = startAt;
+    }
+}

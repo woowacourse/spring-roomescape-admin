4bc140e471280e3829b32a18cb34900760304faa
sinequanonn <dleck288@gmail.com>
2026-04-30 02:58:18 +0900

    refactor: Repository 애노테이션으로 수정

--- src/main/java/roomescape/step4/repository/ReservationTimeRepository.java
+++ src/main/java/roomescape/step4/repository/ReservationTimeRepository.java
@@ -3,14 +3,14 @@ package roomescape.step4.repository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import roomescape.step4.domain.ReservationTime;
 
 import java.sql.PreparedStatement;
 import java.time.LocalTime;
 import java.util.List;
 
-@Component
+@Repository
 public class ReservationTimeRepository {
 
     private final JdbcTemplate jdbcTemplate;

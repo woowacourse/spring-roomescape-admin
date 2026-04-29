bda4dfa4f321c32a83a8b2ebcfdfb5738c8517c2
alstj2384 <ass91160@naver.com>
2026-04-29 16:54:56 +0900

    refactor: 예약 조회 기능을 DB를 이용하도록 변경

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +16,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     private List<Reservation> reservations = new ArrayList<>();
     private AtomicLong index = new AtomicLong(0);
 
     @GetMapping
     public ResponseEntity<List<Reservation>> get() {
-        return ResponseEntity.ok(new ArrayList<>(reservations));
+        String sql = "select id, name, date, time from reservation";
+        List<Reservation> find = jdbcTemplate.query(
+                sql,
+                (resultSet, rowNum) -> {
+                    return new Reservation(
+                            resultSet.getLong("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("date"),
+                            resultSet.getString("time"));
+                }
+        );
+        return ResponseEntity.ok(find);
     }
 
     @PostMapping

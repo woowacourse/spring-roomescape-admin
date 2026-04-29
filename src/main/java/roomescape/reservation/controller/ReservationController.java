0992ca67650730570dd92d16272230da48da42a0
dahxxn <dahyeonhan4529@gmail.com>
2026-04-29 15:18:35 +0900

    feat(controller): ReservationController 예약 조회/추가/삭제 API 구현

---
+++ src/main/java/roomescape/reservation/controller/ReservationController.java
@@ -0,0 +1,37 @@
+package roomescape.reservation.controller;
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import roomescape.reservation.dto.CreateReservationRequest;
+import roomescape.reservation.dto.ReservationResponse;
+import roomescape.reservation.service.ReservationService;
+
+@Controller
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<ReservationResponse>> read() {
+        return ResponseEntity.ok(reservationService.findAll());
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<ReservationResponse> create(@RequestBody CreateReservationRequest createReservationRequest) {
+        return ResponseEntity.ok(reservationService.create(createReservationRequest));
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<ReservationResponse> delete(@PathVariable Long id) {
+        return ResponseEntity.ok(reservationService.delete(id));
+    }
+}

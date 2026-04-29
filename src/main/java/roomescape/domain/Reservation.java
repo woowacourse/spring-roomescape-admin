967cafa1bd5514972afc8b7e13aa6ddeec7feeb4
나의찬 <cab07036@naver.com>
2026-04-29 16:57:59 +0900

    fix(Reservation): Json 역직렬화 오류 문제 해결
    Json이 역직렬화 하는 과정에서 기본 생성자가 없어서 Reservation 객체로 생성이 안되는 문제가 있었습니다.
    
    이를 해결하기 위해 JsonCreator와 JsonProperty 어노테이션으로 해결하였습니다.

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,25 +1,34 @@
 package roomescape.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Reservation {
 
     private final Long id;
-    private final String name;
+    private final String memberName;
     private final LocalDate date;
     private final LocalTime time;
 
-    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+    @JsonCreator
+    public Reservation(
+            @JsonProperty("id") Long id,
+            @JsonProperty("name") String memberName,
+            @JsonProperty("date") LocalDate date,
+            @JsonProperty("time") LocalTime time
+    ) {
         this.id = id;
-        this.name = name;
+        this.memberName = memberName;
         this.date = date;
         this.time = time;
     }
 
-    public Reservation(String name, LocalDate date, LocalTime time) {
+    public Reservation(String memberName, LocalDate date, LocalTime time) {
         this.id = null;
-        this.name = name;
+        this.memberName = memberName;
         this.date = date;
         this.time = time;
     }
@@ -29,7 +38,7 @@ public class Reservation {
     }
 
     public String getMemberName() {
-        return name;
+        return memberName;
     }
 
     public LocalDate getDate() {

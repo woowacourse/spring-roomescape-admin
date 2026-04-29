5594961f5f9811c9a245a9e62f140d4173a9399b
나의찬 <cab07036@naver.com>
2026-04-29 22:36:35 +0900

    test: 단계별 테스트코드 수정 및 삭제

--- src/test/java/roomescape/MissionStep4Test.java
+++ src/test/java/roomescape/MissionStep4Test.java
@@ -5,27 +5,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.controller.ReservationController;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.lang.reflect.Field;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class MissionStep2Test {
+public class MissionStep4Test {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private ReservationController reservationController;
 
     @Test
-    void 데이터베이스_연동() {
-        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
-            assertThat(connection).isNotNull();
-            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
-            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+    void 계층화_리팩터링() {
+        boolean isJdbcTemplateInjected = false;
+
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
         }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
     }
 }

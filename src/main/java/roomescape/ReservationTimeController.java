cae3b22d04fa726370641aa0e539ebbdc4f5be4f
snowshower <wonsb25@gmail.com>
2026-04-29 23:03:50 +0900

    feat: 시간 조회, 추가, 삭제 API 메서드 추가
    Signed-off-by: snowshower <wonsb25@gmail.com>

---
+++ src/main/java/roomescape/ReservationTimeController.java
@@ -0,0 +1,37 @@
+package roomescape;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class ReservationTimeController {
+    private final ReservationTimeDAO reservationTimeDAO;
+
+    public ReservationTimeController(ReservationTimeDAO reservationTimeDAO) {
+        this.reservationTimeDAO = reservationTimeDAO;
+    }
+
+    @PostMapping("/times")
+    public ResponseEntity<ReservationTime> create(@RequestBody ReservationTime reservationTime) {
+        Long generatedId = reservationTimeDAO.insertWithKeyHolder(reservationTime);
+
+        ReservationTime newReservationTime = ReservationTime.toEntity(reservationTime, generatedId);
+
+        return ResponseEntity.ok().body(newReservationTime);
+    }
+
+    @GetMapping("/times")
+    public ResponseEntity<List<ReservationTime>> read() {
+        return ResponseEntity.ok().body(reservationTimeDAO.findAllReservationTime());
+    }
+
+    @DeleteMapping("/times/{id}")
+    public ResponseEntity<List<ReservationTime>> delete(@PathVariable Long id) {
+        reservationTimeDAO.delete(id);
+
+        return ResponseEntity.ok().body(reservationTimeDAO.findAllReservationTime());
+    }
+}

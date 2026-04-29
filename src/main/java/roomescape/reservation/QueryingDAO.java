1519e9c99ed332842c3e45d3957e3bb38db2068b
gahyeonnni <hyeonnni@naver.com>
2026-04-29 20:12:42 +0900

    feat(reservation): JdbcTemplate 기반 예약 전체 조회 QueryingDAO 추가

---
+++ src/main/java/roomescape/reservation/QueryingDAO.java
@@ -0,0 +1,30 @@
+package roomescape.reservation;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class QueryingDAO {
+    private JdbcTemplate jdbcTemplate;
+
+    public QueryingDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<Reservation> reservationRowMapper = (resultSet, rowNum) -> {
+        return new Reservation(
+                resultSet.getLong("id"),
+                resultSet.getString("name"),
+                resultSet.getDate("date").toLocalDate(),
+                resultSet.getTime("time").toLocalTime()
+        );
+    };
+
+    public List<Reservation> findAll() {
+        String sql = "select id, name, date, time from reservation";
+        return jdbcTemplate.query(sql, reservationRowMapper);
+    }
+}

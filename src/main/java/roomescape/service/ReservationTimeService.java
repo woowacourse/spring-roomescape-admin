58ba57b8e4497007900eeab0806135302e3eb288
frombunny <rucy011121@gmail.com>
2026-04-29 20:04:30 +0900

    feat(service): 예약 시간대 추가/수정/삭제 로직 작성

---
+++ src/main/java/roomescape/service/ReservationTimeService.java
@@ -0,0 +1,45 @@
+package roomescape.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import roomescape.domain.ReservationTime;
+import roomescape.dto.ReservationTimeRequest;
+import roomescape.dto.ReservationTimeResponse;
+import roomescape.repository.ReservationTimeRepository;
+import roomescape.util.DateAndTimeConverter;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationTimeService {
+    private final ReservationTimeRepository reservationTimeRepository;
+
+    @Transactional
+    public ReservationTimeResponse save(ReservationTimeRequest reservationTimeRequest) {
+        ReservationTime reservationTime = ReservationTime.create(
+                null,
+                DateAndTimeConverter.parseToLocalDateTime(reservationTimeRequest.time())
+        );
+
+        return new ReservationTimeResponse(
+                reservationTimeRepository.save(reservationTime),
+                reservationTimeRequest.time()
+        );
+    }
+
+    public List<ReservationTimeResponse> getAll() {
+        return reservationTimeRepository.getAll()
+                .stream()
+                .map(reservationTime -> new ReservationTimeResponse(
+                        reservationTime.getId(),
+                        DateAndTimeConverter.formatDateAndTime(reservationTime.getStartAt()))
+                ).toList();
+    }
+
+    @Transactional
+    public void delete(Long id){
+        reservationTimeRepository.delete(id);
+    }
+}

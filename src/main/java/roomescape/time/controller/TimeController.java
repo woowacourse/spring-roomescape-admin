389d8f35713f2017a4cd540084f45786f895a51b
dahxxn <dahyeonhan4529@gmail.com>
2026-04-29 22:53:15 +0900

    feat(controller): TimeController 추가/조회/삭제 API 구현

---
+++ src/main/java/roomescape/time/controller/TimeController.java
@@ -0,0 +1,38 @@
+package roomescape.time.controller;
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import roomescape.time.dto.CreateReservationTimeRequest;
+import roomescape.time.dto.ReservationTimeResponse;
+import roomescape.time.service.ReservationTimeService;
+
+@Controller
+public class TimeController {
+    private final ReservationTimeService reservationTimeService;
+
+    public TimeController(ReservationTimeService reservationTimeService) {
+        this.reservationTimeService = reservationTimeService;
+    }
+
+    @PostMapping("/times")
+    public ResponseEntity<ReservationTimeResponse> create(
+            @RequestBody CreateReservationTimeRequest createReservationTimeRequest) {
+        return ResponseEntity.ok(reservationTimeService.create(createReservationTimeRequest));
+    }
+
+    @GetMapping("/times")
+    public ResponseEntity<List<ReservationTimeResponse>> read() {
+        return ResponseEntity.ok(reservationTimeService.findAll());
+    }
+
+    @DeleteMapping("/times/{id}")
+    public ResponseEntity<ReservationTimeResponse> delete(@PathVariable Long id) {
+        return ResponseEntity.ok(reservationTimeService.delete(id));
+    }
+}

b080ec8dd667a371675c65d0c1cb787b7bdba356
quddaz <quddnddl35@naver.com>
2026-04-29 22:32:47 +0900

    refactor(Entity) : 엔티티 객체 id 불변화

--- src/main/java/roomescape/reservation/entity/Reservation.java
+++ src/main/java/roomescape/reservation/entity/Reservation.java
@@ -5,7 +5,7 @@ import roomescape.time.entity.ReservationTime;
 
 public class Reservation {
 
-    private Long id;
+    private final Long id;
     private final String name;
     private final LocalDate date;
     private final ReservationTime time;
--- src/main/java/roomescape/time/entity/ReservationTime.java
+++ src/main/java/roomescape/time/entity/ReservationTime.java
@@ -4,7 +4,7 @@ import java.time.LocalTime;
 
 public class ReservationTime {
 
-    private Long id;
+    private final Long id;
     private final LocalTime startAt;
 
     private ReservationTime(Long id, LocalTime startAt) {

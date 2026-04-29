87c557b126d0e4ae1cf5d1dd97715eeb92dd5f84
jyt6640 <jyt6640@gmail.com>
2026-04-29 10:20:51 +0900

    refactor: 예약 레포지토리 반환값 수정

--- src/main/java/roomescape/domain/reservations/infrastructure/ReservationRepository.java
+++ src/main/java/roomescape/domain/reservations/infrastructure/ReservationRepository.java
@@ -1,11 +1,12 @@
 package roomescape.domain.reservations.infrastructure;
 
 import java.util.List;
+import java.util.Optional;
 import roomescape.domain.reservations.entity.Reservation;
 
 public interface ReservationRepository {
-    int save(Reservation reservation);
-    Reservation findById(Long id);
+    Reservation save(Reservation reservation);
+    Optional<Reservation> findById(Long id);
     List<Reservation> findAll();
     void deleteById(Long id);
 }

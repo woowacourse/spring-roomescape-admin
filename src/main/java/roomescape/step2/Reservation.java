6f26aa3241dfb900a314ccdc5034d58565db66fa
sinequanonn <dleck288@gmail.com>
2026-04-29 09:58:33 +0900

    feat: 2단계 구현 완료
    - 환경 설정 추가
    - 테이블 스키마 작성
    - CRUD API를 JdbcTemplate 기반으로 전환

---
+++ src/main/java/roomescape/step2/Reservation.java
@@ -0,0 +1,41 @@
+package roomescape.step2;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+    private Long id;
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
+
+    public Reservation() {
+    }
+
+    private Reservation(Long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public static Reservation toEntity(Long id, String name, LocalDate date, LocalTime time) {
+        return new Reservation(id, name, date, time);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+}

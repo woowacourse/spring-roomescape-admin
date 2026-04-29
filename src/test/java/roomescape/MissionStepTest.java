41058ab4289b9411497d21307101463bc265b482
smiinii <sm020320@naver.com>
2026-04-29 20:05:11 +0900

    test(MissionStepTest): 예약 조회 응답 필드 경로 수정

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -86,7 +86,7 @@ public class MissionStepTest {
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200).extract()
-                .jsonPath().getList(".", Reservation.class);
+                .jsonPath().getList("reservationsResponse", Reservation.class);
 
         Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
 

1692b8c0cc17a0c32b948648e905c93158b80dc4
jyt6640 <jyt6640@gmail.com>
2026-04-29 17:03:24 +0900

    feat: ReservationTimeRepository JdbcTemplate를 이용하여 구현

---
+++ src/main/java/roomescape/domain/reservations/infrastructure/ReservationTimeJdbcTemplateRepository.java
@@ -0,0 +1,47 @@
+package roomescape.domain.reservations.infrastructure;
+
+import java.util.List;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.reservations.entity.ReservationTime;
+
+@Repository
+public class ReservationTimeJdbcTemplateRepository implements ReservationTimeRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    public ReservationTimeJdbcTemplateRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("reservation_time")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    @Override
+    public ReservationTime save(ReservationTime reservation) {
+        Map<String, Object> params = Map.of(
+                "start_at", reservation.getStartAt()
+        );
+        Long id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
+        return ReservationTime.of(id, reservation.getStartAt());
+    }
+
+    @Override
+    public List<ReservationTime> findAll() {
+        String sql = "SELECT * FROM reservation_time";
+        return jdbcTemplate.query(sql,
+                (rs, rowNum) -> ReservationTime.of(
+                        rs.getLong("id"),
+                        rs.getTime("start_at").toLocalTime()
+                ));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        jdbcTemplate.update("DELETE FROM reservation_time WHERE id = ?", id);
+    }
+}

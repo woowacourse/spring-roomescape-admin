7ade7010fff16aa58c25fbbd02028a4aaf543e66
yj9107v <yj9107v@gmail.com>
2026-04-29 16:22:06 +0900

    feat: 예약 생성에 필요한 도메인 및 요청 객체 추가

---
+++ src/main/java/roomescape/ReservationRequest.java
@@ -0,0 +1,23 @@
+package roomescape;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ReservationRequest {
+
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
+    
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+}

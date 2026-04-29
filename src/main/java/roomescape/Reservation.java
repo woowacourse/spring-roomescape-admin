2e43d85b4d763bfdcff94277f7c6c4db6c80fb6b
wontop02 <wjy2002kr@gmail.com>
2026-04-29 16:34:53 +0900

    refactor: Reservation을 record로 변경, 사용하지 않는 equals, hashcode 메서드 삭제

--- src/main/java/roomescape/Reservation.java
+++ src/main/java/roomescape/Reservation.java
@@ -1,47 +1,4 @@
 package roomescape;
 
-import java.util.Objects;
-
-public class Reservation {
-    private final long id;
-    private final String name;
-    private final String date;
-    private final String time;
-
-    public Reservation(long id, String name, String date, String time) {
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.time = time;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Reservation that = (Reservation) o;
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getTime() {
-        return time;
-    }
+public record Reservation(long id, String name, String date, String time) {
 }

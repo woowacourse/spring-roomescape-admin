0ea2c3437021c7ee2e11e571594ea6f17592ddb4
haeyoon1 <hynam1220@naver.com>
2026-04-29 21:00:25 +0900

    feat: time 엔티티 생성

---
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -0,0 +1,30 @@
+package roomescape.domain;
+
+import java.time.LocalTime;
+
+public class ReservationTime {
+
+    private final Long id;
+    private final LocalTime startAt;
+
+    private ReservationTime(Long id, LocalTime startAt) {
+        this.id = id;
+        this.startAt = startAt;
+    }
+
+    public static ReservationTime of(Long id, LocalTime startAt) {
+        return new ReservationTime(id, startAt);
+    }
+
+    public static ReservationTime of(LocalTime startAt) {
+        return new ReservationTime(null, startAt);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalTime getStartAt() {
+        return startAt;
+    }
+}

265e34ff00ff52621fdd04382c2189af1b0a5801
Jeshin Kim <jjason0904@cau.ac.kr>
2026-04-29 10:36:23 +0900

    fix(ReservationDao): 없는 ID를 삭제했을때, Exception 안터지는 문제, 예약을 추가했을때, 추가한 예약 반환 안되는 문제 수정

--- src/main/java/roomescape/dao/ReservationDao.java
+++ src/main/java/roomescape/dao/ReservationDao.java
@@ -1,8 +1,12 @@
 package roomescape.dao;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import roomescape.exception.ReservationNotFoundException;
 import roomescape.model.Reservation;
 
 @Component
@@ -13,9 +17,20 @@ public class ReservationDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void save(String name, String date, String time) {
+    public Reservation save(String name, String date, String time) {
         String sql = "INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)";
-        jdbcTemplate.update(sql, name, date, time);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, name);
+            ps.setString(2, date);
+            ps.setString(3, time);
+            return ps;
+        }, keyHolder);
+
+        long generatedId = keyHolder.getKey().longValue();
+        return new Reservation(generatedId, name, date, time);
     }
 
     public List<Reservation> findAll() {
@@ -31,6 +46,11 @@ public class ReservationDao {
 
     public void delete(Long id) {
         String sql = "DELETE FROM reservation WHERE id = ?";
-        jdbcTemplate.update(sql, id);
+        int affectedRows = jdbcTemplate.update(sql, id);
+
+        if (affectedRows == 0) {
+            throw new ReservationNotFoundException(id);
+        }
     }
+
 }

ec632c686a07cbcefabca5b6e66a96f906eadf8c
JYL35 <154980427+JYL35@users.noreply.github.com>
2026-04-30 00:19:20 +0900

    refactor: 예약 시간 API를 DAO로 분리 (DB 도입)
    - JdbcTemplate을 사용한 DAO 계층 추가
    - 예약 데이터 영속성 처리 구조로 변경

---
+++ src/main/java/roomescape/repository/ReservationTimeDao.java
@@ -0,0 +1,62 @@
+package roomescape.repository;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.entity.ReservationTime;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+
+@Repository
+public class ReservationTimeDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationTimeDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<ReservationTime> reservationTimeRowMapper = (resultSet, rowNum) -> {
+        ReservationTime reservationTime = new ReservationTime(
+                resultSet.getLong("id"),
+                resultSet.getTime("start_at").toLocalTime()
+        );
+        return reservationTime;
+    };
+
+    public Long save(ReservationTime reservationTime) {
+        String sql = "INSERT INTO reservation_time (start_at) VALUES (?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    sql, new String[]{"id"}
+            );
+            preparedStatement.setString(1, String.valueOf(reservationTime.startAt()));
+            return preparedStatement;
+        }, keyHolder);
+        return keyHolder.getKey().longValue();
+    }
+
+    public List<ReservationTime> findAll() {
+        String sql = "SELECT * FROM reservation_time";
+        return jdbcTemplate.query(sql, reservationTimeRowMapper);
+    }
+
+    public ReservationTime findById(Long id) {
+        String sql = "SELECT * FROM reservation_time WHERE id = ?";
+        try {
+            return jdbcTemplate.queryForObject(sql, reservationTimeRowMapper, id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new IllegalArgumentException("존재하지 않는 예약 시간입니다.");
+        }
+    }
+
+    public void deleteById(Long id) {
+        String sql = "DELETE FROM reservation_time WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+}

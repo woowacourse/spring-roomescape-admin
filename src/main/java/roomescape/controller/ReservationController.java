bff8a9716d386083a651ebb85ed867c0e491dd9c
yItWorks <poiu7825@pusan.ac.kr>
2026-04-29 19:25:37 +0900

    feat: 2단계 요구사항 로직 작성

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,44 +1,78 @@
 package roomescape.controller;
 
-import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import roomescape.domain.Name;
 import roomescape.domain.Reservation;
 
 @Controller
 public class ReservationController {
+    private JdbcTemplate jdbcTemplate;
 
-    private final List<Reservation> reservations = new ArrayList<>();
-    private final AtomicLong reservationId = new AtomicLong(1);
+    public ReservationController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<Reservation> actorRowMapper = (resultSet, rowNum) -> {
+        Reservation reservation = new Reservation(
+                resultSet.getLong("id"),
+                Name.parse(resultSet.getString("name")),
+                LocalDate.parse(resultSet.getString("date")),
+                LocalTime.parse(resultSet.getString("time"))
+        );
+        return reservation;
+    };
 
     @GetMapping("/reservations")
     public ResponseEntity<List<Reservation>> read() {
+        String sql = "select id, name, date, time from reservations";
+        List<Reservation> reservations = jdbcTemplate.query(sql, actorRowMapper);
+
         return ResponseEntity.ok().body(reservations);
     }
 
     @PostMapping("/reservations")
     public ResponseEntity<Reservation> create(@RequestBody Reservation reservation) {
-        Reservation newReservation = Reservation.toEntity(reservation, reservationId.getAndIncrement());
-        reservations.add(newReservation);
+        String sql = "insert into reservations (name, date, time) values (?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, reservation.getName().toString());
+            ps.setString(2, reservation.getDate().toString());
+            ps.setString(3, reservation.getTime().toString());
+            return ps;
+        }, keyHolder);
+
+        Long generatedId = Objects.requireNonNull(keyHolder.getKey()).longValue();
+        Reservation newReservation = new Reservation(
+                generatedId,
+                reservation.getName(),
+                reservation.getDate(),
+                reservation.getTime()
+        );
         return ResponseEntity.ok().body(newReservation);
     }
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        Reservation reservation = reservations.stream()
-                .filter(it -> Objects.equals(it.getId(), id))
-                .findFirst()
-                .orElseThrow(RuntimeException::new);
+        String sql = "delete from reservations where id = ?";
 
-        reservations.remove(reservation);
+        jdbcTemplate.update(sql, id);
 
         return ResponseEntity.ok().build();
     }

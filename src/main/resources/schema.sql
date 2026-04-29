434cd0ea23ddf7dbe4517d7ba524281f11631f64
JYL35 <154980427+JYL35@users.noreply.github.com>
2026-04-29 23:45:20 +0900

    feat: 테이블 스키마 추가 (reservation_time)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,13 @@
 CREATE TABLE reservation (
-                             id      BIGINT       NOT NULL AUTO_INCREMENT,
-                             name    VARCHAR(255) NOT NULL,
-                             date    VARCHAR(255) NOT NULL,
-                             time    VARCHAR(255) NOT NULL,
-                             PRIMARY KEY (id)
+    id      BIGINT       NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(255) NOT NULL,
+    date    VARCHAR(255) NOT NULL,
+    time    VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE reservation_time (
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );

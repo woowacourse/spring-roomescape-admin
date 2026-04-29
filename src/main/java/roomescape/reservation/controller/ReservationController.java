ecd6e6ea3670736c45d271b04ada0981e46ca408
Jinriro <riro12.hanyang.ac.kr>
2026-04-30 00:28:59 +0900

    feat: 패키지 분리

--- src/main/java/roomescape/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/reservation/controller/ReservationController.java
@@ -1,11 +1,11 @@
-package roomescape.controller;
+package roomescape.reservation.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import roomescape.dao.QueryingDao;
-import roomescape.dao.UpdatingDao;
-import roomescape.dto.ReservationRequestDto;
-import roomescape.entity.Reservation;
+import roomescape.reservation.dao.QueryingDao;
+import roomescape.reservation.dao.UpdatingDao;
+import roomescape.reservation.dto.ReservationRequestDto;
+import roomescape.reservation.entity.Reservation;
 
 import java.util.List;
 
@@ -28,7 +28,7 @@ public class ReservationController {
 
     @PostMapping
     public ResponseEntity<Reservation> create(@RequestBody ReservationRequestDto requestDto) {
-        Long id = updatingDao.insertWithKeyHolder(requestDto);
+        Long id = updatingDao.insert(requestDto);
         Reservation savedReservation = queryingDao.findById(id);
         return ResponseEntity.ok(savedReservation);
     }
--- src/test/java/roomescape/SecondMissionStepTest.java
+++ src/test/java/roomescape/SecondMissionStepTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
-import roomescape.entity.Reservation;
+import roomescape.reservation.entity.Reservation;
 
 import java.sql.Connection;
 import java.sql.SQLException;

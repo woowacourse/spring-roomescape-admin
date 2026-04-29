23bcc60880f300883983775974c15ca0973d30b0
Chocoding1 <galmeagi2@naver.com>
2026-04-29 19:09:52 +0900

    refactor: JdbcTemplate 도입

--- src/main/java/roomescape/ReservationRepository.java
+++ src/main/java/roomescape/ReservationRepository.java
@@ -1,39 +1,67 @@
 package roomescape;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.concurrent.atomic.AtomicLong;
 
+@Repository
 public class ReservationRepository {
 
-    private final List<Reservation> reservations;
-    private final AtomicLong idGenerator;
+    private final JdbcTemplate jdbcTemplate;
 
-    public ReservationRepository() {
-        reservations = new ArrayList<>();
-        idGenerator = new AtomicLong(0);
+    public ReservationRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<Reservation> findAll() {
-        return List.copyOf(reservations);
+        return jdbcTemplate.query(
+                "SELECT id, name, date, time FROM reservation",
+                ((rs, rowNum) -> new Reservation(
+                        rs.getLong("id"),
+                        rs.getString("name"),
+                        LocalDate.parse(rs.getString("date")),
+                        LocalTime.parse(rs.getString("time"))
+                ))
+        );
     }
 
     public Reservation save(String name, LocalDate reservationDate, LocalTime reservationTime) {
-        Reservation reservation = new Reservation(idGenerator.incrementAndGet(), name, reservationDate, reservationTime);
-        reservations.add(reservation);
-        return reservation;
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("reservation")
+                .usingGeneratedKeyColumns("id");
+
+        long savedId = simpleJdbcInsert.executeAndReturnKey(Map.of(
+                "name", name,
+                "date", reservationDate.toString(),
+                "time", reservationTime.toString()
+        )).longValue();
+
+        return findById(savedId);
     }
 
     public void deleteById(Long id) {
-        for (Reservation reservation : reservations) {
-            if (reservation.getId().equals(id)) {
-                reservations.remove(reservation);
-                return;
-            }
+        int updatedRow = jdbcTemplate.update("DELETE FROM reservation WHERE id = ?", id);
+        if (updatedRow == 0) {
+            throw new NoSuchElementException("[ERROR] 해당 id의 Reservation이 존재하지 않습니다.");
         }
-        throw new NoSuchElementException("[ERROR] 해당 id의 Reservation이 존재하지 않습니다.");
+    }
+
+    private Reservation findById(Long id) {
+        return jdbcTemplate.queryForObject(
+                "SELECT id, name, date, time FROM reservation WHERE id = ?",
+                (rs, rowNum) -> new Reservation(
+                        rs.getLong("id"),
+                        rs.getString("name"),
+                        LocalDate.parse(rs.getString("date")),
+                        LocalTime.parse(rs.getString("time"))
+                ),
+                id
+        );
     }
 }

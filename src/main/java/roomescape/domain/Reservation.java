be50225aaf2d565668ed4dae848ff76795a3b863
poketopa <lhs5427ll@gmail.com>
2026-04-29 23:45:45 +0900

    feat(domain): 시간 관리 domain 추가

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,4 +1,4 @@
 package roomescape.domain;
 
-public record Reservation(Long id, String name, String date, String time) {
+public record Reservation(Long id, String name, String date, ReservationTime time) {
 }

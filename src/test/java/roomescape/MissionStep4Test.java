f4b5d71c10ff4d40812b373bf83be2edb7634a9e
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-04-30 13:55:33 +0900

    docs(log-04.md) : 4단계 학습 로그 작성

--- src/test/java/roomescape/MissionStep4Test.java
+++ src/test/java/roomescape/MissionStep4Test.java
@@ -1,13 +1,13 @@
 package roomescape;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.lang.reflect.Field;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import roomescape.controller.ReservationController;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MissionStep4Test {

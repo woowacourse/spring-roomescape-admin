451e3a9ad97240df56c8e0521875896480451ae8
jyt6640 <jyt6640@gmail.com>
2026-04-30 18:56:05 +0900

    refactor: ReservationTime 엔티티가 응답 DTO에 포함 되어있던 사항을 DTO로 변경하여 저장하여 반환

--- src/main/java/roomescape/domain/reservations/presentation/dto/ReservationResponse.java
+++ src/main/java/roomescape/domain/reservations/presentation/dto/ReservationResponse.java
@@ -7,14 +7,14 @@ public record ReservationResponse(
         Long id,
         String name,
         String date,
-        ReservationTime time
+        ReservationTimeResponse time
 ) {
     public static ReservationResponse from(Reservation reservation) {
         return new ReservationResponse(
                 reservation.getId(),
                 reservation.getName(),
                 reservation.getDate().toString(),
-                reservation.getTime()
+                ReservationTimeResponse.from(reservation.getTime())
         );
     }
 }

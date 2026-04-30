559904d0f855359b3db455e1f554d6e8b1d1ac66
dahxxn <dahyeonhan4529@gmail.com>
2026-04-30 14:52:11 +0900

    refactor(domain): record에서 class로 변환

--- src/main/java/roomescape/time/domain/ReservationTime.java
+++ src/main/java/roomescape/time/domain/ReservationTime.java
@@ -2,5 +2,20 @@ package roomescape.time.domain;
 
 import java.time.LocalTime;
 
-public record ReservationTime(Long id, LocalTime startAt) {
+public class ReservationTime {
+    private final Long id;
+    private final LocalTime startAt;
+
+    public ReservationTime(Long id, LocalTime startAt) {
+        this.id = id;
+        this.startAt = startAt;
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public LocalTime startAt() {
+        return startAt;
+    }
 }

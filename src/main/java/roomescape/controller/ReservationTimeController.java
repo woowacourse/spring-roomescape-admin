675a193026f55b2e4eef34b0c615750a01daf32c
xxbeann <yjb4945@gmail.com>
2026-04-30 15:59:43 +0900

    refactor: change controller annotation to restController

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -2,16 +2,16 @@ package roomescape.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.ReservationTime;
 import roomescape.service.ReservationTimeService;
 
-@Controller
+@RestController
 public class ReservationTimeController {
 
     private final ReservationTimeService reservationTimeService;

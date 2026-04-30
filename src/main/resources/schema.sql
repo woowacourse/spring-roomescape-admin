b13008b5a6b35452b09ec790cb9b219d1d49df00
quddaz <quddnddl35@naver.com>
2026-04-30 09:45:06 +0900

    feat : 타임 테이블 sql 유니크 제약 조건 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,8 @@ CREATE TABLE reservation_time
 (
     id       BIGINT NOT NULL AUTO_INCREMENT,
     start_at TIME   NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    UNIQUE (start_at)
 );
 
 CREATE TABLE reservation

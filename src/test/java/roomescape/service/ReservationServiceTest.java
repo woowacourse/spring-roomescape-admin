442a5d678e84f5877da6ab527b29895641189e5b
wonsang.choi <wonsang.choi@e4net.net>
2026-04-30 17:34:04 +0900

    test: 테스트 수정

--- src/test/java/roomescape/service/ReservationServiceTest.java
+++ src/test/java/roomescape/service/ReservationServiceTest.java
@@ -1,59 +1,78 @@
-//package roomescape.service;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//import java.time.LocalDateTime;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//import roomescape.domain.Reservation;
-//import roomescape.repository.ReservationRepository;
-//
-//@SpringBootTest
-//@Transactional
-//public class ReservationServiceTest {
-//    @Autowired
-//    private ReservationService reservationService;
-//
-//    @Autowired
-//    private ReservationRepository reservationRepository;
-//
-//    @Test
-//    void 예약_생성() {
-//        //given
-//        LocalDateTime localDateTime = LocalDateTime.of(2023, 8, 5, 15, 40);
-//        // when
-//        Long id = reservationService.createReservation("브라운", localDateTime);
-//        //then
-//        Reservation reservation = reservationRepository.findById(id);
-//        assertThat(reservation).isNotNull();
-//        assertThat(reservation.getReservationName()).isEqualTo("브라운");
-//        assertThat(reservation.getReservationDateTime()).isEqualTo(localDateTime);
-//    }
-//
-//    @Test
-//    void 예약_삭제() {
-//        //given
-//        LocalDateTime localDateTime = LocalDateTime.of(2023, 8, 5, 15, 40);
-//        Long id = reservationService.createReservation("브라운", localDateTime);
-//        // when
-//        reservationService.deleteReservation(id);
-//        //then
-//        Reservation reservation = reservationRepository.findById(id);
-//        assertThat(reservation).isNull();
-//    }
-//
-//    @Test
-//    void 예약_조회() {
-//        //given
-//        LocalDateTime localDateTime = LocalDateTime.of(2023, 8, 5, 15, 40);
-//        Long id = reservationService.createReservation("브라운", localDateTime);
-//        // when
-//        Reservation reservation = reservationService.findReservationById(id);
-//        //then
-//        assertThat(reservation).isNotNull();
-//        assertThat(reservation.getReservationName()).isEqualTo("브라운");
-//        assertThat(reservation.getReservationDateTime()).isEqualTo(localDateTime);
-//    }
-//}
+package roomescape.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import roomescape.domain.Reservation;
+import roomescape.repository.ReservationRepository;
+
+@SpringBootTest
+@Transactional
+public class ReservationServiceTest {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @Autowired
+    ReservationTimeService reservationTimeService;
+
+    @Test
+    void 예약_생성() {
+        //given
+        final LocalDate localDate = LocalDate.of(2023, 8, 5);
+        final long timeId = 타임_생성();
+        // when
+        Long id = reservationService.createReservation("브라운", localDate, timeId);
+        //then
+        Reservation reservation = reservationRepository.findById(id);
+        assertThat(reservation).isNotNull();
+        assertThat(reservation.getReservationName()).isEqualTo("브라운");
+        assertThat(reservation.getReservationDate()).isEqualTo(localDate);
+        assertThat(reservation.getReservationTime().getId()).isEqualTo(timeId);
+        assertThat(reservation.getReservationTime().getReservationTime()).isEqualTo(LocalTime.of(10, 0));
+    }
+
+    @Test
+    void 예약_삭제() {
+        //given
+        final LocalDate localDate = LocalDate.of(2023, 8, 5);
+        final long timeId = 타임_생성();
+        final Long id = reservationService.createReservation("브라운", localDate, timeId);
+        // when
+        reservationService.deleteReservation(id);
+        //then
+        Reservation reservation = reservationRepository.findById(id);
+        assertThat(reservation).isNull();
+    }
+
+    @Test
+    void 예약_조회() {
+        //given
+        final LocalDate localDate = LocalDate.of(2023, 8, 5);
+        final long timeId = 타임_생성();
+
+        // when
+        Long id = reservationService.createReservation("브라운", localDate, timeId);
+        Reservation reservation = reservationService.findReservationById(id);
+        //then
+        assertThat(reservation).isNotNull();
+        assertThat(reservation.getReservationName()).isEqualTo("브라운");
+        assertThat(reservation.getReservationDate()).isEqualTo(localDate);
+        assertThat(reservation.getReservationTime().getId()).isEqualTo(timeId);
+        assertThat(reservation.getReservationTime().getReservationTime()).isEqualTo(LocalTime.of(10, 0));
+    }
+
+    
+    long 타임_생성() {
+        final long timeId = reservationTimeService.createTime(LocalTime.of(10, 0));
+        return timeId;
+    }
+}

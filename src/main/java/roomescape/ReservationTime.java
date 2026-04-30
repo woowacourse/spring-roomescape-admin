4b62eef5a187ca6c37933a98926b8fae7e6f0386
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-30 11:48:11 +0900

    feat: 예약 시간 도메인 객체 구현

---
+++ src/main/java/roomescape/ReservationTime.java
@@ -0,0 +1,22 @@
+package roomescape;
+
+public class ReservationTime {
+    private Long id;
+    private String startAt;
+
+    public ReservationTime() {
+    }
+
+    public ReservationTime(Long id, String startAt) {
+        this.id = id;
+        this.startAt = startAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStartAt() {
+        return startAt;
+    }
+}

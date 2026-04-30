93f750070ae9074a9af277a9e3699343b14bf917
haeseon lee <haeseon4816@gmail.com>
2026-04-30 10:36:22 +0900

    test: 예약과 시간 연결 기능 테스트 추가

--- src/test/java/roomescape/MissionStep3Test.java
+++ src/test/java/roomescape/MissionStep3Test.java
@@ -41,6 +41,16 @@ public class MissionStep3Test {
 
     @Test
     void 예약과_시간_연결() {
+        Map<String, String> timeParams = new HashMap<>();
+        timeParams.put("startAt", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(timeParams)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(200);
+
         Map<String, Object> reservation = new HashMap<>();
         reservation.put("name", "브라운");
         reservation.put("date", "2023-08-05");

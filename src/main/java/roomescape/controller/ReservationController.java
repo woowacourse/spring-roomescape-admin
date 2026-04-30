ca30b13b5820090c845ca4d9b603043656860ed4
choubung <choubung@sookmyung.ac.kr>
2026-04-30 14:15:15 +0900

    refactor : 컨트롤러의 비즈니스로직을 서비스 계층으로 이동

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,85 +2,55 @@ package roomescape.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import roomescape.domain.Reservation;
-import roomescape.domain.ReservationTime;
 import roomescape.dto.reservation.ReservationRequestDto;
 import roomescape.dto.reservation.ReservationResponseDto;
 import roomescape.dto.reservationTime.ReservationTimeRequesetDto;
 import roomescape.dto.reservationTime.ReservationTimeResponseDto;
-import roomescape.repository.ReservationRepository;
-import roomescape.repository.ReservationTimeRepository;
+import roomescape.service.ReservationService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class ReservationController {
-    private final ReservationRepository reservationRepository;
-    private final ReservationTimeRepository reservationTimeRepository;
+    private final ReservationService reservationService;
 
-    public ReservationController(ReservationRepository reservationRepository, ReservationTimeRepository reservationTimeRepository) {
-        this.reservationRepository = reservationRepository;
-        this.reservationTimeRepository = reservationTimeRepository;
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
     }
 
     @GetMapping("/reservations")
     @ResponseStatus(HttpStatus.OK)
     public List<ReservationResponseDto> getReservations() {
-        List<ReservationResponseDto> responseDtos = new ArrayList<>();
-        List<Reservation> reservations = reservationRepository.findAll();
-
-        for (Reservation reservation : reservations) {
-            Long timeId = reservation.getTime().getId();
-            ReservationTime time = reservationTimeRepository.findById(timeId);
-            Reservation reservationIncludingTime = new Reservation(reservation, time);
-
-            responseDtos.add(ReservationResponseDto.from(reservationIncludingTime));
-        }
-
-        return responseDtos;
+        return reservationService.getReservations();
     }
 
     @PostMapping("/reservations")
     @ResponseStatus(HttpStatus.OK)
     public ReservationResponseDto addReservation(@RequestBody ReservationRequestDto requestDto) {
-        ReservationTime time = reservationTimeRepository.findById(requestDto.time_id());
-        Long id = reservationRepository.createReservation(new Reservation(null, requestDto.name(), requestDto.date(), time));
-        Reservation reservation = reservationRepository.findById(id);
-
-        return ReservationResponseDto.from(reservation);
+        return reservationService.addReservation(requestDto);
     }
 
     @DeleteMapping("/reservations/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteReservation(@PathVariable("id") Long id) {
-        reservationRepository.deleteById(id);
+        reservationService.deleteReservation(id);
     }
 
     @GetMapping("/times")
     @ResponseStatus(HttpStatus.OK)
     public List<ReservationTimeResponseDto> getReservationTimes() {
-        List<ReservationTimeResponseDto> responseDtos = new ArrayList<>();
-        List<ReservationTime> times = reservationTimeRepository.findAll();
-
-        for (ReservationTime time : times) {
-            responseDtos.add(ReservationTimeResponseDto.from(time));
-        }
-
-        return responseDtos;
+        return reservationService.getReservationTimes();
     }
 
     @PostMapping("/times")
     @ResponseStatus(HttpStatus.OK)
     public ReservationTimeResponseDto addReservationTime(@RequestBody ReservationTimeRequesetDto requestDto) {
-        ReservationTime reservationTime = reservationTimeRepository.createReservationTime(new ReservationTime(null, requestDto.startAt()));
-
-        return ReservationTimeResponseDto.from(reservationTime);
+        return reservationService.addReservationTime(requestDto);
     }
 
     @DeleteMapping("/times/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteReservationTime(@PathVariable("id") Long id) {
-        reservationTimeRepository.deleteById(id);
+        reservationService.deleteReservationTime(id);
     }
 }
--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,4 +1,73 @@
 package roomescape.service;
 
+import org.springframework.stereotype.Service;
+import roomescape.domain.Reservation;
+import roomescape.domain.ReservationTime;
+import roomescape.dto.reservation.ReservationRequestDto;
+import roomescape.dto.reservation.ReservationResponseDto;
+import roomescape.dto.reservationTime.ReservationTimeRequesetDto;
+import roomescape.dto.reservationTime.ReservationTimeResponseDto;
+import roomescape.repository.ReservationRepository;
+import roomescape.repository.ReservationTimeRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ReservationService {
+    private final ReservationRepository reservationRepository;
+    private final ReservationTimeRepository reservationTimeRepository;
+
+    public ReservationService(ReservationRepository reservationRepository, ReservationTimeRepository reservationTimeRepository) {
+        this.reservationRepository = reservationRepository;
+        this.reservationTimeRepository = reservationTimeRepository;
+    }
+
+    public List<ReservationResponseDto> getReservations() {
+        List<ReservationResponseDto> responseDtos = new ArrayList<>();
+        List<Reservation> reservations = reservationRepository.findAll();
+
+        for (Reservation reservation : reservations) {
+            Long timeId = reservation.getTime().getId();
+            ReservationTime time = reservationTimeRepository.findById(timeId);
+            Reservation reservationIncludingTime = new Reservation(reservation, time);
+
+            responseDtos.add(ReservationResponseDto.from(reservationIncludingTime));
+        }
+
+        return responseDtos;
+    }
+
+    public ReservationResponseDto addReservation(ReservationRequestDto requestDto) {
+        ReservationTime time = reservationTimeRepository.findById(requestDto.time_id());
+        Long id = reservationRepository.createReservation(new Reservation(null, requestDto.name(), requestDto.date(), time));
+        Reservation reservation = reservationRepository.findById(id);
+
+        return ReservationResponseDto.from(reservation);
+    }
+
+    public void deleteReservation(Long id) {
+        reservationRepository.deleteById(id);
+    }
+
+    public List<ReservationTimeResponseDto> getReservationTimes() {
+        List<ReservationTimeResponseDto> responseDtos = new ArrayList<>();
+        List<ReservationTime> times = reservationTimeRepository.findAll();
+
+        for (ReservationTime time : times) {
+            responseDtos.add(ReservationTimeResponseDto.from(time));
+        }
+
+        return responseDtos;
+    }
+
+    public ReservationTimeResponseDto addReservationTime(ReservationTimeRequesetDto requestDto) {
+        ReservationTime reservationTime = reservationTimeRepository.createReservationTime(new ReservationTime(null, requestDto.startAt()));
+
+        return ReservationTimeResponseDto.from(reservationTime);
+    }
+
+    public void deleteReservationTime(Long id) {
+        reservationTimeRepository.deleteById(id);
+    }
 }

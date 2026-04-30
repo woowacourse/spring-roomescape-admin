72298fe46a35a8d1ea6f9b5e28bbb2b78a12e8c6
YEJIN KIM <glow.yejin@gmail.com>
2026-04-30 16:23:03 +0900

    fix: 미션3 테스트 오류 수정

--- src/test/java/roomescape/MissionStep3Test.java
+++ src/test/java/roomescape/MissionStep3Test.java
@@ -40,6 +40,16 @@ public class MissionStep3Test {
 
     @Test
     void 예약과_시간_연결() {
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(200);
+
         Map<String, Object> reservation = new HashMap<>();
         reservation.put("name", "브라운");
         reservation.put("date", "2023-08-05");

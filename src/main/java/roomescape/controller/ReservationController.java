492c2c758552c9e6b2b02c795a5c7c90d0e2684d
sun <sun007021@gmail.com>
2026-04-30 16:12:34 +0900

    refactor(step4): Controller내 @ResponseBody중복 @RestController 적용

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,20 +2,19 @@ package roomescape.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 import roomescape.dto.CreateReservationRequest;
 import roomescape.dto.CreateReservationTimeRequest;
 import roomescape.service.ReservationService;
 
-@Controller
+@RestController
 public class ReservationController {
 
     private final ReservationService reservationService;
@@ -25,13 +24,11 @@ public class ReservationController {
     }
 
     @GetMapping("/reservations")
-    @ResponseBody
     public ResponseEntity<List<Reservation>> readReservations() {
         return ResponseEntity.ok().body(reservationService.getReservations());
     }
 
     @PostMapping("/reservations")
-    @ResponseBody
     public ResponseEntity<Reservation> createReservation(
             @RequestBody CreateReservationRequest createReservationRequest) {
         Reservation createdReservation = reservationService.createReservation(
@@ -54,7 +51,6 @@ public class ReservationController {
     }
 
     @PostMapping("/times")
-    @ResponseBody
     public ResponseEntity<ReservationTime> createTime(
             @RequestBody CreateReservationTimeRequest createReservationTimeRequest) {
         ReservationTime createdTime = reservationService.createReservationTime(createReservationTimeRequest.startAt());

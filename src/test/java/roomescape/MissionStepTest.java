d11e5846d913df4d2af48087cb3f0c84c2ab6188
JinGoo <zzgg3040@naver.com>
2026-04-30 13:06:52 +0900

    test: 컨트롤러에서 db 의존성 없는지 확인

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -18,7 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.DirtiesContext;
+import roomescape.controller.ReservationController;
 import roomescape.entity.Reservation;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -29,6 +30,9 @@ public class MissionStepTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private ReservationController reservationController;
 
     @BeforeEach
     void setUp() {
@@ -200,6 +204,24 @@ public class MissionStepTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
+        }
+    }
+
+    @Nested
+    class StepFour {
+
+        @Test
+        void 계층화_리팩터링() {
+            boolean isJdbcTemplateInjected = false;
+
+            for (Field field : reservationController.getClass().getDeclaredFields()) {
+                if (field.getType().equals(JdbcTemplate.class)) {
+                    isJdbcTemplateInjected = true;
+                    break;
+                }
+            }
+
+            assertThat(isJdbcTemplateInjected).isFalse();
         }
     }
 }

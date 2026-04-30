0257add71a7970741bce2115b15b16ca8f8fea62
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-30 16:50:09 +0900

    refactor: 비즈니스 로직을 Service 계층이 담당하도록 분리

---
+++ src/main/java/roomescape/service/ReservationService.java
@@ -0,0 +1,29 @@
+package roomescape.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import roomescape.dao.ReservationDao;
+import roomescape.domain.Reservation;
+import roomescape.domain.ReservationTime;
+
+@Service
+public class ReservationService {
+    private final ReservationDao reservationDao;
+
+    public ReservationService(ReservationDao reservationDao) {
+        this.reservationDao = reservationDao;
+    }
+
+    public List<Reservation> findAll() {
+        return reservationDao.findAll();
+    }
+
+    public Reservation save(Reservation reservation) {
+        ReservationTime time = reservationDao.findTimeById(reservation.getTimeId());
+        return reservationDao.save(reservation, time);
+    }
+
+    public void delete(Long id) {
+        reservationDao.delete(id);
+    }
+}

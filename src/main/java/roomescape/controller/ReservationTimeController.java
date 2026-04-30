924c2e3ede3aa7a4e61cc94ace1ca7c563349f04
picetea44 <picetea44@gmail.com>
2026-04-30 14:24:00 +0900

    feat: 시간 추가 기능 구현

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -1,15 +1,22 @@
 package roomescape.controller;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.dto.ReservationResponseDto;
+import roomescape.domain.ReservationTime;
+import roomescape.dto.ReservationTimeRequestDto;
 import roomescape.dto.ReservationTimeResponseDto;
 
 @RestController
@@ -29,6 +36,23 @@ public class ReservationTimeController {
                 ));
         return ResponseEntity.ok(responseDtoList);
 
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationTimeResponseDto> createReservationTime(@RequestBody ReservationTimeRequestDto reservationTimeRequestDto) {
+        String sql = "INSERT INTO reservation_time (start_at) VALUES (?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setObject(1, reservationTimeRequestDto.startAt());
+            return ps;
+        }, keyHolder);
+
+        Long id = keyHolder.getKey().longValue();
+
+        ReservationTime newReservationTime = reservationTimeRequestDto.toEntity(id);
+        return ResponseEntity.ok(ReservationTimeResponseDto.from(newReservationTime));
     }
 
 }
---
+++ src/main/java/roomescape/dto/ReservationTimeRequestDto.java
@@ -0,0 +1,16 @@
+package roomescape.dto;
+
+import java.time.LocalTime;
+import roomescape.domain.Reservation;
+import roomescape.domain.ReservationTime;
+
+public record ReservationTimeRequestDto(
+        LocalTime startAt
+) {
+    public ReservationTime toEntity(Long id) {
+        return ReservationTime.builder()
+                .id(id)
+                .startAt(this.startAt)
+                .build();
+    }
+}

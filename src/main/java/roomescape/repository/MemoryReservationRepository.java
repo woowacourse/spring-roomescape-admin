87e0fcad2de39ccd7788f5c2ebd2fe9ea54bb2b2
JinGoo <zzgg3040@naver.com>
2026-04-30 16:55:04 +0900

    feat: 레포지토리 추상화 및 콘솔 용 메모리 레포지토리 추가

---
+++ src/main/java/roomescape/repository/MemoryReservationRepository.java
@@ -0,0 +1,36 @@
+package roomescape.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.Reservation;
+
+public class MemoryReservationRepository implements ReservationRepository {
+
+    private final Map<Long, Reservation> reservations = new HashMap<>();
+    private final AtomicLong nextId = new AtomicLong(1);
+
+    @Override
+    public List<Reservation> getAll() {
+        return reservations.values().stream().toList();
+    }
+
+    @Override
+    public Reservation save(Reservation reservation) {
+        long id = nextId.getAndIncrement();
+        reservations.put(id, reservation.withId(id));
+
+        return reservation.withId(id);
+    }
+
+    @Override
+    public void deleteById(long reservationId) {
+        Reservation removed = reservations.remove(reservationId);
+        if (removed == null) {
+            throw new NoSuchElementException("존재하지 않는 예약 아이디 입니다. reservationId: " + reservationId);
+        }
+    }
+}
---
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -0,0 +1,13 @@
+package roomescape.repository;
+
+import java.util.List;
+import roomescape.domain.Reservation;
+
+public interface ReservationRepository {
+
+    List<Reservation> getAll();
+
+    Reservation save(Reservation reservation);
+
+    void deleteById(long reservationId);
+}

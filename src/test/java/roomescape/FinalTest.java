71e7810bca51325de9357830eea2ace6f394fe71
Soojin6943 <sjhan0711@gmail.com>
2026-04-30 15:30:00 +0900

    docs(log-04.md): log-04.md 작성

--- src/test/java/roomescape/FinalTest.java
+++ src/test/java/roomescape/FinalTest.java
@@ -1,6 +1,5 @@
 package roomescape;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;

38d71b8c34765cf2c873aba0c288904c8845265d
JinGoo <zzgg3040@naver.com>
2026-04-30 17:16:11 +0900

    fix: 테이블이 없을 때만 생성하도록 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE reservation_time(
+CREATE TABLE IF NOT EXISTS reservation_time(
     id          BIGINT NOT NULL AUTO_INCREMENT,
     start_at    TIME   NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation(
+CREATE TABLE IF NOT EXISTS reservation(
     id                  BIGINT       NOT NULL AUTO_INCREMENT,
     name                VARCHAR(255) NOT NULL,
     date                DATE         NOT NULL,

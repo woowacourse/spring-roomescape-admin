6102e0cd211d0ef747a1bf8d676381a102350463
MODUGGAGI <kiroro0814@naver.com>
2026-04-30 13:33:56 +0900

    refactor: 불필요한 설정 제거

--- src/main/java/roomescape/exception/ExceptionAdvice.java
+++ src/main/java/roomescape/exception/ExceptionAdvice.java
@@ -6,10 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(annotations = {RestController.class})
+@RestControllerAdvice
 public class ExceptionAdvice {
 
     @ExceptionHandler

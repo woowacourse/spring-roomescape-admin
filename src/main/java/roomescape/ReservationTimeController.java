fff283517db99b999a6ee404f5dd915667b85276
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-04-30 14:20:23 +0900

    fix: 누락된 Controller 어노테이션 추가

--- src/main/java/roomescape/ReservationTimeController.java
+++ src/main/java/roomescape/ReservationTimeController.java
@@ -6,12 +6,14 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Controller
 public class ReservationTimeController {
     private final JdbcTemplate jdbcTemplate;
 

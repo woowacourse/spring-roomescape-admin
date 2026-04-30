14a918859175703733a06847375b2ff6658b84da
kdongsu5509 <kod66170@gmail.com>
2026-04-30 16:45:33 +0900

    fix : change @Controller to @RestController
    - ResponseEntity를 이용할 때에는 @Controller와 @RestController 모두 같은 흐름을 타는 것을 확인.
    
    이 때, 애노테이션의 본질인 정보 전달 측면에서 해당 컨트롤러가 무엇인지 명확히 하고자 `RestController` 애노테이션으로 변경

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -3,19 +3,19 @@ package roomescape.controller;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.controller.dto.ReservationTimeRequest;
 import roomescape.controller.dto.ReservationTimeResponse;
 import roomescape.domain.ReservationTime;
 import roomescape.service.ReservationTimeService;
 
-@Controller
+@RestController
 @RequestMapping("/times")
 public class ReservationTimeController {
     private final ReservationTimeService reservationTimeService;

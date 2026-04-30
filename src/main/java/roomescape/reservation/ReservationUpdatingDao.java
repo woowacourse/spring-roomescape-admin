df9937661628b235fe99519626a2d3b7a4b1c97e
gahyeonnni <hyeonnni@naver.com>
2026-04-30 11:02:57 +0900

    refactor(reservation): 네이밍 수정 및 DTO 수신으로 변경

--- src/main/java/roomescape/reservation/ReservationUpdatingDao.java
+++ src/main/java/roomescape/reservation/ReservationUpdatingDao.java
@@ -4,14 +4,15 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import roomescape.dto.ReservationRequest;
 
 import java.sql.PreparedStatement;
 
 @Repository
-public class UpdatingDAO {
+public class ReservationUpdatingDao {
     private JdbcTemplate jdbcTemplate;
 
-    public UpdatingDAO(JdbcTemplate jdbcTemplate) {
+    public ReservationUpdatingDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -20,15 +21,15 @@ public class UpdatingDAO {
         return jdbcTemplate.update(sql, id);
     }
 
-    public Long insert(Reservation reservation) {
-        String sql = "insert into reservation (name, date, time) values (?, ?, ?)";
+    public Long insert(ReservationRequest request) {
+        String sql = "insert into reservation (name, date, time_id) values (?, ?, ?)";
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
         jdbcTemplate.update(connection -> {
             PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
-            ps.setString(1, reservation.getName());
-            ps.setObject(2, reservation.getDate());
-            ps.setObject(3, reservation.getTime());
+            ps.setString(1, request.getName());
+            ps.setString(2, request.getDate());
+            ps.setLong(3, request.getTimeId());
             return ps;
         }, keyHolder);
         return keyHolder.getKey().longValue();

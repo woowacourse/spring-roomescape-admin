1cfbaa609ca36d021e94e48cdadc0f58a922107a
haeseon lee <haeseon4816@gmail.com>
2026-04-30 10:28:36 +0900

    feat: 예약과 시간 연결 inner join 적용

--- src/main/java/roomescape/reservation/QueryingDAO.java
+++ src/main/java/roomescape/reservation/QueryingDAO.java
@@ -3,6 +3,7 @@ package roomescape.reservation;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import roomescape.reservationtime.ReservationTime;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -18,22 +19,31 @@ public class QueryingDAO {
     }
 
     private final RowMapper<Reservation> actorRowMapper = (resultSet, rowNum) -> {
-        Reservation reservation = new Reservation(
-                resultSet.getLong("id"),
+        ReservationTime reservationTime = new ReservationTime(
+                resultSet.getLong("time_id"),
+                resultSet.getObject("start_at", LocalTime.class)
+        );
+
+        return new Reservation(
+                resultSet.getLong("reservation_id"),
                 resultSet.getString("name"),
                 resultSet.getObject("date", LocalDate.class),
-                resultSet.getObject("time", LocalTime.class)
+                reservationTime
         );
-        return reservation;
     };
 
     public Reservation findReservationById(long id) {
-        String sql = "select id, name, date, time from reservation where id = ?";
+        String sql = "select r.id as reservation_id, r.name, r.date, t.id as time_id, t.start_at " +
+                "from reservation as r " +
+                "inner join reservation_time as t on r.time_id = t.id " +
+                "where r.id = ?";
         return jdbcTemplate.queryForObject(sql, actorRowMapper, id);
     }
 
     public List<Reservation> findAllReservations() {
-        String sql = "select id, name, date, time from reservation";
+        String sql = "select r.id as reservation_id, r.name, r.date, t.id as time_id, t.start_at " +
+                "from reservation as r " +
+                "inner join reservation_time as t on r.time_id = t.id";
         return jdbcTemplate.query(sql, actorRowMapper);
     }
 }
--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -1,19 +1,20 @@
 package roomescape.reservation;
 
+import roomescape.reservationtime.ReservationTime;
+
 import java.time.LocalDate;
-import java.time.LocalTime;
 
 public class Reservation {
 
     private Long id;
     private String name;
     private LocalDate date;
-    private LocalTime time;
+    private ReservationTime time;
 
     public Reservation() {
     }
 
-    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+    public Reservation(Long id, String name, LocalDate date, ReservationTime time) {
         this.id = id;
         this.name = name;
         this.date = date;
@@ -32,17 +33,8 @@ public class Reservation {
         return date;
     }
 
-    public LocalTime getTime() {
+    public ReservationTime getTime() {
         return time;
     }
 
-    public static Reservation toEntity(Reservation reservation, Long id) {
-        return new Reservation(id, reservation.getName(), reservation.getDate(), reservation.getTime());
-    }
-
-    public void update(Reservation newReservation) {
-        this.name = newReservation.name;
-        this.date = newReservation.date;
-        this.time = newReservation.time;
-    }
 }

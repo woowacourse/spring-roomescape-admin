631ebb44b5c507cc640375c975db20d555f0cbb5
haechanmoon <moonhc.dev@gmail.com>
2026-04-30 13:08:49 +0900

    feat: 시간관리 3단계 구현

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -13,48 +13,70 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Reservation;
+import roomescape.domain.ReservationTime;
+import roomescape.dto.ReservationRequest;
 
 @RestController
 public class ReservationController {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public ReservationController(JdbcTemplate jdbcTemplate){
+    public ReservationController(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @GetMapping("/reservations")
-    public List<Reservation> getReservations(){
-        String sql = "SELECT id, name, date, time FROM reservation";
-        return jdbcTemplate.query(sql, (resultSet, rowNum)-> {
-            long id = resultSet.getLong("id");
+    public List<Reservation> getReservations() {
+        String sql = """
+                 SELECT 
+                     r.id as reservation_id, 
+                     r.name, 
+                     r.date, 
+                     rt.id as time_id, 
+                     rt.start_at as time_value  
+                 FROM reservation r
+                 inner join reservation_time rt
+                 ON r.time_id = rt.id;
+                """;
+
+        return jdbcTemplate.query(sql, (resultSet, rowNum) -> {
+            long id = resultSet.getLong("reservation_id");
             String name = resultSet.getString("name");
             String date = resultSet.getString("date");
-            String time = resultSet.getString("time");
 
+            long timeId = resultSet.getLong("time_id");
+            String timeValue = resultSet.getString("time_value");
+            ReservationTime time = new ReservationTime(timeId, timeValue);
             return new Reservation(id, name, date, time);
         });
     }
 
     @PostMapping("/reservations")
-    public Reservation postReservations(@RequestBody Reservation reservation){
-        String sql = "INSERT INTO reservation (name, date, time) VALUES(?,?,?)";
+    public Reservation postReservations(@RequestBody ReservationRequest reservationRequest) {
+        String sql = "INSERT INTO reservation (name, date, time_id) VALUES(?,?,?)";
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(connection -> {
             PreparedStatement preparedStatement = connection.prepareStatement(sql, new String[]{"id"});
 
-            preparedStatement.setString(1, reservation.getName());
-            preparedStatement.setString(2, reservation.getDate());
-            preparedStatement.setString(3, reservation.getTime());
+            preparedStatement.setString(1, reservationRequest.getName());
+            preparedStatement.setString(2, reservationRequest.getDate());
+            preparedStatement.setLong(3, reservationRequest.getTimeId());
 
             return preparedStatement;
         }, keyHolder);
         long newId = Objects.requireNonNull(keyHolder.getKey()).longValue();
-        return new Reservation(newId, reservation.getName(), reservation.getDate(), reservation.getTime());
+        String findTimeSql = "select id, start_at from reservation_time where id = ?";
+        ReservationTime reservationTime = jdbcTemplate.queryForObject(findTimeSql, (resultSet, rowNum)->{
+            long id = resultSet.getLong("id");
+            String startAt = resultSet.getString("start_at");
+            return new ReservationTime(id, startAt);
+        }, reservationRequest.getTimeId());
+
+        return new Reservation(newId, reservationRequest.getName(), reservationRequest.getDate(), reservationTime);
     }
 
     @DeleteMapping("/reservations/{id}")
-    public void deleteReservation(@PathVariable Long id){
+    public void deleteReservation(@PathVariable Long id) {
         String sql = "DELETE FROM reservation WHERE id = ?";
         jdbcTemplate.update(sql, id);
     }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +21,20 @@ import roomescape.domain.Reservation;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
+
+    @BeforeEach
+    void 시간_넣어주기(){
+        Map<String, String> timeParams = new HashMap<>();
+        timeParams.put("startAt", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(timeParams)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(200);
+    }
+
 
     @Test
     void 예약_조회() {
@@ -32,10 +47,10 @@ public class MissionStepTest {
 
     @Test
     void 예약_추가_및_삭제() {
-        Map<String, String> params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
-        params.put("time", "15:40");
+        params.put("timeId", 1);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -79,7 +94,7 @@ public class MissionStepTest {
 
     @Test
     void DB_조회_API_전환() {
-        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)", "브라운", "2023-08-05", "1");
 
         List<Reservation> reservations = RestAssured.given().log().all()
                 .when().get("/reservations")
@@ -94,10 +109,10 @@ public class MissionStepTest {
 
     @Test
     void DB_추가_삭제_API_전환() {
-        Map<String, String> params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
         params.put("name", "브라운");
         params.put("date", "2023-08-05");
-        params.put("time", "10:00");
+        params.put("timeId", 1);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -120,16 +135,6 @@ public class MissionStepTest {
 
     @Test
     void 시간_관리_API() {
-        Map<String, String> params = new HashMap<>();
-        params.put("startAt", "10:00");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/times")
-                .then().log().all()
-                .statusCode(200);
-
         RestAssured.given().log().all()
                 .when().get("/times")
                 .then().log().all()
@@ -162,5 +167,4 @@ public class MissionStepTest {
                 .statusCode(200)
                 .body("size()", is(1));
     }
-
 }

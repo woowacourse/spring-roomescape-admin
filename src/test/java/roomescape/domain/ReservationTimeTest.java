b21766769fa6d0c08d5f36da5b6a0f30698ff155
kdongsu5509 <kod66170@gmail.com>
2026-05-01 16:31:26 +0900

    fix : delete not meaningful test
    - 의미 없어진 도메인에서의 시간 형식 검증 테스트 삭제

--- src/test/java/roomescape/domain/ReservationTimeTest.java
+++ src/test/java/roomescape/domain/ReservationTimeTest.java
@@ -3,8 +3,6 @@ package roomescape.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class ReservationTimeTest {
 
@@ -22,22 +20,5 @@ class ReservationTimeTest {
         Assertions.assertThatThrownBy(() -> new ReservationTime(null, null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("startAt은 도메인에서 필수값이며 공백일 수 없습니다.");
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"24:00", "1:00", "12:60", "abc", "12:0"})
-    @DisplayName("시간 형식이 올바르지 않으면 오류가 발생한다")
-    void startAt_invalid_format_throw_exception(String invalidTime) {
-        Assertions.assertThatThrownBy(() -> new ReservationTime(null, invalidTime))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("시간 형식이 올바르지 않습니다 : " + invalidTime);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"00:00", "12:00", "23:59"})
-    @DisplayName("올바른 시간 형식이면 성공적으로 생성된다")
-    void startAt_valid_format_success(String validTime) {
-        Assertions.assertThatCode(() -> new ReservationTime(null, validTime))
-                .doesNotThrowAnyException();
     }
 }

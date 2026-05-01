a3fb497cd4896608546e8bdd8baa750332e0033e
armd482 <armd482@naver.com>
2026-05-01 15:38:17 +0900

    feat: DataReferencedException 추가

--- src/main/java/roomescape/exception/ErrorMessage.java
+++ src/main/java/roomescape/exception/ErrorMessage.java
@@ -7,6 +7,8 @@ public enum ErrorMessage {
     RESERVATION_TIME_NOT_FOUND(HttpStatus.NO_CONTENT, "해당 id의 시간 데이터를 찾을 수 없습니다."),
 
     INVALID_RESERVATION_TIME_ID(HttpStatus.NOT_FOUND, "유효하지 않은 시간 id입니다."),
+
+    CANNOT_DELETE_RESERVATION_TIME_IN_USE(HttpStatus.CONFLICT, "해당 시간을 참조하는 예약 데이터가 존재하기 때문에 삭제할 수 없습니다.")
     ;
 
     private final HttpStatus httpStatus;
--- src/main/java/roomescape/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/handler/GlobalExceptionHandler.java
@@ -4,12 +4,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.exception.CustomException;
+import roomescape.exception.DataReferencedException;
 import roomescape.exception.NotFoundResourceException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(NotFoundResourceException.class)
-    public ResponseEntity<String> handleNotFoundResourceException(CustomException customException) {
+    @ExceptionHandler({NotFoundResourceException.class, DataReferencedException.class})
+    public ResponseEntity<String> handleCustomException(CustomException customException) {
         return new ResponseEntity<>(customException.getMessage(), customException.getStatus());
     }
 }

d75ced89262595476ac34242782b612524dc8d72
jyt6640 <jyt6640@gmail.com>
2026-05-01 17:50:49 +0900

    feat: 에러코드 추가

--- src/main/java/roomescape/global/exception/customException/ReservationException.java
+++ src/main/java/roomescape/global/exception/customException/ReservationException.java
@@ -1,4 +1,4 @@
-package roomescape.global.exception;
+package roomescape.global.exception.customException;
 
 public class ReservationException extends RuntimeException {
 
--- src/main/java/roomescape/global/exception/customException/ReservationTimeException.java
+++ src/main/java/roomescape/global/exception/customException/ReservationTimeException.java
@@ -1,4 +1,4 @@
-package roomescape.global.exception;
+package roomescape.global.exception.customException;
 
 public class ReservationTimeException extends RuntimeException{
 

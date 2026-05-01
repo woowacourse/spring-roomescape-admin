676ef51802af1c2322052f555cf4bf1f5e3dd89d
MODUGGAGI <kiroro0814@naver.com>
2026-05-01 15:03:06 +0900

    refactor: 불필요한 @AllArgsContructor 삭제

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -1,14 +1,12 @@
 package roomescape.reservation.domain;
 
 import java.time.LocalDate;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import roomescape.reservationtime.domain.ReservationTime;
 
 @Getter
 @Builder
-@AllArgsConstructor
 public class Reservation {
 
     private final Long id;
--- src/main/java/roomescape/reservationtime/domain/ReservationTime.java
+++ src/main/java/roomescape/reservationtime/domain/ReservationTime.java
@@ -1,13 +1,11 @@
 package roomescape.reservationtime.domain;
 
 import java.time.LocalTime;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-@AllArgsConstructor
 public class ReservationTime {
     private final Long id;
     private final LocalTime startAt;

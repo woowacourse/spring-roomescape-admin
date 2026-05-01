ce9a81ff8a09c80680e44951e3836283092b7536
Jeshin Kim <jjason0904@cau.ac.kr>
2026-05-01 22:57:06 +0900

    feat(ReservationExceptionHandler): Reservation 도메인에서 발생하는 ReservationTimeException 처리 추가

--- src/main/java/roomescape/reservation/exception/ReservationExceptionHandler.java
+++ src/main/java/roomescape/reservation/exception/ReservationExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.exception.ErrorResponse;
+import roomescape.reservationtime.exception.ReservationTimeException;
 
 @RestControllerAdvice(basePackages = "roomescape.reservation")
 public class ReservationExceptionHandler {
@@ -18,5 +19,15 @@ public class ReservationExceptionHandler {
                 .status(e.getErrorCode().getStatus())
                 .body(response);
     }
-}
 
+    @ExceptionHandler(ReservationTimeException.class)
+    public ResponseEntity<ErrorResponse> handleReservationTimeException(ReservationTimeException e) {
+        System.err.println("[ERROR] " + e.getMessage());
+
+        ErrorResponse response = new ErrorResponse(e.getUserMessage());
+
+        return ResponseEntity
+                .status(e.getErrorCode().getStatus())
+                .body(response);
+    }
+}

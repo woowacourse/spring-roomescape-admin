c1b826946c302879d23405a5f0fbf180cf2a9f3f
Soojin6943 <sjhan0711@gmail.com>
2026-05-01 21:10:01 +0900

    refactor(test): 입출력 형식 변경으로 깨지던 테스트 수정

--- src/test/java/roomescape/JDBCTemplateTest.java
+++ src/test/java/roomescape/JDBCTemplateTest.java
@@ -9,71 +9,84 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import roomescape.domain.Reservation;
 
-/**
- * 단계 2 요구사항 테스트
- *  - 단계 3에서 입력 형식과 출력 형식이 변경되어 해당 테스트는 실패합니다.
- */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class JDBCTemplateTest {
-//    @Autowired
-//    private JdbcTemplate jdbcTemplate;
-//
-//    @Test
-//    void 데이터베이스_연동() {
-//        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
-//            assertThat(connection).isNotNull();
-//            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
-//            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    @Test
-//    void DB_조회_API_전환() {
-//        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
-//
-//        List<Reservation> reservations = RestAssured.given().log().all()
-//                .when().get("/reservations")
-//                .then().log().all()
-//                .statusCode(200).extract()
-//                .jsonPath().getList(".", Reservation.class);
-//
-//        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM reservation", Integer.class);
-//
-//        assertThat(reservations.size()).isEqualTo(count);
-//    }
-//
-//    @Test
-//    void DB_추가_삭제_API_전환() {
-//        Map<String, String> params = new HashMap<>();
-//        params.put("name", "브라운");
-//        params.put("date", "2023-08-05");
-//        params.put("time", "10:00");
-//
-//        RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .body(params)
-//                .when().post("/reservations")
-//                .then().log().all()
-//                .statusCode(200);
-//
-//        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
-//        assertThat(count).isEqualTo(1);
-//
-//        RestAssured.given().log().all()
-//                .when().delete("/reservations/1")
-//                .then().log().all()
-//                .statusCode(200);
-//
-//        Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
-//        assertThat(countAfterDelete).isEqualTo(0);
-//    }
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    @AfterEach
+    void cleanUp() {
+        jdbcTemplate.execute("DELETE FROM reservation");
+        jdbcTemplate.execute("DELETE FROM reservation_time");
+    }
+
+    @Test
+    void 데이터베이스_연동() {
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.getCatalog()).isEqualTo("DATABASE");
+            assertThat(connection.getMetaData().getTables(null, null, "RESERVATION", null).next()).isTrue();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void DB_조회_API_전환() {
+
+        Long timeId = insertReservationTime();
+
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)", "브라운", "2023-08-05", timeId);
+
+        List<Reservation> reservations = RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200).extract()
+                .jsonPath().getList(".", Reservation.class);
+
+        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM reservation", Integer.class);
+
+        assertThat(reservations.size()).isEqualTo(count);
+    }
+
+    @Test
+    void DB_추가_삭제_API_전환() {
+
+        insertReservationTime();
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2023-08-05");
+        params.put("timeId", 1);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200);
+
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+        assertThat(count).isEqualTo(1);
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(200);
+
+        Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+        assertThat(countAfterDelete).isEqualTo(0);
+    }
+
+    private Long insertReservationTime() {
+        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES (?)", "10:00");
+        return jdbcTemplate.queryForObject("SELECT id FROM reservation_time WHERE start_at = ?", Long.class, "10:00");
+    }
 }

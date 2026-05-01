a91fee078cbe3293a0e78b7d5d91368a7fdb4f29
armd482 <armd482@naver.com>
2026-05-01 20:12:36 +0900

    refactor: 잘못된 timeId로 reservation 생성 응답 코드 변경, 미사용 코드 제거

--- src/main/java/roomescape/exception/ErrorMessage.java
+++ src/main/java/roomescape/exception/ErrorMessage.java
@@ -3,10 +3,7 @@ package roomescape.exception;
 import org.springframework.http.HttpStatus;
 
 public enum ErrorMessage {
-    RESERVATION_NOT_FOUND(HttpStatus.NO_CONTENT, "해당 id의 예약 데이터를 찾을 수 없습니다."),
-    RESERVATION_TIME_NOT_FOUND(HttpStatus.NO_CONTENT, "해당 id의 시간 데이터를 찾을 수 없습니다."),
-
-    INVALID_RESERVATION_TIME_ID(HttpStatus.NOT_FOUND, "유효하지 않은 시간 id입니다."),
+    INVALID_RESERVATION_TIME_ID(HttpStatus.BAD_REQUEST, "유효하지 않은 시간 id입니다."),
 
     CANNOT_DELETE_RESERVATION_TIME_IN_USE(HttpStatus.CONFLICT, "해당 시간을 참조하는 예약 데이터가 존재하기 때문에 삭제할 수 없습니다.")
     ;

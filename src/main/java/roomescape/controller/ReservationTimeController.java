9e7e5871c6dd1bd8bc4db88b616191a640c09381
smiinii <sm020320@naver.com>
2026-05-01 14:30:19 +0900

    refactor(ReservationTimeController): 불필요한 import 제거

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.ReservationTimeRequest;
 import roomescape.dto.ReservationTimeResponse;
-import roomescape.model.ReservationTime;
 import roomescape.service.ReservationTimeService;
 
 import java.util.List;

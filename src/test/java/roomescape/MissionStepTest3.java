c2b43779e62ccfd4bd2e4a5bc8e8f662c6ac625f
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-05-01 19:44:28 +0900

    test: 상태코드 변경으로 인한 테스트의 상태코드도 변경

--- src/test/java/roomescape/MissionStepTest3.java
+++ src/test/java/roomescape/MissionStepTest3.java
@@ -24,7 +24,7 @@ public class MissionStepTest3 {
                 .body(params)
                 .when().post("/times")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(201);
 
         RestAssured.given().log().all()
                 .when().get("/times")
@@ -35,7 +35,7 @@ public class MissionStepTest3 {
         RestAssured.given().log().all()
                 .when().delete("/times/1")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(204);
     }
 
     @Test
@@ -53,14 +53,14 @@ public class MissionStepTest3 {
                 .body(time)
                 .when().post("/times")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(201);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(reservation)
                 .when().post("/reservations")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(201);
 
         RestAssured.given().log().all()
                 .when().get("/reservations")

6ab12de818de26aa04b8460a8464082fa1e7d3b3
dahye <dahye9011@naver.com>
2026-05-01 19:17:56 +0900

    test: @DirtiesContext 추가

--- src/test/java/roomescape/LayerSeparationTest.java
+++ src/test/java/roomescape/LayerSeparationTest.java
@@ -7,10 +7,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import roomescape.domain.reservation.controller.ReservationController;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 public class LayerSeparationTest {
     @Autowired
     private ReservationController reservationController;

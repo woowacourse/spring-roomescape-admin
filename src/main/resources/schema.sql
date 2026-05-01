4db66bfdd3b0491cfdeedc6ee9f035b9d4e1f950
SooHyeon Lee <dalsu4594@naver.com>
2026-05-02 00:38:33 +0900

    refactor: schema.sql 수정
    - date, start_at 타입(DATE, TIME) 수정
    - name 길이 20으로 제한

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,14 +3,14 @@ DROP TABLE IF EXISTS reservation_time;
 
 CREATE TABLE reservation_time (
                                   id       BIGINT       NOT NULL AUTO_INCREMENT,
-                                  start_at VARCHAR(255) NOT NULL,
+                                  start_at TIME NOT NULL,
                                   PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation (
                              id      BIGINT       NOT NULL AUTO_INCREMENT,
-                             name    VARCHAR(255) NOT NULL,
-                             date    VARCHAR(255) NOT NULL,
+                             name    VARCHAR(20) NOT NULL,
+                             date    DATE NOT NULL,
                              time_id BIGINT,
                              PRIMARY KEY (id),
                              FOREIGN KEY (time_id) REFERENCES reservation_time (id)

08e7adba423a6263204a29b4faec557dc102d541
Chocoding1 <galmeagi2@naver.com>
2026-05-02 11:38:29 +0900

    refactor: String 타입이 아닌 필드에 대해서는 @NotBlank -> @NotNull 변경

--- src/main/java/roomescape/ReservationTimeSaveDto.java
+++ src/main/java/roomescape/ReservationTimeSaveDto.java
@@ -1,8 +1,8 @@
 package roomescape;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalTime;
 
-public record ReservationTimeSaveDto(@NotBlank LocalTime startAt) {
+public record ReservationTimeSaveDto(@NotNull LocalTime startAt) {
 }

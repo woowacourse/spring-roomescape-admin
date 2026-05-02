7f3d842a5c8c9f50c61502fedc07b175e0f8ac7c
MODUGGAGI <kiroro0814@naver.com>
2026-05-03 00:31:12 +0900

    refactor: equals 메서드에 id가 null인 경우 false 판별 로직 추가

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -1,14 +1,13 @@
 package roomescape.reservation.domain;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import roomescape.reservationtime.domain.ReservationTime;
 
 @Builder
 @Getter
-@EqualsAndHashCode(of = "id")
 public class Reservation {
 
     private final Long id;
@@ -27,5 +26,22 @@ public class Reservation {
                 .date(this.date)
                 .time(this.time)
                 .build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (id == null) {
+            return false;
+        }
+        Reservation that = (Reservation) o;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
     }
 }
--- src/main/java/roomescape/reservationtime/domain/ReservationTime.java
+++ src/main/java/roomescape/reservationtime/domain/ReservationTime.java
@@ -1,13 +1,12 @@
 package roomescape.reservationtime.domain;
 
 import java.time.LocalTime;
+import java.util.Objects;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Builder
 @Getter
-@EqualsAndHashCode(of = "id")
 public class ReservationTime {
     private final Long id;
     private final LocalTime startAt;
@@ -17,5 +16,22 @@ public class ReservationTime {
                 .id(generatedId)
                 .startAt(this.startAt)
                 .build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (id == null) {
+            return false;
+        }
+        ReservationTime that = (ReservationTime) o;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
     }
 }

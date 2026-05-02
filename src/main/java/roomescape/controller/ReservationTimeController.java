c50c23cac8786074b6e88172eed11847d5d751b2
symflee <symflee@gmail.com>
2026-05-02 11:35:27 +0900

    refactor: Controller를 RestController로 변경 작업

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -2,17 +2,16 @@ package roomescape.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.ReservationTime;
 import roomescape.service.ReservationTimeService;
 
-@Controller
+@RestController
 public class ReservationTimeController {
     private final ReservationTimeService reservationTimeService;
 
@@ -20,25 +19,21 @@ public class ReservationTimeController {
         this.reservationTimeService = reservationTimeService;
     }
 
-    @ResponseBody
     @PostMapping("/times")
     public ResponseEntity<ReservationTime> create(@RequestBody ReservationTime reservationTime) {
         return ResponseEntity.ok(reservationTimeService.create(reservationTime));
     }
 
-    @ResponseBody
     @GetMapping("/times")
     public ResponseEntity<List<ReservationTime>> readAll() {
         return ResponseEntity.ok(reservationTimeService.findAll());
     }
 
-    @ResponseBody
     @GetMapping("/times/{id}")
     public ResponseEntity<ReservationTime> read(@PathVariable Long id) {
         return ResponseEntity.ok(reservationTimeService.findById(id));
     }
 
-    @ResponseBody
     @DeleteMapping("/times/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         reservationTimeService.deleteById(id);

4835046c4326fd70aa4208da26305118f69a7411
dahye <dahye9011@naver.com>
2026-05-02 11:54:57 +0900

    feat: 도메인 검증 추가

--- src/main/java/roomescape/domain/reservation/Reservation.java
+++ src/main/java/roomescape/domain/reservation/Reservation.java
@@ -10,10 +10,39 @@ public class Reservation {
     private final ReservationTime time;
 
     public Reservation(Long id, String name, LocalDate date, ReservationTime time) {
+        validate(name, date, time);
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    private void validate(String name, LocalDate date, ReservationTime time) {
+        validateName(name);
+        validateDate(date);
+        validateTime(time);
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 예약자의 이름은 비어 있거나, 공백일 수 없습니다.");
+        }
+    }
+
+    private void validateDate(LocalDate date) {
+        if (date == null) {
+            throw new IllegalArgumentException("[ERROR] 예약 날짜는 비어 있을 수 없습니다.");
+        }
+
+        if (date.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("[ERROR] 예약 날짜는 과거일 수 없습니다.");
+        }
+    }
+
+    private void validateTime(ReservationTime time) {
+        if (time == null) {
+            throw new IllegalArgumentException("[ERROR] 예약 시간은 비어 있을 수 없습니다.");
+        }
     }
 
     public Long getId() {

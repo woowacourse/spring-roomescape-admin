ed7b4f40591a0527442bdfefd671cb260e76129a
poketopa <lhs5427ll@gmail.com>
2026-05-02 09:54:43 +0900

    feat(domain): 도메인 객체 record -> class 변경 후 검증 로직 추가

--- src/main/java/roomescape/domain/ReservationTime.java
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -1,4 +1,27 @@
 package roomescape.domain;
 
-public record ReservationTime(Long id, String startAt) {
+public class ReservationTime {
+
+    private final Long id;
+    private final String startAt;
+
+    public ReservationTime(final Long id, final String startAt) {
+        validate(startAt);
+        this.id = id;
+        this.startAt = startAt;
+    }
+
+    private void validate(final String startAt) {
+        if (startAt == null || startAt.isBlank()) {
+            throw new IllegalArgumentException("시작 시간은 비어있을 수 없습니다.");
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStartAt() {
+        return startAt;
+    }
 }

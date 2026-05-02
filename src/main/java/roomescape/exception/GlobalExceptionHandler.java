47244991e9ca99515830cb0f24593d840e32c218
MODUGGAGI <kiroro0814@naver.com>
2026-05-02 23:47:33 +0900

    refactor: 예외 처리기의 동작 범위 제한

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -9,9 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(annotations = {RestController.class})
 public class GlobalExceptionHandler {
 
     private static final Map<Class<?>, String> ERROR_MESSAGES = Map.of(

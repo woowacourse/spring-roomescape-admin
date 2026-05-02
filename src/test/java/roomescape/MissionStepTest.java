2ed37537fec770eab35973a6096c10665697ffc7
구민서 <minseo12379@gmail.com>
2026-05-03 04:55:17 +0900

    test: 존재하지 않는 예약 삭제 시 404 응답 검증 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -35,7 +35,7 @@ public class MissionStepTest {
                 .body(params)
                 .when().post("/reservations")
                 .then().log().all()
-                .statusCode(200)
+                .statusCode(201)
                 .body("id", is(1));
 
         RestAssured.given().log().all()
@@ -47,12 +47,20 @@ public class MissionStepTest {
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(204);
 
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(0));
+    }
+
+    @Test
+    void 존재하지_않는_예약_삭제() {
+        RestAssured.given().log().all()
+                .when().delete("/reservations/100")
+                .then().log().all()
+                .statusCode(404);
     }
 }

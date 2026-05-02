4fa2abea991736d504ed195999b5080393d1ec29
dahye <dahye9011@naver.com>
2026-05-02 15:30:27 +0900

    refactor: ReservationResponseDTO time 필드를 ReservationTimeResponseDTO로 교체

--- src/main/java/roomescape/domain/reservation/dto/ReservationResponseDTO.java
+++ src/main/java/roomescape/domain/reservation/dto/ReservationResponseDTO.java
@@ -2,16 +2,16 @@ package roomescape.domain.reservation.dto;
 
 import java.time.LocalDate;
 import roomescape.domain.reservation.Reservation;
-import roomescape.domain.time.ReservationTime;
+import roomescape.domain.time.dto.ReservationTimeResponseDTO;
 
-public record ReservationResponseDTO(Long id, String name, LocalDate date, ReservationTime time) {
+public record ReservationResponseDTO(Long id, String name, LocalDate date, ReservationTimeResponseDTO time) {
 
     public static ReservationResponseDTO from(Reservation reservation) {
         return new ReservationResponseDTO(
                 reservation.getId(),
                 reservation.getName(),
                 reservation.getDate(),
-                reservation.getTime()
+                ReservationTimeResponseDTO.from(reservation.getTime())
         );
     }
 }

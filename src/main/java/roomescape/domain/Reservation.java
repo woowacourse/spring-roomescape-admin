a21a7ac6f678fec6168f6fb9af6f6c3df7de5bbc
Jihyun3478 <leejihyun3478@gmail.com>
2026-05-02 15:37:25 +0900

    refactor: 사용하지 않는 setId() 메서드 삭제

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -23,10 +23,6 @@ public class Reservation {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }

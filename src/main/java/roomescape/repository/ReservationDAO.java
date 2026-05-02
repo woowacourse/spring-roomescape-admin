533eed9b57eaef6c3134284bea6a28e710a30e61
sun <sun007021@gmail.com>
2026-05-02 23:27:15 +0900

    refactor: DAO @Repository를 Component로 변경

--- src/main/java/roomescape/repository/ReservationDAO.java
+++ src/main/java/roomescape/repository/ReservationDAO.java
@@ -7,11 +7,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 
-@Repository
+@Component
 public class ReservationDAO {
 
     private final JdbcTemplate jdbcTemplate;

0df5d7bbd3842d63f51055778baf12279c344311
alstj2384 <ass91160@naver.com>
2026-05-02 17:51:24 +0900

    refactor: Controller Advice에서 IllegarArgumentException을 정상적으로 처리하도록 수정

--- src/main/java/roomescape/controller/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package roomescape.controller;
 
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<String> dataIntegrityViolationExceptionHandle(DataIntegrityViolationException e) {
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<String> dataAccessExceptionHandle(DataAccessException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body("요청한 시간이 예약되어 있어 삭제할 수 없습니다");
+                .body(e.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> illegalArgumentExceptionHandle(DataIntegrityViolationException e) {
+    public ResponseEntity<String> illegalArgumentExceptionHandle(IllegalArgumentException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(e.getMessage());
     }

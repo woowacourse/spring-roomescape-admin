283a6df0af49674ce61871c0a005e0688e74c4a0
wontop02 <wjy2002kr@gmail.com>
2026-05-02 17:13:13 +0900

    refactor: domain을 record에서 class로 변경

--- src/main/java/roomescape/domain/ReservationTime.java
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -2,5 +2,20 @@ package roomescape.domain;
 
 import java.time.LocalTime;
 
-public record ReservationTime(Long id, LocalTime startAt) {
+public class ReservationTime {
+    private final Long id;
+    private final LocalTime startAt;
+
+    public ReservationTime(Long id, LocalTime startAt) {
+        this.id = id;
+        this.startAt = startAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalTime getStartAt() {
+        return startAt;
+    }
 }

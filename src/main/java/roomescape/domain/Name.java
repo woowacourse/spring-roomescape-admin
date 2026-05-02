9859123db1e49f7a1b0792bd47fb9648981fcdf0
yItWorks <poiu7825@pusan.ac.kr>
2026-05-02 16:20:06 +0900

    refactor: Override equals(), hashCode()

--- src/main/java/roomescape/domain/Name.java
+++ src/main/java/roomescape/domain/Name.java
@@ -17,4 +17,18 @@ public class Name {
     public String toString() {
         return name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
 }

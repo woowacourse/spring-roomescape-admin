237b5e24c5dc2321cc29eb110a77a80d12f51c5b
snowshower <wonsb25@gmail.com>
2026-05-02 16:05:06 +0900

    style: 불필요한 import문 제거
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,7 +1,6 @@
 package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import roomescape.dto.ReservationRequestDTO;
 import roomescape.domain.Reservation;
--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -1,7 +1,6 @@
 package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import roomescape.domain.ReservationTime;
 import roomescape.service.ReservationTimeService;

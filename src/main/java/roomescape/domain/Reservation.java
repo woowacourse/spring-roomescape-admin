321fdc607ce608eb7a6218e964c8c1cc5a967bbd
haechanmoon <moonhc.dev@gmail.com>
2026-05-03 01:33:26 +0900

    feat: 이름, 시간, 기간 검증 로직 구현

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,5 +1,9 @@
 package roomescape.domain;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Reservation {
 
     private final Long id;
@@ -10,14 +14,18 @@ public class Reservation {
 
     public Reservation(Long id, String name, String date, ReservationTime time) {
         this.id = id;
+        validateNameFormat(name);
         this.name = name;
+        validateDateFormat(date);
         this.date = date;
         this.time = time;
     }
 
     public Reservation(String name, String date, ReservationTime time) {
         this.id = 0L;
+        validateNameFormat(name);
         this.name = name;
+        validateDateFormat(date);
         this.date = date;
         this.time = time;
     }
@@ -36,5 +44,23 @@ public class Reservation {
 
     public ReservationTime getTime() {
         return time;
+    }
+
+    private void validateNameFormat(String name) {
+        if(name == null || name.isBlank()){
+            throw new IllegalArgumentException("이름 형식은 2글자 이상 10글자 이하입니다");
+        }
+
+        if(name.length()<2|| name.length()>10){
+            throw new IllegalArgumentException("이름 형식은 2글자 이상 10글자 이하입니다");
+        }
+    }
+
+    private void validateDateFormat(String date) {
+        try{
+            LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        } catch (DateTimeParseException | NullPointerException e){
+            throw new IllegalArgumentException("날짜 형식은 yyyy-MM-dd 입니다 예) 2023-04-13");
+        }
     }
 }
--- src/main/java/roomescape/domain/ReservationTime.java
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -1,5 +1,9 @@
 package roomescape.domain;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class ReservationTime {
 
     private final Long id;
@@ -7,6 +11,7 @@ public class ReservationTime {
 
     public ReservationTime(Long id, String startAt) {
         this.id = id;
+        validateStartAtTimeFormat(startAt);
         this.startAt = startAt;
     }
 
@@ -16,5 +21,13 @@ public class ReservationTime {
 
     public String getStartAt() {
         return startAt;
+    }
+
+    private void validateStartAtTimeFormat(String startAt) {
+        try{
+            LocalTime.parse(startAt, DateTimeFormatter.ofPattern("HH:mm"));
+        } catch (DateTimeParseException | NullPointerException e){
+        throw new IllegalArgumentException("시간 형식은 HH:mm 입니다. 예) 15:23");
+        }
     }
 }

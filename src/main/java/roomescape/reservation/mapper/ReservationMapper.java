101e9669b362d3867dc6129b5bbc2edd4a0958ed
rin <chaerin0916@gmail.com>
2026-05-03 00:38:09 +0900

    fix(ReservationMapper): 매핑 오류 수정
    - ReservationResponse에 name, date, time 필드가 누락되었던 버그 수정

--- src/main/java/roomescape/reservation/mapper/ReservationMapper.java
+++ src/main/java/roomescape/reservation/mapper/ReservationMapper.java
@@ -1,9 +1,12 @@
 package roomescape.reservation.mapper;
 
+import java.util.Optional;
 import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.dto.ReservationRequest;
 import roomescape.reservation.dto.ReservationResponse;
 import roomescape.reservationtime.domain.ReservationTime;
+import roomescape.reservationtime.dto.ReservationTimeResponse;
+import roomescape.reservationtime.mapper.ReservationTimeMapper;
 
 public class ReservationMapper {
 
@@ -20,6 +23,9 @@ public class ReservationMapper {
     public static ReservationResponse toResponse(Reservation reservation) {
         return ReservationResponse.builder()
                 .id(reservation.getId())
+                .name(reservation.getName())
+                .date(reservation.getDate())
+                .time(ReservationTimeMapper.toResponse(reservation.getTime()))
                 .build();
     }
 }

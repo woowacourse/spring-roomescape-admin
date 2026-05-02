3ed05dfaee254e31d9416a4b8624c546fdef6fc6
Jeshin Kim <jjason0904@cau.ac.kr>
2026-05-03 00:03:59 +0900

    refactor(Reservation): id같으면 같은값으로 판별하게끔 변경

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -4,4 +4,23 @@ import java.time.LocalDate;
 import roomescape.reservationtime.ReservationTime;
 
 public record Reservation(Long id, String name, LocalDate date, ReservationTime time) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Reservation other)) {
+            return false;
+        }
+        if (this.id == null || other.id == null) {
+            return false;
+        }
+        return this.id.equals(other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return (id == null) ? 0 : id.hashCode();
+    }
 }

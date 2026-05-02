ede475eaf8de2d6d969d8b9ab13aa16928cafbc7
koreaioi <koreaioi@gachon.ac.kr>
2026-05-02 18:29:10 +0900

    refactor: flyway 삭제

--- src/main/resources/db/migration/V1__schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE reservation
-(
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    date VARCHAR(255) NOT NULL,
-    time VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);

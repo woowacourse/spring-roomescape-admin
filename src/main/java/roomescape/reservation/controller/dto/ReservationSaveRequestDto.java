212ce9eba01e97ea725d9ccfda150eb19b0d884d
Yoonhojoon <dbsghwns1209@gmail.com>
2026-05-02 15:45:23 +0900

    refactor: ReservatopmSaveRequestDto record로 변경

--- src/main/java/roomescape/reservation/controller/dto/ReservationSaveRequestDto.java
+++ src/main/java/roomescape/reservation/controller/dto/ReservationSaveRequestDto.java
@@ -2,22 +2,7 @@ package roomescape.reservation.controller.dto;
 
 import roomescape.reservation.service.dto.ReservationSaveServiceDto;
 
-public class ReservationSaveRequestDto {
-    private String name;
-    private String date;
-    private Long timeId;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public void setTimeId(Long timeId) {
-        this.timeId = timeId;
-    }
+public record ReservationSaveRequestDto(String name, String date, Long timeId) {
 
     public ReservationSaveServiceDto toServiceDto() {
         return new ReservationSaveServiceDto(name, date, timeId);

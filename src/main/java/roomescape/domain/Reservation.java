a7c53a0b3f8c9f097e47f9f341392d16985ba44c
yj9107v <yj9107v@gmail.com>
2026-05-02 18:27:24 +0900

    feat: 도메인 검증 메서드 추가
    - Reservation id를 제외한 name, date, time에 대한 검증(null, 빈값) 메서드 추가
    - ReservationTime id 제외한 startAt 검증(null) 메서드 추가

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -4,6 +4,10 @@ import java.time.LocalDate;
 
 public class Reservation {
 
+    public static final String RESERVATION_NAME_IS_NOT_EMPTY = "예약자 이름은 비어 있을 수 없습니다.";
+    public static final String RESERVATION_DATE_IS_NOT_EMPTY = "예약 날짜는 비어 있을 수 없습니다.";
+    public static final String RESERVATION_TIME_IS_NOT_EMPTY = "예약 시간은 비어 있을 수 없습니다.";
+
     private Long id;
     private String name;
     private LocalDate date;
@@ -17,10 +21,32 @@ public class Reservation {
     }
 
     public Reservation(Long id, String name, LocalDate date, ReservationTime time) {
+        validateName(name);
+        validateDate(date);
+        validateTime(time);
+
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(RESERVATION_NAME_IS_NOT_EMPTY);
+        }
+    }
+
+    private void validateDate(LocalDate date) {
+        if (date == null) {
+            throw new IllegalArgumentException(RESERVATION_DATE_IS_NOT_EMPTY);
+        }
+    }
+
+    private void validateTime(ReservationTime time) {
+        if (time == null) {
+            throw new IllegalArgumentException(RESERVATION_TIME_IS_NOT_EMPTY);
+        }
     }
 
     public Long getId() {
--- src/main/java/roomescape/domain/ReservationTime.java
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -4,6 +4,8 @@ import java.time.LocalTime;
 
 public class ReservationTime {
 
+    public static final String START_AT_IS_NOT_EMPTY = "시간은 비어 있을 수 없습니다.";
+
     private Long id;
     private LocalTime startAt;
 
@@ -15,8 +17,16 @@ public class ReservationTime {
     }
 
     public ReservationTime(Long id, LocalTime startAt) {
+        validateTime(startAt);
+
         this.id = id;
         this.startAt = startAt;
+    }
+
+    private void validateTime(LocalTime startAt) {
+        if (startAt == null) {
+            throw new IllegalArgumentException(START_AT_IS_NOT_EMPTY);
+        }
     }
 
     public Long getId() {

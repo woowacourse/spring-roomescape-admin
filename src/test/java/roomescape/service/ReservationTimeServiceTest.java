2ef6ba903dcba305053bacb9cc123eaa12426a80
koreaioi <koreaioi@gachon.ac.kr>
2026-05-03 01:02:56 +0900

    test(ReservationTimeServiceTest): Register 테스트 코드 추가

--- src/test/java/roomescape/service/ReservationTimeServiceTest.java
+++ src/test/java/roomescape/service/ReservationTimeServiceTest.java
@@ -7,10 +7,12 @@ import roomescape.domain.ReservationTime;
 import roomescape.fixture.TestReservationTimeDao;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReservationTimeServiceTest {
 
+    private static final int INSERT_INCREMENT = 1;
     private static final LocalTime time = LocalTime.of(10, 0);
     private static final ReservationTime reservationTime = new ReservationTime(time);
 
@@ -43,6 +45,43 @@ public class ReservationTimeServiceTest {
             // then
             Assertions.assertThat(actual)
                     .hasSize(reservationTimes.size());
+        }
+
+    }
+
+    @Nested
+    class Register {
+
+        @Test
+        void 등록시_예약시간_데이터수가_1증가한다() {
+            // given
+            List<ReservationTime> emptyReservationTimes = List.of();
+            ReservationTimeService service = new ReservationTimeService(
+                    new TestReservationTimeDao(emptyReservationTimes)
+            );
+
+            // when
+            service.register(time);
+
+            // then
+            Assertions.assertThat(service.readAll())
+                    .hasSize(emptyReservationTimes.size() + INSERT_INCREMENT);
+        }
+
+        @Test
+        void 예약시간을_등록하면_반환된_시작시간이_입력값과_일치한다() {
+            // given
+            List<ReservationTime> emptyReservationTimes = List.of();
+            ReservationTimeService service = new ReservationTimeService(
+                    new TestReservationTimeDao(emptyReservationTimes)
+            );
+
+            // when
+            ReservationTime actual = service.register(time);
+
+            // then
+            Assertions.assertThat(actual.getStartAt())
+                    .isEqualTo(time);
         }
 
     }

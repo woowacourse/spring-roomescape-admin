cb289c885986f65ff4395f26923738d763243505
Andrew Kim <38521355+nn98@users.noreply.github.com>
2026-05-02 15:54:03 +0900

    refactor(feedback03): 비영속(Transient) Reservation 팩토리 메서드 추가

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,4 +1,10 @@
 package roomescape.domain;
 
-public record Reservation(long id, String name, String date, ReservationTime reservationTime) {
+import roomescape.controller.ReservationRequest;
+
+public record Reservation(long id, String name, String date, long reservationTimeId) {
+
+    public static Reservation transientOf(ReservationRequest reservationRequest) {
+        return new Reservation(0, reservationRequest.name(), reservationRequest.date(), reservationRequest.timeId());
+    }
 }

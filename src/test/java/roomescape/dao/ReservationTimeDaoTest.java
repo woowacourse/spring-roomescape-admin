223bac5c38c63a064f6cfcd682d6b2cb193d05f6
koreaioi <koreaioi@gachon.ac.kr>
2026-05-02 21:35:18 +0900

    test: ReservationTimeDao 테스트 클래스 추가

--- src/test/java/roomescape/dao/ReservationTimeDaoTest.java
+++ src/test/java/roomescape/dao/ReservationTimeDaoTest.java
@@ -1,22 +1,30 @@
 package roomescape.dao;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
+import roomescape.domain.ReservationTime;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.time.LocalTime;
 
-public class ReservationTimeDaoTest {
+class ReservationTimeDaoTest {
 
-    private JdbcTemplate jdbcTemplate;
+    private static final int hour = 11;
+    private static final int minute = 4;
+    private static final LocalTime time = LocalTime.of(hour, minute);
+    private static final ReservationTime reservationTime = new ReservationTime(time);
+
     private JdbcReservationTimeDao reservationTimeDao;
 
-    @BeforeAll
-    static void setUpDatabase() throws SQLException {
-        DriverManagerDataSource dataSource = getDriverManagerDataSource();
+    @BeforeEach
+    void setUp() throws SQLException {
+        DataSource dataSource = generateDataSource();
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        reservationTimeDao = new JdbcReservationTimeDao(jdbcTemplate);
 
         ScriptUtils.executeSqlScript(
                 dataSource.getConnection(),
@@ -24,20 +32,7 @@ public class ReservationTimeDaoTest {
         );
     }
 
-    @BeforeEach
-    void setUp() throws SQLException {
-        DriverManagerDataSource dataSource = getDriverManagerDataSource();
-
-        jdbcTemplate = new JdbcTemplate(dataSource);
-        reservationTimeDao = new JdbcReservationTimeDao(jdbcTemplate);
-
-        ScriptUtils.executeSqlScript(
-                dataSource.getConnection(),
-                new ClassPathResource("test-clear-schema.sql")
-        );
-    }
-
-    private static DriverManagerDataSource getDriverManagerDataSource() {
+    private static DataSource generateDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
         dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");

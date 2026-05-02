4bd54ea724532e48da4d51e98bae721cb7549b2e
armd482 <armd482@naver.com>
2026-05-02 11:47:15 +0900

    feat: memory reservationTime repository 구현

--- src/main/java/roomescape/repository/reservationTime/JdbcReservationTimeRepository.java
+++ src/main/java/roomescape/repository/reservationTime/JdbcReservationTimeRepository.java
@@ -3,12 +3,14 @@ package roomescape.repository.reservationTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 import roomescape.dao.ReservationTimeDao;
 import roomescape.domain.ReservationTime.ReservationTime;
 import roomescape.domain.ReservationTime.ReservationTimeCommand;
 
 @Repository
+@Profile("web")
 public class JdbcReservationTimeRepository implements ReservationTimeRepository {
     private final ReservationTimeDao reservationTimeDao;
 

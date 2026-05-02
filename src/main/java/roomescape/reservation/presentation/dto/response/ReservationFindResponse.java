b3e1a84e927b711a480ea3bd163d1771193918c0
조상준 <juncho12011201@gmail.com>
2026-05-02 16:23:34 +0900

    refactor: response 객체 유효 범위 최소한 지정

--- src/main/java/roomescape/reservation/presentation/dto/response/ReservationFindResponse.java
+++ src/main/java/roomescape/reservation/presentation/dto/response/ReservationFindResponse.java
@@ -1,12 +1,11 @@
 package roomescape.reservation.presentation.dto.response;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import roomescape.reservation.presentation.dto.response.dto.TimeInformation;
 
 public record ReservationFindResponse(@NotNull Long id,
-                                      @NotEmpty String name,
+                                      @NotNull String name,
                                       @NotNull LocalDate date,
                                       @NotNull TimeInformation time) {
 }
--- src/main/java/roomescape/reservation/presentation/dto/response/ReservationTimeFindResponse.java
+++ src/main/java/roomescape/reservation/presentation/dto/response/ReservationTimeFindResponse.java
@@ -1,9 +1,8 @@
 package roomescape.reservation.presentation.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 
 public record ReservationTimeFindResponse(@NotNull Long id,
-                                          @JsonFormat(pattern = "HH:mm") @NotNull LocalTime startAt) {
+                                          @NotNull LocalTime startAt) {
 }

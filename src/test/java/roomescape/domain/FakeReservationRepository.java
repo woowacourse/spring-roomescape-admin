453dbf4efc02dd878cae159a01e75700244250a2
yj9107v <yj9107v@gmail.com>
2026-05-02 18:35:58 +0900

    test: service 테스트 추가
    - 예약, 예약 시간 Repository의 Fake 객체 생성
    - 추가/조회/생성 테스트

---
+++ src/test/java/roomescape/domain/FakeReservationRepository.java
@@ -0,0 +1,42 @@
+package roomescape.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import roomescape.dao.ReservationRepository;
+
+public class FakeReservationRepository implements ReservationRepository {
+
+    private AtomicLong id = new AtomicLong(1);
+    private List<Reservation> reservations = new ArrayList<>();
+
+    @Override
+    public List<Reservation> findAll() {
+        return reservations;
+    }
+
+    @Override
+    public Reservation findById(Long id) {
+        return reservations.stream()
+                .filter(reservation -> reservation.getId().equals(id))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    @Override
+    public Reservation save(Reservation reservation) {
+        Reservation savedReservation = new Reservation(
+                id.getAndIncrement(),
+                reservation.getName(),
+                reservation.getDate(),
+                reservation.getTime()
+        );
+        reservations.add(savedReservation);
+        return savedReservation;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        reservations.removeIf(reservation -> reservation.getId().equals(id));
+    }
+}
---
+++ src/test/java/roomescape/service/ReservationServiceTest.java
@@ -0,0 +1,128 @@
+package roomescape.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.Test;
+import roomescape.dao.ReservationRepository;
+import roomescape.dao.ReservationTimeRepository;
+import roomescape.domain.FakeReservationRepository;
+import roomescape.domain.FakeReservationTimeRepository;
+import roomescape.domain.Reservation;
+import roomescape.domain.ReservationTime;
+import roomescape.dto.ReservationRequest;
+
+class ReservationServiceTest {
+
+    private final ReservationRepository reservationRepository = new FakeReservationRepository();
+    private final ReservationTimeRepository reservationTimeRepository = new FakeReservationTimeRepository();
+
+    @Test
+    void 예약_생성_테스트() {
+        ReservationService reservationService = new ReservationService(reservationRepository,
+                reservationTimeRepository);
+        ReservationTime time = new ReservationTime(1L, LocalTime.of(15, 30));
+        ReservationRequest request = new ReservationRequest(
+                "브라운",
+                LocalDate.of(2023, 8, 5),
+                1L
+        );
+
+        reservationTimeRepository.save(time);
+        Reservation savedReservation = reservationService.createReservation(request);
+
+        assertThat(savedReservation).extracting(
+                Reservation::getId,
+                Reservation::getName,
+                Reservation::getDate,
+                reservation -> reservation.getTime().getStartAt()
+        ).containsExactly(
+                1L,
+                "브라운",
+                LocalDate.of(2023, 8, 5),
+                time.getStartAt()
+        );
+    }
+
+    @Test
+    void 아이디에_해당하는_예약_조회_테스트() {
+        ReservationService reservationService = new ReservationService(reservationRepository,
+                reservationTimeRepository);
+
+        ReservationTime time = new ReservationTime(1L, LocalTime.of(15, 30));
+        ReservationRequest request = new ReservationRequest(
+                "브라운",
+                LocalDate.of(2023, 8, 5),
+                1L
+        );
+
+        reservationTimeRepository.save(time);
+        reservationService.createReservation(request);
+        Long findId = 1L;
+
+        assertThat(reservationService.findReservation(findId)).extracting(
+                Reservation::getId,
+                Reservation::getName,
+                Reservation::getDate,
+                reservation -> reservation.getTime().getStartAt()
+        ).containsExactly(
+                1L,
+                "브라운",
+                LocalDate.of(2023, 8, 5),
+                time.getStartAt()
+        );
+    }
+
+    @Test
+    void 모든_예약_조회_테스트() {
+        ReservationService reservationService = new ReservationService(reservationRepository,
+                reservationTimeRepository);
+
+        ReservationTime timeBrown = new ReservationTime(1L, LocalTime.of(15, 30));
+        ReservationRequest requestBrown = new ReservationRequest(
+                "브라운",
+                LocalDate.of(2023, 8, 5),
+                1L
+        );
+
+        ReservationTime timePobi = new ReservationTime(2L, LocalTime.of(15, 30));
+        ReservationRequest requestPobi = new ReservationRequest(
+                "포비",
+                LocalDate.of(2023, 9, 5),
+                2L
+        );
+
+        reservationTimeRepository.save(timeBrown);
+        reservationTimeRepository.save(timePobi);
+        reservationService.createReservation(requestBrown);
+        reservationService.createReservation(requestPobi);
+
+        assertThat(reservationService.findAllReservations()).hasSize(2);
+    }
+
+    @Test
+    void 아이디에_해당하는_예약_삭제_테스트() {
+        ReservationService reservationService = new ReservationService(reservationRepository,
+                reservationTimeRepository);
+
+        ReservationTime time = new ReservationTime(1L, LocalTime.of(15, 30));
+        ReservationRequest request = new ReservationRequest(
+                "브라운",
+                LocalDate.of(2023, 8, 5),
+                1L
+        );
+
+        reservationTimeRepository.save(time);
+        reservationService.createReservation(request);
+        Long deleteId = 1L;
+
+        reservationService.deleteReservation(deleteId);
+
+        assertThatThrownBy(() -> reservationService.findReservation(deleteId))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+}

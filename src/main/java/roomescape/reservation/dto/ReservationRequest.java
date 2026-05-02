773d20310df6fe30da70d82bf6d715483b53e393
rin <chaerin0916@gmail.com>
2026-05-03 02:39:23 +0900

    refactor: Request DTO에서 @Builder 어노테이션 삭제

--- src/main/java/roomescape/reservation/dto/ReservationRequest.java
+++ src/main/java/roomescape/reservation/dto/ReservationRequest.java
@@ -1,7 +1,5 @@
 package roomescape.reservation.dto;
 
 import java.time.LocalDate;
-import lombok.Builder;
 
-@Builder
 public record ReservationRequest (String name, LocalDate date, Long timeId) { }
--- src/main/java/roomescape/reservationtime/dto/ReservationTimeRequest.java
+++ src/main/java/roomescape/reservationtime/dto/ReservationTimeRequest.java
@@ -1,7 +1,5 @@
 package roomescape.reservationtime.dto;
 
 import java.time.LocalTime;
-import lombok.Builder;
 
-@Builder
 public record ReservationTimeRequest (LocalTime startAt) { }

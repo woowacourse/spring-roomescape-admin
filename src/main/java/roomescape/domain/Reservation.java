6ddc6aaead443bf5851db9d4e00004ac924c643e
JinGoo <zzgg3040@naver.com>
2026-05-02 16:50:55 +0900

    fix: 도메인 엔티티 동등성 비교에서 동일성 비교로 전환

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,23 +1,65 @@
 package roomescape.domain;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
-public record Reservation(
-    Long id,
-    String name,
-    LocalDate date,
-    ReservationTime time
-) {
+public final class Reservation {
+
+    private final Long id;
+    private final String name;
+    private final LocalDate date;
+    private final ReservationTime time;
+
+    public Reservation(Long id, String name, LocalDate date, ReservationTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
 
     public Reservation(String name, LocalDate date, ReservationTime time) {
         this(null, name, date, time);
+    }
+
+    public Reservation withId(long id) {
+        return new Reservation(id, name, date, time);
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public LocalDate date() {
+        return date;
+    }
+
+    public ReservationTime time() {
+        return time;
     }
 
     public Long getTimeId() {
         return time.id();
     }
 
-    public Reservation withId(long id) {
-        return new Reservation(id, name, date, time);
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Reservation other = (Reservation) obj;
+        return this.id != null
+            && Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/main/java/roomescape/domain/ReservationTime.java
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -1,11 +1,17 @@
 package roomescape.domain;
 
 import java.time.LocalTime;
+import java.util.Objects;
 
-public record ReservationTime(
-    Long id,
-    LocalTime startAt
-) {
+public final class ReservationTime {
+
+    private final Long id;
+    private final LocalTime startAt;
+
+    public ReservationTime(Long id, LocalTime startAt) {
+        this.id = id;
+        this.startAt = startAt;
+    }
 
     public ReservationTime(LocalTime startAt) {
         this(null, startAt);
@@ -13,5 +19,31 @@ public record ReservationTime(
 
     public ReservationTime withId(long id) {
         return new ReservationTime(id, startAt);
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public LocalTime startAt() {
+        return startAt;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        ReservationTime other = (ReservationTime) obj;
+        return this.id != null
+            && Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }

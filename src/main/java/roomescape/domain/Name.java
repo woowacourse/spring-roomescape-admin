c81932de54410abd2b667393386a9cb566f7d7d1
yItWorks <poiu7825@pusan.ac.kr>
2026-05-03 00:00:34 +0900

    refactor: 필드 불변 설정

--- src/main/java/roomescape/domain/Name.java
+++ src/main/java/roomescape/domain/Name.java
@@ -3,7 +3,7 @@ package roomescape.domain;
 import java.util.Objects;
 
 public class Name {
-    private String name;
+    private final String name;
 
     public Name(String name) {
         this.name = name;

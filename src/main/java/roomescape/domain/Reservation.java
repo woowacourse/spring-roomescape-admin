c6a5a697e94d03b24f35afc934daaa2443149ff8
wontop02 <wjy2002kr@gmail.com>
2026-05-02 22:01:40 +0900

    feat: 도메인에 검증 로직 추가

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,11 +1,20 @@
 package roomescape.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public class Reservation {
+    @NotNull(message = "[ERROR] id는 비어 있을 수 없습니다.")
     private final Long id;
+
+    @NotBlank(message = "[ERROR] 이름은 비어 있을 수 없습니다.")
     private final String name;
+
+    @NotNull(message = "[ERROR] 날짜는 비어 있을 수 없습니다.")
     private final LocalDate date;
+
+    @NotNull(message = "[ERROR] 예약 시간은 비어 있을 수 없습니다.")
     private final ReservationTime time;
 
     public Reservation(Long id, String name, LocalDate date, ReservationTime time) {
--- src/main/java/roomescape/domain/ReservationTime.java
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -1,9 +1,13 @@
 package roomescape.domain;
 
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 
 public class ReservationTime {
+    @NotNull(message = "[ERROR] id는 비어 있을 수 없습니다.")
     private final Long id;
+
+    @NotNull(message = "[ERROR] 예약 시간은 비어 있을 수 없습니다.")
     private final LocalTime startAt;
 
     public ReservationTime(Long id, LocalTime startAt) {

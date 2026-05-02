65b598f0662155a9ff85f943cfba3b97c9ff95d1
koreaioi <koreaioi@gachon.ac.kr>
2026-05-03 01:45:03 +0900

    test(ReservationServiceTest): cancel 테스트 코드 추가

--- src/test/java/roomescape/service/ReservationServiceTest.java
+++ src/test/java/roomescape/service/ReservationServiceTest.java
@@ -11,7 +11,6 @@ import roomescape.fixture.TestReservationTimeDao;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 
 class ReservationServiceTest {
@@ -101,13 +100,45 @@ class ReservationServiceTest {
             // given
             Long wrongId = Long.MIN_VALUE;
             ReservationService service = generateReservationService(List.of(), List.of());
-            ReservationSaveDto dto = new ReservationSaveDto(name, date, wrongId);
+            ReservationSaveDto saveDto = new ReservationSaveDto(name, date, wrongId);
 
             // when & then
-            Assertions.assertThatThrownBy(() -> service.reserve(dto))
+            Assertions.assertThatThrownBy(() -> service.reserve(saveDto))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining("[ERROR] 존재하지 않는 ReservationTime 입니다.");
         }
+    }
+
+    @Nested
+    class Cancel {
+
+        @Test
+        void 존재하는_ID로_취소하면_저장소에서_제거된다() {
+            // given
+            ReservationService service = generateReservationService(List.of(), List.of(reservationTime));
+            ReservationSaveDto saveDto = new ReservationSaveDto(name, date, reservationTime.getId());
+            Reservation savedReservation = service.reserve(saveDto);
+
+            // when
+            service.cancel(savedReservation.getId());
+
+            // then
+            Assertions.assertThat(service.readAll())
+                    .isEmpty();
+        }
+
+        @Test
+        void 존재하지_않는_ID로_취소하면_예외가_발생한다() {
+            // given
+            Long wrongId = Long.MIN_VALUE;
+            ReservationService service = generateReservationService(List.of(), List.of());
+
+            // when & then
+            Assertions.assertThatThrownBy(() -> service.cancel(wrongId))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("[ERROR] 존재하지 않는 id 이므로, Reservation을 삭제할 수 없습니다.");
+        }
+
     }
 
     private ReservationService generateReservationService(List<Reservation> reservations) {

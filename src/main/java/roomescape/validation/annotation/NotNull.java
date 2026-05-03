bca14949bc9f68cfec0dbee78f3d95d0d0ab78b9
koreaioi <koreaioi@gachon.ac.kr>
2026-05-03 16:20:52 +0900

    feat: @NotNull 커스텀 검증 어노테이션 추가

---
+++ src/main/java/roomescape/validation/annotation/NotNull.java
@@ -0,0 +1,14 @@
+package roomescape.validation.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotNull {
+
+    String message() default "Null 일 수 없습니다.";
+
+}

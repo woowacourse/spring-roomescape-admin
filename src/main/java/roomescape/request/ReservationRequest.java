53c33e42517beb00ae21c1dc0d8f8b92977e598c
sauter001 <lsj457@naver.com>
2026-05-03 11:30:22 +0900

    refactor resevation request의 사용되지 않는 메소드 제거

--- src/main/java/roomescape/request/ReservationRequest.java
+++ src/main/java/roomescape/request/ReservationRequest.java
@@ -7,9 +7,6 @@ import roomescape.domain.ReservationTime;
 import java.time.LocalDate;
 
 public record ReservationRequest(String name, LocalDate date, Long timeId) {
-    public Reservation toReservation(ReservationTime time) {
-        return new Reservation(null, name, date, time);
-    }
 
     public ReservationSaveCommand toSaveCommand() {
         return new ReservationSaveCommand(name, date, timeId);

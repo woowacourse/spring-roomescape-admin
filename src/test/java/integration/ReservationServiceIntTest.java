d9251dfc119e28aa137ab403bc16e919efdd65c7
jihwankim128 <jihwan1535@nate.com>
2026-05-03 17:09:37 +0900

    refactor: 통합 테스트 책임 분리

--- src/test/java/integration/ReservationServiceIntTest.java
+++ src/test/java/integration/ReservationServiceIntTest.java
@@ -2,49 +2,37 @@ package integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import integration.data.ReservationDataSource;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.ActiveProfiles;
-import roomescape.RoomescapeApplication;
 import roomescape.service.ReservationService;
 import roomescape.service.command.ReservationCommand;
 
-@SpringBootTest(classes = {
-        RoomescapeApplication.class,
-        ReservationServiceIntTest.class
-})
-@ActiveProfiles("test")
-class ReservationServiceIntTest {
+class ReservationServiceIntTest extends BaseIntegrationTest {
 
     @Autowired
     private ReservationService reservationService;
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private ReservationDataSource reservationDataSource;
 
     @BeforeEach
     void setUp() {
-        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY FALSE");
+        reservationDataSource.insertReservationTime(LocalTime.of(10, 0));
+    }
 
-        jdbcTemplate.execute("TRUNCATE TABLE reservation");
-        jdbcTemplate.execute("TRUNCATE TABLE reservation_time");
-
-        // H2 - id 초기화
-        jdbcTemplate.execute("ALTER TABLE reservation ALTER COLUMN id RESTART WITH 1");
-        jdbcTemplate.execute("ALTER TABLE reservation_time ALTER COLUMN id RESTART WITH 1");
-
-        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY TRUE");
-
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES (?)", LocalTime.of(11, 0));
+    @AfterEach
+    void tearDown() {
+        reservationDataSource.clearTable();
+        reservationDataSource.clearId();
     }
 
     @Test

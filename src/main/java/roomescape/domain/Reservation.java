03f14a59cdeb6ab51f538f414c85a9113725bae7
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-05-04 02:05:08 +0900

    feat: 예약 객체의 정보에 대한 유효성 검사

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -13,6 +13,9 @@ public class Reservation {
     }
 
     public Reservation(String name, LocalDate date, ReservationTime time) {
+        validateName(name);
+        validateDate(date);
+        validateTime(time);
         this.name = name;
         this.date = date;
         this.time = time;
@@ -20,9 +23,30 @@ public class Reservation {
 
     public Reservation(Long id, String name, LocalDate date, ReservationTime time) {
         this.id = id;
+        validateName(name);
+        validateDate(date);
+        validateTime(time);
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    private void validateName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("예약자명이 유효하지 않습니다.");
+        }
+    }
+
+    private void validateDate(LocalDate date) {
+        if (date == null) {
+            throw new IllegalArgumentException("예약 날짜가 유효하지 않습니다.");
+        }
+    }
+
+    private void validateTime(ReservationTime time) {
+        if (time == null) {
+            throw new IllegalArgumentException("예약 시간이 유효하지 않습니다.");
+        }
     }
 
     public Long getId() {

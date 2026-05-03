3d55ce9981a96e3079a7d617f8cc175fb71448c1
bee9827 <ehfrhfo9494@naver.com>
2026-05-03 21:42:26 +0900

    test: @DirtiesContext 제거, Test환경 격리
    @DirtiesContext는 매번 빈을 새로 불러오기 때문에, 빈 변경이 없는 지금 환경에서는 DB만 초기화 한다.
    이때 DB초기화는 테스트 환경에서만 작동하도록 환경 격리했다.

--- src/test/java/roomescape/controller/ReservationTest.java
+++ src/test/java/roomescape/controller/ReservationTest.java
@@ -9,21 +9,38 @@ import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class ReservationTest {
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    @LocalServerPort
+    private int port;
+
     @BeforeEach
     void setUp() {
+        RestAssured.port = port;
+        namedParameterJdbcTemplate.update("DELETE FROM reservation", new MapSqlParameterSource());
+        namedParameterJdbcTemplate.update("DELETE FROM reservation_time", new MapSqlParameterSource());
+    }
+
+    private Long createTime() {
         Map<String, String> time = new HashMap<>();
         time.put("startAt", "10:00");
-        RestAssured.given().log().all()
+
+        return RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(time)
-                .when().post("/times");
+                .when().post("/times")
+                .then().extract().body().jsonPath().getLong("id");
     }
 
     @Test
@@ -32,24 +49,57 @@ class ReservationTest {
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
-                .statusCode(200)
+                .statusCode(HttpStatus.OK.value())
                 .body("size()", is(0)); // 아직 생성 요청이 없으니 0개
 
-        create();
+        createReservation();
 
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
-                .statusCode(200)
+                .statusCode(HttpStatus.OK.value())
                 .body("size()", is(1));
     }
 
     @Test
     @DisplayName("예약을 추가할 수 있다.")
     void create() {
+        Long timeId = createTime();
         Map<String, Object> reservation = new HashMap<>();
         reservation.put("name", "브라운");
         reservation.put("date", "2023-08-05");
+        reservation.put("timeId", timeId);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservation)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("예약 추가시, 예약시간이 존재하지 않으면 BadRequest을 반환한다. ")
+    void reservationTimeNotExists_returnBadRequest() {
+        Map<String, Object> reservation = new HashMap<>();
+        reservation.put("name", "티온");
+        reservation.put("date", "2026-05-03");
+        reservation.put("timeId", 2);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservation)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("예약 추가시, 날짜 형식이 맞지 않는다면 예외를 반환한다.")
+    void reservationTimeNotExists() {
+        Map<String, Object> reservation = new HashMap<>();
+        reservation.put("name", "티온");
+        reservation.put("date", "2026:05:03");
         reservation.put("timeId", 1);
 
         RestAssured.given().log().all()
@@ -57,17 +107,33 @@ class ReservationTest {
                 .body(reservation)
                 .when().post("/reservations")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
     @Test
     @DisplayName("예약을 삭제할 수 있다.")
     void delete() {
-        create();
+        Long id = createReservation();
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
-                .when().delete("/reservations/1")
+                .when().delete("/reservations/" + id)
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value());
     }
+
+    private Long createReservation() {
+        Long timeId = createTime();
+        Map<String, Object> reservation = new HashMap<>();
+        reservation.put("name", "티온");
+        reservation.put("date", "2026-05-03");
+        reservation.put("timeId", timeId);
+
+        return RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservation)
+                .when().post("/reservations")
+                .then().extract()
+                .body().jsonPath().getLong("id");
+    }
+
 }
--- src/test/java/roomescape/controller/ReservationTimeTest.java
+++ src/test/java/roomescape/controller/ReservationTimeTest.java
@@ -6,15 +6,32 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class ReservationTimeTest {
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        namedParameterJdbcTemplate.update("DELETE FROM reservation", new MapSqlParameterSource());
+        namedParameterJdbcTemplate.update("DELETE FROM reservation_time", new MapSqlParameterSource());
+    }
 
     @Test
     @DisplayName("예약 시간을 추가할 수 있다.")
@@ -33,7 +50,7 @@ class ReservationTimeTest {
     @Test
     @DisplayName("예약시간을 조회할 수 있다.")
     void get() {
-        create();
+        createTime();
         RestAssured.given().log().all()
                 .when().get("/times")
                 .then().log().all()
@@ -44,21 +61,21 @@ class ReservationTimeTest {
     @Test
     @DisplayName("예약시간을 삭제할 수 있다.")
     void delete() {
-        create();
+        Long id = createTime();
         RestAssured.given().log().all()
-                .when().delete("/times/1")
+                .when().delete("/times/" + id)
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value());
     }
 
     @Test
-    @DisplayName("시간 삭제시, 예약이 존재할 경우 예외를 반환한다.")
+    @DisplayName("시간 삭제시, 예약이 존재할 경우 BadRequest를 반환한다.")
     void deleteReservationTime_existsReservation_IllegalArgument() {
-        create();
+        Long id = createTime();
         Map<String, Object> reservation = new HashMap<>();
         reservation.put("name", "브라운");
         reservation.put("date", "2023-08-05");
-        reservation.put("timeId", 1);
+        reservation.put("timeId", id);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -68,8 +85,23 @@ class ReservationTimeTest {
                 .statusCode(HttpStatus.OK.value());
 
         RestAssured.given().log().all()
-                .when().delete("/times/1")
+                .when().delete("/times/" + id)
                 .then().log().all()
                 .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    private Long createTime() {
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10:00");
+
+        return RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .post("/times")
+                .then()
+                .extract()
+                .body()
+                .jsonPath()
+                .getLong("id");
     }
 }

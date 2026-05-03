c6758779755a754b2af8460fb48e5686d41885a9
kangrae <kangrae0501@gmail.com>
2026-05-03 21:21:28 +0900

    test: 예약 관련 api 테스트 추가

--- src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
@@ -1,9 +1,12 @@
 package roomescape.reservation.controller;
 
+import static org.hamcrest.Matchers.is;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,13 +16,91 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationControllerTest {
 
+    @Test
+    void 예약을_추가한다() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", "밀란");
+        params.put("date", "2026-05-03");
+        params.put("timeId", 1);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("name", is("밀란"))
+                .body("date", is("2026-05-03"))
+                .body("time.id", is(1));
+    }
+
+    @Test
+    void 예약_목록을_조회한다() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", "밀란");
+        params.put("date", "2026-05-03");
+        params.put("timeId", 1);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1))
+                .body("[0].name", is("밀란"))
+                .body("[0].date", is("2026-05-03"))
+                .body("[0].time.id", is(1));
+    }
+
+    @Test
+    void 예약을_삭제한다() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", "밀란");
+        params.put("date", "2026-05-03");
+        params.put("timeId", 1);
+
+        Integer id = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .path("id");
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/" + id)
+                .then().log().all()
+                .statusCode(200);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @Test
+    void 존재하지_않는_예약을_삭제하면_404를_응답한다() {
+        RestAssured.given().log().all()
+                .when().delete("/reservations/999")
+                .then().log().all()
+                .statusCode(404);
+    }
+
     @ParameterizedTest(name = "{0}은 1에서 10자 이내의 예약자 이름이 아니다")
     @ValueSource(strings = {"", "12345678901"})
-    void 예약을_추가할_때_이름이_1자에서_10자이내가_아니면_오류를_일으킨다(String name) {
-        Map<String, String> params = new HashMap<>();
+    void 예약을_추가할_때_이름이_1자에서_10자이내가_아니면_400을_응답한다(String name) {
+        Map<String, Object> params = new HashMap<>();
         params.put("name", name);
         params.put("date", "2023-08-05");
-        params.put("time", "15:40");
+        params.put("timeId", 1);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)

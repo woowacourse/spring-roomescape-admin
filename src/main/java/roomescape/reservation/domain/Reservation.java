260e71afce1b05bea00c280411141d1c5c87f95b
rin <chaerin0916@gmail.com>
2026-05-03 12:38:15 +0900

    feat: 도메인 엔티티 내 비즈니스 유효성 검증 로직 추가
    - Reservation/ReservationTime 생성 시 도메인 무결성(이름, 날짜, 시간) 검증 강제
    - 테스트 코드 내 날짜 데이터를 오늘 이후의 날짜가 들어가도록 수정
    - 빌더 패턴 사용 시에도 검증 로직이 수행되도록 커스텀 생성자에 @Builder 적용

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -6,10 +6,49 @@ import lombok.Getter;
 import roomescape.reservationtime.domain.ReservationTime;
 
 @Getter
-@Builder
 public class Reservation {
     private Long id;
     private String name;
     private LocalDate date;
     private ReservationTime time;
+
+    @Builder
+    public Reservation(Long id, String name, LocalDate date, ReservationTime time) {
+        validate(name, date, time);
+
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    private void validate(String name, LocalDate date, ReservationTime time) {
+        validateName(name);
+        validateDate(date);
+        validateReservationTime(time);
+    }
+
+    private void validateReservationTime(ReservationTime time) {
+        if(time == null) {
+            throw new IllegalArgumentException("[ERROR] 시간 정보는 필수입니다.");
+        }
+    }
+
+    private void validateDate(LocalDate date) {
+        if(date == null) {
+            throw new IllegalArgumentException("[ERROR] 예약 날짜는 필수입니다.");
+        }
+        if (date.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("[ERROR] 예약 날짜는 오늘 이후여야 합니다.");
+        }
+    }
+
+    private void validateName(String name) {
+        if(name == null || name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 이름은 필수입니다.");
+        }
+        if(name.length() < 2 || name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 이름은 2자 이상 5자 이하만 가능합니다.");
+        }
+    }
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -81,7 +81,7 @@ public class MissionStepTest {
 
         Map<String, Object> reservation = new HashMap<>();
         reservation.put("name", "브라운");
-        reservation.put("date", "2023-08-05");
+        reservation.put("date", "2026-08-05");
         reservation.put("timeId", 1);
 
         RestAssured.given().log().all()

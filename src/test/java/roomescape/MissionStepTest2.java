a15d1f271175b200cc5cf5ca874e01bc6da0ef13
wonsang.choi <wonsang.choi@e4net.net>
2026-05-03 22:31:03 +0900

    chore: 불필요한 테스트 제거

--- src/test/java/roomescape/MissionStepTest2.java
+++ src/test/java/roomescape/MissionStepTest2.java
@@ -32,21 +32,6 @@ public class MissionStepTest2 {
         }
     }
 
-    /*@Test
-    void DB_조회_API_전환() {
-        jdbcTemplate.update("INSERT INTO reservation (name, date, time) VALUES (?, ?, ?)", "브라운", "2023-08-05", "15:40");
-
-        List<ReservationResponse> reservations = RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200).extract()
-                .jsonPath().getList(".", ReservationResponse.class);
-
-        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
-
-        assertThat(reservations.size()).isEqualTo(count);
-    }*/
-
     @Test
     void DB_추가_삭제_API_전환() {
         long timeId = createTime("10:00");

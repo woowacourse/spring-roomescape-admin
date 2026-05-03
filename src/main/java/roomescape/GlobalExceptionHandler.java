0891d0cf74b75125c416c8212014ff19ef60d66d
구민서 <minseo12379@gmail.com>
2026-05-03 10:28:08 +0900

    [🚀 미션 (방탈출 예약 관리)] 밍구(구민서) 미션 제출합니다. (#435)
    * docs(README): 학습법 작성
    
    * feat: 예약 관리 API 및 도메인 구현
    
    * docs(log-01): 미션 1 학습 로그 작성
    
    * feat: 전역 예외 처리 기능 구현
    
    * test: 존재하지 않는 예약 삭제 시 404 응답 검증 추가
    
    * docs(log-01): Postman 실습 및 전역 예외 처리 학습 로그 추가
    
    * chore: Postman 컬렉션 및 전역 변수 설정 파일 추가

---
+++ src/main/java/roomescape/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package roomescape;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse("요청 수행 중 에러가 발생했습니다"));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse("예상하지 못한 에러가 발생했습니다"));
+    }
+
+    public static class ErrorResponse {
+        private final String message;
+
+        public ErrorResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}

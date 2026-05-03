a7dfac91aa65c139732c6e395a5e171782e4af8f
sauter001 <lsj457@naver.com>
2026-05-03 11:28:59 +0900

    refactor: reservation의 사용되지 않는 메소드 제거

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -3,10 +3,6 @@ package roomescape.domain;
 import java.time.LocalDate;
 
 public record Reservation(Long id, String name, LocalDate date, ReservationTime time) {
-    public boolean hasId(Long id) {
-        return this.id.equals(id);
-    }
-
     public long timeId() {
         return time.id();
     }

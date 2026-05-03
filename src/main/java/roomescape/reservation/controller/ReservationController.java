64e7b5c814e8c2318d48692779159ab8a713e87a
dahxxn <dahyeonhan4529@gmail.com>
2026-05-03 10:33:52 +0900

    refactor(controller): RestController로 변경

--- src/main/java/roomescape/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/reservation/controller/ReservationController.java
@@ -2,17 +2,17 @@ package roomescape.reservation.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.reservation.dto.CreateReservationRequest;
 import roomescape.reservation.dto.ReservationResponse;
 import roomescape.reservation.service.ReservationService;
 
-@Controller
+@RestController
 public class ReservationController {
     private final ReservationService reservationService;
 
--- src/main/java/roomescape/time/controller/TimeController.java
+++ src/main/java/roomescape/time/controller/TimeController.java
@@ -2,17 +2,17 @@ package roomescape.time.controller;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.time.dto.CreateReservationTimeRequest;
 import roomescape.time.dto.ReservationTimeResponse;
 import roomescape.time.service.ReservationTimeService;
 
-@Controller
+@RestController
 public class TimeController {
     private final ReservationTimeService reservationTimeService;
 

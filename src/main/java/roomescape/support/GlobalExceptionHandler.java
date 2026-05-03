5c6298d55a29d68c9d31c7c013a7953eaf77a4b0
Sumin <new.sumyang@gmail.com>
2026-05-03 14:34:19 +0900

    feat: 예상치 못한 예외 핸들링 메서드 추가

--- src/main/java/roomescape/support/GlobalExceptionHandler.java
+++ src/main/java/roomescape/support/GlobalExceptionHandler.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(RoomescapeException.class)
     public ResponseEntity<ErrorResponse> handleRoomescapeException(RoomescapeException exception) {
         final RoomescapeErrorCode errorCode = exception.getErrorCode();
+        return ErrorResponse.of(errorCode);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        final RoomescapeErrorCode errorCode = RoomescapeErrorCode.INTERNAL_SERVER_ERROR;
         return ErrorResponse.of(errorCode);
     }
 }
--- src/main/java/roomescape/support/RoomescapeErrorCode.java
+++ src/main/java/roomescape/support/RoomescapeErrorCode.java
@@ -11,6 +11,7 @@ public enum RoomescapeErrorCode {
     INVALID_RESERVATION_TIME_FORMAT(HttpStatus.BAD_REQUEST, "시간은 HH:MM 형식이어야 합니다."),
     RESERVATION_TIME_NOT_EXIST(HttpStatus.NOT_FOUND, "존재하지 않는 예약 시간대 입니다."),
     RESERVATION_TIME_IN_USE(HttpStatus.CONFLICT, "이미 예약이 존재하는 시간대는 삭제할 수 없습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"서버 내부 오류가 발생했습니다"),
     ;
 
     private final HttpStatus httpStatus;

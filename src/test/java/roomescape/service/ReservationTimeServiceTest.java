0d70db04aef55ec96f91d37313236d8386e739ee
Andrew Kim <38521355+nn98@users.noreply.github.com>
2026-05-03 23:15:52 +0900

    test(service): 수정된 로직과 구조 반영

--- src/test/java/roomescape/service/ReservationTimeServiceTest.java
+++ src/test/java/roomescape/service/ReservationTimeServiceTest.java
@@ -1,14 +1,15 @@
 package roomescape.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.time.LocalTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import roomescape.domain.ReservationTime;
 import roomescape.repository.FakeReservationTimeDao;
-import roomescape.repository.JdbcReservationTimeRepository;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ReservationTimeServiceTest {
 
@@ -17,23 +18,37 @@ class ReservationTimeServiceTest {
     @BeforeEach
     void setUp() {
         FakeReservationTimeDao fakeReservationTimeDao = new FakeReservationTimeDao();
-        JdbcReservationTimeRepository jdbcReservationTimeRepository = new JdbcReservationTimeRepository(
-                fakeReservationTimeDao);
-        reservationTimeService = new ReservationTimeService(jdbcReservationTimeRepository);
+        reservationTimeService = new ReservationTimeService(fakeReservationTimeDao);
     }
 
     @Test
-    @DisplayName("유효한 시간 정보를 통해 새로운 예약 시간을 생성하고 반환한다.")
-    void saveTime_ValidTime_ReturnsSavedReservationTime() {
+    @DisplayName("시간 정보를 입력하여 새로운 예약 시간을 생성하고 반환한다.")
+    void saveTime() {
         ReservationTime reservationTime = reservationTimeService.saveTime(LocalTime.of(10, 0));
         assertThat(reservationTime.startAt()).isEqualTo(LocalTime.of(10, 0));
     }
 
     @Test
-    @DisplayName("존재하는 예약 시간을 식별자를 통해 삭제하면 전체 목록에서 사라진다.")
-    void removeTime_ExistingId_RemovesTimeFromStorage() {
+    @DisplayName("존재하는 예약 시간을 삭제하면 전체 목록에서 사라진다.")
+    void removeTime() {
         ReservationTime reservationTime = reservationTimeService.saveTime(LocalTime.of(10, 0));
         reservationTimeService.removeTime(reservationTime.id());
         assertThat(reservationTimeService.allTimes()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("모든 예약 시간 목록을 조회하여 반환한다.")
+    void allTimes() {
+        reservationTimeService.saveTime(LocalTime.of(10, 0));
+        List<ReservationTime> times = reservationTimeService.allTimes();
+        assertThat(times).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("식별자를 통해 특정 예약 시간 객체를 조회한다.")
+    void findTime() {
+        ReservationTime savedTime = reservationTimeService.saveTime(LocalTime.of(10, 0));
+        ReservationTime foundTime = reservationTimeService.findTime(savedTime.id());
+        assertThat(foundTime.startAt()).isEqualTo(LocalTime.of(10, 0));
     }
 }
